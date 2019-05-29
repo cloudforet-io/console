6003eee22a73ce8ad1a406f64826486a05f9f6b1
wanjin <wanjin@mz.co.kr>
2019-05-29 18:03:52 +0900

    conf eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,31 @@
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+  extends: [
+    'standard',
+    'plugin:vue/recommended'
   ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'html',
+    'standard',
+    'vue'
+  ],
+  env: {
+    browser: true
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/max-attributes-per-line': ['off', {
+      'singleline': 1,
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': false
+      }
+    }]
   }
 }
