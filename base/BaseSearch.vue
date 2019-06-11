30ff946c5a9fbccd54c4a933d83ec463fa7f85c2
wanjin <wanjin@mz.co.kr>
2019-06-11 18:40:00 +0900

    added basesearch, basetags components

---
+++ base/BaseSearch.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-input-group>
+    <b-form-input type="search" placeholder="Search" />
+    <b-input-group-append>
+      <b-button variant="primary">
+        Search
+      </b-button>
+    </b-input-group-append>
+  </b-input-group>
+</template>
+
+<script>
+export default {
+  name: 'BaseSearch'
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
