e7b13067bed67feaba3a17be5dcc5e88fe6eb548
dongyo <dkang@mz.co.kr>
2019-06-26 13:11:14 +0900

    CLOUD-160

--- src/components/base/BaseTree.vue
+++ src/components/base/BaseTree.vue
@@ -1,57 +1,19 @@
 <template>
-  <AppHeader fixed>
-    <SidebarToggler class="d-lg-none" display="md" mobile />
-    <b-link class="navbar-brand" to="/dashboard">
-      <!-- <img class="navbar-brand-full" src="img/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
-      <img class="navbar-brand-zed" src="img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo"> -->
-      [LOGO]
-    </b-link>
-    <SidebarToggler ref="sidebarToggler" class="d-none" display="lg" :default-open="navOpen" />
-    <b-navbar-nav class="d-md-down-none">
-      <SiteMapDropdown />
-      <!-- <b-nav-item class="px-3" to="/identity">
-        Identity
-      </b-nav-item>
-      <b-nav-item class="px-3" to="/inventory">
-        Inventory
-      </b-nav-item>
-      <b-nav-item class="px-3" to="/plugin">
-        Plugin
-      </b-nav-item> -->
-    </b-navbar-nav>
 
-    <b-navbar-nav class="ml-auto">
-      <LanguageDropdown /> &nbsp;&nbsp;&nbsp;
-      <AccountDropdown />
-    </b-navbar-nav>
-  </AppHeader>
 </template>
 
 <script>
-import { Header as AppHeader, SidebarToggler } from '@coreui/vue'
-import SiteMapDropdown from '@/components/dropdown/DPO_001_SiteMapDropdown'
-import AccountDropdown from '@/components/dropdown/DPO_002_AccountDropdown'
-import LanguageDropdown from '@/components/dropdown/DPO_003_LanguageDropdown'
-import { mapState } from 'vuex'
 
 export default {
   name: 'BaseHeader',
   components: {
-    AppHeader,
-    SidebarToggler,
-    LanguageDropdown,
-    SiteMapDropdown,
-    AccountDropdown
+
   },
   computed: {
-    ...mapState('nav', {
-      navOpen: state => state.navOpen
-    })
+
   },
   watch: {
-    navOpen () {
-      this.$refs.sidebarToggler.$el.click()
-    }
+
   }
 }
 </script>
