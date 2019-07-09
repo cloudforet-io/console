64736e30a70a6ad9c0b686653c99a7907735745e
wanjin <wanjin@mz.co.kr>
2019-07-09 18:32:44 +0900

    changed to event driven way

--- base/table/BATB_001_BaseTable.vue
+++ base/table/BATB_001_BaseTable.vue
@@ -6,9 +6,7 @@
           {{ caption }}
         </b-col>
         <b-col cols="10" md="6" xl="7" class="mb-md-0 mb-3">
-          <BaseSearch v-if="searchable" :search-fn="listFn" :query-data="queryData"
-                      :limit="limit" :skip="skip" :sort="sortBy"
-          />
+          <BaseSearch v-if="searchable" :query-data="queryData" @search="onSearch" />
         </b-col>
         <b-col cols="12" md="4" xl="3" class="text-right">
           <b-button type="button" @click.prevent="onPrev">
@@ -21,7 +19,7 @@
             Next
           </b-button>
           <b-button type="button" class="ml-4 refresh-btn"
-                    variant="light" @click.prevent="onRefresh"
+                    variant="light" @click="onRefresh"
           >
             <i class="icon-refresh" />
           </b-button>
@@ -45,6 +43,7 @@
 import BaseSearch from '@/components/base/search/BASR_001_BaseSearch.vue'
 export default {
   name: 'BaseTable',
+  event: ['list'],
   components: {
     BaseSearch
   },
@@ -61,10 +60,6 @@ export default {
     queryData: {
       type: Array,
       default: () => []
-    },
-    listFn: {
-      type: Function,
-      required: true
     },
     hover: {
       type: Boolean,
@@ -96,7 +91,7 @@ export default {
     },
     perPage: {
       type: Number,
-      default: 5
+      default: 10
     },
     dark: {
       type: Boolean,
@@ -114,8 +109,9 @@ export default {
   data () {
     return {
       currentPage: 1,
-      selected: null,
-      sortBy: null
+      selectedRow: null,
+      sortBy: undefined,
+      searchList: []
 
     }
   },
@@ -137,30 +133,34 @@ export default {
             : status === 'Banned' ? 'danger' : 'primary'
     },
     rowClicked (item, idx, target) {
-      if (this.selected) {
-        delete this.selected._rowVariant
-        this.tableData.splice(idx, 1, this.selected)
+      if (this.selectedRow) {
+        delete this.selectedRow._rowVariant
+        this.tableData.splice(idx, 1, this.selectedRow)
       }
-      this.selected = Object.assign({}, item, { _rowVariant: 'success' })
-      this.tableData.splice(idx, 1, this.selected)
+      this.selectedRow = Object.assign({}, item, { _rowVariant: 'success' })
+      this.tableData.splice(idx, 1, this.selectedRow)
       this.rowClickedFn(item, idx, target)
     },
     onPrev () {
       if (this.currentPage <= 1) return
       this.currentPage--
-      this.listFn(this.limit, this.skip, this.sortBy)
+      this.$emit('list', this.limit, this.skip, this.sortBy, this.searchList)
     },
     onNext () {
       if (this.currentPage >= this.maxPage) return
       this.currentPage++
-      this.listFn(this.limit, this.skip, this.sortBy)
+      this.$emit('list', this.limit, this.skip, this.sortBy, this.searchList)
     },
     sortingChanged (ctx) {
       this.sortBy = ctx.sortDesc ? `-${ctx.sortBy}` : ctx.sortBy
     },
     onRefresh () {
       this.currentPage = 1
-      this.listFn(this.limit, this.skip, this.sortBy)
+      this.$emit('list', this.limit, this.skip, this.sortBy, this.searchList)
+    },
+    onSearch (conditionList) {
+      this.searchList = conditionList
+      this.$emit('list', this.limit, this.skip, this.sortBy, conditionList)
     }
   }
 }
