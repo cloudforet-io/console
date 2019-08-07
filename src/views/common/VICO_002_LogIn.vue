cc7aa0b3115c9e4c9f77aa193e2b589d9cb10dc7
wanjin <wanjin@mz.co.kr>
2019-08-07 19:02:41 +0900

    Update VICO_002_LogIn.vue

--- src/views/common/VICO_002_LogIn.vue
+++ src/views/common/VICO_002_LogIn.vue
@@ -241,9 +241,9 @@ export default {
 
   .container {
     position: absolute;
-    top: 30%;
-    left: 70%;
-    margin-left: -50%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 
   .card-group {
