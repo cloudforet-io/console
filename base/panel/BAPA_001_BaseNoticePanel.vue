6c423b0af662270c71d95628b5aa5667b512e71a
dongyo <dkang@mz.co.kr>
2019-08-13 22:46:19 +0900

    Added a rest of Views.

--- base/panel/BAPA_001_BaseNoticePanel.vue
+++ base/panel/BAPA_001_BaseNoticePanel.vue
@@ -2,38 +2,30 @@
   <div class="row">
     <b-col class="col-xs-6 col-sm-6 col-md-6 col-lg-12">
       <b-card class="base summary border-top-0">
-        <slot name="header"/>
-        <slot name="contents"/>
-        <slot name="footer"/>
-
-        <tempate v-if="!useSlot">
-          <div class="alert tab-content-detail-warning" id="server-summary-warning-linux">
-            <h4>
-                <b class="text-primary">
-                  <i :class="selectIconHtml(noticePanelData.header.icon, true)"></i>{{noticePanelData.header.text}}
-                </b>
-            </h4>
-            <div class="mt-15 tab-content-detail-warning-msg">
-              <template v-for="(opt, idx) in noticePanelData.contents">
-
-              </template>
-              1. 네트워크 ACL에서 <span class="text-pink"><b>SSH (TCP 22)</b></span> 및 <span class="text-pink"><b>ICMP</b></span> 포트 오픈 확인
-              <br> 2. 클라우드 인스턴스 인 경우 Security Group 추가 확인
-              <br> 3. 사내 LDAP 연동 또는 리눅스 계정(<span class="text-pink"><b>sdiadmin</b></span>) 생성
-              <br> 4. SUDO NOPASSWD 권한 설정
-              <br> 5.
-              <label class="label label-inverse">Actions &gt; Collect Info</label> 버튼 클릭 후 Linux Collector 실행
-              <br> 6. 리눅스 수집 실패 시 COMMAND 탭에서 로그 확인 (<a href="javascript:void(0)">수집 실패 원인</a>)
-
-              <div class="mt-15">
-                <b class="mr-10">수집 관련 문의:</b> sdi-dev@ncsoft.com
+        <template v-if="useSlot">
+          <slot name="header" />
+          <slot name="contents" />
+          <slot name="footer" />
+        </template>
+        <div v-else id="server-summary-warning-linux" class="alert tab-content-detail-warning">
+          <h4>
+            <b class="text-primary">
+              <i :class="selectIconHtml(selectedHeader.headerIcon, true)" />&nbsp;&nbsp;{{ selectedHeader.text }}
+            </b>
+          </h4>
+          <br>
+          <div class="mt-15 tab-content-detail-warning-msg">
+            <div v-for="(opt, idx) in selectedContents" :key="idx">
+              <div>
+                {{ idx+1 }}.  {{ opt.text }}
               </div>
-
+              <br>
+            </div>
+            <div class="mt-15">
+              <b class="mr-10">{{ selectedFooter.title }}</b> {{ selectedFooter.text }}
             </div>
           </div>
-        </tempate>
-
-
+        </div>
       </b-card>
     </b-col>
   </div>
@@ -48,12 +40,16 @@ export default {
             default: false
         },
         noticePanelData: {
-          type: Object,
-          default: () => {}
-        },
+            type: Object,
+            default: () => {}
+        }
     },
-    methods: {
-
+    data () {
+        return {
+            selectedHeader: this.noticePanelData.header,
+            selectedContents: this.noticePanelData.contents,
+            selectedFooter: this.noticePanelData.footer
+        };
     }
 };
 </script>
