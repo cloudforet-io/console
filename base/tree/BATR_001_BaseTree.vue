e5c46c8264ef8662bdbff800d5f0d3a5dee29f4c
dongyo <dkang@mz.co.kr>
2019-08-29 13:01:13 +0900

    Update Sitation message when user takes action that hasn't allowed.

--- base/tree/BATR_001_BaseTree.vue
+++ base/tree/BATR_001_BaseTree.vue
@@ -55,12 +55,15 @@
 
             <template #rightContainer="{ width }">
               <transition name="panel-trans">
-                <div v-if="hasSelected" 
+                <div v-if="hasSelected"
                      :key="nodeKey"
                      class="panel"
                      :style="{ width: width }"
                 >
                   <slot name="treeSubPanel" />
+                </div>
+                <div v-else class="empty">
+                  <span class="msg">Please, Click a item from left tree Panel.</span>
                 </div>
               </transition>
             </template>
@@ -199,9 +202,13 @@ export default {
     },
     methods: {
         nodeClicked (node) {
-            this.nodeKey = (this.nodeKey !== node.data.id) ? node.data.id : this.nodeKey;
-            this.hasSelected = true;
-            this.$emit('selected', { node: node, treeV: this.$refs.slVueTree });
+            if (!node.data.hasOwnProperty('init')) {
+                this.nodeKey = (this.nodeKey !== node.data.id) ? node.data.id : this.nodeKey;
+                this.hasSelected = true;
+                this.$emit('selected', { node: node, treeV: this.$refs.slVueTree });
+            } else {
+                this.hasSelected = false;
+            }
         },
         showContextMenu (node, event, hasClicked) {
             if (!hasClicked) {
@@ -474,5 +481,14 @@ export default {
   .panel {
     padding: 50px $side-pad $bottom-pad $side-pad;
   }
+  .empty {
+    text-align: left;
+    margin-top: 20px;
+    .msg {
+      color: $darkgray;
+      font-size: 1.3rem;
+      font-weight: 600;
+    }
 
+  }
 </style>
