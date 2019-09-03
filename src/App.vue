088459edb893178eb4f2d8264ccaefdaca941a76
dongyo <dkang@mz.co.kr>
2019-09-04 01:14:41 +0900

    Update Sitation message when user takes action that hasn't allowed.

--- src/App.vue
+++ src/App.vue
@@ -15,22 +15,6 @@ export default {
             apis: null,
         }
     },
-    async beforeMount() {
-        let gapiScript = document.createElement('script');
-        //let isLoaded = false;
-        await gapiScript.setAttribute('src', 'https://apis.google.com/js/platform.js');
-       /* gapiScript.async = true;
-        gapiScript.defer = true;*/
-        await document.head.appendChild(gapiScript);
-        /*gapiScript.onload = (e) => {
-            isLoaded = true;
-        };
-        let gapiInterval = setInterval(() => {
-            if (isLoaded) {
-                clearInterval(gapiInterval);
-            }
-        }, 1000);*/
-    }
 };
 
 </script>
--- src/views/login/only_admin/LOOA_001_AdminOnlyLogIn.vue
+++ src/views/login/only_admin/LOOA_001_AdminOnlyLogIn.vue
@@ -147,6 +147,22 @@ export default {
             password: 'cloudone'
         };
     },
+    async beforeMount() {
+        let gapiScript = document.createElement('script');
+      //let isLoaded = false;
+        await gapiScript.setAttribute('src', 'https://apis.google.com/js/platform.js');
+      /* gapiScript.async = true;
+       gapiScript.defer = true;*/
+        await document.head.appendChild(gapiScript);
+      /*gapiScript.onload = (e) => {
+          isLoaded = true;
+      };
+      let gapiInterval = setInterval(() => {
+          if (isLoaded) {
+              clearInterval(gapiInterval);
+          }
+      }, 1000);*/
+    },
     computed: {
         ...mapGetters('auth', [
             'nextPath'
