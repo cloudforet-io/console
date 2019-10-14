9b057b033d6e2aceb956e0d11b5f122f7830eaf6
sinsky <sinsky@mz.co.kr>
2019-10-14 20:31:28 +0900

    add chart stories

--- base/charts/BACT_001_BaseChart.stories.js
+++ base/charts/BACT_001_BaseChart.stories.js
@@ -1,27 +1,85 @@
-import {withKnobs, number} from '@storybook/addon-knobs/vue';
-import BaseChart from './BACT_001_BaseChart.vue';
+import { withKnobs, number, boolean } from '@storybook/addon-knobs/vue';
+import BaseChart from './BACT_009_BaseChart.vue';
 
 export default {
-    title:'Base/BaseChart',
+    title: 'Base/BaseChart',
     component: BaseChart,
-    decorators:[withKnobs],
+    decorators: [withKnobs],
+};
+var dataset ={
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
 };
 
-export const chart = ()=>({
-    components:{BaseChart},
+export const chart = () => ({
+    components: { BaseChart },
     props:{
-        width:{
-            default:number('width',500,'사이즈')
-        },
-        height:{
-            default: number('height',300,'사이즈')
-        }
+      width:{
+          default:number('width',500)
+      }
     },
-    template:`<BaseChart :width="width" :height="height"></BaseChart>`,
-    data(){
-        return{
-
+    template: `<BaseChart ref="chart" :width="width" :height="height" :data="chartData" v-if="reload"></BaseChart>`,
+    data() {
+        return {
+            height:300,
+            chartData: dataset,
+            reload:true,
         };
     },
 });
 
+export const line = () => ({
+    components: { BaseChart },
+    template: `<BaseChart ref="chart" type="line" :width="width" :height="height" :data="chartData"></BaseChart>`,
+    data() {
+        return {
+            width:500,
+            height:300,
+            chartData: dataset
+        };
+    },
+});
+
+export const bar = () => ({
+    components: { BaseChart },
+    template: `<BaseChart ref="chart" type="bar" :width="width" :height="height" :data="chartData"></BaseChart>`,
+    data() {
+        return {
+            width:500,
+            height:300,
+            chartData: dataset
+        };
+    },
+});
+
+export const radar = () => ({
+    components: { BaseChart },
+    template: `<BaseChart ref="chart" type="radar" :width="width" :height="height" :data="chartData"></BaseChart>`,
+    data() {
+        return {
+            width:500,
+            height:300,
+            chartData: dataset
+        };
+    },
+});
