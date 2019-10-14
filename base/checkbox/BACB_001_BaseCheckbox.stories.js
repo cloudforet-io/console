03d4b623dbb2da0e8e16cbf797787894e111fa04
sinsky <sinsky@mz.co.kr>
2019-10-14 15:21:15 +0900

    fix storybook config

---
+++ base/checkbox/BACB_001_BaseCheckbox.stories.js
@@ -0,0 +1,22 @@
+import { withKnobs, text } from '@storybook/addon-knobs/vue';
+import BaseCheckbox from './BACB_001_BaseCheckbox';
+import {action} from '@storybook/addon-actions';
+export default {
+    title: 'base/BaseCheckBox',
+    component: BaseCheckbox,
+    decorators: [withKnobs]
+};
+
+export const checkbox = () => ({
+    name:'checkbox',
+    components: { BaseCheckbox },
+    template: "<BaseCheckbox @change='change' @input='input'></BaseCheckbox>",
+    data(){
+        return{
+          change:action('change'),
+          input:action('input'),
+        };
+    }
+});
+
+
