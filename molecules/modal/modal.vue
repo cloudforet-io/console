127be845ef749e835dce1a90b61fee3e93091fc2
sinsky <sinsky@mz.co.kr>
2019-10-24 00:43:07 +0900

    [CLOUD-481] modal

---
+++ molecules/modal/modal.vue
@@ -0,0 +1,84 @@
+<template>
+    <div :class="classObject" style="display: block" v-if="visible" role="dialog">
+        <div :class="dialogClassObject" role="document">
+            <div class="modal-content">
+            <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { size_mapping } from './modal_mapping';
+export default {
+    name: 'p-modal',
+    model: {
+        prop: 'visible',
+        event: 'change'
+    },
+    events: ['beforeClose'],
+    props: {
+        visible: {
+            type: Boolean,
+            default: false
+        },
+        fade: {
+            type: Boolean,
+            default: false
+        },
+        scrollable: {
+            type: Boolean,
+            default: true
+        },
+        size: {
+            type: String,
+            default: null,
+            validator: (value) => size_mapping.hasOwnProperty(value)
+        },
+        centered: {
+            type: Boolean,
+            default: false
+        }
+    },
+    computed: {
+        classObject() {
+            return [
+                'modal',
+                { fade: this.fade }
+            ];
+        },
+        dialogClassObject() {
+            return [
+                'modal-dialog',
+                { 'modal-dialog-scrollable': this.scrollable },
+                { 'modal-dialog-centered' : this.centered },
+                size_mapping[this.size]
+            ];
+        }
+    },
+    methods: {
+        show() {
+            if (!this.visible) {
+                /**
+                 * modal visible change
+                 * @event chagne
+                 * @type boolean
+                 */
+                this.$emit('change', true);
+            }
+        },
+        hide() {
+            if (this.visible) {
+                this.$emit('change', false);
+            }
+        },
+        toggle() {
+            this.$emit('change', !this.visible);
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
