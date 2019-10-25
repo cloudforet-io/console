5f3b3b5b34d8f0b4cf50081f263228c89a5a5ab3
sinsky <sinsky@mz.co.kr>
2019-10-25 11:27:06 +0900

    [CLOUD-55] content-modal

--- molecules/modals/Modal.stories.js
+++ molecules/modals/Modal.stories.js
@@ -1,73 +1,90 @@
-import PModal from './Modal';
-import { size_mapping } from './ModalMapping';
-import { number, select,button } from '@storybook/addon-knobs/vue';
-import { autoProps } from '../../../setup/storybook-util';
+import { number, select, button } from '@storybook/addon-knobs/vue';
 import faker from 'faker';
+import { action } from '@storybook/addon-actions';
+import PModal from './Modal.vue';
+import { sizeMapping } from './ModalMapping';
+import { autoProps } from '../../../setup/storybook-util';
 
 export default {
-    title: 'molecules/modals',
-    component: PModal,
-    parameters: {
-        info: {
-            summary: '',
-            components: { PModal }
-        },
-        knobs: {
-            escapeHTML: false
-        }
-    }
+  title: 'molecules/modals',
+  component: PModal,
+  parameters: {
+    info: {
+      summary: '',
+      components: { PModal },
+    },
+    knobs: { escapeHTML: false },
+    centered: { disable: true },
+  },
 };
 
 const data = {
-    visible: true
+  visible: false,
+};
+
+const actions = {
+  shown: action('shown'),
+  hidden: action('hidden'),
 };
 
 export const modal = () => ({
-    components: { PModal },
-    template: `
+  components: { PModal },
+  template: `
+<div>
+<button @click="showAction">모달 열기</button>
 <p-modal
-    ref="Nmodal"
-    v-model="visible" 
-    :scrollable="scrollable" 
+    ref="modal"
     :centered="centered"
     :size="size"
+    :fade="fade"
+    :keyboard="keyboard"
+    :backdrop="backdrop"
+    @shown="shown"
+    @hidden="hidden"
     >
     <p style="min-width: 300px;min-height: 200px;">{{lorem}}</p>
+    <button @click="closeAction">모달 닫기</button>
 </p-modal>
+</div>
+
 `,
-    data() {
-        return {
-            ...data
-        };
+  data() {
+    return {
+      ...data,
+    };
+  },
+
+  props: {
+    loremLength: {
+      default: number('loremLength', 10, {
+        range: true, min: 1, max: 80, step: 10,
+      }),
     },
-    mounted(){
-        button('show',()=>this.showAction());
+    size: {
+      default: select('size', [null, ...Object.keys(sizeMapping)]),
     },
-    props: {
-        loremLength: {
-            default: number('loremLength', 10, { range: true, min: 1, max: 80, step: 10 })
-        },
-        size: {
-            default: select('size', [null, ...Object.keys(size_mapping)])
-        },
-        ...autoProps(PModal, [
-            { name: 'scrollable' },
-            { name: 'centered' }
-        ]),
-        // showBtn:{
-        //     default:button('show',()=>this.showAction())
-        // }
+    ...autoProps(PModal, [
+      { name: 'centered' },
+      { name: 'backdrop' },
+      { name: 'fade' },
+      { name: 'keyboard' },
+    ]),
+    // showBtn:{
+    //     default:button('show',()=>this.showAction())
+    // }
+  },
+  computed: {
+    lorem() {
+      return faker.lorem.lines(this.loremLength);
     },
-    computed: {
-        lorem() {
-            return faker.lorem.lines(this.loremLength);
-        }
+  },
+  methods: {
+    showAction() {
+      this.$refs.modal.show();
     },
-    methods:{
-        showAction(){
-            this.$refs.Nmodal.show();
-        }
-    }
+    closeAction() {
+      this.$refs.modal.hide();
+    },
+    ...actions,
+  },
 });
-
-
