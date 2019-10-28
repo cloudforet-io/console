614528d24ef1247d5fb37e4e9951f37682b1f941
sinsky <sinsky@mz.co.kr>
2019-10-28 13:46:38 +0900

    CLOUD-406 add atoms

---
+++ atoms/table/Tr.vue
@@ -0,0 +1,16 @@
+<template>
+    <tr :class="classObject"><slot></slot></tr>
+</template>
+
+<script>
+import styleMixin from '@/components/atoms/table/styleMixin';
+
+export default {
+    name: 'p-tr',
+    mixins: [styleMixin],
+};
+</script>
+
+<style scoped>
+
+</style>
