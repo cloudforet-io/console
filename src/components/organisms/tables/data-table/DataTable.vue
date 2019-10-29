0529bc3ee172cdf43593db711f1e56385c72d0d2
sinsky <sinsky@mz.co.kr>
2019-10-29 14:18:30 +0900

    select talbe set version

--- src/components/organisms/tables/data-table/DataTable.vue
+++ src/components/organisms/tables/data-table/DataTable.vue
@@ -15,6 +15,7 @@
         <template #head>
             <slot name="head" :fields="fieldsData">
                 <p-tr>
+                    <p-th v-if="selectable"><input type="checkbox" ></p-th>
                     <p-th v-for="(field,index) in fieldsData" :key="index" @click.native="theadClick(field,index,$event)">
                         {{field.name}}
                         <template v-if="sortable">
@@ -29,7 +30,7 @@
         <template #body>
             <slot name="body" :items="items">
                 <template v-for="(item,index) in items">
-                    <slot name="row" :fields="fieldsName" :item="item" >
+                    <slot name="row" :fields="fieldsName" :item="item" :index="index">
                         <p-tr
                             @click.left.native="rowLeftClick( item, index, $event )"
                             @click.right.native="rowRightClick( item, index, $event )"
@@ -38,8 +39,9 @@
                             @mouseout.native="rowMouseOut(item,index, $event)"
                             v-bind="item.hasOwnProperty('vbind') ? item.vbind : null"
                             :key="index">
+                            <p-td v-if="selectable"><input type="checkbox" :value="index" @click.stop @input="inputSelect(index)"></p-td>
                             <template v-for="field in fieldsName">
-                                <slot :name="'col-'+field" :item="item" :value="item[field]" :field="field">
+                                <slot :name="'col-'+field" :item="item" :value="item[field]" :index="index" :field="field">
                                     <p-td >{{item[field]}}</p-td>
                                 </slot>
                             </template>
@@ -73,6 +75,14 @@ export default {
         sortable: {
             type: Boolean,
             default: false,
+        },
+        selectable: {
+            type: Boolean,
+            default: false,
+        },
+        selectIndex: {
+            type: Set,
+            default: () => new Set(),
         },
         sortBy: {
             type: String,
@@ -109,10 +119,19 @@ export default {
             }
             return 'fa-sort-up';
         },
+        isSelected() {
+            return index => this.selectIndex.has(index);
+        },
     },
+
+
     methods: {
         rowLeftClick(item, index, event) {
             this.$emit('rowLeftClick', item, index, event);
+            if (this.selectable) {
+                // this.$refs[`checkbox-${index}`].$el.value = !this.selectIndex.has(index);
+                this.inputSelect(index);
+            }
         },
         rowRightClick(item, index, event) {
             this.$emit('rowRightClick', item, index, event);
@@ -136,6 +155,22 @@ export default {
                 this.$emit('update:sortDesc', !this.sortDesc);
             }
             this.$emit('theadClick', field, index, event);
+        },
+        // checkboxInput(index) {
+        //     if (this.$refs[`checkbox-${index}`].$el.localValue) {
+        //
+        //     }
+        //
+        // },
+        inputSelect(index) {
+            const newSelect = new Set(this.selectIndex);
+            if (this.selectIndex.has(index)) {
+                newSelect.delete(index);
+            } else {
+                newSelect.add(index);
+            }
+            this.$emit('update:selectIndex', newSelect);
+            console.log(this.checkboxData);
         },
     },
 };
