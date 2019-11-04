e6105a0cb536914a61c9ec22fe7f5a0155d8b1f6
dongyo <dkang@mz.co.kr>
2019-11-04 18:42:27 +0900

    Update tree Organism 1. Default Tree 2. Simple Tree

---
+++ src/components/atoms/icons/p-icons/ic_tree_arrow--opened.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_tree_arrow--opened': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 16 16',
+    data: '<path pid="0" _fill="none" d="M0 0h16v16H0z"/><path pid="1" d="M3.625 5.375l3.5 5.25 3.5-5.25z" _fill="#a5acce" _stroke="#a5acce" fill-rule="evenodd"/>'
+  }
+})
---
+++ src/components/atoms/icons/p-icons/ic_tree_no-child.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_tree_no-child': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 16 16',
+    data: '<path pid="0" _fill="none" d="M0 0h16v16H0z"/><path pid="1" data-name="패스 440" d="M5.774 3.253v6h6" _fill="none" _stroke="#a5acce"/>'
+  }
+})
