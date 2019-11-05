0658fb679eae9a7f475df37135e151518c48d909
sinsky <sinsky@mz.co.kr>
2019-11-05 19:46:10 +0900

    [CLOUD-644] add tag input

---
+++ src/components/molecules/forms/TagInput.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="form-row p-tag-input">
+        <p-input-text
+            class="tag-input tag-key-input"
+            :value="tagKey"
+            :disabled="disabled"
+            :placeholder="tagKeyPlaceholder"
+            @input="$emit('update:tagKey',$event)"
+        />
+        <span class="tag-input-split">:</span>
+        <p-input-text
+            class="tag-input tag-value-input"
+            :value="tagValue"
+            :disabled="disabled"
+            :placeholder="tagValuePlaceholder"
+            @input="$emit('update:tagValue',$event)"
+        />
+    </div>
+</template>
+
+<script>
+import PInputText from '@/components/atoms/inputs/TextInput';
+
+export default {
+    name: 'PTagInput',
+    components: { PInputText },
+    props: {
+        tagKey: String,
+        tagValue: String,
+        tagKeyPlaceholder: String,
+        tagValuePlaceholder: String,
+        disabled: Boolean,
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    .tag-input-split{
+        font: 14px/28px Arial;
+        padding-left: 0.125rem;
+        padding-right: 0.125rem;
+    }
+    .tag-input{
+
+        &.tag-key-input{
+            width: 7.5rem;
+        }
+        &.tag-value-input{
+            width: 15rem;
+        }
+    }
+</style>
