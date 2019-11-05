0f49ffc61316d71da08bbc2dc371571f2c50ad35
dongyo <dkang@mz.co.kr>
2019-11-06 02:26:46 +0900

    Added infoPanel, 1. Info Panel

---
+++ organisms/panels/info-panel/InfoPanel.vue
@@ -0,0 +1,35 @@
+<template>
+    <panel-top :panel-title="infoTitle">
+        <template #body>
+            <panel-content :definition-list="contentData" />
+        </template>
+    </panel-top>
+</template>
+
+<script>
+import PanelTop from '@/components/molecules/panel/panel-top/PanelTop';
+import PanelContent from '@/components/molecules/panel/panel-content/PanelContent';
+export default {
+    name: 'InfoPanel',
+    components: {
+        PanelContent,
+        PanelTop,
+    },
+    props: {
+        infoTitle: {
+            type: String,
+            default: 'NoTitle',
+        },
+        contentData: {
+            type: String,
+            default: 'NoTitle',
+        },
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
