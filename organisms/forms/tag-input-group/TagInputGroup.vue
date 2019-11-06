60f8580a86be045ed1e29ae0b652dea3ff726560
sinsky <sinsky@mz.co.kr>
2019-11-06 17:51:40 +0900

    add tag logic

--- organisms/forms/tag-input-group/TagInputGroup.vue
+++ organisms/forms/tag-input-group/TagInputGroup.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="p-tag-input-group">
         <div v-for="(tag,index) in destructTags" :key="index" class="tag-input-form change-form">
-            <p-icon-button v-if="editMode" class="delete-btn" name="ic_delete" />
-
+            <p-icon-button v-if="editMode" class="delete-btn" name="ic_delete"
+                           @click="deleteTag(index)"
+            />
             <p-tag-input
-                :name="tag.key"
+                :name="tag.name"
                 :value="tag.value"
                 :disabled="!editMode"
                 @update:name="updateTag(index,'key',$event )"
@@ -15,14 +16,15 @@
             <p-icon-button
                 class="delete-btn"
                 name="ic_delete"
-                @click="newTagReset"
+                @click="resetTag"
             />
-            <p-tag-input tag-key="" />
+            <p-tag-input :name.sync="newTagName" :value.sync="newTagValue" />
             <p-icon-button
                 class="add-btn"
                 button-style="dark"
-                name="ic_pluse"
-                @click="newTag"
+                name="ic_plus"
+                color="transparent white"
+                @click="addTag"
             />
         </div>
     </div>
@@ -34,11 +36,11 @@ import PTagInput from '@/components/molecules/forms/TagInput';
 import PIconButton from '@/components/molecules/buttons/IconButton';
 
 const mergeTags = tags => _.transform(tags, (result, tag) => {
-    result[tag.key] = tag.value;
+    result[tag.name] = tag.value;
 }, {});
 
-const destruct = tags => _.transform(tags, (result, value, key) => {
-    result.push({ key, value });
+const destruct = tags => _.transform(tags, (result, value, name) => {
+    result.push({ name, value });
 }, []);
 
 export default {
@@ -57,6 +59,8 @@ export default {
     data() {
         return {
             destructTags: destruct(this.tags),
+            newTagName: '',
+            newTagValue: '',
         };
     },
     watch: {
@@ -67,12 +71,27 @@ export default {
         },
     },
     methods: {
-        updateTag(index, position, value) {
-            this.destructTags[index][position] = value;
+        syncTags() {
             this.$emit('update:tags', mergeTags(this.destructTags));
         },
-        resetNewTag() {
-
+        updateTag(index, position, value) {
+            this.destructTags[index][position] = value;
+            this.syncTags();
+        },
+        deleteTag(index) {
+            this.destructTags.splice(index, 1);
+            this.syncTags();
+        },
+        resetTag() {
+            this.newTagName = '';
+            this.newTagValue = '';
+        },
+        addTag() {
+            if (this.newTagName) {
+                this.destructTags.push({ name: this.newTagName, value: this.newTagValue });
+                this.syncTags();
+            }
+            this.resetTag();
         },
     },
 };
@@ -82,7 +101,6 @@ export default {
 .p-tag-input-group{
     display: flex;
     flex-wrap:  wrap;
-    justify-content: space-between;
 }
 .form-row.p-tag-input{
     display: inline-flex;
@@ -105,10 +123,13 @@ export default {
     white-space: nowrap;
 }
 .tag-input-form{
+    display: inline-flex;
     flex-wrap: nowrap;
     white-space: nowrap;
     flex-shrink: 1;
     margin-bottom: 0.5rem;
-    margin-right: 2.5rem;
+    &:not(.new-form){
+        margin-right: 2.5rem;
+    }
 }
 </style>
