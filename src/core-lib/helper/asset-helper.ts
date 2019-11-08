8377dba7cec5e709e8ff00c52221da12bbd794fd
wanjin <wanjin@mz.co.kr>
2019-11-08 10:54:59 +0900

    [CLOUD-536] HorizontalBarChart Component Development

---
+++ src/core-lib/helper/asset-helper.ts
@@ -0,0 +1,20 @@
+import config from '@/lib/config';
+import { isEmpty, some } from 'lodash';
+
+/**
+ * @function
+ * @name assetUrlConverter
+ * @param src
+ * @description Replace assets base url by config
+ */
+export const assetUrlConverter = (src: string) => {
+    const endpoints = config.get('ASSET_PATH');
+    if (isEmpty(endpoints) || !src) return src;
+
+    let url = src;
+    some(endpoints, (dest, origin) => {
+        if (src.startsWith(origin)) url = src.replace(origin, dest);
+        return src.startsWith(origin);
+    });
+    return url;
+};
