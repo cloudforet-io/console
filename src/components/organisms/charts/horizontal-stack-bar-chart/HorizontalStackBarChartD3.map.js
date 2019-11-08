76979d2293512fec9785206c2d90cd8ba20bc50a
wanjin <wanjin@mz.co.kr>
2019-11-08 13:58:43 +0900

    [CLOUD-536] HorizontalBarChart Component Development

--- src/components/organisms/charts/horizontal-stack-bar-chart/HorizontalStackBarChartD3.map.js
+++ src/components/organisms/charts/horizontal-stack-bar-chart/HorizontalStackBarChartD3.map.js
@@ -18,30 +18,13 @@ export const HORIZONTAL_STACK_OPTIONS = Object.freeze({
         },
     },
 });
-//
-// export const SAMPLE_DATA = {
-//     INSERVICE: Math.round(Math.random() * 10),
-//     MAINTENANCE: Math.round(Math.random() * 10),
-//     CLOSED: Math.round(Math.random() * 10),
-// };
 
 
 export const sampleDataGenerator = function () {
-    const length = 1;//Math.round(Math.random() * 10) || 5;
-    const arr = [];
-
-    for (let i = 0; i < length; i++) {
-        arr.push({
-            apples: Math.round(Math.random() * 10),
-            bananas: Math.round(Math.random() * 10),
-            cherries: Math.round(Math.random() * 10),
-            dates: Math.round(Math.random() * 10),
-        });
-    }
-    return arr;
-    // return [
-    //     {
-    //         apples: 15, bananas: 30, cherries: 24, dates: 2,
-    //     },
-    // ];
+    return [{
+        apples: Math.round(Math.random() * 10),
+        bananas: Math.round(Math.random() * 10),
+        cherries: Math.round(Math.random() * 10),
+        dates: Math.round(Math.random() * 10),
+    }];
 };
