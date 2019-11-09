1d240cacc307606c635490c57eb8adfbba6e11fb
wanjin <wanjin@mz.co.kr>
2019-11-09 21:51:39 +0900

    SERVER MODEL, CASUAL LIB IMPORT BUG FIXED

--- src/views/inventory/server/modules/ServerSummary.vue
+++ src/views/inventory/server/modules/ServerSummary.vue
@@ -1,49 +1,50 @@
 <template>
-  <div>
-    <BasePanel :panels="panelData" 
-               @edit="showTagEditModal"
-    />
-    <BaseModal ref="IVSV002_TagEditModal" 
-               :title="tr('TITLE', [tr('BTN_EDIT'), tr('TAG')])"
-               centered
-               size="md"
-               type="primary"
-               interactive
-               @ok="onEditTags"
-               @cancel="hideTagEditModal"
-    >
-      <template #contents>
-        <BaseTag ref="IVSV002_Tags" 
-                 :tag-data="tags" 
-                 editable
-                 align="between"
+    <div>
+        <BasePanel :panels="panelData"
+                   @edit="showTagEditModal"
         />
-      </template>
-    </BaseModal>
-  </div>
+        <BaseModal ref="IVSV002_TagEditModal"
+                   :title="tr('TITLE', [tr('BTN_EDIT'), tr('TAG')])"
+                   centered
+                   size="md"
+                   type="primary"
+                   interactive
+                   @ok="onEditTags"
+                   @cancel="hideTagEditModal"
+        >
+            <template #contents>
+                <BaseTag ref="IVSV002_Tags"
+                         :tag-data="tags"
+                         editable
+                         align="between"
+                />
+            </template>
+        </BaseModal>
+    </div>
 </template>
 
 <script>
+import { server } from '@/views/inventory/server/models/server-model';
+
 const BasePanel = () => import('@/components/base/panel/BasePanel');
 const BaseModal = () => import('@/components/base/modal/BaseModal');
 const BaseTag = () => import('@/components/base/tags/BaseTag');
-import serverModel from '@/views/inventory/server/models/server-model';
 
 export default {
     name: 'ServerSummary',
     components: {
         BasePanel,
         BaseModal,
-        BaseTag
+        BaseTag,
     },
     props: {
         serverData: {
             type: Object,
-            default: () => (serverModel),
-            required: true
-        }
+            default: () => (server),
+            required: true,
+        },
     },
-    data () {
+    data() {
         return {
             sampleNoticePanel: {
                 header: {
@@ -51,61 +52,65 @@ export default {
                         icon: 'fa-exclamation-circle',
                         type: 'l',
                         size: 1,
-                        color: 'primary'
+                        color: 'primary',
                     },
-                    text:'리눅스 정보 추가 수집 필요'
-                }, 
+                    text: '리눅스 정보 추가 수집 필요',
+                },
                 contents: [
-                    { text:' This is sample test' },
-                    { text:'네트워크 ACL에서 SSH (TCP 22) 및 ICMP 포트 오픈 확인' },
-                    { text:'클라우드 인스턴스 인 경우 Security Group 추가 확인' },
-                    { text:'사내 LDAP 연동 또는 리눅스 계정(sdiadmin) 생성' },
-                    { text:'SUDO NOPASSWD 권한 설정' },
-                    { text:'Actions > Collect Info 버튼 클릭 후 Linux Collector 실행' },
-                    { text:'리눅스 수집 실패 시 COMMAND 탭에서 로그 확인 (수집 실패 원인)\n' }
-                ], 
+                    { text: ' This is sample test' },
+                    { text: '네트워크 ACL에서 SSH (TCP 22) 및 ICMP 포트 오픈 확인' },
+                    { text: '클라우드 인스턴스 인 경우 Security Group 추가 확인' },
+                    { text: '사내 LDAP 연동 또는 리눅스 계정(sdiadmin) 생성' },
+                    { text: 'SUDO NOPASSWD 권한 설정' },
+                    { text: 'Actions > Collect Info 버튼 클릭 후 Linux Collector 실행' },
+                    { text: '리눅스 수집 실패 시 COMMAND 탭에서 로그 확인 (수집 실패 원인)\n' },
+                ],
                 footer: {
                     title: '수집 관련 문의:',
-                    text: 'sdi-dev@ncsoft.com'
-                }
-            }
+                    text: 'sdi-dev@ncsoft.com',
+                },
+            },
         };
     },
     computed: {
-        os () {
+        os() {
             return this.serverData.data.os || {};
         },
-        domain () {
+        domain() {
             return this.serverData.data.domain || {};
         },
-        vm () {
+        vm() {
             return this.serverData.data.vm || {};
         },
-        compute () {
+        compute() {
             return this.serverData.data.compute || {};
         },
         // computeSecurityGroups () {
         //     return this.compute.security_groups ? this.compute.security_groups.toString() : '';
         // },
-        poolInfo () {
+        poolInfo() {
             return this.serverData.pool_info || {};
         },
-        base () {
+        base() {
             return this.serverData.data.base || {};
         },
-        regionInfo () {
+        regionInfo() {
             return this.serverData.region_info || {};
         },
-        zoneInfo () {
+        zoneInfo() {
             return this.serverData.zone_info || {};
         },
-        serverInfo () {
+        serverInfo() {
             return [
                 { title: `${this.tr('SERVER')} ${this.tr('ID')}`, contents: this.serverData.server_id, copyFlag: true },
                 { title: this.tr('COL_NM.NAME'), contents: this.serverData.name, copyFlag: true },
-                { title: this.tr('COL_NM.STATE'), state: this.serverData.state, stateType: 'SERVER_STATE', copyFlag: true },
+                {
+                    title: this.tr('COL_NM.STATE'), state: this.serverData.state, stateType: 'SERVER_STATE', copyFlag: true,
+                },
                 { title: this.tr('COL_NM.PRI_IP'), contents: this.serverData.primary_ip_address, copyFlag: true },
-                { title: this.tr('COL_NM.SE_TYPE'), badge: this.serverData.server_type, badgeType: 'SERVER_TYPE', copyFlag: true },
+                {
+                    title: this.tr('COL_NM.SE_TYPE'), badge: this.serverData.server_type, badgeType: 'SERVER_TYPE', copyFlag: true,
+                },
                 { title: this.tr('COL_NM.CORE'), contents: this.base.core, copyFlag: true },
                 { title: this.tr('COL_NM.FQDN'), contents: this.domain.fqdn, copyFlag: true },
                 { title: this.tr('COL_NM.MEMORY'), contents: this.base.memory, copyFlag: true },
@@ -121,75 +126,85 @@ export default {
                 { title: this.tr('COL_NM.LT_BOOT'), contents: this.getDate(this.base.booted_at), copyFlag: true },
                 { title: this.tr('COL_NM.CREATE'), contents: this.getDate(this.base.created_at), copyFlag: true },
                 { title: this.tr('COL_NM.UPDATE'), contents: this.getDate(this.base.updated_at), copyFlag: true },
-                { title: this.tr('COL_NM.DELETE'), contents: this.getDate(this.base.deleted_at), copyFlag: true }
+                { title: this.tr('COL_NM.DELETE'), contents: this.getDate(this.base.deleted_at), copyFlag: true },
             ];
         },
-        vmInfo () {
+        vmInfo() {
             return [
                 { title: `${this.tr('VM')} ${this.tr('COL_NM.ID')}`, contents: this.vm.vm_id, copyFlag: true },
                 { title: `${this.tr('VM')} ${this.tr('COL_NM.NAME')}`, contents: this.vm.vm_name, copyFlag: true },
-                { title: this.tr('COL_NM.PLATFORM'), badge: this.vm.platform_type, badgeType: 'PLATFORM_TYPE',copyFlag: true },
+                {
+                    title: this.tr('COL_NM.PLATFORM'), badge: this.vm.platform_type, badgeType: 'PLATFORM_TYPE', copyFlag: true,
+                },
                 { title: this.tr('COL_NM.IMAGE'), contents: this.vm.image, copyFlag: true },
-                { title: this.tr('COL_NM.CREATE'), contents: this.getDate(this.vm.created_at), copyFlag: true }
+                { title: this.tr('COL_NM.CREATE'), contents: this.getDate(this.vm.created_at), copyFlag: true },
             ];
         },
-        computeInfo () {
+        computeInfo() {
             return [
                 { title: this.tr('COL_NM.INST_ID'), contents: this.compute.instance_id, copyFlag: true },
                 { title: this.tr('COL_NM.KEY_PAIR'), contents: this.compute.keypair, copyFlag: true },
                 { title: this.tr('COL_NM.INST_TYPE'), contents: this.compute.instance_type, copyFlag: true },
                 { title: this.tr('COL_NM.CREATE_BY'), contents: this.compute.created_by_user_id, copyFlag: true },
-                { title: this.tr('COL_NM.SEC_GROUP'), contents: this.compute.security_groups, copyFlag: true }
+                { title: this.tr('COL_NM.SEC_GROUP'), contents: this.compute.security_groups, copyFlag: true },
             ];
         },
-        tag () {
-            let tag = [];
-            for (var key in this.serverData.tags) {
-                tag.push({ 
-                    title: key, 
+        tag() {
+            const tag = [];
+            for (const key in this.serverData.tags) {
+                tag.push({
+                    title: key,
                     contents: this.serverData.tags[key],
-                    copyFlag: true 
+                    copyFlag: true,
                 });
             }
             return tag;
         },
-        tags () {
+        tags() {
             return this.dictToKeyValueArray(this.serverData.tags);
         },
-        panelData () {
+        panelData() {
             return [
-                { 
+                {
                     panelTitle: this.tr('PANEL.BASE_INFO'),
-                    panelIcon: { icon: 'fa-hashtag', type: 'l', size: 1, color: 'primary' },
-                    data: this.serverInfo
+                    panelIcon: {
+                        icon: 'fa-hashtag', type: 'l', size: 1, color: 'primary',
+                    },
+                    data: this.serverInfo,
                 },
-                { 
+                {
                     panelTitle: this.tr('PANEL.VM'),
-                    panelIcon: { icon: 'fa-hashtag', type: 'l', size: 1, color: 'primary' },
-                    data: this.vmInfo
+                    panelIcon: {
+                        icon: 'fa-hashtag', type: 'l', size: 1, color: 'primary',
+                    },
+                    data: this.vmInfo,
                 },
-                { 
+                {
                     panelTitle: this.tr('PANEL.COMPUTE'),
-                    panelIcon: { icon: 'fa-hashtag', type: 'l', size: 1, color: 'primary' },
-                    data: this.computeInfo
+                    panelIcon: {
+                        icon: 'fa-hashtag', type: 'l', size: 1, color: 'primary',
+                    },
+                    data: this.computeInfo,
                 },
                 {
                     panelTitle: this.tr('PANEL.TAG'),
-                    panelIcon: { icon: 'fa-tags', type: 'l', size: 1, color: 'danger' },
+                    panelIcon: {
+                        icon: 'fa-tags', type: 'l', size: 1, color: 'danger',
+                    },
                     data: this.tag,
-                    editable: true
-                }
+                    editable: true,
+                },
             ];
-        }
+        },
     },
     methods: {
-        async onEditTags () {
+        async onEditTags() {
             if (this.$refs.IVSV002_Tags.validate()) {
                 let res = null;
                 try {
                     res = await this.$http.post('/identity/server/update', {
                         server_id: this.serverData.user_id,
-                        tags: this.$refs.IVSV002_Tags.tags
+                        tags: this.$refs.IVSV002_Tags.tags,
                     });
                     this.hideTagEditModal();
                     this.$emit('update', res.data);
@@ -198,16 +213,16 @@ export default {
                 }
             }
         },
-        getDate (timestampObj) {
+        getDate(timestampObj) {
             return timestampObj ? this.getDatefromTimeStamp(timestampObj.seconds, localStorage.getItem('timezone')) : '';
         },
-        showTagEditModal () {
+        showTagEditModal() {
             this.$refs.IVSV002_TagEditModal.showModal();
         },
-        hideTagEditModal () {
+        hideTagEditModal() {
             this.$refs.IVSV002_TagEditModal.hideModal();
-        }
-    }
+        },
+    },
 };
 </script>
 
