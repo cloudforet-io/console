4bc84f54f64c1e3052c139ee055bb13c64d68e0d
sinsky <sinsky@mz.co.kr>
2019-11-14 02:47:57 +0900

    change info panel

--- src/components/molecules/panel/panel-content/PanelContent.stories.js
+++ src/components/molecules/panel/panel-content/PanelContent.stories.js
@@ -1,4 +1,7 @@
+import { ref, computed } from '@vue/composition-api';
 import PanelContent from '@/components/molecules/panel/panel-content/PanelContent';
+import PBadge from '@/components/atoms/badges/Badge';
+import { makeTrDefs } from '@/components/molecules/panel/panel-content/PanelContent.uitl';
 
 export default {
     title: 'molecules/panel/panel-content',
@@ -11,36 +14,59 @@ export default {
     },
 };
 export const panelContents = () => ({
-    components: { PanelContent },
-    template: '<div style="width: 80vw;"><panel-content :definitionList="renderData"></panel-content></div>',
+    components: { PanelContent, PBadge },
+    template: `
+<div style="width: 80vw;">
+    <panel-content :defs="renderData">
+    <template #def-state-format="scope">
+    <p-badge styleType="primary">{{scope.value}}</p-badge>
+</template>
+    </panel-content>
+</div>`,
     data() {
         return {
             renderData: [
                 {
-                    title: 'ID',
-                    contents: {
-                        text: 'Fire Birds',
-                        link: 'www.google.com',
-                    },
+                    name: 'id',
+                    label: 'ID',
+                    value: 'Fire Birds',
                     copyFlag: true,
-                    visible: false,
                 },
                 {
-                    title: 'Name',
-                    contents: {
-                        text: 'Shawn Mandus',
-                    },
-                    visible: false,
+                    name: 'name',
+                    label: 'NAME',
+                    value: 'Shawn Mandus',
                 },
                 {
-                    title: 'Created',
-                    contents: {
-                        text: '2019-09-08 23:11:23',
-                    },
+                    name: 'state',
+                    label: 'state',
+                    value: 'enabled',
                     copyFlag: true,
-                    visible: false,
                 },
             ],
         };
     },
+});
+
+export const trHelper = () => ({
+    components: { PanelContent, PBadge },
+    template: `
+<div style="width: 80vw;">
+    <panel-content :defs="defs">
+    <template #def-state-format="scope">
+    <p-badge styleType="primary">{{scope.value}}</p-badge>
+</template>
+    </panel-content>
+</div>`,
+    setup(props, context) {
+        const sampleDefs = ref([
+            ['name', 'COL_NM.NAME', 'abcd'],
+            ['state', ['COL_NM.STATE', 'en'], 'enabled'],
+            ['primary_ip_address', 'COL_NM.IP', '1.1.1.1', { copyFlag: true }],
+        ]);
+        return {
+            defs: computed(() => makeTrDefs(sampleDefs.value, context.parent)),
+        };
+    },
+
 });
---
+++ src/views/inventory/server/modules/ServerDetail.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p-info-panel info-title="Base Information" />
+        <p-info-panel info-title="VM" />
+        <p-info-panel info-title="Compute" />
+        <p-tag-panel :tags.sync="proxyTags" @confirm="confirm" />
+    </div>
+</template>
+
+<script>
+import PInfoPanel from '@/components/organisms/panels/info-panel/InfoPanel';
+import PTagPanel from '@/components/organisms/panels/tag-panel/TagPanel';
+
+export default {
+    name: 'PServerDetail',
+    components: { PInfoPanel, PTagPanel },
+    pros: {
+        item: Object,
+        tags: Object,
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
