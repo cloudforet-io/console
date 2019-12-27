01f1a43b022de86861c2977a6109f26051b6078d
dongyo <dkang@mz.co.kr>
2019-12-27 17:28:38 +0900

    1.Test Code to review Test.

--- .storybook/config.js
+++ .storybook/config.js
@@ -37,7 +37,7 @@ Vue.use(SvgIcon, {
 })
 
 Vue.prototype.$velocity = velocity;
-//This is Test Code to review
+//This is Test Code to review test
 
 /*****************************************************************
  * This is a Global Bus Event;
