c64dc3cf8266c903eba11c2066c6c4905b8f6885
wanjin <wanjin@mz.co.kr>
2019-12-31 22:41:07 +0900

    [CLOUD-825] CardItem Component Dev

---
+++ src/components/molecules/layouts/pane-layout/PaneLayout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="p-pane-layout" v-on="$listeners">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PPaneLayout',
+};
+</script>
+
+<style lang="scss" scoped>
+.p-pane-layout {
+    background-color: $white;
+    box-shadow: 0 0 8px rgba($primary, 0.08);
+    border: 1px solid $gray3;
+    border-radius: 2px;
+}
+</style>
