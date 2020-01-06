58434de9544be5833fb36d5de21b5ba047d79071
sinsky <sinsky@mz.co.kr>
2020-01-06 23:27:42 +0900

    change p-search

--- molecules/search/Search.vue
+++ molecules/search/Search.vue
@@ -1,37 +1,40 @@
 <template>
-    <div class="row" style="width:100%">
-        <div class="input-group" :class="getColSizer">
-            <p-input-text v-focus.lazy="proxyFocused"
-                          class="form-control py-2 border-right-0 border"
-                          :value="searchText"
-                          :disabled="disabled"
-                          :placeholder="searchPlaceholder"
-                          @input="$emit('update:searchText',$event)"
-                          @keyup.enter="onSearch"
-                          @focus="proxyFocused = true"
-                          @blur="proxyFocused = false"
-            />
-            <slot name="input-text" />
-            <div>
-                <p-button class="search-btn" @click="onSearch">
-                    <p-i color="transparent inherit"
-                         width="1.3rem"
-                         height="1.3rem"
-                         name="ic_search"
-                    />
-                </p-button>
-            </div>
-            <slot name="input-button" />
+    <div class="p-search">
+        <p-input-text v-focus.lazy="proxyFocused"
+                      class="p-search-input"
+                      :class="bindClass"
+                      :value="searchText"
+                      :disabled="disabled"
+                      :placeholder="searchPlaceholder"
+                      @input="$emit('update:searchText',$event)"
+                      @keyup.enter="onSearch"
+                      @focus="proxyFocused = true"
+                      @blur="proxyFocused = false"
+                      @mouseover="onMouseOver"
+                      @mouseout="onMouseOut"
+        />
+        <div class="p-search-btn"
+             :class="bindClass"
+             @mouseover="onMouseOver"
+             @mouseout="onMouseOut"
+        >
+            <p-button class="search-btn" @click="onSearch">
+                <p-i color="transparent inherit"
+                     width="1.3rem"
+                     height="1.3rem"
+                     name="ic_search"
+                />
+            </p-button>
         </div>
     </div>
 </template>
 <script>
 import { focus } from 'vue-focus';
-import { computed } from '@vue/composition-api';
+import { computed, ref } from '@vue/composition-api';
 import PInputText from '@/components/atoms/inputs/TextInput.vue';
 import PI from '@/components/atoms/icons/PI.vue';
 import PButton from '@/components/atoms/buttons/Button.vue';
-import { makeProxy } from '@/lib/compostion-util';
+import { makeProxy, mouseOverState } from '@/lib/compostion-util';
 
 export default {
     name: 'PSearch',
@@ -56,51 +59,55 @@ export default {
         },
         focused: {
             type: Boolean,
-            default: false,
+            default: undefined,
         },
     },
     setup(props, context) {
-        // todo: col를 외부에서 정의하도록 하기 (row,col 제거)
-        const getColSizer = computed(() => `col-${props.size}`);
-        const proxyFocused = makeProxy('focused', props, context.emit);
+        const proxyFocused = (typeof props.focused === 'boolean') ? makeProxy('focused', props, context.emit) : ref(false);
+        const { isMouseOver, onMouseOver, onMouseOut } = mouseOverState(props.disabled);
+        const bindClass = computed(() => ({ 'p-search-board': isMouseOver.value, 'p-search-focus': proxyFocused.value }));
         return {
+            proxyFocused,
             onSearch() {
                 context.emit('onSearch', props.searchText);
             },
-            proxyFocused,
-            getColSizer,
+            onMouseOver,
+            onMouseOut,
+            bindClass,
         };
     },
 };
 </script>
 
 <style lang="scss" scoped>
-    .row {
-        margin: 0;
-        .input-group {
-            padding: 0;
+    .p-search{
+        position: relative;
+        display: inline-flex;
+        flex-wrap: nowrap;
+        width: 100%;
+        .p-search-input{
+            flex-grow: 1;
+            border-right-width: 0px;
+            border-top-right-radius:0;
+            border-bottom-right-radius:0;
         }
-    }
-    .form-control{
-        height: 2rem !important;
-    }
-    .search-btn {
-        min-width: 32px;
-        background-color: white;
-        border-radius: 0px 2px 2px 0px;
-        border-top: 1px solid $gray2;
-        border-right: 1px solid $gray2;
-        border-bottom: 1px solid $gray2;
-    }
+        .p-search-btn{
+            flex-grow: 0;
+            border-radius: 0px 2px 2px 0px;
+            border: 1px solid $gray2;
+            border-left-width: 0px;
+            .search-btn {
+                min-width: 32px;
+                background-color: white;
 
-    .form-control:focus {
-        outline: 0 !important;
-        border-color: initial;
-        box-shadow: none;
-    }
-    .search-btn:hover {
-      border-color: $primary;
-      background-color: $primary;
-      color: white;
+            }
+        }
+        .p-search-board{
+            border-color: $dark;
+        }
+        .p-search-focus{
+            color: $dark;
+            border-color: $dark;
+        }
     }
 </style>
