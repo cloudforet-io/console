67f749e5fa80a567a3b90762dcbb39d44017ab35
sylee <sylee@mz.co.kr>
2020-01-08 18:09:12 +0900

    casual setting

--- src/views/secret/credentials-group/CredentialsGroup.stories.js
+++ src/views/secret/credentials-group/CredentialsGroup.stories.js
@@ -1,24 +1,43 @@
-import { toRefs, reactive } from '@vue/composition-api';
+import { toRefs, reactive, computed } from '@vue/composition-api'
 import { action } from '@storybook/addon-actions';
-import PCredentialsGroup from './CredentialsGroup.template.vue';
+import CredentialsGroup from './CredentialsGroup.template.vue';
+import PToolboxTable from '@/components/organisms/tables/toolbox-table/ToolboxTable.vue'
+import PHorizontalLayout from '@/components/organisms/layouts/horizontal-layout/HorizontalLayout.vue'
+import { eventNames, credentialsGroupSetup } from '@/views/secret/credentials-group/CredentialsGroup.template'
+import { arrayOf } from '@/lib/casual'
+import casual from '@/views/secret/models/credential-model'
+import { mountBusEvent } from '@/lib/compostion-util';
+import CGPEventBus from '@/views/secret/credentials-group/CredentialsGroupEventBus'
 
 export default {
     title: 'view/secret/credentialsGroup',
-    component: PCredentialsGroup,
+    component: CredentialsGroup,
     parameters: {
         info: {
             summary: '',
-            components: { PCredentialsGroup },
+            components: { CredentialsGroup },
         },
     },
 };
 const actions = {};
 export const defaultCase = () => ({
-    components: { PCredentialsGroup },
-    template: '<PCredentialsGroup ></PCredentialsGroup>',
+    template: `${CredentialsGroup.template}`,
+    components: { PToolboxTable, PHorizontalLayout },
+    mixins: [CredentialsGroup],
     setup(props, context) {
+        const CGPEventNames = eventNames;
+        CGPEventNames.getCredentialsGroupList = 'getCredentialsGroupList';
+        CGPEventNames.tagConfirmEvent = 'ServerTagConfirmEvent';
+        CGPEventNames.tagResetEvent = 'resetTagEvent';
+        CGPEventNames.addCredentialsGroup = 'addCredentialsGroup';
+        CGPEventNames.deleteCredentialsGroup = 'deleteCredentialsGroup';
+        CGPEventNames.updateCredentialsGroup = 'updateCredentialsGroup';
+        const state = credentialsGroupSetup(props, context, CGPEventNames);
+        console.log(state);
+        const items = computed(() => arrayOf(state.pageSize, casual._credential_group));
         return {
-            ...actions,
+            ...toRefs(state),
+            items,
         };
     },
 });
--- src/views/secret/credentials-group/CredentialsGroup.template.vue
+++ src/views/secret/credentials-group/CredentialsGroup.template.vue
@@ -22,64 +22,121 @@
                     :loading="loading"
                     :use-spinner-loading="true"
                     :use-cursor-loading="true"
-                    @changePageSize="getCredentialGroups"
-                    @changePageNumber="getCredentialGroups"
-                    @clickRefresh="getCredentialGroups"
-                    @changeSort="getCredentialGroups"
+                    @changePageSize="getCredentialsGroup"
+                    @changePageNumber="getCredentialsGroup"
+                    @clickRefresh="getCredentialsGroup"
+                    @changeSort="getCredentialsGroup"
                 >
-                    <template slot="toolbox-left">
-                        <p-button style-type="primary" @click="clickAdd">
-                            {{ tr('COMMON.BTN_ADD') }}
-                        </p-button>
-                        <PDropdownMenuBtn
-                            id="server-dropdown-btn"
-                            class="left-toolbox-item"
-                            :menu="dropdown"
-                            @click-enable="clickEnable"
-                            @click-disable="clickDisable"
-                            @click-delete="clickDelete"
-                            @click-update="clickUpdate"
-                        >
-                            Action
-                        </PDropdownMenuBtn>
-                        <div class="left-toolbox-item">
-                            <p-search :search-text.sync="searchText" @onSearch="getCredentialGroups" />
-                        </div>
-                    </template>
-                    <template v-slot:col-state-format="{value}">
-                        <p-status v-bind="credentialGroupStateFormatter(value)" />
-                    </template>
+                    <!--                    <template slot="toolbox-left">-->
+                    <!--                        <p-button style-type="primary" @click="clickAdd">-->
+                    <!--                            {{ tr('COMMON.BTN_ADD') }}-->
+                    <!--                        </p-button>-->
+                    <!--                        <PDropdownMenuBtn-->
+                    <!--                            id="server-dropdown-btn"-->
+                    <!--                            class="left-toolbox-item"-->
+                    <!--                            :menu="dropdown"-->
+                    <!--                            @click-enable="clickEnable"-->
+                    <!--                            @click-disable="clickDisable"-->
+                    <!--                            @click-delete="clickDelete"-->
+                    <!--                            @click-update="clickUpdate"-->
+                    <!--                        >-->
+                    <!--                            Action-->
+                    <!--                        </PDropdownMenuBtn>-->
+                    <!--                        <div class="left-toolbox-item">-->
+                    <!--                            <p-search :search-text.sync="searchText" @onSearch="getCredentialGroups" />-->
+                    <!--                        </div>-->
+                    <!--                    </template>-->
+                    <!--                    <template v-slot:col-state-format="{value}">-->
+                    <!--                        <p-status v-bind="credentialGroupStateFormatter(value)" />-->
+                    <!--                    </template>-->
                 </p-toolbox-table>
             </template>
         </p-horizontal-layout>
-        <PTab v-if="isSelectedOne" :tabs="tabs" :active-tab.sync="activeTab">
-            <template #detail="{tabName}">
-                <p-credential-group-detail ref="credentialGroupDetail"
-                                           :item="items[selectIndex[0]]"
-                                           :tag-confirm-event="tagConfirmEvent"
-                                           :tag-reset-event="tagResetEvent"
-                />
-            </template>
-            <template #credentials="{tabName}">
-                <p-credential-group-data
+        <!--        <PTab v-if="isSelectedOne" :tabs="tabs" :active-tab.sync="activeTab">-->
+        <!--            <template #detail="{tabName}">-->
+        <!--                <p-credential-group-detail ref="credentialGroupDetail"-->
+        <!--                                           :item="items[selectIndex[0]]"-->
+        <!--                                           :tag-confirm-event="tagConfirmEvent"-->
+        <!--                                           :tag-reset-event="tagResetEvent"-->
+        <!--                />-->
+        <!--            </template>-->
+        <!--            <template #credentials="{tabName}">-->
+        <!--                <p-credential-group-data-->
 
-                />
-            </template>
-        </PTab>
-    </div>
-</template>
-                </p-toolbox-table>
-            </template>
-        </p-horizontal-layout>
+        <!--                />-->
+        <!--            </template>-->
+        <!--        </PTab>-->
     </div>
 </template>
 
 <script>
-import PToolboxTable from '../../../components/organisms/tables/toolbox-table/ToolboxTable';
+import {
+    reactive, toRefs, ref, computed,
+} from '@vue/composition-api';
+import { requestToolboxTableMetaReactive } from '@/components/organisms/tables/toolbox-table/ToolboxTable.util';
+import { timestampFormatter, getValue, userStateFormatter } from '@/lib/util';
+import PToolboxTable from '@/components/organisms/tables/toolbox-table/ToolboxTable.vue';
+import grpEventBus from '@/views/secret/credentials-group/CredentialsGroupEventBus';
+import { makeTrItems } from '@/lib/view-helper';
+import PHorizontalLayout from '@/components/organisms/layouts/horizontal-layout/HorizontalLayout.vue';
 
+export const CGPTableReactive = parent => reactive({
+    fields: makeTrItems([
+        ['credential_group_id', 'COMMON.ID'],
+        ['name', 'COMMON.NAME'],
+        ['created', 'COMMON.CREATE'],
+    ],
+    parent),
+    multiSelectFields: makeTrItems([
+        ['credential_group_id', 'COMMON.ID'],
+        ['name', 'COMMON.NAME'],
+        ['created', 'COMMON.CREATE'],
+    ],
+    parent),
+    selectIndex: [],
+    items: [],
+    searchText: '',
+    loading: false,
+    toolbox: null, // template refs
+});
+export const eventNames = {
+    tagResetEvent: '',
+    tagConfirmEvent: '',
+    getCredentialsGroupList: '',
+    addCredentialsGroup: '',
+    deleteCredentialsGroup: '',
+    updateCredentialsGroup: '',
+};
+
+export const credentialsGroupSetup = (props, context, eventName) => {
+    const eventBus = grpEventBus;
+    const tableState = CGPTableReactive(context.parent);
+    const state = requestToolboxTableMetaReactive();
+
+    const getCredentialsGroup = () => {
+        console.log('credentialsGroup event');
+        eventBus.$emit(eventName.getCredentialsGroup);
+    };
+    return reactive({
+        ...toRefs(state),
+        ...toRefs(tableState),
+        getCredentialsGroup,
+    });
+};
 export default {
     name: 'CredentialGroup',
-    components: { PToolboxTable },
+    components: { PToolboxTable, PHorizontalLayout },
+    setup(props, context) {
+        const dataBind = reactive({
+            items: computed(() => []),
+        });
+        const state = credentialsGroupSetup(props, context, dataBind.items);
+
+        return {
+            ...toRefs(state),
+            ...toRefs(dataBind),
+        };
+    },
 };
 </script>
 
