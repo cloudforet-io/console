60961b17a33b30d25ee5200b3df68969115fcedb
siyeons <siyeonleeme@gmail.com>
2020-01-09 18:13:30 +0900

    credentialGroup -> cdg로 전체 수정

--- src/views/secret/credentials-group/CredentialsGroup.template.vue
+++ src/views/secret/credentials-group/CredentialsGroup.template.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="credentialsGroup">
+    <div class="cdg">
         <p-horizontal-layout>
             <template #container="{ height }">
                 <p-toolbox-table
@@ -22,50 +22,52 @@
                     :loading="loading"
                     :use-spinner-loading="true"
                     :use-cursor-loading="true"
-                    @changePageSize="getCredentialsGroup"
-                    @changePageNumber="getCredentialsGroup"
-                    @clickRefresh="getCredentialsGroup"
-                    @changeSort="getCredentialsGroup"
+                    @changePageSize="getCdg"
+                    @changePageNumber="getCdg"
+                    @clickRefresh="getCdg"
+                    @changeSort="getCdg"
                 >
-                    <!--                    <template slot="toolbox-left">-->
-                    <!--                        <p-button style-type="primary" @click="clickAdd">-->
-                    <!--                            {{ tr('COMMON.BTN_ADD') }}-->
-                    <!--                        </p-button>-->
-                    <!--                        <PDropdownMenuBtn-->
-                    <!--                            id="server-dropdown-btn"-->
-                    <!--                            class="left-toolbox-item"-->
-                    <!--                            :menu="dropdown"-->
-                    <!--                            @click-enable="clickEnable"-->
-                    <!--                            @click-disable="clickDisable"-->
-                    <!--                            @click-delete="clickDelete"-->
-                    <!--                            @click-update="clickUpdate"-->
-                    <!--                        >-->
-                    <!--                            Action-->
-                    <!--                        </PDropdownMenuBtn>-->
-                    <!--                        <div class="left-toolbox-item">-->
-                    <!--                            <p-search :search-text.sync="searchText" @onSearch="getCredentialGroups" />-->
-                    <!--                        </div>-->
-                    <!--                    </template>-->
-                    <!--                    <template v-slot:col-state-format="{value}">-->
-                    <!--                        <p-status v-bind="credentialGroupStateFormatter(value)" />-->
-                    <!--                    </template>-->
+                    <template slot="toolbox-left">
+                        <p-button style-type="primary">
+                            {{ tr('COMMON.BTN_CRT') }}
+                        </p-button>
+                        <!--                        <PDropdownMenuBtn-->
+                        <!--                            id="server-dropdown-btn"-->
+                        <!--                            class="left-toolbox-item"-->
+                        <!--                            :menu="dropdown"-->
+                        <!--                            @click-enable="clickEnable"-->
+                        <!--                            @click-disable="clickDisable"-->
+                        <!--                            @click-delete="clickDelete"-->
+                        <!--                            @click-update="clickUpdate"-->
+                        <!--                        >-->
+                        <!--                            Action-->
+                        <!--                        </PDropdownMenuBtn>-->
+                        <!--                        <div class="left-toolbox-item">-->
+                        <!--                            <p-search :search-text.sync="searchText" @onSearch="getCdg" />-->
+                        <!--                        </div>-->
+                        <!--                    </template>-->
+                        <!--                    <template v-slot:col-state-format="{value}">-->
+                        <!--                        <p-status v-bind="cdgStateFormatter(value)" />-->
+                        <!--                    </template>-->
+                        <!--                </p-toolbox-table>-->
+                        <!--            </template>-->
+                        <!--        </p-horizontal-layout>-->
+                        <!--        <PTab v-if="isSelectedOne" :tabs="tabs" :active-tab.sync="activeTab">-->
+                        <!--            <template #detail="{tabName}">-->
+                        <!--                <p-cdg-detail ref="cdgDetail"-->
+                        <!--                              :item="items[selectIndex[0]]"-->
+                        <!--                              :tag-confirm-event="tagConfirmEvent"-->
+                        <!--                              :tag-reset-event="tagResetEvent"-->
+                        <!--                />-->
+                        <!--            </template>-->
+                        <!--            <template #credentials="{tabName}">-->
+                        <!--                <p-cdg-data />-->
+                        <!--            </template>-->
+                        <!--        </PTab>-->
+                    </template>
                 </p-toolbox-table>
             </template>
         </p-horizontal-layout>
-        <!--        <PTab v-if="isSelectedOne" :tabs="tabs" :active-tab.sync="activeTab">-->
-        <!--            <template #detail="{tabName}">-->
-        <!--                <p-credential-group-detail ref="credentialGroupDetail"-->
-        <!--                                           :item="items[selectIndex[0]]"-->
-        <!--                                           :tag-confirm-event="tagConfirmEvent"-->
-        <!--                                           :tag-reset-event="tagResetEvent"-->
-        <!--                />-->
-        <!--            </template>-->
-        <!--            <template #credentials="{tabName}">-->
-        <!--                <p-credential-group-data-->
-
-        <!--                />-->
-        <!--            </template>-->
-        <!--        </PTab>-->
     </div>
 </template>
 
@@ -74,13 +76,13 @@ import {
     reactive, toRefs, ref, computed,
 } from '@vue/composition-api';
 import { requestToolboxTableMetaReactive } from '@/components/organisms/tables/toolbox-table/ToolboxTable.util';
-import { timestampFormatter, getValue, userStateFormatter } from '@/lib/util';
+import { timestampFormatter, getValue, cdgStateFormatter } from '@/lib/util';
 import PToolboxTable from '@/components/organisms/tables/toolbox-table/ToolboxTable.vue';
-import grpEventBus from '@/views/secret/credentials-group/CredentialsGroupEventBus';
+import cdgEventBus from '@/views/secret/credentials-group/CredentialsGroupEventBus';
 import { makeTrItems } from '@/lib/view-helper';
 import PHorizontalLayout from '@/components/organisms/layouts/horizontal-layout/HorizontalLayout.vue';
 
-export const CGPTableReactive = parent => reactive({
+export const CdgTableReactive = parent => reactive({
     fields: makeTrItems([
         ['credential_group_id', 'COMMON.ID'],
         ['name', 'COMMON.NAME'],
@@ -102,35 +104,38 @@ export const CGPTableReactive = parent => reactive({
 export const eventNames = {
     tagResetEvent: '',
     tagConfirmEvent: '',
-    getCredentialsGroupList: '',
-    addCredentialsGroup: '',
-    deleteCredentialsGroup: '',
-    updateCredentialsGroup: '',
+    getCdgList: '',
+    addCdg: '',
+    deleteCdg: '',
+    updateCdg: '',
 };
 
-export const credentialsGroupSetup = (props, context, eventName) => {
-    const eventBus = grpEventBus;
-    const tableState = CGPTableReactive(context.parent);
+export const cdgSetup = (props, context, eventName) => {
+    const eventBus = cdgEventBus;
+    const tableState = CdgTableReactive(context.parent);
+
     const state = requestToolboxTableMetaReactive();
 
-    const getCredentialsGroup = () => {
+    const getCdg = () => {
         console.log('credentialsGroup event');
-        eventBus.$emit(eventName.getCredentialsGroup);
+        eventBus.$emit(eventName.getCdgList);
     };
+
     return reactive({
         ...toRefs(state),
         ...toRefs(tableState),
-        getCredentialsGroup,
+        getCdg,
+        ...eventNames,
     });
 };
 export default {
-    name: 'CredentialGroup',
+    name: 'CdgTemplate',
     components: { PToolboxTable, PHorizontalLayout },
     setup(props, context) {
         const dataBind = reactive({
             items: computed(() => []),
         });
-        const state = credentialsGroupSetup(props, context, dataBind.items);
+        const state = cdgSetup(props, context, dataBind.items);
 
         return {
             ...toRefs(state),
