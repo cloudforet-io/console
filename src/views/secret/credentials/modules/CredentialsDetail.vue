c3272176eb6f8201134e2e5a8cd9916a8ce8b8fb
dongyo <dkang@mz.co.kr>
2020-01-16 17:00:20 +0900

    1. Added PBadge for Group

--- src/views/secret/credentials/modules/CredentialsDetail.vue
+++ src/views/secret/credentials/modules/CredentialsDetail.vue
@@ -4,6 +4,15 @@
             <template #def-created_at-format="data">
                 {{ timestampFormatter(data.value) }}
             </template>
+            <template #def-credential_group_id-format="{item}">
+                <div>
+                    <PBadge v-for="(label, idx) in item.credential_groups" :key="idx" class="p-label"
+                            :style-type="'gray2'"
+                    >
+                        {{ getEmptyString(label.name) }}
+                    </PBadge>
+                </div>
+            </template>
         </p-info-panel>
 
         <p-dict-panel ref="dictPanel" :dict.sync="tags" @confirm="confirm" />
@@ -17,12 +26,13 @@ import PDictPanel from '@/components/organisms/panels/dict-panel/DictPanel.vue';
 import { timestampFormatter, arrayFormatter } from '@/lib/util';
 import { mountBusEvent } from '@/lib/compostion-util';
 import { makeTrItems } from '@/lib/view-helper';
+import PBadge from '@/components/atoms/badges/Badge.vue';
 import credentialsEventBus from '@/views/secret/credentials/CredentialsEventBus';
 
 export default {
     name: 'PCredentialsDetail',
     components: {
-        PInfoPanel, PDictPanel,
+        PInfoPanel, PDictPanel, PBadge,
     },
     props: {
         item: {
@@ -35,11 +45,12 @@ export default {
     },
     setup(props, { parent }) {
         const baseDefs = makeTrItems([
-            ['credential_id', 'COMMON.ID'],
-            ['name', 'COMMON.NAME'],
-            ['issue_type', 'COMMON.ISSUE_TYPE'],
-            ['created_at', 'COMMON.CREAT'],
-        ], parent, { copyFlag: true });
+            ['credential_id', 'COMMON.ID', { copyFlag: true }],
+            ['name', 'COMMON.NAME', { copyFlag: true }],
+            ['issue_type', 'COMMON.ISSUE_TYPE', { copyFlag: true }],
+            ['created_at', 'COMMON.CREAT', { copyFlag: true }],
+            ['credential_group_id', 'COMMON.GROUP', { full: true }],
+        ], parent, {});
         const tags = ref({ ...props.tags });
         watch(() => props.item, (value) => {
             tags.value = _.isEmpty(value) ? {} : value.tags;
@@ -48,10 +59,12 @@ export default {
         const resetTag = () => {
             dictPanel.value.reset();
         };
+        const getEmptyString = object => (_.isEmpty(object) ? '' : object);
         mountBusEvent(credentialsEventBus, props.tagResetEvent, resetTag);
         return {
             baseDefs,
             tags,
+            getEmptyString,
             dictPanel,
             confirm(...event) {
                 credentialsEventBus.$emit(props.tagConfirmEvent, props.item.credential_id, ...event);
@@ -62,3 +75,12 @@ export default {
     },
 };
 </script>
+
+<style lang="scss" scoped>
+    .p-label {
+        left-margin: 0.5rem;
+        margin-bottom:5px;
+        margin-right: 0.5rem;
+        color:$dark;
+    }
+</style>
