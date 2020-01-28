285caf57796bdfe315230dab7d9b8ddd0f63e1b5
sinsky <sinsky@mz.co.kr>
2020-01-28 18:29:24 +0900

    add state type

---
+++ src/components/organisms/dynamic-view/dynamic-field/templates/state/index.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import _ from 'lodash';
+import PStatus from '@/components/molecules/status/Status.vue';
+
+export default {
+    name: 'State',
+    functional: true,
+    components: { PStatus },
+    props: {
+        viewOption: {
+            type: Object,
+            default: () => {},
+        },
+        data: {
+            type: String,
+        },
+    },
+    render(h, { props }) {
+        return h(PStatus, {
+            props: {
+                icon: _.get(props.viewOption, ['icon', 'image'], null),
+                iconColor: _.get(props.viewOption, ['icon', 'color'], null),
+                textColor: _.get(props.viewOption, ['text_color'], null),
+                text: props.data,
+            },
+        });
+    },
+};
+</script>
