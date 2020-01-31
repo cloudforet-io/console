b49f9426a41ccd0cb91f5d8524bda7924e65e993
sinsky <sinsky@mz.co.kr>
2020-01-31 23:30:56 +0900

    Add Dynamic View & Change Server Page to Dynimc

--- src/views/inventory/server/modules/ServerDetail.vue
+++ src/views/inventory/server/modules/ServerDetail.vue
@@ -3,11 +3,10 @@
         <p-dynamic-view name="Base Information" view_type="item" :data="item||{}"
                         :data_source="baseDataSource" :root-mode="true"
         />
-        <p-dynamic-view name="VM" view_type="item" :data="item.data||{}"
-                        :data_source="vmDataSource"
-        />
-        <p-dynamic-view name="Compute" view_type="item" :data="item.data||{}"
-                        :data_source="computeDataSource"
+        <p-dynamic-view v-for="dv in item.metadata.details" :key="dv.name" :name="dv.name"
+                        :view_type="dv.view_type||'item'"
+                        :data="item.data||{}"
+                        :data_source="dv.data_source"
         />
         <p-dict-panel ref="dictPanel" :dict.sync="dict" @confirm="confirm" />
     </div>
@@ -94,19 +93,11 @@ export default {
             },
             { name: 'Primary IP', key: 'primary_ip_address' },
             { name: 'Server Type', key: 'server_type' },
-            { name: 'Core', key: 'data.base.core' },
-            { name: 'FQDN', key: 'data.domain' },
-            { name: 'Memory', key: 'data.base.memory' },
             { name: 'OS Type', key: 'os_type' },
-            { name: 'OS Distro', key: 'data.os.os_distro' },
             { name: 'Project', key: 'project' },
-            { name: 'OS Details', key: 'os_detail' },
             { name: 'Region', key: 'region_info.region_id' },
-            { name: 'OS Architecture', key: 'data.os.os_arch' },
             { name: 'Zone', key: 'zone_info.zone_id' },
-            { name: 'Kernel', key: 'data.base.kernel' },
             { name: 'Pool', key: 'pool_info.pool_id' },
-            { name: 'Last Boot Time', key: 'booted_at' },
             {
                 name: 'Created at',
                 key: 'created_at.seconds',
@@ -138,64 +129,6 @@ export default {
                 },
             },
         ];
-        const vmDataSource = [
-            { name: 'ID', key: 'data.vm.vm_id' },
-            { name: 'Name', key: 'data.vm.vm_name' },
-            {
-                name: 'Platform',
-                key: 'data.vm.platform_type',
-                view_type: 'enum',
-                view_option: {
-                    AWS: {
-                        view_type: 'badge',
-                        view_option: {
-                            text_color: 'white',
-                            background_color: '#FFCE02',
-                        },
-                    },
-                    GCP: {
-                        view_type: 'badge',
-                        view_option: {
-                            text_color: 'white',
-                            background_color: '#60B731',
-                        },
-                    },
-                    AZURE: {
-                        view_type: 'badge',
-                        view_option: {
-                            text_color: 'white',
-                            background_color: '#0080FB',
-                        },
-                    },
-                    OPENSTACK: {
-                        view_type: 'badge',
-                        view_option: {
-                            text_color: 'white',
-                            background_color: '#EF3817',
-                        },
-                    },
-                },
-
-            },
-            { name: 'Image', key: 'data.vm.image' },
-        ];
-        const computeDataSource = [
-            { name: 'Instance ID', key: 'data.compute.instance_id' },
-            { name: 'Key Pair', key: 'data.compute.keypair' },
-            { name: 'Instance Type', key: 'data.compute.instance_type' },
-            { name: 'Created By', key: 'data.compute.created_by_user_id' },
-            {
-                name: 'Security Group',
-                key: 'data.compute.security_groups',
-                view_type: 'list',
-                view_option: {
-                    item: {
-                        view_type: 'text',
-                    },
-                },
-
-            },
-        ];
         const dict = ref({ ...props.item.tags });
         watch(() => props.item, (value) => {
             if (value) {
@@ -210,8 +143,6 @@ export default {
 
         return {
             baseDataSource,
-            vmDataSource,
-            computeDataSource,
             dict,
             dictPanel,
             confirm(...event) {
