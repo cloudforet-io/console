7344029672fc96a2aa55d2b8840ed2c29b044523
sinsky <sinsky@mz.co.kr>
2020-02-24 16:35:35 +0900

    [CLOUD-1022] add sync state

--- organisms/tables/toolbox-table/toolset.ts
+++ organisms/tables/toolbox-table/toolset.ts
@@ -1,9 +1,10 @@
 import { reactive } from '@vue/composition-api';
+import { number } from '@storybook/addon-knobs';
 import {
     // eslint-disable-next-line import/named
     DataTablePropsType,
     DataTableSelectState,
-    DataTableState,
+    DataTableState, DataTableSyncType,
     DataTableToolSet, initSelectState,
 } from '@/components/organisms/tables/data-table/toolset';
 import { TableState } from '@/components/molecules/tables/toolset';
@@ -15,40 +16,66 @@ export interface ToolBoxTablePropsType extends DataTablePropsType{
     border?: boolean;
     settingVisible?: boolean;
     refreshVisible?: boolean;
-    pageSize?: Number;
-    allPage?: Number;
-    thisPage?: Number;
-    pageNationValues?: Number[];
+    allPage?: number;
+    pageNationValues?: number[];
     padding?: boolean;
     toolboxBackground?: boolean;
 }
+export interface ToolBoxTableSyncType extends DataTableSyncType {
+    pageSize?: number;
+    thisPage?: number;
+}
 
+export interface ToolBoxTableSetupProps extends ToolBoxTablePropsType, ToolBoxTableSyncType{
+    pagenationVisible: boolean;
+    pageSizeVisible: boolean;
+    shadow: boolean;
+    border: boolean;
+    settingVisible: boolean;
+    refreshVisible: boolean;
+    allPage: number;
+    pageNationValues: number[];
+    padding: boolean;
+    toolboxBackground: boolean;
+    pageSize: number;
+    thisPage: number;
+}
 
 export class ToolboxTableState extends DataTableState {
     public state:ToolBoxTablePropsType;
+
+    public syncState:ToolBoxTableSyncType;
 
     static initToolBoxState:ToolBoxTablePropsType = {
         pagenationVisible: true,
         pageSizeVisible: true,
         shadow: true,
         border: true,
+        allPage: 1,
         settingVisible: false,
         refreshVisible: true,
-        pageSize: 15,
-        allPage: 1,
-        thisPage: 1,
         pageNationValues: undefined,
         padding: true,
         toolboxBackground: true,
     };
 
-    constructor(public initData:object = {}) {
+    static initToolBoxSyncState:ToolBoxTableSyncType = {
+        pageSize: 15,
+        thisPage: 1,
+    };
+
+    constructor(public initData:object = {}, public initSyncData:object = {}) {
         super();
         this.state = reactive({
             ...TableState.initTableState,
             ...DataTableState.initDataTableState,
             ...ToolboxTableState.initToolBoxState,
             ...this.initData,
+        });
+        this.syncState = reactive({
+            ...DataTableState.initDataTableSyncState,
+            ...ToolboxTableState.initToolBoxSyncState,
+            ...this.initSyncData,
         });
     }
 }
@@ -57,8 +84,12 @@ export class ToolboxTableState extends DataTableState {
 export class ToolboxTableToolSet extends ToolboxTableState implements DataTableToolSet {
     public selectState:DataTableSelectState;
 
-    constructor(public initData:object = {}) {
-        super(initData);
+    constructor(public initData:object = {}, public initSyncData:object = {}) {
+        super(initData, initSyncData);
         this.selectState = initSelectState(this.state);
+    }
+
+    setAllPage(totalCount:number) {
+        this.state.allPage = Math.ceil(totalCount / (this.syncState.pageSize as number)) || 1;
     }
 }
