44b9f15da676cd4daed9db54332e24a9512a8324
sinsky <sinsky@mz.co.kr>
2020-03-03 01:06:48 +0900

    fix google oauth

--- src/views/sign-in/templates/google_oauth2/index.vue
+++ src/views/sign-in/templates/google_oauth2/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="user-info">
         <div class="g-signin2">
-            <div id="g-signin-btn" style="width: 70%;"  />
+            <div id="g-signin-btn" style="width: 70%;" />
         </div>
         <div class="btn-divider">
             OR
@@ -34,7 +34,60 @@ export default defineComponent({
     components: {
         PButton,
     },
-};
+    setup(props, context) {
+        const vm = getCurrentInstance()as CombinedVueInstance<any, any, any, any, any>;
+        const login = async (userId, param) => {
+            console.debug('start oauth login');
+            const response = await vm.$http.post('/identity/token/issue', {
+                credentials: param,
+                domain_id: vm.$ls.domain.state.domainId,
+            });
+            context.emit('onLogin', userId, response.data);
+        };
+        const onLogIn = async (googleUser) => {
+            const profile = googleUser.getBasicProfile();
+            const userId = profile.getEmail();
+            const param = {
+                // eslint-disable-next-line camelcase
+                access_token: googleUser.getAuthResponse().access_token,
+            };
+            const auth2 = await gapi.auth2.getAuthInstance();
+            if (!auth2.isSignedIn.get()) {
+                return;
+            }
+            auth2.disconnect();
+            await login(userId, param);
+        };
+        const goToAdmin = () => {
+            vm.$router.push({ name: 'AdminLogin' });
+        };
+        onMounted(async () => {
+            gapi.load('auth', () => {
+                const auth2 = gapi.auth2.init({
+                    // eslint-disable-next-line camelcase
+                    client_id: vm.$ls.domain.state.pluginOption.client_id,
+                    // eslint-disable-next-line camelcase
+                    fetch_basic_profile: false,
+                    scope: 'profile',
+                });
+
+                gapi.signin2.render('g-signin-btn', {
+                    scope: 'email',
+                    width: 300,
+                    height: 50,
+                    longtitle: true,
+                    theme: 'dark',
+                    onsuccess: onLogIn,
+                    onfailure: null,
+                });
+            });
+        });
+        return {
+            goToAdmin,
+            onLogIn,
+        };
+    },
+});
 </script>
 
 <style lang="postcss" scoped>
