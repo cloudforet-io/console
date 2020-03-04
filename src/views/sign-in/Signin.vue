3ff683e286cf9e4ddbd929e0d3c8b9d759b7a632
siyeons <siyeonleeme@gmail.com>
2020-03-04 18:21:47 +0900

    Fix logo and wrapper css in login view

--- src/views/sign-in/Signin.vue
+++ src/views/sign-in/Signin.vue
@@ -2,9 +2,7 @@
     <div>
         <div class="flex items-center text-center h-screen w-full wrapper default-theme">
             <div id="login-container" class="max-w-md mx-auto justify-center bg-white lg sm:max-w-xl md:max-w-sm md:mx-auto">
-                <div class="logo">
-                    <img src="@/assets/images/brand/brand_logo.png">
-                </div>
+                <img id="logo" src="@/assets/images/brand/brand_logo.png">
                 <div class="header">
                     <p id="title">
                         Sign in to SPACEONE
@@ -104,8 +102,11 @@ export default defineComponent({
     .wrapper {
         position: absolute;
         width: 100%;
-        height: 100%;
+        height: auto;
         top: 0;
+        bottom: 0;
+        overflow: no-display;
+        -webkit-overflow-scrolling: touch;
         @include background-theme('default-theme', $white);
         @include background-theme('color-theme', $gray);
         @include background-theme('image-theme', url('~@/assets/images/landing/cloudone_console_sign-in_bg.jpg'));
@@ -117,11 +118,13 @@ export default defineComponent({
         padding: 24px 16px 10px;
     }
 
-    .logo {
+    #logo {
         display:inline-block;
         text-align: center;
         vertical-align: middle;
         margin: 0 auto;
+        width: 100px;
+        height: 100px;
     }
 
     .header{
