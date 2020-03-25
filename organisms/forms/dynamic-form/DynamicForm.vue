b39017d6afd37b0600e3bbbfb1c0598b30bc1ef9
WANJIN <wanjin@megazone.com>
2020-03-25 11:37:28 +0900

    Applied fluentapi to Collect Data Modal
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- organisms/forms/dynamic-form/DynamicForm.vue
+++ organisms/forms/dynamic-form/DynamicForm.vue
@@ -45,10 +45,10 @@
     </div>
 </template>
 
-<script>
+<script lang="ts">
 import _ from 'lodash';
 import {
-    ref, computed, reactive, toRefs, watch,
+    ref, computed, reactive, toRefs, defineComponent,
 } from '@vue/composition-api';
 import {
     formValidation, makeProxy, requiredValidation,
@@ -159,7 +159,7 @@ export const setValidation = (forms, values) => {
     };
 };
 
-export default {
+export default defineComponent({
     name: 'PDynamicForm',
     events: ['input', 'change'],
     components: {
@@ -198,7 +198,7 @@ export default {
             ...changeActions,
         };
     },
-};
+});
 </script>
 
 <style lang="postcss" scoped>
