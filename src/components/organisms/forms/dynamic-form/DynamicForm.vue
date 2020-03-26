239c8c0fdb776e9f163f25904987fe590a65137d
Wanjin Noh <wanjin@megazone.com>
2020-03-26 15:00:35 +0900

    build fail bug fixed
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- src/components/organisms/forms/dynamic-form/DynamicForm.vue
+++ src/components/organisms/forms/dynamic-form/DynamicForm.vue
@@ -48,10 +48,10 @@
 <script lang="ts">
 import _ from 'lodash';
 import {
-    ref, computed, reactive, toRefs, defineComponent,
+    computed, reactive, toRefs, defineComponent,
 } from '@vue/composition-api';
 import {
-    formValidation, makeProxy, requiredValidation,
+    formValidation, requiredValidation,
 } from '@/lib/compostion-util';
 
 
