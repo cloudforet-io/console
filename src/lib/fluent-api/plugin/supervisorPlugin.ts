cfe0adda7bfc88dac3a0865203128fe0081e0c3e
sinsky <sinsky@magazone.com>
2020-04-09 21:42:27 +0900

    add plugin page
    Signed-off-by: sinsky <sinsky@magazone.com>

--- src/lib/fluent-api/plugin/supervisorPlugin.ts
+++ src/lib/fluent-api/plugin/supervisorPlugin.ts
@@ -1,11 +1,12 @@
 /* eslint-disable camelcase */
+// eslint-disable-next-line import/no-cycle
 import {
     GetAction, ListAction,
     Resource,
-    ResourceActions,
+    ResourceActions, SubMultiItemAction,
 } from '@/lib/fluent-api/toolset';
 import {
-    ListType,  Tags, TimeStamp,
+    ListType, Tags, TimeStamp,
 } from '@/lib/fluent-api/type';
 
 const idField = 'plugin_id';
@@ -14,17 +15,29 @@ interface IdParameter {
 }
 
 export interface SupervisorPluginModel extends IdParameter, Tags {
-    version: string
-    state :string
-    endpoint:string
+    version: string;
+    state: string;
+    endpoint: string;
+    managed: boolean;
+    supervisor_id: string;
+    supervisor_name: string;
 }
 
 export type SupervisorListResp = ListType<SupervisorPluginModel>
 
 class List extends ListAction<any, SupervisorListResp> {}
 
+class Recovery extends SubMultiItemAction<any, any> {
+    path='recover'
+
+    idField = 'supervisor_id'
+
+    subIdsField = 'plugins'
+}
 export default class SupervisorPlugin extends Resource implements ResourceActions<'list'> {
     protected name = 'supervisor/plugin';
 
     list() { return new List(this.baseUrl); }
+
+    recovery() { return new Recovery(this.baseUrl); }
 }
