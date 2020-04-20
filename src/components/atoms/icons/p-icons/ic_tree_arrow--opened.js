ace76132b6772ddf51faad81ee7dbbc45790d88d
sinsky <sinsky@magazone.com>
2020-04-21 00:36:57 +0900

    fix icon style

--- src/components/atoms/icons/p-icons/ic_tree_arrow--opened.js
+++ src/components/atoms/icons/p-icons/ic_tree_arrow--opened.js
@@ -7,10 +7,6 @@ icon.register({
     width: 16,
     height: 16,
     viewBox: '0 0 16 16',
-<<<<<<< HEAD
-    data: '<path pid="0" d="M4 5.5l4 5.3 4-5.3z"/>'
-=======
     data: '<path pid="0" _fill="#202433" d="M4 5.5l4 5.3 4-5.3z"/>'
->>>>>>> CLOUD-1497-change-table-style
   }
 })
--- src/components/atoms/icons/p-icons/ic_tree_arrow.js
+++ src/components/atoms/icons/p-icons/ic_tree_arrow.js
@@ -7,10 +7,6 @@ icon.register({
     width: 16,
     height: 16,
     viewBox: '0 0 16 16',
-<<<<<<< HEAD
-    data: '<path pid="0" d="M5.5 4v8l5.3-4-5.3-4zm1 2l2.7 2-2.7 2V6z"/>'
-=======
     data: '<path pid="0" d="M5.5 4v8l5.3-4-5.3-4zm1 2l2.7 2-2.7 2V6z" _fill="#202433"/>'
->>>>>>> CLOUD-1497-change-table-style
   }
 })
