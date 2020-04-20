d8cd09a42b17808354c43a9dae65ed32587d748d
siyeon <sylee@megazone.com>
2020-04-21 00:17:53 +0900

    Upload assets
    Signed-off-by: siyeon <sylee@megazone.com>

--- atoms/icons/p-icons/ic_table_sort.js
+++ atoms/icons/p-icons/ic_table_sort.js
@@ -7,6 +7,6 @@ icon.register({
     width: 16,
     height: 16,
     viewBox: '0 0 24 24',
-    data: '<path pid="0" _fill="none" d="M0 0h24v24H0z"/><path pid="1" d="M8 14l4 4 4-4H8zM8 10l4-4 4 4H8z" fill-rule="evenodd" clip-rule="evenodd" _fill="#dcdde2"/>'
+    data: '<path pid="0" d="M9 10l3 4 3-4H9z" _fill="none" _stroke="#a7a9b2" stroke-linejoin="round"/>'
   }
 })
