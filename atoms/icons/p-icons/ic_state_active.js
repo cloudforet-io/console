200ffd239428d5cb9dfd82e70721ca930a2ad0c0
siyeon <sylee@megazone.com>
2020-04-23 14:45:45 +0900

    Upload Asset and change sitemap icon
    Signed-off-by: siyeon <sylee@megazone.com>

---
+++ atoms/icons/p-icons/ic_state_active.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_state_active': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 16 16',
+    data: '<path pid="0" d="M13.7 1.9l-8.4 8.7-3.2-3.1-2.1 2 2.6 2.7 2.6 2.6 2.6-2.6L16 4l-2.3-2.1z" _fill="#60b731"/>'
+  }
+})
