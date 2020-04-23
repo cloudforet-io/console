c6680f0d1fc50d6cdf590c50baeb44294c77b5d4
siyeon <sylee@megazone.com>
2020-04-23 14:45:45 +0900

    Upload Asset and change sitemap icon
    Signed-off-by: siyeon <sylee@megazone.com>

---
+++ src/components/atoms/icons/p-icons/ic_state_duplicated.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_state_duplicated': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 16 17',
+    data: '<defs><path pid="0" id="svgicon_ic_state_duplicated_a" d="M0 .5h16v16H0z"/></defs><clipPath id="svgicon_ic_state_duplicated_b"><use xlink:href="#svgicon_ic_state_duplicated_a" overflow="visible"/></clipPath><g clip-path="url(#svgicon_ic_state_duplicated_b)"><path pid="1" d="M15.8 13.5c.5.9-.1 2-1.2 2H1.3c-1 0-1.7-1.1-1.2-2L6.8 2.2c.5-.9 1.8-.9 2.3 0l6.7 11.3zM8 11.2c-.7 0-1.3.6-1.3 1.3s.6 1.3 1.3 1.3c.7 0 1.3-.6 1.3-1.3s-.6-1.3-1.3-1.3zM6.8 6.7l.2 3.7c0 .2.2.3.3.3h1.3c.2 0 .3-.1.3-.3l.2-3.7c0-.2-.1-.3-.3-.3H7.1c-.2-.1-.3.1-.3.3z" _fill="#ffce02"/></g>'
+  }
+})
