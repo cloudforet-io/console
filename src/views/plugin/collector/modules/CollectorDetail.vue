fb06f5344347518a216e65430a869aa55b24ee6a
Wanjin Noh <wanjin@megazone.com>
2020-05-08 13:10:51 +0900

    Collector Detail updated
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- src/views/plugin/collector/modules/CollectorDetail.vue
+++ src/views/plugin/collector/modules/CollectorDetail.vue
@@ -1,6 +1,7 @@
 <template>
-    <p-dynamic-details :details="details"
-                       :data="data"
+    <s-dynamic-layout :key="layout.name" :layout="layout"
+                      v-bind="layout"
+                      :api="api"
     />
 </template>
 
@@ -11,24 +12,25 @@ import {
 import PDynamicDetails from '@/components/organisms/dynamic-view/dynamic-details/DynamicDetails.vue';
 import SDynamicLayout from '@/components/organisms/dynamic-view/dynamic-layout/SDynamicLayout.vue';
 import { dateTimeViewType } from '@/lib/data-source';
+import { fluentApi } from '@/lib/fluent-api';
 
 
 export default defineComponent({
     name: 'CollectorDetail',
     components: {
         PDynamicDetails,
+        SDynamicLayout,
     },
     props: {
-        data: {
-            type: Object,
-            default: () => ({}),
+        collectorId: {
+            type: String,
+            required: true,
         },
     },
     setup(props, { parent }) {
-        console.debug('data', props.data);
         const vm: any = getCurrentInstance();
         const state = reactive({
-            dataSource: computed(() => [
+            baseInfoFields: computed(() => [
                 { name: vm.$t('WORD.ID'), key: 'collector_id' },
                 { name: vm.$t('COMMON.NAME'), key: 'name' },
                 { name: vm.$t('SERVICE.PROVIDER'), key: 'provider' },
@@ -47,25 +49,40 @@ export default defineComponent({
                 { name: vm.$t('COMMON.LAST_COL'), key: 'last_collected_at.seconds', ...dateTimeViewType },
                 { name: vm.$t('COMMON.CREATED'), key: 'created_at.seconds', ...dateTimeViewType },
             ]),
-            filtersDataSource: computed(() => [
+            filtersFields: computed(() => [
                 { name: vm.$t('COMMON.NAME'), key: 'name' },
                 { name: vm.$t('WORD.KEY'), key: 'key' },
                 { name: vm.$t('COMMON.TYPE'), key: 'type' },
                 { name: vm.$t('COMMON.RESOURCE'), key: 'resource_type' },
             ]),
-            details: computed(() => [{
-                name: vm.$t('WORD.BASE_INFO'),
-                // eslint-disable-next-line camelcase
-                data_source: state.dataSource,
-            }, {
-                name: vm.$t('PANEL.FILTER_FORMAT'),
-                // eslint-disable-next-line camelcase
-                data_source: state.filtersDataSource,
-                // eslint-disable-next-line camelcase
-                key_path: 'plugin_info.options.filter_format',
-                // eslint-disable-next-line camelcase
-                view_type: 'simple-table',
-            }]),
+            layouts: computed(() => ([
+                {
+                    name: vm.$t('WORD.BASE_INFO'),
+                    type: 'item',
+                    options: {
+                        fields: state.baseInfoFields,
+                    },
+                },
+                {
+                    name: vm.$t('PANEL.FILTER_FORMAT'),
+                    type: 'simple-table',
+                    options: {
+                        // eslint-disable-next-line camelcase
+                        root_path: 'plugin_info.options.filter_format',
+                        fields: state.filtersFields,
+                    },
+                },
+            ])),
+            layout: computed(() => ({
+                name: 'Base Information',
+                type: 'list',
+                options: {
+                    layouts: state.layouts,
+                },
+            })),
+            api: computed(() => ({
+                resource: fluentApi.inventory().collector().get().setId(props.collectorId),
+            })),
         });
         return {
             ...toRefs(state),
