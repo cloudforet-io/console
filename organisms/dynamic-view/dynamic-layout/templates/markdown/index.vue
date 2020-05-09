60b4818452a012a99a0b7994407ffce255721510
sinsky <sinsky@magazone.com>
2020-05-10 02:32:25 +0900

    add api mode

--- organisms/dynamic-view/dynamic-layout/templates/markdown/index.vue
+++ organisms/dynamic-view/dynamic-layout/templates/markdown/index.vue
@@ -8,11 +8,18 @@
 </template>
 
 <script lang="ts">
-import { computed, onMounted, watch } from '@vue/composition-api';
+import {
+    computed, onMounted, reactive, watch,
+} from '@vue/composition-api';
 import PPanelTop from '@/components/molecules/panel/panel-top/PanelTop.vue';
 import nunjucks from 'nunjucks';
 import hljs from 'highlight.js/lib/core';
 import 'highlight.js/scss/dracula.scss';
+import { checkCanGetData, DynamicLayoutApiProp } from '@/components/organisms/dynamic-view/dynamic-layout/toolset';
+import { GetAction, ResourceActions } from '@/lib/fluent-api';
+import _ from 'lodash';
+import { useStore } from '@/store/toolset';
+import construct from '@babel/runtime-corejs2/helpers/esm/construct';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const marked = require('marked');
 
@@ -42,14 +49,84 @@ export default {
             type: Boolean,
             default: true,
         },
+        language: {
+            type: String,
+            default: 'en',
+        },
     },
     setup(props) {
         // const regx = new RegExp('<pre(?:.|\\n)*(?<codeblock><code.*>(?<code>(?:.|\\n)*)</code>)(?:.|\\n)*</pre>');
+        const state = reactive({
+            isApiMode: computed(() => !!props.api),
+            data: null,
+        });
 
+
+        const getData = async () => {
+            if (checkCanGetData(props)) {
+                let action: GetAction<any, any>;
+                if (props.api?.resource instanceof GetAction) {
+                    action = props.api.resource;
+                } else {
+                    action = (props.api?.resource as ResourceActions<'get'>).get() as GetAction<any, any>;
+                }
+                if (props.api?.getAction) {
+                    action = props.api.getAction(action) as GetAction<any, any>;
+                }
+                if (props.options.root_path) {
+                    action = action.setOnly(props.options.root_path);
+                }
+                const resp = await action.execute();
+                state.data = resp.data || {};
+            }
+        };
+        // const getData = _.debounce(getDataFunc, 50);
+
+        let apiWatchStop: any = null;
+        watch(() => state.isApiMode, (after, before) => {
+            if (after !== before) {
+                if (after) {
+                    // @ts-ignore
+                    apiWatchStop = watch(() => [props.isShow, props.api], (aft, bef) => {
+                        const isShow: boolean = aft[0] as boolean;
+                        const beforeIsShow = bef ? bef[0] : false;
+                        const afterApi: DynamicLayoutApiProp = aft[1] as DynamicLayoutApiProp;
+                        const beforeApi: undefined|DynamicLayoutApiProp = bef ? bef[1] as DynamicLayoutApiProp : undefined;
+                        if ((isShow && isShow !== beforeIsShow) || (afterApi.resource !== beforeApi?.resource || afterApi.getAction !== beforeApi?.getAction)) {
+                            getData();
+                        }
+                    });
+                } else if (apiWatchStop) {
+                    apiWatchStop();
+                }
+            }
+        });
+
+        const readonlyData = computed(() => (state.isApiMode ? state.data : props.data));
+        const rootData = computed(() => {
+            if (props.options.root_path) {
+                return _.get(readonlyData.value, props.options.root_path);
+            }
+            return readonlyData.value;
+        });
+
+        const getI18nMd = (md: any) => {
+            const DEFAULT_LANGUAGE = 'en';
+            return _.get(md, props.language, md[DEFAULT_LANGUAGE] || Object.values(md)[0]);
+        };
         const md = computed(() => {
             if (props.options.markdown) {
                 let doc = props.options.markdown;
-                if (props.data) {
+                if (typeof doc === 'object') {
+                    try {
+                        doc = getI18nMd(doc);
+                    } catch (e) {
+                        console.debug(e);
+                        return '';
+                    }
+                }
+                console.debug(rootData.value, 'root data');
+                if (rootData.value) {
                     doc = nunjucks.renderString(doc, props.data);
                 }
                 const renderMD = marked(doc);
@@ -60,7 +137,6 @@ export default {
                 //     return `<pre class="hljs">${group.codeblock}</pre>`;
                 // });
                 // return toHljs;
-                console.debug(renderMD);
                 const toHljs = renderMD.replace(/<pre>/g, () => '<pre class="hljs">');
                 return toHljs;
             }
