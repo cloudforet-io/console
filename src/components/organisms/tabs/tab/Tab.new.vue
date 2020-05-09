ac364e8a15eaf81750d3d4b6bafaa505de554b03
sinsky <sinsky@magazone.com>
2020-05-10 02:31:32 +0900

    hotfix  type error

---
+++ src/components/organisms/tabs/tab/Tab.new.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="p-tab">
+        <p-tab-bar class="p-tab-bar" :class="{'is-one':isOne}" :active-tab.sync="proxyActiveTab"
+                   :tabs="tabs"
+        />
+        <div class="tab-pane">
+            <keep-alive>
+                <slot v-for="tab in keepTabs"
+                      :name="proxyActiveTab === tab.name ? proxyActiveTab : undefined"
+                      :tabName="tab.name" :label="tab.label"
+                />
+            </keep-alive>
+            <slot v-for="tab in nonKeepTabs"
+                  :name="proxyActiveTab === tab.name ? proxyActiveTab : undefined"
+                  :tabName="tab.name" :label="tab.label"
+            />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import PTabBar from '@/components/molecules/tabs/tab-bar/TabBar.vue';
+import { makeProxy } from '@/lib/compostion-util';
+import {
+    computed, defineComponent,
+} from '@vue/composition-api';
+import { isOne, tabBarProps, TabBarProps } from '@/components/molecules/tabs/tab-bar/toolset';
+
+export default defineComponent({
+    name: 'PTab',
+    components: { PTabBar },
+    mixins: [tabBarProps],
+    setup(props: TabBarProps) {
+        return {
+            proxyActiveTab: makeProxy('activeTab'),
+            isOne: isOne(props),
+            nonKeepTabs: computed(() => props.tabs.filter(tab => (typeof tab === 'string' ? true : !tab.keepAlive))),
+            keepTabs: computed(() => props.tabs.filter(tab => (typeof tab === 'string' ? false : tab.keepAlive))),
+        };
+    },
+});
+</script>
+
+<style lang="postcss" scoped>
+.p-tab {
+    @apply rounded-sm border  border-gray-200 bg-white;
+}
+.p-tab-bar {
+    @apply border-b-4 border-gray-100;
+    &.is-one {
+        @apply border-b-2;
+    }
+}
+.tab-pane {
+    @apply w-full;
+}
+
+</style>
