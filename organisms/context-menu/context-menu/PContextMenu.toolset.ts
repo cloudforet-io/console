2f04421e3bb279578baa2d1ae0c3febe944e9066
Wanjin Noh <wanjin@megazone.com>
2020-05-11 18:16:46 +0900

    Updating dropdown components
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- organisms/context-menu/context-menu/PContextMenu.toolset.ts
+++ organisms/context-menu/context-menu/PContextMenu.toolset.ts
@@ -4,3 +4,34 @@ export interface MenuItem {
     type: 'divider'|'header'|'item';
     disabled?: boolean;
 }
+
+export const contextMenuProps = {
+    menu: {
+        type: [Array, Object],
+        default: () => [],
+    },
+    theme: {
+        type: String,
+        default: 'secondary',
+        validator(theme) {
+            return ['secondary', 'gray900'].includes(theme);
+        },
+    },
+    loading: {
+        type: Boolean,
+        default: false,
+    },
+    autoHeight: {
+        type: Boolean,
+        default: false,
+    },
+};
+
+export type ContextMenuTheme = 'secondary'|'gray900'
+
+export interface ContextMenuProps {
+    menu: MenuItem[];
+    theme: ContextMenuTheme;
+    loading: boolean;
+    autoHeight: boolean;
+}
