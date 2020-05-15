c8c4a7799126fe6833b7055fe42b1e7a48b6c629
Jenny <jennykim@mz.co.kr>
2020-05-15 19:18:24 +0900

    [CLOUD-1671] Add Service Account Button Mouse Hover

--- src/views/project/project/pages/ProjectPage.vue
+++ src/views/project/project/pages/ProjectPage.vue
@@ -119,14 +119,21 @@
                                     <div v-if="item.force_console_data.providers.length == 0" class="empty-providers"
                                          @click.stop="clickServiceAccount"
                                     >
-                                        <p>
+                                        <!-- <p>
                                             <p-simple-icon-button :normal-icon-name="'btn_circle_plus_blue'"
                                                                   :hovered-icon-name="'btn_circle_plus_blue--hover'"
-                                                                  width="24px" height="24px"
+                                                                  width="1.5rem" height="1.5rem"
                                                                   class="add-service-account-btn"
                                             />
                                             Add Service Account
-                                        </p>
+                                        </p> -->
+                                            <div class="w-6 h-6 bg-blue-100 rounded-full inline-block">
+                                                <p-i name="ic_plus_bold" color="inherit"
+                                                    width=".75rem" height=".75rem"
+                                                />
+                                            </div>
+                                            <span class="text-sm ml-2"> Add Service Account</span>
+                                    
                                     </div>
                                     <div v-else-if="item.force_console_data.providers" class="providers">
                                         <img v-for="(url, index) in item.force_console_data.providers" :key="index" :src="url"
@@ -681,7 +688,7 @@ export default {
         }
 
         .empty-providers {
-            p {
+            /* p {
                 @apply relative z-10 text-secondary text-sm;
                 max-height: 1.5rem;
                 min-height: 1.5rem;
@@ -691,6 +698,12 @@ export default {
                 &:hover {
                      @apply font-bold;
                  }
+            } */
+            @apply relative text-blue-600 ;
+            div { padding:0.125rem 0.375rem; }
+            &:hover {
+                @apply text-secondary font-bold;
+               div{ @apply bg-blue-300 ; } 
             }
         }
     }
