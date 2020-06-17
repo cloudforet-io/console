d48399506fd51995e76fba8da6b7164dba202a5a
Wanjin Noh <wanjin@megazone.com>
2020-06-17 11:11:31 +0900

    Updated treenode usage

--- organisms/modals/tree-api-modal/ProjectTreeModal.vue
+++ organisms/modals/tree-api-modal/ProjectTreeModal.vue
@@ -1,55 +1,67 @@
 <template>
-    <p-button-modal :header-title="headerTitle"
-                    :scrollable="false"
-                    size="sm"
+    <p-button-modal header-title="Change Project"
+                    scrollable
+                    size="md"
                     centered
                     fade
                     backdrop
                     :visible.sync="proxyVisible"
-                    @cancel="close"
-                    @close="close"
+                    :disabled="treeApiHandler.ts.metaState.firstSelectedNode && treeApiHandler.ts.metaState.firstSelectedNode.node.data.item_type === 'PROJECT_GROUP'"
                     @confirm="confirm"
     >
         <template #body>
-            <p-tree-node v-for="(node, idx) in treeApiHandler.ts.metaState.nodes" :key="idx"
-                         v-bind="node"
-                         :data.sync="node.data"
-                         :children.sync="node.children"
-                         :state.sync="node.state"
-                         @toggle:click="treeApiHandler.getData"
-                         @node:click="treeApiHandler.ts.setSelectedNodes"
-            >
-                <template #data="{data}">
-                    {{ data.name }}
-                </template>
-                <template #toggle="{state, toggleSize}">
-                    <p-i v-if="state.loading" name="ic_working" :width="toggleSize"
-                         :height="toggleSize"
-                    />
-                </template>
-            </p-tree-node>
+            <div class="mb-8">
+                Selected Project: {{ treeApiHandler.ts.metaState.firstSelectedNode ?
+                    treeApiHandler.ts.metaState.firstSelectedNode.node.data.name
+                    : 'No Project is selected' }}
+            </div>
+            <div>
+                <p-tree-node v-for="(node, idx) in treeApiHandler.ts.metaState.nodes" :key="idx"
+                             v-bind="treeApiHandler.ts.state"
+                             :data.sync="node.data"
+                             :children.sync="node.children"
+                             :state.sync="node.state"
+                             @toggle:click="treeApiHandler.getData"
+                             @node:click="selectItem"
+                             @checkbox:click="selectItem"
+                >
+                    <template #data="{data}">
+                        {{ data.name }}
+                    </template>
+                    <template #left-extra="{state, getListeners, data}">
+                        <span>
+                            <p-check-box v-if="data.item_type === 'PROJECT'"
+                                         :selected="state.selected" :value="true" v-on="getListeners('checkbox')"
+                            />
+                        </span>
+                    </template>
+                    <template #toggle="{state, toggleSize}">
+                        <p-i v-if="state.loading" name="ic_working" :width="toggleSize"
+                             :height="toggleSize"
+                        />
+                    </template>
+                </p-tree-node>
+            </div>
         </template>
     </p-button-modal>
 </template>
 
 <script lang="ts">
-import {
-    computed, defineComponent, reactive, ref, toRefs, watch,
-} from '@vue/composition-api';
-import PTreeModal from '@/components/organisms/modals/tree-modal/TreeModal.vue';
+import { watch } from '@vue/composition-api';
 import { makeProxy } from '@/lib/compostion-util';
 import PCheckBox from '@/components/molecules/forms/checkbox/CheckBox.vue';
 import PI from '@/components/atoms/icons/PI.vue';
 import PButtonModal from '@/components/organisms/modals/button-modal/ButtonModal.vue';
-import { ProjectTreeFluentAPI } from '@/lib/api/tree-node';
+import { ProjectNodeState, ProjectTreeFluentAPI } from '@/lib/api/tree-node';
 import PTreeNode from '@/components/molecules/tree/PTreeNode.vue';
 import { fluentApi } from '@/lib/fluent-api';
+import { TreeItem } from '@/components/molecules/tree/PTreeNode.toolset';
+import { ProjectItemResp } from '@/lib/fluent-api/identity/project';
 
 export default {
     name: 'SProjectTreeModal',
     components: {
         PTreeNode,
-        PTreeModal,
         PCheckBox,
         PI,
         PButtonModal,
@@ -59,28 +71,18 @@ export default {
             type: Boolean,
             default: false,
         },
-        headerTitle: {
-            type: String,
-            default: 'Change Project',
-        },
         projectId: {
             type: String,
-            default: undefined,
+            default: '',
         },
     },
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
     setup(props, { emit }) {
         const projectApi = fluentApi.identity().project();
-        const treeAction = projectApi.tree()
-            .setSortBy('name')
-            .setSortDesc(false);
 
         const treeApiHandler = new ProjectTreeFluentAPI({
-            treeAction, treeSearchAction: projectApi.treeSearch(),
-        });
-
-        const state = reactive({
-            release: ref(false),
-            error: ref(false),
+            treeAction: projectApi.tree().setSortBy('name').setSortDesc(false),
+            treeSearchAction: projectApi.treeSearch(),
         });
 
         watch(() => props.visible, async (after, before) => {
@@ -96,15 +98,15 @@ export default {
         return {
             proxyVisible: makeProxy('visible'),
             treeApiHandler,
-            ...toRefs(state),
-            update: (event) => {
+            selectItem(item: TreeItem<ProjectItemResp, ProjectNodeState>): void {
+                if (item.node.data.item_type === 'PROJECT') treeApiHandler.ts.setSelectedNodes(item);
             },
-            click() {
-            },
-            close() {
-            },
-            confirm() {
-                state.error = false;
+            confirm(): void {
+                if (treeApiHandler.ts.metaState.firstSelectedNode) {
+                    emit('confirm', treeApiHandler.ts.metaState.firstSelectedNode.node.data);
+                } else {
+                    emit('confirm', null);
+                }
             },
         };
     },
@@ -112,10 +114,13 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
-.alert{
-    @apply text-alert mt-4 align-middle;
-    .alert-msg{
-        @apply align-middle;
+.p-tree-node::v-deep {
+    .basic {
+        .left-extra {
+            display: inline-block;
+            width: 1rem;
+            margin-left: 0.5rem;
+        }
     }
 }
 </style>
