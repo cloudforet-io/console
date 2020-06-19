fe149d0541c698a6cb35fab348cfa61032aad58e
Wanjin Noh <wanjin@megazone.com>
2020-06-19 13:41:43 +0900

    tree toggle--opened svg updated

--- atoms/icons/p-icons/ic_tree_arrow--opened.js
+++ atoms/icons/p-icons/ic_tree_arrow--opened.js
@@ -7,6 +7,6 @@ icon.register({
     width: 16,
     height: 16,
     viewBox: '0 0 16 16',
-    data: '<path pid="0" _fill="#202433" d="M4 5.5l4 5.3 4-5.3z"/>'
+    data: '<path pid="0" fill-rule="evenodd" clip-rule="evenodd" d="M4.374 5.5a.7.7 0 00-.582 1.088l3.626 5.44a.7.7 0 001.165 0l3.626-5.44a.7.7 0 00-.582-1.088H4.374z" _fill="#222532"/>'
   }
 })
