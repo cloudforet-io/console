76cb6b3df7f3fd829a53c0539b3d4cb9aa2ffcda
Siyeon Lee <sylee@megazone.com>
2020-07-28 18:13:32 +0900

    Add Page Navigation (#22)

---
+++ molecules/page-navigation/PPageNavigation.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="p-page-navigation">
+        <template>
+            <span v-for="(route, idx) in routes" :key="idx">
+                <span v-if="route.path">
+                    <router-link class="menu"
+                                 :to="getPath(route)"
+                    >
+                        <span class="inline-block link">{{ route.name }}</span>
+                        <span v-if="idx < routes.length - 1">
+                            <p-i name="ic_breadcrumb_arrow" width="1rem" height="1rem"
+                                 class="arrow-icon" color="inherit white"
+                            />
+                        </span>
+                    </router-link>
+                </span>
+                <span v-else>
+                    <span class="inline-block link" @click="$emit('click', route, idx)">{{ route.name }}</span>
+                    <span v-if="idx < routes.length - 1">
+                        <p-i name="ic_breadcrumb_arrow" width="1rem" height="1rem"
+                             class="arrow-icon"
+                        />
+                    </span>
+                </span>
+            </span>
+        </template>
+    </div>
+</template>
+
+<script lang="ts">
+import PI from '@/components/atoms/icons/PI.vue';
+
+export default {
+    name: 'PPageNavigation',
+    components: {
+        PI,
+    },
+    props: {
+        routes: {
+            type: Array,
+            default: null,
+        },
+    },
+    setup(props, context) {
+        const getPath = route => `${route.path}`;
+        return {
+            getPath,
+        };
+    },
+};
+</script>
+
+<style lang="postcss">
+    .p-page-navigation {
+        .link {
+            @apply text-xs text-gray-900 mb-1 cursor-pointer;
+            opacity: 0.5;
+
+            &:hover {
+                opacity: 1;
+            }
+        }
+
+        .arrow-icon {
+            margin-left: 0.25rem;
+            margin-right: 0.25rem;
+            opacity: 0.5;
+        }
+    }
+</style>
