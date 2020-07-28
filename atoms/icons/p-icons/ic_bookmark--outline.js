d12f95e607ad78b3fc60bc08be4e46aa679becbd
Dahyun Yu <aydha0110@gmail.com>
2020-07-28 10:32:03 +0900

    Update: assets (#20)
    * Changed icons
    
    * add temp files
    
    * Change: hex color to theme color
    
    * Update: assets

---
+++ atoms/icons/p-icons/ic_bookmark--outline.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_bookmark--outline': {
+    width: 12,
+    height: 12,
+    viewBox: '0 0 12 12',
+    data: '<g clip-path="url(#clip0)"><path pid="0" d="M11.385 4.02l-3.277-.5L6.644.417a.705.705 0 00-1.288 0L3.892 3.52l-3.277.5c-.588.088-.824.845-.397 1.279l2.37 2.414-.56 3.41c-.101.616.52 1.077 1.04.79L6 10.302l2.932 1.61c.52.285 1.141-.174 1.04-.79l-.56-3.41 2.37-2.414c.427-.434.191-1.19-.397-1.28zM8.256 7.318l.532 3.243L6 9.032l-2.788 1.53.532-3.243-2.257-2.297 3.118-.473L6 1.596 7.395 4.55l3.118.473L8.256 7.32z" _fill="#858895"/></g><defs><clipPath id="clip0"><path pid="1" _fill="#fff" d="M0 0h12v12H0z"/></clipPath></defs>'
+  }
+})
