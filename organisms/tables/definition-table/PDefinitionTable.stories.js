e7d49f1a12727e733d176a574ad24ea887da5d4a
Dahyun Yu <aydha0110@gmail.com>
2020-08-04 16:13:00 +0900

    Update: PDefinitionTable storybook (#38)

--- organisms/tables/definition-table/PDefinitionTable.stories.js
+++ organisms/tables/definition-table/PDefinitionTable.stories.js
@@ -1,15 +1,14 @@
 import {
-    toRefs, reactive, ref, computed,
+    toRefs, reactive,
 } from '@vue/composition-api';
-import { action } from '@storybook/addon-actions';
 import { getKnobProps } from '@sb/storybook-util';
 import {
-    text, number, select, object, boolean, array,
+    text, number, select, object, boolean,
 } from '@storybook/addon-knobs/vue';
 import { definitionTableProps } from '@/components/organisms/tables/definition-table/PDefinitionTable.toolset';
 import PDefinitionTable from '@/components/organisms/tables/definition-table/PDefinitionTable.vue';
-import casual, { arrayOf } from '@/components/util/casual';
 import PIconTextButton from '@/components/molecules/buttons/icon-text-button/PIconTextButton.vue';
+import PLazyImg from '@/components/organisms/lazy-img/PLazyImg.vue';
 
 export default {
     title: 'organisms/tables/DefinitionTable',
@@ -64,19 +63,40 @@ export const defaultCase = () => ({
 
 
 export const slotCase = () => ({
-    components: { PDefinitionTable, PIconTextButton },
-    props: getKnobProps(definitionTableProps, {
-        items: [{
-            name: 'test',
-            data: 'test data',
-        }, {
-            name: 'test2',
-            data: 'test data2',
-        }],
-    }),
+    components: { PDefinitionTable, PIconTextButton, PLazyImg },
+    props: {
+        fields: {
+            default: object('fields', [
+                { name: 'id', key: 'collector_id' },
+                { name: 'name', key: 'name' },
+                { name: 'provider', key: 'provider' },
+            ]),
+        },
+        data: {
+            default: object('data', {
+                // eslint-disable-next-line camelcase
+                collector_id: 'collector-6746d641c98b',
+                name: 'collector name',
+                provider: 'aws',
+                tags: {
+                    icon: 'https://assets-console-spaceone-stg.s3.ap-northeast-2.amazonaws.com/console-assets/icons/aws-ec2.svg',
+                },
+            }),
+        },
+        loading: {
+            default: boolean('loading', false),
+        },
+        skeletonRows: {
+            default: number('skeletonRows', 5),
+        },
+    },
     template: `
         <div style="width: 80vw; background-color: white;">
             <PDefinitionTable v-bind="$props">
+                <template #data-name>
+                    <p-lazy-img :img-url="data.tags.icon" width="1rem" height="1rem" />
+                    <span class="ml-2 leading-none">{{ data.name }}</span>
+                </template>
                 <template #data-test="{data}">
                     <p-icon-text-button class="bg-primary text-white" name="ic_alert" fill>
                         {{data}}
