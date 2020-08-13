6a2b25a916a2b44abe6c63517aeb88491910c083
WANJIN <wanjin@megazone.com>
2020-08-13 18:42:02 +0900

    dynamic field > datetime bug fixed (#56)

--- organisms/dynamic-field/templates/datetime/index.vue
+++ organisms/dynamic-field/templates/datetime/index.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { DateTime as dt } from 'luxon';
 import { DatetimeOptions } from '@/components/organisms/dynamic-field/type';
+import moment, { Moment } from 'moment';
 
 export default {
     name: 'PDynamicFieldDatetime',
@@ -26,25 +27,20 @@ export default {
         let result = '';
         const options: DatetimeOptions = props.options;
         if (props.data) {
-            let time: dt;
+            let time: Moment;
             if (options.source_type === 'iso8601') {
-                if (options.source_format) {
-                    time = dt.fromFormat(props.data, options.source_format);
-                } else {
-                    time = dt.fromISO(props.data);
-                }
+                time = moment(props.data, options.source_format);
             } else if (options.source_format === 'seconds') {
-                time = dt.fromSeconds(Number(props.data));
+                time = moment.unix(props.data);
             } else {
-                time = dt.fromISO(props.data);
+                time = moment(props.data);
             }
 
-            time = time.setZone(props.extra.timezone ? props.extra.timezone : 'UTC');
+            time = moment.tz(time, props.extra.timezone || 'UTC');
             if (options.display_format) {
-                result = time.toFormat(options.display_format);
+                result = time.format(options.display_format);
             } else {
-                result = time.toFormat('yyyy-LL-dd HH:mm:ss'); // 'yyyy-LL-dd HH:mm:ss ZZZZ' For display Timezone
-                // result = time.toFormat('yyyy-LL-dd HH:mm:ss ZZZZ'); // 'yyyy-LL-dd HH:mm:ss ZZZZ' For display Timezone
+                result = time.format('YYYY-MM-DD HH:mm:ss');
             }
         }
         return h('span', data, result);
