115dd720b3be8cff853222e40ea0474aa651934b
WANJIN <wanjin@megazone.com>
2020-08-25 18:10:00 +0900

    Added Anchor component

---
+++ molecules/anchors/PAnchor.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { getBindClass } from '@/components/util/functional-helpers';
+
+export default {
+    name: 'PAnchor',
+    functional: true,
+    render(h, { data, children }) {
+        return h('a', {
+            ...data,
+            class: {
+                'p-anchor': true,
+                ...getBindClass(data.class),
+            },
+        },
+        children);
+    },
+};
+</script>
+
+<style lang="postcss">
+.p-anchor {
+    @apply cursor-pointer;
+    &:hover {
+       @apply text-secondary underline;
+    }
+}
+</style>
