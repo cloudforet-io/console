f9cfefd7693cd031ad77680f1c08251ff1d0fb00
Siyeon Lee <sylee@megazone.com>
2020-08-27 20:07:09 +0900

    Update: assets (#81)

--- atoms/icons/p-icons/ic_checkbox--disabled.js
+++ atoms/icons/p-icons/ic_checkbox--disabled.js
@@ -4,9 +4,9 @@
 import icon from 'vue-svgicon'
 icon.register({
   'ic_checkbox--disabled': {
-    width: 16,
-    height: 16,
+    width: 24,
+    height: 24,
     viewBox: '0 0 24 24',
-    data: '<path pid="0" d="M18 4H6c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2z" _fill="#f2f2f2"/><path pid="1" d="M18 20.5H6c-1.4 0-2.5-1.1-2.5-2.5V6c0-1.4 1.1-2.5 2.5-2.5h12c1.4 0 2.5 1.1 2.5 2.5v12c0 1.4-1.1 2.5-2.5 2.5zM6 4.5c-.8 0-1.5.7-1.5 1.5v12c0 .8.7 1.5 1.5 1.5h12c.8 0 1.5-.7 1.5-1.5V6c0-.8-.7-1.5-1.5-1.5H6z" _fill="#bbbcc4"/>'
+    data: '<path pid="0" d="M18 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2z" _fill="#F7F7F7" _stroke="#CED0D6"/>'
   }
 })
---
+++ atoms/icons/p-icons/ic_radio--disabled.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_radio--disabled': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: '<path pid="0" d="M21.5 12a9.5 9.5 0 11-19 0 9.5 9.5 0 0119 0z" _fill="#F7F7F7" _stroke="#CED0D6"/><path pid="1" d="M12 21.5a9.5 9.5 0 100-19 9.5 9.5 0 000 19z" _fill="#F7F7F7" _stroke="#CED0D6"/>'
+  }
+})
