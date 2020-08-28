33168164995e9265306afa8b596d7aca2f56f66a
WANJIN <wanjin@megazone.com>
2020-08-28 13:10:58 +0900

    Server Details > copy bug fixed (#201)

--- src/views/inventory/server/modules/ServerBaseInfo.vue
+++ src/views/inventory/server/modules/ServerBaseInfo.vue
@@ -24,14 +24,17 @@
                 </p-anchor>
             </template>
             <template #data-provider="{data: value}">
-                <p-badge :background-color="providers[value] ? providers[value].color : null">
-                    {{ providers[value] ? providers[value].name : '' }}
+                <p-badge v-if="providers[value] && providers[value].name"
+                         :background-color="providers[value] ? providers[value].color : null"
+                >
+                    {{ providers[value].name }}
                 </p-badge>
             </template>
             <template #data-collection_info.service_accounts="{data: value}">
                 <p-text-list :items="value" delimiter=" ">
                     <template #default="{value: d}">
-                        <p-badge :link="referenceRouter('identity.ServiceAccount', d)"
+                        <p-badge v-if="serviceAccounts[d]"
+                                 :link="referenceRouter('identity.ServiceAccount', d)"
                                  target="_blank"
                                  outline background-color="violet.500"
                         >
@@ -43,7 +46,9 @@
             <template #data-collection_info.secrets="{data: value}">
                 <p-text-list :items="value" delimiter=" ">
                     <template #default="{value: d}">
-                        <p-badge outline background-color="violet.500">
+                        <p-badge v-if="secrets[d]"
+                                 outline background-color="violet.500"
+                        >
                             {{ secrets[d] }}
                         </p-badge>
                     </template>
@@ -52,7 +57,8 @@
             <template #data-collection_info.collectors="{data: value}">
                 <p-text-list :items="value" delimiter=" ">
                     <template #default="{value: d}">
-                        <p-badge :link="referenceRouter('inventory.Collector', d)"
+                        <p-badge v-if="collectors[d]"
+                                 :link="referenceRouter('inventory.Collector', d)"
                                  target="_blank"
                                  outline background-color="violet.500"
                         >
@@ -134,19 +140,19 @@ export default {
                 { name: 'server_id', label: 'ID' },
                 { name: 'name', label: 'Name' },
                 { name: 'primary_ip_address', label: 'Primary IP' },
-                { name: 'collection_info.state', label: 'Collection State' },
-                { name: 'state', label: 'Life Cycle' },
+                { name: 'collection_info.state', label: 'Collection State', formatter: data => COLLECTION_STATE[data] },
+                { name: 'state', label: 'Life Cycle', formatter: data => LIFE_CYCLE[data] },
                 { name: 'reference.resource_id', label: 'Resource ID' },
                 { name: 'os_type', label: 'OS Type' },
-                { name: 'server_type', label: 'Server Type' },
-                { name: 'project_id', label: 'Project' },
-                { name: 'provider', label: 'Provider' },
-                { name: 'collection_info.service_accounts', label: 'Service Accounts' },
-                { name: 'collection_info.secrets', label: 'Secrets' },
-                { name: 'collection_info.collectors', label: 'Collected By' },
-                { name: 'created_at', label: 'Created' },
-                { name: 'updated_at', label: 'Updated' },
-                { name: 'deleted_at', label: 'Deleted' },
+                { name: 'server_type', label: 'Server Type', formatter: data => SERVER_TYPE[data] },
+                { name: 'project_id', label: 'Project', formatter: data => state.projects[data] },
+                { name: 'provider', label: 'Provider', formatter: data => state.providers[data] },
+                { name: 'collection_info.service_accounts', label: 'Service Accounts', formatter: data => state.serviceAccounts[data] },
+                { name: 'collection_info.secrets', label: 'Secrets', formatter: data => state.secrets[data] },
+                { name: 'collection_info.collectors', label: 'Collected By', formatter: data => state.collectors[data] },
+                { name: 'created_at', label: 'Created', formatter: timestampFormatter },
+                { name: 'updated_at', label: 'Updated', formatter: timestampFormatter },
+                { name: 'deleted_at', label: 'Deleted', formatter: timestampFormatter },
             ] as DefinitionField[],
             osFields: [
                 { name: 'os_distro', label: 'OS Distribution' },
@@ -169,6 +175,10 @@ export default {
                 { name: 'bios_version', label: 'Bios Version' },
                 { name: 'bios_released_at', label: 'Bios Released' },
             ] as DefinitionField[],
+            customBaseInfoFields: ['collection_info.state', 'state', 'server_type',
+                'project_id', 'provider', 'collection_info.service_accounts',
+                'collection_info.secrets', 'collection_info.collectors',
+                'created_at', 'updated_at', 'deleted_at'],
 
             // data
             osData: computed(() => get(props.data, 'data.os', {})),
