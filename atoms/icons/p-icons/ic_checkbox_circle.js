e44e7213a10cfafcd09986a75d81ecb7244770a2
Dahyun Yu <aydha0110@gmail.com>
2020-09-18 16:48:13 +0900

    Add: icons (#146)

---
+++ atoms/icons/p-icons/ic_checkbox_circle.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_checkbox_circle': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: '<path pid="0" d="M20.5 12a8.5 8.5 0 11-17 0 8.5 8.5 0 0117 0z" _stroke="#CED0D6"/>'
+  }
+})
