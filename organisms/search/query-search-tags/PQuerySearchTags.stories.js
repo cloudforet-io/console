b853d2762392fd1d8ec7c2e73bac130e765f7766
WANJIN <wanjin@megazone.com>
2020-09-21 00:14:43 +0900

    QuerySearchTags: slots added, converting order bug fixed, css bug fixed

--- organisms/search/query-search-tags/PQuerySearchTags.stories.js
+++ organisms/search/query-search-tags/PQuerySearchTags.stories.js
@@ -79,3 +79,68 @@ export const defaultCase = () => ({
         };
     },
 });
+
+
+export const slotCase = () => ({
+    components: { PQuerySearchTags },
+    props: {
+        timezone: {
+            default: text('timezone', 'UTC'),
+        },
+    },
+    template: `
+    <div style="width: 80vw;">
+        <div class="my-8">
+            It has slots per data type.<br>
+            - data-type-string<br>
+            - data-type-integer<br>
+            - data-type-float<br>
+            - data-type-datetime<br>
+            <br>
+            Slot props: <br>
+            - all props <br>
+            - tag: current QueryTag item.<br>
+        </div>
+        <PQuerySearchTags :timezone="timezone"
+                          :tags="tags"
+                          v-on="actions"
+        >
+            <template #data-type-datetime="{tag}">
+                {{tag.value.label}}
+            </template>
+        </PQuerySearchTags>
+    </div>`,
+    setup(props, context) {
+        const state = reactive({
+            tags: [
+                { key: { label: 'ID', name: 'id' }, value: { label: 'Hello', name: 'hello' }, operator: '' },
+                { key: null, value: { label: 'No key', name: 'no key' }, operator: '' },
+                {
+                    key: { label: 'DataTypes', name: 'data types' },
+                    value: { label: 'The same with QuerySearch data types.', name: 'datetime, integer, ...' },
+                    operator: '',
+                },
+                {
+                    key: { label: 'Operators', name: 'operators' },
+                    value: { label: 'The same with QuerySearch operators.', name: '=, !=, ...' },
+                    operator: '=',
+                },
+                {
+                    key: { label: 'Invalid Case', name: 'invalid case', dataType: 'datetime' },
+                    value: { label: 'If the value is not suitable format for data type, it displays tags like this.', name: 'invalid!' },
+                    operator: '',
+                },
+                {
+                    key: { label: 'Clear all', name: 'clear all' },
+                    value: { label: 'If there is no tag, nothing will be displayed.', name: 'no tags' },
+                    operator: '',
+                },
+            ],
+        });
+
+        return {
+            ...toRefs(state),
+            actions,
+        };
+    },
+});
