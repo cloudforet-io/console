ee9e52b2ef066b8ce26ab8cc2f9333939223c894
Dahyun Yu <aydha0110@gmail.com>
2020-09-25 15:10:06 +0900

    Add: ic_download (#170)

---
+++ atoms/icons/p-icons/ic_download.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_download': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 16 16',
+    data: '<path pid="0" d="M7.126 1.875a.875.875 0 111.75 0v6.263l2.837-2.837 1.238 1.237L8 11.488l-4.95-4.95 1.237-1.237 2.838 2.837V1.875z" _fill="#222532"/><path pid="1" d="M1 9.75h1.75v3.5h10.5v-3.5H15v3.5A1.75 1.75 0 0113.25 15H2.75A1.75 1.75 0 011 13.25v-3.5z" _fill="#222532"/>'
+  }
+})
