92a61f90c96a56009862cfed20b16457468c3fe2
WANJIN <wanjin@megazone.com>
2020-10-19 15:19:53 +0900

    Modals type updated

--- molecules/modals/type.ts
+++ molecules/modals/type.ts
@@ -1,7 +1,3 @@
-import {
-    initReactive, optionalType, StateToolSet, SyncStateToolSet,
-} from '@/components/util/toolset-helpers';
-
 export enum sizeMapping {
     sm = 'modal-sm',
     md = '',
@@ -9,15 +5,6 @@ export enum sizeMapping {
     xl = 'modal-xl',
 }
 export type ModalSizeType = keyof typeof sizeMapping;
-export interface ModalStateType {
-    fade: boolean;
-    scrollable: boolean;
-    size: ModalSizeType;
-}
-
-export interface ModalSyncStateType {
-    visible: boolean;
-}
 
 export interface ModalProps {
     fade: boolean;
@@ -26,34 +13,4 @@ export interface ModalProps {
     visible: boolean; // sync
     centered: boolean;
     backdrop: boolean;
-}
-
-@StateToolSet<ModalStateType>()
-@SyncStateToolSet<ModalSyncStateType>()
-export class ModalState<
-        initData=any, initSyncData=any,
-        initState extends ModalStateType=ModalStateType,
-        initSyncState extends ModalSyncStateType = ModalSyncStateType> {
-    state: optionalType<initState, initData>;
-
-     syncState: optionalType<initSyncState, initSyncData>;
-
-     static initState() {
-         return {
-             fade: false,
-             scrollable: false,
-             size: 'md' as ModalSizeType,
-         };
-     }
-
-     static initSyncState() {
-         return {
-             visible: false,
-         };
-     }
-
-     constructor(initData: initData = <initData>{}, initSyncData: initSyncData = <initSyncData>{}, lazy = false) {
-         this.state = initReactive(lazy, ModalState.initState(), initData);
-         this.syncState = initReactive(lazy, ModalState.initSyncState(), initSyncData);
-     }
 }
