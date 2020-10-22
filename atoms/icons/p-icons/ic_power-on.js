239c3465b0d2a3a38dc686c3aafb585ada2ed57d
Siyeon Lee <sylee@megazone.com>
2020-10-22 20:51:37 +0900

    Add: power on/off icons (#211)

---
+++ atoms/icons/p-icons/ic_power-on.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_power-on': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: '<path pid="0" d="M12 14a.666.666 0 01-.667-.667V2.667a.667.667 0 111.334 0v10.666A.667.667 0 0112 14z" _fill="#60B731"/><path pid="1" d="M20 9h-.207a8.666 8.666 0 11-12.14-3.734.667.667 0 00-.666-1.153 10 10 0 1014.206 4.733c-.39.1-.79.15-1.193.154zM20 7.333a3.333 3.333 0 100-6.667 3.333 3.333 0 000 6.667z" _fill="#60B731"/>'
+  }
+})
