1be7853569ad54d6765385459aee35cb03925a6e
WANJIN <wanjin@megazone.com>
2020-11-05 17:16:56 +0900

    Anchor: added highlight

--- molecules/anchors/PAnchor.vue
+++ molecules/anchors/PAnchor.vue
@@ -1,5 +1,7 @@
 <template>
-    <a :href="href" :target="target" class="p-anchor">
+    <a :href="href" :target="target" class="p-anchor"
+       :class="{highlight}"
+    >
         <span class="text">
             <slot v-bind="$props">
                 {{ text }}
@@ -34,8 +36,12 @@ export default {
             type: String,
             default: '_blank',
         },
+        highlight: {
+            type: Boolean,
+            default: false,
+        },
     },
-    setup(props) {
+    setup(props: AnchorProps) {
         return {
 
         };
@@ -48,6 +54,9 @@ export default {
     @apply cursor-pointer inline-flex items-center;
     &:hover {
         @apply underline;
+    }
+    &.highlight {
+        @apply text-secondary;
     }
     .text {
         margin-right: 2px;
---
+++ molecules/anchors/type.ts
@@ -0,0 +1,7 @@
+export interface AnchorProps {
+    text?: string;
+    showIcon?: boolean;
+    href?: string;
+    target?: string;
+    highlight?: boolean;
+}
