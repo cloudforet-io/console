c208a8c29125ae646f921b81eb0e81d74960ab55
Dahyun Yu <aydha0110@gmail.com>
2020-11-20 14:25:21 +0900

    [1.5.1] Update: referenceRouter (#651)

--- src/lib/reference/type.ts
+++ src/lib/reference/type.ts
@@ -1,5 +1,9 @@
 /* eslint-disable camelcase */
 
+/*
+ * Some reference types are added for routing purposes.
+ * These are used only on the front end, so be careful when binding to the api.
+ */
 const referenceTypes = ['inventory.Server', 'identity.Project', 'identity.ProjectGroup',
     'inventory.Collector', 'identity.ServiceAccount', 'identity.Provider',
     'inventory.Region', 'inventory.CloudService', 'inventory.CloudServiceType',
