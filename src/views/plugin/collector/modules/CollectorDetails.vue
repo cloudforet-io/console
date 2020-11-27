42c41b8ea7c6f8aaffddc75545e74d8713ab0f16
siyeon <sylee@megazone.com>
2020-11-27 17:46:00 +0900

    Update: data in collector detail tab

--- src/views/plugin/collector/modules/CollectorDetails.vue
+++ src/views/plugin/collector/modules/CollectorDetails.vue
@@ -4,12 +4,15 @@
         <p-definition-table :fields="baseState.fields" :data="baseState.data" :loading="baseState.loading"
                             :skeleton-rows="7" v-on="$listeners"
         >
-            <template #data-name>
-                <p-lazy-img :src="baseState.data.tags.icon" width="1rem" height="1rem" />
-                <span class="ml-2 leading-none">{{ baseState.data.name }}</span>
+            <template #data-state="{data}">
+                <p-status :text="data" :theme="data === 'DISABLED' ? 'red' : 'green'" />
+            </template>
+            <template #data-plugin_name="{data}">
+                <p-lazy-img :src="baseState.data.plugin_icon" width="1rem" height="1rem" />
+                <span class="ml-2 leading-none">{{ data }}</span>
             </template>
             <template #data-plugin_info.metadata.metadata.supported_resource_type="{data}">
-                <p-text-list :items="data || []" delimiter="," class="text-list"/>
+                <p-text-list :items="data || []" delimiter="<br>" class="text-list" />
             </template>
         </p-definition-table>
         <p-panel-top :use-total-count="true" :total-count="filterState.items? filterState.items.length:0">
@@ -34,10 +37,13 @@ import PTextList from '@/components/molecules/lists/text-list/PTextList.vue';
 
 import { timestampFormatter } from '@/lib/util';
 import { SpaceConnector } from '@/lib/space-connector';
+import { store } from '@/store';
+import PStatus from '@/components/molecules/status/PStatus.vue';
 
 export default {
     name: 'CollectorDetails',
     components: {
+        PStatus,
         PTextList,
         PPanelTop,
         PDefinitionTable,
@@ -56,11 +62,13 @@ export default {
             name: computed(() => vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_TITLE')),
             loading: true,
             fields: computed(() => [
-                { label: vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_LABEL_ID'), name: 'collector_id' },
+                // { label: vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_LABEL_ID'), name: 'collector_id' },
                 { label: vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_LABEL_NAME'), name: 'name' },
+                { label: vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_LABEL_STATE'), name: 'state' },
+                { label: vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_LABEL_PRIORITY'), name: 'priority' },
+                { label: vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_LABEL_PLUGIN_NAME'), name: 'plugin_name' },
                 { label: vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_LABEL_VERSION'), name: 'plugin_info.version' },
                 { label: vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_LABEL_PROVIDER'), name: 'provider' },
-                { label: vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_LABEL_PRIORITY'), name: 'priority' },
                 { label: vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_LABEL_RESOURCE_TYPE'), name: 'plugin_info.metadata.metadata.supported_resource_type' },
                 { label: vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_LABEL_LAST_COLLECTED'), name: 'last_collected_at', formatter: timestampFormatter },
                 { label: vm.$t('PLUGIN.COLLECTOR.MAIN.DETAILS_BASE_LABEL_CREATED'), name: 'created_at', formatter: timestampFormatter },
@@ -84,10 +92,21 @@ export default {
         const getCollectorDetailData = async () => {
             const res = await SpaceConnector.client.inventory.collector.get({
                 collector_id: props.collectorId,
-                only: [...baseState.fields.map(d => d.name), 'plugin_info.options.filter_format', 'tags'],
+                only: ['name', 'priority', 'provider', 'state', 'plugin_info.version', 'plugin_info.metadata.metadata.supported_resource_type',
+                    'last_collected_at', 'created_at', 'plugin_info.options.filter_format', 'tags', 'plugin_info.plugin_id'],
+
+                // ...baseState.fields.map(d => d.name)
             });
             baseState.loading = false;
-            if (res) baseState.data = res;
+            if (res) {
+                baseState.data = {
+                    // eslint-disable-next-line camelcase
+                    plugin_name: store.state.resource.plugin.items[res.plugin_info.plugin_id]?.label,
+                    // eslint-disable-next-line camelcase
+                    plugin_icon: store.state.resource.plugin.items[res.plugin_info.plugin_id]?.icon,
+                    ...res,
+                };
+            }
         };
         watch(() => props.collectorId, () => {
             getCollectorDetailData();
