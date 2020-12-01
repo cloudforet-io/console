c294d82dbb4e6b0887b8dc722bb7e41907f3baf2
siyeon <sylee@megazone.com>
2020-12-01 21:13:14 +0900

    Add: sign out page

---
+++ src/views/common/pages/SignOut.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>Sign Out..</div>
+</template>
+
+<script lang="ts">
+import { store } from '@/store';
+import router from '@/routes/index';
+// @ts-ignore
+const { gapi } = window;
+
+export default {
+    name: 'SignOut',
+    props: {
+        authSystem: {
+            type: String,
+            default: undefined,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    beforeRouteEnter(to, from, next) {
+        (async () => {
+            try {
+                if (to.params.authSystem === 'GOOGLE_OAUTH2') {
+                    const auth2 = gapi.auth2.getAuthInstance();
+                    auth2.signOut();
+                    auth2.disconnect();
+                    await store.dispatch('user/signOut');
+                }
+                if (to.params.authSystem === 'ID_PW') {
+                    await store.dispatch('user/signOut');
+                }
+            } catch (e) {
+                next({ name: 'error' });
+            }
+            const routerMeta: any = {
+                name: 'Login',
+                meta: { excludeAuth: true },
+            };
+            if (router && router.currentRoute.path) {
+                routerMeta.query = { nextPath: router.currentRoute.path };
+            }
+            if (router) {
+                await router.push(routerMeta);
+            }
+        })();
+    },
+};
+</script>
