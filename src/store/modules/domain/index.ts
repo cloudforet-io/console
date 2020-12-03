c08fedfd740332c1360ed58042a510cd04b00a83
Jongmin Kim <whdalsrnt@megazone.com>
2020-12-03 21:41:39 +0900

    Add extendedAuthType field to domain store
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/store/modules/domain/index.ts
+++ src/store/modules/domain/index.ts
@@ -8,6 +8,7 @@ const state: DomainState = {
     name: undefined,
     authType: undefined,
     authSystem: undefined,
+    extendedAuthType: undefined,
     authOptions: undefined,
 };
 
--- src/store/modules/domain/type.ts
+++ src/store/modules/domain/type.ts
@@ -1,10 +1,12 @@
 export type AuthSystem = 'GOOGLE_OAUTH2' | 'ID_PW';
 export type AuthType = 'INTERNAL' | 'EXTERNAL';
+export type ExtendedAuthType = 'GOOGLE_OAUTH2' | 'KEYCLOCK';
 
 export interface DomainState {
     domainId?: string;
     name?: string;
-    authType?: AuthType;
-    authSystem?: AuthSystem;
+    authType?: AuthType; // deprecated
+    authSystem?: AuthSystem; // deprecated
+    extendedAuthType?: ExtendedAuthType;
     authOptions?: Record<string, any>;
 }
