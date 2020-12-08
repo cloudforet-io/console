8a8b7843befaf1ab87a84384d2322416b148c34d
Dahyun Yu <aydha0110@gmail.com>
2020-12-08 21:39:11 +0900

    Add: TA router links (#765)

--- src/views/common/components/widgets/trusted-advisor/TrustedAdvisor.vue
+++ src/views/common/components/widgets/trusted-advisor/TrustedAdvisor.vue
@@ -7,28 +7,31 @@
             </div>
         </template>
         <div class="content-wrapper grid grid-cols-12 gap-6">
-            <div class="overall-wrapper col-span-12 lg:col-span-3">
-                <div class="title">
+            <div class="overall-wrapper col-span-12 xs:col-span-2 lg:col-span-3 grid grid-cols-12">
+                <div class="title col-span-12">
                     <span class="text">{{ $t('COMMON.WIDGETS.TRUSTED_ADVISOR.SUB_TITLE_OVERALL') }}</span>
                 </div>
-                <div class="chart-wrapper">
+                <div class="chart-wrapper col-span-6 xs:col-span-12">
                     <div ref="chartRef" class="chart" />
                 </div>
-                <div class="legend-wrapper">
-                    <div v-for="([k, v]) of Object.entries(legendData)" :key="k"
-                         class="legend-row"
-                    >
-                        <div class="left-part">
-                            <span class="legend-circle" :style="{ 'background-color': v.color }" />
-                            <span class="legend-text">{{ v.label }}</span>
-                        </div>
-                        <div class="right-part">
-                            <span :style="{ 'color': v.color }">{{ v.count }}</span>
-                        </div>
-                    </div>
+                <div class="legend-wrapper col-span-6 xs:col-span-12">
+                    <template v-for="([k, v]) of Object.entries(legendData)">
+                        <router-link :key="k"
+                                     :to="overallLinkFormatter(v.name)"
+                                     class="legend-row" :class="v.name"
+                        >
+                            <div class="left-part">
+                                <span class="legend-circle" :style="{ 'background-color': v.color }" />
+                                <span class="legend-text hidden lg:inline-block" :class="v.name">{{ v.label }}</span>
+                            </div>
+                            <div class="right-part relative lg:absolute">
+                                <span :style="{ 'color': v.color }">{{ v.count }}</span>
+                            </div>
+                        </router-link>
+                    </template>
                 </div>
             </div>
-            <div class="project-summary-wrapper col-span-12 lg:col-span-9">
+            <div class="project-summary-wrapper col-span-12 xs:col-span-10 lg:col-span-9">
                 <div class="title">
                     <span class="text">{{ $t('COMMON.WIDGETS.TRUSTED_ADVISOR.SUB_TITLE_PROJECT_SUMMARY') }}</span>
                     <p-text-pagination
@@ -47,7 +50,7 @@
                                      width="0.875rem" height="0.875rem"
                                      color="inherit transparent"
                                 />
-                                <span class="text">{{ tableData[rowNum].label }}</span>
+                                <span class="text hidden lg:inline-block">{{ tableData[rowNum].label }}</span>
                             </div>
                         </div>
                         <div class="right-part grid grid-cols-12 gap-1">
@@ -60,10 +63,15 @@
                                     </span>
                                 </template>
                                 <template v-else>
-                                    <div class="box" :class="getProjectBoxClass(rowNum, colNum * thisPage)">
-                                        <span class="box-text">
-                                            {{ getProjectBoxText(rowNum, colNum * thisPage) }}
-                                        </span>
+                                    <router-link v-if="getProjectBoxCount(rowNum, colNum * thisPage) > 0"
+                                                 :to="projectSummaryLinkFormatter(rowNum, colNum * thisPage)"
+                                    >
+                                        <div class="box" :class="getProjectBoxStatus(rowNum, colNum * thisPage)">
+                                            <span class="box-text">{{ getProjectBoxCount(rowNum, colNum * thisPage) }}</span>
+                                        </div>
+                                    </router-link>
+                                    <div v-else class="box empty">
+                                        <span class="box-text">0</span>
                                     </div>
                                 </template>
                             </div>
@@ -77,25 +85,27 @@
 
 <script lang="ts">
 /* eslint-disable camelcase */
-import { range, size, forEach } from 'lodash';
+import {
+    findKey, forEach, range, size,
+} from 'lodash';
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4charts from '@amcharts/amcharts4/charts';
 import am4themes_animated from '@amcharts/amcharts4/themes/animated';
 
 import {
-    computed, reactive, toRefs, watch,
-    getCurrentInstance, ComponentRenderProxy,
+    ComponentRenderProxy, computed, getCurrentInstance, reactive, toRefs, watch,
 } from '@vue/composition-api';
 
 import WidgetLayout from '@/views/common/components/layouts/WidgetLayout.vue';
 import PTextPagination from '@/components/organisms/paginations/text-pagination/PTextPagination.vue';
 import PI from '@/components/atoms/icons/PI.vue';
+import { QueryTag } from '@/components/organisms/search/query-search-tags/type';
 import { getAllPage } from '@/components/organisms/paginations/text-pagination/helper';
 
 import { SpaceConnector } from '@/lib/space-connector';
-import {
-    red, green, yellow,
-} from '@/styles/colors';
+import { referenceRouter } from '@/lib/reference/referenceRouter';
+import { queryTagsToQueryString } from '@/lib/router-query-string';
+import { green, red, yellow } from '@/styles/colors';
 
 am4core.useTheme(am4themes_animated);
 
@@ -117,6 +127,7 @@ interface OverallData {
     count: number;
 }
 interface LegendData {
+    name: string;
     label: string;
     color: string;
     count: number;
@@ -127,6 +138,7 @@ interface ProjectSummaryData {
     counts: [STATUS, number][];
 }
 
+const TRUSTED_ADVISER = 'TrustedAdvisor';
 const ERROR_COLOR = red[500];
 const WARNING_COLOR = yellow[500];
 const OK_COLOR = green[500];
@@ -137,12 +149,6 @@ export default {
         PTextPagination,
         WidgetLayout,
         PI,
-    },
-    props: {
-        providers: {
-            type: Object,
-            default: () => ({}),
-        },
     },
     setup() {
         const vm = getCurrentInstance() as ComponentRenderProxy;
@@ -162,18 +168,26 @@ export default {
             thisPage: 1,
             allPage: 1,
             projects: computed(() => vm.$store.state.resource.project.items),
+            trustedAdvisorId: computed(() => {
+                const cloudServiceTypes = vm.$store.state.resource.cloudServiceType.items;
+                const trustedAdvisorId = findKey(cloudServiceTypes, { name: TRUSTED_ADVISER });
+                return trustedAdvisorId || '';
+            }),
             legendData: computed(() => ({
                 error: {
+                    name: STATUS.error,
                     label: vm.$t('COMMON.WIDGETS.TRUSTED_ADVISOR.LABEL_ERROR'),
                     color: ERROR_COLOR,
                     count: chartState.data.find(d => d.status === STATUS.error)?.count,
                 },
                 warning: {
+                    name: STATUS.warning,
                     label: vm.$t('COMMON.WIDGETS.TRUSTED_ADVISOR.LABEL_WARNING'),
                     color: WARNING_COLOR,
                     count: chartState.data.find(d => d.status === STATUS.warning)?.count,
                 },
                 ok: {
+                    name: STATUS.ok,
                     label: vm.$t('COMMON.WIDGETS.TRUSTED_ADVISOR.LABEL_OK'),
                     color: OK_COLOR,
                     count: chartState.data.find(d => d.status === STATUS.ok)?.count,
@@ -238,23 +252,65 @@ export default {
         const changePageNumber = (page) => {
             state.thisPage = page;
         };
-        const getProjectBoxClass = (rowNum, colNum) => {
+        const getProjectBoxStatus = (rowNum, colNum) => {
             const projectData = state.projectSummaryData[colNum];
-            const htmlClass: any = {};
-            if (!projectData) {
-                htmlClass.empty = true;
-            } else {
+            if (projectData) {
                 const type = projectData.counts[rowNum][0];
                 const count = projectData.counts[rowNum][1];
-                if (count === 0) {
-                    htmlClass.empty = true;
-                } else {
-                    htmlClass[type] = true;
-                }
+                if (count === 0) return 'empty';
+                return type;
             }
-            return htmlClass;
+            return 'empty';
         };
-        const getProjectBoxText = (rowNum, colNum) => (state.projectSummaryData[colNum] ? state.projectSummaryData[colNum].counts[rowNum][1] : '0');
+        const getProjectBoxCount = (rowNum, colNum) => (state.projectSummaryData[colNum] ? state.projectSummaryData[colNum].counts[rowNum][1] : 0);
+        const overallLinkFormatter = (status) => {
+            const filters: QueryTag[] = [];
+            filters.push({
+                key: { label: 'Status', name: 'data.status' },
+                operator: '=',
+                value: { label: status, name: status },
+            });
+            return referenceRouter(
+                state.trustedAdvisorId,
+                { resource_type: 'inventory.CloudServiceType' },
+                {
+                    filters: queryTagsToQueryString(filters),
+                },
+            );
+        };
+        const projectSummaryLinkFormatter = (rowNum, colNum) => {
+            const status = getProjectBoxStatus(rowNum, colNum * state.thisPage);
+            const category = CATEGORY[rowNum];
+            const projectId = state.projectSummaryData[colNum]?.projectId;
+
+            const filters: QueryTag[] = [];
+            filters.push({
+                key: { label: 'Status', name: 'data.status' },
+                operator: '=',
+                value: { label: status, name: status },
+            });
+            if (category) {
+                filters.push({
+                    key: { label: 'Category', name: 'data.category' },
+                    operator: '=',
+                    value: { label: category, name: category },
+                });
+            }
+            if (projectId) {
+                filters.push({
+                    key: { label: 'Project Id', name: 'project_id' },
+                    operator: '=',
+                    value: { label: projectId, name: projectId },
+                });
+            }
+            return referenceRouter(
+                state.trustedAdvisorId,
+                { resource_type: 'inventory.CloudServiceType' },
+                {
+                    filters: queryTagsToQueryString(filters),
+                },
+            );
+        };
 
         /* api */
         const getOverallData = async () => {
@@ -326,11 +382,21 @@ export default {
         return {
             ...toRefs(state),
             chartState,
+            referenceRouter,
+            CATEGORY,
             changePageNumber,
             range,
-            getProjectBoxClass,
-            getProjectBoxText,
+            getProjectBoxStatus,
+            getProjectBoxCount,
+            overallLinkFormatter,
+            projectSummaryLinkFormatter,
         };
+    },
+    props: {
+        providers: {
+            type: Object,
+            default: () => ({}),
+        },
     },
 };
 </script>
@@ -360,28 +426,67 @@ export default {
         }
     }
     .legend-wrapper {
+        @media screen and (width < 1024px) {
+            width: 50%;
+            margin: auto;
+        }
         .legend-row {
             position: relative;
             display: flex;
+            height: 1.75rem;
             font-size: 0.875rem;
+            cursor: pointer;
             padding: 0.25rem 0;
+            &:hover {
+                @apply bg-secondary2;
+                .legend-text {
+                    text-decoration: underline;
+                    &.error {
+                        @apply text-red-500;
+                    }
+                    &.warning {
+                        @apply text-yellow-500;
+                    }
+                    &.ok {
+                        @apply text-green-500;
+                    }
+                }
+                .right-part {
+                    text-decoration: underline;
+                }
+            }
+            &.error {
+                @apply text-red-500;
+            }
+            &.warning {
+                @apply text-yellow-500;
+            }
+            &.ok {
+                @apply text-green-500;
+            }
             .left-part {
-                display: table-cell;
+                display: inline-flex;
+                width: 90%;
                 .legend-circle {
                     display: inline-block;
                     width: 0.5rem;
                     height: 0.5rem;
                     border-radius: 50%;
-                    margin-right: 0.25rem;
+                    margin: auto 0.25rem auto 0;
                 }
                 .legend-text {
                     @apply text-gray-700;
                     line-height: 1.4;
+                    width: 95%;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    white-space: nowrap;
                 }
             }
             .right-part {
                 position: absolute;
                 right: 0;
+                vertical-align: text-bottom;
             }
         }
     }
@@ -397,18 +502,32 @@ export default {
 
             .left-part {
                 width: 20%;
+                @media screen and (576px < width < 1024px) {
+                    width: 4%;
+                }
+                @media screen and (width < 576px) {
+                    width: 8%;
+                }
                 .label-wrapper {
                     @apply text-gray-400;
+                    display: inline-flex;
                     font-size: 0.75rem;
                     padding: 6px 0;
                     .text {
                         @apply text-gray-500;
+                        white-space: nowrap;
                         margin-left: 0.25rem;
                     }
                 }
             }
             .right-part {
                 width: 80%;
+                @media screen and (576px < width < 1024px) {
+                    width: 96%;
+                }
+                @media screen and (width < 576px) {
+                    width: 92%;
+                }
                 font-size: 0.75rem;
                 .col-wrapper {
                     position: relative;
@@ -441,6 +560,18 @@ export default {
                     }
                     &.empty {
                         @apply border border-gray-200 text-gray-300;
+                    }
+                    &:hover {
+                        text-decoration: underline;
+                        &.error {
+                            @apply bg-coral-200;
+                        }
+                        &.warning {
+                            @apply bg-yellow-100;
+                        }
+                        &.ok {
+                            @apply bg-peacock-100;
+                        }
                     }
                     .box-text {
                         display: table-cell;
