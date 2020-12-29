90198b940ff8df3bf2ef910f7b4401cae152af31
WANJIN <wanjin@megazone.com>
2020-12-29 16:13:38 +0900

    added config files

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable global-require, @typescript-eslint/no-var-requires */
+
+module.exports = {
+    parser: 'postcss-comment',
+    plugins: () => {
+        const res = [
+            require('postcss-easy-import')({
+                path: ['src', 'node_modules'],
+            }),
+            require('tailwindcss'),
+            require('postcss-hexrgba'),
+            require('postcss-mixins'),
+            require('postcss-conditionals'),
+            require('postcss-nested'),
+            require('postcss-simple-vars')({
+                variables() {
+                    return require('./src/styles/variables');
+                },
+            }),
+            require('autoprefixer'),
+            require('postcss-preset-env')({ stage: 3 }),
+        ];
+        return res;
+    },
+};
---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: ['stylelint-config-standard'],
+    rules: {
+        'selector-pseudo-element-no-unknown': [true, { ignorePseudoElements: ['v-deep'] }],
+        indentation: [4, { baseIndentLevel: 0 }],
+        'declaration-empty-line-before': null,
+        'at-rule-no-unknown': [true, { ignoreAtRules: ['define-mixin', 'mixin', 'at', 'screen', 'tailwind'] }],
+        'rule-empty-line-before': null,
+        'selector-list-comma-newline-after': null,
+        'function-calc-no-invalid': null,
+        'no-descending-specificity': null,
+        'property-no-unknown': [true, { ignoreProperties: [/\$(.+?)/] }],
+    },
+};
