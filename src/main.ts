902e55a38ede537dbff582e3d1cbb96f5885411d
WANJIN <wanjin@megazone.com>
2021-01-07 00:34:06 +0900

    removed unnecessary dependencies and global plugins (#892)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import Vue from 'vue';
-import velocity from 'velocity-animate';
-import SvgIcon from 'vue-svgicon';
 import VueCompositionApi from '@vue/composition-api';
-import Notifications from 'vue-notification';
-import LiquorTree from 'liquor-tree';
 import router from '@/routes/index';
 import { store } from '@/store';
 import directive from '@/directives';
@@ -12,8 +8,6 @@ import webFontLoader from 'webfontloader';
 import { webFonts, fontUrls } from '@/styles/web-fonts';
 import Fragment from 'vue-fragment';
 import VTooltip from 'v-tooltip';
-import Codemirror from 'vue-codemirror';
-import 'codemirror/lib/codemirror.css';
 import '@/styles/style.pcss';
 import config from '@/lib/config';
 import { SpaceConnector } from '@/lib/space-connector';
@@ -24,18 +18,10 @@ import App from './App.vue';
 
 /** ********** SET VUE PLUGINS ************** */
 Vue.use(VueCompositionApi);
-Vue.use(Notifications, { velocity });
-Vue.use(SvgIcon, {
-    tagName: 'svgicon',
-    classPrefix: 'p-i',
-});
-Vue.use(LiquorTree);
 // @ts-ignore
 Vue.use(Fragment.Plugin);
 Vue.use(VTooltip, { defaultClass: 'p-tooltip', defaultBoundariesElement: document.body });
-Vue.use(Codemirror);
 
-Vue.prototype.$velocity = velocity;
 
 directive(Vue);
 
