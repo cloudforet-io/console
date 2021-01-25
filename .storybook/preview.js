8a0296f12b304725536fe78facd0980bab2b5ea5
Lee Jeong Min <danmin20@khu.ac.kr>
2021-01-25 17:42:15 +0900

    PButton: update mdx (#374)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -106,4 +106,5 @@ export const parameters = {
         storySort: (a, b) =>
             a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
     },
+    actions: { argTypesRegex: "^on[A-Z].*" },
 }
