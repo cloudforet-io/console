67cf18ce8370a7944367efbb801a1d741f149852
WANJIN <wanjin@megazone.com>
2021-02-03 15:48:26 +0900

    Separate reset.css and make it preload in index.html (#1012)
    * Fix ProjectMemberTab api query binding bug
    
    * Separate reset.css and make it preload in index.html

---
+++ build/reset-css.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const postcss = require('postcss');
+const fs = require('fs');
+const postcssConfig = require('../postcss.config');
+
+console.log('processing...');
+
+const pcss = fs.readFileSync('src/styles/reset.pcss');
+
+postcss(postcssConfig.plugins()).process(pcss, { from: 'src/styles/reset.pcss', to: 'public/reset.css' })
+    .then((result) => {
+        fs.writeFileSync('public/reset.css', result.css);
+        if (result.map) {
+            fs.writeFileSync('public/reset.css.map', result.map.toString());
+        }
+        console.log('succeed');
+    });
