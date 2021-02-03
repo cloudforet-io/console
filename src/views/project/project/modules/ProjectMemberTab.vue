8b884f45cbdcbd04380031e4a1cabbc829017227
Siyeon Lee <sylee@megazone.com>
2021-02-03 13:16:01 +0900

    Add project member tab module (#1008)

---
+++ src/views/project/project/modules/ProjectMemberTab.vue
@@ -0,0 +1,285 @@
+<template>
+    <div class="member-tab">
+        <p-search-table class="member-tab"
+                        :shadow="false"
+                        :border="false"
+                        selectable
+                        :excel-visible="false"
+                        :fields="memberTableState.fields"
+                        :items="memberTableState.items"
+                        :select-index.sync="memberTableState.selectIndex"
+                        :loading="memberTableState.loading"
+                        :style="{height: '30rem', padding: 0}"
+                        :total-count="memberTableState.totalCount"
+                        @change="onChangeMemberTable"
+                        @init="onChangeMemberTable"
+        >
+            <template #toolbox-top>
+                <p-panel-top :title="$t('PROJECT.DETAIL.MEMBER_TITLE')" use-total-count :total-count="memberTableState.totalCount" />
+            </template>
+            <template #toolbox-left>
+                <p-icon-text-button style-type="primary-dark" class="mr-4 add-btn"
+                                    name="ic_plus_bold"
+                                    @click="openMemberAddForm()"
+                >
+                    {{ $t('PROJECT.DETAIL.ADD') }}
+                </p-icon-text-button>
+                <p-dropdown-menu-btn
+                    class="mr-4"
+                    :menu="memberTableState.dropdownMenu"
+                    @click-delete="memberDeleteClick"
+                    @click-update="openMemberUpdateForm"
+                >
+                    {{ $t('IDENTITY.USER.MAIN.ACTION') }}
+                </p-dropdown-menu-btn>
+            </template>
+            <template #col-resource_id-format="{ value }">
+                {{ memberTableState.users[value].name }}
+            </template>
+            <template #col-labels-format="{ value }">
+                <p v-if="value.length === 0" />
+                <p-badge v-for="(label, idx) in value" :key="idx" style-type="gray200"
+                         class="mr-2"
+                >
+                    {{ label }}
+                </p-badge>
+            </template>
+        </p-search-table>
+        <s-project-member-add-modal v-if="memberAddFormVisible" :visible.sync="memberAddFormVisible" @confirm="onAddMemberConfirm()" />
+        <project-member-update-modal v-if="memberUpdateFormVisible" :visible.sync="memberUpdateFormVisible" :selected-member="memberTableState.selectedItems[0]"
+                                     @confirm="onAddMemberConfirm"
+        />
+        <p-table-check-modal
+            :fields="checkMemberDeleteState.fields"
+            :mode="checkMemberDeleteState.mode"
+            :items="checkMemberDeleteState.items"
+            :header-title="checkMemberDeleteState.headerTitle"
+            :sub-title="checkMemberDeleteState.subTitle"
+            :theme-color="checkMemberDeleteState.themeColor"
+            size="lg"
+            :visible.sync="checkMemberDeleteState.visible"
+            @confirm="memberDeleteConfirm"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+import {
+    PBadge, PDropdownMenuBtn, PIconTextButton, PPanelTop, PSearchTable, PTableCheckModal,
+} from '@spaceone/design-system';
+import {
+    Options, SearchTableListeners,
+} from '@spaceone/design-system/dist/src/data-display/tables/search-table/type';
+import { ApiQueryHelper } from '@/lib/space-connector/helper';
+import {
+    ComponentRenderProxy, computed, getCurrentInstance, reactive, toRefs,
+} from '@vue/composition-api';
+
+import { MenuItem } from '@spaceone/design-system/dist/src/inputs/context-menu/type';
+import { SpaceConnector } from '@/lib/space-connector';
+import { getPageStart } from '@/lib/component-utils/pagination';
+
+import { TranslateResult } from 'vue-i18n';
+import { showErrorMessage, showSuccessMessage } from '@/lib/util';
+
+import SProjectMemberAddModal from '@/views/project/project/modules/ProjectMemberAddModal.vue';
+import ProjectMemberUpdateModal from '@/views/project/project/modules/ProjectMemberUpdateModal.vue';
+
+export default {
+    name: 'ProjectMemberTab',
+    components: {
+        PSearchTable,
+        PPanelTop,
+        PIconTextButton,
+        PDropdownMenuBtn,
+        PBadge,
+        PTableCheckModal,
+        SProjectMemberAddModal,
+        ProjectMemberUpdateModal,
+    },
+    props: {
+        projectId: {
+            type: String,
+            default: '',
+        },
+    },
+    setup(props, { root }) {
+        const vm = getCurrentInstance() as ComponentRenderProxy;
+
+        // List api Handler for query search table
+        const memberTableQuery = new ApiQueryHelper();
+        const memberTableState = reactive({
+            users: computed(() => vm.$store.state.resource.user.items),
+            selectIndex: [] as number[],
+            fields: [
+                { label: 'User ID', name: 'user_id', type: 'item' },
+                { label: 'User Name', name: 'resource_id', type: 'item' },
+                { label: 'Role', name: 'role_info.name', type: 'item' },
+                { label: 'Labels', name: 'labels', type: 'item' },
+            ],
+            items: [] as any[],
+            loading: true,
+            totalCount: 0,
+            options: {
+                searchText: '',
+            } as Partial<Options>,
+            selectedItems: computed(() => memberTableState.selectIndex.map(i => memberTableState.items[i])),
+            isSelected: computed(() => memberTableState.selectIndex.length > 0),
+            dropdownMenu: computed(() => ([
+                {
+                    type: 'item',
+                    name: 'update',
+                    label: vm.$t('IDENTITY.USER.MAIN.UPDATE'),
+                    disabled: memberTableState.selectIndex.length > 1 || !memberTableState.isSelected,
+                },
+                {
+                    type: 'item', name: 'delete', label: vm.$t('IDENTITY.USER.MAIN.DELETE'), disabled: !memberTableState.isSelected,
+                },
+            ] as MenuItem[])),
+        });
+
+        const formState = reactive({
+            updateMode: false,
+            headerTitle: '' as TranslateResult,
+            themeColor: '',
+            memberAddFormVisible: false,
+            memberDeleteFormVisible: false,
+            memberUpdateFormVisible: false,
+        });
+
+        const listMemberApi = SpaceConnector.client.identity.project.member.list;
+        const listMembers = async () => {
+            memberTableState.loading = true;
+            memberTableState.selectIndex = [];
+            try {
+                const res = await listMemberApi({
+                    project_id: props.projectId,
+                    query: memberTableQuery.data,
+                });
+                memberTableState.items = res.results.map(d => ({
+                    ...d,
+                    user_id: d.resource_id,
+                }));
+                memberTableState.totalCount = res.total_count;
+            } catch (e) {
+                memberTableState.items = [];
+                memberTableState.totalCount = 0;
+                console.error(e);
+            }
+            memberTableState.loading = false;
+        };
+
+        const onChangeMemberTable: SearchTableListeners['init'|'change'] = async (options, _changed?) => {
+            const changed = _changed || options;
+
+            if (changed.sortBy !== undefined) {
+                memberTableState.options.sortBy = changed.sortBy;
+                memberTableState.options.sortDesc = !!changed.sortDesc;
+                memberTableQuery.setSort(changed.sortBy, changed.sortDesc);
+            }
+            if (changed.pageLimit !== undefined) {
+                memberTableState.options.pageSize = changed.pageSize;
+                memberTableQuery.setPageLimit(changed.pageSize);
+            }
+            if (changed.pageStart !== undefined) {
+                memberTableState.options.thisPage = changed.thisPage;
+                memberTableQuery.setPageStart(getPageStart(changed.thisPage, memberTableState.options.pageSize));
+            }
+            if (changed.searchText !== undefined) {
+                memberTableState.options.searchText = changed.searchText;
+                memberTableQuery.setFilters([{ v: changed.searchText }]);
+            }
+            await listMembers();
+        };
+
+
+        const openMemberAddForm = () => {
+            formState.memberAddFormVisible = true;
+        };
+
+        const onAddMemberConfirm = async () => {
+            await listMembers();
+        };
+
+        const openMemberUpdateForm = () => {
+            formState.memberUpdateFormVisible = true;
+        };
+
+        const onUpdateMemberConfirm = async () => {
+            await listMembers();
+        };
+
+        const checkMemberDeleteState = reactive({
+            fields: computed(() => [
+                { name: 'resource_id', label: 'User ID' },
+                // { name: '-', label: 'User Name' },
+                { name: 'role_info.name', label: 'Role' },
+                // { name: 'labels', label: 'Labels' },
+            ]),
+            mode: '',
+            items: [] as any[],
+            headerTitle: '' as TranslateResult,
+            subTitle: '' as TranslateResult,
+            themeColor: '',
+            visible: false,
+            action: null as any,
+        });
+
+        const memberDeleteClick = () => {
+            checkMemberDeleteState.mode = 'delete';
+            // checkMemberDeleteState.action = memberDeleteAction;
+            checkMemberDeleteState.items = memberTableState.selectedItems as any[];
+            checkMemberDeleteState.headerTitle = vm.$t('PROJECT.DETAIL.MODAL_DELETE_MEMBER_TITLE');
+            checkMemberDeleteState.subTitle = vm.$t('PROJECT.DETAIL.MODAL_DELETE_MEMBER_CONTENT');
+            checkMemberDeleteState.themeColor = 'alert';
+            checkMemberDeleteState.visible = true;
+        };
+
+        const memberDeleteConfirm = async (items) => {
+            try {
+                await SpaceConnector.client.identity.project.member.remove({
+                    project_id: props.projectId,
+                    users: items.map(it => it.resource_id),
+                });
+                showSuccessMessage(vm.$t('PROJECT.DETAIL.ALT_S_DELETE_MEMBER'), '', root);
+            } catch (e) {
+                showErrorMessage(vm.$t('PROJECT.DETAIL.ALT_E_DELETE_MEMBER'), e, root);
+            } finally {
+                checkMemberDeleteState.visible = false;
+                await listMembers();
+            }
+        };
+
+        (async () => {
+            await listMembers();
+        })();
+
+
+        return {
+            memberTableState,
+            ...toRefs(formState),
+            checkMemberDeleteState,
+            openMemberAddForm,
+            onAddMemberConfirm,
+            openMemberUpdateForm,
+            onUpdateMemberConfirm,
+            memberDeleteClick,
+            memberDeleteConfirm,
+            listMembers,
+            onChangeMemberTable,
+        };
+    },
+};
+</script>
+
+<style lang="postcss" scoped>
+.member-tab {
+    @apply border border-gray-200;
+    >>> &.p-toolbox-table .toolbox {
+        @apply pt-0;
+    }
+    .p-panel-top {
+        @apply ml-0;
+    }
+}
+</style>
