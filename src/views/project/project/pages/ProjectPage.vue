a1cb3fd03518c2c3efd1d88488df8345220d39fb
WANJIN <wanjin@megazone.com>
2021-02-16 22:53:03 +0900

    Add translation submodule and Update prject group icon (#1036)
    * Update Favorite icon in Project Page
    
    * Remove language pack
    
    * Add translation language pack submodule

--- src/views/project/project/pages/ProjectPage.vue
+++ src/views/project/project/pages/ProjectPage.vue
@@ -13,7 +13,7 @@
                         </header>
                         <favorite-list :items="favoriteItems" :before-route="beforeFavoriteRoute" @delete="onFavoriteDelete">
                             <template #icon="{item}">
-                                <p-i :name="item.id.startsWith('project') ? 'ic_tree_project' : 'ic_tree_project-group'"
+                                <p-i name="ic_tree_project-group"
                                      width="1rem" height="1rem" color="inherit transparent"
                                 />
                             </template>
