5104669970fd706aad64c6b9410ae66778dea156
WANJIN <wanjin@megazone.com>
2021-03-11 20:25:09 +0900

    Add right extra slot to TextInput (#448)
    * Update assets and Add icons, lotties
    
    * Add right-extra slot to TextInput
    
    * Refactor TextInput storybook
    
    * Update default color of right extra in TextInput

---
+++ src/foundation/icons/p-icons/ic_favorite--added.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_favorite--added': {
+    width: 12,
+    height: 12,
+    viewBox: '0 0 12 12',
+    data: '<g clip-path="url(#clip0)"><path pid="0" d="M8.108 3.52l3.277.5c.588.088.824.845.397 1.279l-2.37 2.414.56 3.41c.101.616-.52 1.075-1.04.79L6 10.302l-2.932 1.61c-.52.287-1.141-.174-1.04-.79l.56-3.41-2.37-2.414c-.427-.434-.191-1.19.397-1.28l3.277-.499L5.356.417a.705.705 0 011.288 0L8.108 3.52z" _fill="#FFCE02"/></g><defs><clipPath id="clip0"><path pid="1" _fill="#fff" d="M0 0h12v12H0z"/></clipPath></defs>'
+  }
+})
--- src/inputs/input/PTextInput.vue
+++ src/inputs/input/PTextInput.vue
@@ -1,12 +1,13 @@
 <script lang="ts">
 import { getBindClass } from '@/util/functional-helpers';
+import { inputTypes } from '@/inputs/input/config';
 
 export default {
     name: 'PTextInput',
     functional: true,
     model: {
         prop: 'value',
-        event: 'onInput',
+        event: 'input',
     },
     props: {
         value: {
@@ -20,7 +21,7 @@ export default {
         type: {
             type: String,
             default: 'text',
-            validator: value => ['text', 'number', 'password'].includes(value),
+            validator: value => inputTypes.includes(value),
         },
         block: {
             type: Boolean,
@@ -30,49 +31,80 @@ export default {
             type: Boolean,
             default: false,
         },
-        focus: {
-            type: Boolean,
-            default: false,
-        },
     },
-    render(h, { data, props, listeners }) {
-        return h('input', {
-            ...data,
+    render(h, {
+        data, props, listeners, slots,
+    }) {
+        const childrenEl = [];
+
+        /* Input */
+        childrenEl.push(
+            h('input', {
+                ...data,
+                attrs: {
+                    ...data.attrs,
+                    disabled: props.disabled,
+                    type: props.type,
+                },
+                domProps: {
+                    value: props.value,
+                },
+                ...(data.directives?.length && {
+                    directives: [data.directives[0]],
+                }),
+                on: {
+                    ...listeners,
+                    input: (event) => {
+                        if (listeners.input) {
+                            if (Array.isArray(listeners.input)) {
+                                listeners.input.forEach((d) => { d(event.target.value); });
+                            } else listeners.input(event.target.value);
+                        }
+                    },
+                },
+            }),
+        );
+
+        /* Slots */
+        const allSlots = slots ? slots() : {};
+        if (allSlots['right-extra']) {
+            childrenEl.push(
+                h('span', {
+                    class: 'right-extra',
+                }, allSlots['right-extra']),
+            );
+        }
+
+        return h('span', {
             class: {
                 ...getBindClass(data.class),
                 'p-text-input': true,
                 block: props.block,
-                'is-invalid': props.invalid,
             },
-            attrs: {
-                ...data.attrs,
-                disabled: props.disabled,
-                type: props.type,
-            },
-            domProps: {
-                value: props.value,
-            },
-            ...(data.directives?.length && {
-                directives: [data.directives[0]],
-            }),
-            on: {
-                ...listeners,
-                input: (event) => {
-                    // do not change order
-                    if (listeners.onInput) listeners.onInput(event.target.value);
-                    if (listeners.input) listeners.input(event.target.value);
+            staticStyle: data.staticStyle,
+            style: data.style,
+        }, [
+            h('span', {
+                class: {
+                    'input-container': true,
+                    invalid: props.invalid,
+                    disabled: props.disabled,
                 },
-            },
-        });
+            }, childrenEl),
+        ]);
     },
 };
 </script>
 
 <style lang="postcss">
-
-@define-mixin setInput {
-    &.p-text-input {
-        @apply border bg-white text-gray-900;
+.p-text-input {
+    display: inline-block;
+    width: 15rem;
+    &.block {
+        @apply w-full;
+    }
+    .input-container {
+        @apply w-full inline-flex border bg-white text-gray-900;
         min-height: 2rem;
         height: 2rem;
         padding-left: 0.5rem;
@@ -80,26 +112,47 @@ export default {
         border-radius: 2px;
         font-size: 0.875rem;
         line-height: 2rem;
-        appearance: none;
-        &:hover {
-            @apply border-secondary;
-        }
-        &:focus {
-            @apply border-secondary;
-        }
-        &:disabled {
-            @apply border-gray-200 bg-gray-100;
-        }
-        &.is-invalid {
+        &.invalid {
             @apply border-alert;
+        }
+        &.disabled {
+            @apply border-gray-200 bg-gray-100
+        }
+        &:focus-within {
+            @apply border-secondary;
+        }
+        &:hover:not(.disabled) {
+            @apply border-secondary;
+        }
+    }
+    input {
+        @apply truncate;
+        display: inline-block;
+        flex-grow: 1;
+        border-width: 0;
+        height: 100%;
+        appearance: none;
+        line-height: inherit;
+        font-size: inherit;
+        color: inherit;
+        &:disabled {
+            @apply bg-transparent border-0;
         }
         &::placeholder {
             @apply text-gray-300;
         }
     }
+    .right-extra {
+        @apply text-gray-400;
+        display: inline-flex;
+        margin-left: 0.5rem;
+        flex-shrink: 0;
+        height: 100%;
+        overflow: hidden;
+        line-height: inherit;
+        font-size: inherit;
+        color: inherit;
+    }
 }
 
-input {
-    @mixin setInput;
-}
 </style>
