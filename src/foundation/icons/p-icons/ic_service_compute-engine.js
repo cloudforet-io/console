f0235a216506ac604980987506054d8634d1716f
WANJIN <wanjin@megazone.com>
2021-03-15 23:14:09 +0900

    Update assets head and fix wrong icon name causing build error

--- src/foundation/icons/p-icons/ic_service_compute-engine.js
+++ src/foundation/icons/p-icons/ic_service_compute-engine.js
@@ -3,7 +3,7 @@
 // @ts-ignore
 import icon from 'vue-svgicon'
 icon.register({
-  'ic_service_compute engine': {
+  'ic_service_compute-engine': {
     width: 16,
     height: 16,
     viewBox: '0 0 32 32',
