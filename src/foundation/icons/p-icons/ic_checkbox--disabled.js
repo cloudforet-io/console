122e6a42a09698f51f25ea5041501c311b3dd7d3
Dahyun Yu <aydha0110@gmail.com>
2021-04-20 18:44:53 +0900

    Update asset (#485)

--- src/foundation/icons/p-icons/ic_checkbox--disabled.js
+++ src/foundation/icons/p-icons/ic_checkbox--disabled.js
@@ -4,9 +4,9 @@
 import icon from 'vue-svgicon'
 icon.register({
   'ic_checkbox--disabled': {
-    width: 24,
-    height: 24,
-    viewBox: '0 0 24 24',
-    data: '<path pid="0" d="M18 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2z" _fill="#F7F7F7" _stroke="#CED0D6"/>'
+    width: 20,
+    height: 20,
+    viewBox: '0 0 20 20',
+    data: '<path pid="0" d="M14.666 3.333H5.333a2 2 0 00-2 2v9.334a2 2 0 002 2h9.333a2 2 0 002-2V5.333a2 2 0 00-2-2z" _fill="#F7F7F7" _stroke="#A7A9B2"/>'
   }
 })
--- src/foundation/icons/p-icons/ic_checkbox.js
+++ src/foundation/icons/p-icons/ic_checkbox.js
@@ -4,9 +4,9 @@
 import icon from 'vue-svgicon'
 icon.register({
   'ic_checkbox': {
-    width: 16,
-    height: 16,
-    viewBox: '0 0 24 24',
-    data: '<path pid="0" d="M18 20.5H6c-1.4 0-2.5-1.1-2.5-2.5V6c0-1.4 1.1-2.5 2.5-2.5h12c1.4 0 2.5 1.1 2.5 2.5v12c0 1.4-1.1 2.5-2.5 2.5zM6 4.5c-.8 0-1.5.7-1.5 1.5v12c0 .8.7 1.5 1.5 1.5h12c.8 0 1.5-.7 1.5-1.5V6c0-.8-.7-1.5-1.5-1.5H6z" _fill="#bbbcc4"/>'
+    width: 20,
+    height: 20,
+    viewBox: '0 0 20 20',
+    data: '<path pid="0" d="M14.666 3.333H5.333a2 2 0 00-2 2v9.334a2 2 0 002 2h9.333a2 2 0 002-2V5.333a2 2 0 00-2-2z" _stroke="#A7A9B2"/>'
   }
 })
