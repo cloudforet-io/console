776741193e305f9ba3fe4c929b064fdb330b53dd
Dahyun Yu <aydha0110@gmail.com>
2021-04-22 11:18:42 +0900

    Update PTag, PStatus mdx (#491)
    * Update PTag and Add mdx
    
    * Add PStatus mdx

--- src/data-display/tags/PTag.vue
+++ src/data-display/tags/PTag.vue
@@ -1,11 +1,21 @@
 <template>
-    <p-badge class="p-tag" :class="{deletable: deletable}"
+    <p-badge v-tooltip.bottom="isError ? errorMessage : ''"
+             class="p-tag"
+             :class="{deletable: deletable, activated: activated, outline: outline, error: isError}"
              :style-type="styleType" shape="square" :outline="outline"
              v-on="$listeners"
     >
+        <p-i v-if="isError" class="error-icon"
+             name="ic_alert"
+             width="0.8rem"
+             height="0.8rem"
+        />
         <slot />
-        <p-i v-if="deletable" name="ic_delete" width="1rem"
-             height="1rem" class="icon"
+        <p-i v-if="deletable"
+             name="ic_delete"
+             width="1rem"
+             height="1rem"
+             class="icon"
              color="inherit"
              @click="$emit('delete')"
         />
@@ -35,6 +45,18 @@ export default {
             type: Boolean,
             default: false,
         },
+        activated: {
+            type: Boolean,
+            default: false,
+        },
+        isError: {
+            type: Boolean,
+            default: false,
+        },
+        errorMessage: {
+            type: String,
+            default: '',
+        },
     },
     setup() {
         return { };
@@ -44,21 +66,40 @@ export default {
 
 <style lang="postcss">
 .p-tag {
-    margin-right: 0.5rem;
+    color: inherit;
     vertical-align: middle;
     white-space: nowrap;
-    color: inherit;
+    margin-right: 0.5rem;
     &.deletable {
         padding-right: 0.15rem;
         .icon {
-            @apply text-gray-400 ml-2;
+            @apply text-gray-400;
             cursor: pointer;
+            margin-left: 0.5rem;
         }
         &:hover {
             @apply bg-gray-100;
+
+            &.outline, &.error {
+                @apply border-gray-100;
+            }
+
             .icon {
                 @apply text-alert;
             }
+        }
+    }
+    &.activated {
+        @apply bg-blue-300;
+    }
+    &.outline {
+        @apply text-gray-dark;
+    }
+    &.error {
+        @apply bg-white border border-alert;
+
+        .error-icon {
+            margin-right: 0.3rem;
         }
     }
 }
