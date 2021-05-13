91804b1f0fc6c36371c737b30646c52187da4769
WANJIN <wanjin@megazone.com>
2021-05-13 23:08:48 +0900

    fix:  add fix commit due to npm version conflict
    update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-    branches: ['master'],
+    branches: ['master', { name: 'beta', prerelease: true }],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
-        ['@semantic-release/npm', {
-            // npmPublish: false,
-        }],
+        '@semantic-release/npm',
         '@semantic-release/git',
     ],
 };
