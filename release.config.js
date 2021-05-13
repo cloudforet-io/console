e72bc4f4e749242aae5ec493ef8b9faec4323bbb
WANJIN <wanjin@megazone.com>
2021-05-13 19:46:36 +0900

    fix: test release by update release.config.js

--- release.config.js
+++ release.config.js
@@ -3,8 +3,10 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
         ['@semantic-release/npm', {
             npmPublish: false,
         }],
+        '@semantic-release/git',
     ],
 };
