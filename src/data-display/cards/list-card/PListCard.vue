9f0c38a9259e2ebeecd0c0ed05c295d68490f53c
WANJIN <wanjin@megazone.com>
2021-06-01 16:47:20 +0900

    feat: add list card component

---
+++ src/data-display/cards/list-card/PListCard.vue
@@ -0,0 +1,92 @@
+<template>
+    <p-card class="p-list-card" :header="header" :style-type="styleType"
+            :class="{'no-data': items.length === 0}"
+    >
+        <template #header>
+            <slot name="header" />
+        </template>
+        <p-data-loader :data="items" :loading="loading" :spinner-size="1.5">
+            <ul>
+                <li v-for="(item, index) in items" :key="`${contextKey}-${index}`">
+                    <slot name="item" v-bind="{item, index}">
+                        {{ item }}
+                    </slot>
+                </li>
+            </ul>
+        </p-data-loader>
+    </p-card>
+</template>
+
+<script lang="ts">
+
+import {
+    defineComponent, reactive, toRefs, watch,
+} from '@vue/composition-api';
+import PCard from '@/data-display/cards/card/PCard.vue';
+import { CARD_STYLE_TYPE } from '@/data-display/cards/card/config';
+import PDataLoader from '@/feedbacks/loading/data-loader/PDataLoader.vue';
+import { CardProps } from '@/data-display/cards/card/type';
+
+interface Props extends CardProps {
+    items: any[];
+    loading?: boolean;
+}
+
+
+export default defineComponent<Props>({
+    name: 'PListCard',
+    components: { PDataLoader, PCard },
+    props: {
+        /* card props */
+        header: {
+            type: String,
+            default: '',
+        },
+        styleType: {
+            type: String,
+            default: CARD_STYLE_TYPE.gray,
+            validator(styleType: any) {
+                return Object.values(CARD_STYLE_TYPE).includes(styleType);
+            },
+        },
+        /* list card props */
+        items: {
+            type: Array,
+            default: () => [],
+        },
+        loading: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    setup(props: Props) {
+        const state = reactive({
+            contextKey: Math.floor(Math.random() * Date.now()),
+        });
+
+        watch(() => props.items, (after, before) => {
+            if (after !== before) state.contextKey = Math.floor(Math.random() * Date.now());
+        });
+        return {
+            ...toRefs(state),
+        };
+    },
+});
+</script>
+
+<style lang="postcss">
+.p-list-card {
+    &.p-card:not(.no-data) .body {
+        padding: 0;
+    }
+    li {
+        @apply border-b border-gray-200;
+        font-size: 0.875rem;
+        line-height: 1.25;
+        padding: 0.5rem 1rem;
+        &:last-of-type {
+            @apply border-b-0 rounded-b-lg;
+        }
+    }
+}
+</style>
---
+++ src/data-display/cards/list-card/story-helper.ts
@@ -0,0 +1,69 @@
+import { ArgTypes } from '@storybook/addons';
+import faker from 'faker';
+import { range } from 'lodash';
+import { argTypes as cardArgTypes } from '@/data-display/cards/card/story-helper';
+
+const getArgTypes = () => {
+    const argTypes: ArgTypes = {
+        ...cardArgTypes,
+        items: {
+            name: 'items',
+            type: { name: 'array' },
+            description: 'List items',
+            defaultValue: range(10).map(() => faker.lorem.sentence(8)),
+            table: {
+                type: {
+                    summary: 'array',
+                },
+                category: 'props',
+                defaultValue: {
+                    summary: '[]',
+                },
+            },
+            control: {
+                type: 'object',
+            },
+        },
+        loading: {
+            name: 'loading',
+            type: { name: 'boolean' },
+            description: 'Whether to show loader or not.',
+            defaultValue: false,
+            table: {
+                type: {
+                    summary: 'boolean',
+                },
+                category: 'props',
+                defaultValue: {
+                    summary: 'false',
+                },
+            },
+            control: {
+                type: 'boolean',
+            },
+        },
+        itemSlot: {
+            name: 'itemSlot',
+            description: 'Slot for list item.',
+            defaultValue: null,
+            table: {
+                type: {
+                    summary: null,
+                },
+                defaultValue: {
+                    summary: null,
+                },
+                category: 'slots',
+            },
+            control: {
+                type: 'text',
+            },
+        },
+    };
+
+    delete argTypes.defaultSLot;
+
+    return argTypes;
+};
+
+export const argTypes: ArgTypes = getArgTypes();
