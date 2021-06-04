7800e45ecb5282e9f06f007886397bbcb77d07f9
Dahyun Yu <aydha0110@gmail.com>
2021-06-04 14:13:36 +0900

    feat: bind activate alert API with button (#1400)
    * fix: update language pack
    
    * feat: bind activate alert API with button

--- src/views/project/project/pages/ProjectAlertPage.vue
+++ src/views/project/project/pages/ProjectAlertPage.vue
@@ -1,23 +1,148 @@
 <template>
-    <div>
-        <project-alert-tab
-            :project-id="id"
-            :is-activated="true"
-        />
+    <div class="project-alert-page">
+        <div v-if="!isActivated" class="activation-wrapper">
+            <strong>{{ $t('PROJECT.DETAIL.PROJECT_ALERT_ACTIVATION_DESC_1') }}</strong>
+            <p>{{ $t('PROJECT.DETAIL.PROJECT_ALERT_ACTIVATION_DESC_2') }}</p>
+            <p-button
+                style-type="safe"
+                @click="onActivateAlert"
+            >
+                {{ $t('PROJECT.DETAIL.PROJECT_ALERT_ACTIVATE') }}
+            </p-button>
+        </div>
+        <p-button-tab
+            v-else
+            :tabs="tabState.tabs"
+            :active-tab.sync="tabState.activeTab"
+            keep-alive-all
+            @change="onChangeTab"
+        >
+            <template #alert>
+                <div>Project Alert</div>
+            </template>
+            <template #webhook>
+                <project-webhook-tab :project-id="id" />
+            </template>
+            <template #settings>
+                <project-settings :project-id="id" />
+            </template>
+        </p-button-tab>
     </div>
 </template>
 
 <script lang="ts">
-import ProjectAlertTab from '@/views/project/project/modules/ProjectAlertTab.vue';
+import { isEmpty } from 'lodash';
+
+import {
+    ComponentRenderProxy, computed, getCurrentInstance, reactive, toRefs, watch,
+} from '@vue/composition-api';
+
+import { PButtonTab, PButton } from '@spaceone/design-system';
+import ProjectWebhookTab from '@/views/project/project/modules/ProjectWebhookTab.vue';
+import ProjectSettings from '@/views/project/project/modules/project-alert/ProjectSettings.vue';
+
+import { TabItem } from '@spaceone/design-system/dist/src/navigation/tabs/tab/type';
+import { SpaceConnector } from '@/lib/space-connector';
+import { showErrorMessage, showSuccessMessage } from '@/lib/util';
 
 export default {
     name: 'ProjectAlertPage',
-    components: { ProjectAlertTab },
+    components: {
+        ProjectSettings,
+        ProjectWebhookTab,
+        PButtonTab,
+        PButton,
+    },
     props: {
         id: {
             type: String,
             default: undefined,
         },
     },
+    setup(props, { root }) {
+        const vm = getCurrentInstance() as ComponentRenderProxy;
+
+        const state = reactive({
+            isActivated: false,
+        });
+        const tabState = reactive({
+            tabs: computed(() => ([
+                { name: 'alert', label: vm.$t('PROJECT.DETAIL.SUBTAB_ALERT'), keepAlive: true },
+                { name: 'webhook', label: vm.$t('PROJECT.DETAIL.SUBTAB_WEBHOOK'), keepAlive: true },
+                { name: 'settings', label: vm.$t('PROJECT.DETAIL.SUBTAB_SETTINGS'), keepAlive: true },
+            ] as TabItem[])),
+            activeTab: 'alert',
+        });
+
+        /* api */
+        const getProjectAlertConfig = async () => {
+            try {
+                const res = await SpaceConnector.client.monitoring.projectAlertConfig.get({
+                    project_id: props.id,
+                });
+                if (!isEmpty(res)) state.isActivated = true;
+            } catch (e) {
+                state.isActivated = false;
+                console.error(e);
+            }
+        };
+        const onActivateAlert = async () => {
+            try {
+                await SpaceConnector.client.monitoring.projectAlertConfig.create({
+                    project_id: props.id,
+                });
+                state.isActivated = true;
+                tabState.activeTab = 'settings';
+                showSuccessMessage(vm.$t('PROJECT.DETAIL.ALT_S_ACTIVATE_ALERT'), '', root);
+            } catch (e) {
+                state.isActivated = false;
+                console.error(e);
+                showErrorMessage(vm.$t('PROJECT.DETAIL.ALT_E_ACTIVATE_ALERT'), e, root);
+            }
+        };
+
+        /* event */
+        const onChangeTab = async (tab) => {
+            tabState.activeTab = tab;
+        };
+
+        watch(() => props.id, (projectId) => {
+            if (projectId) getProjectAlertConfig();
+        }, { immediate: true });
+
+        return {
+            ...toRefs(state),
+            tabState,
+            onChangeTab,
+            onActivateAlert,
+        };
+    },
 };
 </script>
+<style lang="postcss" scoped>
+.activation-wrapper {
+    @apply text-center;
+    margin-top: 2.5rem;
+    strong {
+        @apply font-bold;
+        font-size: 1.125rem;
+        line-height: 1.55;
+    }
+    p {
+        margin: 0.5rem 0 1rem;
+        font-size: 0.875rem;
+        line-height: 1.5;
+    }
+}
+.project-alert-page {
+    .p-button-tab::v-deep .tab-pane {
+        @apply pb-0;
+    }
+}
+
+@screen mobile {
+    .activation-wrapper {
+        padding: 0 1rem;
+    }
+}
+</style>
