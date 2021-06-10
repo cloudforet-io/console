e075fe79e6a28eac98e17afcdb03c2213ce11284
Songyi Kim <sykim@mz.co.kr>
2021-06-10 15:27:18 +0900

    feat: add card styles (#590)
    * feat: add card styles
    
    * feat: fix card styles
    
    Co-authored-by: kimsongyi <sykim@megazone.com>

--- src/data-display/cards/card/PCard.vue
+++ src/data-display/cards/card/PCard.vue
@@ -26,7 +26,7 @@ export default defineComponent<CardProps>({
         },
         styleType: {
             type: String,
-            default: CARD_STYLE_TYPE.gray,
+            default: CARD_STYLE_TYPE.gray100,
             validator(styleType: any) {
                 return Object.values(CARD_STYLE_TYPE).includes(styleType);
             },
@@ -40,25 +40,52 @@ export default defineComponent<CardProps>({
 
 <style lang="postcss">
 .p-card {
-    @apply border border-gray-200 rounded-lg;
+    @apply border rounded-lg;
     header {
-        @apply text-gray-500 rounded-t-lg border-b border-gray-200;
+        @apply rounded-t-lg border-b;
         padding: 0.5rem 1rem;
-        font-size: 0.75rem;
-        line-height: 1.5;
-        min-height: 2rem;
     }
     .body {
         padding: 0.75rem 0.875rem;
     }
 
-    @define-mixin style-type $bg-color {
+    @define-mixin style-type $bg-color, $border-color {
+        border-color: $border-color;
         header {
+            @apply text-gray-500;
+            min-height: 1.875rem;
+            font-size: 0.75rem;
+            line-height: 1.2;
             background-color: $bg-color;
+            border-color: $border-color;
         }
     }
 
-    &.gray { @mixin style-type theme('colors.gray.100'); }
-    &.yellow { @mixin style-type theme('colors.yellow.100'); }
+    @define-mixin style-type2 $bg-color, $border-color {
+        border-color: $border-color;
+        header {
+            @apply font-bold text-white;
+            min-height: 2.25rem;
+            font-size: 1rem;
+            line-height: 1.2;
+            background-color: $bg-color;
+            border-color: $border-color;
+        }
+    }
+    &.gray100 {
+        @mixin style-type theme('colors.gray.100'), theme('colors.gray.200');
+    }
+    &.yellow100 {
+        @mixin style-type theme('colors.yellow.100'), theme('colors.gray.200');
+    }
+    &.yellow500 {
+        @mixin style-type2 theme('colors.yellow.500'), theme('colors.yellow.500');
+    }
+    &.indigo400 {
+        @mixin style-type2 theme('colors.indigo.400'), theme('colors.indigo.400');
+    }
+    &.red400 {
+        @mixin style-type2 theme('colors.red.400'), theme('colors.red.400');
+    }
 }
 </style>
--- src/data-display/cards/card/config.ts
+++ src/data-display/cards/card/config.ts
@@ -1,4 +1,7 @@
 export enum CARD_STYLE_TYPE {
-    gray = 'gray',
-    yellow = 'yellow'
+    gray100 = 'gray100',
+    yellow100 = 'yellow100',
+    yellow500 = 'yellow500',
+    indigo400 = 'indigo400',
+    red400 = 'red400'
 }
