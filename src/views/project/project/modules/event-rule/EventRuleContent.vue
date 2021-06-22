fae6046ffc637e706b297c5ecaeec7805f3faf68
Siyeon Lee <sylee@megazone.com>
2021-06-22 20:04:02 +0900

    feat: add event rule card (#1468)

--- src/views/project/project/modules/event-rule/EventRuleContent.vue
+++ src/views/project/project/modules/event-rule/EventRuleContent.vue
@@ -1,64 +1,81 @@
 <template>
     <fragment>
         <section class="left-section">
-            {{ data.conditions_policy }} of the following are met
+            <h4><b>{{ data.conditions_policy }}</b> of the following are met</h4>
             <ul v-for="(condition, idx) in data.conditions" :key="`${condition}-${idx}`" class="condition-list">
-                <li>{{ condition.key }} {{ condition.operator }} {{ condition.value }}</li>
+                <li>
+                    <span class="text-blue-800">{{ condition.key }}</span>
+                    {{ condition.operator }}
+                    <span class="text-blue-800">{{ condition.value }}</span>
+                </li>
             </ul>
         </section>
         <section class="right-section">
-            <p-definition-table :fields="fields" :data="data" :skeleton-rows="9"
-                                :disable-copy="true" :style-type="'white'"
-            >
-                <template #data-actions.no_notification>
-                    <span v-if="data.actions.no_notification">On</span>
-                    <span v-else>Off</span>
-                </template>
-                <template #data-project_id>
-                    <p-anchor :to="referenceRouter(
-                                  data.project_id,
-                                  { resource_type: 'identity.Project' })"
-                              highlight
-                    >
-                        {{ projects[data.project_id] ? projects[data.project_id].label : data.project_id }}
-                    </p-anchor>
-                </template>
-                <template #data-actions.add_project_dependency>
-                    <p v-for="(item, index) in data.actions.add_project_dependency" :key="`${item}-${index}`"
-                       class="project-name"
-                    >
-                        <p-anchor :to="referenceRouter(
-                                      item,
-                                      { resource_type: 'identity.Project' })"
-                                  highlight
+            <h4><b>Do</b> these things</h4>
+            <table>
+                <tbody>
+                    <tr v-for="(item, index) in fields" :key="`${item}-${index}`">
+                        <template v-if="items[item.name].length ||
+                            item.name === 'stop_processing' && !!items[item.name] ||
+                            item.name === 'no_notification'"
                         >
-                            {{ projects[item] ? projects[item].label : item }}
-                        </p-anchor>
-                    </p>
-                </template>
-                <template #data-actions.add_responder>
-                    <p v-for="(item, index) in data.actions.add_responder" :key="`${item}-${index}`">
-                        {{ item.resource_id }}
-                    </p>
-                </template>
-                <template #data-options.stop_processing>
-                    <span />
-                </template>
-            </p-definition-table>
+                            <td>{{ item.label }}</td>
+                            <td v-if="item.name === 'no_notification'">
+                                <span v-if="items[item.name]">On</span>
+                                <span v-else>Pause Notification</span>
+                            </td>
+                            <td v-else-if="item.name === 'change_project'">
+                                <p-anchor :to="referenceRouter(
+                                              items[item],
+                                              { resource_type: 'identity.Project' })"
+                                          highlight
+                                >
+                                    {{ projects[items[item.name]] ? projects[items[item.name]].label : items[item.name] }}
+                                </p-anchor>
+                            </td>
+                            <td v-else-if="item.name === 'add_project_dependency'">
+                                <p v-for="(projectId, idx) in items[item.name]" :key="`${projectId}-${idx}`"
+                                   class="project-name"
+                                >
+                                    <p-anchor :to="referenceRouter(
+                                                  projectId,
+                                                  { resource_type: 'identity.Project' })"
+                                              highlight
+                                    >
+                                        {{ projects[projectId] ? projects[projectId].label : projectId }}
+                                    </p-anchor>
+                                </p>
+                            </td>
+                            <td v-else-if="item.name === 'add_responder'">
+                                <p v-for="(user, idx) in items[item.name]" :key="`${user}-${idx}`" class="user-name">
+                                    {{ user.resource_id }}
+                                </p>
+                            </td>
+                            <td v-else-if="item.name === 'stop_processing'">
+                                <span v-if="items[item.name]" />
+                            </td>
+                            <td v-else>
+                                {{ items[item.name] }}
+                            </td>
+                        </template>
+                    </tr>
+                </tbody>
+            </table>
         </section>
     </fragment>
 </template>
 
 <script lang="ts">
-import { computed, reactive, toRefs } from '@vue/composition-api';
-import { PAnchor, PDefinitionTable } from '@spaceone/design-system';
+import {
+    computed, reactive, toRefs, watch,
+} from '@vue/composition-api';
+import { PAnchor } from '@spaceone/design-system';
 import { referenceRouter } from '@/lib/reference/referenceRouter';
 import { store } from '@/store';
 
 export default {
     name: 'EventRuleContent',
     components: {
-        PDefinitionTable,
         PAnchor,
     },
     props: {
@@ -70,20 +87,31 @@ export default {
     setup(props) {
         const state = reactive({
             fields: [
-                { name: 'actions.no_notification', label: 'Notifications' },
-                { name: 'project_id', label: 'Project Routing' },
-                { name: 'actions.add_project_dependency', label: 'Project Dependencies' },
-                { name: 'actions.change_urgency', label: 'Urgency' },
-                { name: 'actions.change_assignee', label: 'Assignee' },
-                { name: 'actions.add_responder', label: 'Responder' },
-                { name: 'options.stop_processing', label: 'Then Stop Processing' },
+                { name: 'no_notification', label: 'Notifications' },
+                { name: 'change_project', label: 'Project Routing' },
+                { name: 'add_project_dependency', label: 'Project Dependencies' },
+                { name: 'change_urgency', label: 'Urgency' },
+                { name: 'change_assignee', label: 'Assignee' },
+                { name: 'add_responder', label: 'Responder' },
+                { name: 'stop_processing', label: 'Then Stop Processing' },
             ],
+            items: [] as any,
             projects: computed(() => store.state.resource.project.items),
         });
 
+        const getData = () => {
+            state.items = { ...props.data.change_project, ...props.data.actions, ...props.data.options };
+        };
+
+        watch(() => props.data, () => {
+            getData();
+        }, { immediate: false });
+
         (async () => {
+            getData();
             await store.dispatch('resource/project/load');
         })();
+
         return {
             ...toRefs(state),
             referenceRouter,
@@ -93,14 +121,66 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
+h4 {
+    font-size: 1rem;
+    line-height: 160%;
+}
 .left-section {
     @apply col-span-4;
     .condition-list {
         list-style: disc;
+        font-size: 1rem;
+        line-height: 160%;
+
+        li {
+            margin-left: 1.5rem;
+            line-height: 150%;
+            font-size: 0.875rem;
+        }
     }
 }
 .right-section {
     @apply col-span-8;
+}
+table {
+    display: flex;
+    width: 100%;
+    td, th {
+        @apply px-4 py-2;
+    }
+
+    thead {
+        tr {
+            th, td {
+                @apply border-black border-t border-b font-bold;
+            }
+        }
+    }
+
+    tbody {
+        width: 100%;
+        tr {
+            @apply border-gray-300 border-b;
+            td, th {
+
+                width: 100%;
+                &:first-child {
+                    @apply text-gray-500;
+                    font-size: 0.875rem;
+                    line-height: 150%;
+                    width: 11rem;
+                }
+                &:last-child {
+                    font-size: 0.875rem;
+                    line-height: 150%;
+                }
+            }
+            &:last-child {
+                @apply border-none;
+            }
+        }
+
+    }
 }
 @screen tablet {
     .left-section {
@@ -108,9 +188,7 @@ export default {
     }
     .right-section {
         @apply col-span-12;
+        margin-top: 1rem;
     }
-}
-.p-definition-table::v-deep {
-    border-width: 0;
 }
 </style>
