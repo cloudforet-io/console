8030c157f0314ad15212cf6162bfa09ce360711d
yuda <aydha0110@gmail.com>
2021-07-08 11:38:48 +0900

    fix: config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,84 +1,72 @@
 module.exports = {
-  root: true,
-
-  env: {
-    node: true
-  },
-
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-  ],
-
-  rules: {
-    "max-len": ["error", { code: 200 }],
-    indent: ["error", 4],
-    "prefer-destructuring": ["error", { object: false, array: false }],
-    radix: ["error", "as-needed"],
-    "no-prototype-builtins": "error",
-    "no-empty": ["error", { allowEmptyCatch: true }],
-    "import/extensions": [
-      "error",
-      "always",
-      {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never"
-      }
+    root: true,
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended'
     ],
-    camelcase: ["error", {
-        properties: "always",
-      }
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2018
+    },
+    plugins: [
+        '@typescript-eslint'
     ],
-    "no-this-before-super": ["off"],
-    "no-useless-constructor": ["off"],
-    "no-empty-function": ["error", { allow: ["constructors", "arrowFunctions"] }],
-    "no-param-reassign": ["error", { props: false }],
-    "no-underscore-dangle": ["off"],
-    "import/no-extraneous-dependencies": [
-      "error",
-      {
-        devDependencies: true
-      }
-    ],
-    "import/prefer-default-export": ["off"],
-    "no-new": ["off"],
-    "prefer-template": ["error"],
-    "no-plusplus": ["off"],
-    "no-tabs": ["off"],
-
-    // typescript rules
-    "@typescript-eslint/explicit-member-accessibility": [
-      "error",
-      {
-        accessibility: "no-public",
-        overrides: {
-          accessors: "no-public",
-          methods: "no-public",
-          properties: "no-public",
-          parameterProperties: "explicit"
-        }
-      }
-    ],
-    "@typescript-eslint/no-object-literal-type-assertion": ["off"],
-    "@typescript-eslint/no-parameter-properties": [
-      "error",
-      { allows: ["protected", "public"] }
-    ],
-    "@typescript-eslint/camelcase": ["off"], // use eslint camelcase rule
-    "@typescript-eslint/no-empty-function": ["off"], // use eslint no-empty-function rule
-    "@typescript-eslint/no-use-before-define": ["off"], // use eslint no-use-before-define rule
-    "@typescript-eslint/ban-ts-ignore": ["off"],
-    "@typescript-eslint/explicit-function-return-type": ["off"]
-  },
-  parserOptions: {
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
-    ecmaVersion: 2018
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {}
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+    },
+    globals: {
+    },
+    rules: {
+        'indent': ['error', 4, {
+            'ObjectExpression': 1,
+            'flatTernaryExpressions': true,
+            'ignoreComments': true,
+            'ArrayExpression': 1
+        }],
+        'quotes': ['error', 'single'],
+        'comma-dangle': ['error', {
+            'arrays': 'never',
+            'objects': 'never',
+            'imports': 'never',
+            'exports': 'never',
+            'functions': 'never'
+        }],
+        'no-irregular-whitespace': ['error', {'skipComments': true}],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'max-len': ['error', { code: 200 }],
+        'no-empty': 'error',
+        'no-duplicate-imports': 'error',
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'semi': ['error', 'always'],
+        // typescript rules
+        '@typescript-eslint/explicit-member-accessibility': [
+            'error',
+            {
+                accessibility: 'no-public',
+                overrides: {
+                    accessors: 'no-public',
+                    methods: 'no-public',
+                    properties: 'no-public',
+                    parameterProperties: 'explicit'
+                }
+            }
+        ],
+        '@typescript-eslint/no-object-literal-type-assertion': ['off'],
+        '@typescript-eslint/no-parameter-properties': [
+            'error',
+            { allows: ['protected', 'public'] }
+        ],
+        '@typescript-eslint/camelcase': ['off'], // use eslint camelcase rule
+        '@typescript-eslint/no-empty-function': ['off'], // use eslint no-empty-function rule
+        '@typescript-eslint/no-use-before-define': ['off'], // use eslint no-use-before-define rule
+        '@typescript-eslint/ban-ts-ignore': ['off'],
+        '@typescript-eslint/explicit-function-return-type': ['off']
     }
-  }
 };
