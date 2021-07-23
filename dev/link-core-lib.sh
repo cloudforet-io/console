28e67bc9bc9ff90fb2b04ee7aa615c2eac5b2737
Dahyun Yu <aydha0110@gmail.com>
2021-07-23 14:12:34 +0900

    fix: edit link-core-lib (#1625)

--- dev/link-core-lib.sh
+++ dev/link-core-lib.sh
@@ -1,4 +1,6 @@
 cd ${CORE_LIB_PATH} &&
+cp -r package.json dist &&
+cd dist &&
 echo "make ConsoleCoreLib link" &&
 npm link &&
 cd ${CONSOLE_PATH} &&
--- dev/unlink-core-lib.sh
+++ dev/unlink-core-lib.sh
@@ -2,5 +2,8 @@ cd ${CONSOLE_PATH} &&
 echo "unlink ConsoleCoreLib from Console" &&
 npm unlink --no-save @spaceone/console-core-lib &&
 cd ${CORE_LIB_PATH} &&
-echo "unlink ConsoleCoreLib"
+cd dist &&
+rimraf package.json &&
+cd .. &&
+echo "unlink ConsoleCoreLib" &&
 npm unlink
