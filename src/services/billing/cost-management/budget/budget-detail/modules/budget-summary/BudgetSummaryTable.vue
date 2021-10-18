d69bf67a9d5a4677ffbb9da9ffb6928d00bbcd73
Siyeon Lee <sylee@megazone.com>
2021-10-18 13:44:54 +0900

    feat: add budget summary table (#1749)

--- src/services/billing/cost-management/budget/budget-detail/modules/budget-summary/BudgetSummaryTable.vue
+++ src/services/billing/cost-management/budget/budget-detail/modules/budget-summary/BudgetSummaryTable.vue
@@ -1,27 +1,82 @@
 <template>
-    <p-definition-table :fields="fields" :data="data"
-                        :skeleton-rows="3"
-                        :stripe="false"
-                        :disable-copy="true"
+    <p-data-table :fields="fields" :items="data"
+                  :skeleton-rows="3"
+                  :stripe="false"
+                  :disable-copy="true"
     />
 </template>
 
 <script lang="ts">
-import { PDefinitionTable } from '@spaceone/design-system';
-import { map } from 'lodash';
+import { PDataTable } from '@spaceone/design-system';
 import { computed, reactive, toRefs } from '@vue/composition-api';
+
+const tempData = [
+    {
+
+        date: '2021-08', limit: 100, usd_cost: 120,
+    },
+    {
+        date: '2021-07', limit: 100, usd_cost: 88,
+    },
+    {
+        date: '2021-06', limit: 120, usd_cost: 50,
+    },
+    {
+        date: '2021-05', limit: 100, usd_cost: 70,
+    },
+    {
+        date: '2021-04', limit: 100, usd_cost: 90,
+    },
+];
 
 export default {
     name: 'BudgetDetailSummaryTable',
     components: {
-        PDefinitionTable,
+        PDataTable,
     },
     setup() {
+        const getBudgetData = () => {
+            const headerData = {
+                date: '', limit: 'Budgeted', usd_cost: 'Actual Cost', ratio: 'current vs budget.',
+            };
+            const budgetDataWithRatio = tempData.map((d) => {
+                const ratio = Math.round((d.usd_cost / d.limit) * 100);
+                return {
+                    ...d, ratio,
+                };
+            });
+            return [headerData, ...budgetDataWithRatio];
+        };
         const state = reactive({
-            fields: computed(() => map(state.data, (d, k) => ({ name: k, label: k.replace(/\_/g, ' ') }))),
-            data: {},
+            budgetData: computed(() => getBudgetData()),
+            fields: computed(() => {
+                const res = state.budgetData.map(d => ({
+                    name: d.date,
+                    label: d.date,
+                }));
+                return res;
+            }),
+            data: [],
             loading: true,
         });
+
+        const setItems = (name, path) => {
+            const items = { name };
+            state.budgetData.forEach((d) => {
+                items[d.date] = d[path];
+            });
+            return items;
+        };
+
+        const setTableData = () => {
+            // set keys and items
+            const keys = [{ name: 'Budgeted', path: 'limit' }, { name: 'Actual Cost', path: 'usd_cost' }, { name: 'Current vs Budget.', path: 'ratio' }];
+            keys.forEach((key) => {
+                state.data.push(setItems(key.name, key.path));
+            });
+        };
+        setTableData();
+
         return {
             ...toRefs(state),
         };
