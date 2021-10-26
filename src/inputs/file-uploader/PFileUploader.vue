5ac7abb9ad9134ff7262f38cd57644c205cad75b
Siyeon Lee <sylee@megazone.com>
2021-10-26 17:05:46 +0900

    feat: add file uploader component (#708)
    * feat: add file uploader component
    
    * feat: add story of file uploader component
    
    * chore: change object to func
    
    * chore: add props type
    
    * fix: variable names

---
+++ src/inputs/file-uploader/PFileUploader.vue
@@ -0,0 +1,135 @@
+<template>
+    <div class="p-file-uploader">
+        <file-pond
+            ref="fileRef"
+            label-idle="Drag & Drop files here or
+            <span class='filepond--label-action'>Browse</span>
+            "
+            :allow-revert="false"
+            :allow-multiple="true"
+            :server="serverOption"
+            :accepted-file-types="ACCEPTED_FILE_TYPES"
+            @updatefiles="handleUpdateFiles"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+import vueFilePond from 'vue-filepond';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import {
+    ComponentRenderProxy, getCurrentInstance, reactive, toRefs,
+} from '@vue/composition-api';
+import { makeOptionalProxy } from '@/util/composition-helpers';
+import { FileUploaderProps } from '@/inputs/file-uploader/type';
+
+
+const FilePond = vueFilePond(
+    FilePondPluginFileValidateType,
+);
+const ACCEPTED_FILE_TYPES = [
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'application/vnd.ms-excel',
+    'application/x-msdownload',
+    'text/csv',
+];
+export default {
+    name: 'PFileUploader',
+    components: {
+        FilePond,
+    },
+    model: {
+        prop: 'uploadedFiles',
+        event: 'update:uploadedFiles',
+    },
+    props: {
+        uploadedFiles: {
+            type: Array,
+            default: () => ([]),
+        },
+        serverEndpoint: {
+            type: String,
+            default: '',
+        },
+    },
+    setup(props: FileUploaderProps) {
+        const vm = getCurrentInstance() as ComponentRenderProxy;
+        const state = reactive({
+            proxyUploadedFiles: makeOptionalProxy<File[]>('uploadedFiles', vm, props.uploadedFiles),
+            fileRef: null as Element|null,
+        });
+
+        const serverOption = {
+            process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
+                const formData = new FormData();
+                formData.append(fieldName, file, file.name);
+
+                const request = new XMLHttpRequest();
+                request.open('POST', props.serverEndpoint ?? '');
+                request.upload.onprogress = (e) => {
+                    progress(e.lengthComputable, e.loaded, e.total);
+                };
+                request.onload = () => {
+                    if (request.status >= 200 && request.status < 300) {
+                        load(request.responseText);
+                    } else {
+                        error('Upload Error!');
+                    }
+                };
+                request.send(formData);
+
+                return {
+                    abort: () => {
+                        request.abort();
+                        abort();
+                    },
+                };
+            },
+        };
+
+        const handleUpdateFiles = (files: File[]) => {
+            state.proxyUploadedFiles = files.map(fileItems => fileItems);
+        };
+
+        return {
+            ...toRefs(state),
+            ACCEPTED_FILE_TYPES,
+            serverOption,
+            handleUpdateFiles,
+        };
+    },
+};
+</script>
+
+<style lang="postcss">
+@import 'filepond/dist/filepond.min.css';
+
+.p-file-uploader {
+    .filepond--root .filepond--credits {
+        display: none;
+    }
+    .filepond--label-action {
+        @apply text-secondary;
+        text-decoration: underline;
+    }
+    .filepond--drop-label {
+        @apply bg-blue-200 rounded-md;
+    }
+    .filepond--drip {
+        @apply bg-blue-200;
+        opacity: 1;
+        min-height: 5rem;
+    }
+    .filepond--drop-label.filepond--drop-label label {
+        @apply text-gray-700;
+        font-size: 0.875rem;
+    }
+    .filepond--item-panel {
+        @apply rounded-md;
+    }
+
+    .filepond--file {
+        @apply rounded-md;
+    }
+}
+</style>
