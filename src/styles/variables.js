bc25711ad07a8abe6bbfa5f5f3686f31fddcd874
Songyi Kim <sykim@mz.co.kr>
2021-11-08 17:23:05 +0900

    feat: update assets & add icon (#736)
    Co-authored-by: kimsongyi <sykim@megazone.com>

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,7 +1,7 @@
 /** Usage: https://github.com/postcss/postcss-simple-vars
  * */
 module.exports = {
-    'gnb-height': '3rem',
+    'gnb-height': '3.25rem',
     'fnb-height': '2.5rem',
     'fa-font-path': '~@fortawesome/fontawesome-free/webfonts',
 };
