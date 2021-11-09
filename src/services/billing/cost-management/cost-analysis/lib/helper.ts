8ded61886b339d2690106cafb4863f329ae302b3
Dahyun Yu <aydha0110@gmail.com>
2021-11-09 13:53:04 +0900

    feat: bind api to Cost Analysis data table (#1802)
    * refactor: split code to helper
    
    * feat: bind api to Cost Analysis data table
    
    * chore: change variable name
    
    * refactor: use early return
    
    * chore: edit code

---
+++ src/services/billing/cost-management/cost-analysis/lib/helper.ts
@@ -0,0 +1,26 @@
+import { Granularity } from '@/services/billing/cost-management/cost-analysis/store/type';
+import dayjs, { Dayjs } from 'dayjs';
+import { GRANULARITY } from '@/services/billing/cost-management/cost-analysis/lib/config';
+import { TimeUnit } from '@amcharts/amcharts4/core';
+
+
+export const getConvertedGranularity = (selectedDates: Array<string>, granularity: Granularity): Granularity => {
+    const start = dayjs(selectedDates[0]);
+    const end = dayjs(selectedDates[1]);
+
+    if (granularity !== GRANULARITY.ACCUMULATED) return granularity;
+    if (end.diff(start, 'month') < 2) return GRANULARITY.DAILY;
+    if (end.diff(start, 'year') < 2) return GRANULARITY.MONTHLY;
+    return GRANULARITY.YEARLY;
+};
+
+export const getTimeUnit = (granularity: Granularity, start: Dayjs, end: Dayjs): TimeUnit => {
+    if (granularity !== GRANULARITY.ACCUMULATED) {
+        if (granularity === GRANULARITY.DAILY) return 'day';
+        if (granularity === GRANULARITY.MONTHLY) return 'month';
+        return 'year';
+    }
+    if (end.diff(start, 'month') < 2) return 'day';
+    if (end.diff(start, 'year') < 2) return 'month';
+    return 'year';
+};
--- src/services/billing/cost-management/cost-analysis/modules/CostAnalysisDataTable.vue
+++ src/services/billing/cost-management/cost-analysis/modules/CostAnalysisDataTable.vue
@@ -1,14 +1,74 @@
 <template>
-    <p-toolbox-table :fields="fields"
-                     :exportable="true"
-    />
+    <p-toolbox-table :loading="tableState.loading"
+                     :fields="tableState.fields"
+                     :items="tableState.items"
+                     :total-count="tableState.totalCount"
+                     :sort-desc="true"
+                     sort-by="total_count"
+                     searchable
+                     exportable
+                     sortable
+                     @change="handleChange"
+                     @refresh="handleChange()"
+    >
+        <template v-for="field in tableState.fields" v-slot:[`col-${field.name}-format`]="{value, index}">
+            <div :key="`${field.name}-${index}-${value}`">
+                <span v-if="field.name === GROUP_BY_ITEM.PROJECT">
+                    {{ projects[value] ? projects[value].label : value }}
+                </span>
+                <span v-else-if="field.name === GROUP_BY_ITEM.PROVIDER">
+                    {{ providers[value] ? providers[value].label : value }}
+                </span>
+                <span v-else-if="field.name === GROUP_BY_ITEM.REGION">
+                    {{ regions[value] ? regions[value].name : value }}
+                </span>
+                <span v-else>
+                    {{ value ? value : '--' }}
+                </span>
+            </div>
+        </template>
+    </p-toolbox-table>
 </template>
 
 <script lang="ts">
-import { computed, reactive, toRefs } from '@vue/composition-api';
+import dayjs from 'dayjs';
+
+import {
+    computed, reactive, toRefs, watch,
+} from '@vue/composition-api';
+
 import { PToolboxTable } from '@spaceone/design-system';
+import { DataTableField } from '@spaceone/design-system/dist/src/data-display/tables/data-table/type';
+
+import { SpaceConnector } from '@spaceone/console-core-lib/space-connector';
+import { ApiQueryHelper } from '@spaceone/console-core-lib/space-connector/helper';
+import { commaFormatter, numberFormatter } from '@spaceone/console-core-lib';
+import { setApiQueryWithToolboxOptions } from '@spaceone/console-core-lib/component-util/toolbox';
+
+import { Granularity, GroupByItem } from '@/services/billing/cost-management/cost-analysis/store/type';
+import { getConvertedGranularity, getTimeUnit } from '@/services/billing/cost-management/cost-analysis/lib/helper';
+import { GROUP_BY_ITEM } from '@/services/billing/cost-management/cost-analysis/lib/config';
+import ErrorHandler from '@/common/composables/error/errorHandler';
 import { store } from '@/store';
 
+
+interface CostAnalysisItem {
+    [key: string]: string | number | object;
+}
+
+interface TableData {
+    [key: string]: any;
+}
+
+interface Value {
+    usd_cost: number;
+    date: string;
+}
+
+interface RawTableData {
+    [key: string]: any;
+    values: Array<Value>;
+}
 
 export default {
     name: 'CostAnalysisDataTable',
@@ -18,20 +78,130 @@ export default {
     props: {},
     setup() {
         const state = reactive({
-            timezone: computed(() => store.state.user.timezone),
+            projects: computed(() => store.state.resource.project.items),
+            providers: computed(() => store.state.resource.provider.items),
+            regions: computed(() => store.state.resource.region.items),
+            //
             granularity: computed(() => store.state.service.costAnalysis.granularity),
             currency: computed(() => store.state.service.costAnalysis.currency),
             selectedDates: computed(() => store.state.service.costAnalysis.selectedDates),
             groupByItems: computed(() => store.state.service.costAnalysis.groupByItems),
-            // todo: 표에 영향을 주는 데이터: granularity, group by, period, filter, currency
-            fields: computed(() => ([
-                { name: 'project_group', label: 'Project Group' },
-                { name: '2021-08-01', label: '8/1' },
-            ])),
         });
+        const tableState = reactive({
+            loading: true,
+            fields: [] as Array<DataTableField>,
+            items: [] as Array<CostAnalysisItem>,
+            totalCount: 0,
+        });
+
+        /* util */
+        const setTableFields = (granularity: Granularity, groupByItems: Array<GroupByItem>) => {
+            /* get group by fields (ex. Provider, Region) */
+            const groupByFields: Array<DataTableField> = [...groupByItems];
+            if (!groupByItems.length) {
+                groupByFields.push({
+                    name: 'total_cost', label: ' ', sortable: false,
+                });
+            }
+
+            /* get date fields (ex. 11/1, 11/2) */
+            const dateFields: Array<DataTableField> = [];
+            const start = dayjs(state.selectedDates[0]);
+            const end = dayjs(state.selectedDates[1]);
+            const timeUnit = getTimeUnit(granularity, start, end);
+
+            let nameDateFormat = 'YYYY-MM-DD';
+            let labelDateFormat = 'M/D';
+            if (timeUnit === 'month') {
+                nameDateFormat = 'YYYY-MM';
+                labelDateFormat = 'MMM YYYY';
+            } else if (timeUnit === 'year') {
+                nameDateFormat = 'YYYY';
+                labelDateFormat = 'YYYY';
+            }
+
+            let now = start;
+            while (now.isSameOrBefore(end, timeUnit)) {
+                dateFields.push({
+                    name: now.format(nameDateFormat),
+                    label: now.format(labelDateFormat),
+                    sortable: false,
+                });
+                now = now.add(1, timeUnit);
+            }
+
+            tableState.fields = groupByFields.concat(dateFields);
+        };
+        const getTableDataFromRawData = (rawData: Array<RawTableData>, groupByItems: Array<GroupByItem>): Array<TableData> => {
+            const tableData: Array<TableData> = [];
+            rawData.forEach((eachRawData) => {
+                const rowData: TableData = {};
+
+                /* extract group by data (ex. { provider: 'aws', region_code: 'us-west-1' }) */
+                if (groupByItems.length) {
+                    groupByItems.forEach((item) => {
+                        rowData[item.name] = eachRawData[item.name];
+                    });
+                } else {
+                    rowData.total_cost = 'Total Cost';
+                }
+
+                /* extract data per each date (ex. { 2021-11-01: '29.4K', 2021-11-02: '8,962' } ) */
+                eachRawData.values.forEach((value) => {
+                    rowData[value.date] = commaFormatter(numberFormatter(value.usd_cost));
+                });
+                tableData.push(rowData);
+            });
+            return tableData;
+        };
+
+        /* api */
+        const costApiQueryHelper = new ApiQueryHelper()
+            .setPageStart(1).setPageLimit(15)
+            .setSort('total_count', true);
+        const listCostAnalysisTableData = async () => {
+            try {
+                const granularity = getConvertedGranularity(state.selectedDates, state.granularity);
+                const { results, total_count } = await SpaceConnector.client.costAnalysis.cost.analyze({
+                    granularity,
+                    group_by: state.groupByItems.map(d => d.name),
+                    filter: [],
+                    start: state.selectedDates[0],
+                    end: state.selectedDates[1],
+                    pivot_type: 'TABLE',
+                    ...costApiQueryHelper.data,
+                });
+                tableState.items = getTableDataFromRawData(results, state.groupByItems);
+                tableState.totalCount = total_count;
+            } catch (e) {
+                tableState.items = [];
+                tableState.totalCount = 0;
+                ErrorHandler.handleError(e);
+            }
+        };
+
+        /* event */
+        const handleChange = async (options: any = {}) => {
+            setApiQueryWithToolboxOptions(costApiQueryHelper, options, { queryTags: true });
+            await listCostAnalysisTableData();
+        };
+        const handleRefresh = async () => {
+            // await getTableData();
+        };
+
+        watch([() => state.granularity, () => state.groupByItems, () => state.selectedDates], async () => {
+            tableState.loading = true;
+            await listCostAnalysisTableData();
+            await setTableFields(state.granularity, state.groupByItems);
+            tableState.loading = false;
+        }, { immediate: true });
 
         return {
             ...toRefs(state),
+            tableState,
+            GROUP_BY_ITEM,
+            handleChange,
+            handleRefresh,
         };
     },
 };
