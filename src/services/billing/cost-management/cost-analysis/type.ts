a4392afc6d1a3915287c66872c718103aa535610
WANJIN <wanjin@megazone.com>
2021-12-03 19:10:15 +0900

    chore: add CostAnalysisPageQueryValue type (#1900)

--- src/services/billing/cost-management/cost-analysis/type.ts
+++ src/services/billing/cost-management/cost-analysis/type.ts
@@ -1,5 +1,16 @@
 import { RouteQueryString } from '@/lib/router-query-string';
 import { COST_ANALYSIS_PAGE_URL_QUERY_KEY } from '@/services/billing/cost-management/cost-analysis/lib/config';
+import { CostQueryFilters, Period } from '@/services/billing/cost-management/cost-analysis/store/type';
+import { CHART_TYPE } from '@/services/billing/cost-management/widgets/lib/config';
+import { GRANULARITY } from '@/services/billing/cost-management/lib/config';
 
 
 export type CostAnalysisPageUrlQuery = Partial<Record<COST_ANALYSIS_PAGE_URL_QUERY_KEY, RouteQueryString>>
+
+export interface CostAnalysisPageQueryValue {
+    period?: Period;
+    groupBy?: string[];
+    filters?: CostQueryFilters;
+    chartType?: CHART_TYPE;
+    granularity?: GRANULARITY;
+}
