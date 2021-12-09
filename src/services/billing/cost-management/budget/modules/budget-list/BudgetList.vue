710013f8dd49ad9fa18dc1f8271e49fbaf9c00b0
WANJIN <wanjin@megazone.com>
2021-12-09 10:47:24 +0900

    feat: apply event bindings of BudgetToolbox in BudgetList (#1932)

--- src/services/billing/cost-management/budget/modules/budget-list/BudgetList.vue
+++ src/services/billing/cost-management/budget/modules/budget-list/BudgetList.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="budget-list">
-        <budget-toolbox />
+        <budget-toolbox @update-pagination="handleUpdatePagination"
+                        @update-period="handleUpdatePeriod"
+                        @update-filters="handleUpdateFilters"
+                        @refresh="handleRefresh"
+                        @export="handleExport"
+        />
         <budget-stat :loading="loading" :budgets="budgets" />
         <div class="budget-list-card-box">
             <budget-list-card v-for="budget in budgets" :key="budget.budget_id" :budget="budget"
@@ -16,9 +21,11 @@ import {
 } from '@vue/composition-api';
 
 import BudgetListCard from '@/services/billing/cost-management/budget/modules/budget-list/BudgetListCard.vue';
-import BudgetToolbox from '@/services/billing/cost-management/budget/modules/budget-toolbox/BudgetToolbox.vue';
+import BudgetToolbox, { Pagination } from '@/services/billing/cost-management/budget/modules/budget-toolbox/BudgetToolbox.vue';
 import BudgetStat from '@/services/billing/cost-management/budget/modules/budget-stat/BudgetStat.vue';
 import { BudgetData } from '@/services/billing/cost-management/budget/type';
+import { QueryStoreFilter } from '@spaceone/console-core-lib/query/type';
+import { Period } from '@/services/billing/cost-management/type';
 import ErrorHandler from '@/common/composables/error/errorHandler';
 import { SpaceConnector } from '@spaceone/console-core-lib/space-connector';
 
@@ -29,6 +36,11 @@ export default {
         const state = reactive({
             budgets: [] as BudgetData[],
             loading: false,
+            // query
+            pageStart: 1,
+            pageLimit: 24,
+            filters: [] as QueryStoreFilter[],
+            period: {} as Period,
         });
 
         const listBudget = async () => {
@@ -45,11 +57,44 @@ export default {
             }
         };
 
+
+        /* Handlers */
+        const handleUpdatePagination = ({ pageStart, pageLimit }: Pagination) => {
+            state.pageStart = pageStart;
+            state.pageLimit = pageLimit;
+            listBudget();
+        };
+
+        const handleUpdatePeriod = (period: Period) => {
+            state.period = period;
+            listBudget();
+        };
+
+        const handleUpdateFilters = (filters: QueryStoreFilter[]) => {
+            state.filters = filters;
+            listBudget();
+        };
+
+        const handleRefresh = () => {
+            listBudget();
+        };
+
+        const handleExport = () => {
+
+        };
+
+        /* Init */
         (async () => {
             await listBudget();
         })();
+
         return {
             ...toRefs(state),
+            handleUpdatePagination,
+            handleUpdatePeriod,
+            handleUpdateFilters,
+            handleRefresh,
+            handleExport,
         };
     },
 };
