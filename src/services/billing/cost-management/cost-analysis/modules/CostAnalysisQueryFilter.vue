c7526d03eae6106eb28a83b67257d9849d7db6ed
Dahyun Yu <aydha0110@gmail.com>
2021-12-29 18:31:23 +0900

    fix: limit 12 months to range modal (#2068)
    * fix: hide yearly option from cost analysis
    
    * fix: limit to 12 months

--- src/services/billing/cost-management/cost-analysis/modules/CostAnalysisQueryFilter.vue
+++ src/services/billing/cost-management/cost-analysis/modules/CostAnalysisQueryFilter.vue
@@ -92,11 +92,11 @@ export default {
                     name: GRANULARITY.MONTHLY,
                     label: i18n.t('BILLING.COST_MANAGEMENT.COST_ANALYSIS.MONTHLY'),
                 },
-                {
-                    type: 'item',
-                    name: GRANULARITY.YEARLY,
-                    label: i18n.t('BILLING.COST_MANAGEMENT.COST_ANALYSIS.YEARLY'),
-                },
+                // {
+                //     type: 'item',
+                //     name: GRANULARITY.YEARLY,
+                //     label: i18n.t('BILLING.COST_MANAGEMENT.COST_ANALYSIS.YEARLY'),
+                // },
             ])),
             setQueryModalVisible: false,
         });
