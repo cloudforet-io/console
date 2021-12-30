86641a4d48c4996f3e1c03ac9f8f1a8f629246c4
WANJIN <wanjin@megazone.com>
2021-12-30 12:47:48 +0900

    fix: attach display plugins to store plugins (#2091)

--- src/store/plugins.ts
+++ src/store/plugins.ts
@@ -1,11 +1,13 @@
 import createLogger from 'vuex/dist/logger';
 import userPlugins from '@/store/modules/user/plugins';
 import settingsPlugins from '@/store/modules/settings/plugins';
+import displayPlugins from '@/store/modules/display/plugins';
 
 const DEBUG = process.env.NODE_ENV !== 'production';
 const PLUGINS = [
     ...userPlugins,
     ...settingsPlugins,
+    ...displayPlugins,
 ];
 
 export default DEBUG ? [createLogger(), ...PLUGINS] : PLUGINS;
