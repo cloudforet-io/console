8e73a54c771a0234f64de87bc64358c6d2acbcce
Dahyun Yu <aydha0110@gmail.com>
2022-01-18 10:14:27 +0900

    fix: fix chart bugs (#2117)
    * fix: delete period input from Set Query Modal
    
    * fix: delete unused chart hook
    
    * chore: delete refresh button
    
    * fix: change DateAxis to CategoryAxis
    
    * fix: select 'custom' item when there's selected period
    
    * fix: fix tooltip bug

--- src/services/billing/cost-management/cost-analysis/composables/cost-analysis-dynamic-chart/index.ts
+++ src/services/billing/cost-management/cost-analysis/composables/cost-analysis-dynamic-chart/index.ts
@@ -1,52 +1,17 @@
-import * as am4charts from '@amcharts/amcharts4/charts';
-import * as am4core from '@amcharts/amcharts4/core';
 import { PieChart, XYChart } from '@amcharts/amcharts4/charts';
 
 import {
     reactive,
 } from '@vue/composition-api';
 
-import {
-    gray,
-} from '@/styles/colors';
-
 import drawStackedColumnChart from '@/services/billing/cost-management/cost-analysis/composables/cost-analysis-dynamic-chart/draw-stacked-column-chart';
-import drawColumnChart from '@/services/billing/cost-management/cost-analysis/composables/cost-analysis-dynamic-chart/draw-column-chart';
-import drawLineChart from '@/services/billing/cost-management/cost-analysis/composables/cost-analysis-dynamic-chart/draw-line-chart';
-import drawStackedLineChart from '@/services/billing/cost-management/cost-analysis/composables/cost-analysis-dynamic-chart/draw-stacked-line-chart';
 import drawPieChart from '@/services/billing/cost-management/cost-analysis/composables/cost-analysis-dynamic-chart/draw-pie-chart';
 
 import { DynamicChartStateArgs } from '@/services/billing/cost-management/cost-analysis/composables/cost-analysis-dynamic-chart/type';
 import { CHART_TYPE } from '@/services/billing/cost-management/widgets/lib/config';
 
 
-const createCursor = (chart) => {
-    chart.cursor = new am4charts.XYCursor();
-    chart.cursor.maxTooltipDistance = 20;
-    chart.cursor.lineX.stroke = am4core.color(gray[900]);
-    chart.cursor.lineX.strokeDasharray = '';
-    chart.cursor.lineX.strokeOpacity = 1;
-    chart.cursor.lineY.stroke = am4core.color(gray[900]);
-    chart.cursor.lineY.strokeDasharray = '';
-    chart.cursor.lineY.strokeOpacity = 1;
-};
-
 /* hooks */
-const useColumnChart = ({
-    data, valueOptions, categoryOptions, chartContainer,
-}: DynamicChartStateArgs) => {
-    const state = reactive({
-        data, valueOptions, categoryOptions, chartContainer,
-    });
-
-    const chart = drawColumnChart(state.data, state.chartContainer, state.valueOptions, state.categoryOptions);
-    createCursor(chart);
-
-    return {
-        chart,
-    };
-};
-
 const useStackedColumnChart = ({
     data, valueOptions, categoryOptions, chartContainer,
 }: DynamicChartStateArgs) => {
@@ -55,37 +20,6 @@ const useStackedColumnChart = ({
     });
 
     const chart = drawStackedColumnChart(state.data, state.chartContainer, state.valueOptions, state.categoryOptions);
-    createCursor(chart);
-
-    return {
-        chart,
-    };
-};
-
-const useLineChart = ({
-    data, valueOptions, categoryOptions, chartContainer,
-}: DynamicChartStateArgs) => {
-    const state = reactive({
-        data, valueOptions, categoryOptions, chartContainer,
-    });
-
-    const chart = drawLineChart(state.data, state.chartContainer, state.valueOptions, state.categoryOptions);
-    createCursor(chart);
-
-    return {
-        chart,
-    };
-};
-
-const useStackedLineChart = ({
-    data, valueOptions, categoryOptions, chartContainer,
-}: DynamicChartStateArgs) => {
-    const state = reactive({
-        data, valueOptions, categoryOptions, chartContainer,
-    });
-
-    const chart = drawStackedLineChart(state.data, state.chartContainer, state.valueOptions, state.categoryOptions);
-    createCursor(chart);
 
     return {
         chart,
@@ -111,12 +45,6 @@ export const useDynamicChart = (chartType: CHART_TYPE, params: DynamicChartState
     let chart: XYChart|PieChart;
     if (chartType === CHART_TYPE.STACKED_COLUMN) {
         ({ chart } = useStackedColumnChart(params));
-    } else if (chartType === CHART_TYPE.COLUMN) {
-        ({ chart } = useColumnChart(params));
-    } else if (chartType === CHART_TYPE.LINE) {
-        ({ chart } = useLineChart(params));
-    } else if (chartType === CHART_TYPE.STACKED_LINE) {
-        ({ chart } = useStackedLineChart(params));
     } else if (chartType === CHART_TYPE.DONUT) {
         ({ chart } = usePieChart(params));
     } else {
