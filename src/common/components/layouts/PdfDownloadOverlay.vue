a464992cb4b7a5df8314c3a7057abee80bc7b07a
WANJIN <wanjin@megazone.com>
2022-02-15 17:49:23 +0900

    feat: add pdf download overlay (#2228)

---
+++ src/common/components/layouts/PdfDownloadOverlay.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="pdf-download-overlay">
+        <div class="preview">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import {
+    reactive, toRefs,
+} from '@vue/composition-api';
+
+
+export default {
+    name: 'PdfDownloadOverlay',
+    components: {},
+    props: {
+        loading: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    setup() {
+        const state = reactive({});
+
+
+        return {
+            ...toRefs(state),
+        };
+    },
+};
+</script>
+
+<style lang="postcss" scoped>
+.pdf-download-overlay {
+    height: 100vh;
+}
+</style>
