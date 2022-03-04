60ee7ab1ec8a1b95ba40605e6f7a3645b3765904
Dahyun Yu <aydha0110@gmail.com>
2022-03-04 14:16:40 +0900

    fix: show widget name even when loading or no data (#2331)

--- src/services/billing/cost-management/widgets/modules/CostDashboardSimpleCardWidget.vue
+++ src/services/billing/cost-management/widgets/modules/CostDashboardSimpleCardWidget.vue
@@ -2,30 +2,32 @@
     <p-pane-layout class="card-widget-layout"
                    :class="{'inactive': !loading && noData}"
     >
-        <div v-if="loading" class="flex flex-col">
-            <p-skeleton width="11rem" height="3rem" />
-            <p-skeleton width="9.5rem" height="1rem" class="mt-1 mb-6" />
-            <p-skeleton width="14rem" height="1.75rem" />
-        </div>
-        <div v-if="!loading && noData" class="no-data">
-            No Item
-        </div>
-        <div v-else-if="!loading && !noData" class="w-full">
-            <p class="title-wrapper">
-                <span class="title">{{ title }}</span>
-                <span v-if="$scopedSlots['title-extra']" class="title-extra">
-                    <slot name="title-extra" />
-                </span>
-            </p>
-            <p class="value-wrapper">
-                <span v-if="unitType === UNIT_TYPE.CURRENCY" class="unit-type">{{ currencySymbol }}</span>
-                <span class="value">{{ formattedValue }}</span>
-                <span v-if="unitType === UNIT_TYPE.PERCENT" class="unit-type">%</span>
-            </p>
-            <span class="description">{{ description }}</span>
-            <p-divider v-if="showDivider" class="divider" />
-            <slot />
-            <router-link v-if="widgetLink" :to="widgetLink" />
+        <p class="title-wrapper">
+            <span class="title">{{ title }}</span>
+            <span v-if="$scopedSlots['title-extra']" class="title-extra">
+                <slot name="title-extra" />
+            </span>
+        </p>
+        <div class="content-wrapper flex">
+            <div v-if="loading" class="flex flex-col">
+                <p-skeleton width="11rem" height="3rem" />
+                <p-skeleton width="9.5rem" height="1rem" class="mt-1 mb-6" />
+                <p-skeleton width="14rem" height="1.75rem" />
+            </div>
+            <div v-if="!loading && noData" class="no-data">
+                No Item
+            </div>
+            <div v-else-if="!loading && !noData" class="w-full">
+                <p class="value-wrapper">
+                    <span v-if="unitType === UNIT_TYPE.CURRENCY" class="unit-type">{{ currencySymbol }}</span>
+                    <span class="value">{{ formattedValue }}</span>
+                    <span v-if="unitType === UNIT_TYPE.PERCENT" class="unit-type">%</span>
+                </p>
+                <span class="description">{{ description }}</span>
+                <p-divider v-if="showDivider" />
+                <slot />
+                <router-link v-if="widgetLink" :to="widgetLink" />
+            </div>
         </div>
     </p-pane-layout>
 </template>
@@ -129,7 +131,7 @@ export default defineComponent<Props>({
 
 <style lang="postcss" scoped>
 .card-widget-layout {
-    @apply relative flex;
+    @apply relative block;
     width: 100%;
     height: 100%;
     min-height: 12.8125rem;
@@ -144,6 +146,9 @@ export default defineComponent<Props>({
         margin-bottom: 0.25rem;
         font-size: 1rem;
         line-height: 160%;
+    }
+    .content-wrapper {
+        height: calc(100% - 2rem);
     }
     .value-wrapper {
         @apply text-gray-900;
@@ -167,7 +172,7 @@ export default defineComponent<Props>({
         font-size: 0.875rem;
         line-height: 150%;
     }
-    .divider {
+    .p-divider {
         margin-top: 0.75rem;
         margin-bottom: 0.75rem;
     }
