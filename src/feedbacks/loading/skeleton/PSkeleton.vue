85e9130498437013f2f8799c9387a359eb03a12e
Minseo Lee <minseolee@mz.co.kr>
2022-03-10 14:21:43 +0900

    feat: create opacity props on PSkeleton (#888)
    * feat: create opacity props on PSkeleton
    
    * Update src/feedbacks/loading/skeleton/PSkeleton.stories.mdx
    
    Co-authored-by: WANJIN <wanjin911003@gmail.com>
    
    * chore: update fsevents as 2.3.2
    
    fix type error
    
    Co-authored-by: WANJIN <wanjin911003@gmail.com>

--- src/feedbacks/loading/skeleton/PSkeleton.vue
+++ src/feedbacks/loading/skeleton/PSkeleton.vue
@@ -43,12 +43,16 @@ export default {
             type: Boolean,
             default: true,
         },
+        opacity: {
+            type: Number,
+            default: 0.4,
+        },
     },
     render(h, {
         props, slots, data,
     }) {
         const {
-            loading, duration, width, height, tag, animation,
+            loading, duration, width, height, tag, animation, opacity,
         } = props;
         const style: CSSStyleDeclaration = {} as CSSStyleDeclaration;
         const showLoading = loading || isEmptyVNode(slots().default);
@@ -56,6 +60,7 @@ export default {
         if (showLoading) {
             if (width) style.width = width;
             if (height) style.height = height;
+            style.opacity = opacity;
 
             if (animation) {
                 style.backgroundImage = `linear-gradient(90deg, ${gray[200]}, ${gray[100]}, ${gray[200]})`;
@@ -86,7 +91,6 @@ export default {
 <style lang="postcss">
 .p-skeleton {
     @apply rounded-lg inline-block w-full bg-gray-200;
-    opacity: 0.4;
     height: 0.875rem;
     line-height: inherit;
     background-repeat: no-repeat;
