7c6a5dc17f36bcf3c43408ab5ecb624e152f927d
Songyi Kim <sykim@mz.co.kr>
2022-03-18 13:30:14 +0900

    style: add no data & loading case markup to GNB Search (#2401)
    Co-authored-by: kimsongyi <sykim@megazone.com>

--- src/common/modules/navigations/gnb/modules/gnb-search/GNBSearch.vue
+++ src/common/modules/navigations/gnb/modules/gnb-search/GNBSearch.vue
@@ -23,6 +23,12 @@
                        :data="allSuggestionItems"
                        :loading="loading"
         >
+            <template #loader>
+                <div class="skeleton-wrapper">
+                    <p-skeleton width="36%" height="1rem" class="mb-2" />
+                    <p-skeleton width="100%" height="2rem" />
+                </div>
+            </template>
             <g-n-b-search-suggestion-list title="recent searches menu"
                                           :items="menuItems"
                                           :input-text="inputText"
@@ -35,11 +41,19 @@
                                           default-icon="ic_provider_other"
             />
             <template #no-data>
-                <div v-if="trimmedInputText">
+                <div v-if="trimmedInputText" class="no-data">
                     <!-- TODO: no data case -->
+                    <img src="@/assets/images/illust_ghost.svg" class="no-data-img">
+                    <p class="no-data-text">
+                        Search for navigation menus or cloud services.
+                    </p>
                 </div>
-                <div v-else>
+                <div v-else class="no-data">
                     <!-- TODO: before searching case -->
+                    <img src="@/assets/images/illust_microscope.svg" class="no-data-img">
+                    <p class="no-data-text">
+                        No result found for "<em>search keyword</em>" <br>Try again with different term.
+                    </p>
                 </div>
             </template>
         </p-data-loader>
@@ -51,7 +65,9 @@ import {
     computed,
     reactive, toRefs,
 } from '@vue/composition-api';
-import { PDataLoader, PDivider, PI } from '@spaceone/design-system';
+import {
+    PDataLoader, PDivider, PI, PSkeleton,
+} from '@spaceone/design-system';
 import GNBSearchSuggestionList, { Item as SuggestionItem } from '@/common/modules/navigations/gnb/modules/gnb-search/modules/GNBSearchSuggestionList.vue';
 
 export default {
@@ -61,6 +77,7 @@ export default {
         PDivider,
         PDataLoader,
         PI,
+        PSkeleton,
     },
     setup() {
         const state = reactive({
@@ -98,5 +115,27 @@ export default {
 <style lang="postcss" scoped>
 .gnb-search {
     /* TODO */
+    .p-data-loader {
+        @apply bg-white;
+        width: 27.5rem;
+        padding: 0 0.75rem;
+        min-height: 14.875rem;
+    }
+    .skeleton-wrapper {
+        @apply flex flex-col w-full self-start;
+    }
+    .no-data {
+        padding: 2.5rem 0;
+        .no-data-img {
+            @apply ml-auto mr-auto;
+        }
+        .no-data-text {
+            margin-top: 1.5rem;
+            font-size: 0.875rem;
+            em {
+                @apply font-bold;
+            }
+        }
+    }
 }
 </style>
