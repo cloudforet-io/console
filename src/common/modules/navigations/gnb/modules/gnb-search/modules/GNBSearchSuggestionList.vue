de7648a73eb2daffe0af08068a6aca400af600a9
WANJIN <wanjin@megazone.com>
2022-03-24 18:00:59 +0900

    feat: apply search text highlighting in gnb search (#2429)
    * feat: apply search text highlighting in gnb search
    
    * refactor: remove unnecessary code

--- src/common/modules/navigations/gnb/modules/gnb-search/modules/GNBSearchSuggestionList.vue
+++ src/common/modules/navigations/gnb/modules/gnb-search/modules/GNBSearchSuggestionList.vue
@@ -21,11 +21,23 @@
             <span>
                 <template v-if="item.parents">
                     <template v-for="parent in item.parents">
-                        <span :key="item.name + parent.name">{{ parent.label }}</span>
+                        <span v-for="(text, i) in parent.label.split(searchRegex)"
+                              :key="`parent-label-${text}-${i}`"
+                        >
+                            <span v-if="i !== 0"
+                                  class="matched-character"
+                            >{{ getMatchText(parent.label) }}</span><span>{{ text }}</span>
+                        </span>
                         <p-i :key="item.name + parent.name + 'arrow'" name="ic_breadcrumb_arrow" />
                     </template>
                 </template>
-                <span>{{ item.label }}</span>
+                <span v-for="(text, i) in item.label.split(searchRegex)"
+                      :key="`label-${text}-${i}`"
+                >
+                    <span v-if="i !== 0"
+                          class="matched-character"
+                    >{{ getMatchText(item.label) }}</span><span>{{ text }}</span>
+                </span>
             </span>
         </template>
     </p-context-menu>
@@ -33,6 +45,7 @@
 
 <script lang="ts">
 import {
+    computed,
     defineComponent, PropType,
     reactive, toRefs, watch,
 } from '@vue/composition-api';
@@ -83,7 +96,26 @@ export default defineComponent<Props>({
     setup(props, { emit }) {
         const state = reactive({
             contextMenuRef: null as null | any,
+            searchRegex: computed(() => {
+                let regex = '';
+                if (props.inputText) {
+                    // remove spaces in the search term
+                    const text = props.inputText.replace(/\s/g, '');
+                    for (let i = 0; i < text.length; i++) {
+                        regex += text[i];
+                        // add space regex after every single character to find matching keywords ignoring spaces
+                        if (i < text.length - 1) regex += '\\s*';
+                    }
+                }
+                return new RegExp(regex, 'i');
+            }),
         });
+
+        const getMatchText = (text: string): string => {
+            const res = state.searchRegex.exec(text);
+            if (res) return res[0];
+            return '';
+        };
 
         const handleSelect = (item, index) => {
             emit('select', item, index);
@@ -101,6 +133,7 @@ export default defineComponent<Props>({
 
         return {
             ...toRefs(state),
+            getMatchText,
             handleSelect,
         };
     },
@@ -110,5 +143,9 @@ export default defineComponent<Props>({
 <style lang="postcss" scoped>
 .gnb-search-suggestion-list {
     @apply bg-white;
+
+    .matched-character {
+        @apply text-blue-700 bg-blue-200;
+    }
 }
 </style>
