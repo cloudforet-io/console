03ce84dd7525f5401a8bdb313c03351435d75340
WANJIN <wanjin@megazone.com>
2022-04-20 15:17:56 +0900

    feat: add text highlighting component (#2631)

---
+++ src/common/components/text/text-highlighting/TextHighlighting.vue
@@ -0,0 +1,115 @@
+<template>
+    <span class="text-highlighting" :class="styleType">
+        <span v-for="({text, matched}, i) in getTextList(text)"
+              :key="`label-${text}-${i}`"
+        >
+            <span :class="{'matched-character': matched}">{{ text }}</span>
+        </span>
+    </span>
+</template>
+
+<script lang="ts">
+import {
+    computed, defineComponent, PropType, reactive, toRefs,
+} from '@vue/composition-api';
+import { getTextHighlightRegex } from '@/common/components/text/text-highlighting/helper';
+
+const STYLE_TYPE = ['primary', 'secondary'] as const;
+type StyleType = typeof STYLE_TYPE[number];
+
+interface Props {
+    text: string;
+    term: string;
+    styleType: StyleType;
+}
+
+export default defineComponent<Props>({
+    name: 'TextHighlighting',
+    components: {},
+    props: {
+        text: {
+            type: String,
+            default: '',
+        },
+        term: {
+            type: String,
+            default: '',
+        },
+        styleType: {
+            type: String as PropType<StyleType>,
+            default: STYLE_TYPE[0],
+            validator(styleType: StyleType) {
+                return STYLE_TYPE.includes(styleType);
+            },
+        },
+    },
+    setup(props) {
+        const state = reactive({
+            regex: computed(() => getTextHighlightRegex(props.term)),
+        });
+
+        const getFirstMatchedString = (str: string): string => {
+            const matched = state.regex.exec(str);
+            if (matched?.index === 0) {
+                return matched[0];
+            }
+            return '';
+        };
+
+        const getTextList = (str = ''): {text: string; matched: boolean}[] => {
+            if (!props.term?.trim()) return [{ text: str, matched: false }];
+
+            const regex = state.regex;
+            const unmatchedTexts = str.split(regex);
+            const textList: {text: string; matched: boolean}[] = [];
+
+            let remainedStr = str;
+
+            const setMatchedItem = () => {
+                const text = getFirstMatchedString(remainedStr);
+                if (text) {
+                    textList.push({ text, matched: true });
+                    remainedStr = remainedStr.slice(text.length);
+                }
+            };
+
+            setMatchedItem();
+            unmatchedTexts.forEach((t) => {
+                if (t) {
+                    textList.push({ text: t, matched: false });
+                    remainedStr = remainedStr.slice(t.length);
+                }
+                if (remainedStr) {
+                    setMatchedItem();
+                }
+            });
+
+            return textList;
+        };
+
+        return {
+            ...toRefs(state),
+            getTextList,
+        };
+    },
+});
+
+</script>
+
+<style lang="postcss" scoped>
+.text-highlighting {
+    color: inherit;
+    font-weight: inherit;
+    font-size: inherit;
+    &.primary {
+        .matched-character {
+            @apply text-blue-700 bg-blue-200;
+        }
+    }
+    &.secondary {
+        .matched-character {
+            font-weight: bold;
+        }
+    }
+}
+</style>
