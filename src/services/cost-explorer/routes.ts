dc04c390c2986a4383621598e47104991b567192
Sian <sylee@megazone.com>
2022-04-27 07:39:02 +0900

    fix: minor issues (#2685)

--- src/services/cost-explorer/routes.ts
+++ src/services/cost-explorer/routes.ts
@@ -78,6 +78,7 @@ export default {
                     path: ':budgetId',
                     name: COST_EXPLORER_ROUTE.BUDGET.DETAIL._NAME,
                     props: true,
+                    meta: { lnbVisible: true },
                     component: BudgetDetailPage,
                 },
             ],
