6337067cd2bbf8af8cdfdc4d71051b13da6cf1f2
Minseo Lee <minseolee@mz.co.kr>
2022-05-03 15:19:17 +0900

    feat: administration policy markup (#2736)
    * feat: create policy page
    
    route config
    
    * feat: create policy page
    
    set LNB
    
    * feat: administration policy markup
    
    set up ground rules

--- src/services/administration/iam/policy/PolicyPage.vue
+++ src/services/administration/iam/policy/PolicyPage.vue
@@ -1,9 +1,41 @@
 <template>
-    <p>PolicyPage</p>
+    <section>
+        <p-page-title
+            title="Policy"
+            use-total-count
+            :total-count="1"
+        >
+            <template #extra>
+                <router-link to="#">
+                    <p-icon-text-button
+                        style-type="primary-dark"
+                        name="ic_plus_bold"
+                    >
+                        {{ $t('PLUGIN.COLLECTOR.MAIN.CREATE') }}
+                    </p-icon-text-button>
+                </router-link>
+            </template>
+        </p-page-title>
+        <policy-list-data-table />
+    </section>
 </template>
 
 <script lang="ts">
+import { PPageTitle, PIconTextButton } from '@spaceone/design-system';
+import { ADMINISTRATION_ROUTE } from '@/services/administration/route-config';
+import PolicyListDataTable from '@/services/administration/iam/policy/modules/PolicyListDataTable.vue';
+
 export default {
     name: 'PolicyPage',
+    components: {
+        PPageTitle,
+        PIconTextButton,
+        PolicyListDataTable,
+    },
+    setup() {
+        return {
+            ADMINISTRATION_ROUTE,
+        };
+    },
 };
 </script>
