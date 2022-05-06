fc1b74d5a484c41c1924cc57971b6f699a8d910c
Dahyun Yu <aydha0110@gmail.com>
2022-05-06 11:51:46 +0900

    chore: edit eslintrc.js (#2750)

--- .eslintrc.js
+++ .eslintrc.js
@@ -110,6 +110,7 @@ module.exports = {
         '@typescript-eslint/no-use-before-define': ['off'], // use eslint no-use-before-define rule
         '@typescript-eslint/ban-ts-ignore': ['off'],
         '@typescript-eslint/explicit-function-return-type': ['off'],
+        "@typescript-eslint/explicit-module-boundary-types": ['off'],
         '@typescript-eslint/no-explicit-any': ['off'],
         '@typescript-eslint/no-unused-vars': ['error']
     },
