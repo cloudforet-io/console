d3e4e8a296e8d30436514e1552449718b8cd61c6
Minseo Lee <minseolee@mz.co.kr>
2022-05-12 23:26:00 +0900

    feat: connect create api to policy create page (#2772)
    * feat: connect create api to policy create page
    
    * feat: connect create api to policy create page
    
    * feat: connect create api to policy create page
    
    Approved PR comments

--- src/services/administration/iam/policy/policy-create/PolicyCreatePage.vue
+++ src/services/administration/iam/policy/policy-create/PolicyCreatePage.vue
@@ -1,5 +1,5 @@
 <template>
-    <section>
+    <section class="policy-create-page">
         <p-page-title
             child
             :title="$t('IAM.POLICY.FORM.CREATE_TITLE')"
@@ -14,8 +14,22 @@
                 </p-badge>
             </div>
             <div class="policy-create-contents">
-                <p-field-group :label="$t('IAM.POLICY.MODAL.NAME')" required>
-                    <p-text-input />
+                <!--                song-lang-->
+                <p-field-group
+                    label="$t('IAM.POLICY.MODAL.NAME')"
+                    required
+                    :invalid="isNameModified"
+                    invalid-text="Must be longer than 2 characters"
+                >
+                    <template #default="{invalid}">
+                        <!--                    song-lang-->
+                        <p-text-input
+                            :value.sync="name"
+                            :invalid="invalid"
+                            invalid-text="Must be longer than 2 characters"
+                            @input="handleNameInput"
+                        />
+                    </template>
                 </p-field-group>
             </div>
             <div class="policy-create-contents">
@@ -24,15 +38,25 @@
                 </p-field-group>
             </div>
             <div class="policy-create-contents">
-                <p-field-group :label="$t('IAM.POLICY.FORM.CONTENT')" />
-                <p-text-editor :code="code" />
+                <p-field-group :label="$t('IAM.POLICY.FORM.CONTENT')" required />
+                <p-text-editor :code="code" @update:code="handleCodeUpdate" />
             </div>
         </p-pane-layout>
         <div class="policy-modify-buttons">
-            <p-button style-type="gray-border" size="lg">
+            <p-button
+                style-type="gray-border"
+                size="lg"
+                @click="$router.go(-1)"
+            >
                 {{ $t('IAM.POLICY.FORM.CANCEL') }}
             </p-button>
-            <p-button style-type="primary-dark" size="lg">
+            <!--            song-lang-->
+            <p-button
+                style-type="primary-dark"
+                size="lg"
+                :disabled="isNameModified"
+                @click="handleCreatePolicy"
+            >
                 {{ $t('IAM.POLICY.FORM.CREATE') }}
             </p-button>
         </div>
@@ -43,7 +67,11 @@
 import {
     PPageTitle, PPaneLayout, PBadge, PLabel, PFieldGroup, PTextInput, PTextEditor, PButton,
 } from '@spaceone/design-system';
-import { reactive, toRefs } from '@vue/composition-api';
+import { reactive, toRefs, computed } from '@vue/composition-api';
+import { SpaceConnector } from '@spaceone/console-core-lib/space-connector';
+import { showSuccessMessage } from '@/lib/helper/notice-alert-helper';
+import { SpaceRouter } from '@/router';
+import ErrorHandler from '@/common/composables/error/errorHandler';
 
 export default {
     name: 'PolicyCreatePage',
@@ -59,29 +87,64 @@ export default {
     },
     setup() {
         const state = reactive({
-            code: '{}',
+            name: '',
+            code: '',
         });
+
+        const permissionsParser = (code: string) => code.split('\n').filter(d => d !== '');
+
+        const handleNameInput = (inputtedName: string) => { state.name = inputtedName; };
+        const handleCreatePolicy = async () => {
+            try {
+                await SpaceConnector.client.identity.policy.create({ permissions: permissionsParser(state.code), name: state.name });
+                // song-lang
+                showSuccessMessage('Successfully created policy', '');
+                SpaceRouter.router.go(-1);
+            } catch (e: any) {
+                // song-lang
+                ErrorHandler.handleRequestError(e, 'Failed to create policy');
+            }
+        };
+
+        const handleCodeUpdate = (modifiedCode: string) => { state.code = modifiedCode; };
+
+        const isNameModified = computed(() => !(state.name.length >= 2));
 
         return {
             ...toRefs(state),
+            handleCreatePolicy,
+            handleNameInput,
+            handleCodeUpdate,
+            isNameModified,
         };
     },
 };
 </script>
 
 <style lang="postcss" scoped>
-.policy-create-info-wrapper {
-    padding: 1.125rem 1rem 2.5rem;
-}
-.policy-create-contents {
-    margin-bottom: 1.125rem;
-}
-.policy-modify-buttons {
-    @apply inline-flex;
-    float: right;
-    margin-top: 1rem;
-    & .p-button {
-        margin-left: 1rem;
+.policy-create-page::v-deep {
+    .policy-create-info-wrapper {
+        padding: 1.125rem 1rem 2.5rem;
+    }
+
+    .policy-create-contents {
+        margin-bottom: 1.125rem;
+    }
+
+    .policy-modify-buttons {
+        @apply inline-flex;
+        float: right;
+        margin-top: 1rem;
+
+        & .p-button {
+            margin-left: 1rem;
+        }
+    }
+
+    .p-text-editor {
+        & .CodeMirror {
+            min-height: 480px;
+        }
     }
 }
 </style>
