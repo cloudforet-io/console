a795ac7db7b866bd748322755397d605ec823efb
WANJIN <wanjin@megazone.com>
2022-05-17 18:21:44 +0900

    fix: use menu id to project route config & fix getMenuIdByRouteName bug (#2849)

--- src/services/project/route-config.ts
+++ src/services/project/route-config.ts
@@ -4,24 +4,24 @@ import { MENU_ID } from '@/lib/menu/config';
 export const PROJECT_ROUTE = Object.freeze({
     _NAME: MENU_ID.PROJECT,
     DETAIL: {
-        _NAME: 'projectDetail',
+        _NAME: `${MENU_ID.PROJECT}.detail`,
         TAB: {
-            _NAME: 'projectTab',
-            SUMMARY: { _NAME: 'projectSummary' },
-            MEMBER: { _NAME: 'projectMember' },
+            _NAME: `${MENU_ID.PROJECT}.detail.tab`,
+            SUMMARY: { _NAME: `${MENU_ID.PROJECT}.detail.tab.summary` },
+            MEMBER: { _NAME: `${MENU_ID.PROJECT}.detail.tab.member` },
             ALERT: {
-                _NAME: 'projectAlert',
-                ALERT: { _NAME: 'projectAlertList' },
-                WEBHOOK: { _NAME: 'projectWebhook' },
-                MAINTENANCE_WINDOW: { _NAME: 'projectMaintenanceWindow' },
-                SETTINGS: { _NAME: 'projectSettings' },
+                _NAME: `${MENU_ID.PROJECT}.detail.tab.alert`,
+                ALERT: { _NAME: `${MENU_ID.PROJECT}.detail.tab.alert.alert_list` },
+                WEBHOOK: { _NAME: `${MENU_ID.PROJECT}.detail.tab.alert.webhook` },
+                MAINTENANCE_WINDOW: { _NAME: `${MENU_ID.PROJECT}.detail.tab.alert.maintenance_window` },
+                SETTINGS: { _NAME: `${MENU_ID.PROJECT}.detail.tab.alert.settings` },
             },
             NOTIFICATIONS: {
-                _NAME: 'projectNotifications',
-                ADD: { _NAME: 'addProjectNotification' },
+                _NAME: `${MENU_ID.PROJECT}.detail.tab.notification`,
+                ADD: { _NAME: `${MENU_ID.PROJECT}.detail.tab.notification.add` },
             },
-            TAG: { _NAME: 'projectTag' },
+            TAG: { _NAME: `${MENU_ID.PROJECT}.detail.tab.tag` },
         },
-        EVENT_RULE: { _NAME: 'projectEventRule' },
+        EVENT_RULE: { _NAME: `${MENU_ID.PROJECT}.detail.tab.alert.event_rule` },
     },
 });
