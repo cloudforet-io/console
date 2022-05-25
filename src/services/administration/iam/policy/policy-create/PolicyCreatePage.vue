82c074acc64974a01416091b58e8ec02c6af9da1
Minseo Lee <minseolee@mz.co.kr>
2022-05-25 14:39:03 +0900

    fix: apply isAllValid at policyCreatePage (#2914)

--- src/services/administration/iam/policy/policy-create/PolicyCreatePage.vue
+++ src/services/administration/iam/policy/policy-create/PolicyCreatePage.vue
@@ -36,7 +36,7 @@
             </div>
             <div class="policy-create-contents">
                 <p-field-group :label="$t('IAM.POLICY.FORM.CONTENT')" required />
-                <p-text-editor :code="code" @update:code="handleCodeUpdate" />
+                <p-text-editor :code="code" @update:code="setForm('policyCode', $event)" />
             </div>
         </p-pane-layout>
         <div class="policy-modify-buttons">
@@ -47,11 +47,10 @@
             >
                 {{ $t('IAM.POLICY.FORM.CANCEL') }}
             </p-button>
-            <!-- Apply disabled prop as isAllValid -->
             <p-button
                 style-type="primary-dark"
                 size="lg"
-                :disabled="!(policyName.length > 2 && code.length)"
+                :disabled="!isAllValid"
                 @click="handleCreatePolicy"
             >
                 {{ $t('IAM.POLICY.FORM.CREATE') }}
@@ -97,19 +96,22 @@ export default {
         const {
             forms: {
                 policyName,
+                policyCode,
             },
             setForm,
             invalidState,
             invalidTexts,
+            isAllValid,
         } = useFormValidator({
             policyName: '',
+            policyCode: '',
         }, {
             policyName(value: string) { return value.trim().length > 2 ? '' : i18n.t('IAM.POLICY.FORM.VALIDATION_NAME'); },
+            policyCode(value: string) { return value.trim().length ? '' : false; },
         });
 
         const state = reactive({
             description: '',
-            code: '',
         });
 
         const permissionsParser = (code: string) => code.split('\n').filter(d => d !== '');
@@ -121,7 +123,7 @@ export default {
                     tags: {
                         description: state.description,
                     },
-                    permissions: permissionsParser(state.code),
+                    permissions: permissionsParser(policyCode.value),
                 });
                 showSuccessMessage(i18n.t('IAM.POLICY.MODAL.ALT_S_CREATE_POLICY'), '');
                 await SpaceRouter.router.push({ name: ADMINISTRATION_ROUTE.IAM.POLICY._NAME, query: { policy_type: POLICY_TYPES.CUSTOM } });
@@ -130,16 +132,14 @@ export default {
             }
         };
 
-        const handleCodeUpdate = (modifiedCode: string) => { state.code = modifiedCode; };
-
         return {
             ...toRefs(state),
             handleCreatePolicy,
-            handleCodeUpdate,
             policyName,
             setForm,
             invalidState,
             invalidTexts,
+            isAllValid,
         };
     },
 };
