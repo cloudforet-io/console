1150c0c3322cd19ba3e20563b45431713bfc0816
Wanjin Noh <wanjin@megazone.com>
2022-07-13 22:00:19 +0900

    refactor: remove unnecessary files in packages

--- packages/@spaceone/console-core-lib/dev/link-ds.sh
+++
@@ -1,6 +0,0 @@
-cd ${DS_PATH} &&
-echo "make DesignSystem link" &&
-npm link &&
-cd ${CORE_LIB_PATH} &&
-echo "link DesignSystem to ConsoleCoreLib" &&
-npm link @spaceone/design-system
--- packages/@spaceone/console-core-lib/dev/unlink-ds.sh
+++
@@ -1,6 +0,0 @@
-cd ${CORE_LIB_PATH} &&
-echo "unlink DesignSystem from ConsoleCoreLib" &&
-npm unlink --no-save @spaceone/design-system &&
-cd ${DS_PATH} &&
-echo "unlink DesignSystem"
-npm unlink
