6ce5d137a94747d271741136c6695adae7ee598e
Minseo Lee <minseolee@mz.co.kr>
2022-07-13 19:16:01 +0900

    fix: add read-only at dynamic-layout raw (#1014)

--- src/data-display/dynamic/dynamic-layout/templates/raw/index.vue
+++ src/data-display/dynamic/dynamic-layout/templates/raw/index.vue
@@ -4,8 +4,7 @@
             {{ layoutName }}
         </p-panel-top>
         <p-text-editor class="m-4" :code="rootData" :loading="loading"
-                       need-refine
-                       folded
+                       need-refine folded read-only
         />
     </div>
 </template>
