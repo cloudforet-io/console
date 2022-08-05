60590d973edd103b293a5bf5b46c33c7e1ceeb1c
WANJIN <wanzargen@gmail.com>
2022-08-05 12:01:50 +0900

    feat: add TextEditorContents component (#3204)
    * feat: add TextEditorContents component
    
    * feat: add example of using TextEditorContents to NoticeDetailPage
    
    * refactor: rename to TextEditorViewer

---
+++ src/common/components/editor/TextEditorViewer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="text-editor-contents" v-html="contents" />
+</template>
+
+<script lang="ts">
+import {
+    defineComponent,
+} from '@vue/composition-api';
+
+
+interface Props {
+    contents: string
+}
+
+export default defineComponent<Props>({
+    name: 'TextEditorViewer',
+    components: {},
+    props: {
+        contents: {
+            type: String,
+            default: '',
+        },
+    },
+});
+</script>
+
+<style lang="postcss" scoped>
+@import './text-editor-nodes.pcss';
+.text-editor-contents::v-deep {
+    @mixin all-nodes-style;
+}
+</style>
--- src/services/my-page/notice/notice-detail/NoticeDetailPage.vue
+++ src/services/my-page/notice/notice-detail/NoticeDetailPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="notice-detail-page">
-        detail-page
+        <text-editor-viewer :contents="contents" />
     </div>
 </template>
 
@@ -9,10 +9,16 @@ import {
     reactive, toRefs,
 } from '@vue/composition-api';
 
+import TextEditorViewer from '@/common/components/editor/TextEditorViewer.vue';
+
 export default {
     name: 'NoticeDetailPage',
+    components: { TextEditorViewer },
     setup() {
-        const state = reactive({});
+        const state = reactive({
+            // eslint-disable-next-line max-len
+            contents: '<p>normal text</p><h1>h1</h1><h2>h2</h2><h3>h3</h3><p style="text-align: center">align center</p><p style="text-align: right">align right</p><p style="text-align: justify">align justify</p><p style="text-align: justify">color1 <span style="color: #7D5DD2">color2</span> <span style="color: #49A7F7">color3</span> ...</p><p style="text-align: justify"><strong>bold </strong></p><p style="text-align: justify"><em>italic</em></p><p style="text-align: justify"><u>underline</u></p><p style="text-align: justify"><s>strike</s></p><p style="text-align: justify"><code class="inline-code">this is inline code</code></p><ul><li><p style="text-align: justify">bullet</p><ul><li><p style="text-align: justify">bullet2</p><ul><li><p style="text-align: justify">bullet3</p></li></ul></li></ul></li><li><p style="text-align: justify">bullet4</p></li></ul><ol><li><p style="text-align: justify">number</p><ol><li><p style="text-align: justify">number1</p><ol><li><p style="text-align: justify">number2</p></li></ol></li></ol></li><li><p style="text-align: justify">number3</p><ol><li><p style="text-align: justify">number4</p></li></ol></li></ol><p style="text-align: justify"><a target="_blank" rel="noopener noreferrer nofollow" href="http://www.google.com">http://www.google.com</a></p><p style="text-align: justify"><img src="https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/spaceone.svg"></p><pre><code>const a = \'This is Code Block!\'nconsole.log(a)</code></pre><blockquote><p>This is Quote Block!</p></blockquote><hr><p></p><p>Good bye...!</p>',
+        });
         return {
             ...toRefs(state),
         };
