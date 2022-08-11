d6a6395875df4d7c22cd4ba065b6c3a0aca29008
sulmo <50662170+JeoungSulMo@users.noreply.github.com>
2022-08-11 09:18:06 +0900

    feat(notice-list): add search function to toolbox (#3231)

--- src/services/info/notice/modules/NoticeList.vue
+++ src/services/info/notice/modules/NoticeList.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="notice-list">
         <div class="notice-header">
-            <p-toolbox :pagination-visible="false" :page-size-changeable="false" :refreshable="false">
+            <p-toolbox :pagination-visible="false" :page-size-changeable="false" :refreshable="false"
+                       @change="handleChange"
+            >
                 <template #left-area>
-                    <p-select-dropdown :items="dropdownItems" :selected="selectedItem" />
+                    <p-select-dropdown :items="dropdownItems" :selected.sync="selectedScope" />
                 </template>
             </p-toolbox>
         </div>
         <p-data-loader :data="noticeItems" :loading="loading">
-            <ul v-if="noticeItems.length" class="list-wrapper">
-                <!-- // todo: item.[id]-->
+            <ul class="list-wrapper">
                 <list-item v-for="(item, index) in noticeItems"
                            :key="`notice-${item.post_id}-${index}`"
                            class="list-item"
@@ -20,13 +21,15 @@
                            @click.native="handleClickNotice(item.post_id)"
                 />
             </ul>
-            <div v-else class="no-data">
-                <img src="@/assets/images/illust_satellite.svg" class="no-data-img">
-                <p class="no-data-text">
-                    <!--song-lang-->
-                    {{ $t('No notices') }}
-                </p>
-            </div>
+            <template #no-data>
+                <div class="no-data">
+                    <img src="@/assets/images/illust_satellite.svg" class="no-data-img">
+                    <p class="no-data-text">
+                        <!--song-lang-->
+                        {{ $t('No notices') }}
+                    </p>
+                </div>
+            </template>
             <div class="pagination-wrapper">
                 <p-pagination class="pagination"
                               :total-count="noticeItems.length"
@@ -48,7 +51,9 @@ import {
     PDataLoader,
     PPagination, PSelectDropdown, PToolbox,
 } from '@spaceone/design-system';
+import type { ToolboxOptions } from '@spaceone/design-system/dist/src/navigation/toolbox/type';
 
+import type { QueryStoreFilter } from '@/query/type';
 import { SpaceRouter } from '@/router';
 
 import { getNoticeBoardId } from '@/lib/helper/notice-helper';
@@ -96,13 +101,14 @@ export default defineComponent<Props>({
                     name: NOTICE_TYPE.DOMAIN,
                 },
             ],
-            selectedItem: 'ALL',
+            selectedScope: 'ALL',
             loading: false,
             noticeItems: [] as NoticePostModel[],
+            boardId: undefined as undefined | string,
         });
 
         /* Api */
-        const noticeApiHelper = new ApiQueryHelper()
+        let noticeApiHelper = new ApiQueryHelper()
             .setPage(1, NOTICE_ITEM_LIMIT)
             .setSort('created_at', true);
         const listNotice = async (boardId: string) => {
@@ -118,19 +124,37 @@ export default defineComponent<Props>({
             }
         };
 
+        /* Util */
+        function getSearchFilter(options: ToolboxOptions) {
+            const filterHelper = new ApiQueryHelper()
+                .setPage(1, NOTICE_ITEM_LIMIT)
+                .setSort('created_at', true);
+            const filter = [] as QueryStoreFilter[];
+            if (state.selectedScope !== 'ALL') filter.push({ k: 'scope', v: state.selectedScope, o: '=' });
+            if (options?.searchText) filter.push({ k: 'title', v: options?.searchText, o: '' });
+            filterHelper.setFilters(filter);
+            return filterHelper;
+        }
+
+        /* event */
+        const handleChange = async (options: ToolboxOptions = {}) => {
+            noticeApiHelper = getSearchFilter(options);
+            if (state.boardId) await listNotice(state.boardId);
+        };
         const handleClickNotice = (id: string) => {
             SpaceRouter.router.push({ name: INFO_ROUTE.NOTICE.DETAIL._NAME, params: { id } });
         };
 
         (async () => {
             state.loading = true;
-            const boardId = await getNoticeBoardId();
-            if (boardId) await listNotice(boardId);
+            state.boardId = await getNoticeBoardId();
+            if (state.boardId) await listNotice(state.boardId);
             state.loading = false;
         })();
 
         return {
             ...toRefs(state),
+            handleChange,
             handleClickNotice,
         };
     },
