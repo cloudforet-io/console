6ec0f79753893b8d82d984d003b817141230881f
Dahyun Yu <aydha0110@gmail.com>
2022-08-18 16:46:20 +0900

    fix(system-user): redirect from dashboard to iam page (#3285)
    * fix(system-user): redirect from dashboard to iam page
    
    * fix(router-beforeEach): rollback

--- src/services/dashboard/routes.ts
+++ src/services/dashboard/routes.ts
@@ -1,7 +1,10 @@
 import type { RouteConfig } from 'vue-router';
 
+import { store } from '@/store';
+
 import { ACCESS_LEVEL } from '@/lib/access-control/config';
 
+import { ADMINISTRATION_ROUTE } from '@/services/administration/route-config';
 import { DASHBOARD_ROUTE } from '@/services/dashboard/route-config';
 
 const Dashboard = () => import(/* webpackChunkName: "Dashboard" */ '@/services/dashboard/DashboardPage.vue');
@@ -11,4 +14,8 @@ export default {
     name: DASHBOARD_ROUTE._NAME,
     meta: { accessLevel: ACCESS_LEVEL.VIEW_PERMISSION },
     component: Dashboard,
+    redirect: () => {
+        if (store.getters['user/isDomainOwner'] || store.getters['user/hasSystemRole']) return { name: ADMINISTRATION_ROUTE._NAME };
+        return ({ name: DASHBOARD_ROUTE._NAME });
+    },
 } as RouteConfig;
