d304309c86bfaa0a298148841aa487ac28c7555b
Dahyun Yu <aydha0110@gmail.com>
2022-08-18 18:59:54 +0900

    fix(file): fix useFileAttachments() (#3291)

--- src/common/composables/file-attachments/index.ts
+++ src/common/composables/file-attachments/index.ts
@@ -12,8 +12,11 @@ export const useFileAttachments = (files: ComputedRef<FileInfo[]>| Ref<FileInfo[
         if (files.value.length === 0) return [];
 
         let results;
-        if (files.value[0].download_url) results = files.value.map(file => file.download_url ?? '');
-        else results = await Promise.allSettled(files.value.map(file => getDownloadUrl(file.file_id)));
+        if (files.value[0].download_url) {
+            results = files.value.map(file => file.download_url ?? '');
+            return results.map((result, idx) => ({ downloadUrl: result ?? '', fileId: files.value[idx].file_id }));
+        }
+        results = await Promise.allSettled(files.value.map(file => getDownloadUrl(file.file_id)));
         return results.map((result, idx) => {
             if (result.status === 'fulfilled') return { downloadUrl: result.value ?? '', fileId: files.value[idx].file_id };
             return { downloadUrl: '', fileId: files.value[idx].file_id };
