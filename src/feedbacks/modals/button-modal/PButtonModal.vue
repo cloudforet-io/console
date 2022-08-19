6796523ee3e294b1fb31109891da2586bafd1970
Minseo Lee <minseolee@mz.co.kr>
2022-08-19 15:44:46 +0900

    fix(button-modal): set `max-height` at `absolute` modal (#1060)
    * fix(button-modal): set `max-height` at `absolute` modal
    
    * style: remove comma

--- src/feedbacks/modals/button-modal/PButtonModal.vue
+++ src/feedbacks/modals/button-modal/PButtonModal.vue
@@ -12,11 +12,8 @@
                      tabindex="1"
                 >
                     <article class="modal-content"
-                             :class="[
-                                 `modal-${themeColor}`,
-                                 {'no-footer': hideFooter},
-                                 absolute ? {'max-height': `calc(100vh - 4rem - ${absolute}rem`} : {}
-                             ]"
+                             :class="[`modal-${themeColor}`, {'no-footer': hideFooter}]"
+                             :style="absolute ? {'max-height': `calc(100vh - 4rem - ${absolute}rem`} : {}"
                     >
                         <h3 class="header">
                             <slot v-if="!hideHeader" name="header">
