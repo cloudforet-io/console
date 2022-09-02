64d62f5c157b2d961cf0f0bbbf5e4b100d813c61
Wanjin Noh <wanzargen@gmail.com>
2022-09-02 17:43:00 +0900

    refactor(json-schema-form): use library type declaration & remove unused file

--- src/inputs/forms/new-json-schema-form/type.ts
+++ src/inputs/forms/new-json-schema-form/type.ts
@@ -1,30 +1,16 @@
+import type { JSONSchemaType } from 'ajv';
+
 import type { SupportLanguage } from '@/translations';
 
 export const TEXT_INPUT_TYPES = ['string', 'number', 'integer'] as const;
 
-type JsonSchemaType = typeof TEXT_INPUT_TYPES[number]
 
-export interface JsonSchema<T extends JsonSchemaType = JsonSchemaType> {
-    type: T;
-    title?: string;
-    minLength?: number;
-    pattern?: string;
-    minimum?: number;
-    maximum?: number;
-    examples?: any[];
-    default?: any;
-    properties?: Record<string, JsonSchema>;
-    enum?: any[];
-    items?: JsonSchema;
-    required?: string[];
-}
+export type JsonSchema<Properties = object> = JSONSchemaType<Properties>
 
 export const VALIDATION_MODES = ['input', 'all', 'none'] as const;
 export type ValidationMode = typeof VALIDATION_MODES[number]
 
-export interface InnerJsonSchema extends JsonSchema {
-    id: string;
-}
+export type InnerJsonSchema = JsonSchema & { id: string; }
 
 export interface JsonSchemaFormProps {
     schema?: JsonSchema;
