ca72f2d964561600adb09bf968554fa2fbcf096c
Dahyun Yu <aydha0110@gmail.com>
2022-09-05 21:24:55 +0900

    feat(service-account): add ServiceAccountAccountType file (#4)
    * feat(service-account): add ServiceAccountAccountType file
    
    * fix(service-account): use PPanelTop

---
+++ src/services/asset-inventory/service-account/modules/ServiceAccountAccountType.vue
@@ -0,0 +1,44 @@
+<template>
+    <p-pane-layout class="service-account-account-type">
+        <!--song-lang-->
+        <p-panel-top title="Account Type" />
+        <div class="content-wrapper">
+            Read, Create만 있음
+        </div>
+    </p-pane-layout>
+</template>
+
+<script lang="ts">
+import type { PropType } from '@vue/composition-api';
+
+import {
+    PPaneLayout, PPanelTop,
+} from '@spaceone/design-system';
+
+import type { PageMode } from '@/services/asset-inventory/service-account/type';
+
+
+export default {
+    name: 'ServiceAccountAccountType',
+    components: {
+        PPaneLayout,
+        PPanelTop,
+    },
+    props: {
+        mode: {
+            type: String as PropType<PageMode>,
+            default: 'READ',
+        },
+    },
+    setup() {
+        return {};
+    },
+};
+</script>
+<style lang="postcss" scoped>
+.service-account-account-type {
+    .content-wrapper {
+        padding: 0.5rem 1rem 2.5rem 1rem;
+    }
+}
+</style>
