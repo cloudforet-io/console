1b2d2da7889952d05cf19c61fe588ba4ef55a355
yuda <aydha0110@gmail.com>
2022-09-13 13:49:15 +0900

    feat(service-account): add read mode to base information

--- src/services/asset-inventory/service-account/modules/ServiceAccountBaseInformation.vue
+++ src/services/asset-inventory/service-account/modules/ServiceAccountBaseInformation.vue
@@ -1,8 +1,25 @@
 <template>
     <p-pane-layout class="service-account-base-information">
-        <p-panel-top :title="$t('IDENTITY.SERVICE_ACCOUNT.ADD.BASE_TITLE')" />
+        <p-panel-top :title="$t('IDENTITY.SERVICE_ACCOUNT.ADD.BASE_TITLE')">
+            <template v-if="mode === 'READ'" #extra>
+                <p-button icon="ic_edit">
+                    <!--song-lang-->
+                    Edit
+                </p-button>
+            </template>
+        </p-panel-top>
         <div class="content-wrapper">
-            <template v-if="mode === 'CREATE'">
+            <template v-if="mode === 'READ'">
+                <p-dynamic-layout v-if="readState.detailSchema"
+                                  v-bind="readState.detailSchema"
+                                  :type-options="{
+                                      loading: readState.loading
+                                  }"
+                                  :data="readState.items"
+                                  :field-handler="fieldHandler"
+                />
+            </template>
+            <template v-if="EDIT_MODE.includes(mode)">
                 <p-field-group :label="$t('IDENTITY.SERVICE_ACCOUNT.ADD.NAME_LABEL')"
                                :invalid="invalidState.serviceAccountName"
                                :invalid-text="invalidTexts.serviceAccountName"
@@ -17,10 +34,10 @@
                         />
                     </template>
                 </p-field-group>
-                <p-json-schema-form v-if="serviceAccountSchema"
+                <p-json-schema-form v-if="formState.serviceAccountSchema"
                                     class="p-json-schema-form"
-                                    :form-data.sync="customSchemaForm"
-                                    :schema="serviceAccountSchema"
+                                    :form-data.sync="formState.customSchemaForm"
+                                    :schema="formState.serviceAccountSchema"
                                     :language="$store.state.user.language"
                                     @validate="handleAccountValidate"
                 />
@@ -28,9 +45,9 @@
                                help-text="Set Account's tag.
 The Key - Value pair is a required field. Only underscores (_), characters, and numbers are allowed. International characters are allowed."
                 >
-                    <tags-input-group :tags="tags"
+                    <tags-input-group :tags="formState.tags"
                                       show-validation
-                                      :is-valid.sync="isTagsValid"
+                                      :is-valid.sync="formState.isTagsValid"
                                       @update-tags="handleUpdateTags"
                     />
                 </p-field-group>
@@ -42,24 +59,40 @@ The Key - Value pair is a required field. Only underscores (_), characters, and 
 <script lang="ts">
 import { SpaceConnector } from '@spaceone/console-core-lib/space-connector';
 import {
-    PJsonSchemaForm, PFieldGroup, PPaneLayout, PTextInput, PPanelTop,
+    PButton,
+    PDynamicLayout,
+    PFieldGroup,
+    PJsonSchemaForm,
+    PPaneLayout,
+    PPanelTop,
+    PTextInput,
 } from '@spaceone/design-system';
 import type { PropType } from 'vue';
 import {
-    computed, defineComponent, reactive, toRefs, watch,
+    computed, defineComponent, reactive, watch,
 } from 'vue';
-
 
 import { i18n } from '@/translations';
 
+import { referenceFieldFormatter } from '@/lib/reference/referenceFieldFormatter';
+
 import TagsInputGroup from '@/common/components/forms/tags-input-group/TagsInputGroup.vue';
 import type { Tag } from '@/common/components/forms/tags-input-group/type';
+import ErrorHandler from '@/common/composables/error/errorHandler';
 import { useFormValidator } from '@/common/composables/form-validator';
 
-import type { BaseInformationForm, PageMode } from '@/services/asset-inventory/service-account/type';
+import type { BaseInformationForm, PageMode, ProviderModel } from '@/services/asset-inventory/service-account/type';
+import { EDIT_MODE } from '@/services/asset-inventory/service-account/type';
 
 
-export default defineComponent({
+interface Props {
+    mode: PageMode;
+    providerData: ProviderModel;
+    isValid: boolean;
+    serviceAccountId?: string;
+}
+
+export default defineComponent<Props>({
     name: 'ServiceAccountBaseInformation',
     components: {
         TagsInputGroup,
@@ -68,6 +101,8 @@ export default defineComponent({
         PPaneLayout,
         PTextInput,
         PPanelTop,
+        PButton,
+        PDynamicLayout,
     },
     props: {
         mode: {
@@ -82,6 +117,10 @@ export default defineComponent({
             type: Boolean,
             default: false,
         },
+        serviceAccountId: {
+            type: String,
+            default: undefined,
+        },
     },
     setup(props, { emit }) {
         const {
@@ -95,13 +134,13 @@ export default defineComponent({
             serviceAccountName: (val: string) => {
                 if (val.length < 2) {
                     return i18n.t('IDENTITY.SERVICE_ACCOUNT.ADD.NAME_INVALID');
-                } if (state.serviceAccountNames.includes(val)) {
+                } if (formState.serviceAccountNames.includes(val)) {
                     return i18n.t('IDENTITY.SERVICE_ACCOUNT.ADD.NAME_DUPLICATED');
                 }
                 return true;
             },
         });
-        const state = reactive({
+        const formState = reactive({
             serviceAccountNames: [] as string[],
             serviceAccountSchema: computed(() => props.providerData.template?.service_account?.schema ?? null),
             customSchemaForm: {},
@@ -110,28 +149,66 @@ export default defineComponent({
             isTagsValid: true,
             formData: computed<BaseInformationForm>(() => ({
                 accountName: serviceAccountName.value,
-                customSchemaForm: state.customSchemaForm,
-                tags: state.tags,
+                customSchemaForm: formState.customSchemaForm,
+                tags: formState.tags,
             })),
             isAllValid: computed(() => !invalidState.serviceAccountName
-                && state.isTagsValid
-                && (state.serviceAccountSchema ? state.isCustomSchemaFormValid : true)),
+                && formState.isTagsValid
+                && (formState.serviceAccountSchema ? formState.isCustomSchemaFormValid : true)),
         });
+        const readState = reactive({
+            loading: true,
+            detailSchema: {},
+            items: [],
+            fieldHandler: [],
+        });
+
+        /* Util */
+        const fieldHandler = (field) => {
+            if (field.extraData?.reference) {
+                return referenceFieldFormatter(field.extraData.reference, field.data);
+            }
+            return {};
+        };
 
         /* Api */
         const listServiceAccounts = async () => {
             const { results } = await SpaceConnector.client.identity.serviceAccount.list({
                 only: 'name',
             });
-            state.serviceAccountNames = results.map(v => v.name);
+            formState.serviceAccountNames = results.map(v => v.name);
+        };
+        const getDetailSchema = async (provider) => {
+            const result = await SpaceConnector.client.addOns.pageSchema.get({
+                resource_type: 'identity.ServiceAccount',
+                schema: 'details',
+                options: {
+                    provider,
+                },
+            });
+            readState.detailSchema = result.details[0];
+        };
+        const getServiceAccount = async (serviceAccountId) => {
+            try {
+                readState.loading = true;
+                const result = await SpaceConnector.client.identity.serviceAccount.get({
+                    service_account_id: serviceAccountId,
+                });
+                readState.items = result;
+            } catch (e) {
+                ErrorHandler.handleError(e);
+                readState.items = [];
+            } finally {
+                readState.loading = false;
+            }
         };
 
         /* Event */
         const handleUpdateTags = (tags: Tag) => {
-            state.tags = tags;
+            formState.tags = tags;
         };
         const handleAccountValidate = (isValid) => {
-            state.isCustomSchemaFormValid = isValid;
+            formState.isCustomSchemaFormValid = isValid;
         };
 
         /* Init */
@@ -140,29 +217,45 @@ export default defineComponent({
         })();
 
         /* Watcher */
-        watch(() => state.isAllValid, (isAllValid) => {
+        watch(() => formState.isAllValid, (isAllValid) => {
             emit('update:isValid', isAllValid);
         });
-        watch(() => state.formData, (formData) => {
+        watch(() => formState.formData, (formData) => {
             emit('change', formData);
         });
+        watch(() => props.providerData, (providerData) => {
+            if (providerData.provider) {
+                getDetailSchema(providerData.provider);
+            }
+        });
+        watch(() => props.serviceAccountId, (serviceAccountId) => {
+            if (serviceAccountId) getServiceAccount(serviceAccountId);
+        }, { immediate: true });
 
         return {
-            ...toRefs(state),
+            formState,
+            readState,
             serviceAccountName,
             invalidState,
             invalidTexts,
             setForm,
+            EDIT_MODE,
             handleUpdateTags,
             handleAccountValidate,
+            fieldHandler,
         };
     },
 });
 </script>
 <style lang="postcss" scoped>
 .service-account-base-information {
-    .content-wrapper {
-        padding: 0.5rem 1rem 2.5rem 1rem;
+    .p-panel-top::v-deep {
+        .extra {
+            text-align: right;
+        }
+    }
+    .content-wrapper::v-deep {
+        padding: 0.5rem 1rem;
         .account-name-input::v-deep {
             .input-container {
                 width: 50%;
@@ -175,6 +268,9 @@ export default defineComponent({
                     width: 50%;
                 }
             }
+        }
+        .p-panel-top {
+            display: none;
         }
     }
 
