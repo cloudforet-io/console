f167c5ba997a63b1feb6de32564b7901bc862045
MinseoLee <minseolee@mz.co.kr>
2022-09-14 10:30:53 +0900

    feat: add service account type

--- src/services/asset-inventory/service-account/modules/ServiceAccountAccountType.vue
+++ src/services/asset-inventory/service-account/modules/ServiceAccountAccountType.vue
@@ -3,35 +3,89 @@
         <!--song-lang-->
         <p-panel-top title="Account Type" />
         <div class="content-wrapper">
-            Read, Create만 있음
+            <div v-if="mode === 'CREATE' || mode === 'UPDATE'" class="create-mode-wrapper">
+                <div class="card-wrapper">
+                    <!--                    song-lang-->
+                    <p-select-card
+                        v-model="selectedType"
+                        value="GENERAL"
+                        label="General Account"
+                        class="card"
+                    />
+                    <!--                    song-lang-->
+                    <p-select-card
+                        v-model="selectedType"
+                        value="TRUST"
+                        label="Trust Account"
+                        class="card"
+                    />
+                </div>
+                <div class="information-wrapper">
+                    <p-i name="ic_outlined-info"
+                         height="1em" width="1em"
+                         color="inherit"
+                         class="external-icon"
+                    />
+                    <!--                    song-lang-->
+                    <span>Trust Account can allow entities in other AWS accounts belonging to you.
+                        Get more details</span>
+                    <!--                    song-lang-->
+                    <p-anchor highlight text="here" />
+                </div>
+            </div>
+            <div v-else class="read-mode-wrapper">
+                <p-badge v-if="badgeType === 'GENERAL'" outline style-type="gray">
+                    <!--                    song-lang-->
+                    General Account
+                </p-badge>
+                <p-badge v-else outline style-type="primary">
+                    <!--                    song-lang-->
+                    Trust Account
+                </p-badge>
+            </div>
         </div>
     </p-pane-layout>
 </template>
 
 <script lang="ts">
 import {
-    PPaneLayout, PPanelTop,
+    PPaneLayout, PPanelTop, PSelectCard, PI, PAnchor, PBadge,
 } from '@spaceone/design-system';
 import type { PropType } from 'vue';
+import { reactive, toRefs } from 'vue';
 
-
-import type { PageMode } from '@/services/asset-inventory/service-account/type';
+import type { PageMode, AccountType } from '@/services/asset-inventory/service-account/type';
 
 
 export default {
     name: 'ServiceAccountAccountType',
     components: {
+        PAnchor,
         PPaneLayout,
         PPanelTop,
+        PSelectCard,
+        PI,
+        PBadge,
     },
     props: {
         mode: {
             type: String as PropType<PageMode>,
             default: 'READ',
         },
+        accountType: {
+            type: String as PropType<AccountType>,
+            default: 'GENERAL',
+        },
     },
     setup() {
-        return {};
+        const state = reactive({
+            selectedType: 'GENERAL' as AccountType,
+            badgeType: 'GENERAL' as AccountType,
+        });
+
+        return {
+            ...toRefs(state),
+        };
     },
 };
 </script>
@@ -39,6 +93,32 @@ export default {
 .service-account-account-type {
     .content-wrapper {
         padding: 0.5rem 1rem 2.5rem 1rem;
+        .create-mode-wrapper {
+            .card-wrapper {
+                @apply flex;
+                gap: 0.5rem;
+                margin-bottom: 1rem;
+                .card {
+                    width: calc(50% - 0.25rem);
+                    &::v-deep {
+                        .marker {
+                            display: none;
+                        }
+                        &.selected {
+                            .marker {
+                                display: block;
+                            }
+                        }
+                    }
+                }
+            }
+            .information-wrapper {
+                @apply flex;
+                gap: 0.3rem;
+                align-content: flex-start;
+                align-items: center;
+            }
+        }
     }
 }
 </style>
