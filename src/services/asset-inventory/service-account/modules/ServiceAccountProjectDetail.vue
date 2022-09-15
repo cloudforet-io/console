11017d02552d15342298ce24913f25fa91d30f4c
MinseoLee <minseolee@mz.co.kr>
2022-09-15 17:35:37 +0900

    feat(service-account): project detail edit mode

--- src/services/asset-inventory/service-account/modules/ServiceAccountProjectDetail.vue
+++ src/services/asset-inventory/service-account/modules/ServiceAccountProjectDetail.vue
@@ -2,16 +2,35 @@
     <p-pane-layout class="service-account-project-detail">
         <p-panel-top :title="$t('IDENTITY.SERVICE_ACCOUNT.ADD.PROJECT_TITLE')">
             <template #extra>
-                <p-button icon="ic_edit">
+                <p-button v-if="!editMode" :disabled="!projectName" icon="ic_edit"
+                          @click="handleClickEditButton"
+                >
                     <!--song-lang-->
                     Edit
                 </p-button>
+                <div v-else>
+                    <p-button @click="handleClickEditButton">
+                        <!--                        song-lang-->
+                        Cancel
+                    </p-button>
+                    <p-button style-type="primary-dark" @click="handleClickSave">
+                        <!--                        song-lang-->
+                        Save
+                    </p-button>
+                </div>
             </template>
         </p-panel-top>
         <div class="content-wrapper">
-            <p-anchor :href="state.projectLink">
-                {{ state.projectName }}
+            <p-anchor v-if="!editMode && !!projectName" :href="projectLink">
+                {{ projectName }}
             </p-anchor>
+            <span v-if="!editMode && !projectName">N/A</span>
+            <project-select-dropdown v-if="editMode"
+                                     class="project-select-dropdown"
+                                     project-selectable
+                                     :selected-project-id="selectedProjects"
+                                     :use-fixed-menu-style="false"
+            />
         </div>
     </p-pane-layout>
 </template>
@@ -20,13 +39,22 @@
 import {
     PPaneLayout, PPanelTop, PButton, PAnchor,
 } from '@spaceone/design-system';
-import { computed, reactive } from 'vue';
+import type { SetupContext } from 'vue';
+import { computed, reactive, toRefs } from 'vue';
 
 
 import { SpaceRouter } from '@/router';
 import { store } from '@/store';
 
+import type { ProjectReferenceMap } from '@/store/modules/reference/project/type';
+
 import { referenceRouter } from '@/lib/reference/referenceRouter';
+
+import { useProxyValue } from '@/common/composables/proxy-state';
+import ProjectSelectDropdown from '@/common/modules/project/ProjectSelectDropdown.vue';
+
+import type { ProjectForm } from '@/services/asset-inventory/service-account/type';
+import type { ProjectGroupTreeItem } from '@/services/project/type';
 
 
 export default {
@@ -36,6 +64,7 @@ export default {
         PPanelTop,
         PButton,
         PAnchor,
+        ProjectSelectDropdown,
     },
     props: {
         isValid: {
@@ -47,16 +76,16 @@ export default {
             default: undefined,
         },
     },
-    setup(props) {
+    setup(props, { emit }: SetupContext) {
         const state = reactive({
-            projects: computed(() => store.getters['reference/projectItems']),
-            projectName: computed(() => {
+            projects: computed<ProjectReferenceMap>(() => store.getters['reference/projectItems']),
+            projectName: computed<string>(() => {
                 if (props.projectId) {
                     return state.projects[props.projectId]?.label ?? '';
                 }
                 return '';
             }),
-            projectLink: computed(() => {
+            projectLink: computed<string|undefined>(() => {
                 if (props.projectId) {
                     return SpaceRouter.router.resolve(referenceRouter(props.projectId, {
                         resource_type: 'identity.Project',
@@ -64,8 +93,21 @@ export default {
                 }
                 return undefined;
             }),
+            editMode: false,
+            selectedProjects: [] as ProjectGroupTreeItem[],
+            formData: computed<ProjectForm>(() => ({
+                selectedProject: state.selectedProjects,
+            })),
+            proxyIsValid: useProxyValue('is-valid', props, emit),
         });
 
+
+        const handleClickEditButton = () => { state.editMode = !state.editMode; };
+        const handleSelectProject = (selectedProject) => {
+            state.formData.selectedProject = selectedProject.length ? selectedProject[0] : null;
+            state.proxyIsValid = !!selectedProject.length;
+        };
+        const handleClickSave = () => { state.editMode = !state.editMode; };
 
         /* Init */
         (async () => {
@@ -75,7 +117,10 @@ export default {
         })();
 
         return {
-            state,
+            ...toRefs(state),
+            handleClickEditButton,
+            handleSelectProject,
+            handleClickSave,
         };
     },
 };
@@ -91,7 +136,7 @@ export default {
     .content-wrapper {
         padding: 0.5rem 1rem 2.5rem 1rem;
         .project-select-dropdown {
-            width: 50%;
+            width: 100%;
         }
     }
 
