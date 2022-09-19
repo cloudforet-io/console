082364a81e1723f0697daf9cef001501acb3be6b
Minseo Lee <minseolee@mz.co.kr>
2022-09-19 14:03:42 +0900

    feat(service-account): project detail edit mode (#50)
    * feat(service-account): project detail edit mode
    
    * feat: add transparent on edit button
    
    * feat(service-account): connect api at changing project

--- src/services/asset-inventory/service-account/modules/ServiceAccountProjectDetail.vue
+++ src/services/asset-inventory/service-account/modules/ServiceAccountProjectDetail.vue
@@ -2,31 +2,64 @@
     <p-pane-layout class="service-account-project-detail">
         <p-panel-top :title="$t('IDENTITY.SERVICE_ACCOUNT.ADD.PROJECT_TITLE')">
             <template #extra>
-                <p-button icon="ic_edit">
+                <p-button v-if="!editMode" :disabled="!projectName" icon="ic_edit"
+                          :style-type="!projectName && 'transparent'"
+                          @click="handleEditMode"
+                >
                     <!--song-lang-->
                     Edit
                 </p-button>
+                <div v-else>
+                    <p-button @click="handleEditMode">
+                        <!--                        song-lang-->
+                        Cancel
+                    </p-button>
+                    <p-button style-type="primary-dark" @click="handleClickSave">
+                        <!--                        song-lang-->
+                        Save
+                    </p-button>
+                </div>
             </template>
         </p-panel-top>
         <div class="content-wrapper">
-            <p-anchor :href="state.projectLink">
-                {{ state.projectName }}
+            <p-anchor v-if="!editMode && !!projectName" :href="projectLink">
+                {{ projectName }}
             </p-anchor>
+            <span v-if="!editMode && !projectName">N/A</span>
+            <project-select-dropdown v-if="editMode"
+                                     class="project-select-dropdown"
+                                     project-selectable
+                                     :selected-project-id="selectedProjects"
+                                     :use-fixed-menu-style="false"
+                                     @select="handleSelectProject"
+            />
         </div>
     </p-pane-layout>
 </template>
 
 <script lang="ts">
+import { SpaceConnector } from '@spaceone/console-core-lib/space-connector';
 import {
     PPaneLayout, PPanelTop, PButton, PAnchor,
 } from '@spaceone/design-system';
-import { computed, reactive } from 'vue';
+import type { SetupContext } from 'vue';
+import { computed, reactive, toRefs } from 'vue';
 
 
 import { SpaceRouter } from '@/router';
 import { store } from '@/store';
 
+import type { ProjectReferenceMap } from '@/store/modules/reference/project/type';
+
+import { showSuccessMessage } from '@/lib/helper/notice-alert-helper';
 import { referenceRouter } from '@/lib/reference/referenceRouter';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+import { useProxyValue } from '@/common/composables/proxy-state';
+import ProjectSelectDropdown from '@/common/modules/project/ProjectSelectDropdown.vue';
+
+import type { ProjectForm } from '@/services/asset-inventory/service-account/type';
+import type { ProjectGroupTreeItem, ProjectItemResp } from '@/services/project/type';
 
 
 export default {
@@ -36,6 +69,7 @@ export default {
         PPanelTop,
         PButton,
         PAnchor,
+        ProjectSelectDropdown,
     },
     props: {
         isValid: {
@@ -47,16 +81,16 @@ export default {
             default: undefined,
         },
     },
-    setup(props) {
+    setup(props, { emit }: SetupContext) {
         const state = reactive({
-            projects: computed(() => store.getters['reference/projectItems']),
-            projectName: computed(() => {
+            projects: computed<ProjectReferenceMap>(() => store.getters['reference/projectItems']),
+            projectName: computed<string>(() => {
                 if (props.projectId) {
                     return state.projects[props.projectId]?.label ?? '';
                 }
                 return '';
             }),
-            projectLink: computed(() => {
+            projectLink: computed<string|undefined>(() => {
                 if (props.projectId) {
                     return SpaceRouter.router.resolve(referenceRouter(props.projectId, {
                         resource_type: 'identity.Project',
@@ -64,8 +98,34 @@ export default {
                 }
                 return undefined;
             }),
+            editMode: false,
+            selectedProjects: [] as ProjectGroupTreeItem[],
+            formData: computed<ProjectForm>(() => ({
+                selectedProject: state.selectedProjects,
+            })),
+            proxyIsValid: useProxyValue('is-valid', props, emit),
         });
 
+
+        const handleEditMode = () => { state.editMode = !state.editMode; };
+        const handleSelectProject = (selectedProject: ProjectItemResp[]) => {
+            state.formData.selectedProject = selectedProject.length ? selectedProject[0] : null;
+            state.proxyIsValid = !!selectedProject.length;
+        };
+        const handleClickSave = async () => {
+            state.editMode = !state.editMode;
+            if (!state.formData.selectedProject?.id) return;
+            try {
+                await SpaceConnector.client.identity.serviceAccount.update({
+                    service_account_id: state.formData.selectedProject?.id ?? '',
+                    project_id: props.projectId,
+                });
+                // song-lang
+                showSuccessMessage('Successfully changed project', '');
+            } catch (e: unknown) {
+                ErrorHandler.handleError(e);
+            }
+        };
 
         /* Init */
         (async () => {
@@ -75,7 +135,10 @@ export default {
         })();
 
         return {
-            state,
+            ...toRefs(state),
+            handleEditMode,
+            handleSelectProject,
+            handleClickSave,
         };
     },
 };
@@ -91,7 +154,7 @@ export default {
     .content-wrapper {
         padding: 0.5rem 1rem 2.5rem 1rem;
         .project-select-dropdown {
-            width: 50%;
+            width: 100%;
         }
     }
 
