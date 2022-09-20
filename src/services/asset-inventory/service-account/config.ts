24f9bf4d0ee7e8f48471c6a38b4ce2700a0925fe
sulmo <50662170+sulmoJ@users.noreply.github.com>
2022-09-20 16:52:57 +0900

    feat(service-account-badge): create service-account-badge (#71)

--- src/services/asset-inventory/service-account/config.ts
+++ src/services/asset-inventory/service-account/config.ts
@@ -1,1 +1,11 @@
 export const TRUST_ACCOUNT_ALLOWED = ['aws'] as const;
+
+export const ACCOUNT_TYPE = Object.freeze({
+    GENERAL: 'GENERAL',
+    TRUST: 'TRUST',
+});
+
+export const ACCOUNT_TYPE_BADGE_OPTION = Object.freeze({
+    [ACCOUNT_TYPE.GENERAL]: { label: 'General Account', styleType: 'gray' },
+    [ACCOUNT_TYPE.TRUST]: { label: 'Trust Account', styleType: 'primary' },
+});
---
+++ src/services/asset-inventory/service-account/modules/ServiceAccountBadge.vue
@@ -0,0 +1,32 @@
+<template>
+    <p-badge :outline="true" :style-type="ACCOUNT_TYPE_BADGE_OPTION[accountType].styleType">
+        {{ ACCOUNT_TYPE_BADGE_OPTION[accountType] ? ACCOUNT_TYPE_BADGE_OPTION[accountType].label : '' }}
+    </p-badge>
+</template>
+
+<script lang="ts">
+import { PBadge } from '@spaceone/design-system';
+import type { PropType } from 'vue';
+
+import { ACCOUNT_TYPE, ACCOUNT_TYPE_BADGE_OPTION } from '@/services/asset-inventory/service-account/config';
+import type { AccountType } from '@/services/asset-inventory/service-account/type';
+
+export default {
+    name: 'ServiceAccountBadge',
+    components: { PBadge },
+    props: {
+        accountType: {
+            type: String as PropType<AccountType>,
+            default: ACCOUNT_TYPE.GENERAL,
+            validator(value: AccountType) {
+                return Object.values(ACCOUNT_TYPE).includes(value);
+            },
+        },
+    },
+    setup() {
+        return {
+            ACCOUNT_TYPE_BADGE_OPTION,
+        };
+    },
+};
+</script>
