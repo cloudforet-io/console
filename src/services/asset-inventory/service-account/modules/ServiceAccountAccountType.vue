80b80f24799504fa93181d0b049cb84a3093f01c
yuda <yuda@mz.co.kr>
2022-10-06 16:09:31 +0900

    fix(style): set padding-bottom to AccountType section

--- src/services/asset-inventory/service-account/modules/ServiceAccountAccountType.vue
+++ src/services/asset-inventory/service-account/modules/ServiceAccountAccountType.vue
@@ -2,31 +2,29 @@
     <p-pane-layout class="service-account-account-type">
         <p-panel-top :title="$t('INVENTORY.SERVICE_ACCOUNT.DETAIL.ACCOUNT_TYPE_TITLE')" />
         <div class="content-wrapper">
-            <div class="create-mode-wrapper">
-                <div class="card-wrapper">
-                    <p-select-card
-                        v-model="selectedType"
-                        value="GENERAL"
-                        label="General Account"
-                        @click="() => handleSelectAccountType('GENERAL')"
-                    />
-                    <p-select-card
-                        v-if="showTrustedAccount"
-                        v-model="selectedType"
-                        value="TRUSTED"
-                        label="Trusted Account"
-                        @click="() => handleSelectAccountType('TRUSTED')"
-                    />
-                </div>
-                <div v-if="showTrustedAccount" class="information-wrapper">
-                    <p-i name="ic_outlined-info"
-                         height="0.875rem" width="0.875rem"
-                         color="inherit"
-                         class="external-icon"
-                    />
-                    <div class="help-wrapper">
-                        <span>{{ $t('INVENTORY.SERVICE_ACCOUNT.DETAIL.TRUST_ACCOUNT_HELP_TEXT') }}</span>
-                    </div>
+            <div class="card-wrapper">
+                <p-select-card
+                    v-model="selectedType"
+                    value="GENERAL"
+                    label="General Account"
+                    @click="() => handleSelectAccountType('GENERAL')"
+                />
+                <p-select-card
+                    v-if="showTrustedAccount"
+                    v-model="selectedType"
+                    value="TRUSTED"
+                    label="Trusted Account"
+                    @click="() => handleSelectAccountType('TRUSTED')"
+                />
+            </div>
+            <div v-if="showTrustedAccount" class="information-wrapper">
+                <p-i name="ic_outlined-info"
+                     height="0.875rem" width="0.875rem"
+                     color="inherit"
+                     class="external-icon"
+                />
+                <div class="help-wrapper">
+                    <span>{{ $t('INVENTORY.SERVICE_ACCOUNT.DETAIL.TRUST_ACCOUNT_HELP_TEXT') }}</span>
                 </div>
             </div>
         </div>
@@ -93,36 +91,35 @@ export default defineComponent({
 .service-account-account-type {
     .content-wrapper {
         padding: 0.5rem 1rem 1.5rem 1rem;
-        .create-mode-wrapper {
-            @apply w-1/2;
-            .card-wrapper {
-                @apply flex;
-                gap: 0.5rem;
-                margin-bottom: 1rem;
 
-                /* custom design-system component - p-select-card */
-                :deep(.p-select-card) {
-                    max-width: 14.75rem;
-                    width: calc(50% - 0.25rem);
+        @apply w-1/2;
+        .card-wrapper {
+            @apply flex;
+            gap: 0.5rem;
+            margin-bottom: 1rem;
+
+            /* custom design-system component - p-select-card */
+            :deep(.p-select-card) {
+                max-width: 14.75rem;
+                width: calc(50% - 0.25rem);
+                .marker {
+                    display: none;
+                }
+                &.selected {
                     .marker {
-                        display: none;
-                    }
-                    &.selected {
-                        .marker {
-                            display: block;
-                        }
+                        display: block;
                     }
                 }
             }
-            .information-wrapper {
-                @apply flex text-gray-700;
-                gap: 0.3rem;
-                align-content: flex-start;
-                padding-bottom: 1rem;
-                .help-wrapper {
-                    width: calc(100% - 1rem);
-                    font-size: 0.75rem;
-                }
+        }
+        .information-wrapper {
+            @apply flex text-gray-700;
+            gap: 0.3rem;
+            align-content: flex-start;
+            padding-bottom: 1rem;
+            .help-wrapper {
+                width: calc(100% - 1rem);
+                font-size: 0.75rem;
             }
         }
     }
