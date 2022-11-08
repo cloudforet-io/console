261f74262c6a8733ddada6b8a9d6d1b8aa780e97
MinseoLee <minseolee@mz.co.kr>
2022-11-08 21:00:24 +0900

    feat(dashboard-create): markups

---
+++ src/services/dashboards/dashboard-create/modules/DashboardScopeForm.vue
@@ -0,0 +1,49 @@
+<template>
+    <section>
+        <p-pane-layout>
+            <!--            song-lang-->
+            <p-panel-top title="Dashboard Scope" />
+            <div class="dashboard-scope-wrapper">
+                <p-radio-group direction="vertical">
+                    <!--                    song-lang-->
+                    <p-radio>Entire Workspaces</p-radio>
+                    <!--                    song-lang-->
+                    <p-radio>Single Project</p-radio>
+                </p-radio-group>
+                <project-select-dropdown />
+            </div>
+        </p-pane-layout>
+    </section>
+</template>
+
+<script lang="ts">
+import {
+    PPaneLayout, PPanelTop, PRadio, PRadioGroup,
+} from '@spaceone/design-system';
+
+import ProjectSelectDropdown from '@/common/modules/project/ProjectSelectDropdown.vue';
+
+export default {
+    name: 'DashboardScopeForm',
+    components: {
+        ProjectSelectDropdown,
+        PRadioGroup,
+        PRadio,
+        PPanelTop,
+        PPaneLayout,
+    },
+};
+</script>
+
+<style lang="postcss" scoped>
+.dashboard-scope-wrapper {
+    .p-radio-group {
+        display: grid;
+        grid-gap: 0.875rem;
+    }
+    .project-select-dropdown {
+        margin: 0.375rem 0 0 1.125rem;
+    }
+    margin: 0.5rem 1rem 2.25rem;
+}
+</style>
---
+++ src/services/dashboards/dashboard-create/modules/DashboardTemplateForm.vue
@@ -0,0 +1,78 @@
+<template>
+    <section>
+        <p-pane-layout>
+            <!--            song-lang-->
+            <p-panel-top title="Start From" />
+            <div class="dashboard-template-wrapper">
+                <div class="dashboard-template-container">
+                    <div class="card-container">
+                        <span class="card-wrapper-title">
+                            <!--                        song-lang-->
+                            Default Template
+                        </span>
+                        <div class="card-wrapper">
+                            <p-select-card
+                                v-for="(item, idx) in [0,1]"
+                                :key="`default-template-${idx}`"
+                                :tabindex="idx"
+                            />
+                        </div>
+                    </div>
+                    <div class="card-container">
+                        <span class="card-wrapper-title">
+                            <!--                        song-lang-->
+                            Existing Dashboard
+                        </span>
+                        <div class="card-wrapper">
+                            <p-select-card
+                                v-for="(item, idx) in [0]"
+                                :key="`existing-dashboard-${idx}`"
+                                :tabindex="idx"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </p-pane-layout>
+    </section>
+</template>
+
+<script lang="ts">
+import {
+    PPaneLayout, PPanelTop, PSelectCard,
+} from '@spaceone/design-system';
+
+export default {
+    name: 'DashboardTemplateForm',
+    components: {
+        PPanelTop,
+        PPaneLayout,
+        PSelectCard,
+    },
+};
+</script>
+
+<style lang="postcss" scoped>
+.dashboard-template-wrapper {
+    padding: 0.5rem 1rem 2.375rem 1rem;
+    .dashboard-template-container {
+        @apply bg-gray-100 border-gray-200;
+        padding: 1rem 1rem 1.25rem;
+        border-width: 1px;
+        border-radius: 0.5rem;
+        display: grid;
+        grid-gap: 1rem;
+        .card-wrapper-title {
+            @apply text-gray-500 !important text-xs;
+            font-weight: 700;
+            margin-bottom: 0.5rem;
+            display: block;
+        }
+        .card-wrapper {
+            display: grid;
+            grid-gap: 0.5rem;
+            grid-template-columns: 50% 50%;
+        }
+    }
+}
+</style>
