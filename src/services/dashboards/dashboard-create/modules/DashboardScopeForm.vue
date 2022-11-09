10c26d0ab09b0eab0098a53d483d752285e22de7
MinseoLee <minseolee@mz.co.kr>
2022-11-09 18:15:09 +0900

    feat(dashboard-create): set state in dashboardScopeForm

--- src/services/dashboards/dashboard-create/modules/DashboardScopeForm.vue
+++ src/services/dashboards/dashboard-create/modules/DashboardScopeForm.vue
@@ -5,25 +5,45 @@
             <p-panel-top title="Dashboard Scope" />
             <div class="dashboard-scope-wrapper">
                 <p-radio-group direction="vertical">
-                    <!--                    song-lang-->
-                    <p-radio>Entire Workspaces</p-radio>
-                    <!--                    song-lang-->
-                    <p-radio>Single Project</p-radio>
+                    <p-radio :selected="isEntireScope"
+                             @change="handleSelectScope('ENTIRE')"
+                    >
+                        <!--                    song-lang-->
+                        Entire Workspaces
+                    </p-radio>
+                    <p-radio :selected="isSingleScope"
+                             @change="handleSelectScope('SINGLE')"
+                    >
+                        <!--                    song-lang-->
+                        Single Project
+                    </p-radio>
                 </p-radio-group>
-                <project-select-dropdown />
+                <project-select-dropdown v-show="isSingleScope"
+                                         project-selectable
+                                         @select="handleSelectProject"
+                />
             </div>
         </p-pane-layout>
     </section>
 </template>
 
 <script lang="ts">
+import type { PropType, SetupContext } from 'vue';
+import {
+    defineComponent, reactive, toRefs,
+} from 'vue';
+
 import {
     PPaneLayout, PPanelTop, PRadio, PRadioGroup,
 } from '@spaceone/design-system';
 
+import { store } from '@/store';
+
 import ProjectSelectDropdown from '@/common/modules/project/ProjectSelectDropdown.vue';
 
-export default {
+import type { DashboardScope } from '@/services/dashboards/dashboard-create/type';
+
+export default defineComponent({
     name: 'DashboardScopeForm',
     components: {
         ProjectSelectDropdown,
@@ -32,7 +52,49 @@ export default {
         PPanelTop,
         PPaneLayout,
     },
-};
+    props: {
+        scope: {
+            type: String as PropType<DashboardScope>,
+            default: undefined,
+        },
+        project: {
+            type: Array as PropType<Array<string>>,
+            default: () => [],
+        },
+    },
+    setup(props, { emit }: SetupContext) {
+        const state = reactive({
+            isEntireScope: undefined as undefined|boolean,
+            isSingleScope: undefined as undefined|boolean,
+        });
+
+        const handleSelectScope = (scopeType: DashboardScope) => {
+            if (scopeType === 'ENTIRE') {
+                state.isEntireScope = true;
+                state.isSingleScope = false;
+            } else {
+                state.isEntireScope = false;
+                state.isSingleScope = true;
+            }
+            emit('update:scope', scopeType);
+        };
+
+        const handleSelectProject = (project: Array<string>) => {
+            emit('update:project', project);
+        };
+
+        // LOAD REFERENCE STORE
+        (async () => {
+            await store.dispatch('reference/project/load');
+        })();
+
+        return {
+            ...toRefs(state),
+            handleSelectScope,
+            handleSelectProject,
+        };
+    },
+});
 </script>
 
 <style lang="postcss" scoped>
---
+++ src/services/dashboards/dashboard-create/type.ts
@@ -0,0 +1,1 @@
+export type DashboardScope = 'ENTIRE' | 'SINGLE';
