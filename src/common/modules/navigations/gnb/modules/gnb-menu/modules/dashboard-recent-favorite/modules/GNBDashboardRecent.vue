03fd9494290858b9b098abbb79a103863a0007b4
sulmo <tjdah0853@gmail.com>
2022-11-09 20:10:10 +0900

    fix(dashboard-recent-tab): remove unused code

--- src/common/modules/navigations/gnb/modules/gnb-menu/modules/dashboard-recent-favorite/modules/GNBDashboardRecent.vue
+++ src/common/modules/navigations/gnb/modules/gnb-menu/modules/dashboard-recent-favorite/modules/GNBDashboardRecent.vue
@@ -4,11 +4,6 @@
                        :loading="loading"
                        :class="{ loading: loading && !items.length }"
         >
-            <g-n-b-suggestion-list :items="items"
-                                   use-favorite
-                                   @close="$emit('close')"
-                                   @select="handleSelect"
-            />
             <template #no-data>
                 <div class="no-data">
                     <img class="img"
@@ -26,48 +21,15 @@
 
 <script lang="ts">
 
-import type { SetupContext } from 'vue';
 import {
-    computed, defineComponent, reactive, toRefs, watch,
+    defineComponent, reactive, toRefs,
 } from 'vue';
 
 import { PDataLoader } from '@spaceone/design-system';
-import { sortBy } from 'lodash';
-
-import { SpaceRouter } from '@/router';
-import { store } from '@/store';
-
-import type { DisplayMenu } from '@/store/modules/display/type';
-import type { RecentConfig, RecentItem } from '@/store/modules/recent/type';
-import { RECENT_TYPE } from '@/store/modules/recent/type';
-import type { CloudServiceTypeReferenceMap } from '@/store/modules/reference/cloud-service-type/type';
-import type { ProjectGroupReferenceMap } from '@/store/modules/reference/project-group/type';
-import type { ProjectReferenceMap } from '@/store/modules/reference/project/type';
-
-import { isUserAccessibleToMenu } from '@/lib/access-control';
-import {
-    convertCloudServiceConfigToReferenceData,
-    convertMenuConfigToReferenceData,
-    convertProjectConfigToReferenceData,
-    convertProjectGroupConfigToReferenceData,
-} from '@/lib/helper/config-data-helper';
-import type { MenuInfo } from '@/lib/menu/config';
-import { MENU_ID } from '@/lib/menu/config';
-import { MENU_INFO_MAP } from '@/lib/menu/menu-info';
-import { referenceRouter } from '@/lib/reference/referenceRouter';
-
-import type { SuggestionItem } from '@/common/modules/navigations/gnb/modules/gnb-search/config';
-import { SUGGESTION_TYPE } from '@/common/modules/navigations/gnb/modules/gnb-search/config';
-import GNBSuggestionList from '@/common/modules/navigations/gnb/modules/GNBSuggestionList.vue';
-
-import { ASSET_INVENTORY_ROUTE } from '@/services/asset-inventory/route-config';
-
-const RECENT_LIMIT = 30;
 
 export default defineComponent({
     name: 'GNBDashboardRecent',
     components: {
-        GNBSuggestionList,
         PDataLoader,
     },
     props: {
@@ -76,96 +38,14 @@ export default defineComponent({
             default: false,
         },
     },
-    setup(props, { emit }: SetupContext) {
-        const storeState = reactive({
-            menuItems: computed<DisplayMenu[]>(() => store.getters['display/allMenuList']),
-            projects: computed<ProjectReferenceMap>(() => store.getters['reference/projectItems']),
-            projectGroups: computed<ProjectGroupReferenceMap>(() => store.getters['reference/projectGroupItems']),
-            cloudServiceTypes: computed<CloudServiceTypeReferenceMap>(() => store.getters['reference/cloudServiceTypeItems']),
-            recents: computed<RecentConfig[]>(() => store.state.recent.allItems),
-        });
+    setup() {
         const state = reactive({
             loading: true,
-            items: computed<SuggestionItem[]>(() => sortBy(
-                state.recentMenuItems
-                    .concat(state.recentCloudServiceItems)
-                    .concat(state.recentProjectItems)
-                    .concat(state.recentProjectGroupItems),
-                (recent) => recent.updatedAt,
-            ).reverse()),
-            recentMenuItems: computed<RecentItem[]>(() => convertMenuConfigToReferenceData(
-                storeState.recents.filter((d) => d.itemType === RECENT_TYPE.MENU),
-                storeState.menuItems,
-            )),
-            recentCloudServiceItems: computed<RecentItem[]>(() => {
-                const isUserAccessible = isUserAccessibleToMenu(MENU_ID.ASSET_INVENTORY_CLOUD_SERVICE, store.getters['user/pagePermissionList']);
-                return isUserAccessible ? convertCloudServiceConfigToReferenceData(
-                    storeState.recents.filter((d) => d.itemType === RECENT_TYPE.CLOUD_SERVICE),
-                    storeState.cloudServiceTypes,
-                ) : [];
-            }),
-            recentProjectItems: computed<RecentItem[]>(() => {
-                const isUserAccessible = isUserAccessibleToMenu(MENU_ID.PROJECT, store.getters['user/pagePermissionList']);
-                return isUserAccessible ? convertProjectConfigToReferenceData(
-                    storeState.recents.filter((d) => d.itemType === RECENT_TYPE.PROJECT),
-                    storeState.projects,
-                ) : [];
-            }),
-            recentProjectGroupItems: computed<RecentItem[]>(() => {
-                const isUserAccessible = isUserAccessibleToMenu(MENU_ID.PROJECT, store.getters['user/pagePermissionList']);
-                return isUserAccessible ? convertProjectGroupConfigToReferenceData(
-                    storeState.recents.filter((d) => d.itemType === RECENT_TYPE.PROJECT_GROUP),
-                    storeState.projectGroups,
-                ) : [];
-            }),
-        });
-
-        const handleSelect = (item: SuggestionItem) => {
-            const itemName = item.name as string;
-            if (item.itemType === SUGGESTION_TYPE.MENU) {
-                const menuInfo: MenuInfo = MENU_INFO_MAP[itemName];
-                if (menuInfo && SpaceRouter.router.currentRoute.name !== itemName) {
-                    SpaceRouter.router.push({ name: itemName }).catch(() => {});
-                }
-            } else if (item.itemType === SUGGESTION_TYPE.PROJECT) {
-                SpaceRouter.router.push(referenceRouter(itemName, { resource_type: 'identity.Project' })).catch(() => {});
-            } else if (item.itemType === SUGGESTION_TYPE.PROJECT_GROUP) {
-                SpaceRouter.router.push(referenceRouter(itemName, { resource_type: 'identity.ProjectGroup' })).catch(() => {});
-            } else if (item.itemType === SUGGESTION_TYPE.CLOUD_SERVICE) {
-                const itemInfo: string[] = itemName.split('.');
-                SpaceRouter.router.push({
-                    name: ASSET_INVENTORY_ROUTE.CLOUD_SERVICE.DETAIL._NAME,
-                    params: {
-                        provider: itemInfo[0],
-                        group: itemInfo[1],
-                        name: itemInfo[2],
-                    },
-                }).catch(() => {});
-            }
-            emit('close');
-        };
-
-        /* Init */
-        (async () => {
-            await Promise.allSettled([
-                store.dispatch('reference/project/load'),
-                store.dispatch('reference/projectGroup/load'),
-                store.dispatch('reference/cloudServiceType/load'),
-            ]);
-        })();
-
-        /* Watcher */
-        watch(() => props.visible, async (visible) => {
-            if (visible) {
-                state.loading = true;
-                await store.dispatch('recent/load', { limit: RECENT_LIMIT });
-                state.loading = false;
-            }
+            items: [],
         });
 
         return {
             ...toRefs(state),
-            handleSelect,
         };
     },
 });
