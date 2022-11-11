d1f2a998b34b7f39a8e26853c7d4f7d415d4982a
yuda <yuda@mz.co.kr>
2022-11-11 19:19:09 +0900

    chore(amcharts5): delete chart types

--- src/common/composables/amcharts5/type.ts
+++ src/common/composables/amcharts5/type.ts
@@ -1,12 +1,3 @@
 export type ChartContext = null | HTMLElement;
-export const CHART_TYPE = Object.freeze({
-    TREEMAP: 'TREEMAP',
-    MAP: 'MAP',
-    LINE: 'LINE',
-    STACKED_COLUMN: 'STACKED_COLUMN',
-    DONUT: 'DONUT',
-    PIE: 'PIE',
-} as const);
-export type ChartType = typeof CHART_TYPE[keyof typeof CHART_TYPE];
 
 export const DATE_VALUE_FIELD = 'date';
