fcc2fe69a12c3ae9d7fc99061080b215ded8a4d2
yuda <yuda@mz.co.kr>
2022-11-16 11:41:51 +0900

    fix(amcharts5): edit hooks
    - edit tooltip hook
    - add `createXYCategoryChart` hook

--- src/common/composables/amcharts5/index.ts
+++ src/common/composables/amcharts5/index.ts
@@ -4,15 +4,19 @@ import {
 } from 'vue';
 
 import * as am5 from '@amcharts/amcharts5';
-import type { Root } from '@amcharts/amcharts5';
+import type { ITooltipSettings, Root } from '@amcharts/amcharts5';
 import type { IPieChartSettings } from '@amcharts/amcharts5/.internal/charts/pie/PieChart';
 import type { PieChart, IPieSeriesSettings } from '@amcharts/amcharts5/percent';
 import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
 import type { IXYChartSettings, IXYSeriesSettings, XYChart } from '@amcharts/amcharts5/xy';
 
+import { Amcharts5GlobalTheme } from '@/lib/site-initializer/amcharts5';
+
 import { createDonutChart, createPieChart, createPieSeries } from '@/common/composables/amcharts5/pie-chart-helper';
 import type { ChartContext } from '@/common/composables/amcharts5/type';
-import { createXYDateChart, createXYLineSeries, createXYStackedColumnSeries } from '@/common/composables/amcharts5/xy-chart-helper';
+import {
+    createXYCategoryChart, createXYDateChart, createXYLineSeries, createXYStackedColumnSeries, createXYTooltip,
+} from '@/common/composables/amcharts5/xy-chart-helper';
 
 export const useAmcharts5 = (
     chartContext: Ref<ChartContext>,
@@ -23,8 +27,12 @@ export const useAmcharts5 = (
     });
 
     const initRoot = (root: Root) => {
-        root.setThemes([am5themes_Animated.new(root)]);
+        root.setThemes([am5themes_Animated.new(root), Amcharts5GlobalTheme.new(root)]);
         root.utc = true;
+        root.dateFormatter.setAll({
+            dateFormat: 'MMMM, yyyy',
+            dateFields: ['valueX'],
+        });
     };
 
     const disposeRoot = () => {
@@ -54,6 +62,10 @@ export const useAmcharts5 = (
             if (!state.root) throw new Error('No root');
             return createXYDateChart(state.root as Root, settings);
         },
+        createXYCategoryChart: (settings?: IXYChartSettings) => {
+            if (!state.root) throw new Error('No root');
+            return createXYCategoryChart(state.root as Root, settings);
+        },
         createPieChart: (settings?: IPieChartSettings) => {
             if (!state.root) throw new Error('No root');
             return createPieChart(state.root as Root, settings);
@@ -63,17 +75,22 @@ export const useAmcharts5 = (
             return createDonutChart(state.root as Root, settings);
         },
         //
-        createXYLineSeries: (chart: XYChart, settings: IXYSeriesSettings, processor?: am5.DataProcessor) => {
+        createXYLineSeries: (chart: XYChart, settings?: Partial<IXYSeriesSettings>) => {
             if (!state.root) throw new Error('No root');
-            return createXYLineSeries(state.root as Root, chart, settings, processor);
+            return createXYLineSeries(state.root as Root, chart, settings);
         },
-        createXYStackedColumnSeries: (chart: XYChart, settings: IXYSeriesSettings, processor?: am5.DataProcessor) => {
+        createXYStackedColumnSeries: (chart: XYChart, settings?: Partial<IXYSeriesSettings>) => {
             if (!state.root) throw new Error('No root');
-            return createXYStackedColumnSeries(state.root as Root, chart, settings, processor);
+            return createXYStackedColumnSeries(state.root as Root, chart, settings);
         },
         createPieSeries: (chart: PieChart, settings?: IPieSeriesSettings) => {
             if (!state.root) throw new Error('No root');
             return createPieSeries(state.root as Root, chart, settings);
+        },
+        //
+        createXYTooltip: (chart: XYChart, settings?: ITooltipSettings) => {
+            if (!state.root) throw new Error('No root');
+            return createXYTooltip(state.root as Root, chart, settings);
         },
     };
 };
--- src/common/composables/amcharts5/xy-chart-helper.ts
+++ src/common/composables/amcharts5/xy-chart-helper.ts
@@ -1,45 +1,34 @@
-import type { Root } from '@amcharts/amcharts5';
+import type { ITooltipSettings, Root } from '@amcharts/amcharts5';
 import * as am5 from '@amcharts/amcharts5';
+import type { IXYAxis } from '@amcharts/amcharts5/.internal/charts/xy/series/XYSeries';
 import type { IXYChartSettings, IXYSeriesSettings } from '@amcharts/amcharts5/xy';
 import * as am5xy from '@amcharts/amcharts5/xy';
 
-import { DATE_VALUE_FIELD } from '@/common/composables/amcharts5/type';
+import { DATE_FIELD_NAME, CATEGORY_FIELD_NAME } from '@/common/composables/amcharts5/type';
 
 import { gray, white } from '@/styles/colors';
 
-const createTooltip = (root: Root, chart: am5xy.XYChart, isSingle = true): am5.Tooltip => {
+export const createXYTooltip = (root: Root, chart: am5xy.XYChart, settings?: ITooltipSettings): am5.Tooltip => {
     const tooltip = am5.Tooltip.new(root, {
         getFillFromSprite: false,
         getStrokeFromSprite: false,
         autoTextColor: false,
-    });
-    tooltip.label.setAll({
-        text: '{categoryX} [bold]{valueY}[/]',
-        fill: am5.color(gray[900]),
-        fontSize: 14,
+        ...settings,
     });
     tooltip.get('background')?.setAll({
         fill: am5.color(white),
         stroke: am5.color(gray[300]),
         fillOpacity: 0.9,
     });
-    if (!isSingle) {
-        tooltip.label.adapters.add('text', (text) => {
-            let _text = text;
-            chart.series.each((series) => {
-                _text += `\n[${series.get('stroke')?.toString()}]●[/] [bold width:100px]${series.get('name')}:[/] {${series.get('valueYField')}}`;
-            });
-            return _text;
-        });
-    }
+    tooltip.label.setAll({
+        text: `[${gray[700]}]{valueX}[/]`,
+        fill: am5.color(gray[800]),
+        fontSize: 12,
+    });
     return tooltip;
 };
 
-export const createXYDateChart = (root: Root, settings?: IXYChartSettings): {
-    chart: am5xy.XYChart,
-    xAxis: am5xy.DateAxis<am5xy.AxisRenderer>,
-    yAxis: am5xy.ValueAxis<am5xy.AxisRenderer>
-} => {
+const createXYChart = (root: Root, settings?: IXYChartSettings): am5xy.XYChart => {
     const cursor = am5xy.XYCursor.new(root, {});
     cursor.lineX.setAll({
         visible: false,
@@ -48,7 +37,7 @@ export const createXYDateChart = (root: Root, settings?: IXYChartSettings): {
         visible: false,
     });
 
-    const chart = root.container.children.push(am5xy.XYChart.new(root, {
+    return root.container.children.push(am5xy.XYChart.new(root, {
         panX: false,
         panY: false,
         layout: root.verticalLayout,
@@ -56,6 +45,14 @@ export const createXYDateChart = (root: Root, settings?: IXYChartSettings): {
         cursor,
         ...settings,
     }));
+};
+
+export const createXYDateChart = (root: Root, settings?: IXYChartSettings): {
+    chart: am5xy.XYChart,
+    xAxis: am5xy.DateAxis<am5xy.AxisRenderer>,
+    yAxis: am5xy.ValueAxis<am5xy.AxisRenderer>
+} => {
+    const chart = createXYChart(root, settings);
 
     const xRenderer = am5xy.AxisRendererX.new(root, {
         strokeOpacity: 1,
@@ -67,15 +64,17 @@ export const createXYDateChart = (root: Root, settings?: IXYChartSettings): {
     });
     const xAxis = chart.xAxes.push(am5xy.DateAxis.new(root, {
         baseInterval: {
-            timeUnit: 'day',
+            timeUnit: 'month',
             count: 1,
         },
         renderer: xRenderer,
         dateFormats: {
             day: 'M/dd',
             month: 'MMM',
+            year: 'yyyy',
         },
         periodChangeDateFormats: {
+            day: 'M/dd',
             month: 'yyyy',
         },
     }));
@@ -94,28 +93,65 @@ export const createXYDateChart = (root: Root, settings?: IXYChartSettings): {
     return { chart, xAxis, yAxis };
 };
 
-export const createXYLineSeries = (root: Root, chart: am5xy.XYChart, settings: IXYSeriesSettings, processor?: am5.DataProcessor) => {
+export const createXYCategoryChart = (root: Root, settings?: IXYChartSettings): {
+    chart: am5xy.XYChart,
+    xAxis: am5xy.CategoryAxis<am5xy.AxisRenderer>,
+    yAxis: am5xy.ValueAxis<am5xy.AxisRenderer>
+} => {
+    const chart = createXYChart(root, settings);
+
+    const xRenderer = am5xy.AxisRendererX.new(root, {
+        strokeOpacity: 1,
+        strokeWidth: 1,
+        stroke: am5.color(gray[300]),
+    });
+    xRenderer.grid.template.setAll({
+        strokeOpacity: 0,
+    });
+    const xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+        categoryField: CATEGORY_FIELD_NAME,
+        renderer: xRenderer,
+    }));
+
+    const yRenderer = am5xy.AxisRendererY.new(root, {
+        strokeOpacity: 0,
+    });
+    yRenderer.grid.template.setAll({
+        strokeOpacity: 1,
+        strokeWidth: 1,
+        stroke: am5.color(gray[200]),
+    });
+    const yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+        renderer: yRenderer,
+    }));
+    return { chart, xAxis, yAxis };
+};
+
+export const createXYLineSeries = (
+    root: Root,
+    chart: am5xy.XYChart,
+    settings?: Partial<IXYSeriesSettings>,
+): am5xy.LineSeries => {
     const series = chart.series.push(am5xy.LineSeries.new(root, {
-        valueXField: DATE_VALUE_FIELD,
-        tooltip: createTooltip(root, chart, false),
+        xAxis: chart.xAxes.getIndex(0) as IXYAxis,
+        yAxis: chart.yAxes.getIndex(0) as IXYAxis,
+        valueXField: DATE_FIELD_NAME,
         ...settings,
     }));
-    if (processor) series.data.processor = processor;
     series.strokes.template.setAll({
         strokeWidth: 2,
     });
     return series;
 };
 
-export const createXYStackedColumnSeries = (root: Root, chart: am5xy.XYChart, settings: IXYSeriesSettings, processor?: am5.DataProcessor) => {
-    const series = chart.series.push(am5xy.ColumnSeries.new(root, {
-        stacked: true,
-        valueXField: DATE_VALUE_FIELD,
-        tooltip: am5.Tooltip.new(root, {
-            labelText: '{valueY}',
-        }),
-        ...settings,
-    }));
-    if (processor) series.data.processor = processor;
-    return series;
-};
+export const createXYStackedColumnSeries = (
+    root: Root,
+    chart: am5xy.XYChart,
+    settings?: Partial<IXYSeriesSettings>,
+): am5xy.ColumnSeries => chart.series.push(am5xy.ColumnSeries.new(root, {
+    xAxis: chart.xAxes.getIndex(0) as IXYAxis,
+    yAxis: chart.yAxes.getIndex(0) as IXYAxis,
+    stacked: true,
+    valueXField: DATE_FIELD_NAME,
+    ...settings,
+}));
