21233c25d09d3ab1d91b7cac8011fd8f8e9438b4
Wanjin Noh <wanzargen@gmail.com>
2022-11-21 19:57:35 +0900

    feat: add empty base pie widget, and related widget configs

--- src/services/dashboards/widgets/cost-pie/widget-config.ts
+++ src/services/dashboards/widgets/cost-pie/widget-config.ts
@@ -1,3 +1,12 @@
 import type { WidgetConfig } from '@/services/dashboards/widgets/config';
 
-export default {} as WidgetConfig;
+const costPieWidgetConfig: Partial<WidgetConfig> = {
+    widget_config_id: 'costPie',
+    title: 'Cost Pie',
+    base_configs: [{ config_id: 'basePie' }],
+    widget_options: {
+        chart_type: 'PIE',
+    },
+};
+
+export default costPieWidgetConfig;
