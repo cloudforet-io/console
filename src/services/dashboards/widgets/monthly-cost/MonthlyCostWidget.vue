bd316de56300148fa82f3b0100b5580467725392
Wanjin Noh <wanzargen@gmail.com>
2022-11-22 23:04:28 +0900

    refactor: rename use widget data to use widget state

--- src/services/dashboards/widgets/monthly-cost/MonthlyCostWidget.vue
+++ src/services/dashboards/widgets/monthly-cost/MonthlyCostWidget.vue
@@ -13,8 +13,8 @@ import { defineProps } from 'vue';
 import WidgetFrame from '@/services/dashboards/widgets/_components/WidgetFrame.vue';
 import type { WidgetProps } from '@/services/dashboards/widgets/config';
 // eslint-disable-next-line import/no-cycle
-import { useWidgetData } from '@/services/dashboards/widgets/use-widget-data';
+import { useWidgetState } from '@/services/dashboards/widgets/use-widget-state';
 
 const props = defineProps<WidgetProps>();
-const state = useWidgetData(props);
+const state = useWidgetState(props);
 </script>
