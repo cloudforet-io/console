8c4013465ef8a07d1befe1176ed294bf5a6839e2
Yongtae Park <samuel.park@mz.co.kr>
2022-11-24 11:36:17 +0900

    fix(dashboards): remove dashboards route for code freezing

--- src/router/service-routes.ts
+++ src/router/service-routes.ts
@@ -11,7 +11,6 @@ import alertManagerRoute from '@/services/alert-manager/routes';
 import assetInventoryRoute from '@/services/asset-inventory/routes';
 import authRoutes from '@/services/auth/routes';
 import costExplorerRoute from '@/services/cost-explorer/routes';
-import dashboardsRoute from '@/services/dashboards/routes';
 import { HOME_DASHBOARD_ROUTE } from '@/services/home-dashboard/route-config';
 import homeDashboardRoute from '@/services/home-dashboard/routes';
 import infoRoute from '@/services/info/routes';
@@ -34,7 +33,6 @@ export const serviceRoutes: RouteConfig[] = [
         component: { template: '<router-view />' },
         children: [
             homeDashboardRoute,
-            dashboardsRoute,
             administrationRoute,
             assetInventoryRoute,
             projectRoute,
