f58f4951cb1c5e436fdcd7883c0447d8d3bdd011
yuda <yuda@mz.co.kr>
2022-11-24 10:30:17 +0900

    chore(amcharts5): move constants from type.ts to config.ts

---
+++ src/common/composables/amcharts5/config.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_DATE_FIELD_NAME = 'date';
+export const DEFAULT_CATEGORY_FIELD_NAME = 'category';
+
+export const DEFAULT_DATE_FORMAT = 'yyyy-MM';
--- src/common/composables/amcharts5/type.ts
+++ src/common/composables/amcharts5/type.ts
@@ -1,5 +1,1 @@
 export type ChartContext = null | HTMLElement;
-
-export const DATE_FIELD_NAME = 'date';
-export const CATEGORY_FIELD_NAME = 'category';
-export const VALUE_FIELD_NAME = 'value';
