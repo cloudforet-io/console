4ef374cea4f250de56692d68750f2924848f2fe3
yuda <yuda@mz.co.kr>
2022-11-30 17:37:53 +0900

    refactor(colors): use `semanticColors`

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -21,15 +21,15 @@ export const peacock = palette.peacock;
 export const indigo = palette.indigo;
 
 // TEMPORARY CONSTANTS FOR semanticColors
-export const primary = palette.violet[600];
-export const primaryDark = palette.violet[800];
-export const primary1 = palette.violet[400];
-export const primary2 = palette.violet[300];
-export const primary3 = palette.violet[200];
-export const primary4 = palette.violet[100];
-export const secondary = palette.blue[600];
-export const secondaryDark = palette.blue[800];
-export const secondary1 = palette.blue[500];
-export const secondary2 = palette.blue[100];
-export const alert = palette.red[500];
-export const safe = palette.green[600];
+export const primary = semanticColors.primary;
+export const primaryDark = semanticColors.primaryDark;
+export const primary1 = semanticColors.primary1;
+export const primary2 = semanticColors.primary2;
+export const primary3 = semanticColors.primary3;
+export const primary4 = semanticColors.primary4;
+export const secondary = semanticColors.secondary;
+export const secondaryDark = semanticColors.secondaryDark;
+export const secondary1 = semanticColors.secondary1;
+export const secondary2 = semanticColors.secondary2;
+export const alert = semanticColors.alert;
+export const safe = semanticColors.safe;
