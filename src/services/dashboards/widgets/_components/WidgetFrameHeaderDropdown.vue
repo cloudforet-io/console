80282c6da27299b30f38c51e5923c5c4d392c92a
yuda <yuda@mz.co.kr>
2022-11-30 22:01:41 +0900

    feat(widget-frame-header-dropdown): add component

---
+++ src/services/dashboards/widgets/_components/WidgetFrameHeaderDropdown.vue
@@ -0,0 +1,26 @@
+<template>
+    <p-select-dropdown :selected="selected"
+                       :items="props.items"
+                       style-type="transparent"
+                       class="widget-frame-header-dropdown"
+                       @select="handleSelectItem"
+    />
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+import { PSelectDropdown } from '@spaceone/design-system';
+import type { MenuItem } from '@spaceone/design-system/dist/src/inputs/context-menu/type';
+
+interface Props {
+    items: MenuItem[];
+    selected: string;
+}
+const props = defineProps<Props>();
+const emit = defineEmits(['select']);
+
+const handleSelectItem = (selected: string) => {
+    emit('select', selected);
+};
+</script>
