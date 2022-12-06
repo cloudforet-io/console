020eda40f7f2b1f3f8f18eecc8e23b725a4a27d2
MinseoLee <minseolee@mz.co.kr>
2022-12-06 16:03:21 +0900

    feat(dashboard): markups for dateDropdown

--- src/services/dashboards/dashboard-customize/DashboardCustomizePage.vue
+++ src/services/dashboards/dashboard-customize/DashboardCustomizePage.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="dashboard-customize-page">
         CUSTOMIZE DASHBOARD
+        <dashboard-date-dropdown />
     </div>
 </template>
 
 <script lang="ts">
+import DashboardDateDropdown from '@/services/dashboards/modules/dashboard-toolset/DashboardDateDropdown.vue';
+
 export default {
     name: 'DashboardCustomizePage',
+    components: { DashboardDateDropdown },
 };
 </script>
