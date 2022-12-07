8000568b4ff79c73329f212b2083a5d3ab0026ec
MinseoLee <minseolee@mz.co.kr>
2022-12-07 17:34:55 +0900

    feat(currency): add `defaultMode` in dropdown

--- src/services/dashboards/dashboard-customize/DashboardCustomizePage.vue
+++ src/services/dashboards/dashboard-customize/DashboardCustomizePage.vue
@@ -2,14 +2,19 @@
     <div class="dashboard-customize-page">
         CUSTOMIZE DASHBOARD
         <dashboard-date-dropdown />
+        <currency-select-dropdown default-currency-mode
+                                  default-mode
+        />
     </div>
 </template>
 
 <script lang="ts">
+import CurrencySelectDropdown from '@/common/modules/dropdown/currency-select-dropdown/CurrencySelectDropdown.vue';
+
 import DashboardDateDropdown from '@/services/dashboards/modules/dashboard-toolset/DashboardDateDropdown.vue';
 
 export default {
     name: 'DashboardCustomizePage',
-    components: { DashboardDateDropdown },
+    components: { CurrencySelectDropdown, DashboardDateDropdown },
 };
 </script>
