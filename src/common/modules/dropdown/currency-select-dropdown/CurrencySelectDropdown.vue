0c7d1e93dfb00938a887ac7389de8d291f21be61
Minseo Lee <minseolee@mz.co.kr>
2022-12-07 18:09:05 +0900

    feat(currency): add `defaultMode` in dropdown (#411)
    * feat(currency): add `defaultMode` in dropdown
    
    * chore: type

--- src/common/modules/dropdown/currency-select-dropdown/CurrencySelectDropdown.vue
+++ src/common/modules/dropdown/currency-select-dropdown/CurrencySelectDropdown.vue
@@ -6,45 +6,82 @@
                        class="currency-select-dropdown"
                        :class="{ 'print-mode': printMode }"
                        @select="handleSelectCurrency"
-    />
+    >
+        <template v-if="defaultMode"
+                  #default="{ item }"
+        >
+            <span>
+                <span>{{ item.label }}</span>
+                <p-badge v-if="item.badge"
+                         style-type="primary3"
+                         shape="round"
+                >{{ item.badge }}</p-badge>
+            </span>
+        </template>
+        <template v-if="defaultMode"
+                  #menu-item--format="{ item }"
+        >
+            <span>
+                <span>{{ item.label }}</span>
+                <p-badge v-if="item.badge"
+                         style-type="primary3"
+                         shape="round"
+                >
+                    {{ item.badge }}
+                </p-badge>
+            </span>
+        </template>
+    </p-select-dropdown>
 </template>
 
 <script lang="ts">
-
+import type { PropType } from 'vue';
 import {
     computed,
     reactive, toRefs,
 } from 'vue';
 
-import { PSelectDropdown } from '@spaceone/design-system';
+import { PSelectDropdown, PBadge } from '@spaceone/design-system';
 import type { MenuItem } from '@spaceone/design-system/dist/src/inputs/context-menu/type';
 
 import { store } from '@/store';
 
-import { CURRENCY_SYMBOL } from '@/store/modules/display/config';
+import type { Currency } from '@/store/modules/display/config';
+import { CURRENCY, CURRENCY_SYMBOL } from '@/store/modules/display/config';
 
 export default {
     name: 'CurrencySelectDropdown',
     components: {
         PSelectDropdown,
+        PBadge,
     },
     props: {
         printMode: {
             type: Boolean,
             default: false,
         },
+        defaultMode: {
+            type: Boolean,
+            default: false,
+        },
+        defaultCurrency: {
+            type: String as PropType<Currency>,
+            default: CURRENCY.USD,
+        },
     },
     setup(props, { emit }) {
         const state = reactive({
-            currency: computed(() => store.state.display.currency),
+            currency: props.defaultMode ? props.defaultCurrency : computed(() => store.state.display.currency),
             currencyItems: computed<MenuItem[]>(() => Object.keys(store.state.display.currencyRates).map((currency) => ({
                 type: 'item',
                 name: currency,
                 label: `${CURRENCY_SYMBOL[currency]}${currency}`,
+                // song-lang
+                badge: currency === props.defaultCurrency ? 'Default' : '',
             }))),
         });
 
-        const handleSelectCurrency = (currency) => {
+        const handleSelectCurrency = (currency: Currency) => {
             store.commit('display/setCurrency', currency);
             emit('update', currency);
         };
@@ -63,5 +100,8 @@ export default {
     .text {
         white-space: nowrap;
     }
+}
+.p-badge {
+    margin-left: 0.25rem;
 }
 </style>
