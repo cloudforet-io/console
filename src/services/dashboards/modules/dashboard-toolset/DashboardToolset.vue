43579dbba3822ed0c47020ef96b652366e63d4c5
MinseoLee <minseolee@mz.co.kr>
2022-12-07 19:05:10 +0900

    feat(dashboard): add dashboardToolset

---
+++ src/services/dashboards/modules/dashboard-toolset/DashboardToolset.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="dashboard-toolset">
+        <dashboard-date-range-badge />
+        <dashboard-date-dropdown />
+        <currency-select-dropdown default-mode />
+    </div>
+</template>
+
+<script setup lang="ts">
+import CurrencySelectDropdown from '@/common/modules/dropdown/currency-select-dropdown/CurrencySelectDropdown.vue';
+
+import DashboardDateDropdown from '@/services/dashboards/modules/dashboard-toolset/DashboardDateDropdown.vue';
+import DashboardDateRangeBadge from '@/services/dashboards/modules/dashboard-toolset/DashboardDateRangeBadge.vue';
+</script>
+
+<style scoped>
+.dashboard-date-range-badge {
+    margin-right: 0.5rem;
+}
+</style>
