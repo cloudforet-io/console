335ccd2adc83a88b0e965ef6067616da8f67b55e
Minseo Lee <minseolee@mz.co.kr>
2022-12-08 10:08:56 +0900

    feat(dashboard): add dashboardToolset (#414)

--- src/services/dashboards/dashboard-customize/DashboardCustomizePage.vue
+++ src/services/dashboards/dashboard-customize/DashboardCustomizePage.vue
@@ -1,20 +1,10 @@
 <template>
     <div class="dashboard-customize-page">
         CUSTOMIZE DASHBOARD
-        <dashboard-date-dropdown />
-        <currency-select-dropdown default-currency-mode
-                                  default-mode
-        />
+        <dashboard-toolset />
     </div>
 </template>
 
-<script lang="ts">
-import CurrencySelectDropdown from '@/common/modules/dropdown/currency-select-dropdown/CurrencySelectDropdown.vue';
-
-import DashboardDateDropdown from '@/services/dashboards/modules/dashboard-toolset/DashboardDateDropdown.vue';
-
-export default {
-    name: 'DashboardCustomizePage',
-    components: { CurrencySelectDropdown, DashboardDateDropdown },
-};
+<script setup lang="ts">
+import DashboardToolset from '@/services/dashboards/modules/dashboard-toolset/DashboardToolset.vue';
 </script>
---
+++ src/services/dashboards/modules/dashboard-toolset/DashboardDateRangeBadge.vue
@@ -0,0 +1,13 @@
+<template>
+    <p-badge style-type="gray200"
+             class="dashboard-date-range-badge"
+    >
+        {{ MOCK_DATE }}
+    </p-badge>
+</template>
+
+<script setup lang="ts">
+import { PBadge } from '@spaceone/design-system';
+
+const MOCK_DATE = 'UTC December 18 ~ 25, 2022';
+</script>
