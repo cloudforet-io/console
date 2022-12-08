468c502af352afb6c9ecf539be4a7d9b3fa6c3c8
yuda <yuda@mz.co.kr>
2022-12-08 14:27:50 +0900

    fix(widget): change widget files and configs

--- src/services/dashboards/widgets/aws-data-transfer-cost-trend/widget-config.ts
+++ src/services/dashboards/widgets/aws-data-transfer-cost-trend/widget-config.ts
@@ -1,11 +1,9 @@
 import type { WidgetConfig } from '@/services/dashboards/widgets/config';
-import { CHART_TYPE, GRANULARITY, GROUP_BY } from '@/services/dashboards/widgets/config';
+import { CHART_TYPE, GRANULARITY } from '@/services/dashboards/widgets/config';
 
 const awsDataTransferCostTrendWidgetConfig: WidgetConfig = {
     widget_config_id: 'awsDataTransferCostTrend',
-    widget_component: () => ({
-        component: import('@/services/dashboards/widgets/aws-data-transfer-cost-trend/AWSDataTransferCostTrend.vue'),
-    }),
+    base_configs: [{ config_id: 'baseTrend' }],
     title: 'AWS Data-Transfer Cost Trend',
     labels: ['Cost'],
     description: {
@@ -19,9 +17,8 @@ const awsDataTransferCostTrendWidgetConfig: WidgetConfig = {
     },
     sizes: ['lg', 'full'],
     widget_options: {
-        chart_type: CHART_TYPE.STACKED_COLUMN,
-        granularity: GRANULARITY.ACCUMULATED,
-        group_by: GROUP_BY.PROJECT,
+        chart_type: CHART_TYPE.LINE,
+        granularity: GRANULARITY.MONTHLY,
         legend_options: {
             enabled: true,
             show_at: 'chart',
