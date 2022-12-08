f31151d004dddcedfa1d1cb8faa996574ab25963
yuda <yuda@mz.co.kr>
2022-12-08 11:35:41 +0900

    feat(customize-sidebar): add dashboard customize sidebar

--- src/services/dashboards/dashboard-customize/DashboardCustomizePage.vue
+++ src/services/dashboards/dashboard-customize/DashboardCustomizePage.vue
@@ -2,9 +2,19 @@
     <div class="dashboard-customize-page">
         CUSTOMIZE DASHBOARD
         <dashboard-toolset />
+        <dashboard-customize-sidebar />
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import DashboardCustomizeSidebar from '@/services/dashboards/dashboard-customize/modules/DashboardCustomizeSidebar.vue';
 import DashboardToolset from '@/services/dashboards/modules/dashboard-toolset/DashboardToolset.vue';
+
+export default {
+    name: 'DashboardCustomizePage',
+    components: {
+        DashboardToolset,
+        DashboardCustomizeSidebar,
+    },
+};
 </script>
