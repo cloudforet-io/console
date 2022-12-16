2b076eb1fd5f52f71f99f208e415aeffd0237ed5
Dahyun Yu <yuda@mz.co.kr>
2022-12-16 15:00:31 +0900

    fix(widget-helper): fix bug when merging array (#479)

--- src/services/dashboards/widgets/__tests__/widget-helper.test.ts
+++ src/services/dashboards/widgets/__tests__/widget-helper.test.ts
@@ -1,12 +1,15 @@
 import { describe, expect, it } from 'vitest';
 
 import basePieWidgetConfig from '@/services/dashboards/widgets/_base/base-pie/widget-config';
+import baseTrendWidgetConfig from '@/services/dashboards/widgets/_base/base-trend/widget-config';
 import dashboardCommonWidgetConfig from '@/services/dashboards/widgets/_base/dashboard-common/widget-config';
 import costPieWidgetConfig from '@/services/dashboards/widgets/cost-pie/widget-config';
+import costTrendWidgetConfig from '@/services/dashboards/widgets/cost-trend/widget-config';
 import { getWidgetConfig } from '@/services/dashboards/widgets/widget-helper';
 
 const costPieConfigId = costPieWidgetConfig.widget_config_id;
 const basePieConfigId = basePieWidgetConfig.widget_config_id;
+const costTrendConfigId = costTrendWidgetConfig.widget_config_id;
 const dashboardCommonConfigId = dashboardCommonWidgetConfig.widget_config_id;
 const dashboardCommonSchema = dashboardCommonWidgetConfig.widget_options_schema?.schema;
 const costPieSchema = costPieWidgetConfig.widget_options_schema?.schema;
@@ -33,5 +36,12 @@ describe('[Widget Helper] getWidgetConfig', () => {
         if (!costPieConfigId) throw new Error('Can not test. No widget config id in costPieConfigId');
         const mergedCostPieConfig = getWidgetConfig(costPieConfigId);
         expect(mergedCostPieConfig.widget_options_schema?.schema?.properties['filters.provider']).toBeTruthy();
+    });
+    it('Merge default_properties of base trend config', () => {
+        expect(baseTrendWidgetConfig?.widget_options_schema?.default_properties).toMatchObject(['group_by']);
+        const mergedBaseTrendConfig = getWidgetConfig(costTrendConfigId);
+        expect(mergedBaseTrendConfig?.widget_options_schema?.default_properties).toEqual(expect.arrayContaining([
+            'group_by', 'filters.provider', 'filters.project_id', 'filters.service_account_id',
+        ]));
     });
 });
