23f96debe5edbb23e5dddc1f6d3cea9a8d477794
sulmo <tjdah0853@gmail.com>
2022-12-16 18:01:14 +0900

    feat(dashboard-gnb): apply favorite and recent apis
    fix: remove log

--- src/lib/helper/router-recent-helper.ts
+++ src/lib/helper/router-recent-helper.ts
@@ -7,9 +7,17 @@ import type { MenuId } from '@/lib/menu/config';
 import { MENU_INFO_MAP } from '@/lib/menu/menu-info';
 
 import { ASSET_INVENTORY_ROUTE } from '@/services/asset-inventory/route-config';
+import { DASHBOARDS_ROUTE } from '@/services/dashboards/route-config';
 import { PROJECT_ROUTE } from '@/services/project/route-config';
 
 export const getRecentConfig = (to: Route): RecentConfig | undefined => {
+    /* DASHBOARD */
+    if (to.name === DASHBOARDS_ROUTE.DETAIL._NAME) {
+        const dashboardId = to?.params?.dashboardId;
+        console.log('dashboardId', dashboardId);
+        if (!dashboardId) return undefined;
+        return { itemType: RECENT_TYPE.DASHBOARD, itemId: dashboardId };
+    }
     /* ClOUD SERVICE */
     if (to.name === ASSET_INVENTORY_ROUTE.CLOUD_SERVICE.DETAIL._NAME) {
         const provider = to?.params?.provider;
