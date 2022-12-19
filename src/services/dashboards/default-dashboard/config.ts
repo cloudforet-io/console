b4465b3b3b841a1c52fcefebdd1e2efa8e7fa742
Wanjin Noh <wanzargen@gmail.com>
2022-12-19 22:43:37 +0900

    feat: add error case widget info (must add this case widget too)

---
+++ src/services/dashboards/default-dashboard/config.ts
@@ -0,0 +1,14 @@
+import type { DashboardConfig } from '@/services/dashboards/config';
+import type { DashboardLayoutWidgetInfo } from '@/services/dashboards/widgets/config';
+import { WIDGET_SIZE } from '@/services/dashboards/widgets/config';
+
+export type DefaultDashboardPreviewConfig = Pick<DashboardConfig, 'name'|'version'|'labels'|'description'>;
+
+export const ERROR_CASE_WIDGET_INFO: DashboardLayoutWidgetInfo = {
+    title: 'Error',
+    widget_name: 'widgetError',
+    widget_options: {},
+    size: WIDGET_SIZE.md,
+    version: '1',
+    inherit_options: {},
+};
