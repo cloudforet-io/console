fc14e770f37d13b366e10307fa865d7e3b1ebb78
MinseoLee <minseolee@mz.co.kr>
2022-12-22 13:45:37 +0900

    chore: add trycatch

--- src/services/dashboards/dashboard-create/DashboardCreatePage.vue
+++ src/services/dashboards/dashboard-create/DashboardCreatePage.vue
@@ -37,8 +37,10 @@ import { PPageTitle, PButton } from '@spaceone/design-system';
 
 import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 
+import { SpaceRouter } from '@/router';
 import { i18n } from '@/translations';
 
+import ErrorHandler from '@/common/composables/error/errorHandler';
 import { useFormValidator } from '@/common/composables/form-validator';
 
 import {
@@ -49,6 +51,7 @@ import type { DashboardScope, DashboardViewer } from '@/services/dashboards/conf
 import DashboardScopeForm from '@/services/dashboards/dashboard-create/modules/DashboardScopeForm.vue';
 import DashboardTemplateForm from '@/services/dashboards/dashboard-create/modules/DashboardTemplateForm.vue';
 import DashboardViewerForm from '@/services/dashboards/dashboard-create/modules/DashboardViewerForm.vue';
+import { DASHBOARDS_ROUTE } from '@/services/dashboards/route-config';
 import type { ProjectItemResp } from '@/services/project/type';
 
 export default {
@@ -84,19 +87,29 @@ export default {
             dashboardViewerType: DASHBOARD_VIEWER.PUBLIC as DashboardViewer,
         });
 
-        const handleClickCreate = () => {
+        const handleClickCreate = async () => {
             const dashboardCreateParams = {
                 // TODO:: connect real name after bind templates
                 name: `${state.dashboardScope} ${state.dashboardViewerType} dashboard - ${(Math.random().toString().slice(3, 6))}`,
                 viewers: state.dashboardViewerType,
             };
 
-            if (state.dashboardScope === DASHBOARD_SCOPE.PROJECT) {
-                SpaceConnector.clientV2.dashboard.projectDashboard.create({
-                    ...dashboardCreateParams,
-                    project_id: dashboardProject.value?.id ?? '',
-                });
-            } else SpaceConnector.clientV2.dashboard.domainDashboard.create(dashboardCreateParams);
+            try {
+                let dashboardId = '';
+                if (state.dashboardScope === DASHBOARD_SCOPE.PROJECT) {
+                    const result = await SpaceConnector.clientV2.dashboard.projectDashboard.create({
+                        ...dashboardCreateParams,
+                        project_id: dashboardProject.value?.id ?? '',
+                    });
+                    dashboardId = result.project_dashboard_id;
+                } else {
+                    const result = await SpaceConnector.clientV2.dashboard.domainDashboard.create(dashboardCreateParams);
+                    dashboardId = result.domain_dashboard_id;
+                }
+                await SpaceRouter.router.push({ name: DASHBOARDS_ROUTE.DETAIL._NAME, params: { dashboardId } });
+            } catch (e) {
+                ErrorHandler.handleError(e);
+            }
         };
 
         return {
