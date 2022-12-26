516113f8e1b5dc49fcb9d22f13834d30565dbf41
Yongtae Park <samuel.park@mz.co.kr>
2022-12-26 13:10:26 +0900

    feat(variable-dropdown): create variable selector dropdown markup

--- src/services/dashboards/dashboard-customize/DashboardCustomizePage.vue
+++ src/services/dashboards/dashboard-customize/DashboardCustomizePage.vue
@@ -9,14 +9,21 @@
             <dashboard-toolset :date-range.sync="state.dateRange" />
         </div>
         <p-divider />
-        <div>
-            <dashboard-variable-dropdown />
-            <p-button icon-left="ic_plus"
-                      style-type="highlight"
-                      @click="handleOpenOverlay"
-            >
-                {{ $t('DASHBOARDS.CUSTOMIZE.VARIABLES.MORE') }}
-            </p-button>
+        <div class="dashboard-selectors">
+            <div class="variable-selector-wrapper">
+                <dashboard-variable-dropdown variable-name="Project" />
+                <dashboard-variable-dropdown variable-name="Project" />
+                <dashboard-variable-dropdown variable-name="Project" />
+                <dashboard-variable-dropdown variable-name="Project" />
+                <dashboard-variable-dropdown variable-name="Project" />
+                <dashboard-variable-dropdown variable-name="Project" />
+                <p-button icon-left="ic_plus"
+                          style-type="highlight"
+                          @click="handleOpenOverlay"
+                >
+                    {{ $t('DASHBOARDS.CUSTOMIZE.VARIABLES.MORE') }}
+                </p-button>
+            </div>
             <dashboard-refresh-dropdown :interval-option.sync="state.refreshInterval"
                                         refresh-disabled
             />
@@ -67,3 +74,18 @@ const handleUpdateLabelList = (labelList: Array<string>) => {
     state.labelList = labelList;
 };
 </script>
+
+<style lang="postcss" scoped>
+.dashboard-customize-page {
+
+    .dashboard-selectors {
+        @apply flex justify-between;
+
+        .variable-selector-wrapper {
+            @apply flex items-center flex-wrap;
+            gap: 0.5rem;
+            padding: 1.5rem 0 1.25rem;
+        }
+    }
+}
+</style>
--- src/services/dashboards/dashboard-customize/modules/DashboardVariableDropdown.vue
+++ src/services/dashboards/dashboard-customize/modules/DashboardVariableDropdown.vue
@@ -2,17 +2,17 @@
     <div v-on-click-outside="hideContextMenu"
          class="dashboard-variable-dropdown"
     >
-        <button ref="targetRef"
-                class="dropdown-box"
-                :class="{ 'is-visible': visibleMenu }"
+        <div ref="targetRef"
+             class="dropdown-box"
+             :class="{ 'is-visible': visibleMenu, 'filled-value': state.selected.length }"
         >
-            <span>{{ variableName }}</span>
+            <span class="variable-label">{{ variableName }}</span>
             <div v-if="state.selected.length"
                  class="selected-items"
             >
                 <span class="item-for-display">{{ state.selected[0].label }}</span>
                 <p-badge v-if="state.selected.length > 1"
-                         style-type="blue200"
+                         style-type="blue300"
                 >
                     +{{ state.selected.length - 1 }}
                 </p-badge>
@@ -23,7 +23,7 @@
                  color="inherit"
                  class="dropdown-icon"
             />
-        </button>
+        </div>
         <p-context-menu v-show="visibleMenu"
                         ref="contextMenuRef"
                         :menu="state.menu"
@@ -39,7 +39,7 @@ import {
 
 import { PBadge, PContextMenu, PI } from '@spaceone/design-system';
 import type { MenuItem } from '@spaceone/design-system/dist/src/inputs/context-menu/type';
-import { useContextMenuController } from '@spaceone/design-system/src/hooks/context-menu-controller';
+// import { useContextMenuController } from '@spaceone/design-system/src/hooks/context-menu-controller';
 
 const props = defineProps<{
     variableName: string;
@@ -48,43 +48,63 @@ const props = defineProps<{
 
 const state = reactive({
     menu: props.menu ?? [] as MenuItem[],
-    selected: [] as MenuItem[],
+    selected: [
+        { name: 'spaceone', label: 'SpaceONE Dev' },
+        { name: 'spaceone', label: 'SpaceONE Dev' },
+        { name: 'spaceone', label: 'SpaceONE Dev' },
+    ] as MenuItem[],
 });
-const {
-    visibleMenu,
-    // showContextMenu,
-    hideContextMenu,
-    // focusOnContextMenu,
-    // reorderMenuBySelection,
-    // fixedMenuStyle,
-} = useContextMenuController({
-    targetRef: ref(null),
-    contextMenuRef: ref(null),
-    useReorderBySelection: true,
-    useFixedStyle: true,
-});
+// const {
+//     visibleMenu,
+//     // showContextMenu,
+//     hideContextMenu,
+//     // focusOnContextMenu,
+//     // reorderMenuBySelection,
+//     // fixedMenuStyle,
+// } = useContextMenuController({
+//     targetRef: ref(null),
+//     contextMenuRef: ref(null),
+//     useReorderBySelection: true,
+//     useFixedStyle: true,
+// });
+const visibleMenu = ref(false);
+const hideContextMenu = () => {
+    console.log('hide menu');
+};
+
 </script>
 
 <style lang="postcss" scoped>
 .dashboard-variable-dropdown {
+    @apply inline-block;
 
     .dropdown-box {
-        @apply border border-solid border-blue-400 bg-blue-200 rounded-md;
+        @apply flex items-center border border-solid border-gray-300 bg-white rounded-md;
+        height: 2rem;
+        padding: 0 0.25rem 0 0.75rem;
+
+        .variable-label {
+            @apply text-gray-900 text-label-md;
+        }
 
         .selected-items {
             @apply inline-flex items-center;
+            padding: 0 0.5rem;
 
             .item-for-display {
-                /* implementation */
+                @apply text-gray-900 text-label-md font-bold;
+                margin-right: 0.25rem;
             }
         }
 
+        &:hover {
+            @apply border-blue-600 bg-blue-100;
+        }
         &.is-visible {
             @apply border-blue-600;
         }
-
-        &:hover {
-            @apply border-blue-600;
+        &.filled-value {
+            @apply border-blue-400 bg-blue-200;
         }
     }
 }
