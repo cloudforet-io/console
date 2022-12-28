89858dba648c87f969759f82ea051152eab232fb
yuda <yuda@mz.co.kr>
2022-12-28 13:27:54 +0900

    fix(widget-form-store): change pinia from options API to composition API

--- src/services/dashboards/dashboard-customize/stores/widget-form.ts
+++ src/services/dashboards/dashboard-customize/stores/widget-form.ts
@@ -1,3 +1,5 @@
+import { reactive, toRefs } from 'vue';
+
 import { defineStore } from 'pinia';
 
 import type { InheritOptions, WidgetOptions } from '@/services/dashboards/widgets/config';
@@ -11,60 +13,65 @@ interface State {
     inheritOptions?: InheritOptions;
     widgetOptions?: WidgetOptions;
 }
-export const useWidgetFormStore = defineStore('form', {
-    state: (): State => ({
+
+export const useWidgetFormStore = defineStore('widget-form', () => {
+    const state = reactive<State>({
         widgetConfigId: undefined,
         widgetTitle: undefined,
         isValid: false,
         inheritOptions: undefined,
         widgetOptions: undefined,
-    }),
-    actions: {
-        setWidgetConfigId(val?: string) {
-            this.widgetConfigId = val;
-        },
-        setWidgetTitle(val?: string) {
-            this.widgetTitle = val;
-        },
-        setIsValid(val: boolean) {
-            this.isValid = val;
-        },
-        setFormData(formData: any, inheritItemMap: Record<string, boolean>) {
-            if (!this.widgetConfigId) return;
-            const widgetConfig = getWidgetConfig(this.widgetConfigId);
-            const widgetOptions: WidgetOptions = {
-                ...widgetConfig.options,
-            };
-            const widgetFiltersMap: WidgetOptions['filters'] = {};
-            const inheritOptions: InheritOptions = {};
-            Object.entries(formData).forEach(([key, val]) => {
-                const _propertyName = key.replace('filters.', '');
-                if (inheritItemMap[key]) {
-                    if (!val) return;
-                    inheritOptions[_propertyName] = {
-                        enabled: true,
-                        variable_info: {
-                            key: val as string,
-                        },
-                    };
-                } else if (key.startsWith('filters.')) {
-                    widgetFiltersMap[_propertyName] = [];
-                    if (Array.isArray(val)) {
-                        val.forEach((v) => {
-                            widgetFiltersMap[_propertyName].push({ k: _propertyName, v, o: '' });
-                        });
-                    } else {
-                        widgetFiltersMap[_propertyName].push({ k: _propertyName, v: val, o: '' });
-                    }
-                } else {
-                    widgetOptions[_propertyName] = val;
-                }
-            });
-            widgetOptions.filters = widgetFiltersMap;
+    });
+    const setWidgetConfigId = (val?: string) => {
+        state.widgetConfigId = val;
+    };
+    const setWidgetTitle = (val?: string) => {
+        state.widgetTitle = val;
+    };
+    const setIsValid = (val: boolean) => {
+        state.isValid = val;
+    };
+    const setFormData = (formData: any, inheritItemMap: Record<string, boolean>) => {
+        if (!state.widgetConfigId) return;
+        const widgetConfig = getWidgetConfig(state.widgetConfigId);
+        const widgetOptions: WidgetOptions = {
+            ...widgetConfig.options,
+        };
+        const widgetFiltersMap: WidgetOptions['filters'] = {};
+        const inheritOptions: InheritOptions = {};
+        Object.entries(formData).forEach(([key, val]) => {
+            const _propertyName = key.replace('filters.', '');
+            if (inheritItemMap[key]) {
+                if (!val) return;
+                inheritOptions[_propertyName] = {
+                    enabled: true,
+                    variable_info: {
+                        key: val as string,
+                    },
+                };
+            } else if (key.startsWith('filters.')) {
+                widgetFiltersMap[_propertyName] = [];
+                if (Array.isArray(val)) {
+                    val.forEach((v) => {
+                        if (v) widgetFiltersMap[_propertyName].push({ k: _propertyName, v, o: '' });
+                    });
+                } else if (val) widgetFiltersMap[_propertyName].push({ k: _propertyName, v: val, o: '' });
+            } else {
+                widgetOptions[_propertyName] = val;
+            }
+        });
+        widgetOptions.filters = widgetFiltersMap;
 
-            //
-            this.widgetOptions = widgetOptions;
-            this.inheritOptions = inheritOptions;
-        },
-    },
+        //
+        state.widgetOptions = widgetOptions;
+        state.inheritOptions = inheritOptions;
+    };
+
+    return {
+        ...toRefs(state),
+        setWidgetConfigId,
+        setWidgetTitle,
+        setIsValid,
+        setFormData,
+    };
 });
