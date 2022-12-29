27460053fbbdbef3958d75d3461b04d2c3f0dafc
yuda <yuda@mz.co.kr>
2022-12-29 13:39:59 +0900

    fix(widget-helepr): add table data converter

--- src/services/dashboards/widgets/widget-helper.ts
+++ src/services/dashboards/widgets/widget-helper.ts
@@ -1,11 +1,17 @@
 import type { AsyncComponent } from 'vue';
 
+
+import type { DataTableFieldType } from '@spaceone/design-system/types/data-display/tables/data-table/type';
+import dayjs from 'dayjs';
 import {
     keyBy, merge, mergeWith, sortBy, values,
 } from 'lodash';
 
+import { GRANULARITY } from '@/services/cost-explorer/lib/config';
+import { getTimeUnitByPeriod } from '@/services/cost-explorer/lib/helper';
+import type { DateRange } from '@/services/dashboards/config';
 import type {
-    BaseConfigInfo,
+    BaseConfigInfo, Granularity,
     GroupBy, WidgetConfig,
 } from '@/services/dashboards/widgets/config';
 import type { HistoryDataModel, XYChartData } from '@/services/dashboards/widgets/type';
@@ -106,4 +112,60 @@ export const getRefinedXYChartData = (
         chartData = mergeByKey(chartData, refinedList, categoryKey);
     });
     return sortBy(chartData, categoryKey);
+};
+
+/**
+ * @name getRefinedDataTableFields
+ * @description Get refined PDataTable fields.
+ */
+export const getRefinedDataTableFields = (granularity: Granularity, dateRange: DateRange): DataTableFieldType[] => {
+    if (!granularity || !dateRange?.end) return [];
+    const dateFields: DataTableFieldType[] = [];
+    const start = dayjs.utc(dateRange.start);
+    const end = dayjs.utc(dateRange.end);
+
+    const timeUnit = getTimeUnitByPeriod(granularity, dayjs.utc(dateRange.start), dayjs.utc(dateRange.end));
+    let dateFormat = 'YYYY-MM-DD';
+    if (granularity === GRANULARITY.MONTHLY) dateFormat = 'YYYY-MM';
+    if (granularity === GRANULARITY.YEARLY) dateFormat = 'YYYY';
+
+    const nameDateFormat = dateFormat;
+    let labelDateFormat = 'M/D';
+    if (timeUnit === 'month') {
+        labelDateFormat = 'MMM';
+    } else if (timeUnit === 'year') {
+        labelDateFormat = 'YYYY';
+    }
+
+    let now = start;
+    while (now.isSameOrBefore(end, timeUnit)) {
+        dateFields.push({
+            name: now.format(nameDateFormat),
+            label: now.locale('en').format(labelDateFormat),
+            textAlign: 'right',
+            sortable: true,
+        });
+        now = now.add(1, timeUnit);
+    }
+    return dateFields;
+};
+
+/**
+ * @name getRefinedDataTableData
+ * @description Get refined data of PDataTable.
+ * @example [{ "provider": "aws", "2022-09": 123, "2022-10": 1, "2022-11": 10 }]
+ */
+export const getRefinedDataTableData = (rawData: HistoryDataModel['results'], groupBy: GroupBy, valueKey = 'usd_cost_sum') => {
+    if (!rawData || !groupBy) return [];
+    const results: any = [];
+    rawData.forEach((d) => {
+        const result = {
+            [groupBy]: d[groupBy],
+        };
+        d[valueKey].forEach((val) => {
+            result[val.date] = val.value;
+        });
+        results.push(result);
+    });
+    return results;
 };
