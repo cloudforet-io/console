0f8d2927f11dd8a4c575a3c527f60f0741558421
sulmo <50662170+sulmoJ@users.noreply.github.com>
2023-01-03 17:06:23 +0900

    feat(widget-frame): add fullSize toggle function (#598)
    * fix(dashboard-widget-container): remove unused event
    
    * feat(dashboard-detail-info): add setFullSizeWidget action to store
    
    * feat(widget-frame): add states for widgetFrame
    
    * feat(widget-frame): add fullSize toggle function
    
    * fix: fix minor things

--- src/services/dashboards/widgets/use-widget-frame-props.ts
+++ src/services/dashboards/widgets/use-widget-frame-props.ts
@@ -1,13 +1,37 @@
 import type { ComputedRef } from 'vue';
 import { computed } from 'vue';
 
-import type { WidgetProps } from '@/services/dashboards/widgets/config';
+import type { Currency } from '@/store/modules/display/config';
 
-export const useWidgetFrameProps = (props: WidgetProps, state):ComputedRef => computed(() => ({
-    title: state.title,
-    size: state.size,
+import type { WidgetConfig, WidgetSize } from '@/services/dashboards/widgets/config';
+import { WIDGET_SIZE } from '@/services/dashboards/widgets/config';
+
+interface WidgetFrameState {
+    title: string;
+    size: WidgetSize;
+    dateRange: string;
+    currency: Currency;
+    disableFullSize: boolean;
+    isOnlyFullSize: boolean;
+    widgetConfig?: WidgetConfig;
+}
+
+interface WidgetFrameProps {
+    widgetKey: string;
+    width?: number;
+    editMode?: boolean;
+}
+
+type WidgetFrameBaseProps = WidgetFrameState & WidgetFrameProps;
+
+export const useWidgetFrameProps = (props: WidgetFrameProps, state: WidgetFrameState):ComputedRef => computed<WidgetFrameBaseProps>(() => ({
+    widgetKey: props.widgetKey,
     width: props.width,
     editMode: props.editMode,
+    title: state.title,
+    size: state.size,
     dateRange: state.dateRange,
     currency: state.currency,
+    disableFullSize: state.disableFullSize,
+    isOnlyFullSize: state.widgetConfig?.sizes.length === 1 && state.widgetConfig?.sizes[0] === WIDGET_SIZE.full,
 }));
