b6540fd89339593d09e6f6b6ca1e3855552a972f
yuda <yuda@mz.co.kr>
2023-01-05 22:20:56 +0900

    fix(BasePieWidget): set api data

--- src/services/dashboards/widgets/cost-pie/widget-config.ts
+++ src/services/dashboards/widgets/cost-pie/widget-config.ts
@@ -1,4 +1,6 @@
 import type { WidgetConfig } from '@/services/dashboards/widgets/_configs/config';
+import { GROUP_BY } from '@/services/dashboards/widgets/_configs/config';
+import { GROUP_BY_ITEM_MAP } from '@/services/dashboards/widgets/_configs/view-config';
 
 const costPieWidgetConfig: Partial<WidgetConfig> = {
     widget_config_id: 'costPie',
@@ -9,8 +11,20 @@ const costPieWidgetConfig: Partial<WidgetConfig> = {
         translation_id: 'DASHBOARDS.WIDGET.COST_PIE.DESC',
         preview_image: 'widget-img_costPie--thumbnail.png',
     },
-    options: {
-        chart_type: 'PIE',
+    options_schema: {
+        default_properties: ['group_by'],
+        schema: {
+            type: 'object',
+            properties: {
+                group_by: {
+                    title: 'Group By',
+                    type: 'string',
+                    enum: Object.values(GROUP_BY),
+                    menuItems: Object.values(GROUP_BY_ITEM_MAP),
+                },
+            },
+            required: ['group_by'],
+        },
     },
 };
 
