240af616de8b24bf2fbb8901506eda63999ca279
sulmo <tjdah0853@gmail.com>
2023-01-05 18:17:41 +0900

    chore(asset-widgets): create files

---
+++ src/services/dashboards/widgets/aws-trusted-advisor/workspace-scope/AwsTrustedAdvisorProjectScope.vue
@@ -0,0 +1,17 @@
+<template>
+    <div />
+</template>
+
+<script setup lang="ts">
+import {
+    defineProps,
+} from 'vue';
+
+
+import type { WidgetProps } from '@/services/dashboards/widgets/_configs/config';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = defineProps<WidgetProps>();
+
+
+</script>
