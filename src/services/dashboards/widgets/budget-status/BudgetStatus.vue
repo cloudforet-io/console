422ede104e5a1bba99d4a77facd5a8ea6867337e
yuda <yuda@mz.co.kr>
2023-01-09 10:34:36 +0900

    chore: edit style

--- src/services/dashboards/widgets/budget-status/BudgetStatus.vue
+++ src/services/dashboards/widgets/budget-status/BudgetStatus.vue
@@ -185,6 +185,7 @@ defineExpose<WidgetExpose<Data>>({
     .chart-wrapper {
         width: 100%;
         height: 100%;
+        position: relative;
         padding: 0 1.5rem 1rem 1.5rem;
         .waffle-chart {
             display: flex;
