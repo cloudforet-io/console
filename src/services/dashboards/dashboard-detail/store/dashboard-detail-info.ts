38d5edf3d2a5a2b5a5b52bee8f13c454dd485ed3
yuda <yuda@mz.co.kr>
2023-01-10 10:11:10 +0900

    refactor(dashboard-detail-info): split states

--- src/services/dashboards/dashboard-detail/store/dashboard-detail-info.ts
+++ src/services/dashboards/dashboard-detail/store/dashboard-detail-info.ts
@@ -1,4 +1,4 @@
-import type { ComputedRef, UnwrapRef } from 'vue';
+import type { ComputedRef } from 'vue';
 import {
     computed, reactive,
 } from 'vue';
@@ -20,22 +20,30 @@ import type {
     DateRange, DashboardViewer, DashboardSettings, DashboardVariables, DashboardVariablesSchema,
 } from '@/services/dashboards/config';
 import { DASHBOARD_VIEWER } from '@/services/dashboards/config';
-import type { DashboardContainerWidgetInfo } from '@/services/dashboards/dashboard-detail/lib/type';
 import type { DashboardModel, ProjectDashboardModel } from '@/services/dashboards/model';
+import type { DashboardLayoutWidgetInfo } from '@/services/dashboards/widgets/_configs/config';
 import { WIDGET_SIZE } from '@/services/dashboards/widgets/_configs/config';
 import { getWidgetConfig } from '@/services/dashboards/widgets/_helpers/widget-helper';
 
 interface WidgetDataMap {
     [widgetKey: string]: any;
 }
+interface WidgetInheritVariablesValidMap {
+    [widgetKey: string]: {
+        [propertyName: string]: boolean;
+    }
+}
 
+interface DashboardDetailInfoOriginState {
+    isProjectDashboard: ComputedRef<boolean>;
+    dashboardViewer: ComputedRef<DashboardViewer>;
+    dashboardInfo: DashboardModel|null;
+    dashboardWidgetInfoList: ComputedRef<DashboardLayoutWidgetInfo[]>;
+}
 interface DashboardDetailInfoStoreState {
     loadingDashboard: boolean;
     dashboardId: string | undefined;
     projectId: string;
-    isProjectDashboard: ComputedRef<boolean>;
-    dashboardInfo: DashboardModel|null;
-    dashboardViewer: ComputedRef<DashboardViewer>;
     dashboardName: string;
     enableCurrency: boolean;
     currency: Currency;
@@ -46,22 +54,30 @@ interface DashboardDetailInfoStoreState {
     variables_schema: DashboardVariablesSchema;
     labels: string[];
     // widget info states
-    dashboardWidgetInfoList: DashboardContainerWidgetInfo[];
+    dashboardWidgetInfoList: DashboardLayoutWidgetInfo[];
     loadingWidgets: boolean;
     widgetDataMap: WidgetDataMap;
 }
+interface ValidationState {
+    isDashboardNameValid?: boolean;
+    isWidgetLayoutValid: ComputedRef<Record<string, boolean>>;
+    widgetInheritVariablesValidMap: ComputedRef<WidgetInheritVariablesValidMap>;
+}
 
 export const useDashboardDetailInfoStore = defineStore('dashboard-detail-info', () => {
-    const state = reactive<DashboardDetailInfoStoreState>({
-        loadingDashboard: false,
-        dashboardId: '',
-        projectId: '',
+    const originState = reactive<DashboardDetailInfoOriginState>({
         isProjectDashboard: computed<boolean>(() => {
             if (state.projectId) return true;
             return !!state.dashboardId?.startsWith('project');
         }),
-        dashboardViewer: computed<DashboardViewer>(() => state.dashboardInfo?.viewers ?? DASHBOARD_VIEWER.PRIVATE),
-        dashboardInfo: null,
+        dashboardViewer: computed<DashboardViewer>(() => originState.dashboardInfo?.viewers ?? DASHBOARD_VIEWER.PRIVATE),
+        dashboardInfo: null as DashboardModel|null,
+        dashboardWidgetInfoList: computed<DashboardLayoutWidgetInfo[]>(() => flattenDeep(originState.dashboardInfo?.layouts ?? [])),
+    });
+    const state = reactive<DashboardDetailInfoStoreState>({
+        loadingDashboard: false,
+        dashboardId: '',
+        projectId: '',
         dashboardName: '',
         enableCurrency: false,
         currency: CURRENCY.USD,
@@ -91,7 +107,18 @@ export const useDashboardDetailInfoStore = defineStore('dashboard-detail-info', 
         dashboardWidgetInfoList: [],
         loadingWidgets: false,
         widgetDataMap: {},
-    }) as UnwrapRef<DashboardDetailInfoStoreState>;
+    });
+    const validationState = reactive<ValidationState>({
+        isDashboardNameValid: undefined,
+        isWidgetLayoutValid: computed(() => ({})), // is all widgets valid
+        widgetInheritVariablesValidMap: computed(() => {
+            const result = {};
+            // originState.dashboardWidgetInfoList.forEach(() => {
+            //     result[d.widget_key] = isWidgetValid(d, state.variables_schema);
+            // });
+            return result;
+        }),
+    });
 
     const resetDashboardSettings = () => {
         state.dateRange = {
@@ -112,7 +139,7 @@ export const useDashboardDetailInfoStore = defineStore('dashboard-detail-info', 
     };
 
     const resetDashboardData = () => {
-        state.dashboardInfo = null;
+        originState.dashboardInfo = null;
         state.dashboardName = '';
         state.projectId = '';
         state.enableCurrency = false;
@@ -136,14 +163,13 @@ export const useDashboardDetailInfoStore = defineStore('dashboard-detail-info', 
         state.variables = {};
         state.variables_schema = { properties: {}, order: [] };
         state.labels = [];
-        state.dashboardWidgetInfoList = [];
     };
 
     const setDashboardInfo = (dashboardInfo: DashboardModel) => {
-        state.dashboardInfo = dashboardInfo;
+        originState.dashboardInfo = dashboardInfo;
+
         state.dashboardName = dashboardInfo.name;
         state.projectId = (dashboardInfo as ProjectDashboardModel).project_id ?? '';
-
         state.enableCurrency = dashboardInfo.settings?.currency?.enabled ?? false;
         state.currency = dashboardInfo.settings.currency?.value ?? CURRENCY.USD;
         state.enableDateRange = dashboardInfo.settings?.date_range?.enabled ?? false;
@@ -164,7 +190,7 @@ export const useDashboardDetailInfoStore = defineStore('dashboard-detail-info', 
         state.labels = dashboardInfo.labels;
         state.dashboardWidgetInfoList = flattenDeep(dashboardInfo?.layouts ?? []).map((info) => ({
             ...info,
-            widgetKey: uuidv4(),
+            widget_key: uuidv4(),
         }));
         state.widgetDataMap = {};
     };
@@ -176,7 +202,7 @@ export const useDashboardDetailInfoStore = defineStore('dashboard-detail-info', 
         state.loadingDashboard = true;
         try {
             let result: DashboardModel;
-            if (state.isProjectDashboard) {
+            if (originState.isProjectDashboard) {
                 result = await SpaceConnector.clientV2.dashboard.projectDashboard.get({ project_dashboard_id: state.dashboardId });
             } else {
                 result = await SpaceConnector.clientV2.dashboard.domainDashboard.get({ domain_dashboard_id: state.dashboardId });
@@ -191,27 +217,21 @@ export const useDashboardDetailInfoStore = defineStore('dashboard-detail-info', 
     };
 
     const toggleWidgetSize = (widgetKey: string) => {
-        state.dashboardWidgetInfoList = state.dashboardWidgetInfoList.map((info) => {
-            const widgetSizes = getWidgetConfig(info.widget_name)?.sizes;
-            if (info.widgetKey === widgetKey && widgetSizes) {
-                return {
-                    ...info,
-                    size: (info.size === WIDGET_SIZE.full) ? (widgetSizes[0] ?? WIDGET_SIZE.md) : WIDGET_SIZE.full,
-                };
-            }
-            return info;
-        });
-    };
-    const initiateAllWidgets = () => {
-        const widgetDataMap = {};
-        state.dashboardWidgetInfoList.forEach((widget) => {
-            widgetDataMap[widget.widgetKey] = state.widgetDataMap[widget.widgetKey];
-        });
-        state.widgetDataMap = widgetDataMap;
+        const _targetIndex = state.dashboardWidgetInfoList.findIndex((info) => info.widget_key === widgetKey);
+        if (_targetIndex > -1) {
+            const _dashboardWidgetInfoList = cloneDeep(state.dashboardWidgetInfoList);
+            const widgetInfo = _dashboardWidgetInfoList[_targetIndex];
+            const widgetSizes = getWidgetConfig(widgetInfo.widget_name)?.sizes;
+            _dashboardWidgetInfoList[_targetIndex] = {
+                ...widgetInfo,
+                size: (widgetInfo.size === WIDGET_SIZE.full) ? (widgetSizes[0] ?? WIDGET_SIZE.md) : WIDGET_SIZE.full,
+            };
+            state.dashboardWidgetInfoList = _dashboardWidgetInfoList;
+        }
     };
 
-    const updateWidgetInfo = (widgetKey: string, data: Partial<DashboardContainerWidgetInfo>) => {
-        const targetIndex = state.dashboardWidgetInfoList.findIndex((info) => info.widgetKey === widgetKey);
+    const updateWidgetInfo = (widgetKey: string, data: Partial<DashboardLayoutWidgetInfo>) => {
+        const targetIndex = state.dashboardWidgetInfoList.findIndex((info) => info.widget_key === widgetKey);
         if (targetIndex > -1) {
             const _dashboardWidgetInfoList = cloneDeep(state.dashboardWidgetInfoList);
             _dashboardWidgetInfoList[targetIndex] = {
@@ -223,7 +243,7 @@ export const useDashboardDetailInfoStore = defineStore('dashboard-detail-info', 
     };
 
     const deleteWidget = (widgetKey: string) => {
-        state.dashboardWidgetInfoList = state.dashboardWidgetInfoList.filter((info) => info.widgetKey !== widgetKey);
+        state.dashboardWidgetInfoList = state.dashboardWidgetInfoList.filter((info) => info.widget_key !== widgetKey);
     };
     const updateVariableUse = (propertyName: string, use: boolean) => {
         if (!state.variables_schema.properties[propertyName]) return;
@@ -234,11 +254,12 @@ export const useDashboardDetailInfoStore = defineStore('dashboard-detail-info', 
 
     return {
         state,
+        originState,
+        validationState,
         getDashboardInfo,
         resetDashboardSettings,
         setDashboardInfo,
         toggleWidgetSize,
-        initiateAllWidgets,
         // getter
         dashboardWidgetInfoList: computed(() => state.dashboardWidgetInfoList),
         // action
