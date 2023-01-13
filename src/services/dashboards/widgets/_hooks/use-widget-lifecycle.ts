d18a82e9c9be551a33a3a5d524c9a5aeff821a0c
yuda <yuda@mz.co.kr>
2023-01-13 19:25:29 +0900

    fix(use-widget-lifecycle): refresh only widget's inherit options changed

--- src/services/dashboards/widgets/_hooks/use-widget-lifecycle.ts
+++ src/services/dashboards/widgets/_hooks/use-widget-lifecycle.ts
@@ -2,7 +2,10 @@ import {
     onUnmounted, watch,
 } from 'vue';
 
+import { isEqual } from 'lodash';
+
 import type { WidgetProps } from '@/services/dashboards/widgets/_configs/config';
+
 
 interface UseWidgetLifecycleOptions {
     disposeWidget?: () => void;
@@ -17,9 +20,18 @@ export const useWidgetLifecycle = ({
     onUnmounted(() => {
         if (disposeWidget) disposeWidget();
     });
-    watch(() => props.dashboardVariables, () => {
-        if (props.initiated && !props.errorMode) refreshWidget();
-        // TODO: if widget has changed inherit options
+    watch(() => props.dashboardVariables, (after, before) => {
+        if (!props.initiated || props.errorMode || !props.inheritOptions) return;
+        let _refresh = false;
+        Object.values(props.inheritOptions).forEach((inheritOption) => {
+            if (_refresh) return;
+            const _variableKey = inheritOption.variable_info?.key;
+            if (!_variableKey || (!after?.[_variableKey]) && !before?.[_variableKey]) return;
+            const _afterVariable = after?.[_variableKey];
+            const _beforeVariable = before?.[_variableKey];
+            if (!isEqual(_afterVariable, _beforeVariable)) _refresh = true;
+        });
+        if (_refresh) refreshWidget();
     }, { deep: true });
     watch(() => props.dashboardVariablesSchema, () => {
         if (props.initiated && props.editMode && !props.errorMode) refreshWidget();
