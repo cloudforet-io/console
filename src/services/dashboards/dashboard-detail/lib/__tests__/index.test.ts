454369fa050d4fa348dcda61745b551ad7a31360
Minseo Lee <minseolee@mz.co.kr>
2023-01-19 11:30:26 +0900

    refactor(widget-frame): renew widget width assigner (#785)
    * refactor(widget-frame): renew widget width assigner
    
    * chore: unit test
    
    * chore: comment

--- src/services/dashboards/dashboard-detail/lib/__tests__/index.test.ts
+++ src/services/dashboards/dashboard-detail/lib/__tests__/index.test.ts
@@ -2,7 +2,7 @@ import { describe, expect } from 'vitest';
 
 import {
     WIDGET_CONTAINER_MAX_WIDTH,
-    WIDGET_CONTAINER_MIN_WIDTH,
+    WIDGET_CONTAINER_MIN_WIDTH, WIDGET_GAP,
 } from '@/services/dashboards/dashboard-detail/lib/config';
 import { widgetWidthAssigner } from '@/services/dashboards/dashboard-detail/lib/width-helper';
 import type { WidgetSize } from '@/services/dashboards/widgets/_configs/config';
@@ -47,7 +47,7 @@ function permutation(arr, selectNum) {
 
 // if AMOUNT is more than 10, call stack boom
 // for more specific test, please scale up the AMOUNT.
-const _SELECT_WIDGET_AMOUNT = 5;
+const _SELECT_WIDGET_AMOUNT = 4;
 // e.g) [['sm', 'sm', 'md'], ['sm', 'sm', 'lg'] ...]
 const widgetCases: Array<Array<WidgetSize>> = permutation(['sm', 'md', 'lg', 'xl', 'full'], _SELECT_WIDGET_AMOUNT);
 const widgetCasesLength = widgetCases.length;
@@ -64,7 +64,7 @@ describe('Assign width for permutation selected widgets', () => {
 
     it('Count of widgets should not be changed after assigning its width', () => {
         for (let caseCount = 0; caseCount < widgetCasesLength; caseCount += 1) {
-            for (let containerWidth = WIDGET_CONTAINER_MIN_WIDTH; containerWidth <= WIDGET_CONTAINER_MAX_WIDTH; containerWidth += 80) {
+            for (let containerWidth = WIDGET_CONTAINER_MIN_WIDTH; containerWidth <= WIDGET_CONTAINER_MAX_WIDTH; containerWidth += WIDGET_GAP) {
                 assignedWidgetCasesCount = 0;
                 const selectedCase = [...widgetCases[caseCount]];
 
--- src/services/dashboards/dashboard-detail/lib/config.ts
+++ src/services/dashboards/dashboard-detail/lib/config.ts
@@ -1,23 +1,11 @@
-import lodash from 'lodash';
-
-//
-// If the RANGE varies for each size, the last element must be added up to the length of the largest RANGE among sizes.
-// e.g.) Added 540, 620 in sm
-// [320, 400, 480, 560, 640]
-// [480, 560, 640, 640, 640]
-// [640, 720, 800, 800, 800]
-// [800, 880, 960, 960, 960
-
 export const WIDGET_WIDTH_RANGE_LIST = {
     sm: [320, 400, 480],
     md: [480, 560, 640],
     lg: [640, 720, 800],
     xl: [800, 880, 960],
 };
-export const WIDGET_WIDTH_RANGE_LENGTH_MAX = lodash.maxBy(
-    Object.values(WIDGET_WIDTH_RANGE_LIST).map((d) => d.length),
-) as number;
-
 
 export const WIDGET_CONTAINER_MIN_WIDTH = 320;
 export const WIDGET_CONTAINER_MAX_WIDTH = 1840;
+
+export const WIDGET_GAP = 16;
