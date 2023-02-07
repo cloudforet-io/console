09540e3c2153dde5eb76af5e9bf96c4c5d23c249
WANJIN <wanjin@megazone.com>
2023-02-07 15:53:09 +0900

    ci(workflow): add branch to release workflow (#277)
    * ci(workflow): add branch to release workflow
    
    * ci(workflow): add branch to release workflow
    
    * chore(ci): fix release.config.js

--- release.config.js
+++ release.config.js
@@ -1,15 +1,22 @@
-module.exports = {
+const config = {
     branches: ['master',
-        { name: '1.1.x', range: '1.1.x', channel: '1.1.x' },
         { name: 'beta', prerelease: true, channel: 'beta' },
-        { name: '^(console-release-)+([0-9])?(\\.+([0-9]))?(\\.+([0-9]))$', prerelease: true, channel: 'console-release' },
+        { name: '(console-release-)([0-9]+)?(.[0-9]+)?(.[0-9]+)', prerelease: true, channel: 'console-release' },
     ],
     plugins: [
         '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
         '@semantic-release/npm',
         '@semantic-release/git',
         '@semantic-release/github',
     ],
 };
+
+
+if (config.branches.some((it) => !it.prerelease)) {
+    config.plugins.push(
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+    );
+}
+
+module.exports = config;
