efd7cd76b7e4a2ad91954ac36b8cc7fc6dfe4536
Wanjin Noh <wanzargen@gmail.com>
2023-02-07 11:33:57 +0900

    ci(semantic-release): add new branch case

--- release.config.js
+++ release.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     branches: ['master',
         { name: '1.1.x', range: '1.1.x', channel: '1.1.x' },
-        { name: 'beta', prerelease: true, channel: 'beta' }],
+        { name: 'beta', prerelease: true, channel: 'beta' },
+        { name: '^(console-release-)+([0-9])?(\\.+([0-9]))?(\\.+([0-9]))$', prerelease: true, channel: 'console-release' },
+    ],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
