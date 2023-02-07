a05a8acf7bcac4840cb46319d53f7afb21479f93
Yongtae Park <samuel.park@mz.co.kr>
2023-02-07 18:25:18 +0900

    fix(dashboard-customize): change object delete property method
    Signed-off-by: Yongtae Park <samuel.park@mz.co.kr>

--- src/services/dashboards/dashboard-detail/store/dashboard-detail-info.ts
+++ src/services/dashboards/dashboard-detail/store/dashboard-detail-info.ts
@@ -1,5 +1,5 @@
 import type { ComputedRef, UnwrapRef } from 'vue';
-import {
+import Vue, {
     computed, reactive,
 } from 'vue';
 
@@ -218,7 +218,7 @@ export const useDashboardDetailInfoStore = defineStore('dashboard-detail-info', 
 
     const deleteWidget = (widgetKey: string) => {
         state.dashboardWidgetInfoList = state.dashboardWidgetInfoList.filter((info) => info.widget_key !== widgetKey);
-        delete validationState.widgetValidMap[widgetKey];
+        Vue.delete(validationState.widgetValidMap, widgetKey);
     };
     const resetVariables = () => {
         const originProperties = { ...managedDashboardVariablesSchema.properties, ...originState.dashboardInfo.variables_schema.properties };
