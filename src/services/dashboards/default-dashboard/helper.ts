2f7ad117eff65446f4f68012fc26f3f02a3f0759
yuda <yuda@mz.co.kr>
2023-02-09 15:49:24 +0900

    fix(widget-template): set default inherit options to templates
    Signed-off-by: Dahyun Yu <yuda@megazone.com>

--- src/services/dashboards/default-dashboard/helper.ts
+++ src/services/dashboards/default-dashboard/helper.ts
@@ -1,12 +1,21 @@
 import { v4 as uuidv4 } from 'uuid';
 
 import { ERROR_CASE_WIDGET_INFO } from '@/services/dashboards/default-dashboard/config';
-import type { DashboardLayoutWidgetInfo } from '@/services/dashboards/widgets/_configs/config';
+import type { DashboardLayoutWidgetInfo, InheritOptions } from '@/services/dashboards/widgets/_configs/config';
 import { getWidgetConfig } from '@/services/dashboards/widgets/_helpers/widget-helper';
 
 export const getDashboardLayoutWidgetInfoList = (widgetList: string[]): DashboardLayoutWidgetInfo[] => widgetList.map((widgetId) => {
     try {
         const widgetConfig = getWidgetConfig(widgetId);
+        const _defaultProperties = widgetConfig.options_schema?.default_properties ?? [];
+        const _requiredProperties = widgetConfig.options_schema?.schema.required ?? [];
+        const _inheritOptions: InheritOptions = {};
+        _defaultProperties.filter((d) => !_requiredProperties.includes(d)).forEach((propertyName) => {
+            _inheritOptions[propertyName] = {
+                enabled: true,
+                variable_info: { key: propertyName.replace('filters.', '') },
+            };
+        });
         const widgetInfo: DashboardLayoutWidgetInfo = {
             widget_key: uuidv4(),
             widget_name: widgetConfig.widget_config_id,
@@ -14,8 +23,8 @@ export const getDashboardLayoutWidgetInfoList = (widgetList: string[]): Dashboar
             widget_options: widgetConfig.options ?? {},
             size: widgetConfig.sizes[0],
             version: '1',
-            inherit_options: {},
-            default_schema_properties: widgetConfig.options_schema?.default_properties ?? [],
+            inherit_options: _inheritOptions,
+            default_schema_properties: _defaultProperties,
         };
         return widgetInfo;
     } catch (e) {
