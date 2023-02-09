17dd5001f4570800c5431c3f2c982c624acf7e9a
yuda <yuda@mz.co.kr>
2023-02-09 15:49:24 +0900

    fix(widget-template): set default inherit options to templates
    Signed-off-by: Dahyun Yu <yuda@megazone.com>

--- src/services/dashboards/default-dashboard/helper.ts
+++ src/services/dashboards/default-dashboard/helper.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { ERROR_CASE_WIDGET_INFO } from '@/services/dashboards/default-dashboard/config';
 import type { WidgetCustomTitleMap } from '@/services/dashboards/default-dashboard/templates/type';
-import type { DashboardLayoutWidgetInfo } from '@/services/dashboards/widgets/_configs/config';
+import type { DashboardLayoutWidgetInfo, InheritOptions } from '@/services/dashboards/widgets/_configs/config';
 import { getWidgetConfig } from '@/services/dashboards/widgets/_helpers/widget-helper';
 
 export const getDashboardLayoutWidgetInfoList = (widgetList: string[], widgetCustomTitleMap?: WidgetCustomTitleMap): DashboardLayoutWidgetInfo[] => widgetList.map((widgetId) => {
@@ -10,6 +10,15 @@ export const getDashboardLayoutWidgetInfoList = (widgetList: string[], widgetCus
         const widgetConfig = getWidgetConfig(widgetId);
         const widgetConfigTitle = widgetConfig.title ?? widgetConfig.widget_config_id;
         const title = widgetCustomTitleMap ? (widgetCustomTitleMap[widgetId] ?? widgetConfigTitle) : widgetConfigTitle;
+        const defaultProperties = widgetConfig.options_schema?.default_properties ?? [];
+        const requiredProperties = widgetConfig.options_schema?.schema.required ?? [];
+        const inheritOptions: InheritOptions = {};
+        defaultProperties.filter((d) => !requiredProperties.includes(d)).forEach((propertyName) => {
+            inheritOptions[propertyName] = {
+                enabled: true,
+                variable_info: { key: propertyName.replace('filters.', '') },
+            };
+        });
         const widgetInfo: DashboardLayoutWidgetInfo = {
             widget_key: uuidv4(),
             widget_name: widgetConfig.widget_config_id,
@@ -17,8 +26,8 @@ export const getDashboardLayoutWidgetInfoList = (widgetList: string[], widgetCus
             widget_options: widgetConfig.options ?? {},
             size: widgetConfig.sizes[0],
             version: '1',
-            inherit_options: {},
-            default_schema_properties: widgetConfig.options_schema?.default_properties ?? [],
+            inherit_options: inheritOptions,
+            default_schema_properties: defaultProperties,
         };
         return widgetInfo;
     } catch (e) {
