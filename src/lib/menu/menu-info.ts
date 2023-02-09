0cc3fdf4ab9cb735db40ec08d79e8ad6ab8aef75
Piggy Park (박용태) <samuel.park@mz.co.kr>
2023-02-09 14:07:03 +0900

    fix(site-map): apply changed site map planning (#892)
    Signed-off-by: Yongtae Park <samuel.park@mz.co.kr>

--- src/lib/menu/menu-info.ts
+++ src/lib/menu/menu-info.ts
@@ -4,7 +4,7 @@ import { MENU_ID } from '@/lib/menu/config';
 export const MENU_INFO_MAP: Record<MenuId, MenuInfo> = Object.freeze({
     [MENU_ID.HOME_DASHBOARD]: {
         translationId: 'MENU.HOME_DASHBOARD',
-        icon: 'ic_dashboard',
+        icon: 'ic_home-menu',
     },
     [MENU_ID.DASHBOARDS]: {
         translationId: 'MENU.DASHBOARDS',
