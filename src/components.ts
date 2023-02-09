111d7ad8fe86b45e595a13097a42ff79eeb7b23e
Dahyun Yu <yuda@mz.co.kr>
2023-02-09 10:18:15 +0900

    feat(grid-layout): delete component (#288)

--- src/components.ts
+++ src/components.ts
@@ -28,7 +28,6 @@ export { default as PCheckboxGroup } from './inputs/checkbox-group/PCheckboxGrou
 export { default as PFieldGroup } from './inputs/forms/field-group/PFieldGroup.vue';
 export { default as PRadio } from './inputs/radio/PRadio.vue';
 export { default as PRadioGroup } from './inputs/radio-group/PRadioGroup.vue';
-export { default as PGridLayout } from './others/deprecated/grid-layout/PGridLayout.vue';
 export { default as PPaneLayout } from './layouts/pane-layout/PPaneLayout.vue';
 export { default as PSidebar } from './layouts/sidebar/PSidebar.vue';
 export { default as PTextList } from './others/console/text-list/PTextList.vue';
