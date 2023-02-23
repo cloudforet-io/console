c7bbb8adccd6e31ee92d08069b9650239733ddf8
yuda <yuda@mz.co.kr>
2023-02-23 17:30:06 +0900

    fix(cloud-service-log): improve log tab
    - fix style bug
    - get all data at first loading (& do not refresh when page changed)
    - get different time list by page

--- src/services/asset-inventory/cloud-service/cloud-service-detail/modules/CloudServiceLogTab.vue
+++ src/services/asset-inventory/cloud-service/cloud-service-detail/modules/CloudServiceLogTab.vue
@@ -22,7 +22,7 @@
                     >
                         <p-dynamic-layout :type="layout.type"
                                           :options="layout.options"
-                                          :data="data"
+                                          :data="data.slice(pageStart - 1, pageStart + pageLimit - 1)"
                                           :type-options="{
                                               searchText,
                                               totalCount,
@@ -66,7 +66,6 @@
     </p-data-loader>
 </template>
 <script lang="ts">
-
 import {
     reactive, toRefs, computed, defineComponent, watch,
 } from 'vue';
@@ -96,15 +95,19 @@ interface Props {
     cloudServiceId: string;
     date: string;
 }
+type PeriodType = 'last1day' | 'last3days' | 'last1week' | 'last2weeks' | 'last1month' | 'last3months';
 interface PeriodItem {
-    name: string;
+    name: PeriodType;
     label: TranslateResult;
     start: Dayjs;
     end: Dayjs;
 }
 
+const HISTORY_LOG_PERIOD_LIST: PeriodType[] = ['last1day', 'last3days', 'last1week'];
+const CLOUD_SERVICE_LOG_PERIOD_LIST: PeriodType[] = ['last1week', 'last2weeks', 'last1month', 'last3months'];
+
 export default defineComponent<Props>({
-    name: 'CloudServiceHistoryLogTab',
+    name: 'CloudServiceLogTab',
     components: {
         PDynamicLayout: PDynamicLayout as any,
         PButtonTab,
@@ -123,7 +126,7 @@ export default defineComponent<Props>({
         },
         date: {
             type: String,
-            default: dayjs.utc().format(),
+            default: undefined,
         },
     },
     setup(props) {
@@ -135,27 +138,50 @@ export default defineComponent<Props>({
             pageLimit: 15,
             tabs: [] as TabItem[],
             activeTab: '',
-            timeWithinList: computed<PeriodItem[]>(() => ([
-                {
-                    name: 'last1day',
-                    label: i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.LAST_1_DAY'),
-                    start: dayjs.utc(props.date).subtract(1, 'day'),
-                    end: dayjs.utc(props.date),
-                },
-                {
-                    name: 'last2day',
-                    label: i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.LAST_3_DAYS'),
-                    start: dayjs.utc(props.date).subtract(3, 'day'),
-                    end: dayjs.utc(props.date),
-                },
-                {
-                    name: 'last1week',
-                    label: i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.LAST_A_WEEK'),
-                    start: dayjs.utc(props.date).subtract(1, 'week'),
-                    end: dayjs.utc(props.date),
-                },
-            ])),
-            selectedTimeWithin: 'last1day',
+            timeWithinList: computed<PeriodItem[]>(() => {
+                const _date = props.date ? dayjs.utc(props.date) : dayjs.utc();
+                const timeWithinList: PeriodItem[] = [
+                    {
+                        name: 'last1day',
+                        label: i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.LAST_1_DAY'),
+                        start: _date.subtract(1, 'day'),
+                        end: _date,
+                    },
+                    {
+                        name: 'last3days',
+                        label: i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.LAST_3_DAYS'),
+                        start: _date.subtract(3, 'day'),
+                        end: _date,
+                    },
+                    {
+                        name: 'last1week',
+                        label: i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.LAST_A_WEEK'),
+                        start: _date.subtract(1, 'week'),
+                        end: _date,
+                    },
+                    {
+                        name: 'last2weeks',
+                        label: i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.LAST_2_WEEKS'),
+                        start: _date.subtract(2, 'week'),
+                        end: _date,
+                    },
+                    {
+                        name: 'last1month',
+                        label: i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.LAST_1_MONTH'),
+                        start: _date.subtract(1, 'month'),
+                        end: _date,
+                    },
+                    {
+                        name: 'last3months',
+                        label: i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.LAST_3_MONTHS'),
+                        start: _date.subtract(3, 'month'),
+                        end: _date,
+                    },
+                ];
+                if (props.date) return timeWithinList.filter((d) => HISTORY_LOG_PERIOD_LIST.includes(d.name));
+                return timeWithinList.filter((d) => CLOUD_SERVICE_LOG_PERIOD_LIST.includes(d.name));
+            }),
+            selectedTimeWithin: props.date ? 'last1day' : 'last1week',
             layouts: [] as DynamicLayout[],
             currentLayout: computed(() => state.layouts.find((layout) => layout.name === state.activeTab)),
             dataSourceIds: {} as { [key: string]: string },
@@ -177,7 +203,7 @@ export default defineComponent<Props>({
                     end: selectedTimeWithin?.end.toISOString(),
                 });
                 state.totalCount = results.length;
-                state.data = results.slice(state.pageStart - 1, state.pageStart + state.pageLimit - 1);
+                state.data = results;
             } catch (e) {
                 ErrorHandler.handleError(e);
                 state.data = [];
@@ -215,14 +241,13 @@ export default defineComponent<Props>({
         // handler
         const dynamicLayoutListeners: Partial<DynamicLayoutEventListener> = {
             fetch(options) {
-                if (options?.pageStart) {
-                    state.pageStart = options.pageStart;
+                if (options?.pageStart) state.pageStart = options.pageStart;
+                if (options?.pageLimit) state.pageLimit = options.pageLimit;
+                if (!state.searchText && !options?.searchText) return;
+                if (state.searchText !== options?.searchText) {
+                    state.searchText = options?.searchText ?? '';
+                    getLogData();
                 }
-                if (options?.pageLimit) {
-                    state.pageLimit = options.pageLimit;
-                }
-                state.searchText = options?.searchText ?? '';
-                getLogData();
             },
             async export() {
                 const fields = state.currentLayout?.options?.fields;
@@ -239,7 +264,7 @@ export default defineComponent<Props>({
                         query: {},
                     },
                     fields: dynamicFieldsToExcelDataFields(fields),
-                    file_name_prefix: FILE_NAME_PREFIX.cloudServiceHistoryLog,
+                    file_name_prefix: FILE_NAME_PREFIX.cloudServiceLog,
                 });
             },
         };
@@ -270,7 +295,7 @@ export default defineComponent<Props>({
     max-width: calc(100vw - 3rem);
 }
 .data-loader {
-    height: 26.125rem;
+    min-height: 26.125rem;
 }
 
 /* custom design-system component - p-dynamic-layout */
