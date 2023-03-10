96f70609aa920672ebe61e4821ff3e54cd979ff1
WANJIN <wanjin@megazone.com>
2023-03-10 11:20:34 +0900

    test: apply automated testing framework (#937)
    * test: init playwright
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    * ci: update playwright workflow
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    * docs: update readme.md
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    * test: update playwright.config.ts
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    * test: update playwright.config.ts
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    ---------
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

---
+++ playwright.config.ts
@@ -0,0 +1,99 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const env = process.env.CI ? 'ci' : 'local';
+dotenv.config({ path: path.resolve(path.dirname(__filename), `playwright/${env}.env`) });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+    testDir: './tests',
+    /* Maximum time one test can run for. */
+    timeout: 30 * 1000,
+    expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+        timeout: 5000,
+    },
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 0,
+    /* Opt out of parallel tests on CI. */
+    workers: process.env.CI ? 1 : undefined,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: 'html',
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        bypassCSP: true,
+        /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+        actionTimeout: 0,
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        baseURL: process.env.BASEURL ?? 'http://localhost:8080',
+
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: 'on-first-retry',
+        video: 'on-first-retry',
+    },
+
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: 'chromium',
+            use: {
+                ...devices['Desktop Chrome'],
+                launchOptions: {
+                    args: ['--disable-site-isolation-trials', '--disable-web-security'],
+                },
+                // ignoreHTTPSErrors: true,
+            },
+        },
+        // {
+        //     name: 'firefox',
+        //     use: { ...devices['Desktop Firefox'] },
+        // },
+        //
+        // {
+        //     name: 'webkit',
+        //     use: { ...devices['Desktop Safari'] },
+        // },
+
+        /* Test against mobile viewports. */
+        // {
+        //   name: 'Mobile Chrome',
+        //   use: { ...devices['Pixel 5'] },
+        // },
+        // {
+        //   name: 'Mobile Safari',
+        //   use: { ...devices['iPhone 12'] },
+        // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { channel: 'chrome' },
+    // },
+    ],
+
+    /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+    // outputDir: 'test-results/',
+
+    /* Run your local dev server before starting the tests */
+    webServer: process.env.BASEURL ? undefined : {
+        command: 'npm run build:packages && npm run serve',
+        port: 8080,
+    },
+});
