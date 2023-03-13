5aa013036d7c471da435dbae280588d856a45fe8
yuda <yuda@megazone.com>
2023-03-13 14:45:57 +0900

    feat(user): delete vuex page store of userPage
    Signed-off-by: yuda <yuda@megazone.com>

--- src/services/administration/store/user/getters.ts
+++
@@ -1,8 +0,0 @@
-import type { Getter } from 'vuex';
-
-import type { UserStoreState } from './type';
-
-export const isManagementModalVisible: Getter<UserStoreState, any> = ({ visibleManagementModal }): boolean => visibleManagementModal;
-export const isCreateModalVisible: Getter<UserStoreState, any> = ({ visibleCreateModal }): boolean => visibleCreateModal;
-export const isUpdateModalVisible: Getter<UserStoreState, any> = ({ visibleUpdateModal }): boolean => visibleUpdateModal;
-export const isUserSelected: Getter<UserStoreState, any> = ({ selectedIndex }): boolean => selectedIndex.length > 0;
