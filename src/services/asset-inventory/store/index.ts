09644de518b4e2ce88b3954208d79cba85024a4b
yuda <yuda@megazone.com>
2023-03-14 16:14:31 +0900

    feat(cloud-service): delete vuex page store
    Signed-off-by: yuda <yuda@megazone.com>

--- src/services/asset-inventory/store/index.ts
+++ src/services/asset-inventory/store/index.ts
@@ -1,7 +1,6 @@
 import type { Module } from 'vuex';
 
 import * as actions from './actions';
-import cloudService from './cloud-service';
 import cloudServiceDetail from './cloud-service-detail';
 import * as getters from './getters';
 import * as mutations from './mutations';
@@ -14,7 +13,6 @@ export const assetInventoryStoreModule: Module<AssetInventoryState, any> = {
     namespaced: true,
     state: () => ({ ...state }),
     modules: {
-        cloudService,
         cloudServiceDetail,
     },
     getters,
