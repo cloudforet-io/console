dad67177a8ca17c1eef99154e3d10107c8959d21
yuda <yuda@megazone.com>
2023-03-14 17:19:50 +0900

    feat(cloud-service-detail): delete vuex page store
    Signed-off-by: yuda <yuda@megazone.com>

--- src/services/asset-inventory/store/cloud-service-detail/type.ts
+++
@@ -1,9 +0,0 @@
-import type { CloudServiceTypeInfo } from '@/services/asset-inventory/cloud-service/cloud-service-detail/type';
-
-export interface CloudServiceDetailStoreState {
-    provider: string;
-    group: string;
-    name?: string;
-    cloudServiceTypeList: CloudServiceTypeInfo[];
-    selectedItem?: CloudServiceTypeInfo;
-}
