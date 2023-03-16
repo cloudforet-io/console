7e99827d1aa4792a5e8bf17667f522f465860b63
sulmo <sulmo@megazone.com>
2023-03-16 17:29:23 +0900

    feat(project-detail): create pinia store
    Signed-off-by: sulmo <sulmo@megazone.com>

---
+++ src/services/project/project-detail/store/project-detail-page-store.ts
@@ -0,0 +1,55 @@
+import { reactive } from 'vue';
+
+import { defineStore } from 'pinia';
+
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+import { ApiQueryHelper } from '@cloudforet/core-lib/space-connector/helper';
+
+import type { MaintenanceHappening, ProjectDetailState } from '@/services/project/project-detail/store/type';
+
+export const useProjectDetailPageStore = defineStore('project-detail-page', () => {
+    const state = reactive<ProjectDetailState>({
+        projectId: '',
+        alertCounts: [],
+        maintenanceHappenings: [],
+    });
+
+    const getAlertCounts = async (): Promise<void> => {
+        try {
+            const { results } = await SpaceConnector.client.monitoring.dashboard.alertCountByState({
+                project_id: state.projectId,
+            });
+            state.alertCounts = results;
+        } catch (e) {
+            state.alertCounts = [];
+            console.error(e);
+        }
+    };
+
+    const loadMaintenanceHappenings = async (): Promise<void> => {
+        const queryHelper = new ApiQueryHelper().setFilters([{
+            k: 'state', v: 'OPEN', o: '=',
+        }]);
+        try {
+            const { results } = await SpaceConnector.client.monitoring.maintenanceWindow.list({
+                project_id: state.projectId,
+                query: queryHelper.data,
+            });
+            const convertedResults: MaintenanceHappening[] = results.map((d) => ({
+                title: d.title,
+                startTime: d.start_time,
+                endTime: d.end_time,
+            }));
+            state.maintenanceHappenings = convertedResults;
+        } catch (e) {
+            state.maintenanceHappenings = [];
+            console.error(e);
+        }
+    };
+
+    return {
+        state,
+        getAlertCounts,
+        loadMaintenanceHappenings,
+    };
+});
