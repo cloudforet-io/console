91a8dc5f53e2f74cbad505bcbf7ee2ed97b5391f
yuda <yuda@megazone.com>
2023-03-17 13:22:01 +0900

    feat(alert): delete vuex page store
    Signed-off-by: yuda <yuda@megazone.com>

--- src/services/alert-manager/store/alert/getters.ts
+++
@@ -1,5 +0,0 @@
-import type { Getter } from 'vuex';
-
-import type { AlertState } from '@/services/alert-manager/store/alert/type';
-
-export const alertId: Getter<AlertState, any> = (state): string|undefined => state.alertData?.alert_id;
