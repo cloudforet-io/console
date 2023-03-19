7180563011faa39fa0ea7daee8e97bdda25e7251
Wanjin Noh <wanjin@megazone.com>
2023-03-19 21:39:19 +0900

    feat: update provider parameter in cloudServiceLogTab (#959)

--- src/services/asset-inventory/cloud-service/cloud-service-detail/modules/CloudServiceLogTab.vue
+++ src/services/asset-inventory/cloud-service/cloud-service-detail/modules/CloudServiceLogTab.vue
@@ -1,69 +1,69 @@
 <template>
-    <p-data-loader :loading="loading"
-                   :class="{ 'data-loader': loading }"
+    <p-data-loader :loading="schemaLoading"
+                   :data="dataSourceList"
+                   :class="{ 'data-loader': schemaLoading }"
                    class="cloud-service-history-log-tab"
     >
-        <div v-if="tabs.length">
-            <p-panel-top :title="$t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG')"
-                         use-total-count
-                         :total-count="totalCount"
-            />
-            <p-button-tab v-if="tabs.length > 0"
-                          :tabs="tabs"
-                          :active-tab="activeTab"
-                          keep-alive-all
-                          @change="handleChangeTab"
-            >
-                <template v-for="(layout, i) in layouts"
-                          #[layout.name]
-                >
-                    <div :key="`${layout.name}-${i}`"
-                         class="log-dynamic-layout"
-                    >
-                        <p-dynamic-layout :type="layout.type"
-                                          :options="layout.options"
-                                          :data="data.slice(pageStart - 1, pageStart + pageLimit - 1)"
-                                          :type-options="{
-                                              totalCount,
-                                              sortable: false,
-                                          }"
-                                          :fetch-options="{
-                                              pageLimit,
-                                              pageStart,
-                                              searchText,
-                                          }"
-                                          v-on="dynamicLayoutListeners"
-                        >
-                            <template #toolbox-top>
-                                <div class="filter">
-                                    <span class="filter-label">{{ $t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.TIME_WITHIN') }}</span>
-                                    <p-select-status
-                                        v-for="(item, index) in timeWithinList"
-                                        :key="`${item.name}-${index}`"
-                                        v-model="selectedTimeWithin"
-                                        :value="item.name"
-                                    >
-                                        {{ item.label }}
-                                    </p-select-status>
-                                </div>
-                            </template>
-                        </p-dynamic-layout>
-                    </div>
-                </template>
-            </p-button-tab>
-        </div>
-        <div v-else
-             class="empty-tab"
+        <p-panel-top :title="$t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG')"
+                     use-total-count
+                     :total-count="totalCount"
+        />
+        <p-button-tab v-if="tabs.length > 0"
+                      :tabs="tabs"
+                      :active-tab="activeTab"
+                      keep-alive-all
+                      @change="handleChangeTab"
         >
-            <div class="container">
-                <img class="image"
-                     src="@/assets/images/illust_microscope.svg"
+            <template v-for="(layout, i) in layouts"
+                      #[layout.name]
+            >
+                <div :key="`${layout.name}-${i}`"
+                     class="log-dynamic-layout"
                 >
-                <p class="desc">
-                    {{ $t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.NO_LOG_HELP_TEXT') }}
-                </p>
+                    <p-dynamic-layout :type="layout.type"
+                                      :options="layout.options"
+                                      :data="data.slice(pageStart - 1, pageStart + pageLimit - 1)"
+                                      :type-options="{
+                                          totalCount,
+                                          sortable: false,
+                                          loading,
+                                      }"
+                                      :fetch-options="{
+                                          pageLimit,
+                                          pageStart,
+                                          searchText,
+                                      }"
+                                      v-on="dynamicLayoutListeners"
+                    >
+                        <template #toolbox-top>
+                            <div class="filter">
+                                <span class="filter-label">{{ $t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.TIME_WITHIN') }}</span>
+                                <p-select-status
+                                    v-for="(item, index) in timeWithinList"
+                                    :key="`${item.name}-${index}`"
+                                    v-model="selectedTimeWithin"
+                                    :value="item.name"
+                                >
+                                    {{ item.label }}
+                                </p-select-status>
+                            </div>
+                        </template>
+                    </p-dynamic-layout>
+                </div>
+            </template>
+        </p-button-tab>
+        <template #no-data>
+            <div class="empty-tab">
+                <div class="container">
+                    <img class="image"
+                         src="@/assets/images/illust_microscope.svg"
+                    >
+                    <p class="desc">
+                        {{ $t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.LOG_TAB.NO_LOG_HELP_TEXT') }}
+                    </p>
+                </div>
             </div>
-        </div>
+        </template>
     </p-data-loader>
 </template>
 <script lang="ts">
@@ -78,11 +78,14 @@ import {
 import type { DynamicLayoutEventListener } from '@spaceone/design-system/types/data-display/dynamic/dynamic-layout/type';
 import type { DynamicLayout } from '@spaceone/design-system/types/data-display/dynamic/dynamic-layout/type/layout-schema';
 import type { TabItem } from '@spaceone/design-system/types/navigation/tabs/tab/type';
+import type { CancelTokenSource } from 'axios';
+import axios from 'axios';
 import dayjs from 'dayjs';
 import type { Dayjs } from 'dayjs';
-import { isEmpty } from 'lodash';
+import { debounce, isEmpty } from 'lodash';
 
 import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+import { ApiQueryHelper } from '@cloudforet/core-lib/space-connector/helper';
 
 import { store } from '@/store';
 import { i18n } from '@/translations';
@@ -107,6 +110,28 @@ interface PeriodItem {
 
 const HISTORY_LOG_PERIOD_LIST: PeriodType[] = ['last1day', 'last3days', 'last1week'];
 const CLOUD_SERVICE_LOG_PERIOD_LIST: PeriodType[] = ['last1week', 'last2weeks', 'last1month', 'last3months'];
+
+interface DataSourceInfo {
+    data_source_id: string;
+    monitoring_type: string;
+    name: string;
+    provider: string;
+    state: string;
+    tags: { [key: string]: string };
+    plugin_info?: {
+        plugin_id: string;
+        provider: string;
+        metadata: {
+            required_keys: string[];
+            supported_providers: string[];
+            view: {
+                table: {
+                    layout: DynamicLayout;
+                }
+            }
+        }
+    }
+}
 
 export default defineComponent<Props>({
     name: 'CloudServiceLogTab',
@@ -133,13 +158,13 @@ export default defineComponent<Props>({
     },
     setup(props) {
         const state = reactive({
+            schemaLoading: true,
             loading: true,
             searchText: '',
-            totalCount: 0,
             pageStart: 1,
             pageLimit: 15,
-            tabs: [] as TabItem[],
             activeTab: '',
+            // time within filter
             timeWithinList: computed<PeriodItem[]>(() => {
                 const _date = props.date ? dayjs.utc(props.date) : dayjs.utc();
                 const timeWithinList: PeriodItem[] = [
@@ -184,87 +209,118 @@ export default defineComponent<Props>({
                 return timeWithinList.filter((d) => CLOUD_SERVICE_LOG_PERIOD_LIST.includes(d.name));
             }),
             selectedTimeWithin: props.date ? HISTORY_LOG_PERIOD_LIST[0] : CLOUD_SERVICE_LOG_PERIOD_LIST[0],
-            layouts: [] as DynamicLayout[],
+            selectedPeriod: computed<PeriodItem|undefined>(() => state.timeWithinList.find((d) => d.name === state.selectedTimeWithin)),
+            // log data
+            data: [] as any[],
+            totalCount: computed(() => state.data.length),
+            // dataSources and computed data by dataSources(schema, tabs)
+            dataSourceList: [] as DataSourceInfo[],
+            layouts: computed<DynamicLayout[]>(() => state.dataSourceList.map((dataSource) => {
+                const layout = dataSource.plugin_info?.metadata?.view?.table?.layout ?? {};
+                return {
+                    ...layout,
+                    name: dataSource.data_source_id,
+                };
+            })),
             currentLayout: computed(() => state.layouts.find((layout) => layout.name === state.activeTab)),
-            dataSourceIds: {} as { [key: string]: string },
-            totalLayoutCount: 0,
-            data: [],
+            tabs: computed<TabItem[]>(() => state.dataSourceList.map((dataSource) => ({
+                name: dataSource.data_source_id,
+                label: dataSource.name,
+            }))),
         });
-        const handleChangeTab = (tab) => {
+
+
+        const handleChangeTab = (tab: string) => {
             state.activeTab = tab;
         };
-        const getLogData = async () => {
+        let getLogDataToken: CancelTokenSource | undefined;
+        const getLogData = debounce(async (dataSourceId: string, resourceId: string) => {
             try {
+                if (getLogDataToken) {
+                    getLogDataToken.cancel('Next request has been called.');
+                    getLogDataToken = undefined;
+                }
+                getLogDataToken = axios.CancelToken.source();
+
                 state.loading = true;
-                const selectedTimeWithin = state.timeWithinList.find((timeWith) => timeWith.name === state.selectedTimeWithin);
                 const { results } = await SpaceConnector.client.monitoring.log.list({
-                    data_source_id: state.dataSourceIds[state.activeTab],
-                    resource_id: props.cloudServiceId,
+                    data_source_id: dataSourceId,
+                    resource_id: resourceId,
                     keyword: state.searchText,
-                    start: selectedTimeWithin?.start.toISOString(),
-                    end: selectedTimeWithin?.end.toISOString(),
+                    start: state.selectedPeriod?.start.toISOString(),
+                    end: state.selectedPeriod?.end.toISOString(),
+                }, {
+                    cancelToken: getLogDataToken.token,
                 });
-                state.totalCount = results.length;
+                getLogDataToken = undefined;
                 state.data = results;
-            } catch (e) {
-                ErrorHandler.handleError(e);
+            } catch (e: any) {
+                if (!axios.isCancel(e.axiosError)) {
+                    ErrorHandler.handleError(e);
+                }
                 state.data = [];
             } finally {
                 state.loading = false;
             }
-        };
-        const getSchema = async () => {
+        }, 300);
+        const schemaApiQueryHelper = new ApiQueryHelper();
+        const fetchDataSources = async (provider: string): Promise<DataSourceInfo[]> => {
             try {
-                const { results, total_count } = await SpaceConnector.client.monitoring.dataSource.list({
+                const { results } = await SpaceConnector.client.monitoring.dataSource.list({
                     monitoring_type: 'LOG',
-                    provider: props.provider,
+                    query: schemaApiQueryHelper.setFilters([
+                        { k: 'plugin_info.metadata.supported_providers', v: [provider, 'all'], o: '=' }, // filtering by provider labels.
+                    ]).data,
                 });
-                state.totalLayoutCount = total_count;
-                if (!total_count) return;
-                const layouts:DynamicLayout[] = [];
-                const dataSourceIds = {};
-                state.tabs = (results ?? []).map((dataSource) => {
-                    const layout = dataSource.plugin_info?.metadata?.view?.table?.layout;
-                    layouts.push(layout);
-                    dataSourceIds[layout?.name] = dataSource.data_source_id;
-                    return { name: layout?.name, label: dataSource.name };
-                });
-                state.activeTab = state.tabs[0]?.name;
-                state.layouts = layouts;
-                state.dataSourceIds = dataSourceIds;
+                return results ?? [];
             } catch (e) {
                 ErrorHandler.handleError(e);
-                state.layouts = [];
-                state.dataSourceIds = {};
-                state.totalLayoutCount = 0;
+                return [];
+            }
+        };
+        const dataSourcesCacheMap = new Map<string, DataSourceInfo[]>(); // provider: DataSourceInfo[]
+        const getSchema = async (provider: string) => {
+            try {
+                state.schemaLoading = true;
+                // check cache map and set data source list to cache map
+                if (!dataSourcesCacheMap.has(provider)) {
+                    const dataSourceList = await fetchDataSources(provider);
+                    dataSourcesCacheMap.set(provider, dataSourceList);
+                }
+                state.dataSourceList = dataSourcesCacheMap.get(provider) ?? [];
+            } catch (e) {
+                ErrorHandler.handleError(e);
+                state.dataSourceList = [];
+            } finally {
+                state.schemaLoading = false;
             }
         };
 
         // handler
         const dynamicLayoutListeners: Partial<DynamicLayoutEventListener> = {
             fetch(options) {
+                if (!state.activeTab || !props.cloudServiceId) return;
                 if (isEmpty(options)) { // refresh case
-                    getLogData();
+                    getLogData(state.activeTab, props.cloudServiceId);
                     return;
                 }
                 if (options?.pageStart) state.pageStart = options.pageStart;
                 if (options?.pageLimit) state.pageLimit = options.pageLimit;
                 if (options?.searchText !== undefined && state.searchText !== options?.searchText) {
                     state.searchText = options?.searchText ?? '';
-                    getLogData();
+                    getLogData(state.activeTab, props.cloudServiceId);
                 }
             },
             async export() {
                 const fields = state.currentLayout?.options?.fields;
                 if (!fields) return;
-                const selectedTimeWithin = state.timeWithinList.find((timeWith) => timeWith.name === state.selectedTimeWithin);
                 await store.dispatch('file/downloadExcel', {
                     url: '/monitoring/log/list',
                     param: {
-                        data_source_id: state.dataSourceIds[state.activeTab],
+                        data_source_id: state.activeTab,
                         resource_id: props.cloudServiceId,
-                        start: selectedTimeWithin?.start.toISOString(),
-                        end: selectedTimeWithin?.end.toISOString(),
+                        start: state.selectedPeriod?.start.toISOString(),
+                        end: state.selectedPeriod?.end.toISOString(),
                         keyword: state.searchText,
                         query: {},
                     },
@@ -273,16 +329,17 @@ export default defineComponent<Props>({
                 });
             },
         };
-        // init
-        (async () => {
-            await getSchema();
-            if (state.totalLayoutCount) await getLogData();
-            state.loading = false;
-        })();
 
         // watcher
-        watch([() => state.selectedTimeWithin, () => props.date, () => props.cloudServiceId], () => {
-            if (state.totalLayoutCount) getLogData();
+        watch(() => props.provider, async (provider) => {
+            if (!provider) return;
+            await getSchema(provider);
+            state.activeTab = state.tabs[0]?.name ?? '';
+        }, { immediate: true });
+        watch([() => state.selectedPeriod, () => props.cloudServiceId, () => state.activeTab], (
+            [, cloudServiceId, activeTab],
+        ) => {
+            if (cloudServiceId && activeTab) getLogData(activeTab, cloudServiceId);
         });
         return {
             ...toRefs(state),
