defb22416e64a95375d0ea9c7765cef0611afe06
Songyi Kim <sykim@megazone.com>
2023-03-22 16:12:47 +0900

    fix(dashboard-create): show dashboard templates depending on scope
    Signed-off-by: Songyi Kim <sykim@megazone.com>

--- apps/web/src/services/dashboards/dashboard-create/DashboardCreatePage.vue
+++ apps/web/src/services/dashboards/dashboard-create/DashboardCreatePage.vue
@@ -1,39 +1,76 @@
 <template>
     <div class="dashboard-create-page">
-        <p-heading
-            show-back-button
-            :title="$t('DASHBOARDS.CREATE.TITLE')"
-            @click-back-button="$router.go(-1)"
-        />
-        <section class="dashboard-create-form-container">
+        <div v-if="currentStep === steps[0].step"
+             class="dashboard-create-step1"
+        >
+            <dashboard-create-header
+                :description="steps[0].description"
+                :total-steps="steps.length"
+                :current-step="currentStep"
+            />
             <dashboard-scope-form :dashboard-scope.sync="dashboardScope"
                                   @set-project="setForm('dashboardProject', $event)"
             />
-            <dashboard-template-form @set-template="setForm('dashboardTemplate', $event)" />
             <dashboard-viewer-form :dashboard-viewer-type.sync="dashboardViewerType" />
-        </section>
-        <div class="dashboard-create-buttons">
-            <p-button style-type="tertiary"
-                      size="lg"
-                      @click="$router.go(-1)"
-            >
-                {{ $t('DASHBOARDS.CREATE.CANCEL') }}
-            </p-button>
-            <p-button style-type="primary"
-                      size="lg"
-                      :disabled="!isAllValid"
-                      @click="handleClickCreate"
-            >
-                {{ $t('DASHBOARDS.CREATE.CREATE') }}
-            </p-button>
+            <div class="button-area">
+                <p-button
+                    style-type="transparent"
+                    size="lg"
+                    @click="$router.go(-1)"
+                >
+                    Cancel
+                </p-button>
+                <p-button
+                    style-type="primary"
+                    size="lg"
+                    :loading="false"
+                    :disabled="!valid"
+                    @click="handleGoStep(2)"
+                >
+                    Continue
+                </p-button>
+            </div>
+        </div>
+        <div v-if="currentStep === steps[1].step"
+             class="dashboard-create-step2 max-w-lg2"
+        >
+            <dashboard-create-header
+                :description="steps[1].description"
+                :total-steps="steps.length"
+                :current-step="currentStep"
+            />
+            <dashboard-template-form
+                :dashboard-scope="dashboardScope"
+                @set-template="setForm('dashboardTemplate', $event)"
+            />
+            <div class="button-area">
+                <p-button
+                    style-type="transparent"
+                    size="lg"
+                    icon-left="ic_arrow-left"
+                    @click="handleGoStep(1)"
+                >
+                    Go to Back
+                </p-button>
+                <p-button
+                    style-type="primary"
+                    size="lg"
+                    :disabled="!isAllValid"
+                    @click="handleClickCreate"
+                >
+                    Create New Dashboard
+                </p-button>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { reactive, toRefs } from 'vue';
+import { computed, reactive, toRefs } from 'vue';
 
-import { PHeading, PButton } from '@spaceone/design-system';
+import {
+    PButton,
+} from '@spaceone/design-system';
 
 import { SpaceRouter } from '@/router';
 import { i18n } from '@/translations';
@@ -45,6 +82,7 @@ import {
     DASHBOARD_VIEWER,
 } from '@/services/dashboards/config';
 import type { DashboardScope, DashboardViewer } from '@/services/dashboards/config';
+import DashboardCreateHeader from '@/services/dashboards/dashboard-create/modules/DashboardCreateHeader.vue';
 import DashboardScopeForm from '@/services/dashboards/dashboard-create/modules/DashboardScopeForm.vue';
 import DashboardTemplateForm from '@/services/dashboards/dashboard-create/modules/DashboardTemplateForm.vue';
 import DashboardViewerForm from '@/services/dashboards/dashboard-create/modules/DashboardViewerForm.vue';
@@ -57,19 +95,16 @@ import type { ProjectItemResp } from '@/services/project/type';
 export default {
     name: 'CreateDashboardPage',
     components: {
+        DashboardCreateHeader,
         DashboardViewerForm,
         DashboardTemplateForm,
         DashboardScopeForm,
-        PHeading,
         PButton,
     },
     setup() {
         const dashboardDetailStore = useDashboardDetailInfoStore();
         const {
-            forms: {
-                dashboardTemplate,
-                dashboardProject,
-            },
+            forms: { dashboardTemplate, dashboardProject },
             setForm,
             isAllValid,
         } = useFormValidator({
@@ -88,8 +123,22 @@ export default {
         const state = reactive({
             dashboardScope: DASHBOARD_SCOPE.DOMAIN as DashboardScope,
             dashboardViewerType: DASHBOARD_VIEWER.PUBLIC as DashboardViewer,
+            steps: [
+                { step: 1, name: 'choose scope and viewer', description: 'Choose dashboard scope and viewer.' },
+                { step: 2, name: 'choose template', description: 'Choose a template or an existing dashboard to get started quickly.' },
+            ],
+            currentStep: 1,
+            valid: computed(() => {
+                if (state.dashboardScope === DASHBOARD_SCOPE.PROJECT) return !!dashboardProject.value?.id;
+                if (state.dashboardScope === DASHBOARD_SCOPE.DOMAIN) return true;
+                return false;
+            }),
         });
 
+
+        const handleGoStep = (step) => {
+            state.currentStep = step;
+        };
         const handleClickCreate = () => {
             let _dashboardTemplate;
             if (state.dashboardScope === DASHBOARD_SCOPE.PROJECT) {
@@ -125,22 +174,42 @@ export default {
             setForm,
             isAllValid,
             handleClickCreate,
+            handleGoStep,
         };
     },
 };
 </script>
 
 <style lang="postcss" scoped>
-.dashboard-create-form-container {
-    display: grid;
-    grid-gap: 1rem;
+.button-area {
+    @apply flex justify-end;
+    margin-top: 2rem;
+    gap: 1rem;
+
+    @screen tablet {
+        @apply flex-col;
+    }
 }
-.dashboard-create-buttons {
-    display: inline-flex;
-    float: right;
-    margin-top: 1rem;
-    & .p-button {
-        margin-left: 1rem;
+.dashboard-create-step1 {
+    @apply w-full;
+    width: 30rem;
+    padding: 2.5rem;
+    margin: 0 auto;
+
+    @screen tablet {
+        @apply w-full;
+    }
+}
+.dashboard-create-step2 {
+    width: 62.5rem;
+    margin: 0 auto;
+    padding: 0.25rem 1.5rem 4.0625rem;
+
+    @screen tablet {
+        @apply w-full;
+    }
+    .dashboard-create-header {
+        @apply mb-8;
     }
 }
 </style>
