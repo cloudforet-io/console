b2420ffd5ccdd93f3bf9712a2c1db7743661f8ce
Songyi Kim <sykim@megazone.com>
2023-03-22 16:12:47 +0900

    fix(dashboard-create): show dashboard templates depending on scope
    Signed-off-by: Songyi Kim <sykim@megazone.com>

---
+++ src/services/dashboards/dashboard-create/modules/DashboardCreateHeader.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="dashboard-create-header">
+        <p class="step">
+            Step {{ props.currentStep }}/{{ props.totalSteps }}
+        </p>
+        <h2 class="heading">
+            Create New Dashboard
+        </h2>
+        <p class="description">
+            {{ props.description }}
+        </p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+    description: string;
+    totalSteps: number;
+    currentStep: number;
+}
+
+const props = defineProps<Props>();
+
+</script>
+
+<style scoped>
+.dashboard-create-header {
+    .step {
+        @apply mb-1 text-label-sm text-gray-500;
+    }
+    .heading {
+        @apply mb-1 text-display-md text-gray-900 font-black;
+    }
+    .description {
+        @apply mt-1 text-label-md text-gray-700;
+    }
+}
+</style>
