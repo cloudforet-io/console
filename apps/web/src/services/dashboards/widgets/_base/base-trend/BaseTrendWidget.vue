eee1a5c03974894f97aded9aa0fca7d918ddee44
Dahyun Yu <yuda@megazone.com>
2023-03-31 13:44:33 +0900

    fix(dashboard-widget): watch dateRange (#1017)
    * fix(dashboard-widget): watch dateRange
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * fix(dashboard-widget): update param of `useWidgetLifecycle`
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/dashboards/widgets/_base/base-trend/BaseTrendWidget.vue
+++ apps/web/src/services/dashboards/widgets/_base/base-trend/BaseTrendWidget.vue
@@ -267,7 +267,7 @@ useWidgetLifecycle({
     disposeWidget: chartHelper.disposeRoot,
     refreshWidget,
     props,
-    settings: toRef(state, 'settings'),
+    state,
     onCurrencyUpdate: async () => {
         if (!state.data) return;
         state.legends = getXYChartLegends(state.data.results, state.groupBy, props.allReferenceTypeInfo, state.disableReferenceColor);
--- apps/web/src/services/dashboards/widgets/budget-usage-by-target/BudgetUsageByTarget.vue
+++ apps/web/src/services/dashboards/widgets/budget-usage-by-target/BudgetUsageByTarget.vue
@@ -195,6 +195,7 @@ useWidgetLifecycle({
     disposeWidget: undefined,
     refreshWidget,
     props,
+    state,
 });
 
 defineExpose<WidgetExpose<FullData>>({
