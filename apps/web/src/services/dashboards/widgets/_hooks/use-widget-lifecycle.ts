14f88fb3518cf6818fbe89489606d470f4be7f3b
Dahyun Yu <yuda@megazone.com>
2023-03-31 11:13:18 +0900

    fix(dashboard-variables): set inherit options to widget when variable is added (#1014)
    * fix(dashboard-variables): update immediately when (un)check variables
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * fix: set inherit options to widget when dashboard variable is added
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * fix(dashboard-variable): update params
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/dashboards/widgets/_hooks/use-widget-lifecycle.ts
+++ apps/web/src/services/dashboards/widgets/_hooks/use-widget-lifecycle.ts
@@ -20,20 +20,51 @@ interface UseWidgetLifecycleOptions {
     onCurrencyUpdate?: (current?: Currency, previous?: Currency) => void|Promise<void>;
 }
 
-const checkRefreshable = (
+
+const dashboardDetailStore = useDashboardDetailInfoStore();
+
+const checkRefreshableByDashboardVariables = (
     inheritOptions: InheritOptions,
     after?: DashboardVariables|DashboardVariablesSchema,
     before?: DashboardVariables|DashboardVariablesSchema,
-    isSchema = false,
 ): boolean => {
     let _refresh = false;
     Object.values(inheritOptions).forEach((inheritOption) => {
         if (_refresh) return;
         const _variableKey = inheritOption.variable_info?.key ?? '';
-        const _after = isSchema ? after?.properties?.[_variableKey] : after?.[_variableKey];
-        const _before = isSchema ? before?.properties?.[_variableKey] : before?.[_variableKey];
+        const _after = after?.[_variableKey];
+        const _before = before?.[_variableKey];
         if (!_variableKey || (!_after && !_before)) return;
         if (!isEqual(_after, _before)) _refresh = true;
+    });
+    return _refresh;
+};
+const checkRefreshableByDashboardVariableSchema = (
+    inheritOptions: InheritOptions,
+    optionsSchemaProperty: string[],
+    widgetKey: string,
+    after?: DashboardVariables|DashboardVariablesSchema,
+    before?: DashboardVariables|DashboardVariablesSchema,
+): boolean => {
+    let _refresh = false;
+    Object.entries(inheritOptions).forEach(([optionName]) => {
+        if (_refresh) return;
+        const _variableKey = optionName.replace('filters.', '');
+        const _after = after?.properties?.[_variableKey];
+        const _before = before?.properties?.[_variableKey];
+
+        if (!_variableKey || (!_after && !_before)) return;
+        if (!isEqual(_after, _before)) _refresh = true;
+
+        // set inherit options to widget when new dashboard variable added (only when options in widget config)
+        const isInOptionsSchema = optionsSchemaProperty.some((d) => d.includes(_variableKey));
+        if (isInOptionsSchema && _before?.use === false && _after?.use === true) {
+            const targetWidgetInfo = dashboardDetailStore.$state.dashboardWidgetInfoList.find((d) => d.widget_key === widgetKey);
+            if (targetWidgetInfo) {
+                targetWidgetInfo.inherit_options[optionName] = { enabled: true, variable_info: { key: _variableKey } };
+                dashboardDetailStore.updateWidgetInfo(widgetKey, targetWidgetInfo);
+            }
+        }
     });
     return _refresh;
 };
@@ -50,12 +81,13 @@ export const useWidgetLifecycle = ({
     });
     watch(() => props.dashboardVariables, (after, before) => {
         if (!props.initiated || props.errorMode || !props.inheritOptions) return;
-        const _isRefreshable = checkRefreshable(props.inheritOptions, after, before);
+        const _isRefreshable = checkRefreshableByDashboardVariables(props.inheritOptions, after, before);
         if (_isRefreshable) refreshWidget();
     }, { deep: true });
     watch(() => props.dashboardVariablesSchema, (after, before) => {
         if (!props.initiated || props.errorMode || !props.editMode || !props.inheritOptions) return;
-        const _isRefreshable = checkRefreshable(props.inheritOptions, after, before, true);
+        const optionsSchemaProperty = Object.keys(widgetConfig.value.options_schema?.schema.properties ?? {});
+        const _isRefreshable = checkRefreshableByDashboardVariableSchema(props.inheritOptions, optionsSchemaProperty, props.widgetKey, after, before);
         if (_isRefreshable) refreshWidget();
     }, { deep: true });
     if (state.settings) {
