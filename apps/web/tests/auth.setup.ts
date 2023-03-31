347483d43a8ba632773a88c89e254cc9048a41a6
Songyi Kim <sykim@megazone.com>
2023-03-31 17:06:18 +0900

    test(create-dashboard): change login.spect.ts to auth.setup.ts
    Signed-off-by: Songyi Kim <sykim@megazone.com>

--- apps/web/tests/auth.setup.ts
+++ apps/web/tests/auth.setup.ts
@@ -1,16 +1,22 @@
-import { expect, test as setup } from '@playwright/test';
+import { expect, test, test as setup } from '@playwright/test';
 
 const authFile = 'playwright/.auth/user.json';
 setup('Authenticate', async ({ page }) => {
     page.goto('/');
-    await page.getByPlaceholder('User ID').click();
-    await page.getByPlaceholder('User ID').fill(process.env.USERNAME as string);
-    await page.getByPlaceholder('User ID').press('Tab');
-    await page.getByPlaceholder('Password').fill(process.env.PASSWORD as string);
-    const navigationPromise = page.waitForNavigation();
-    await page.getByRole('button', { name: 'Sign in', exact: true }).click();
-    await navigationPromise;
-    await expect(page).toHaveURL(/home-dashboard/);
+
+    await test.step('Enter user information', async () => {
+        await page.getByPlaceholder('User ID').click();
+        await page.getByPlaceholder('User ID').fill(process.env.USERNAME as string);
+        await page.getByPlaceholder('User ID').press('Tab');
+        await page.getByPlaceholder('Password').fill(process.env.PASSWORD as string);
+    });
+
+    await test.step('Click the sign in button', async () => {
+        const navigationPromise = page.waitForNavigation();
+        await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+        await navigationPromise;
+        await expect(page).toHaveURL(/home-dashboard/);
+    });
 
     await page.context().storageState({ path: authFile });
 });
