b59bd9d66f50636c0153857ca8cadef9bd2359e0
Songyi Kim <sykim@megazone.com>
2023-04-03 09:30:33 +0900

    feat(create-dashboard): update create dashboard page UI (#1023)
    * feat(centered-page-layout): add centered-page-layout
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(dashboard-create): show dashboard templates depending on scope
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * chore(lang-pack): add language code
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * Update src/services/dashboards/dashboard-create/DashboardCreatePage.vue
    
    Co-authored-by: sulmo <50662170+sulmoJ@users.noreply.github.com>
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * chore(dashboard-container): remove go-back-button
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(dashboard-create): add responsive style
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * chore(dashboard-create): fix minor things for readability
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix: fix solve conflict
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * chore(language): jp/kr translation added (#1006)
    
    translation updated
    
    * feat(dashboard-create): add test code and fix QA (#1020)
    
    * fix(dashboard-create): show dashboard templates depending on scope
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * chore(lang-pack): add language code
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(dashboard-create): add responsive style
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix: fix solve conflict
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * test(dashboard-create): add test
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(entire-workspace-dashboard): fix test code according to the scenario
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * test: set authentication state to be saved
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * test(entire-workspace-dashboard): change describe
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * test(single-project-dashboard): add test
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(dashboard-create): make language changes to be reflected reactively
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(dashboard-create): fix the UI based on whether scrollbar or not
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(dashboard-create): show empty UI only when no templates
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(dashboard-create): add min/max-height
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(dashboard-create): fix style
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(dashboard-create): show when clicking on a single project
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(dashboard-create): set default dashboard to Monthly Cost Summary
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(dashboard-create): make language changes to be reflected reactively
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(dashboard-create): change focus state button style
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * fix(create-dashboard): add default "overflow-auto" to prevent janky scrolling
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * chore: update language
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    ---------
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * test(create-dashboard): fix create dashboard test code (#1021)
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    * test(create-dashboard): change login.spect.ts to auth.setup.ts (#1022)
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    
    ---------
    
    Signed-off-by: Songyi Kim <sykim@megazone.com>
    Co-authored-by: sulmo <50662170+sulmoJ@users.noreply.github.com>
    Co-authored-by: Breana.J <91929073+jbokyung2@users.noreply.github.com>

--- apps/web/src/services/dashboards/dashboard-create/DashboardCreatePage.vue
+++ apps/web/src/services/dashboards/dashboard-create/DashboardCreatePage.vue
@@ -1,39 +1,75 @@
 <template>
     <div class="dashboard-create-page">
-        <p-heading
-            show-back-button
-            :title="$t('DASHBOARDS.CREATE.TITLE')"
-            @click-back-button="$router.go(-1)"
-        />
-        <section class="dashboard-create-form-container">
+        <div v-if="currentStep === steps[0].step"
+             class="dashboard-create-step1"
+        >
+            <dashboard-create-header
+                :description="steps[currentStep - 1].description"
+                :total-steps="steps.length"
+                :current-step="currentStep"
+            />
             <dashboard-scope-form :dashboard-scope.sync="dashboardScope"
                                   @set-project="setForm('dashboardProject', $event)"
             />
-            <dashboard-template-form @set-template="setForm('dashboardTemplate', $event)" />
             <dashboard-viewer-form :dashboard-viewer-type.sync="dashboardViewerType" />
-        </section>
-        <div class="dashboard-create-buttons">
-            <p-button style-type="tertiary"
-                      size="lg"
-                      @click="$router.go(-1)"
-            >
-                {{ $t('DASHBOARDS.CREATE.CANCEL') }}
-            </p-button>
-            <p-button style-type="primary"
-                      size="lg"
-                      :disabled="!isAllValid"
-                      @click="handleClickCreate"
-            >
-                {{ $t('DASHBOARDS.CREATE.CREATE') }}
-            </p-button>
+            <div class="button-area">
+                <p-button
+                    style-type="transparent"
+                    size="lg"
+                    @click="$router.go(-1)"
+                >
+                    {{ $t('DASHBOARDS.CREATE.CANCEL') }}
+                </p-button>
+                <p-button
+                    style-type="primary"
+                    size="lg"
+                    :disabled="!isValid"
+                    @click="handleGoStep('next')"
+                >
+                    {{ $t('DASHBOARDS.CREATE.CONTINUE') }}
+                </p-button>
+            </div>
+        </div>
+        <div v-if="currentStep === steps[1].step"
+             class="dashboard-create-step2"
+        >
+            <dashboard-create-header
+                :description="steps[currentStep - 1].description"
+                :total-steps="steps.length"
+                :current-step="currentStep"
+            />
+            <dashboard-template-form
+                :dashboard-scope="dashboardScope"
+                @set-template="setForm('dashboardTemplate', $event)"
+            />
+            <div class="button-area">
+                <p-button
+                    style-type="transparent"
+                    size="lg"
+                    icon-left="ic_arrow-left"
+                    @click="handleGoStep('prev')"
+                >
+                    {{ $t('DASHBOARDS.CREATE.GO_BACK') }}
+                </p-button>
+                <p-button
+                    style-type="primary"
+                    size="lg"
+                    :disabled="!isAllValid"
+                    @click="handleClickCreate"
+                >
+                    {{ $t('DASHBOARDS.CREATE.CREATE_NEW_DASHBOARD') }}
+                </p-button>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { reactive, toRefs } from 'vue';
+import { computed, reactive, toRefs } from 'vue';
 
-import { PHeading, PButton } from '@spaceone/design-system';
+import {
+    PButton,
+} from '@spaceone/design-system';
 
 import { SpaceRouter } from '@/router';
 import { i18n } from '@/translations';
@@ -45,6 +81,7 @@ import {
     DASHBOARD_VIEWER,
 } from '@/services/dashboards/config';
 import type { DashboardScope, DashboardViewer } from '@/services/dashboards/config';
+import DashboardCreateHeader from '@/services/dashboards/dashboard-create/modules/DashboardCreateHeader.vue';
 import DashboardScopeForm from '@/services/dashboards/dashboard-create/modules/DashboardScopeForm.vue';
 import DashboardTemplateForm from '@/services/dashboards/dashboard-create/modules/DashboardTemplateForm.vue';
 import DashboardViewerForm from '@/services/dashboards/dashboard-create/modules/DashboardViewerForm.vue';
@@ -53,23 +90,19 @@ import { DASHBOARDS_ROUTE } from '@/services/dashboards/route-config';
 import { useDashboardDetailInfoStore } from '@/services/dashboards/store/dashboard-detail-info';
 import type { ProjectItemResp } from '@/services/project/type';
 
-
 export default {
     name: 'CreateDashboardPage',
     components: {
+        DashboardCreateHeader,
         DashboardViewerForm,
         DashboardTemplateForm,
         DashboardScopeForm,
-        PHeading,
         PButton,
     },
     setup() {
         const dashboardDetailStore = useDashboardDetailInfoStore();
         const {
-            forms: {
-                dashboardTemplate,
-                dashboardProject,
-            },
+            forms: { dashboardTemplate, dashboardProject },
             setForm,
             isAllValid,
         } = useFormValidator({
@@ -88,7 +121,22 @@ export default {
         const state = reactive({
             dashboardScope: DASHBOARD_SCOPE.DOMAIN as DashboardScope,
             dashboardViewerType: DASHBOARD_VIEWER.PUBLIC as DashboardViewer,
+            steps: computed(() => [
+                { step: 1, description: i18n.t('DASHBOARDS.CREATE.STEP1_DESC') },
+                { step: 2, description: i18n.t('DASHBOARDS.CREATE.STEP2_DESC') },
+            ]),
+            currentStep: 1,
+            isValid: computed(() => {
+                if (state.dashboardScope === DASHBOARD_SCOPE.PROJECT) return !!dashboardProject.value?.id;
+                if (state.dashboardScope === DASHBOARD_SCOPE.DOMAIN) return true;
+                return false;
+            }),
         });
+
+        const handleGoStep = (direction: 'prev'|'next') => {
+            if (direction === 'prev') state.currentStep--;
+            else state.currentStep++;
+        };
 
         const handleClickCreate = () => {
             let _dashboardTemplate;
@@ -125,22 +173,52 @@ export default {
             setForm,
             isAllValid,
             handleClickCreate,
+            handleGoStep,
         };
     },
 };
 </script>
 
 <style lang="postcss" scoped>
-.dashboard-create-form-container {
-    display: grid;
-    grid-gap: 1rem;
+.button-area {
+    @apply flex justify-end mt-8 gap-4;
 }
-.dashboard-create-buttons {
-    display: inline-flex;
-    float: right;
-    margin-top: 1rem;
-    & .p-button {
-        margin-left: 1rem;
+.dashboard-create-step1 {
+    @apply w-full;
+    max-width: 30rem;
+    padding: 2.5rem;
+    margin: 0 auto;
+
+    .p-button {
+        &.transparent {
+            &:focus {
+                @apply text-gray-900;
+            }
+            &:hover {
+                @apply text-blue-600;
+            }
+        }
+    }
+
+    @screen tablet {
+        @apply w-full;
+    }
+}
+
+.dashboard-create-step2 {
+    width: 62.5rem;
+    margin: 0 auto;
+    padding: 2rem 1.5rem 4.0625rem;
+
+    @screen tablet {
+        @apply w-full p-8;
+
+        .button-area {
+            @apply flex-col w-full mt-4;
+        }
+    }
+    .dashboard-create-header {
+        @apply mb-8;
     }
 }
 </style>
