023529e5ededf3316cadd6f86a4a975ef41fd9bd
Yongtae Park <samuel.park@megazone.com>
2023-04-04 18:03:04 +0900

    build: apply workspace link to link-mirinae
    Signed-off-by: Yongtae Park <samuel.park@megazone.com>

--- apps/web/dev/link-mirinae.sh
+++ apps/web/dev/link-mirinae.sh
@@ -3,4 +3,5 @@ echo "make Mirinae link" &&
 npm link &&
 cd ${CONSOLE_PATH} &&
 echo "link Mirinae to Console" &&
-npm link @spaceone/design-system
+npm link @spaceone/design-system --workspace=web &&
+rm -rf apps/web/node_modules/@spaceone
