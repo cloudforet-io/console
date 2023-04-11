c618cb607847d970586663fb60e77fe56af8ed35
yuda <yuda@megazone.com>
2023-04-11 12:47:17 +0900

    refactor(use-widget-lifecycle): refactor hook
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/dashboards/widgets/_hooks/use-widget-lifecycle.ts
+++ apps/web/src/services/dashboards/widgets/_hooks/use-widget-lifecycle.ts
@@ -3,13 +3,19 @@ import {
     onUnmounted, watch,
 } from 'vue';
 
-import { isEqual } from 'lodash';
+import {
+    cloneDeep, isEmpty, isEqual, union,
+} from 'lodash';
 
 import type { Currency } from '@/store/modules/display/config';
 
 import type { DashboardVariables, DashboardVariablesSchema } from '@/services/dashboards/config';
 import { useDashboardDetailInfoStore } from '@/services/dashboards/store/dashboard-detail-info';
-import type { InheritOptions, WidgetProps, WidgetConfig } from '@/services/dashboards/widgets/_configs/config';
+import type {
+    InheritOptions, WidgetProps, WidgetConfig, DashboardLayoutWidgetInfo, WidgetFilterKey,
+} from '@/services/dashboards/widgets/_configs/config';
+import { getWidgetFilterSchemaPropertyName } from '@/services/dashboards/widgets/_helpers/widget-schema-helper';
+import { getWidgetInheritOptionsErrorMap } from '@/services/dashboards/widgets/_helpers/widget-validation-helper';
 import type { WidgetState } from '@/services/dashboards/widgets/_hooks/use-widget-state';
 
 
@@ -21,14 +27,12 @@ interface UseWidgetLifecycleOptions {
     onCurrencyUpdate?: (current?: Currency, previous?: Currency) => void|Promise<void>;
 }
 
-
 const dashboardDetailStore = useDashboardDetailInfoStore();
-const dashboardDetailState = dashboardDetailStore.$state;
 
 const checkRefreshableByDashboardVariables = (
     inheritOptions: InheritOptions,
-    after?: DashboardVariables|DashboardVariablesSchema,
-    before?: DashboardVariables|DashboardVariablesSchema,
+    after?: DashboardVariables,
+    before?: DashboardVariables,
 ): boolean => {
     let _refresh = false;
     Object.values(inheritOptions).forEach((inheritOption) => {
@@ -41,47 +45,106 @@ const checkRefreshableByDashboardVariables = (
     });
     return _refresh;
 };
-const checkRefreshableByDashboardVariableSchema = (
+
+/* Dashboard Variable Schema */
+const getUpdatedDashboardVariables = (
+    after?: DashboardVariablesSchema,
+    before?: DashboardVariablesSchema,
+): [WidgetFilterKey[], WidgetFilterKey[], WidgetFilterKey[]] => {
+    const added: WidgetFilterKey[] = [];
+    const deleted: WidgetFilterKey[] = [];
+    const changed: WidgetFilterKey[] = [];
+    const afterVariables = Object.keys(after?.properties ?? {});
+    const beforeVariables = Object.keys(before?.properties ?? {});
+    union(afterVariables, beforeVariables).forEach((variable) => {
+        if (!after?.properties[variable].use && !before?.properties[variable].use) return;
+        if (after?.properties[variable].use && before?.properties[variable].use) {
+            if (isEqual(after?.properties[variable], before?.properties[variable])) return; /* Not changed variable */
+            changed.push(variable as WidgetFilterKey);
+        } else if (after?.properties[variable].use && !before?.properties[variable].use) {
+            added.push(variable as WidgetFilterKey);
+        } else {
+            deleted.push(variable as WidgetFilterKey);
+        }
+    });
+    return [added, deleted, changed];
+};
+const isAffectedByChangedVariables = (
+    dashboardVariables: string[],
     inheritOptions: InheritOptions,
-    widgetConfig: WidgetConfig,
-    widgetKey: string,
-    after?: DashboardVariables|DashboardVariablesSchema,
-    before?: DashboardVariables|DashboardVariablesSchema,
 ): boolean => {
-    let _refresh = false;
-    const targetWidgetInfo = dashboardDetailState.dashboardWidgetInfoList.find((d) => d.widget_key === widgetKey);
-    if (!targetWidgetInfo) return false;
-    Object.entries(inheritOptions).forEach(([property, { enabled, variable_info }]) => {
-        if (!enabled) return;
-
-        const _variableKey = variable_info?.key;
-        if (!_variableKey) return;
-
-        const _after = after?.properties?.[_variableKey];
-        const _before = before?.properties?.[_variableKey];
-        if (!isEqual(_after, _before)) _refresh = true;
-
-        /* update widget info (only when options in widget config) */
-        const optionsSchemaProperties: string[] = Object.keys(widgetConfig.options_schema?.schema.properties ?? {});
+    const enabledInheritOptions = Object.entries(inheritOptions)
+        .filter(([, v]) => v.enabled)
+        .map(([, v]) => v.variable_info?.key as WidgetFilterKey);
+    if (!enabledInheritOptions.length || !enabledInheritOptions.some((d) => dashboardVariables.includes(d))) return false;
+    return true;
+};
+const updateWidgetByAddedVariables = (
+    dashboardVariables: WidgetFilterKey[],
+    widgetInfo: DashboardLayoutWidgetInfo,
+    widgetKey: string,
+    widgetConfig: WidgetConfig,
+): boolean => {
+    const _widgetInfo = cloneDeep(widgetInfo);
+    const optionsSchemaProperties: string[] = Object.keys(widgetConfig.options_schema?.schema.properties ?? {});
+    let isAffected = false;
+    dashboardVariables.forEach((variableKey) => {
+        const property = getWidgetFilterSchemaPropertyName(variableKey);
         const isInWidgetOptionsSchema = optionsSchemaProperties.some((d) => d.includes(property));
         if (!isInWidgetOptionsSchema) return;
 
-        if (_before?.use === false && _after?.use === true) { /* checked case */
-            targetWidgetInfo.inherit_options[property] = { enabled: true, variable_info: { key: _variableKey } };
-            dashboardDetailStore.updateWidgetInfo(widgetKey, targetWidgetInfo);
-        } else if (_before?.use === true && _after?.use === false) { /* unchecked case */
-            const isFixedProperty: boolean = widgetConfig.options_schema?.fixed_properties?.includes(property) ?? false;
-            if (isFixedProperty) { /* fixed property case */
-                console.log(property, widgetConfig.options?.[property]);
-                targetWidgetInfo.widget_options[property] = widgetConfig.options?.[property] ?? undefined; // set default value to option
-            } else { /* not-fixed property case */
-                targetWidgetInfo.schema_properties.splice(targetWidgetInfo.schema_properties.indexOf(property), 1);
-            }
-            targetWidgetInfo.inherit_options[property] = { enabled: false };
-            dashboardDetailStore.updateWidgetInfo(widgetKey, targetWidgetInfo);
-        }
+        // enable widget option that match added variable
+        isAffected = true;
+        _widgetInfo.inherit_options[property] = { enabled: true, variable_info: { key: variableKey } };
+        _widgetInfo.schema_properties.push(property);
     });
-    return _refresh;
+    if (isAffected) {
+        dashboardDetailStore.updateWidgetInfo(widgetKey, _widgetInfo);
+    }
+    return isAffected;
+};
+const updateWidgetByDeletedVariables = (
+    dashboardVariables: WidgetFilterKey[],
+    widgetInfo: DashboardLayoutWidgetInfo,
+    widgetKey: string,
+    widgetConfig: WidgetConfig,
+    inheritOptions: InheritOptions,
+    dashboardVariablesSchema?: DashboardVariablesSchema,
+): boolean => {
+    // check whether the deleted variable exists in inherit options
+    const _widgetInfo = cloneDeep(widgetInfo);
+    const enabledInheritOptions = Object.entries(inheritOptions)
+        .filter(([, v]) => v.enabled)
+        .map(([, v]) => v.variable_info?.key as WidgetFilterKey);
+    if (!enabledInheritOptions.length || !enabledInheritOptions.some((d) => dashboardVariables.includes(d))) {
+        return false;
+    }
+
+    // delete or update options using deleted variables
+    dashboardVariables.forEach((variableKey) => {
+        const property = getWidgetFilterSchemaPropertyName(variableKey);
+        const isFixedProperty: boolean = widgetConfig.options_schema?.fixed_properties?.includes(property) ?? false;
+        if (isFixedProperty) { /* fixed property case */
+            _widgetInfo.widget_options[property] = widgetConfig.options?.[property] ?? undefined; // set default value to fixed option
+        } else { /* non-fixed property case */
+            const _schemaProperties = [..._widgetInfo.schema_properties];
+            _widgetInfo.schema_properties = _schemaProperties.filter((d) => d !== property);
+        }
+        _widgetInfo.inherit_options[property] = { enabled: false };
+    });
+
+    dashboardDetailStore.updateWidgetInfo(widgetKey, _widgetInfo);
+    validateWidget(_widgetInfo.inherit_options, widgetKey, widgetConfig, dashboardVariablesSchema);
+    return true;
+};
+const validateWidget = (
+    inheritOptions: InheritOptions,
+    widgetKey: string,
+    widgetConfig: WidgetConfig,
+    dashboardVariableSchema?: DashboardVariablesSchema,
+) => {
+    const _widgetSchemaErrorMap = getWidgetInheritOptionsErrorMap(inheritOptions, widgetConfig.options_schema?.schema, dashboardVariableSchema);
+    dashboardDetailStore.updateWidgetValidation(isEmpty(_widgetSchemaErrorMap), widgetKey);
 };
 
 export const useWidgetLifecycle = ({
@@ -100,10 +163,21 @@ export const useWidgetLifecycle = ({
         if (_isRefreshable) refreshWidget();
     }, { deep: true });
     watch(() => props.dashboardVariablesSchema, (after, before) => {
-        if (!props.initiated || !props.editMode || !props.inheritOptions) return;
-        const _isRefreshable = checkRefreshableByDashboardVariableSchema(props.inheritOptions, state.widgetConfig, props.widgetKey, after, before);
-        if (props.errorMode) return;
-        if (_isRefreshable) refreshWidget();
+        if (!props.initiated || !props.editMode || !props.inheritOptions || isEqual(after, before)) return;
+        if (!state.widgetInfo) return;
+
+        const [added, deleted, changed] = getUpdatedDashboardVariables(after, before);
+        let isChanged; let isAdded; let isDeleted;
+        if (changed.length) {
+            isChanged = isAffectedByChangedVariables(changed, props.inheritOptions);
+        }
+        if (added.length) {
+            isAdded = updateWidgetByAddedVariables(added, state.widgetInfo, props.widgetKey, state.widgetConfig);
+        }
+        if (deleted.length) {
+            isDeleted = updateWidgetByDeletedVariables(deleted, state.widgetInfo, props.widgetKey, state.widgetConfig, props.inheritOptions, after);
+        }
+        if (isChanged || isAdded || isDeleted) refreshWidget();
     }, { deep: true });
     if (state.settings) {
         watch(() => state.settings, (current, previous) => {
