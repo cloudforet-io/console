27027cbbad8732f24f726dd0a94085e091ab6047
WANJIN <wanjin@megazone.com>
2023-04-19 15:47:26 +0900

    chore: add additional scripts (#1053)
    * chore: add additional scripts
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    * chore: remove unnecessary files
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    ---------
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/dev/link-mirinae.sh
+++
@@ -1,7 +0,0 @@
-cd ${MIRINAE_PATH} &&
-echo "make Mirinae link" &&
-npm link &&
-cd ${CONSOLE_PATH} &&
-echo "link Mirinae to Console" &&
-npm link @spaceone/design-system --workspace=web &&
-rm -rf apps/web/node_modules/@spaceone
--- apps/web/dev/unlink-mirinae.sh
+++
@@ -1,6 +0,0 @@
-cd ${CONSOLE_PATH} &&
-echo "unlink Mirinae from Console" &&
-npm unlink --no-save @spaceone/design-system &&
-cd ${MIRINAE_PATH} &&
-echo "unlink Mirinae"
-npm unlink ${MIRINAE_PATH}
