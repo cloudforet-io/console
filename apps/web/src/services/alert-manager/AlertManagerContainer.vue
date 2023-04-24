8fa2374d2df2783b870c32e5df144d429cc656ca
sulmo <sulmo@megazone.com>
2023-04-24 11:55:22 +0900

    feat(alert-manage-store): add alert manager settings store & apply
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/services/alert-manager/AlertManagerContainer.vue
+++ apps/web/src/services/alert-manager/AlertManagerContainer.vue
@@ -19,11 +19,16 @@
 </template>
 
 <script lang="ts">
+import { computed } from 'vue';
+
+import { store } from '@/store';
+
 import { useBreadcrumbs } from '@/common/composables/breadcrumbs';
 import GeneralPageLayout from '@/common/modules/page-layouts/GeneralPageLayout.vue';
 import VerticalPageLayout from '@/common/modules/page-layouts/VerticalPageLayout.vue';
 
 import AlertManagerLNB from '@/services/alert-manager/AlertManagerLNB.vue';
+import { useAlertManagerSettingsStore } from '@/services/alert-manager/store/alert-manager-settings-store';
 
 export default {
     name: 'AlertManagerContainer',
@@ -34,6 +39,20 @@ export default {
     },
     setup() {
         const { breadcrumbs } = useBreadcrumbs();
+        const userId = computed(() => store.state.user.userId);
+        const alertManagerSettings = useAlertManagerSettingsStore();
+        alertManagerSettings.$onAction((action) => {
+            action.after(() => {
+                if (window) {
+                    const settings = window.localStorage.getItem(userId.value);
+                    if (settings) {
+                        const settingsObj = JSON.parse(settings);
+                        settingsObj.alertManager = action.store.$state;
+                        window.localStorage.setItem(userId.value, JSON.stringify(settingsObj));
+                    }
+                }
+            });
+        });
         return {
             breadcrumbs,
         };
