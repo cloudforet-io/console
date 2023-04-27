1e6816f3ab076879235227b8ee246525a20af474
Wanjin Noh <wanjin@megazone.com>
2023-04-27 20:53:53 +0900

    build: add postcss-config-custom package
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/storybook/postcss.config.js
+++ apps/storybook/postcss.config.js
@@ -1,23 +1,19 @@
 /* eslint-disable global-require, @typescript-eslint/no-var-requires */
 const path = require('path');
 
-module.exports = {
-    parser: 'postcss-comment',
-    plugins: [
-        require('postcss-easy-import')({
-            path: ['../../packages/mirinae/src', 'node_modules'],
-        }),
-        require('tailwindcss')({ config: path.resolve(__dirname, 'tailwind.config.js') }),
-        require('postcss-hexrgba'),
-        require('postcss-mixins'),
-        require('postcss-conditionals'),
-        require('postcss-nested'),
-        require('postcss-simple-vars')({
-            variables() {
-                return require('../../packages/mirinae/src/styles/variables.cjs');
-            },
-        }),
-        require('autoprefixer'),
-        require('postcss-preset-env')({ stage: 3 }),
-    ],
-};
+const getPostcssConfig = require('postcss-config-custom');
+
+module.exports = getPostcssConfig({
+    tailwindcssOptions: {
+        config: path.resolve(__dirname, 'tailwind.config.js'),
+    },
+    postcssEasyImportOptions: {
+        path: ['../../packages/mirinae/src', 'node_modules'],
+    },
+    postcssSimpleVarsOptions: {
+        variables() {
+            return require('../../packages/mirinae/src/styles/variables.cjs');
+        },
+    },
+    disablePostcssImport: true,
+});
--- packages/mirinae/postcss.config.cjs
+++ packages/mirinae/postcss.config.cjs
@@ -1,20 +1,17 @@
-module.exports = {
-    parser: 'postcss-comment',
-    plugins: [
-            require('postcss-easy-import')({
-                path: ['src', 'node_modules'],
-            }),
-            require('tailwindcss')({ config: 'tailwind.config.cjs' }),
-            require('postcss-hexrgba'),
-            require('postcss-mixins'),
-            require('postcss-conditionals'),
-            require('postcss-nested'),
-            require('postcss-simple-vars')({
-                variables() {
-                    return require('./src/styles/variables.cjs');
-                },
-            }),
-            require('autoprefixer'),
-            require('postcss-preset-env')({ stage: 3 }),
-    ]
-};
+const getPostcssConfig = require('postcss-config-custom');
+
+module.exports = getPostcssConfig(
+    {
+        tailwindcssOptions: {
+            config: 'tailwind.config.cjs',
+        },
+        postcssEasyImportOptions: {
+            path: ['src', 'node_modules'],
+        },
+        postcssSimpleVarsOptions: {
+            variables() {
+                return require('./src/styles/variables.cjs');
+            }
+        }
+    }
+)
