c0f51e33fdb95fa0f177c28b783881795a924037
Piggy Park (박용태) <samuel.park@megazone.com>
2023-05-18 10:45:46 +0900

    build(svg-icon): create svg icons object builder cli (#1138)
    Signed-off-by: Yongtae Park <samuel.park@megazone.com>

---
+++ packages/mirinae/cli/svg-gen.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+/* eslint-disable */
+
+/**
+ * generate svg icons contents
+ * @since 2023-05-12
+ */
+
+const path = require('path');
+
+const { Command } = require('commander');
+
+const { build } = require('./build');
+
+const program = new Command();
+program
+    .option('-s, --svgSourcePath <path>', 'Svg source path')
+    .option('-t, --targetPath <path>', 'Generate icon path')
+    .option('-e, --ext <ext>', "Generated file's extension", 'js')
+    .option('--svgo <svgo>', 'Svgo config file', 'svgo')
+    .parse(process.argv);
+
+// svg file path
+const sourcePath = path.isAbsolute(program.opts().svgSourcePath)
+    ? program.opts().svgSourcePath
+    : path.join(process.cwd(), program.opts().svgSourcePath);
+
+// generated icon path
+const targetPath = path.isAbsolute(program.opts().targetPath)
+    ? program.opts().targetPath
+    : path.join(process.cwd(), program.opts().targetPath);
+
+// auto run
+(async () => {
+    try {
+        /* TODO: replace to generate function in Vue 3 with @yzfe/svgicon */
+        await build({
+            sourcePath,
+            targetPath,
+        });
+    } catch (err) {
+        console.log(err);
+    }
+})();
