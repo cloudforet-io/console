f307944318bd2de321d54bb8308c4d1da94ebd56
NaYeong,Kim <nayeongkim@megazone.com>
2023-05-22 11:10:59 +0900

    feat(collector-landing): create UI when there is no data (#1149)
    Signed-off-by: nayeongkim <nayeongkim@megazone.com>

---
+++ apps/web/src/services/asset-inventory/collector/modules/CollectorNoData.vue
@@ -0,0 +1,41 @@
+<template>
+    <p-empty
+        show-image
+        show-button
+        image-size="md"
+    >
+        <template #image>
+            <img alt="empty-image"
+                 src="@/assets/images/illust_microscope.svg"
+            >
+        </template>
+        <template #button>
+            <p-button
+                icon-left="ic_plus_bold"
+                @click="handleCreate"
+            >
+                <span>Create Collector</span>
+            </p-button>
+        </template>
+        There’s no collector created yet.
+    </p-empty>
+</template>
+
+<script setup lang="ts">
+import { PEmpty, PButton } from '@spaceone/design-system';
+
+import { SpaceRouter } from '@/router';
+
+import { ASSET_INVENTORY_ROUTE } from '@/services/asset-inventory/route-config';
+
+const handleCreate = () => {
+    SpaceRouter.router.push({ name: ASSET_INVENTORY_ROUTE.COLLECTOR.CREATE._NAME });
+};
+</script>
+
+<style scoped lang="postcss">
+.p-empty {
+    padding-top: 3rem;
+    padding-bottom: 2.5rem;
+}
+</style>
