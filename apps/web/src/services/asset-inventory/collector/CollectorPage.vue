fd3f7c4aafb2406de9dd6b9421e0da20a1147b4c
NaYeong,Kim <nayeongkim@megazone.com>
2023-05-22 11:10:59 +0900

    feat(collector-landing): create UI when there is no data (#1149)
    Signed-off-by: nayeongkim <nayeongkim@megazone.com>

--- apps/web/src/services/asset-inventory/collector/CollectorPage.vue
+++ apps/web/src/services/asset-inventory/collector/CollectorPage.vue
@@ -1,5 +1,167 @@
 <template>
     <div class="collector-page">
-        collect-page
+        <p-heading
+            :title="$t('PLUGIN.COLLECTOR.MAIN.TITLE')"
+            use-total-count
+            use-selected-count
+            :total-count="state.totalCount"
+        >
+            <template #extra>
+                <router-link
+                    :to="{ name: ASSET_INVENTORY_ROUTE.COLLECTOR.HISTORY._NAME }"
+                >
+                    <p-button style-type="tertiary"
+                              class="history-button"
+                    >
+                        {{ $t("MANAGEMENT.COLLECTOR_HISTORY.MAIN.TITLE") }}
+                    </p-button>
+                </router-link>
+            </template>
+        </p-heading>
+        <p-data-loader :data="[]"
+                       :loading="state.loading && !state.items"
+        >
+            <!-- TODO: collector contents-->
+            <template #no-data>
+                <collector-no-data />
+            </template>
+        </p-data-loader>
     </div>
 </template>
+
+<script lang="ts" setup>
+import { computed, reactive } from 'vue';
+
+import { PHeading, PButton, PDataLoader } from '@spaceone/design-system';
+
+import { QueryHelper } from '@cloudforet/core-lib/query';
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+import { ApiQueryHelper } from '@cloudforet/core-lib/space-connector/helper';
+
+import { store } from '@/store';
+
+import type { PluginReferenceMap } from '@/store/modules/reference/plugin/type';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+
+import CollectorNoData from '@/services/asset-inventory/collector/modules/CollectorNoData.vue';
+import type { CollectorModel } from '@/services/asset-inventory/collector/type';
+import { CollectorQueryHelperSet } from '@/services/asset-inventory/collector/type';
+import { ASSET_INVENTORY_ROUTE } from '@/services/asset-inventory/route-config';
+
+const queryHelper = new QueryHelper();
+
+const state = reactive({
+    loading: true,
+    items: undefined as CollectorModel[] | undefined,
+    totalCount: 0,
+    pageStart: 1,
+    pageLimit: 15,
+    sortBy: '',
+    plugins: computed<PluginReferenceMap>(
+        () => store.getters['reference/pluginItems'],
+    ),
+});
+
+/* Query Helper */
+const collectorApiQueryHelper = new ApiQueryHelper()
+    .setOnly(
+        CollectorQueryHelperSet.COLLECTOR_ID,
+        CollectorQueryHelperSet.NAME,
+        CollectorQueryHelperSet.LAST_COLLECTED_AT,
+        CollectorQueryHelperSet.PROVIDER,
+        CollectorQueryHelperSet.TAGS,
+        CollectorQueryHelperSet.PLUGIN_INFO,
+        CollectorQueryHelperSet.STATE,
+    )
+    .setPage(state.pageStart, state.pageLimit)
+    .setSort(state.sortBy, true);
+
+/* API */
+const listCollectors = async () => {
+    state.loading = true;
+    const detailLinkQueryHelper = new QueryHelper();
+    try {
+        collectorApiQueryHelper.setFilters(queryHelper.filters);
+        const res = await SpaceConnector.client.inventory.collector.list({
+            query: collectorApiQueryHelper.data,
+        });
+        state.items = res.results.map((d) => ({
+            plugin_name: state.plugins[d.plugin_info.plugin_id]?.label,
+            plugin_icon: state.plugins[d.plugin_info.plugin_id]?.icon,
+            detailLink: {
+                name: ASSET_INVENTORY_ROUTE.COLLECTOR.DETAIL._NAME,
+                param: { id: d.collector_id },
+                query: {
+                    filters: detailLinkQueryHelper.setFilters([
+                        {
+                            k: CollectorQueryHelperSet.COLLECTOR_ID,
+                            v: d.collector_id,
+                            o: '=',
+                        },
+                    ]).rawQueryStrings,
+                },
+            },
+            ...d,
+        }));
+        state.totalCount = res.total_count || 0;
+    } catch (e) {
+        ErrorHandler.handleError(e);
+        state.items = [];
+        state.totalCount = 0;
+    } finally {
+        state.loading = false;
+    }
+};
+
+/* INIT */
+(async () => {
+    await Promise.allSettled([
+        store.dispatch('reference/plugin/load'),
+        store.dispatch('reference/provider/load'),
+    ]);
+    await Promise.all([listCollectors()]);
+})();
+</script>
+
+<style lang="postcss" scoped>
+/* custom design-system component - p-heading */
+:deep(.p-heading) {
+    @apply items-center;
+
+    .heading-wrapper {
+        flex: 1;
+
+        .total-count {
+            @apply font-normal;
+        }
+    }
+
+    .extra {
+        flex-grow: initial;
+
+        .history-button {
+            @apply bg-white rounded border border-gray-300 text-label-md font-bold;
+            width: 100%;
+            padding: 0.375rem 0.75rem;
+        }
+    }
+}
+
+/* custom design-system component - p-data-loader */
+:deep(.p-data-loader) {
+    min-height: 16.875rem;
+
+    .no-data-wrapper {
+        @apply bg-white rounded-md border border-gray-200;
+        height: auto;
+        max-height: initial;
+    }
+
+    .loader-wrapper {
+        .loader-backdrop {
+            @apply hidden;
+        }
+    }
+}
+</style>
