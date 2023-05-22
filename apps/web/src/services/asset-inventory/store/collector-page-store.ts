aa78cc68a6fbcbfc5bf63f6be4fefe299ccceeb8
nayeongkim <nayeongkim@megazone.com>
2023-05-22 18:08:11 +0900

    feat(collector-landing): filter by provider
    Signed-off-by: nayeongkim <nayeongkim@megazone.com>

--- apps/web/src/services/asset-inventory/store/collector-page-store.ts
+++ apps/web/src/services/asset-inventory/store/collector-page-store.ts
@@ -1,18 +1,32 @@
 import { defineStore } from 'pinia';
 
+import type { ConsoleFilter } from '@cloudforet/core-lib/query/type';
+
 import type { CollectorModel } from '@/services/asset-inventory/collector/type';
 
 export const useCollectorPageStore = defineStore('collector-page', {
     state: () => ({
         selectedProvider: 'all',
+        searchFilters: [] as ConsoleFilter[],
         collectorList: undefined as CollectorModel[] | undefined,
+        filteredList: undefined as CollectorModel[] | undefined,
     }),
+    getters: {
+        allFilters: (state): ConsoleFilter[] => {
+            const filters: ConsoleFilter[] = [];
+            if (state.selectedProvider !== 'all') {
+                filters.push({ k: 'provider', v: state.selectedProvider, o: '=' });
+            }
+            return filters.concat(state.searchFilters);
+        },
+    },
     actions: {
         async setSelectedProvider(provider) {
             this.selectedProvider = provider;
         },
         async setCollectorList(collectorList) {
             this.collectorList = collectorList;
+            this.filteredList = collectorList;
         },
     },
 });
