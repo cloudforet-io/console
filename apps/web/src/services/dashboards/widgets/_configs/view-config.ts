23261ed9906c377aa09893e6ad5e2db3238e2a74
yuda <yuda@megazone.com>
2023-05-23 15:52:14 +0900

    feat: split `GROUP_BY_ITEM_MAP` into cost and asset
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/dashboards/widgets/_configs/view-config.ts
+++ apps/web/src/services/dashboards/widgets/_configs/view-config.ts
@@ -1,16 +1,16 @@
-import { ASSET_GROUP_BY, GROUP_BY } from '@/services/dashboards/widgets/_configs/config';
+import { ASSET_GROUP_BY, COST_GROUP_BY } from '@/services/dashboards/widgets/_configs/config';
 
-export const GROUP_BY_ITEM_MAP = {
-    [GROUP_BY.PROJECT_GROUP]: { name: GROUP_BY.PROJECT_GROUP, label: 'Project Group' },
-    [GROUP_BY.PROJECT]: { name: GROUP_BY.PROJECT, label: 'Project' },
-    [GROUP_BY.PROVIDER]: { name: GROUP_BY.PROVIDER, label: 'Provider' },
-    [GROUP_BY.SERVICE_ACCOUNT]: { name: GROUP_BY.SERVICE_ACCOUNT, label: 'Service Account' },
-    [GROUP_BY.CATEGORY]: { name: GROUP_BY.CATEGORY, label: 'Category' },
-    [GROUP_BY.RESOURCE_GROUP]: { name: GROUP_BY.RESOURCE_GROUP, label: 'Resource Group' },
-    [GROUP_BY.PRODUCT]: { name: GROUP_BY.PRODUCT, label: 'Product' },
-    [GROUP_BY.REGION]: { name: GROUP_BY.REGION, label: 'Region' },
-    [GROUP_BY.TYPE]: { name: GROUP_BY.TYPE, label: 'Type' },
-    [GROUP_BY.ACCOUNT]: { name: GROUP_BY.ACCOUNT, label: 'Account ID' },
+export const COST_GROUP_BY_ITEM_MAP = {
+    [COST_GROUP_BY.PROJECT_GROUP]: { name: COST_GROUP_BY.PROJECT_GROUP, label: 'Project Group' },
+    [COST_GROUP_BY.PROJECT]: { name: COST_GROUP_BY.PROJECT, label: 'Project' },
+    [COST_GROUP_BY.PROVIDER]: { name: COST_GROUP_BY.PROVIDER, label: 'Provider' },
+    [COST_GROUP_BY.SERVICE_ACCOUNT]: { name: COST_GROUP_BY.SERVICE_ACCOUNT, label: 'Service Account' },
+    [COST_GROUP_BY.CATEGORY]: { name: COST_GROUP_BY.CATEGORY, label: 'Category' },
+    [COST_GROUP_BY.RESOURCE_GROUP]: { name: COST_GROUP_BY.RESOURCE_GROUP, label: 'Resource Group' },
+    [COST_GROUP_BY.PRODUCT]: { name: COST_GROUP_BY.PRODUCT, label: 'Product' },
+    [COST_GROUP_BY.REGION]: { name: COST_GROUP_BY.REGION, label: 'Region' },
+    [COST_GROUP_BY.TYPE]: { name: COST_GROUP_BY.TYPE, label: 'Type' },
+    [COST_GROUP_BY.ACCOUNT]: { name: COST_GROUP_BY.ACCOUNT, label: 'Account ID' },
 } as const;
 
 export const ASSET_GROUP_BY_ITEM_MAP = {
