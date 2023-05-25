0b0ec20acc8ef4acd2527829ce535c4370052c27
WANJIN <wanjin@megazone.com>
2023-05-25 10:38:43 +0900

    feat: add sections in collector detail page (#1152)
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

---
+++ apps/web/src/services/asset-inventory/collector/collector-detail/modules/CollectorBaseInfoSection.vue
@@ -0,0 +1,28 @@
+<template>
+    <p-pane-layout>
+        <p-heading :title="i18n.t('INVENTORY.COLLECTOR.DETAIL.BASE_INFO')"
+                   heading-type="sub"
+        >
+            <template #extra>
+                <p-button size="md"
+                          icon-left="ic_edit"
+                          style-type="secondary"
+                >
+                    {{ i18n.t('INVENTORY.COLLECTOR.DETAIL.EDIT') }}
+                </p-button>
+            </template>
+        </p-heading>
+    </p-pane-layout>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+import { PHeading, PButton, PPaneLayout } from '@spaceone/design-system';
+
+import { i18n } from '@/translations';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/ban-types
+const props = defineProps<{}>();
+
+</script>
