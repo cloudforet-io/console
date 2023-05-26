33c48e1b358cdb692208b95a0c75fc26eac6c36b
WANJIN <wanjin@megazone.com>
2023-05-26 15:01:37 +0900

    feat: add CollectorBaseInfoSection read case markups (#1170)
    * refactor: change some collector related types
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    * feat: add CollectorBaseInfoSection read case markups
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    * refactor: separated states
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    ---------
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/asset-inventory/collector/collector-detail/modules/CollectorBaseInfoSection.vue
+++ apps/web/src/services/asset-inventory/collector/collector-detail/modules/CollectorBaseInfoSection.vue
@@ -12,17 +12,124 @@
                 </p-button>
             </template>
         </p-heading>
+
+        <p-definition-table :fields="fields"
+                            :loading="state.loading"
+                            :data="state.collector"
+        >
+            <template #data-pluginName>
+                <p-lazy-img :src="state.pluginIcon"
+                            :loading="state.loading"
+                            width="1rem"
+                            height="1rem"
+                />
+                <!-- TODO: Remove test code below: 'Name!' -->
+                <span class="ml-2 leading-none">{{ state.pluginName || 'Name!' }}</span>
+            </template>
+            <template #data-plugin_info.version="{ value }">
+                {{ value }} {{ isLatestVersion(value) ? ' (latest)' : '' }}
+            </template>
+            <template #data-plugin_info.upgrade_mode="{ value }">
+                {{ value === UPGRADE_MODE.AUTO ? 'ON' : 'OFF' }}
+            </template>
+            <template #data-created_at="{ value }">
+                {{ value ? iso8601Formatter(value, timezone) : '' }}
+            </template>
+            <template #data-last_collected_at="{ value }">
+                {{ value ? iso8601Formatter(value, timezone) : '' }}
+            </template>
+        </p-definition-table>
     </p-pane-layout>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from 'vue';
+import {
+    defineProps, computed, onMounted, reactive,
+} from 'vue';
 
-import { PHeading, PButton, PPaneLayout } from '@spaceone/design-system';
+import {
+    PHeading, PButton, PPaneLayout, PDefinitionTable, PLazyImg,
+} from '@spaceone/design-system';
+import type { DefinitionField } from '@spaceone/design-system/types/data-display/tables/definition-table/type';
 
+import { iso8601Formatter } from '@cloudforet/core-lib/index';
+
+import { store } from '@/store';
 import { i18n } from '@/translations';
+
+import type { PluginReferenceMap } from '@/store/modules/reference/plugin/type';
+
+import type { CollectorModel } from '@/services/asset-inventory/collector/type';
+import { UPGRADE_MODE } from '@/services/asset-inventory/collector/type';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/ban-types
 const props = defineProps<{}>();
+
+const timezone = computed<string>(() => store.state.user.timezone);
+const plugins = computed<PluginReferenceMap>(() => store.getters['reference/pluginItems']);
+const fields = computed<DefinitionField[]>(() => [
+    { name: 'pluginName', label: i18n.t('INVENTORY.COLLECTOR.DETAIL.PLUGIN') },
+    { name: 'plugin_info.plugin_id', label: i18n.t('INVENTORY.COLLECTOR.DETAIL.PLUGIN_ID') },
+    { name: 'plugin_info.version', label: i18n.t('INVENTORY.COLLECTOR.DETAIL.VERSION') },
+    { name: 'tags', label: i18n.t('INVENTORY.COLLECTOR.DETAIL.TAG') },
+    { name: 'plugin_info.upgrade_mode', label: i18n.t('INVENTORY.COLLECTOR.DETAIL.AUTO_UPGRADE') },
+    { name: 'last_collected_at', label: i18n.t('INVENTORY.COLLECTOR.DETAIL.LAST_COLLECTED') },
+    { name: 'created_at', label: i18n.t('INVENTORY.COLLECTOR.DETAIL.CREATED') },
+]);
+
+const state = reactive({
+    collector: null as null|CollectorModel,
+    loading: true,
+    pluginName: computed<string>(() => (state.collector ? plugins.value[state.collector.plugin_info.plugin_id]?.label ?? '' : '')),
+    pluginIcon: computed<string>(() => (state.collector ? plugins.value[state.collector.plugin_info.plugin_id]?.icon ?? '' : '')),
+});
+
+// TODO: Implement isLatestVersion
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const isLatestVersion = (version: string) => true;
+
+
+// TODO: move to upper level
+const getCollector = async (): Promise<CollectorModel> => {
+    state.loading = true;
+    // TODO: change to real data
+    const result = await new Promise<CollectorModel>((resolve) => {
+        setTimeout(() => {
+            resolve({
+                collector_id: 'collector-1',
+                name: 'collector-1',
+                state: 'ENABLED',
+                provider: 'aws',
+                capability: {
+                    supported_schema: ['aws_access_key', 'aws_access_key_pair'],
+                    supported_mode: ['FULL', 'DIFF'],
+                    supported_schedule: ['* * * * *'],
+                },
+                plugin_info: {
+                    plugin_id: 'plugin-1',
+                    version: '1.0',
+                    upgrade_mode: 'AUTO',
+                    options: {
+                        supported_resource_type: ['inventory.Server'],
+                        filter_format: [],
+                    },
+                },
+                priority: 1,
+                tags: {
+                    'spaceone:region': 'kr',
+                    'spaceone:zone': 'kr-1',
+                },
+                last_collected_at: '2021-08-31T00:00:00Z',
+                created_at: '2021-08-31T00:00:00Z',
+            });
+        }, 2000);
+    });
+    state.loading = false;
+    return result;
+};
+
+onMounted(async () => {
+    state.collector = await getCollector();
+});
 
 </script>
