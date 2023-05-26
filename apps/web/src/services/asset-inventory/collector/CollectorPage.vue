409e8a62b25e40ba13ba62191695ec4f01178976
NaYeong,Kim <nayeongkim@megazone.com>
2023-05-26 13:28:21 +0900

    feat(collector-landing): apply search query (#1165)
    * fix: fixed margin because of providers list
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * feat(collector-landing): apply search query
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * feat(collector-page-store): tidy-up the store
    
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>
    
    * fix(collector-lading): fixed class name at collector item
    
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>
    
    * feat(collector-page): changed function logic
    
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>
    
    * chore: changed the function name
    
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>
    
    * fix(collector-page-store): changed the total count name
    
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>
    
    * chore: changed query helper position
    
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>

--- apps/web/src/services/asset-inventory/collector/CollectorPage.vue
+++ apps/web/src/services/asset-inventory/collector/CollectorPage.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="collector-page">
         <p-heading
-            :title="$t('INVENTORY.COLLECTOR.MAIN.TITLE')"
             use-total-count
             use-selected-count
-            :total-count="state.totalCount"
+            :title="$t('INVENTORY.COLLECTOR.MAIN.TITLE')"
+            :total-count="collectorPageState.totalCount"
         >
             <template #extra>
                 <router-link
@@ -18,16 +18,18 @@
                 </router-link>
             </template>
         </p-heading>
+        <!-- FIXME: loading must be fixed after basic function. -->
         <p-data-loader
-            :data="cloudCollectorPageState.collectorList"
-            :loading="state.loading && !cloudCollectorPageState.collectorList"
+            :data="collectorPageState.collectorList"
+            :loading="collectorPageState.loading && !collectorPageState.collectorList"
             loader-backdrop-color="gray.100"
             class="collector-contents-wrapper"
         >
             <collector-contents
-                :total-count="state.totalCount"
-                :page-limit="state.pageLimit"
-                @export-excel="handleExportExcel"
+                :key-item-sets="handlerState.keyItemSets"
+                :provider-list="state.providerList"
+                :search-tags="state.searchTags"
+                @change-toolbox="handleChangeToolbox"
             />
             <template #no-data>
                 <collector-no-data />
@@ -37,45 +39,63 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, reactive, watch } from 'vue';
+import {
+    computed, onUnmounted, reactive, watch,
+} from 'vue';
 
 import { PButton, PDataLoader, PHeading } from '@spaceone/design-system';
 
+import type { KeyItemSet } from '@cloudforet/core-lib/component-util/query-search/type';
+import { setApiQueryWithToolboxOptions } from '@cloudforet/core-lib/component-util/toolbox';
 import { QueryHelper } from '@cloudforet/core-lib/query';
-import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 import { ApiQueryHelper } from '@cloudforet/core-lib/space-connector/helper';
 
 import { store } from '@/store';
 
 import type { PluginReferenceMap } from '@/store/modules/reference/plugin/type';
-
-import { FILE_NAME_PREFIX } from '@/lib/excel-export';
+import type { ProviderReferenceMap } from '@/store/modules/reference/provider/type';
 
 import ErrorHandler from '@/common/composables/error/errorHandler';
 
 import CollectorContents from '@/services/asset-inventory/collector/modules/CollectorContents.vue';
 import CollectorNoData from '@/services/asset-inventory/collector/modules/CollectorNoData.vue';
-import type { CollectorModel } from '@/services/asset-inventory/collector/type';
 import { COLLECTOR_QUERY_HELPER_SET } from '@/services/asset-inventory/collector/type';
 import { ASSET_INVENTORY_ROUTE } from '@/services/asset-inventory/route-config';
 import { useCollectorPageStore } from '@/services/asset-inventory/store/collector-page-store';
 
-const cloudCollectorPageStore = useCollectorPageStore();
-const cloudCollectorPageState = cloudCollectorPageStore.$state;
+const collectorPageStore = useCollectorPageStore();
+const collectorPageState = collectorPageStore.$state;
 
-const detailLinkQueryHelper = new QueryHelper();
+const storeState = reactive({
+    providers: computed<ProviderReferenceMap>(() => store.getters['reference/providerItems']),
+    plugins: computed<PluginReferenceMap>(() => store.getters['reference/pluginItems']),
+});
+
+const handlerState = reactive({
+    keyItemSets: computed<KeyItemSet[]>(() => [{
+        title: 'Properties',
+        items: [
+            { name: 'collector_id', label: 'Collector Id' },
+            { name: 'name', label: 'Name' },
+            { name: 'state', label: 'State' },
+            { name: 'plugin_info.plugin_id', label: 'Plugin' },
+            { name: 'plugin_info.version', label: 'Version' },
+            { name: 'provider', label: 'Provider', valueSet: storeState.providers },
+            { name: 'supported_resource_type', label: 'Resource Type' },
+            { name: 'created_at', label: 'Created' },
+            { name: 'last_collected_at', label: 'Last Collected' },
+        ],
+    }]),
+});
+
+const searchQueryHelper = new QueryHelper().setKeyItemSets(handlerState.keyItemSets ?? []);
 
 const state = reactive({
-    loading: true,
-    totalCount: 0,
-    pageStart: 1,
-    pageLimit: 15,
-    sortBy: '',
-    collectors: undefined as undefined | CollectorModel[],
-    plugins: computed<PluginReferenceMap>(() => store.getters['reference/pluginItems']),
+    providerList: computed(() => ([{ key: 'all', name: 'All Providers' }, ...Object.values(storeState.providers)])),
+    searchTags: computed(() => searchQueryHelper.setFilters(collectorPageState.searchFilters).queryTags),
     items: computed(() => {
-        const plugins = state.plugins;
-        return state.collectors?.map((d) => ({
+        const plugins = storeState.plugins;
+        return collectorPageState.collectors?.map((d) => ({
             collectorId: d.collector_id,
             name: d.name,
             pluginName: plugins[d.plugin_info.plugin_id]?.label,
@@ -85,7 +105,7 @@ const state = reactive({
                 name: ASSET_INVENTORY_ROUTE.COLLECTOR.DETAIL._NAME,
                 param: { id: d.collector_id },
                 query: {
-                    filters: detailLinkQueryHelper.setFilters([
+                    filters: searchQueryHelper.setFilters([
                         {
                             k: COLLECTOR_QUERY_HELPER_SET.COLLECTOR_ID,
                             v: d.collector_id,
@@ -98,45 +118,6 @@ const state = reactive({
     }),
 });
 
-const handlerState = reactive({
-    excelFields: [
-        { key: 'name', name: 'Name' },
-        { key: 'state', name: 'State' },
-        { key: 'plugin_info.plugin_id', name: 'Plugin' },
-        { key: 'plugin_info.version', name: 'Version' },
-        { key: 'last_collected_at', name: 'Last Collected', type: 'datetime' },
-    ],
-});
-
-/* Components */
-const initCollectorList = async () => {
-    state.loading = true;
-    try {
-        await getCollectorList();
-        if (Object.keys(state.items).length > 0) {
-            await cloudCollectorPageStore.setCollectorList(state.items);
-        }
-    } catch (e) {
-        ErrorHandler.handleError(e);
-        state.totalCount = 0;
-        await cloudCollectorPageStore.setCollectorList([]);
-    } finally {
-        state.loading = false;
-    }
-};
-const filterByProvider = async () => {
-    await getCollectorList();
-    await cloudCollectorPageStore.setFilteredCollectorList(state.items);
-};
-const handleExportExcel = async () => {
-    await store.dispatch('file/downloadExcel', {
-        url: '/inventory/collector/list',
-        param: { query: collectorApiQueryHelper.data },
-        fields: handlerState.excelFields,
-        file_name_prefix: FILE_NAME_PREFIX.collector,
-    });
-};
-
 /* Query Helper */
 const collectorApiQueryHelper = new ApiQueryHelper()
     .setOnly(
@@ -148,31 +129,39 @@ const collectorApiQueryHelper = new ApiQueryHelper()
         COLLECTOR_QUERY_HELPER_SET.PLUGIN_INFO,
         COLLECTOR_QUERY_HELPER_SET.STATE,
     )
-    .setPage(state.pageStart, state.pageLimit)
-    .setSort(state.sortBy, true);
+    .setPage(collectorPageState.pageStart, collectorPageState.pageLimit)
+    .setSort(collectorPageState.sortBy, true);
 
-/* API */
-const getCollectorList = async () => {
-    collectorApiQueryHelper.setFilters(cloudCollectorPageStore.allFilters);
+/* Components */
+const initCollectorList = async () => {
+    collectorApiQueryHelper.setFilters(collectorPageStore.allFilters);
     try {
-        const res = await SpaceConnector.client.inventory.collector.list({
-            query: collectorApiQueryHelper.data,
-        });
-        state.collectors = res.results;
-        state.totalCount = res.total_count;
+        await collectorPageStore.getCollectorList(collectorApiQueryHelper.data);
+        if (Object.keys(state.items).length > 0) {
+            await collectorPageStore.setCollectorList(state.items);
+        }
     } catch (e) {
         ErrorHandler.handleError(e);
+        await collectorPageStore.$reset();
     }
+};
+const handleChangeToolbox = async (options) => {
+    setApiQueryWithToolboxOptions(collectorApiQueryHelper, options);
+    await initCollectorList();
 };
 
 /* Watcher */
-watch(() => cloudCollectorPageState.selectedProvider, async () => {
-    await filterByProvider();
+watch(() => collectorPageState.selectedProvider, async () => {
+    await initCollectorList();
+});
+
+/* Unmounted */
+onUnmounted(() => {
+    collectorPageStore.$reset();
 });
 
 /* INIT */
 (async () => {
-    await cloudCollectorPageStore.initState();
     await Promise.allSettled([
         store.dispatch('reference/plugin/load'),
         store.dispatch('reference/provider/load'),
