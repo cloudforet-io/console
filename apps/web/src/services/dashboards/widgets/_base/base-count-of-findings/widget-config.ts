ee1e73dfc875e56eaedc6dbacc258fbbd8a0f7c2
Dahyun Yu <yuda@megazone.com>
2023-05-26 11:35:23 +0900

    feat: split a widget into 2 widgets(pass and fail & fail) (#1168)
    * feat: split a widget into 2 widgets(pass and fail & fail)
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * feat: change the widget to a base widget
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ apps/web/src/services/dashboards/widgets/_base/base-count-of-findings/widget-config.ts
@@ -0,0 +1,24 @@
+import type { WidgetConfig } from '@/services/dashboards/widgets/_configs/config';
+import { ASSET_GROUP_BY, GRANULARITY } from '@/services/dashboards/widgets/_configs/config';
+
+const baseCountOfFindingsWidgetConfig: WidgetConfig = {
+    widget_config_id: 'baseCountOfFindings',
+    widget_component: () => ({
+        component: import('@/services/dashboards/widgets/_base/base-count-of-findings/BaseCountOfFindingsWidget.vue'),
+    }),
+    scopes: ['WORKSPACE'],
+    theme: {
+        inherit: false,
+    },
+    sizes: ['lg', 'full'],
+    options: {
+        granularity: GRANULARITY.ACCUMULATED,
+        asset_group_by: ASSET_GROUP_BY.REGION,
+        pagination_options: {
+            enabled: true,
+            page_size: 8,
+        },
+    },
+};
+
+export default baseCountOfFindingsWidgetConfig;
