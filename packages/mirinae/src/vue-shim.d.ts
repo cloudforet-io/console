b8ba9e8f0c95419a00dcc93a75f56898b47e2519
Yongtae Park <samuel.park@megazone.com>
2023-05-26 16:14:31 +0900

    build: remove unnecessary code
    Signed-off-by: Yongtae Park <samuel.park@megazone.com>

--- packages/mirinae/src/vue-shim.d.ts
+++ packages/mirinae/src/vue-shim.d.ts
@@ -3,11 +3,9 @@ declare module '*.vue' {
 
     import { VTooltip } from 'v-tooltip';
     import { Fragment } from 'vue-fragment';
-    import VueI18n from 'vue-i18n';
     import Notifications from 'vue-notification';
     import SvgIcon from 'vue-svgicon';
 
-    Vue.use(VueI18n);
     Vue.use(Fragment.Plugin);
     Vue.use(Notifications);
     Vue.use(SvgIcon);
