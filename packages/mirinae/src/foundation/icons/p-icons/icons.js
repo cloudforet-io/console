b20ac0b97d5a1aa2a8ac129fbe6d382678726bbe
Yongtae Park <samuel.park@megazone.com>
2023-06-02 16:57:05 +0900

    build: remove icon file
    Signed-off-by: Yongtae Park <samuel.park@megazone.com>

---
+++ packages/mirinae/src/foundation/icons/p-icons/icons.js
@@ -0,0 +1,322 @@
+import ic_alarmClock from '../icon-assets/ic_alarm-clock.svg';
+import ic_arrowDownBold from '../icon-assets/ic_arrow-down-bold.svg';
+import ic_arrowDown from '../icon-assets/ic_arrow-down.svg';
+import ic_arrowLeft from '../icon-assets/ic_arrow-left.svg';
+import ic_arrowUpBoldAlt from '../icon-assets/ic_arrow-up-bold-alt.svg';
+import ic_arrowUpBold from '../icon-assets/ic_arrow-up-bold.svg';
+import ic_arrowUp from '../icon-assets/ic_arrow-up.svg';
+import ic_arrowsCollapseAll from '../icon-assets/ic_arrows-collapse-all.svg';
+import ic_arrowsCollapseVertical from '../icon-assets/ic_arrows-collapse-vertical.svg';
+import ic_arrowsExpandAll from '../icon-assets/ic_arrows-expand-all.svg';
+import ic_arrowsExpandVertical from '../icon-assets/ic_arrows-expand-vertical.svg';
+import ic_arrowsUturnLeft from '../icon-assets/ic_arrows-uturn-left.svg';
+import ic_arrowsUturnRight from '../icon-assets/ic_arrows-uturn-right.svg';
+import ic_calendar from '../icon-assets/ic_calendar.svg';
+import ic_caretDownFilledAlt from '../icon-assets/ic_caret-down-filled-alt.svg';
+import ic_caretDownFilled from '../icon-assets/ic_caret-down-filled.svg';
+import ic_caretDown from '../icon-assets/ic_caret-down.svg';
+import ic_caretRight from '../icon-assets/ic_caret-right.svg';
+import ic_caretUpFilledAlt from '../icon-assets/ic_caret-up-filled-alt.svg';
+import ic_caretUpFilled from '../icon-assets/ic_caret-up-filled.svg';
+import ic_caretUp from '../icon-assets/ic_caret-up.svg';
+import ic_checkCircle from '../icon-assets/ic_check-circle.svg';
+import ic_check from '../icon-assets/ic_check.svg';
+import ic_checkboxCircleSelected from '../icon-assets/ic_checkbox-circle-selected.svg';
+import ic_checkboxDisabledSelected from '../icon-assets/ic_checkbox-disabled-selected.svg';
+import ic_checkboxDisabled from '../icon-assets/ic_checkbox-disabled.svg';
+import ic_checkboxPeacockSelected from '../icon-assets/ic_checkbox-peacock-selected.svg';
+import ic_checkboxSelected from '../icon-assets/ic_checkbox-selected.svg';
+import ic_checkbox from '../icon-assets/ic_checkbox.svg';
+import ic_chevronDown from '../icon-assets/ic_chevron-down.svg';
+import ic_chevronLeftThin from '../icon-assets/ic_chevron-left-thin.svg';
+import ic_chevronLeft from '../icon-assets/ic_chevron-left.svg';
+import ic_chevronRightCircleFilled from '../icon-assets/ic_chevron-right-circle-filled.svg';
+import ic_chevronRightCircle from '../icon-assets/ic_chevron-right-circle.svg';
+import ic_chevronRight from '../icon-assets/ic_chevron-right.svg';
+import ic_chevronSmallDown from '../icon-assets/ic_chevron-small-down.svg';
+import ic_chevronSmallLeft from '../icon-assets/ic_chevron-small-left.svg';
+import ic_chevronSmallRight from '../icon-assets/ic_chevron-small-right.svg';
+import ic_chevronSmallUp from '../icon-assets/ic_chevron-small-up.svg';
+import ic_chevronUp from '../icon-assets/ic_chevron-up.svg';
+import ic_clock from '../icon-assets/ic_clock.svg';
+import ic_close from '../icon-assets/ic_close.svg';
+import ic_cloudFilled from '../icon-assets/ic_cloud-filled.svg';
+import ic_coinFilled from '../icon-assets/ic_coin-filled.svg';
+import ic_copyFilled from '../icon-assets/ic_copy-filled.svg';
+import ic_copy from '../icon-assets/ic_copy.svg';
+import ic_dashboardCustomize from '../icon-assets/ic_dashboard-customize.svg';
+import ic_dashboardTemplate_alertManager from '../icon-assets/ic_dashboard-template_alert-manager.svg';
+import ic_dashboardTemplate_blank from '../icon-assets/ic_dashboard-template_blank.svg';
+import ic_dashboardTemplate_cdnTrafficCost from '../icon-assets/ic_dashboard-template_cdn-traffic-cost.svg';
+import ic_dashboardTemplate_cloudAssetOverview from '../icon-assets/ic_dashboard-template_cloud-asset-overview.svg';
+import ic_dashboardTemplate_monthlyCostSummary from '../icon-assets/ic_dashboard-template_monthly-cost-summary.svg';
+import ic_dashboardTemplate_others from '../icon-assets/ic_dashboard-template_others.svg';
+import ic_dashboardTemplate_projectOverview from '../icon-assets/ic_dashboard-template_project-overview.svg';
+import ic_delete from '../icon-assets/ic_delete.svg';
+import ic_documentFilled from '../icon-assets/ic_document-filled.svg';
+import ic_dot from '../icon-assets/ic_dot.svg';
+import ic_dots4Square from '../icon-assets/ic_dots-4-square.svg';
+import ic_doubleLineDragHandle from '../icon-assets/ic_double-line-drag-handle.svg';
+import ic_download from '../icon-assets/ic_download.svg';
+import ic_dragHandleAlt from '../icon-assets/ic_drag-handle-alt.svg';
+import ic_dragHandle from '../icon-assets/ic_drag-handle.svg';
+import ic_duplicate from '../icon-assets/ic_duplicate.svg';
+import ic_editText from '../icon-assets/ic_edit-text.svg';
+import ic_edit from '../icon-assets/ic_edit.svg';
+import ic_editorCode from '../icon-assets/ic_editor-code.svg';
+import ic_ellipsisHorizontal from '../icon-assets/ic_ellipsis-horizontal.svg';
+import ic_envelopeFilled from '../icon-assets/ic_envelope-filled.svg';
+import ic_errorFilled from '../icon-assets/ic_error-filled.svg';
+import ic_excel from '../icon-assets/ic_excel.svg';
+import ic_externalLink from '../icon-assets/ic_external-link.svg';
+import ic_eye from '../icon-assets/ic_eye.svg';
+import ic_faceFrown from '../icon-assets/ic_face-frown.svg';
+import ic_faceSmile from '../icon-assets/ic_face-smile.svg';
+import ic_favoriteFilled from '../icon-assets/ic_favorite-filled.svg';
+import ic_favorite from '../icon-assets/ic_favorite.svg';
+import ic_folderFilled from '../icon-assets/ic_folder-filled.svg';
+import ic_folderPlus from '../icon-assets/ic_folder-plus.svg';
+import ic_gearFilled from '../icon-assets/ic_gear-filled.svg';
+import ic_globeFilled from '../icon-assets/ic_globe-filled.svg';
+import ic_gnb_bellUnread from '../icon-assets/ic_gnb_bell-unread.svg';
+import ic_gnb_bell from '../icon-assets/ic_gnb_bell.svg';
+import ic_gnb_menu from '../icon-assets/ic_gnb_menu.svg';
+import ic_gnb_recentFavorite from '../icon-assets/ic_gnb_recent-favorite.svg';
+import ic_gnb_search from '../icon-assets/ic_gnb_search.svg';
+import ic_homeFilled from '../icon-assets/ic_home-filled.svg';
+import ic_horizontalRule from '../icon-assets/ic_horizontal-rule.svg';
+import ic_image from '../icon-assets/ic_image.svg';
+import ic_infoCircle from '../icon-assets/ic_info-circle.svg';
+import ic_inlineCode from '../icon-assets/ic_inline-code.svg';
+import ic_limitFilled from '../icon-assets/ic_limit-filled.svg';
+import ic_link from '../icon-assets/ic_link.svg';
+import ic_listBulleted2 from '../icon-assets/ic_list-bulleted-2.svg';
+import ic_listBulleted3 from '../icon-assets/ic_list-bulleted-3.svg';
+import ic_listCard from '../icon-assets/ic_list-card.svg';
+import ic_listNumbered from '../icon-assets/ic_list-numbered.svg';
+import ic_lockFilled from '../icon-assets/ic_lock-filled.svg';
+import ic_megaphoneFilled from '../icon-assets/ic_megaphone-filled.svg';
+import ic_minus from '../icon-assets/ic_minus.svg';
+import ic_moneyBag from '../icon-assets/ic_money-bag.svg';
+import ic_noImage from '../icon-assets/ic_no-image.svg';
+import ic_paperAirplane from '../icon-assets/ic_paper-airplane.svg';
+import ic_performanceFilled from '../icon-assets/ic_performance-filled.svg';
+import ic_pinFilled from '../icon-assets/ic_pin-filled.svg';
+import ic_plugFilled from '../icon-assets/ic_plug-filled.svg';
+import ic_plus from '../icon-assets/ic_plus.svg';
+import ic_plus_bold from '../icon-assets/ic_plus_bold.svg';
+import ic_plus_square from '../icon-assets/ic_plus_square.svg';
+import ic_plus_thin from '../icon-assets/ic_plus_thin.svg';
+import ic_questionMarkCircleFilled from '../icon-assets/ic_question-mark-circle-filled.svg';
+import ic_questionMarkCircle from '../icon-assets/ic_question-mark-circle.svg';
+import ic_quotes from '../icon-assets/ic_quotes.svg';
+import ic_radioDisabled from '../icon-assets/ic_radio-disabled.svg';
+import ic_radioSelected from '../icon-assets/ic_radio-selected.svg';
+import ic_radio from '../icon-assets/ic_radio.svg';
+import ic_refresh from '../icon-assets/ic_refresh.svg';
+import ic_renew from '../icon-assets/ic_renew.svg';
+import ic_repeat from '../icon-assets/ic_repeat.svg';
+import ic_resource_hexagon from '../icon-assets/ic_resource_hexagon.svg';
+import ic_search from '../icon-assets/ic_search.svg';
+import ic_service_administration from '../icon-assets/ic_service_administration.svg';
+import ic_service_alertManager from '../icon-assets/ic_service_alert-manager.svg';
+import ic_service_assetInventory from '../icon-assets/ic_service_asset-inventory.svg';
+import ic_service_automation from '../icon-assets/ic_service_automation.svg';
+import ic_service_costExplorer from '../icon-assets/ic_service_cost-explorer.svg';
+import ic_service_dashboard from '../icon-assets/ic_service_dashboard.svg';
+import ic_service_home from '../icon-assets/ic_service_home.svg';
+import ic_service_info from '../icon-assets/ic_service_info.svg';
+import ic_service_myPage from '../icon-assets/ic_service_my-page.svg';
+import ic_service_plugin from '../icon-assets/ic_service_plugin.svg';
+import ic_service_project from '../icon-assets/ic_service_project.svg';
+import ic_service_workflow from '../icon-assets/ic_service_workflow.svg';
+import ic_settingsFilled from '../icon-assets/ic_settings-filled.svg';
+import ic_settings from '../icon-assets/ic_settings.svg';
+import ic_sortingAscending from '../icon-assets/ic_sorting-ascending.svg';
+import ic_sortingDescending from '../icon-assets/ic_sorting-descending.svg';
+import ic_spannerFilled from '../icon-assets/ic_spanner-filled.svg';
+import ic_subdirectoryArrowRight from '../icon-assets/ic_subdirectory-arrow-right.svg';
+import ic_textAlignCenter from '../icon-assets/ic_text-align-center.svg';
+import ic_textAlignJustify from '../icon-assets/ic_text-align-justify.svg';
+import ic_textAlignLeft from '../icon-assets/ic_text-align-left.svg';
+import ic_textAlignRight from '../icon-assets/ic_text-align-right.svg';
+import ic_textBold from '../icon-assets/ic_text-bold.svg';
+import ic_textClearFormat from '../icon-assets/ic_text-clear-format.svg';
+import ic_textColor from '../icon-assets/ic_text-color.svg';
+import ic_textItalic from '../icon-assets/ic_text-italic.svg';
+import ic_textStrikethrough from '../icon-assets/ic_text-strikethrough.svg';
+import ic_textUnderline from '../icon-assets/ic_text-underline.svg';
+import ic_users from '../icon-assets/ic_users.svg';
+import ic_verified from '../icon-assets/ic_verified.svg';
+import ic_warningFilled from '../icon-assets/ic_warning-filled.svg';
+import ic_webhook from '../icon-assets/ic_webhook.svg';
+import img_avatar_admin from '../icon-assets/img_avatar_admin.svg';
+import img_avatar_projectAdmin from '../icon-assets/img_avatar_project-admin.svg';
+import img_avatar_rootAccount from '../icon-assets/img_avatar_root-account.svg';
+import img_avatar_user from '../icon-assets/img_avatar_user.svg';
+import img_resource_default from '../icon-assets/img_resource_default.svg';
+import img_resource_server from '../icon-assets/img_resource_server.svg';
+import logo_google from '../icon-assets/logo_google.svg';
+
+
+export default {
+    'ic_alarm-clock': ic_alarmClock,
+    'ic_arrow-down-bold': ic_arrowDownBold,
+    'ic_arrow-down': ic_arrowDown,
+    'ic_arrow-left': ic_arrowLeft,
+    'ic_arrow-up-bold-alt': ic_arrowUpBoldAlt,
+    'ic_arrow-up-bold': ic_arrowUpBold,
+    'ic_arrow-up': ic_arrowUp,
+    'ic_arrows-collapse-all': ic_arrowsCollapseAll,
+    'ic_arrows-collapse-vertical': ic_arrowsCollapseVertical,
+    'ic_arrows-expand-all': ic_arrowsExpandAll,
+    'ic_arrows-expand-vertical': ic_arrowsExpandVertical,
+    'ic_arrows-uturn-left': ic_arrowsUturnLeft,
+    'ic_arrows-uturn-right': ic_arrowsUturnRight,
+    ic_calendar,
+    'ic_caret-down-filled-alt': ic_caretDownFilledAlt,
+    'ic_caret-down-filled': ic_caretDownFilled,
+    'ic_caret-down': ic_caretDown,
+    'ic_caret-right': ic_caretRight,
+    'ic_caret-up-filled-alt': ic_caretUpFilledAlt,
+    'ic_caret-up-filled': ic_caretUpFilled,
+    'ic_caret-up': ic_caretUp,
+    'ic_check-circle': ic_checkCircle,
+    ic_check,
+    'ic_checkbox-circle-selected': ic_checkboxCircleSelected,
+    'ic_checkbox-disabled-selected': ic_checkboxDisabledSelected,
+    'ic_checkbox-disabled': ic_checkboxDisabled,
+    'ic_checkbox-peacock-selected': ic_checkboxPeacockSelected,
+    'ic_checkbox-selected': ic_checkboxSelected,
+    ic_checkbox,
+    'ic_chevron-down': ic_chevronDown,
+    'ic_chevron-left-thin': ic_chevronLeftThin,
+    'ic_chevron-left': ic_chevronLeft,
+    'ic_chevron-right-circle-filled': ic_chevronRightCircleFilled,
+    'ic_chevron-right-circle': ic_chevronRightCircle,
+    'ic_chevron-right': ic_chevronRight,
+    'ic_chevron-small-down': ic_chevronSmallDown,
+    'ic_chevron-small-left': ic_chevronSmallLeft,
+    'ic_chevron-small-right': ic_chevronSmallRight,
+    'ic_chevron-small-up': ic_chevronSmallUp,
+    'ic_chevron-up': ic_chevronUp,
+    ic_clock,
+    ic_close,
+    'ic_cloud-filled': ic_cloudFilled,
+    'ic_coin-filled': ic_coinFilled,
+    'ic_copy-filled': ic_copyFilled,
+    ic_copy,
+    'ic_dashboard-customize': ic_dashboardCustomize,
+    'ic_dashboard-template_alert-manager': ic_dashboardTemplate_alertManager,
+    'ic_dashboard-template_blank': ic_dashboardTemplate_blank,
+    'ic_dashboard-template_cdn-traffic-cost': ic_dashboardTemplate_cdnTrafficCost,
+    'ic_dashboard-template_cloud-asset-overview': ic_dashboardTemplate_cloudAssetOverview,
+    'ic_dashboard-template_monthly-cost-summary': ic_dashboardTemplate_monthlyCostSummary,
+    'ic_dashboard-template_others': ic_dashboardTemplate_others,
+    'ic_dashboard-template_project-overview': ic_dashboardTemplate_projectOverview,
+    ic_delete,
+    'ic_document-filled': ic_documentFilled,
+    ic_dot,
+    'ic_dots-4-square': ic_dots4Square,
+    'ic_double-line-drag-handle': ic_doubleLineDragHandle,
+    ic_download,
+    'ic_drag-handle-alt': ic_dragHandleAlt,
+    'ic_drag-handle': ic_dragHandle,
+    ic_duplicate,
+    'ic_edit-text': ic_editText,
+    ic_edit,
+    'ic_editor-code': ic_editorCode,
+    'ic_ellipsis-horizontal': ic_ellipsisHorizontal,
+    'ic_envelope-filled': ic_envelopeFilled,
+    'ic_error-filled': ic_errorFilled,
+    ic_excel,
+    'ic_external-link': ic_externalLink,
+    ic_eye,
+    'ic_face-frown': ic_faceFrown,
+    'ic_face-smile': ic_faceSmile,
+    'ic_favorite-filled': ic_favoriteFilled,
+    ic_favorite,
+    'ic_folder-filled': ic_folderFilled,
+    'ic_folder-plus': ic_folderPlus,
+    'ic_gear-filled': ic_gearFilled,
+    'ic_globe-filled': ic_globeFilled,
+    'ic_gnb_bell-unread': ic_gnb_bellUnread,
+    ic_gnb_bell,
+    ic_gnb_menu,
+    'ic_gnb_recent-favorite': ic_gnb_recentFavorite,
+    ic_gnb_search,
+    'ic_home-filled': ic_homeFilled,
+    'ic_horizontal-rule': ic_horizontalRule,
+    ic_image,
+    'ic_info-circle': ic_infoCircle,
+    'ic_inline-code': ic_inlineCode,
+    'ic_limit-filled': ic_limitFilled,
+    ic_link,
+    'ic_list-bulleted-2': ic_listBulleted2,
+    'ic_list-bulleted-3': ic_listBulleted3,
+    'ic_list-card': ic_listCard,
+    'ic_list-numbered': ic_listNumbered,
+    'ic_lock-filled': ic_lockFilled,
+    'ic_megaphone-filled': ic_megaphoneFilled,
+    ic_minus,
+    'ic_money-bag': ic_moneyBag,
+    'ic_no-image': ic_noImage,
+    'ic_paper-airplane': ic_paperAirplane,
+    'ic_performance-filled': ic_performanceFilled,
+    'ic_pin-filled': ic_pinFilled,
+    'ic_plug-filled': ic_plugFilled,
+    ic_plus,
+    ic_plus_bold,
+    ic_plus_square,
+    ic_plus_thin,
+    'ic_question-mark-circle-filled': ic_questionMarkCircleFilled,
+    'ic_question-mark-circle': ic_questionMarkCircle,
+    ic_quotes,
+    'ic_radio-disabled': ic_radioDisabled,
+    'ic_radio-selected': ic_radioSelected,
+    ic_radio,
+    ic_refresh,
+    ic_renew,
+    ic_repeat,
+    ic_resource_hexagon,
+    ic_search,
+    ic_service_administration,
+    'ic_service_alert-manager': ic_service_alertManager,
+    'ic_service_asset-inventory': ic_service_assetInventory,
+    ic_service_automation,
+    'ic_service_cost-explorer': ic_service_costExplorer,
+    ic_service_dashboard,
+    ic_service_home,
+    ic_service_info,
+    'ic_service_my-page': ic_service_myPage,
+    ic_service_plugin,
+    ic_service_project,
+    ic_service_workflow,
+    'ic_settings-filled': ic_settingsFilled,
+    ic_settings,
+    'ic_sorting-ascending': ic_sortingAscending,
+    'ic_sorting-descending': ic_sortingDescending,
+    'ic_spanner-filled': ic_spannerFilled,
+    'ic_subdirectory-arrow-right': ic_subdirectoryArrowRight,
+    'ic_text-align-center': ic_textAlignCenter,
+    'ic_text-align-justify': ic_textAlignJustify,
+    'ic_text-align-left': ic_textAlignLeft,
+    'ic_text-align-right': ic_textAlignRight,
+    'ic_text-bold': ic_textBold,
+    'ic_text-clear-format': ic_textClearFormat,
+    'ic_text-color': ic_textColor,
+    'ic_text-italic': ic_textItalic,
+    'ic_text-strikethrough': ic_textStrikethrough,
+    'ic_text-underline': ic_textUnderline,
+    ic_users,
+    ic_verified,
+    'ic_warning-filled': ic_warningFilled,
+    ic_webhook,
+    img_avatar_admin,
+    'img_avatar_project-admin': img_avatar_projectAdmin,
+    'img_avatar_root-account': img_avatar_rootAccount,
+    img_avatar_user,
+    img_resource_default,
+    img_resource_server,
+    logo_google,
+};
