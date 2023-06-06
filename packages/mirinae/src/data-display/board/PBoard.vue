2e7092f7e8b29ac298ce91d52c7e9d85ccfabeac
Yongtae Park <samuel.park@megazone.com>
2023-06-06 23:56:24 +0900

    build: update p-board component

--- packages/mirinae/src/data-display/board/PBoard.vue
+++ packages/mirinae/src/data-display/board/PBoard.vue
@@ -1,21 +1,22 @@
 <template>
     <div class="p-board"
          :class="[ [`${styleType}-style`] ]"
-         :style="styleVariableByOptions"
+         :style="state.styleVariableByOptions"
     >
-        <template v-for="(board, index) in boardList">
-            <p-board-item :key="`board-${board.value}-${index}`"
+        <template v-for="(board, index) in state.boardList"
+                  :key="`board-${board.value}-${index}`"
+        >
+            <p-board-item v-model:selected="selectedItem"
                           class="p-board-item"
                           :class="{
                               'first-list-item': index === 0,
-                              'last-list-item': index === boardList.length - 1,
+                              'last-list-item': index === state.boardList.length - 1,
                               'selectable': selectable
                           }"
                           :value="board.value"
                           :left-icon="board.leftIcon"
                           :icon-button-sets="board.iconButtonSets"
                           :rounded="board.rounded"
-                          :selected.sync="selectedItem"
                           @click.stop="handleClickBoardItem(board, index)"
             >
                 <template #left-content>
@@ -43,76 +44,72 @@
     </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import {
-    computed, defineComponent, reactive, toRefs,
+    computed, reactive,
 } from 'vue';
-import type { PropType, SetupContext } from 'vue';
+import type { PropType } from 'vue';
 
 
 import PBoardItem from '@/data-display/board-item/PBoardItem.vue';
 import { BOARD_STYLE_TYPE } from '@/data-display/board/type';
-import type { BoardProps, BoardSet, StyleOptions } from '@/data-display/board/type';
+import type { BoardSet, StyleOptions } from '@/data-display/board/type';
 
 
-export default defineComponent<BoardProps>({
-    name: 'PBoard',
-    components: { PBoardItem },
-    props: {
-        styleType: {
-            type: String,
-            default: BOARD_STYLE_TYPE.list,
-            validator(styleType: any) {
-                return Object.values(BOARD_STYLE_TYPE).includes(styleType);
-            },
-        },
-        styleOptions: {
-            type: Object as PropType<StyleOptions>,
-            default: undefined,
-        },
-        boardSets: {
-            type: Array as PropType<BoardSet[]>,
-            default: () => [],
-        },
-        pageLimit: {
-            type: Number,
-            default: 10,
-        },
-        selectable: {
-            type: Boolean,
-            default: false,
-        },
-        selectedItem: {
-            type: String,
-            default: undefined,
+/* HACK: This defineProps is Bandage solution.
+    Because props validation can't use with props generic type.
+    So, if I find the solution, I will change this code.
+* */
+const props = defineProps({
+    styleType: {
+        type: String,
+        default: BOARD_STYLE_TYPE.list,
+        validator(styleType: any) {
+            return Object.values(BOARD_STYLE_TYPE).includes(styleType);
         },
     },
-    setup(props, { emit }: SetupContext) {
-        const state = reactive({
-            boardList: computed<BoardSet[]>(() => props.boardSets),
-            styleVariableByOptions: computed(() => {
-                const styles = {} as {[prop: string]: any};
-                if (!props.styleOptions) return styles;
-                if (BOARD_STYLE_TYPE.cards) {
-                    if (props.styleOptions.column) {
-                        styles['--columns'] = props.styleOptions.column;
-                    }
-                }
-                return styles;
-            }),
-        });
-
-
-        const handleClickBoardItem = (item: BoardSet, index: number) => {
-            emit('item-click', item, index);
-        };
-
-        return {
-            ...toRefs(state),
-            handleClickBoardItem,
-        };
+    styleOptions: {
+        type: Object as PropType<StyleOptions>,
+        default: undefined,
+    },
+    boardSets: {
+        type: Array as PropType<BoardSet[]>,
+        default: () => [],
+    },
+    pageLimit: {
+        type: Number,
+        default: 10,
+    },
+    selectable: {
+        type: Boolean,
+        default: false,
+    },
+    selectedItem: {
+        type: String,
+        default: undefined,
     },
 });
+
+const emit = defineEmits<{(e: 'item-click', item: BoardSet, index: number): void;}>();
+
+const state = reactive({
+    boardList: computed<BoardSet[]>(() => props.boardSets),
+    styleVariableByOptions: computed(() => {
+        const styles = {} as {[prop: string]: any};
+        if (!props.styleOptions) return styles;
+        if (BOARD_STYLE_TYPE.cards) {
+            if (props.styleOptions.column) {
+                styles['--columns'] = props.styleOptions.column;
+            }
+        }
+        return styles;
+    }),
+});
+
+const handleClickBoardItem = (item: BoardSet, index: number) => {
+    emit('item-click', item, index);
+};
+
 </script>
 
 <style lang="postcss">
