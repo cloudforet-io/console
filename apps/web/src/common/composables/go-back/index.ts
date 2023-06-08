f77ff95de1790d5d42daa0874dd69e7670539586
Wanjin Noh <wanjin@megazone.com>
2023-06-08 18:17:12 +0900

    fix: change to push
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/common/composables/go-back/index.ts
+++ apps/web/src/common/composables/go-back/index.ts
@@ -1,17 +1,20 @@
-import type { RawLocation } from 'vue-router';
+import type { Location } from 'vue-router';
 
 import { SpaceRouter } from '@/router';
 
-export const useGoBack = (mainRoute: RawLocation) => { // RawLocation
+export const useGoBack = (mainRoute: Location) => {
     let pathFrom;
-    const setPathFrom = (path: any) => {
+    const setPathFrom = (path: Location) => {
         pathFrom = path;
     };
 
     const handleClickBackButton = () => {
-        if (pathFrom?.name) SpaceRouter.router.replace(pathFrom);
-        else {
-            SpaceRouter.router.replace(mainRoute);
+        if (!pathFrom?.name) { // in case of direct access from the other site, go to the main page
+            SpaceRouter.router.push(mainRoute);
+        } else if (pathFrom.name === mainRoute.name) { // in case of access from the service main page in the same site, go to the previous page
+            SpaceRouter.router.push(pathFrom);
+        } else { // in case of access from the other page in the same site, go to the main page
+            SpaceRouter.router.push(mainRoute);
         }
     };
 
