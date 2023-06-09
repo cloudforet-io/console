6233f6949df99ef3a8bbc4cc9cb95e4db0125762
Yongtae Park <samuel.park@megazone.com>
2023-06-09 18:44:15 +0900

    build: update p-toolbox component
    Signed-off-by: Yongtae Park <samuel.park@megazone.com>

--- packages/mirinae/src/navigation/toolbox/PToolbox.vue
+++ packages/mirinae/src/navigation/toolbox/PToolbox.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-toolbox">
         <div class="toolbox-inner">
-            <div v-if="$scopedSlots['left-area']"
+            <div v-if="slots['left-area']"
                  class="left-area-wrapper"
             >
                 <slot name="left-area" />
@@ -25,15 +25,15 @@
                      class="tool"
                 >
                     <p-text-pagination :this-page="proxyState.thisPage"
-                                       :all-page="allPage"
-                                       @pageChange="onChangeThisPage"
+                                       :all-page="state.allPage"
+                                       @page-change="onChangeThisPage"
                     />
                 </div>
                 <div v-if="pageSizeChangeable"
                      class="tool"
                 >
                     <p-select-dropdown class="dropdown-list"
-                                       :items="pageMenu"
+                                       :items="state.pageMenu"
                                        @select="onChangePageSize"
                     >
                         {{ proxyState.pageSize }}
@@ -47,7 +47,7 @@
                                        :sort-by="sortBy"
                                        @select="onChangeSortBy"
                     >
-                        {{ selectedSortBy }}
+                        {{ state.selectedSortBy }}
                     </p-select-dropdown>
                 </div>
                 <div class="right-tool-group">
@@ -55,30 +55,30 @@
                          class="tool"
                     >
                         <p-icon-button name="ic_excel"
-                                       @click="$emit('export', $event)"
+                                       @click="handleExport"
                         />
                     </div>
                     <div v-if="settingsVisible"
                          class="tool"
                     >
                         <p-icon-button name="ic_settings-filled"
-                                       @click="$emit('click-settings', $event)"
+                                       @click="handleClickSettings"
                         />
                     </div>
                     <div v-if="refreshable"
                          class="tool"
                     >
                         <p-icon-button name="ic_refresh"
-                                       @click="$emit('refresh', $event)"
+                                       @click="handleRefresh"
                         />
                     </div>
                 </div>
             </div>
             <div class="filters-wrapper">
                 <p-query-search-tags v-show="searchable && filtersVisible && searchType === SEARCH_TYPES.query"
-                                     :tags.sync="proxyState.queryTags"
+                                     v-model:tags="proxyState.queryTags"
                                      :timezone="timezone"
-                                     :converter="queryTagConverter"
+                                     :converter="state.queryTagConverter"
                                      @change="onQueryTagsChange"
                 />
             </div>
@@ -86,12 +86,12 @@
     </div>
 </template>
 
-<script lang="ts">
-import {
-    computed, defineComponent, nextTick, reactive, toRefs,
-} from 'vue';
-
+<script setup lang="ts">
 import { groupBy } from 'lodash';
+import type { PropType } from 'vue';
+import {
+    computed, nextTick, reactive, useSlots,
+} from 'vue';
 
 import { useProxyValue } from '@/hooks/proxy-state';
 import PIconButton from '@/inputs/buttons/icon-button/PIconButton.vue';
@@ -101,217 +101,209 @@ import { defaultConverter } from '@/inputs/search/query-search-tags/helper';
 import PQuerySearchTags from '@/inputs/search/query-search-tags/PQuerySearchTags.vue';
 import type { QueryTag } from '@/inputs/search/query-search-tags/type';
 import PQuerySearch from '@/inputs/search/query-search/PQuerySearch.vue';
-import type { QueryItem, ValueSet } from '@/inputs/search/query-search/type';
+import type {
+    KeyItemSet, QueryItem, ValueSet, ValueHandlerMap,
+} from '@/inputs/search/query-search/type';
 import PSearch from '@/inputs/search/search/PSearch.vue';
 import PTextPagination from '@/navigation/pagination/text-pagination/PTextPagination.vue';
 import { SEARCH_TYPES } from '@/navigation/toolbox/config';
-import type { ToolboxOptions, ToolboxProps } from '@/navigation/toolbox/type';
+import type { ToolboxOptions } from '@/navigation/toolbox/type';
 
-
-export default defineComponent<ToolboxProps>({
-    name: 'PToolbox',
-    components: {
-        PSelectDropdown,
-        PQuerySearchTags,
-        PSearch,
-        PQuerySearch,
-        PTextPagination,
-        PIconButton,
+const props = defineProps({
+    paginationVisible: {
+        type: Boolean,
+        default: true,
     },
-    props: {
-        paginationVisible: {
-            type: Boolean,
-            default: true,
-        },
-        pageSizeChangeable: {
-            type: Boolean,
-            default: true,
-        },
-        settingsVisible: {
-            type: Boolean,
-            default: false,
-        },
-        sortable: {
-            type: Boolean,
-            default: false,
-        },
-        exportable: {
-            type: Boolean,
-            default: false,
-        },
-        refreshable: {
-            type: Boolean,
-            default: true,
-        },
-        searchable: {
-            type: Boolean,
-            default: true,
-        },
-        filtersVisible: {
-            type: Boolean,
-            default: true,
-        },
-        searchType: {
-            type: String,
-            default: SEARCH_TYPES.plain,
-            validator(searchType) {
-                return Object.values(SEARCH_TYPES).includes(searchType as any);
-            },
-        },
-        thisPage: {
-            type: Number,
-            validator(value?: number) {
-                return value === undefined || value > 0;
-            },
-            default: 1,
-        },
-        pageSize: {
-            type: Number,
-            default: 24,
-        },
-        totalCount: {
-            type: Number,
-            default: 0,
-        },
-        sortBy: {
-            type: String,
-            default: '',
-        },
-        pageSizeOptions: {
-            type: Array,
-            default: () => [24, 36, 48],
-        },
-        sortByOptions: {
-            type: Array,
-            default: () => [] as MenuItem[],
-        },
-        keyItemSets: {
-            type: Array,
-            default: () => [],
-        },
-        valueHandlerMap: {
-            type: Object,
-            default: () => ({}),
-        },
-        queryTags: {
-            type: Array,
-            default: () => [],
-        },
-        searchText: {
-            type: String,
-            default: '',
-        },
-        timezone: {
-            type: String,
-            default: 'UTC',
+    pageSizeChangeable: {
+        type: Boolean,
+        default: true,
+    },
+    settingsVisible: {
+        type: Boolean,
+        default: false,
+    },
+    sortable: {
+        type: Boolean,
+        default: false,
+    },
+    exportable: {
+        type: Boolean,
+        default: false,
+    },
+    refreshable: {
+        type: Boolean,
+        default: true,
+    },
+    searchable: {
+        type: Boolean,
+        default: true,
+    },
+    filtersVisible: {
+        type: Boolean,
+        default: true,
+    },
+    searchType: {
+        type: String,
+        default: SEARCH_TYPES.plain,
+        validator(searchType) {
+            return Object.values(SEARCH_TYPES).includes(searchType as any);
         },
     },
-    setup(props: ToolboxProps, { emit }) {
-        const proxyState = reactive({
-            thisPage: useProxyValue<number>('thisPage', props, emit),
-            pageSize: useProxyValue<number>('pageSize', props, emit),
-            sortBy: useProxyValue('sortBy', props, emit),
-            searchText: useProxyValue<string>('searchText', props, emit),
-            queryTags: useProxyValue('queryTags', props, emit),
-        });
-        const sortByOptionsData = (props.sortable ? groupBy(props.sortByOptions, 'name') : undefined);
-        const state = reactive({
-            pageStart: computed(() => ((proxyState.thisPage - 1) * proxyState.pageSize) + 1),
-            allPage: computed(() => Math.ceil((props.totalCount || 0) / proxyState.pageSize) || 1),
-            pageMenu: computed(() => {
-                if (!Array.isArray(props.pageSizeOptions)) return [];
-                return props.pageSizeOptions.map((d) => ({
-                    name: d, label: d, type: 'item',
-                }));
-            }),
-            selectedSortBy: computed(() => ((sortByOptionsData && props.sortable) ? sortByOptionsData[proxyState.sortBy]?.[0]?.label : proxyState.sortBy)),
-            valueSetMap: computed<Record<string, ValueSet>>(() => {
-                const valueSetMap: Record<string, ValueSet> = {};
-                (props.keyItemSets ?? []).forEach((keyItemSet) => keyItemSet.items.forEach((item) => {
-                    if (item.valueSet) valueSetMap[item.name] = item.valueSet;
-                }));
-                return valueSetMap;
-            }),
-            queryTagConverter: computed(() => {
-                const valueSetMap = state.valueSetMap;
-                return (queryTag: QueryItem) => {
-                    const { key, value } = queryTag;
-                    if (key && value && valueSetMap[key.name]) {
-                        const valueLabel = valueSetMap[key.name]?.[value.name]?.label;
-                        return {
-                            ...queryTag,
-                            value: { ...value, label: valueLabel ?? value.label ?? value.name },
-                        };
-                    }
-                    return defaultConverter(queryTag, props.timezone);
-                };
-            }),
-        });
-
-
-        const emitChange = (options: ToolboxOptions) => {
-            emit('change', options);
-        };
-
-        const onChangeThisPage = (thisPage: number) => {
-            proxyState.thisPage = thisPage;
-            nextTick(() => {
-                emitChange({ pageStart: state.pageStart });
-            });
-        };
-
-        const onChangePageSize = (pageSize) => {
-            proxyState.pageSize = pageSize;
-            emitChange({ pageLimit: pageSize });
-        };
-
-        const onChangeSortBy = (sortBy) => {
-            if (props.sortable) {
-                proxyState.sortBy = sortBy;
-                emitChange({ sortBy });
-            }
-        };
-
-        const onSearch = (val?: string|QueryItem) => {
-            if (!val) {
-                proxyState.searchText = '';
-                proxyState.thisPage = 1;
-                nextTick(() => {
-                    emitChange({ searchText: '', pageStart: state.pageStart });
-                });
-            } else if (typeof val === 'string') {
-                proxyState.searchText = val;
-                proxyState.thisPage = 1;
-                nextTick(() => {
-                    emitChange({ searchText: val, pageStart: state.pageStart });
-                });
-            } else {
-                proxyState.queryTags = [...proxyState.queryTags, val];
-            }
-        };
-
-        const onQueryTagsChange = (tags: QueryTag[]) => {
-            if (proxyState.queryTags !== tags) {
-                proxyState.queryTags = tags;
-                proxyState.thisPage = 1;
-                nextTick(() => {
-                    emitChange({ queryTags: tags, pageStart: state.pageStart });
-                });
-            }
-        };
-
-        return {
-            SEARCH_TYPES,
-            proxyState,
-            ...toRefs(state),
-            onChangeThisPage,
-            onChangePageSize,
-            onChangeSortBy,
-            onSearch,
-            onQueryTagsChange,
-        };
+    thisPage: {
+        type: Number,
+        validator(value?: number) {
+            return value === undefined || value > 0;
+        },
+        default: 1,
+    },
+    pageSize: {
+        type: Number,
+        default: 24,
+    },
+    totalCount: {
+        type: Number,
+        default: 0,
+    },
+    sortBy: {
+        type: String,
+        default: '',
+    },
+    pageSizeOptions: {
+        type: Array,
+        default: () => [24, 36, 48],
+    },
+    sortByOptions: {
+        type: Array as PropType<MenuItem[]>,
+        default: () => [] as MenuItem[],
+    },
+    keyItemSets: {
+        type: Array as PropType<KeyItemSet[]>,
+        default: () => [],
+    },
+    valueHandlerMap: {
+        type: Object as PropType<ValueHandlerMap>,
+        default: () => ({}),
+    },
+    queryTags: {
+        type: Array as PropType<QueryTag[]>,
+        default: () => [],
+    },
+    searchText: {
+        type: String,
+        default: '',
+    },
+    timezone: {
+        type: String,
+        default: 'UTC',
     },
 });
+const emit = defineEmits(['update:thisPage', 'update:pageSize', 'update:sortBy', 'update:searchText', 'update:queryTags', 'export', 'refresh', 'click-settings', 'change']);
+const slots = useSlots();
+
+const proxyState = reactive({
+    thisPage: useProxyValue<number>('thisPage', props, emit),
+    pageSize: useProxyValue<number>('pageSize', props, emit),
+    sortBy: useProxyValue('sortBy', props, emit),
+    searchText: useProxyValue<string>('searchText', props, emit),
+    queryTags: useProxyValue('queryTags', props, emit),
+});
+const sortByOptionsData = (props.sortable ? groupBy(props.sortByOptions, 'name') : undefined);
+const state = reactive({
+    pageStart: computed(() => ((proxyState.thisPage - 1) * proxyState.pageSize) + 1),
+    allPage: computed(() => Math.ceil((props.totalCount || 0) / proxyState.pageSize) || 1),
+    pageMenu: computed(() => {
+        if (!Array.isArray(props.pageSizeOptions)) return [];
+        return props.pageSizeOptions.map((d) => ({
+            name: d, label: d, type: 'item',
+        }));
+    }),
+    selectedSortBy: computed(() => ((sortByOptionsData && props.sortable) ? sortByOptionsData[proxyState.sortBy]?.[0]?.label : proxyState.sortBy)),
+    valueSetMap: computed<Record<string, ValueSet>>(() => {
+        const valueSetMap: Record<string, ValueSet> = {};
+        (props.keyItemSets ?? []).forEach((keyItemSet) => keyItemSet.items.forEach((item) => {
+            if (item.valueSet) valueSetMap[item.name] = item.valueSet;
+        }));
+        return valueSetMap;
+    }),
+    queryTagConverter: computed(() => {
+        const valueSetMap = state.valueSetMap;
+        return (queryTag: QueryItem) => {
+            const { key, value } = queryTag;
+            if (key && value && valueSetMap[key.name]) {
+                const valueLabel = valueSetMap[key.name]?.[value.name]?.label;
+                return {
+                    ...queryTag,
+                    value: { ...value, label: valueLabel ?? value.label ?? value.name },
+                };
+            }
+            return defaultConverter(queryTag, props.timezone);
+        };
+    }),
+});
+
+const handleExport = (event) => {
+    emit('export', event);
+};
+
+const handleRefresh = (event) => {
+    emit('refresh', event);
+};
+
+const handleClickSettings = (event) => {
+    emit('click-settings', event);
+};
+
+const emitChange = (options: ToolboxOptions) => {
+    emit('change', options);
+};
+
+const onChangeThisPage = (thisPage: number) => {
+    proxyState.thisPage = thisPage;
+    nextTick(() => {
+        emitChange({ pageStart: state.pageStart });
+    });
+};
+
+const onChangePageSize = (pageSize) => {
+    proxyState.pageSize = pageSize;
+    emitChange({ pageLimit: pageSize });
+};
+
+const onChangeSortBy = (sortBy) => {
+    if (props.sortable) {
+        proxyState.sortBy = sortBy;
+        emitChange({ sortBy });
+    }
+};
+
+const onSearch = (val?: string|QueryItem) => {
+    if (!val) {
+        proxyState.searchText = '';
+        proxyState.thisPage = 1;
+        nextTick(() => {
+            emitChange({ searchText: '', pageStart: state.pageStart });
+        });
+    } else if (typeof val === 'string') {
+        proxyState.searchText = val;
+        proxyState.thisPage = 1;
+        nextTick(() => {
+            emitChange({ searchText: val, pageStart: state.pageStart });
+        });
+    } else {
+        proxyState.queryTags = [...proxyState.queryTags, val];
+    }
+};
+
+const onQueryTagsChange = (tags: QueryTag[]) => {
+    if (proxyState.queryTags !== tags) {
+        proxyState.queryTags = tags;
+        proxyState.thisPage = 1;
+        nextTick(() => {
+            emitChange({ queryTags: tags, pageStart: state.pageStart });
+        });
+    }
+};
+
 </script>
 
 <style lang="postcss">
