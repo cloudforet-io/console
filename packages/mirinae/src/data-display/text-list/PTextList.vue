946b3f330754c2fb2e33ba9bcf02bc93de8d44cb
Yongtae Park <samuel.park@megazone.com>
2023-06-12 17:08:32 +0900

    build: update p-text-list component
    Signed-off-by: Yongtae Park <samuel.park@megazone.com>

--- packages/mirinae/src/data-display/text-list/PTextList.vue
+++ packages/mirinae/src/data-display/text-list/PTextList.vue
@@ -1,23 +1,23 @@
 <template>
     <span class="p-text-list">
-        <component :is="component"
-                   v-for="(item, i) in displayItems"
+        <component :is="state.component"
+                   v-for="(item, i) in state.displayItems"
                    :key="i"
                    class="list-item"
-                   :class="{'line-break': isLineBreak && i < displayItems.length - 1}"
-                   :href="getHref(item, i)"
-                   :target="linkTarget || undefined"
+                   :class="{'line-break': state.isLineBreak && i < state.displayItems.length - 1}"
+                   :href="getHref"
+                   :target="target || undefined"
         >
             <slot name="default"
                   v-bind="{...$props, index: i, item, value: item || ''}"
             >
                 {{ item || '' }}
             </slot>
-            <slot v-if="i < displayItems.length - 1"
+            <slot v-if="i < state.displayItems.length - 1"
                   name="delimiter"
-                  v-bind="{...$props, index: i, item, value: item || ''}"
+                  v-bind="{...props, index: i, item, value: item || ''}"
             >
-                <span v-if="!isLineBreak"
+                <span v-if="!state.isLineBreak"
                       class="delimiter"
                 >{{ delimiter }}</span>
             </slot>
@@ -25,70 +25,41 @@
     </span>
 </template>
 
-<script lang="ts">
-import type { PropType } from 'vue';
+<script setup lang="ts">
+import { get } from 'lodash';
 import {
-    computed, defineComponent, reactive, toRefs,
+    computed, reactive,
 } from 'vue';
 
-import { get } from 'lodash';
 
-import type { TextListItem, TextListProps } from '@/data-display/text-list/type';
+import type { TextListProps } from '@/data-display/text-list/type';
 import PAnchor from '@/inputs/anchors/PAnchor.vue';
 import { isNotEmpty } from '@/utils/helpers';
 
-export default defineComponent<TextListProps>({
-    name: 'PTextList',
-    components: { PAnchor },
-    props: {
-        items: {
-            type: Array as PropType<TextListItem>,
-            default: () => [],
-        },
-        delimiter: {
-            type: String,
-            default: ', ',
-        },
-        subKey: {
-            type: String,
-            default: undefined,
-        },
-        link: {
-            type: String,
-            default: undefined,
-        },
-        linkTarget: {
-            type: String,
-            default: undefined,
-        },
-    },
-    setup(props) {
-        const state = reactive({
-            component: computed(() => (props.link ? PAnchor : 'span')),
-            displayItems: computed(() => props.items.reduce((res, item) => {
-                let data;
-                if (typeof item === 'object' && props.subKey) {
-                    data = get(item, props.subKey, '');
-                } else data = item;
-
-                if (isNotEmpty(data)) res.push(data);
-                return res;
-            }, [] as string[])),
-            isLineBreak: computed(() => ['<br>', '<br/>'].includes(props.delimiter)),
-        });
-
-        const getHref = () => {
-            if (props.link) return props.link;
-            return undefined;
-        };
-
-
-        return {
-            ...toRefs(state),
-            getHref,
-        };
-    },
+const props = withDefaults(defineProps<TextListProps>(), {
+    items: () => [],
+    delimiter: ', ',
 });
+
+const state = reactive({
+    component: computed(() => (props.link ? PAnchor : 'span')),
+    displayItems: computed(() => props.items.reduce((res, item) => {
+        let data;
+        if (typeof item === 'object' && props.subKey) {
+            data = get(item, props.subKey, '');
+        } else data = item;
+
+        if (isNotEmpty(data)) res.push(data);
+        return res;
+    }, [] as string[])),
+    isLineBreak: computed(() => ['<br>', '<br/>'].includes(props.delimiter)),
+});
+
+const getHref = () => {
+    if (props.link) return props.link;
+    return undefined;
+};
+
 </script>
 
 <style lang="postcss">
