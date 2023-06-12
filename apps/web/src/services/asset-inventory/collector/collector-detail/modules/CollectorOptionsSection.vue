edf6f13c75548ea6b4c9aa8ccae24445350dd0cc
Wanjin Noh <wanjin@megazone.com>
2023-06-12 23:00:11 +0900

    feat: apply save logic of CollectorOptionsSection
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/asset-inventory/collector/collector-detail/modules/CollectorOptionsSection.vue
+++ apps/web/src/services/asset-inventory/collector/collector-detail/modules/CollectorOptionsSection.vue
@@ -4,52 +4,149 @@
                    heading-type="sub"
         >
             <template #extra>
-                <p-button size="md"
+                <p-button v-if="!state.isEditMode"
+                          size="md"
                           icon-left="ic_edit"
                           style-type="secondary"
+                          @click="handleClickEdit"
                 >
                     {{ $t('INVENTORY.COLLECTOR.DETAIL.EDIT') }}
                 </p-button>
             </template>
         </p-heading>
-        <p-definition-table :fields="state.fields"
-                            :loading="props.loading"
-                            :data="props.collectorOptions"
+        <p-definition-table v-if="!state.isEditMode"
+                            :fields="state.fields"
+                            :loading="state.loading"
+                            :data="state.collectorOptions"
                             style-type="white"
         />
+        <collector-options-form v-else
+                                has-metadata
+                                reset-on-collector-id-change
+                                @update:isValid="handleUpdateIsOptionsValid"
+        />
+        <div class="button-group">
+            <p-button v-if="state.isEditMode"
+                      style-type="tertiary"
+                      size="lg"
+                      :disabled="state.isUpdating"
+                      @click="handleClickCancel"
+            >
+                {{ $t('INVENTORY.COLLECTOR.DETAIL.CANCEL') }}
+            </p-button>
+            <p-button v-if="state.isEditMode"
+                      style-type="primary"
+                      size="lg"
+                      :loading="state.isUpdating"
+                      class="save-changes-button"
+                      @click="handleClickSave"
+            >
+                {{ $t('INVENTORY.COLLECTOR.DETAIL.SAVE_CHANGES') }}
+            </p-button>
+        </div>
     </p-pane-layout>
 </template>
 
 <script lang="ts" setup>
-import { defineProps, computed, reactive } from 'vue';
+import { computed, reactive } from 'vue';
 
 import {
     PHeading, PButton, PPaneLayout, PDefinitionTable,
 } from '@spaceone/design-system';
 import type { DefinitionField } from '@spaceone/design-system/types/data-display/tables/definition-table/type';
+import type { JsonSchema } from '@spaceone/design-system/types/inputs/forms/json-schema-form/type';
 
-import type { CollectorPluginModel } from '@/services/asset-inventory/collector/model';
+import { i18n } from '@/translations';
 
-const props = defineProps<{
-    loading: boolean;
-    collectorOptions: CollectorPluginModel['options']|null;
-}>();
+import { showSuccessMessage } from '@/lib/helper/notice-alert-helper';
 
+import ErrorHandler from '@/common/composables/error/errorHandler';
+
+import type { CollectorModel, CollectorOptions } from '@/services/asset-inventory/collector/model';
+import { useCollectorFormStore } from '@/services/asset-inventory/collector/shared/collector-forms/collector-form-store';
+import CollectorOptionsForm from '@/services/asset-inventory/collector/shared/collector-forms/CollectorOptionsForm.vue';
+
+
+
+const collectorFormStore = useCollectorFormStore();
+const collectorFormState = collectorFormStore.$state;
 
 const state = reactive({
+    loading: computed<boolean>(() => !collectorFormState.originCollector),
+    collectorOptions: computed<CollectorOptions>(() => collectorFormState.originCollector?.plugin_info?.options ?? {}),
+    collectorOptionsSchema: computed<JsonSchema>(() => collectorFormState.originCollector?.plugin_info?.metadata?.options_schema ?? {
+        type: 'object',
+        properties: {},
+    }),
     fields: computed<DefinitionField[]>(() => {
-        if (!props.collectorOptions) return [];
-        return Object.keys(props.collectorOptions).map((key) => ({
+        const properties = state.collectorOptionsSchema?.properties ?? {};
+        return Object.entries<JsonSchema['properties']>(properties).map(([key, property]) => ({
             name: key,
-            label: key,
+            label: property.title ?? key,
         }));
     }),
+    isEditMode: false,
+    isOptionsValid: false,
+    isUpdating: false,
 });
+
+const fetchCollectorUpdate = async (): Promise<CollectorModel> => {
+    // TODO: change to call api
+    if (!collectorFormState.originCollector) throw new Error('collector is required');
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                ...collectorFormState.originCollector,
+                plugin_info: {
+                    ...(collectorFormState.originCollector as CollectorModel).plugin_info,
+                    options: collectorFormState.options,
+                },
+            } as CollectorModel);
+        }, 1000);
+    });
+};
+
+const handleClickEdit = () => {
+    state.isEditMode = true;
+};
+
+const handleUpdateIsOptionsValid = (value: boolean) => {
+    state.isOptionsValid = value;
+};
+
+const handleClickCancel = () => {
+    state.isEditMode = false;
+};
+
+const handleClickSave = async () => {
+    try {
+        state.isUpdating = true;
+        const collector = await fetchCollectorUpdate();
+        collectorFormStore.setOriginCollector(collector);
+        showSuccessMessage(i18n.t('INVENTORY.COLLECTOR.ALT_S_UPDATE_COLLECTOR_OPTIONS'), '');
+        state.isEditMode = false;
+    } catch (error) {
+        collectorFormStore.resetOptions();
+        ErrorHandler.handleRequestError(error, i18n.t('INVENTORY.COLLECTOR.ALT_E_UPDATE_COLLECTOR_OPTIONS'));
+    } finally {
+        state.isUpdating = false;
+    }
+};
 
 </script>
 
 <style lang="postcss" scoped>
 .p-definition-table {
     border-color: transparent;
+}
+.collector-options-form {
+    padding: 0 1rem;
+}
+.button-group {
+    padding: 1rem;
+    margin-bottom: 1.5rem;
+    .save-changes-button {
+        margin-left: 1rem;
+    }
 }
 </style>
