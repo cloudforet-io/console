df2baab6145cd8dd98f87321892e7ae202363889
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2023-06-13 12:00:26 +0900

    feat(collector): relocation collector main type from model
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>

---
+++ apps/web/src/services/asset-inventory/collector/collector-main/type.ts
@@ -0,0 +1,48 @@
+import type { CollectorPluginModel, Schedule } from '@/services/asset-inventory/collector/model';
+
+export const COLLECTOR_QUERY_HELPER_SET = {
+    COLLECTOR_ID: 'collector_id',
+    NAME: 'name',
+    LAST_COLLECTED_AT: 'last_collected_at',
+    PROVIDER: 'provider',
+    TAGS: 'tags',
+    PLUGIN_INFO: 'plugin_info',
+    STATE: 'state',
+} as const;
+
+export const COLLECTOR_ITEM_INFO_TYPE = {
+    PLUGIN: 'PLUGIN',
+    STATUS: 'STATUS',
+    JOBS: 'JOBS',
+    SCHEDULE: 'SCHEDULE',
+} as const;
+
+interface CollectorPlugin {
+    name?: string;
+    icon?: string;
+    info: CollectorPluginModel;
+}
+
+interface CollectorLink {
+    name: string;
+    params: CollectorDetailLinkParameter;
+    query: CollectorDetailLinkQuery;
+}
+
+interface CollectorDetailLinkParameter {
+    id?: string;
+    collectorId?: string;
+}
+interface CollectorDetailLinkQuery {
+    filters: string[];
+    collectorId?: string;
+}
+
+export interface CollectorItemInfo {
+    collectorId: string;
+    name: string;
+    plugin: CollectorPlugin;
+    historyLink: CollectorLink,
+    detailLink: CollectorLink;
+    schedule: Schedule;
+}
