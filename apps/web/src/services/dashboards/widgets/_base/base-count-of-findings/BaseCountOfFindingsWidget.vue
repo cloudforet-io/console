87b46700f1830dc888cac750263fe784c99f31ff
Dahyun Yu <yuda@megazone.com>
2023-06-14 17:18:03 +0900

    feat: apply reference name to chart value (#1292)
    * feat: apply reference name to chart value
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * refactor: call referenceMap outside of for loop
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/dashboards/widgets/_base/base-count-of-findings/BaseCountOfFindingsWidget.vue
+++ apps/web/src/services/dashboards/widgets/_base/base-count-of-findings/BaseCountOfFindingsWidget.vue
@@ -155,11 +155,14 @@ const fetchData = async (): Promise<Data[]> => {
 const refineChartData = (data: Data[]): ChartData[] => {
     if (!data?.length) return [];
     const refinedChartData: ChartData[] = [];
+    const referenceMap = Object.values(props.allReferenceTypeInfo ?? {}).find((info) => info.key === state.groupBy)?.referenceMap;
     data.forEach((d) => {
         const fail_finding_count = d.value?.find((v) => v.key === 'fail_finding_count')?.value ?? 0;
         const pass_finding_count = d.value?.find((v) => v.key === 'pass_finding_count')?.value ?? 0;
+        const rawValue = d[state.groupByKey];
+        const refinedValue = referenceMap ? referenceMap[rawValue]?.label : rawValue; // google_cloud -> Google Cloud
         refinedChartData.push({
-            [state.groupByKey]: d[state.groupByKey] ?? `no_${state.groupByKey}`,
+            [state.groupByKey]: refinedValue ?? `no_${state.groupByKey}`,
             fail_finding_count,
             pass_finding_count,
         });
