46f1d8553fdc9fddf14bd6dc31359c99014d6f87
yuda <yuda@megazone.com>
2023-06-29 15:12:53 +0900

    test: add test for creating asset dashboard
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ apps/web/tests/dashboards/create-dashboards/create-compliacne-overview-dashboard.spec.ts
@@ -0,0 +1,68 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('Create Compliance Overview Dashboard', () => {
+    test('Create entire Workspace dashboard', async ({ page }) => {
+        page.goto('/');
+
+        await test.step('1. Go to Create New Dashboard page', async () => {
+            //
+            await page.locator('.gnb-menu .menu-button').first().click();
+            const createNewDashboardButton = await page.locator('.g-n-b-dashboard-menu .sub-menu').nth(1).locator('.gnb-sub-contents');
+            await createNewDashboardButton.click();
+            await expect(page).toHaveURL('/dashboards/create');
+        });
+
+        await test.step('2. Select Scope and Viewers in Step 1', async () => {
+            // choose dashboard scope "entire workspace"
+            const dashboardScope = await page.locator('.dashboard-scope-form .dashboard-scope-wrapper .p-radio-group');
+            await dashboardScope.locator('.p-radio').first().click();
+
+            // choose dashboard viewer "public"
+            const dashboardViewer = await page.locator('.dashboard-viewers-form .dashboard-viewers-wrapper .p-radio-group');
+            await dashboardViewer.locator('.p-radio').first().click();
+
+            // click continue button
+            const continueButton = await page.locator('.button-area .p-button').nth(1);
+            await continueButton.click();
+            await expect(page.locator('.dashboard-create-header > .step')).toContainText('2/2');
+        });
+
+        const dashboardName = `playwright-${Date()}`;
+        // const selectedTemplateName = await page.locator('.default-dashboard-board .p-board-item').nth(3).locator('.dashboard-name').innerText();
+
+        await test.step('3. Select Template (Compliance Overview)', async () => {
+            // choose template "Compliance Overview"
+            const complianceOverviewTemplate = await page.locator('.default-dashboard-board .p-board-item').nth(2);
+            await expect(complianceOverviewTemplate).toContainText('Compliance Overview');
+            await complianceOverviewTemplate.click();
+
+            // click create button
+            const createDashboardButton = await page.locator('.button-area .p-button').nth(1);
+            await createDashboardButton.click();
+
+            await expect(page).toHaveURL('/dashboards/workspace/customize');
+        });
+
+        await test.step('4. Enter and create a dashboard name', async () => {
+            await page.getByPlaceholder('Compliance Overview').click();
+            await page.getByPlaceholder('Compliance Overview').fill(`${'Compliance Overview'}-${dashboardName}`);
+            await page.getByRole('button', { name: 'Save' }).click();
+            await expect(page).toHaveURL(/dashboards\/workspace\/detail/);
+        });
+
+        await test.step('5. Check the created dashboard', async () => {
+            const locatorScope = await page.locator('.p-breadcrumbs > span:nth-child(2) .link');
+            const locatorName = await page.locator('.lnb-menu-item .selected .text');
+
+            await expect(locatorScope).toContainText(' Workspace ');
+            await expect(locatorName).toContainText(dashboardName);
+        });
+
+        await test.step('6. Delete dashboard', async () => {
+            const deleteButton = await page.locator('.dashboard-title-icon-buttons-wrapper .delete-button');
+            await deleteButton.click();
+            await page.getByRole('button', { name: 'Confirm' }).click();
+            await expect(page).toHaveURL(/dashboards\/all/);
+        });
+    });
+});
