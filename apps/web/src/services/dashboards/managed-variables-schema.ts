5b89bd34c928f722cfca0909be28cae6f1b2b3c4
Wanjin Noh <wanjin@megazone.com>
2023-07-07 18:04:53 +0900

    chore: test
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/dashboards/managed-variables-schema.ts
+++ apps/web/src/services/dashboards/managed-variables-schema.ts
@@ -8,6 +8,7 @@ import type { DashboardVariablesSchema } from '@/services/dashboards/config';
 
 
 
+
 export const managedDashboardVariablesSchema: DashboardVariablesSchema = {
     properties: {
         [REFERENCE_TYPE_INFO.project.type]: {
