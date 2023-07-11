4b0b6d6968ed21dc01bcdcf9301feb98ad86a8f2
sulmo <sulmo@megazone.com>
2023-07-11 17:51:29 +0900

    feat(collector): add type and config for repositories
    Signed-off-by: sulmo <sulmo@megazone.com>

---
+++ apps/web/src/services/asset-inventory/collector/config.ts
@@ -0,0 +1,13 @@
+import { green, indigo, violet } from '@/styles/colors';
+
+export const repositoryIconMap = {
+    local: 'ic_private-repository',
+    managed: 'ic_badge-with-ribbon',
+    remote: 'ic_marketplace',
+};
+
+export const repositoryColorMap = {
+    local: green[700],
+    managed: violet[600],
+    remote: indigo[600],
+};
