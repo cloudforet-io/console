22437c9ebbdc353b9ff4fb3cacc834bd3fa1f076
sulmo <sulmo@megazone.com>
2023-07-11 18:40:58 +0900

    feat(collector-plugin-contents): add repository badge
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/services/asset-inventory/collector/config.ts
+++ apps/web/src/services/asset-inventory/collector/config.ts
@@ -11,3 +11,9 @@ export const repositoryColorMap = {
     managed: violet[600],
     remote: indigo[600],
 };
+
+export const repositoryBackgroundColorMap = {
+    local: green[100],
+    managed: violet[100],
+    remote: indigo[100],
+};
