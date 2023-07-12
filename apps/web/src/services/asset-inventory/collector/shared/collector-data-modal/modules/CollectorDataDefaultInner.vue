1b5f71996df183ebe8505302d1109db12fbc7efc
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2023-07-12 15:21:15 +0900

    fix: fixing the type error when activating the 'collect data' modal
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>

--- apps/web/src/services/asset-inventory/collector/shared/collector-data-modal/modules/CollectorDataDefaultInner.vue
+++ apps/web/src/services/asset-inventory/collector/shared/collector-data-modal/modules/CollectorDataDefaultInner.vue
@@ -2,12 +2,12 @@
     <div class="collector-data-default-inner">
         <span>{{ $t('INVENTORY.COLLECTOR.MAIN.COLLECT_DATA_MODAL.DESCRIPTION') }}</span>
         <div class="accounts-wrapper">
-            <p-lazy-img :src="collectorDataModalStore.selectedCollector.plugin.icon"
+            <p-lazy-img :src="props.item.plugin ? props.item.plugin.icon : ''"
                         width="1rem"
                         height="1rem"
                         class="plugin-icon"
             />
-            <span>{{ collectorDataModalStore.selectedCollector.name }}</span>
+            <span>{{ props.item.name }}</span>
             <span v-if="collectorDataModalState.secrets.length > 0">
                 ({{ collectorDataModalState.secrets.length }})
             </span>
@@ -21,10 +21,19 @@ import { PLazyImg } from '@spaceone/design-system';
 import {
     useCollectorDataModalStore,
 } from '@/services/asset-inventory/collector/shared/collector-data-modal/collector-data-modal-store';
-
+import type { CollectorData } from '@/services/asset-inventory/collector/shared/collector-data-modal/type';
 
 const collectorDataModalStore = useCollectorDataModalStore();
 const collectorDataModalState = collectorDataModalStore.$state;
+
+interface Props {
+    item: CollectorData;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    item: undefined,
+});
+
 </script>
 
 <style lang="postcss" scoped>
--- apps/web/src/services/asset-inventory/collector/shared/collector-data-modal/type.ts
+++ apps/web/src/services/asset-inventory/collector/shared/collector-data-modal/type.ts
@@ -1,3 +1,5 @@
+import type { CollectorModel } from '@/services/asset-inventory/collector/model';
+
 export const ACCOUNT_TYPE = {
     ALL: 'all',
     SPECIFIC: 'Specific Accounts',
@@ -7,3 +9,12 @@ export const COLLECT_DATA_TYPE = {
     COLLECTOR: 'collector',
     SECRET: 'secret',
 } as const;
+
+export interface CollectorPlugin {
+    name?: string;
+    icon?: string;
+}
+
+export interface CollectorData extends CollectorModel {
+    plugin?: CollectorPlugin;
+}
