6aefdb080a31959b3cdf2815abc275620f5bf8b0
NaYeong,Kim <nayeongkim@megazone.com>
2023-08-09 10:00:07 +0900

    feat: apply test code for collector list at collector main page (#1573)
    * feat: apply collector list test
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * chore: remove '/'
    
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>

---
+++ apps/web/tests/asset-inventory/collector/collector-main/collector-list.spec.ts
@@ -0,0 +1,104 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Collector List', () => {
+    let page;
+
+    const goToCollectorPage = async () => {
+        await page.goto('/asset-inventory/collector');
+    };
+
+    /* Init */
+    test.beforeEach(async ({ browser }) => {
+        page = await browser.newPage();
+        await goToCollectorPage();
+    });
+
+    test.afterEach(async () => {
+        await page.close();
+    });
+
+    /* Test */
+    test('Go to asset collector page.', async () => {
+        const pageTitleName = page.locator('.heading-wrapper>h2.has-right>span.title');
+        await expect(pageTitleName).toContainText('Collector');
+    });
+
+    test('Filtering by providers.', async () => {
+        await test.step('1. Click AppDynamics', async () => {
+            await page.getByRole('button', { name: 'AppDynamics' }).click();
+
+            try {
+                const apiResponse = await page.waitForResponse(`${process.env.APIURL as string}/inventory/collector/list`);
+                await page.waitForLoadState('networkidle');
+                expect(apiResponse.status()).toBe(200);
+            } catch (error) {
+                console.error('Error occurred:', error);
+            }
+        });
+
+        await test.step('2. Click Atlassian', async () => {
+            await page.getByRole('button', { name: 'Atlassian' }).click();
+
+            try {
+                const apiResponse = await page.waitForResponse(`${process.env.APIURL as string}/inventory/collector/list`);
+                await page.waitForLoadState('networkidle');
+                expect(apiResponse.status()).toBe(200);
+            } catch (error) {
+                console.error('Error occurred:', error);
+            }
+        });
+
+        await test.step('3. Click all provider', async () => {
+            await page.getByRole('button', { name: 'All Providers' }).click();
+
+            try {
+                const apiResponse = await page.waitForResponse(`${process.env.APIURL as string}/inventory/collector/list`);
+                await page.waitForLoadState('networkidle');
+                expect(apiResponse.status()).toBe(200);
+            } catch (error) {
+                console.error('Error occurred:', error);
+            }
+        });
+    });
+
+    test('Search Keyword.', async () => {
+        await page.getByPlaceholder('Search', { exact: true }).click();
+        await page.getByPlaceholder('Search', { exact: true }).fill('google');
+        await page.getByPlaceholder('Search', { exact: true }).press('Enter');
+
+        try {
+            const apiResponse = await page.waitForResponse(`${process.env.APIURL as string}/inventory/collector/list`);
+            await page.waitForLoadState('networkidle');
+            expect(apiResponse.status()).toBe(200);
+        } catch (error) {
+            console.error('Error occurred:', error);
+        }
+    });
+
+    test('Exporting to an Excel file.', async () => {
+        const excelButton = page.locator('.tool > .p-button')
+        await excelButton.first().click();
+
+        try {
+            await page.waitForLoadState('networkidle');
+            const downloads = await page.context().downloads();
+            expect(downloads.length).toBe(1);
+            expect(downloads[0].suggestedFilename()).toContain('.xlsx');
+        } catch (error) {
+            console.error('Error occurred:', error);
+        }
+    });
+
+    test('Refresh list.', async () => {
+        const refreshButton = page.locator('.right-tool-group > div:nth-child(2) > .p-button')
+        await refreshButton.click();
+
+        try {
+            const apiResponse = await page.waitForResponse(`${process.env.APIURL as string}/inventory/collector/list`);
+            await page.waitForLoadState('networkidle');
+            expect(apiResponse.status()).toBe(200);
+        } catch (error) {
+            console.error('Error occurred:', error);
+        }
+    });
+})
