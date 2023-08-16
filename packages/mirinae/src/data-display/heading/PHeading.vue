4e597973ec950f53751297a286e334d3e99c5940
sulmo <sulmo@megazone.com>
2023-08-16 10:35:21 +0900

    fix(p-heading): add &zwnj if title length is 0
    Signed-off-by: sulmo <sulmo@megazone.com>
    
    feat: add comment
    
    Signed-off-by: sulmo <sulmo@megazone.com>

--- packages/mirinae/src/data-display/heading/PHeading.vue
+++ packages/mirinae/src/data-display/heading/PHeading.vue
@@ -14,7 +14,8 @@
             <h2 :class="{'has-left': !!$slots['title-left-extra'], 'has-right': useTotalCount || !!$slots['title-right-extra']}">
                 <slot>
                     <slot name="title">
-                        <span class="title">{{ title }}&zwnj;</span>
+                        <!--&zwnj: Added to prevent style bugs if title does not exist-->
+                        <span class="title">{{ title.length ? title : '&zwnj' }}</span>
                     </slot>
                 </slot>
             </h2>
