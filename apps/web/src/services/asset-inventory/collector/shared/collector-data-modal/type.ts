a9ef2b8f22a234a5b23f667473a39d927eff1850
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2023-08-17 14:20:24 +0900

    feat: fix the bug where the defaultInner is temporarily displayed when the modal is activated
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>

--- apps/web/src/services/asset-inventory/collector/shared/collector-data-modal/type.ts
+++ apps/web/src/services/asset-inventory/collector/shared/collector-data-modal/type.ts
@@ -3,9 +3,3 @@ export const COLLECT_DATA_TYPE = {
     SINGLE: 'secret',
 } as const;
 export type CollectDataType = typeof COLLECT_DATA_TYPE[keyof typeof COLLECT_DATA_TYPE];
-
-
-export interface CollectorPlugin {
-    name?: string;
-    icon?: string;
-}
