da02bedfa6591c2f2fa6502622026e5768586d20
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2023-08-18 10:28:18 +0900

    feat: add state for getting job loading
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>

--- apps/web/src/services/asset-inventory/collector/shared/collector-data-modal/CollectorDataModal.vue
+++ apps/web/src/services/asset-inventory/collector/shared/collector-data-modal/CollectorDataModal.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="collector-data-modal">
-        <p-button-modal :visible="collectorDataModalState.visible"
+        <p-button-modal :visible="collectorDataModalState.visible && !collectorDataModalState.initLoading"
                         :header-title="state.headerTitle"
                         :theme-color="state.isDuplicateJobs ? 'alert' : 'primary'"
                         :loading="state.loading"
