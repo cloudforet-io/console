f30342e14fd071d29533e5fc8c143370d638f343
WANJIN <wanjin@megazone.com>
2023-08-21 14:51:07 +0900

    fix: do not use SpaceRouter in the vue instance (#1631)
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/auth/sign-out/SignOutPage.vue
+++ apps/web/src/services/auth/sign-out/SignOutPage.vue
@@ -1,31 +1,27 @@
-<template>
-    <div>Sign Out..</div>
-</template>
 <script lang="ts">
-import { useStore } from 'vuex';
-
-import { SpaceRouter } from '@/router';
+import { defineComponent } from 'vue';
 
 import ErrorHandler from '@/common/composables/error/errorHandler';
 
 import { loadAuth } from '@/services/auth/authenticator/loader';
 import { AUTH_ROUTE } from '@/services/auth/route-config';
 
-export default {
+export default defineComponent({
     name: 'SignOutPage',
-    beforeRouteEnter(to) {
-        (async () => {
-            try {
-                const store = useStore();
-                const authType = store.state.domain.extendedAuthType;
-                await loadAuth(authType).signOut();
-                await store.dispatch('user/setIsSessionExpired', true);
-            } catch (e) {
-                ErrorHandler.handleError(e);
-            } finally {
-                await SpaceRouter.router.push({ name: AUTH_ROUTE.SIGN_IN._NAME, query: { ...to.query } });
-            }
-        })();
+    beforeRouteEnter(to, from, next) {
+        next((vm) => {
+            (async () => {
+                try {
+                    const authType = vm.$store.state.domain.extendedAuthType;
+                    await loadAuth(authType).signOut();
+                    await vm.$store.dispatch('user/setIsSessionExpired', true);
+                } catch (e) {
+                    ErrorHandler.handleError(e);
+                } finally {
+                    vm.$router.push({ name: AUTH_ROUTE.SIGN_IN._NAME, query: { ...to.query } });
+                }
+            })();
+        });
     },
-};
+});
 </script>
