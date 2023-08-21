6253c2a5413de5613d2f8a9abf2fbb1b6426b058
Wanjin Noh <wanjin@megazone.com>
2023-08-21 15:01:58 +0900

    fix: use selected prop as v-model arg for p-select-status component
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- packages/mirinae/src/inputs/select-status/story-helper.ts
+++ packages/mirinae/src/inputs/select-status/story-helper.ts
@@ -199,4 +199,17 @@ export const getSelectStatusArgTypes = (): ArgTypes => ({
             category: 'events',
         },
     },
+    onUpdateSelected: {
+        name: 'update:selected',
+        description: 'Event emitted when selected state is changed.',
+        table: {
+            type: {
+                summary: null,
+            },
+            defaultValue: {
+                summary: null,
+            },
+            category: 'events',
+        },
+    },
 });
