afa2f9d5d938267b8f34b5da25cd795450574e20
Yongtae Park <samuel.park@megazone.com>
2023-08-21 11:42:04 +0900

    fix(cloud-service-usage-overview): migration to vue3
    Signed-off-by: Piggy Park (박용태) <pyt4105@gmail.com>

--- apps/web/src/services/asset-inventory/cloud-service/cloud-service-detail/modules/cloud-service-usage-overview/CloudServiceUsageOverview.vue
+++ apps/web/src/services/asset-inventory/cloud-service/cloud-service-detail/modules/cloud-service-usage-overview/CloudServiceUsageOverview.vue
@@ -1,43 +1,10 @@
-<template>
-    <fragment>
-        <div class="cloud-service-usage-overview">
-            <header>
-                {{ $t('INVENTORY.CLOUD_SERVICE.MAIN.USAGE_OVERVIEW') }}
-                <p-button style-type="tertiary"
-                          size="sm"
-                          :disabled="schemaLoading || !chartWidgetSchemaList.length"
-                          @click="handleClickShowAll"
-                >
-                    {{ $t('INVENTORY.CLOUD_SERVICE.MAIN.SHOW_CHARTS') }}
-                </p-button>
-            </header>
-            <cloud-service-usage-overview-summary :schema-loading="schemaLoading"
-                                                  :data-loading="dataLoading"
-                                                  :data-list="summaryDataList"
-                                                  :widget-schema-list="summaryWidgetSchemaList"
-                                                  :cloud-service-type-id="cloudServiceTypeId"
-                                                  :filters="hiddenFilters"
-            />
-        </div>
-        <cloud-service-usage-overview-detail-modal v-model="usageOverviewDetailModalVisible"
-                                                   :schema-list="widgetSchemaList"
-                                                   :summary-data-list="summaryDataList"
-                                                   :cloud-service-type-info="cloudServiceTypeInfo"
-                                                   :filters="filters"
-                                                   :period="period"
-                                                   :key-item-sets="keyItemSets"
-        />
-    </fragment>
-</template>
+<script lang="ts" setup>
 
-<script lang="ts">
 
-import {
-    computed,
-    defineComponent,
-    reactive, toRefs, watch,
-} from 'vue';
-
+import { QueryHelper } from '@cloudforet/core-lib/query';
+import type { ConsoleFilter } from '@cloudforet/core-lib/query/type';
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+import type { ApiFilter } from '@cloudforet/core-lib/space-connector/type';
 import {
     PButton,
 } from '@spaceone/design-system';
@@ -49,11 +16,11 @@ import type { CancelTokenSource } from 'axios';
 import axios from 'axios';
 import dayjs from 'dayjs';
 import { debounce, isEmpty } from 'lodash';
-
-import { QueryHelper } from '@cloudforet/core-lib/query';
-import type { ConsoleFilter } from '@cloudforet/core-lib/query/type';
-import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
-import type { ApiFilter } from '@cloudforet/core-lib/space-connector/type';
+import {
+    computed,
+    reactive, watch,
+} from 'vue';
+import { useI18n } from 'vue-i18n';
 
 import ErrorHandler from '@/common/composables/error/errorHandler';
 
@@ -78,167 +45,169 @@ interface Data {
     value?: number;
 }
 
-export default defineComponent<Props>({
-    name: 'CloudServiceUsageOverview',
-    components: {
-        CloudServiceUsageOverviewSummary,
-        CloudServiceUsageOverviewDetailModal,
-        PButton,
-
-    },
-    props: {
-        cloudServiceTypeInfo: {
-            type: Object as () => CloudServiceTypeInfo,
-            default: () => ({}),
-        },
-        filters: {
-            type: Array as () => ConsoleFilter[]|undefined,
-            default: undefined,
-        },
-        period: {
-            type: Object as () => Period|undefined,
-            default: undefined,
-        },
-        hiddenFilters: {
-            type: Array as () => ConsoleFilter[]|undefined,
-            default: undefined,
-        },
-        keyItemSets: {
-            type: Array as () => KeyItemSet[],
-            default: () => [],
-        },
-    },
-    setup(props) {
-        const queryHelper = new QueryHelper();
-
-        const state = reactive({
-            usageOverviewDetailModalVisible: false,
-            widgetSchemaList: [] as DynamicWidgetSchema[],
-            summaryWidgetSchemaList: computed<DynamicWidgetSchema[]>(() => state.widgetSchemaList.filter(({ type }) => ['summary', 'card'].includes(type))),
-            chartWidgetSchemaList: computed<DynamicWidgetSchema[]>(() => state.widgetSchemaList.filter(({ type }) => type === 'chart')),
-            schemaLoading: true,
-            summaryDataList: [] as Data[],
-            dataLoading: false,
-            cloudServiceTypeId: computed<string>(() => props.cloudServiceTypeInfo.cloud_service_type_id ?? ''),
-            apiQuery: computed<{filter: ApiFilter[]; keyword: string}>(() => {
-                queryHelper.setFilters([]);
-                if (props.filters) {
-                    queryHelper.addFilter(...props.filters);
-                }
-                if (props.hiddenFilters) {
-                    queryHelper.addFilter(...props.hiddenFilters);
-                }
-                const { filter, keyword } = queryHelper.apiQuery;
-                return { filter, keyword };
-            }),
-            dateRange: computed<Period|undefined>(() => {
-                if (isEmpty(props.period)) return undefined;
-                const period = props.period as Period;
-                const dateRange: Period = {};
-                if (period.start) dateRange.start = dayjs.utc(period.start).format('YYYY-MM-DD');
-                if (period.end) dateRange.end = dayjs.utc(period.end).add(1, 'day').format('YYYY-MM-DD');
-                return dateRange;
-            }),
-        });
-
-        const fetchSchemaList = async (): Promise<DynamicWidgetSchema[]> => {
-            try {
-                const { provider, group, name } = props.cloudServiceTypeInfo as CloudServiceTypeInfo;
-                const { widget } = await SpaceConnector.client.addOns.pageSchema.get({
-                    resource_type: 'inventory.CloudService',
-                    schema: 'widget',
-                    options: {
-                        provider,
-                        cloud_service_group: group,
-                        cloud_service_type: name,
-                        // widget_type: 'chart',
-                        // limit: 15,
-                    },
-                });
-                return widget ?? [];
-            } catch (e) {
-                ErrorHandler.handleError(e);
-                return [];
-            }
-        };
-
-        let fetchDataTokenList: Array<CancelTokenSource|undefined> = [];
-
-        const fetchDataWithSchema = async (schema: DynamicWidgetSchema, idx: number): Promise<Data> => {
-            fetchDataTokenList[idx] = axios.CancelToken.source();
-
-            try {
-                const { results } = await SpaceConnector.client.inventory.cloudService.analyze({
-                    ...state.apiQuery,
-                    default_query: schema.query,
-                    date_range: state.dateRange,
-                }, { cancelToken: fetchDataTokenList[idx]?.token });
-                fetchDataTokenList[idx] = undefined;
-                return results[0] ?? {};
-            } catch (e: any) {
-                if (!axios.isCancel(e.axiosError)) {
-                    ErrorHandler.handleError(e);
-                }
-                return {};
-            }
-        };
-
-        const cachedSchemaList = {};
-        const getWidgetSchemaList = async () => {
-            const schemaList = cachedSchemaList[state.cloudServiceTypeId];
-            if (schemaList) state.widgetSchemaList = schemaList;
-            else state.widgetSchemaList = await fetchSchemaList();
-        };
-
-        const cancelPreviousDataFetchRequests = () => {
-            fetchDataTokenList.forEach((fetchDataToken) => {
-                if (fetchDataToken) {
-                    fetchDataToken.cancel('Next request will called.');
-                }
-            });
-            fetchDataTokenList = [];
-        };
-
-        const getDataListWithSchema = debounce(async () => {
-            if (!state.dataLoading) state.dataLoading = true;
-            cancelPreviousDataFetchRequests();
-            const results: any = await Promise.allSettled(state.summaryWidgetSchemaList.map((schema, i) => fetchDataWithSchema(schema, i)));
-            state.summaryDataList = results.map((d) => {
-                if (d.status === 'fulfilled') return d.value;
-                return {};
-            });
-            state.dataLoading = false;
-        }, 300);
-
-        /* Event Handlers */
-        const handleClickShowAll = () => {
-            state.usageOverviewDetailModalVisible = true;
-        };
-
-        /* Watchers */
-        watch([() => state.apiQuery, () => state.dateRange], () => {
-            if (state.cloudServiceTypeId) {
-                getDataListWithSchema();
-            }
-        });
-
-        watch(() => state.cloudServiceTypeId, async (cloudServiceTypeId) => {
-            if (cloudServiceTypeId) {
-                state.schemaLoading = true;
-                state.dataLoading = true;
-                await getWidgetSchemaList();
-                await getDataListWithSchema();
-                state.schemaLoading = false;
-            }
-        }, { immediate: true });
-
-        return {
-            ...toRefs(state),
-            handleClickShowAll,
-        };
-    },
+const props = withDefaults(defineProps<Props>(), {
+    cloudServiceTypeInfo: () => ({}) as CloudServiceTypeInfo,
+    filters: undefined,
+    period: undefined,
+    hiddenFilters: undefined,
+    keyItemSets: () => [],
 });
+const { t } = useI18n();
+
+const queryHelper = new QueryHelper();
+
+const state = reactive({
+    usageOverviewDetailModalVisible: false,
+    widgetSchemaList: [] as DynamicWidgetSchema[],
+    summaryWidgetSchemaList: computed<DynamicWidgetSchema[]>(() => state.widgetSchemaList.filter(({ type }) => ['summary', 'card'].includes(type))),
+    chartWidgetSchemaList: computed<DynamicWidgetSchema[]>(() => state.widgetSchemaList.filter(({ type }) => type === 'chart')),
+    schemaLoading: true,
+    summaryDataList: [] as Data[],
+    dataLoading: false,
+    cloudServiceTypeId: computed<string>(() => props.cloudServiceTypeInfo.cloud_service_type_id ?? ''),
+    apiQuery: computed<{filter: ApiFilter[]; keyword: string}>(() => {
+        queryHelper.setFilters([]);
+        if (props.filters) {
+            queryHelper.addFilter(...props.filters);
+        }
+        if (props.hiddenFilters) {
+            queryHelper.addFilter(...props.hiddenFilters);
+        }
+        const { filter, keyword } = queryHelper.apiQuery;
+        return { filter, keyword };
+    }),
+    dateRange: computed<Period|undefined>(() => {
+        if (isEmpty(props.period)) return undefined;
+        const period = props.period as Period;
+        const dateRange: Period = {};
+        if (period.start) dateRange.start = dayjs.utc(period.start).format('YYYY-MM-DD');
+        if (period.end) dateRange.end = dayjs.utc(period.end).add(1, 'day').format('YYYY-MM-DD');
+        return dateRange;
+    }),
+});
+
+const fetchSchemaList = async (): Promise<DynamicWidgetSchema[]> => {
+    try {
+        const { provider, group, name } = props.cloudServiceTypeInfo as CloudServiceTypeInfo;
+        const { widget } = await SpaceConnector.client.addOns.pageSchema.get({
+            resource_type: 'inventory.CloudService',
+            schema: 'widget',
+            options: {
+                provider,
+                cloud_service_group: group,
+                cloud_service_type: name,
+                // widget_type: 'chart',
+                // limit: 15,
+            },
+        });
+        return widget ?? [];
+    } catch (e) {
+        ErrorHandler.handleError(e);
+        return [];
+    }
+};
+
+let fetchDataTokenList: Array<CancelTokenSource|undefined> = [];
+
+const fetchDataWithSchema = async (schema: DynamicWidgetSchema, idx: number): Promise<Data> => {
+    fetchDataTokenList[idx] = axios.CancelToken.source();
+
+    try {
+        const { results } = await SpaceConnector.client.inventory.cloudService.analyze({
+            ...state.apiQuery,
+            default_query: schema.query,
+            date_range: state.dateRange,
+        }, { cancelToken: fetchDataTokenList[idx]?.token });
+        fetchDataTokenList[idx] = undefined;
+        return results[0] ?? {};
+    } catch (e: any) {
+        if (!axios.isCancel(e.axiosError)) {
+            ErrorHandler.handleError(e);
+        }
+        return {};
+    }
+};
+
+const cachedSchemaList = {};
+const getWidgetSchemaList = async () => {
+    const schemaList = cachedSchemaList[state.cloudServiceTypeId];
+    if (schemaList) state.widgetSchemaList = schemaList;
+    else state.widgetSchemaList = await fetchSchemaList();
+};
+
+const cancelPreviousDataFetchRequests = () => {
+    fetchDataTokenList.forEach((fetchDataToken) => {
+        if (fetchDataToken) {
+            fetchDataToken.cancel('Next request will called.');
+        }
+    });
+    fetchDataTokenList = [];
+};
+
+const getDataListWithSchema = debounce(async () => {
+    if (!state.dataLoading) state.dataLoading = true;
+    cancelPreviousDataFetchRequests();
+    const results: any = await Promise.allSettled(state.summaryWidgetSchemaList.map((schema, i) => fetchDataWithSchema(schema, i)));
+    state.summaryDataList = results.map((d) => {
+        if (d.status === 'fulfilled') return d.value;
+        return {};
+    });
+    state.dataLoading = false;
+}, 300);
+
+/* Event Handlers */
+const handleClickShowAll = () => {
+    state.usageOverviewDetailModalVisible = true;
+};
+
+/* Watchers */
+watch([() => state.apiQuery, () => state.dateRange], () => {
+    if (state.cloudServiceTypeId) {
+        getDataListWithSchema();
+    }
+});
+
+watch(() => state.cloudServiceTypeId, async (cloudServiceTypeId) => {
+    if (cloudServiceTypeId) {
+        state.schemaLoading = true;
+        state.dataLoading = true;
+        await getWidgetSchemaList();
+        await getDataListWithSchema();
+        state.schemaLoading = false;
+    }
+}, { immediate: true });
+
 </script>
+
+<template>
+    <div class="cloud-service-usage-overview">
+        <header>
+            {{ t('INVENTORY.CLOUD_SERVICE.MAIN.USAGE_OVERVIEW') }}
+            <p-button style-type="tertiary"
+                      size="sm"
+                      :disabled="state.schemaLoading || !state.chartWidgetSchemaList.length"
+                      @click="handleClickShowAll"
+            >
+                {{ t('INVENTORY.CLOUD_SERVICE.MAIN.SHOW_CHARTS') }}
+            </p-button>
+        </header>
+        <cloud-service-usage-overview-summary :schema-loading="state.schemaLoading"
+                                              :data-loading="state.dataLoading"
+                                              :data-list="state.summaryDataList"
+                                              :widget-schema-list="state.summaryWidgetSchemaList"
+                                              :cloud-service-type-id="state.cloudServiceTypeId"
+                                              :filters="props.hiddenFilters"
+        />
+    </div>
+    <cloud-service-usage-overview-detail-modal v-model:visible="state.usageOverviewDetailModalVisible"
+                                               :schema-list="state.widgetSchemaList"
+                                               :summary-data-list="state.summaryDataList"
+                                               :cloud-service-type-info="props.cloudServiceTypeInfo"
+                                               :filters="props.filters"
+                                               :period="props.period"
+                                               :key-item-sets="props.keyItemSets"
+    />
+</template>
 
 <style lang="postcss" scoped>
 .cloud-service-usage-overview {
