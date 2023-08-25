9ead3e48cb94584f2e67a9f2c8ec98e458c849e9
yuda <yuda@megazone.com>
2023-08-25 11:56:32 +0900

    feat: move RegionMap from cost-explorer to reference folder
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ apps/web/src/store/modules/reference/region/config.ts
@@ -0,0 +1,38 @@
+export const RegionMap = {
+    africa: {
+        continent_code: 'africa',
+        continent_label: 'Africa',
+        latitude: 11.081385,
+        longitude: 21.621094,
+    },
+    europe: {
+        continent_code: 'europe',
+        continent_label: 'Europe',
+        latitude: 50.896104,
+        longitude: 19.160156,
+    },
+    north_america: {
+        continent_code: 'north_america',
+        continent_label: 'North America',
+        latitude: 39.563353,
+        longitude: -99.316406,
+    },
+    south_america: {
+        continent_code: 'south_america',
+        continent_label: 'South America',
+        latitude: -13.6631791,
+        longitude: -69.6417454,
+    },
+    asia_pacific: {
+        continent_code: 'asia_pacific',
+        continent_label: 'Asia Pacific',
+        longitude: 103.183594,
+        latitude: 47.212106,
+    },
+    middle_east: {
+        continent_code: 'middle_east',
+        continent_label: 'Middle East',
+        longitude: 26.3842897,
+        latitude: 26.8448363,
+    },
+};
