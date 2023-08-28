20f69da4cc2af3c7b8a9aa1b364fb8122505f522
yuda <yuda@megazone.com>
2023-08-28 19:39:46 +0900

    feat: delete currency selector and edit minor style
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/cost-explorer/cost-analysis/modules/CostAnalysisQueryFilter.vue
+++ apps/web/src/services/cost-explorer/cost-analysis/modules/CostAnalysisQueryFilter.vue
@@ -2,13 +2,11 @@
 import { computed, reactive } from 'vue';
 
 import {
-    PIconButton, PSelectDropdown,
+    PSelectDropdown,
 } from '@spaceone/design-system';
 import type { MenuItem } from '@spaceone/design-system/types/inputs/context-menu/type';
 
 import { i18n } from '@/translations';
-
-import CurrencySelectDropdown from '@/common/modules/dropdown/currency-select-dropdown/CurrencySelectDropdown.vue';
 
 import CostAnalysisPeriodSelectDropdown
     from '@/services/cost-explorer/cost-analysis/modules/CostAnalysisPeriodSelectDropdown.vue';
@@ -17,7 +15,6 @@ import { getInitialDates } from '@/services/cost-explorer/lib/helper';
 import { useCostAnalysisPageStore } from '@/services/cost-explorer/store/cost-analysis-page-store';
 import type { Granularity } from '@/services/cost-explorer/type';
 
-const CostAnalysisSetQueryModal = () => import('@/services/cost-explorer/cost-analysis/modules/CostAnalysisSetQueryModal.vue');
 
 const costAnalysisPageStore = useCostAnalysisPageStore();
 const costAnalysisPageState = costAnalysisPageStore.$state;
@@ -52,47 +49,23 @@ const handleSelectedDates = (period) => {
         _state.period = period;
     });
 };
-const handleClickSetFilter = () => {
-    state.setQueryModalVisible = true;
-};
 </script>
 
 <template>
     <div class="cost-analysis-query-filter">
-        <div class="filter-wrapper tablet-off">
+        <div class="filter-wrapper">
             <div class="left-part">
-                <div class="filter-item">
-                    <b class="label">{{ $t('BILLING.COST_MANAGEMENT.COST_ANALYSIS.GRANULARITY') }}</b>
-                    <p-select-dropdown :items="state.granularityItems"
-                                       :selected="costAnalysisPageState.granularity"
-                                       style-type="transparent"
-                                       class="granularity-select"
-                                       @select="handleSelectGranularity"
-                    />
-                </div>
-            </div>
-            <div class="right-part">
-                <span class="timezone-text">UTC</span>
+                <p-select-dropdown :items="state.granularityItems"
+                                   :selected="costAnalysisPageState.granularity"
+                                   class="granularity-selector"
+                                   @select="handleSelectGranularity"
+                />
                 <cost-analysis-period-select-dropdown :fixed-period="costAnalysisPageState.period"
                                                       @update="handleSelectedDates"
                 />
-                <span class="v-divider" />
-                <currency-select-dropdown />
             </div>
+            <div class="right-part" />
         </div>
-        <div class="filter-wrapper tablet-on">
-            <div class="right-part">
-                <cost-analysis-period-select-dropdown :fixed-period="costAnalysisPageState.period"
-                                                      @update="handleSelectedDates"
-                />
-                <span class="v-divider" />
-                <p-icon-button class="filter-item"
-                               name="ic_settings-filled"
-                               @click="handleClickSetFilter"
-                />
-            </div>
-        </div>
-        <cost-analysis-set-query-modal :visible.sync="state.setQueryModalVisible" />
     </div>
 </template>
 
@@ -102,56 +75,21 @@ const handleClickSetFilter = () => {
         display: flex;
         justify-content: space-between;
         font-size: 0.875rem;
-        .timezone-text {
-            @apply text-gray-400;
-            font-size: 0.875rem;
-            font-weight: bold;
-            line-height: 1.5;
-            padding-right: 0.5rem;
-        }
+        margin-bottom: 1rem;
         .left-part {
             display: flex;
             align-items: center;
-            .filter-item {
-                .p-select-dropdown {
-                    padding-left: 0.5rem;
-                }
+            .granularity-selector {
+                margin-right: 0.5rem;
             }
         }
         .right-part {
             display: flex;
             align-items: center;
         }
-        .filter-item {
-            display: flex;
-            align-items: center;
-        }
-        .v-divider {
-            @apply bg-gray-300;
-            display: inline-block;
-            position: relative;
-            width: 1px;
-            height: 1rem;
-            margin: 0 0.5rem;
-        }
-        .p-select-dropdown {
-            @apply bg-transparent;
-        }
-    }
-    .tablet-on {
-        display: none;
     }
 
     @screen tablet {
-        .tablet-off {
-            display: none;
-        }
-        .tablet-on {
-            display: flex;
-            justify-content: space-between;
-            font-size: 0.875rem;
-            padding-bottom: 1rem;
-        }
         .right-part {
             margin-left: auto;
             flex-wrap: wrap;
@@ -163,9 +101,6 @@ const handleClickSetFilter = () => {
         .filter-wrapper {
             .cost-analysis-period-select-dropdown {
                 width: 100%;
-            }
-            .v-divider {
-                @apply hidden;
             }
         }
     }
