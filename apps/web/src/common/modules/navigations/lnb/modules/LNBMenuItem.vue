e1483fa78ebe1af8bda016be25bab5bd94964228
Piggy Park (박용태) <samuel.park@megazone.com>
2023-08-29 12:32:20 +0900

    fix(l-n-b-menu-item): separate LNBMenuItem component (#1697)
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/common/modules/navigations/lnb/modules/LNBMenuItem.vue
+++ apps/web/src/common/modules/navigations/lnb/modules/LNBMenuItem.vue
@@ -36,49 +36,22 @@
                 <span class="top-title">{{ item.label }}</span>
             </p>
 
-            <div v-if="item.type === MENU_ITEM_TYPE.DIVIDER && showMenu"
-                 class="divider"
+            <l-n-b-divider-menu-item v-if="item.type === MENU_ITEM_TYPE.DIVIDER && showMenu" />
+            <l-n-b-router-menu-item v-if="item.type === MENU_ITEM_TYPE.ITEM && showMenu"
+                                    :item="item"
+                                    :depth="depth"
+                                    :is-domain-owner="isDomainOwner"
+                                    :idx="idx"
+                                    :current-path="currentPath"
             >
-                <p-divider />
-            </div>
-            <router-link
-                v-if="item.type === MENU_ITEM_TYPE.ITEM && showMenu"
-                class="menu-item"
-                :class="[{'second-depth': depth === 2}, {'selected': isSelectedMenu(item.to)}]"
-                :to="item.to"
-                @click.native="$event.stopImmediatePropagation()"
-                @mouseenter.native="hoveredItem = item.id"
-                @mouseleave.native="hoveredItem = ''"
-            >
-                <slot name="before-text"
-                      v-bind="{...$props, item, index: idx}"
-                />
-                <div class="text-wrapper">
-                    <p-i v-if="item.icon"
-                         :name="item.icon"
-                         width="1rem"
-                         height="1rem"
-                         class="icon"
+                <template v-for="(_, slot) of $scopedSlots"
+                          #[slot]="scope"
+                >
+                    <slot :name="slot"
+                          v-bind="scope"
                     />
-                    <span class="text">{{ item.label }}</span>
-                    <slot name="after-text"
-                          v-bind="{...$props, item, index: idx}"
-                    />
-                    <new-mark v-if="item.isNew" />
-                    <beta-mark v-if="item.isBeta" />
-                </div>
-                <slot name="right-extra"
-                      v-bind="{...$props, item, index: idx}"
-                />
-                <favorite-button
-                    v-if="!item.hideFavorite && !isDomainOwner"
-                    :item-id="item.id"
-                    :favorite-type="item.favoriteType ? item.favoriteType : FAVORITE_TYPE.MENU"
-                    :visible-active-case-only="!getIsHovered(item.id)"
-                    scale="0.8"
-                    class="favorite-button"
-                />
-            </router-link>
+                </template>
+            </l-n-b-router-menu-item>
         </div>
     </div>
 </template>
@@ -88,18 +61,15 @@ import type { PropType } from 'vue';
 import {
     computed, defineComponent, reactive, toRefs,
 } from 'vue';
-import type { Location } from 'vue-router';
 
-import { PDivider, PI } from '@spaceone/design-system';
+import { PI } from '@spaceone/design-system';
 
-import { SpaceRouter } from '@/router';
 import { store } from '@/store';
-
-import { FAVORITE_TYPE } from '@/store/modules/favorite/type';
 
 import BetaMark from '@/common/components/marks/BetaMark.vue';
 import NewMark from '@/common/components/marks/NewMark.vue';
-import FavoriteButton from '@/common/modules/favorites/favorite-button/FavoriteButton.vue';
+import LNBDividerMenuItem from '@/common/modules/navigations/lnb/modules/LNBDividerMenuItem.vue';
+import LNBRouterMenuItem from '@/common/modules/navigations/lnb/modules/LNBRouterMenuItem.vue';
 import type { LNBMenu } from '@/common/modules/navigations/lnb/type';
 import { MENU_ITEM_TYPE } from '@/common/modules/navigations/lnb/type';
 
@@ -111,11 +81,11 @@ interface Props {
 export default defineComponent<Props>({
     name: 'LNBMenuItem',
     components: {
-        FavoriteButton,
         PI,
         BetaMark,
         NewMark,
-        PDivider,
+        LNBRouterMenuItem,
+        LNBDividerMenuItem,
     },
     props: {
         menuData: {
@@ -135,7 +105,7 @@ export default defineComponent<Props>({
     setup(props) {
         const state = reactive({
             isDomainOwner: computed(() => store.getters['user/isDomainOwner']),
-            processedMenuData: computed(() => (Array.isArray(props.menuData) ? props.menuData : [props.menuData])),
+            processedMenuData: computed<LNBMenu>(() => (Array.isArray(props.menuData) ? props.menuData : [props.menuData])),
             isFolded: false,
             isFoldableMenu: computed(() => state.processedMenuData?.some((item) => item.foldable)),
             showMenu: computed(() => (state.isFoldableMenu && !state.isFolded) || !state.isFoldableMenu), // toggle menu
@@ -146,31 +116,10 @@ export default defineComponent<Props>({
             state.isFolded = !state.isFolded;
         };
 
-        const isSelectedMenu = (selectedMenuRoute: Location): boolean => {
-            let currentPath = props.currentPath;
-            if (!currentPath) return false;
-
-            const resolved = SpaceRouter.router.resolve(selectedMenuRoute);
-            if (!resolved) return false;
-
-            if (currentPath.indexOf('?') > 0) {
-                currentPath = currentPath.slice(0, currentPath.indexOf('?'));
-            }
-            let resolvedHref = resolved.href;
-            if (!currentPath.endsWith('/')) currentPath += '/';
-            if (!resolvedHref.endsWith('/')) resolvedHref += '/';
-            return currentPath.startsWith(resolvedHref);
-        };
-
-        const getIsHovered = (itemId: string) => state.hoveredItem && state.hoveredItem === itemId;
-
         return {
             ...toRefs(state),
             handleFoldableToggle,
-            isSelectedMenu,
-            FAVORITE_TYPE,
             MENU_ITEM_TYPE,
-            getIsHovered,
         };
     },
 });
@@ -205,51 +154,6 @@ export default defineComponent<Props>({
         padding-top: 1.25rem;
         padding-left: 0.5rem;
         padding-bottom: 0.75rem;
-    }
-    .menu-item {
-        @apply border border-transparent inline-flex items-center w-full h-full justify-between;
-        font-size: 0.875rem;
-        line-height: 125%;
-        border-radius: 4px;
-        box-sizing: border-box;
-        padding-left: 0.5rem;
-        padding-right: 0.5rem;
-        outline: 0;
-        height: 2rem;
-
-        &.second-depth {
-            padding-left: 1.25rem;
-        }
-        &:focus, &:focus-within, &:active {
-            @apply bg-white border-secondary1;
-            box-shadow: 0 0 0 2px rgba(theme('colors.secondary1'), 0.2);
-        }
-        &.selected {
-            @apply bg-blue-200;
-        }
-        &:hover {
-            @apply bg-blue-100 cursor-pointer;
-        }
-        .text-wrapper {
-            @apply inline-flex overflow-hidden whitespace-no-wrap;
-            .text {
-                @apply overflow-hidden whitespace-no-wrap;
-                text-overflow: ellipsis;
-            }
-            .icon {
-                flex-shrink: 0;
-                margin-right: 0.25rem;
-            }
-        }
-        .favorite-button {
-            flex-shrink: 0;
-            margin-left: 0.25rem;
-        }
-    }
-    .divider {
-        margin-top: 1.25rem;
-        margin-bottom: 1.25rem;
-        height: 0;
     }
 }
 </style>
