0589c767a0dcd4b8b84cc0e6734a88fccf0705d1
sulmo <50662170+sulmoJ@users.noreply.github.com>
2023-08-29 10:01:45 +0900

    refactor(budget-page): refactor to script setup (#1694)
    * fix(budget): swap the locations of script and template
    
    Signed-off-by: sulmo <sulmo@megazone.com>
    
    * refactor(budget-page): refactor to script setup
    
    Signed-off-by: sulmo <sulmo@megazone.com>
    
    * refactor(budget-list): refactor to script setup
    
    Signed-off-by: sulmo <sulmo@megazone.com>
    
    ---------
    
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/services/cost-explorer/budget/BudgetPage.vue
+++ apps/web/src/services/cost-explorer/budget/BudgetPage.vue
@@ -1,36 +1,5 @@
-<template>
-    <div class="budget-page">
-        <p-heading :title="$t('BILLING.COST_MANAGEMENT.MAIN.BUDGET')">
-            <template #extra>
-                <!--                <p-select-dropdown-->
-                <!--                    class="create-budget-box"-->
-                <!--                    use-fixed-menu-style-->
-                <!--                    :items="createButtonItemList"-->
-                <!--                    :placeholder="$t('BILLING.COST_MANAGEMENT.BUDGET.MAIN.CREATE_BUDGET')"-->
-                <!--                    type="outline-button"-->
-                <!--                    @select="handleCreateBudgetSelect"-->
-                <!--                />-->
-                <p-button style-type="secondary"
-                          icon-left="ic_plus_bold"
-                          :disabled="!hasManagePermission"
-                          @click="handleCreateBudgetSelect(createButtonItemList[0].name)"
-                >
-                    {{ $t('BILLING.COST_MANAGEMENT.BUDGET.MAIN.CREATE') }}
-                </p-button>
-            </template>
-        </p-heading>
-        <p-divider />
-        <budget-list :filters="filters"
-                     @update:filters="handleUpdateFilters"
-        />
-    </div>
-</template>
-
-<script lang="ts">
-import {
-    computed, getCurrentInstance, reactive, toRefs,
-} from 'vue';
-import type { Vue } from 'vue/types/vue';
+<script setup lang="ts">
+import { computed, reactive } from 'vue';
 
 import {
     PHeading, PDivider, PButton,
@@ -47,55 +16,64 @@ import { useManagePermissionState } from '@/common/composables/page-manage-permi
 import BudgetList from '@/services/cost-explorer/budget/modules/budget-list/BudgetList.vue';
 import { COST_EXPLORER_ROUTE } from '@/services/cost-explorer/route-config';
 
-export default {
-    name: 'BudgetPage',
-    components: {
-        BudgetList,
-        PHeading,
-        PDivider,
-        PButton,
-    },
-    setup() {
-        const vm = getCurrentInstance()?.proxy as Vue;
+const currentRoute = SpaceRouter.router.currentRoute;
 
-        const queryHelper = new QueryHelper();
+const queryHelper = new QueryHelper();
 
-        const state = reactive({
-            createButtonItemList: computed(() => [
-                {
-                    label: i18n.t('BILLING.COST_MANAGEMENT.BUDGET.MAIN.SINGLE_BUDGET'),
-                    name: COST_EXPLORER_ROUTE.BUDGET.CREATE._NAME,
-                },
-                // {
-                //     label: i18n.t('BILLING.COST_MANAGEMENT.BUDGET.MAIN.BULK_BUDGET'),
-                //     name: COST_EXPLORER_ROUTE.BUDGET.BULK_CREATE._NAME,
-                // },
-            ]),
-            filters: queryHelper.setFiltersAsRawQueryString(vm.$route.query.filters).filters,
-            hasManagePermission: useManagePermissionState(),
-        });
+const state = reactive({
+    createButtonItemList: computed(() => [
+        {
+            label: i18n.t('BILLING.COST_MANAGEMENT.BUDGET.MAIN.SINGLE_BUDGET'),
+            name: COST_EXPLORER_ROUTE.BUDGET.CREATE._NAME,
+        },
+    ]),
+    filters: queryHelper.setFiltersAsRawQueryString(currentRoute.query.filters).filters,
+    hasManagePermission: useManagePermissionState(),
+});
 
-        const handleCreateBudgetSelect = (name) => {
-            SpaceRouter.router.push({ name });
-        };
-
-        const handleUpdateFilters = (filters: ConsoleFilter[]) => {
-            state.filters = filters;
-            SpaceRouter.router.replace({
-                query: {
-                    filters: queryHelper.setFilters(filters).rawQueryStrings,
-                },
-            });
-        };
-
-        return {
-            ...toRefs(state),
-            handleCreateBudgetSelect,
-            handleUpdateFilters,
-        };
-    },
+const handleCreateBudgetSelect = (name) => {
+    SpaceRouter.router.push({ name });
 };
+
+const handleUpdateFilters = (filters: ConsoleFilter[]) => {
+    state.filters = filters;
+    SpaceRouter.router.replace({
+        query: {
+            filters: queryHelper.setFilters(filters).rawQueryStrings,
+        },
+    });
+};
+
 </script>
+
+<template>
+    <div class="budget-page">
+        <p-heading :title="$t('BILLING.COST_MANAGEMENT.MAIN.BUDGET')">
+            <template #extra>
+                <!--                <p-select-dropdown-->
+                <!--                    class="create-budget-box"-->
+                <!--                    use-fixed-menu-style-->
+                <!--                    :items="createButtonItemList"-->
+                <!--                    :placeholder="$t('BILLING.COST_MANAGEMENT.BUDGET.MAIN.CREATE_BUDGET')"-->
+                <!--                    type="outline-button"-->
+                <!--                    @select="handleCreateBudgetSelect"-->
+                <!--                />-->
+                <p-button style-type="secondary"
+                          icon-left="ic_plus_bold"
+                          :disabled="!state.hasManagePermission"
+                          @click="handleCreateBudgetSelect(state.createButtonItemList[0].name)"
+                >
+                    {{ $t('BILLING.COST_MANAGEMENT.BUDGET.MAIN.CREATE') }}
+                </p-button>
+            </template>
+        </p-heading>
+        <p-divider />
+        <budget-list :filters="state.filters"
+                     @update:filters="handleUpdateFilters"
+        />
+    </div>
+</template>
+
 <style scoped lang="postcss">
 .budget-page {
     .create-budget-box {
