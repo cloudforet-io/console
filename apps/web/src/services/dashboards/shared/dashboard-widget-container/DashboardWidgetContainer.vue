6fde55d73cd3b4ff8d299b66422f55ca759e78a7
WANJIN <wanjin@megazone.com>
2023-09-01 13:48:43 +0900

    refactor: move validation logics to widget-validation-helper and remove duplicated logic (#1726)
    * refactor: remove duplicated widget validation hook
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    * refactor: move validation logics to widget-validation-helper
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    * chore: fix lint error
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>
    
    ---------
    
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/dashboards/shared/dashboard-widget-container/DashboardWidgetContainer.vue
+++ apps/web/src/services/dashboards/shared/dashboard-widget-container/DashboardWidgetContainer.vue
@@ -19,9 +19,6 @@ import {
 import {
     useWidgetReformer,
 } from '@/services/dashboards/shared/dashboard-widget-container/composables/use-widget-reformer';
-import {
-    useWidgetValidator,
-} from '@/services/dashboards/shared/dashboard-widget-container/composables/use-widget-validator';
 import { useDashboardDetailInfoStore } from '@/services/dashboards/store/dashboard-detail-info';
 import WidgetViewModeModal from '@/services/dashboards/widgets/_components/WidgetViewModeModal.vue';
 import type {
@@ -55,23 +52,11 @@ const containerRef = ref<HTMLElement|null>(null);
 const { containerWidth } = useContainerWidth({ containerRef, observeResize: true });
 
 /* reform widget info list */
-const { reformedWidgetInfoList, widgetConfigMap } = useWidgetReformer({
+const { reformedWidgetInfoList } = useWidgetReformer({
     dashboardWidgetInfoList: toRef(dashboardDetailState, 'dashboardWidgetInfoList'),
     containerWidth,
 });
 
-/* widget validation */
-useWidgetValidator({
-    validateOnVariablesSchemaChange: computed(() => props.editMode),
-    dashboardWidgetInfoList: reformedWidgetInfoList,
-    widgetConfigMap,
-    variablesSchema: toRef(dashboardDetailState, 'variablesSchema'),
-    updateWidgetValidMap: (validMap) => {
-        dashboardDetailStore.$patch((_state) => {
-            _state.widgetValidMap = validMap;
-        });
-    },
-});
 
 /* init widgets */
 const handleIntersectionObserver = async ([{ isIntersecting, target }]) => {
@@ -88,7 +73,7 @@ const handleIntersectionObserver = async ([{ isIntersecting, target }]) => {
         }
     }
 };
-const handleUpdateData = (widgetKey: string, data: any) => {
+const handleWidgetRefreshed = (widgetKey: string, data: any) => {
     dashboardDetailStore.$patch((_state) => {
         _state.widgetDataMap[widgetKey] = data;
     });
@@ -201,25 +186,27 @@ onMounted(async () => {
                            :dashboard-settings="dashboardDetailState.settings"
                            :dashboard-variables-schema="dashboardDetailState.variablesSchema"
                            :dashboard-variables="dashboardDetailState.variables"
-                           @update-data="handleUpdateData(widget.widget_key, $event)"
+                           @refreshed="handleWidgetRefreshed(widget.widget_key, $event)"
                            @update-widget-info="handleUpdateWidgetInfo(widget.widget_key, $event)"
                            @update-widget-validation="handleUpdateValidation(widget.widget_key, $event)"
                 />
-                <!--
+                <!-- TODO: remove this comment after refactoring
                 <component :is="widget.component"
                            :id="widget.widget_key"
                            :key="widget.widget_key"
                            ref="widgetRef"
                            v-intersection-observer="handleIntersectionObserver"
                            :widget-info="widget"
-                           :dashboard-info="dashboardDetailState.dashboardInfo"
-                           :currency-rates="currencyRates"
                            :edit-mode="editMode"
                            :error-mode="editMode && dashboardDetailState.widgetValidMap[widget.widget_key] === false"
-                           :all-reference-type-info="allReferenceTypeInfo"
-                           :initiated="!!initiatedWidgetMap[widget.widget_key]"
                            :disable-refresh-on-variable-change="dashboardDetailState.widgetViewModeModalVisible && widget.widget_key !== widgetFormState.widgetKey"
-                           @update-data="handleUpdateData(widget.widget_key, $event)"
+                           :initiated="!!initiatedWidgetMap[widget.widget_key]"
+                           :currency-rates="currencyRates"
+                           :all-reference-type-info="allReferenceTypeInfo"
+                           :settings="dashboardDetailState.settings"
+                           :variables-schema="dashboardDetailState.variablesSchema"
+                           :variables="dashboardDetailState.variables"
+                           @refreshed="handleWidgetRefreshed(widget.widget_key, $event)"
                            @update-widget-info="handleUpdateWidgetInfo(widget.widget_key, $event)"
                            @update-widget-validation="handleUpdateValidation(widget.widget_key, $event)"
                 />
