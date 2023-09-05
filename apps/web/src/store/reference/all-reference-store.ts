d2a0fdb71802a1b8302290ce2df5d074090beb5f
Wanjin Noh <wanjin@megazone.com>
2023-09-05 18:27:29 +0900

    feat: make all reference store getters load on read time
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/store/reference/all-reference-store.ts
+++ apps/web/src/store/reference/all-reference-store.ts
@@ -1,4 +1,5 @@
-import { computed, reactive } from 'vue';
+import { asyncComputed } from '@vueuse/core';
+import { reactive } from 'vue';
 
 import { camelCase } from 'lodash';
 import { defineStore } from 'pinia';
@@ -32,26 +33,74 @@ export const useAllReferenceStore = defineStore('all-reference-store', () => {
     const costDataSourceReferenceStore = useCostDataSourceReferenceStore();
 
     const getters = reactive({
-        allReferenceTypeInfo: computed(() => ({
-            ...store.getters['reference/allReferenceTypeInfo'],
-            costDataSource: costDataSourceReferenceStore.getters.costDataSourceTypeInfo,
-            cost_data_source: costDataSourceReferenceStore.getters.costDataSourceTypeInfo,
-        })),
-        projectGroup: computed(() => store.getters['reference/projectGroupItems']),
-        project_group: computed(() => store.getters['reference/projectGroupItems']),
-        project: computed(() => store.getters['reference/projectItems']),
-        protocol: computed(() => store.getters['reference/protocolItems']),
-        cloudServiceType: computed(() => store.getters['reference/cloudServiceTypeItems']),
-        cloud_service_type: computed(() => store.getters['reference/cloudServiceTypeItems']),
-        collector: computed(() => store.getters['reference/collectorItems']),
-        plugin: computed(() => store.getters['reference/pluginItems']),
-        provider: computed(() => store.getters['reference/providerItems']),
-        region: computed(() => store.getters['reference/regionItems']),
-        secret: computed(() => store.getters['reference/secretItems']),
-        serviceAccount: computed(() => store.getters['reference/serviceAccountItems']),
-        service_account: computed(() => store.getters['reference/serviceAccountItems']),
-        user: computed(() => store.getters['reference/userItems']),
-        webHook: computed(() => store.getters['reference/webHookItems']),
+        allReferenceTypeInfo: asyncComputed(async () => {
+            await actions.loadAll();
+            return {
+                ...store.getters['reference/allReferenceTypeInfo'],
+                costDataSource: costDataSourceReferenceStore.getters.costDataSourceTypeInfo,
+                cost_data_source: costDataSourceReferenceStore.getters.costDataSourceTypeInfo,
+            };
+        }),
+        projectGroup: asyncComputed(async () => {
+            await store.dispatch('reference/projectGroup/load');
+            return store.getters['reference/projectGroupItems'];
+        }),
+        project_group: asyncComputed(async () => {
+            await store.dispatch('reference/project_group/load');
+            return store.getters['reference/projectGroupItems'];
+        }),
+        project: asyncComputed(async () => {
+            await store.dispatch('reference/project/load');
+            return store.getters['reference/projectItems'];
+        }),
+        protocol: asyncComputed(async () => {
+            await store.dispatch('reference/protocol/load');
+            return store.getters['reference/protocolItems'];
+        }),
+        cloudServiceType: asyncComputed(async () => {
+            await store.dispatch('reference/cloudServiceType/load');
+            return store.getters['reference/cloudServiceTypeItems'];
+        }),
+        cloud_service_type: asyncComputed(async () => {
+            await store.dispatch('reference/cloud_service_type/load');
+            return store.getters['reference/cloudServiceTypeItems'];
+        }),
+        collector: asyncComputed(async () => {
+            await store.dispatch('reference/collector/load');
+            return store.getters['reference/collectorItems'];
+        }),
+        plugin: asyncComputed(async () => {
+            await store.dispatch('reference/plugin/load');
+            return store.getters['reference/pluginItems'];
+        }),
+        provider: asyncComputed(async () => {
+            await store.dispatch('reference/provider/load');
+            return store.getters['reference/providerItems'];
+        }),
+        region: asyncComputed(async () => {
+            await store.dispatch('reference/region/load');
+            return store.getters['reference/regionItems'];
+        }),
+        secret: asyncComputed(async () => {
+            await store.dispatch('reference/secret/load');
+            return store.getters['reference/secretItems'];
+        }),
+        serviceAccount: asyncComputed(async () => {
+            await store.dispatch('reference/serviceAccount/load');
+            return store.getters['reference/serviceAccountItems'];
+        }),
+        service_account: asyncComputed(async () => {
+            await store.dispatch('reference/service_account/load');
+            return store.getters['reference/serviceAccountItems'];
+        }),
+        user: asyncComputed(async () => {
+            await store.dispatch('reference/user/load');
+            return store.getters['reference/userItems'];
+        }),
+        webHook: asyncComputed(async () => {
+            await store.dispatch('reference/webHook/load');
+            return store.getters['reference/webHookItems'];
+        }),
     });
 
     const actions = {
