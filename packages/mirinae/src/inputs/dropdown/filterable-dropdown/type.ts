09cd67a2b86f3c49c4146298f761bc1316402fe4
NaYeong,Kim <nayeongkim@megazone.com>
2023-09-07 16:56:09 +0900

    feat: merge for updated p-filterable-dropdown spec (#1775)
    * feat: update filterable dropdown spec (#1771)
    
    * refactor: refactoring basic code
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * feat: update css style
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * feat: update filterable dropdown spec
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * chore: changed text at storybook
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * fix: changed binding event naming
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * fix: changed dropdownMenuItem to original
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * chore: remove line-height (#1774)
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- packages/mirinae/src/inputs/dropdown/filterable-dropdown/type.ts
+++ packages/mirinae/src/inputs/dropdown/filterable-dropdown/type.ts
@@ -2,7 +2,7 @@ import type { MenuItem } from '@/inputs/context-menu/type';
 
 export type FilterableDropdownMenuItem = MenuItem;
 
-export interface HandlerRes {
+interface HandlerRes {
     results: FilterableDropdownMenuItem[];
     totalCount?: number;
     more?: boolean;
@@ -11,12 +11,9 @@ export interface AutocompleteHandler {
     (inputText: string, pageStart?: number, pageLimit?: number): Promise<HandlerRes>|HandlerRes;
 }
 
-export const FILTERABLE_DROPDOWN_TYPE = Object.freeze({
-    default: 'default',
-    radioButton: 'radioButton',
-} as const);
-
-export type FILTERABLE_DROPDOWN_TYPE = typeof FILTERABLE_DROPDOWN_TYPE[keyof typeof FILTERABLE_DROPDOWN_TYPE];
-
 export const FILTERABLE_DROPDOWN_APPEARANCE_TYPES = ['basic', 'stack', 'badge'] as const;
+export const FILTERABLE_DROPDOWN_STYLE_TYPES = ['basic', 'rounded'] as const;
+
+
 export type FilterableDropdownAppearanceType = typeof FILTERABLE_DROPDOWN_APPEARANCE_TYPES[number];
+export type FilterableDropdownStyleType = typeof FILTERABLE_DROPDOWN_STYLE_TYPES[number];
