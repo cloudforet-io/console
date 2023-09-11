4ca63006f71d16968e2cd1b1fdffa1d241a8cd68
yuda <yuda@megazone.com>
2023-09-11 19:34:50 +0900

    feat: create `Add More` button to filters popper
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ apps/web/src/services/cost-explorer/cost-analysis/modules/CostAnalysisFiltersAddMoreButton.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+import { onClickOutside } from '@vueuse/core';
+import {
+    computed, defineEmits, reactive, ref, toRef, watch,
+} from 'vue';
+
+import {
+    PButton, PContextMenu, useContextMenuController,
+} from '@spaceone/design-system';
+import type { MenuItem } from '@spaceone/design-system/types/inputs/context-menu/type';
+
+import { useCostAnalysisPageStore } from '@/services/cost-explorer/store/cost-analysis-page-store';
+
+
+const emit = defineEmits<{(e: 'disable-filter', value: string): void;
+    (e: 'disable-all-filters'): void;
+}>();
+
+const costAnalysisPageStore = useCostAnalysisPageStore();
+const costAnalysisPageState = costAnalysisPageStore.$state;
+
+const state = reactive({
+    menuItems: computed<MenuItem[]>(() => costAnalysisPageStore.defaultGroupByItems),
+    selectedItems: [] as MenuItem[],
+    searchText: '',
+});
+
+const contextMenuRef = ref<any|null>(null);
+const targetRef = ref<HTMLElement | null>(null);
+const {
+    visibleMenu,
+    refinedMenu,
+    contextMenuStyle,
+    showContextMenu,
+    hideContextMenu,
+    initiateMenu,
+} = useContextMenuController({
+    targetRef,
+    contextMenuRef,
+    useReorderBySelection: true,
+    menu: toRef(state, 'menuItems'),
+    selected: toRef(state, 'selectedItems'),
+    searchText: toRef(state, 'searchText'),
+});
+onClickOutside(contextMenuRef, hideContextMenu);
+
+const handleClickAddMore = () => {
+    if (visibleMenu.value) {
+        hideContextMenu();
+    } else {
+        initiateMenu();
+        showContextMenu();
+    }
+};
+const handleSelectAddMoreMenuItem = (item: MenuItem, _, isSelected: boolean) => {
+    costAnalysisPageStore.$patch((_state) => {
+        if (isSelected) {
+            _state.enabledFiltersProperties = [...(_state.enabledFiltersProperties ?? []), item.name as string];
+        } else {
+            _state.enabledFiltersProperties = _state.enabledFiltersProperties?.filter((d) => d !== item.name);
+            emit('disable-filter', item.name as string);
+        }
+    });
+};
+const handleClearAddMoreMenuItems = () => {
+    costAnalysisPageStore.$patch((_state) => {
+        _state.enabledFiltersProperties = [];
+        emit('disable-all-filters');
+    });
+};
+
+watch(() => costAnalysisPageState.enabledFiltersProperties, (_enabledFiltersProperties) => {
+    if (_enabledFiltersProperties?.length) {
+        state.selectedItems = state.menuItems.filter((d) => _enabledFiltersProperties.includes(d.name));
+    }
+});
+</script>
+
+<template>
+    <div class="cost-analysis-add-more-button">
+        <p-button ref="targetRef"
+                  style-type="transparent"
+                  icon-left="ic_plus_bold"
+                  @click="handleClickAddMore"
+        >
+            Add More
+        </p-button>
+        <p-context-menu v-show="visibleMenu"
+                        ref="contextMenuRef"
+                        class="add-more-context-menu"
+                        searchable
+                        :search-text="state.searchText"
+                        :style="contextMenuStyle"
+                        :menu="refinedMenu"
+                        :selected="state.selectedItems"
+                        multi-selectable
+                        show-select-marker
+                        show-clear-selection
+                        @select="handleSelectAddMoreMenuItem"
+                        @clear-selection="handleClearAddMoreMenuItems"
+        />
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+.cost-analysis-add-more-button {
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+    .add-more-context-menu {
+        min-width: 10rem;
+    }
+}
+</style>
