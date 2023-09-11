9e3661cc17441ae00d3e56165c9f8214626f84f1
Dahyun Yu <yuda@megazone.com>
2023-09-11 11:34:18 +0900

    feat: watch only `selectedQuerySet` in CostAnalysisPage (#1795)
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/cost-explorer/cost-analysis/CostAnalysisPage.vue
+++ apps/web/src/services/cost-explorer/cost-analysis/CostAnalysisPage.vue
@@ -4,14 +4,9 @@ import {
 } from 'vue';
 import { useRoute } from 'vue-router/composables';
 
-import { isEqual } from 'lodash';
-
 import { SpaceRouter } from '@/router';
 
 import {
-    arrayToQueryString,
-    objectToQueryString,
-    primitiveToQueryString,
     queryStringToArray,
     queryStringToObject,
     queryStringToString,
@@ -29,98 +24,35 @@ import type {
 } from '@/services/cost-explorer/type';
 
 
-interface Props {
-    costQuerySetId?: string;
-}
-const props = defineProps<Props>();
 const route = useRoute();
 
 const costAnalysisPageStore = useCostAnalysisPageStore();
-const costAnalysisPageState = costAnalysisPageStore.$state;
 
 /* util */
-const setQueryOptions = (options?: CostQuerySetModel['options']) => {
-    if (options) costAnalysisPageStore.setQueryOptions(options);
-    else costAnalysisPageStore.initState();
-};
-
 const getQueryOptionsFromUrlQuery = (urlQuery: CostAnalysisPageUrlQuery): CostQuerySetModel['options'] => ({
     granularity: queryStringToString(urlQuery.granularity) as Granularity,
     group_by: queryStringToArray(urlQuery.group_by),
-    period: queryStringToObject(urlQuery.period) ?? {},
+    period: queryStringToObject(urlQuery.period),
+    relative_period: queryStringToObject(urlQuery.relative_period),
     filters: queryStringToObject(urlQuery.filters),
 });
 
-const getQueryWithKey = (queryItemKey: string): Partial<CostQuerySetModel> => (costAnalysisPageStore.costQueryList.find((item) => item.cost_query_set_id === queryItemKey)) || {};
-
-let unregisterStoreWatch;
-const registerStoreWatch = (currentQuery) => {
-    unregisterStoreWatch = watch(() => costAnalysisPageState, () => {
-        if (props.costQuerySetId) return;
-
-        const newQuery: CostAnalysisPageUrlQuery = {
-            granularity: primitiveToQueryString(costAnalysisPageState.granularity),
-            group_by: arrayToQueryString(costAnalysisPageState.groupBy),
-            period: objectToQueryString(costAnalysisPageState.period),
-            filters: objectToQueryString(costAnalysisPageStore.filters),
-        };
-
-
-        if (!isEqual(newQuery, currentQuery)) {
-            SpaceRouter.router.replace({ query: newQuery }).catch((e) => {
-                if (e.name !== 'NavigationDuplicated') console.error(e);
-            });
-        }
-    }, { immediate: false });
-};
-
 onUnmounted(() => {
-    if (unregisterStoreWatch) {
-        unregisterStoreWatch();
-    }
     costAnalysisPageStore.$dispose();
     costAnalysisPageStore.$reset();
 });
 
-watch(() => route.params, async (after, before) => {
-    const costQuerySetId = after.costQuerySetId;
-
-    if (costQuerySetId === before?.costQuerySetId) return;
-
-    if (costQuerySetId) {
-        const { options } = getQueryWithKey(costQuerySetId);
-        await costAnalysisPageStore.setQueryOptions(options);
-        costAnalysisPageStore.selectQueryId(costQuerySetId);
+watch(() => costAnalysisPageStore.selectedQuerySet, async (selectedQuerySet) => {
+    if (selectedQuerySet) {
+        await costAnalysisPageStore.setQueryOptions(selectedQuerySet.options);
+    } else if (route.params.costQuerySetId === 'dynamic') {
+        const currentQuery = SpaceRouter.router.currentRoute.query;
+        const costQuerySetOptions = getQueryOptionsFromUrlQuery(currentQuery);
+        await costAnalysisPageStore.setQueryOptions(costQuerySetOptions);
     } else {
         await costAnalysisPageStore.setQueryOptions();
-        costAnalysisPageStore.selectQueryId(undefined);
     }
 }, { immediate: true });
-
-/* Page Init */
-(async () => {
-    const currentQuery = SpaceRouter.router.currentRoute.query;
-
-    // init states
-    if (props.costQuerySetId) {
-        const { name, options } = getQueryWithKey(props.costQuerySetId);
-        if (name) {
-            setQueryOptions(options);
-            costAnalysisPageStore.selectQueryId(props.costQuerySetId);
-        } else {
-            costAnalysisPageStore.selectQueryId(undefined);
-        }
-    } else if (Object.keys(currentQuery).length) {
-        const options = getQueryOptionsFromUrlQuery(currentQuery);
-        setQueryOptions(options);
-    } else {
-        await costAnalysisPageStore.initState();
-    }
-
-    // register store watch
-    registerStoreWatch(currentQuery);
-})();
-
 </script>
 
 <template>
