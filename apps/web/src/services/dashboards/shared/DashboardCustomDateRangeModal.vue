327e5014b61c2d702c10a81e8c104d6009f4500b
Yongtae Park <samuel.park@megazone.com>
2023-09-14 15:20:22 +0900

    feat(dashboard-date-range-modal): create new custom date range modal
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/services/dashboards/shared/DashboardCustomDateRangeModal.vue
@@ -0,0 +1,98 @@
+<script setup lang="ts">
+import { computed, reactive, watch } from 'vue';
+
+import {
+    PButtonModal, PFieldGroup, PDatetimePicker, useProxyValue,
+} from '@spaceone/design-system';
+import dayjs from 'dayjs';
+
+import type { Period } from '@/services/cost-explorer/type';
+
+const props = withDefaults(defineProps<{
+    visible: boolean;
+    selectedDateRange?: Period;
+}>(), {
+    visible: false,
+    selectedDateRange: () => ({}),
+});
+
+const emit = defineEmits<{(event: 'update:visible', visible: boolean): void;
+    (event: 'confirm', period: Period): void;
+}>();
+
+const state = reactive({
+    proxyVisible: useProxyValue('visible', props, emit),
+    dateType: 'yearToMonth',
+    invalid: computed(() => !state.selectedDates.length),
+    selectedDates: [] as string[],
+});
+
+const handleUpdateVisible = (visible: boolean) => {
+    state.proxyVisible = visible;
+};
+const handleUpdateSelectedDates = (selectedDates: string[]) => {
+    if (!selectedDates.length) return;
+    const originDates = state.selectedDates;
+
+    if (dayjs.utc(originDates[0]).isSame(dayjs.utc(selectedDates[0]), 'day')) return;
+    state.selectedDates = selectedDates;
+};
+const handleConfirm = () => {
+    state.proxyVisible = false;
+    const period: Period = {};
+    period.start = dayjs.utc(state.selectedDates[0]).format('YYYY-MM-01');
+    period.end = dayjs.utc(state.selectedDates[0]).endOf('month').format('YYYY-MM-DD');
+    emit('confirm', period);
+};
+
+watch(() => props.visible, (visible) => {
+    if (visible) {
+        if (props.selectedDateRange?.start) {
+            state.selectedDates = [props.selectedDateRange?.start];
+        } else {
+            state.selectedDates = [];
+        }
+    } else {
+        state.selectedDates = [];
+    }
+});
+
+</script>
+
+<template>
+    <p-button-modal :header-title="$t('Apply Custom Range')"
+                    centered
+                    size="sm"
+                    fade
+                    backdrop
+                    :visible.sync="state.proxyVisible"
+                    :disable="state.invalid"
+                    @update:visible="handleUpdateVisible"
+                    @confirm="handleConfirm"
+    >
+        <template #body>
+            <p-field-group class="period-select"
+                           :label="$t('Month')"
+                           required
+            >
+                <p-datetime-picker class="datetime-picker"
+                                   :data-type="state.dateType"
+                                   :selected-dates="state.selectedDates"
+                                   :invalid="state.invalid"
+                                   @update:selected-dates="handleUpdateSelectedDates"
+                />
+            </p-field-group>
+        </template>
+        <template #confirm-button>
+            {{ $t('Apply') }}
+        </template>
+    </p-button-modal>
+</template>
+
+<style scoped lang="postcss">
+.period-select {
+    .datetime-picker {
+        width: 100%;
+    }
+}
+</style>
