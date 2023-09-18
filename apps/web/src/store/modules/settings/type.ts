7edce260cbe2292d45297050a2ae10f19777b258
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2023-09-18 10:03:58 +0900

    refactor: remove store exchange rate
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>

--- apps/web/src/store/modules/settings/type.ts
+++ apps/web/src/store/modules/settings/type.ts
@@ -3,12 +3,7 @@ import type { CURRENCY, CURRENCY_SYMBOL } from '@/store/modules/settings/config'
 export type Currency = typeof CURRENCY[keyof typeof CURRENCY];
 export type CurrencySymbol = typeof CURRENCY_SYMBOL[keyof typeof CURRENCY_SYMBOL];
 
-export type CurrencyRates = {
-    [K in Currency]: number;
-};
-
 export interface SettingsState {
-    currencyRates: CurrencyRates;
     currencyUpdateTime: number|undefined;
     currency: Currency;
     gnbNotificationLastReadTime: string;
