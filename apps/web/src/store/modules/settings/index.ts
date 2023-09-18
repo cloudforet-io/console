5f3544ad40ff6a9f88902290215b5711e5697479
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2023-09-18 11:32:49 +0900

    refactor: remove store currency
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>

--- apps/web/src/store/modules/settings/index.ts
+++ apps/web/src/store/modules/settings/index.ts
@@ -1,17 +1,14 @@
 import * as actions from './actions';
-import * as getters from './getters';
 import * as mutations from './mutations';
 import type { SettingsState } from './type';
 
 const state: SettingsState = {
-    currency: 'USD',
     gnbNotificationLastReadTime: '',
 };
 
 export default {
     namespaced: true,
     state,
-    getters,
     actions,
     mutations,
 };
