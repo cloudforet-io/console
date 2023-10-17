79e10144f5e744eafc32acacf06fb99b17154f40
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2023-10-17 13:29:09 +0900

    refactor: update template to script setup
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>

--- packages/mirinae/src/navigation/breadcrumbs/PBreadcrumbs.vue
+++ packages/mirinae/src/navigation/breadcrumbs/PBreadcrumbs.vue
@@ -1,79 +1,5 @@
-<template>
-    <div class="p-breadcrumbs">
-        <span v-for="(route, idx) in routes"
-              :key="idx"
-        >
-            <span v-if="route.to || route.path">
-                <router-link
-                    v-if="isLengthOverFive(idx)"
-                    class="menu"
-                    :to="getLocation(route)"
-                >
-                    <span v-if="idx !== routes.length - 1"
-                          class="link"
-                    >{{ route.name }}</span>
-                    <span v-else
-                          class="current-page"
-                    >
-                        {{ route.name }}
-                        <p-copy-button v-if="copiable"
-                                       :value="route.name"
-                        />
-                    </span>
-                    <span v-if="idx < routes.length - 1">
-                        <p-i name="ic_chevron-right-thin"
-                             width="1rem"
-                             height="1rem"
-                             class="arrow-icon"
-                             color="inherit white"
-                        />
-                    </span>
-                </router-link>
-            </span>
-            <span v-else>
-                <span v-if="isLengthOverFive(idx)"
-                      class="menu"
-                >
-                    <span v-if="idx !== routes.length - 1"
-                          class="link"
-                          @click="$emit('click', route, idx)"
-                    >{{ route.name }}</span>
-                    <span v-else
-                          class="current-page"
-                    >
-                        {{ route.name }}
-                        <p-copy-button v-if="copiable"
-                                       :value="route.name"
-                        />
-                    </span>
-                    <span v-if="idx < routes.length - 1">
-                        <p-i name="ic_chevron-right-thin"
-                             width="1rem"
-                             height="1rem"
-                             class="arrow-icon"
-                             color="inherit white"
-                        />
-                    </span>
-                </span>
-            </span>
-            <span v-if="routes.length >= 5 && idx === 2 && !state.isShown">
-                <span class="link"
-                      @click="showHidden"
-                >...</span>
-                <p-i name="ic_chevron-right-thin"
-                     width="1rem"
-                     height="1rem"
-                     class="arrow-icon"
-                     color="inherit white"
-                />
-            </span>
-        </span>
-    </div>
-</template>
-
-<script lang="ts">
-import type { PropType } from 'vue';
-import { defineComponent, reactive } from 'vue';
+<script setup lang="ts">
+import { reactive } from 'vue';
 
 import type { Location } from 'vue-router';
 
@@ -91,43 +17,95 @@ interface Props {
     copiable?: boolean;
 }
 
-export default defineComponent<Props>({
-    name: 'PBreadcrumbs',
-    components: {
-        PCopyButton,
-        PI,
-    },
-    props: {
-        routes: {
-            type: Array as PropType<Route[]>,
-            default: () => [],
-        },
-        copiable: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    setup(props) {
-        const state = reactive({
-            isShown: false,
-        });
-        const getLocation = (route: Route): Location => {
-            if (route.to) return route.to;
-            if (route.path) return { path: route.path };
-            return {};
-        };
-        const showHidden = () => { state.isShown = true; };
-        const isLengthOverFive = (idx) => props.routes.length < 5 || (props.routes.length >= 5 && (idx < 1 || idx > props.routes.length - 3)) || state.isShown;
-
-        return {
-            state,
-            getLocation,
-            showHidden,
-            isLengthOverFive,
-        };
-    },
+const props = withDefaults(defineProps<Props>(), {
+    routes: () => [],
+    copiable: false,
 });
+
+const state = reactive({
+    isShown: false,
+});
+const getLocation = (route: Route): Location => {
+    if (route.to) return route.to;
+    if (route.path) return { path: route.path };
+    return {};
+};
+const showHidden = () => { state.isShown = true; };
+const isLengthOverFive = (idx) => props.routes.length < 5 || (props.routes.length >= 5 && (idx < 1 || idx > props.routes.length - 3)) || state.isShown;
 </script>
+
+<template>
+    <div class="p-breadcrumbs">
+        <span v-for="(route, idx) in props.routes"
+              :key="idx"
+        >
+            <span v-if="route.to || route.path">
+                <router-link
+                    v-if="isLengthOverFive(idx)"
+                    class="menu"
+                    :to="getLocation(route)"
+                >
+                    <span v-if="idx !== props.routes.length - 1"
+                          class="link"
+                    >{{ route.name }}</span>
+                    <span v-else
+                          class="current-page"
+                    >
+                        {{ route.name }}
+                        <p-copy-button v-if="props.copiable"
+                                       :value="route.name"
+                        />
+                    </span>
+                    <span v-if="idx < props.routes.length - 1">
+                        <p-i name="ic_chevron-right-thin"
+                             width="1rem"
+                             height="1rem"
+                             class="arrow-icon"
+                             color="inherit white"
+                        />
+                    </span>
+                </router-link>
+            </span>
+            <span v-else>
+                <span v-if="isLengthOverFive(idx)"
+                      class="menu"
+                >
+                    <span v-if="idx !== props.routes.length - 1"
+                          class="link"
+                          @click="$emit('click', route, idx)"
+                    >{{ route.name }}</span>
+                    <span v-else
+                          class="current-page"
+                    >
+                        {{ route.name }}
+                        <p-copy-button v-if="props.copiable"
+                                       :value="route.name"
+                        />
+                    </span>
+                    <span v-if="idx < props.routes.length - 1">
+                        <p-i name="ic_chevron-right-thin"
+                             width="1rem"
+                             height="1rem"
+                             class="arrow-icon"
+                             color="inherit white"
+                        />
+                    </span>
+                </span>
+            </span>
+            <span v-if="props.routes.length >= 5 && idx === 2 && !state.isShown">
+                <span class="link"
+                      @click="showHidden"
+                >...</span>
+                <p-i name="ic_chevron-right-thin"
+                     width="1rem"
+                     height="1rem"
+                     class="arrow-icon"
+                     color="inherit white"
+                />
+            </span>
+        </span>
+    </div>
+</template>
 
 <style lang="postcss">
 .p-breadcrumbs {
