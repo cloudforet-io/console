28db9c6f5ea02e1b61c491d98bcb3f7439e5bc51
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2023-10-24 10:03:49 +0900

    refactor: static data format change due to 'widget-config' spec update
    Signed-off-by: NaYeong,Kim(김나영) <nayeongkim@megazone.com>

--- apps/web/src/services/dashboards/widgets/_configs/widget-options-schema-config.ts
+++ apps/web/src/services/dashboards/widgets/_configs/widget-options-schema-config.ts
@@ -1,5 +1,9 @@
 import type { EnumModelOptions, SearchResourceModelOptions, ReferenceResourceModelOptions } from '@/models/widget';
 
+import { ASSET_VARIABLE_TYPE_INFO } from '@/lib/reference/asset-reference-config';
+import { COST_VARIABLE_TYPE_INFO } from '@/lib/reference/cost-reference-config';
+import { REFERENCE_TYPE_INFO } from '@/lib/reference/reference-config';
+
 export interface WidgetOptionsSchemaProperty {
     key: string; // e.g. cost_data_source
     name: string; // e.g. Data Source
@@ -15,6 +19,73 @@ export interface WidgetOptionsSchemaProperty {
     };
 }
 export type WidgetOptionsSchemaConfig = WidgetOptionsSchemaProperty[];
+
+export const ResourceReferenceSchema:WidgetOptionsSchemaConfig = [
+    {
+        key: REFERENCE_TYPE_INFO.provider.type,
+        name: REFERENCE_TYPE_INFO.provider.name,
+        selection_type: 'MULTI',
+    },
+    {
+        key: REFERENCE_TYPE_INFO.project.type,
+        name: REFERENCE_TYPE_INFO.project.name,
+        selection_type: 'MULTI',
+    },
+    {
+        key: REFERENCE_TYPE_INFO.service_account.type,
+        name: REFERENCE_TYPE_INFO.service_account.name,
+        selection_type: 'MULTI',
+    },
+    {
+        key: REFERENCE_TYPE_INFO.project_group.type,
+        name: REFERENCE_TYPE_INFO.project_group.name,
+        selection_type: 'MULTI',
+    },
+    {
+        key: REFERENCE_TYPE_INFO.region.type,
+        name: REFERENCE_TYPE_INFO.region.name,
+        selection_type: 'MULTI',
+    },
+];
+export const CostReferenceSchema:WidgetOptionsSchemaConfig = [
+    {
+        key: COST_VARIABLE_TYPE_INFO.cost_data_source.type,
+        name: COST_VARIABLE_TYPE_INFO.cost_data_source.name,
+        selection_type: 'SINGLE',
+    },
+    {
+        key: COST_VARIABLE_TYPE_INFO.cost_product.type,
+        name: COST_VARIABLE_TYPE_INFO.cost_product.name,
+        selection_type: 'MULTI',
+        item_options: [
+            { type: 'SEARCH_RESOURCE', resource_type: 'cost_analysis.Cost', resource_key: COST_VARIABLE_TYPE_INFO.cost_product.key },
+        ],
+    },
+    {
+        key: COST_VARIABLE_TYPE_INFO.cost_usage_type.type,
+        name: COST_VARIABLE_TYPE_INFO.cost_usage_type.name,
+        selection_type: 'MULTI',
+        item_options: [
+            { type: 'SEARCH_RESOURCE', resource_type: 'cost_analysis.Cost', resource_key: COST_VARIABLE_TYPE_INFO.cost_usage_type.key },
+        ],
+    },
+];
+
+export const AssetReferenceSchema:WidgetOptionsSchemaConfig = [
+    {
+        key: ASSET_VARIABLE_TYPE_INFO.asset_query_set.type,
+        name: ASSET_VARIABLE_TYPE_INFO.asset_query_set.name,
+        selection_type: 'MULTI',
+    },
+    {
+        key: ASSET_VARIABLE_TYPE_INFO.asset_account.type,
+        name: ASSET_VARIABLE_TYPE_INFO.asset_account.name,
+        selection_type: 'MULTI',
+        item_options: [
+            { type: 'SEARCH_RESOURCE', resource_type: 'inventory.CloudService', resource_key: ASSET_VARIABLE_TYPE_INFO.asset_account.key },
+        ],
+    },
+];
 
 /* example
 [
