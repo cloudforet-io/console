89e8153336ab8a4714d33fd5832a61504eb46aca
sulmo <50662170+sulmoJ@users.noreply.github.com>
2023-10-25 17:35:22 +0900

    refactor(cloud-service-detail-page): refactor to setup script (#2100)
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/services/asset-inventory/server/ServerPage.vue
+++ apps/web/src/services/asset-inventory/server/ServerPage.vue
@@ -1,26 +1,8 @@
+<script setup lang="ts">
+import CloudServiceDetailPage
+    from '@/services/asset-inventory/cloud-service/cloud-service-detail/CloudServiceDetailPage.vue';
+</script>
+
 <template>
     <cloud-service-detail-page :is-server-page="true" />
 </template>
-
-<script lang="ts">
-import CloudServiceDetailPage
-    from '@/services/asset-inventory/cloud-service/cloud-service-detail/CloudServiceDetailPage.vue';
-
-const PROVIDER = 'aws';
-const CLOUD_SERVICE_NAME = 'Instance';
-const CLOUD_SERVICE_GROUP = 'EC2';
-
-export default {
-    name: 'Server',
-    components: {
-        CloudServiceDetailPage,
-    },
-    setup() {
-        return {
-            PROVIDER,
-            CLOUD_SERVICE_NAME,
-            CLOUD_SERVICE_GROUP,
-        };
-    },
-};
-</script>
