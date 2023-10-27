6e9c3d2ef044f77c0006d748fa559a4820095161
Wanjin Noh <wanjin@megazone.com>
2023-10-27 18:00:57 +0900

    feat: update getInitialWidgetInheritOptions to fit updated spec
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/dashboards/widgets/_helpers/widget-inherit-options-helper.ts
+++ apps/web/src/services/dashboards/widgets/_helpers/widget-inherit-options-helper.ts
@@ -1,27 +1,47 @@
 import type { DashboardVariablesSchema } from '@/services/dashboards/config';
-import {
-    getVariableKeyFromWidgetSchemaProperty,
-} from '@/services/dashboards/shared/helpers/dashboard-variable-schema-helper';
 import type { InheritOptions, WidgetConfig } from '@/services/dashboards/widgets/_configs/config';
 
-export const getInitialWidgetInheritOptions = (widgetConfig?: WidgetConfig, storedInheritOptions?: InheritOptions, variablesSchema?: DashboardVariablesSchema): InheritOptions => {
+export const getInitialWidgetInheritOptions = (
+    widgetConfig?: WidgetConfig,
+    storedInheritOptions?: InheritOptions,
+    variablesSchema?: DashboardVariablesSchema,
+): InheritOptions => {
     const refined: InheritOptions = {};
 
-    widgetConfig?.options_schema?.properties?.forEach((property) => {
-        if (property.non_inheritable) return;
+    Object.entries(widgetConfig?.options_schema?.properties ?? {}).forEach(([optionName, property]) => {
+        const inheritOption = storedInheritOptions?.[optionName];
+        const inheritanceMode = property.inheritance_mode;
 
-        const inheritOption = storedInheritOptions?.[property.key];
-        if (inheritOption && inheritOption.enabled === false && inheritOption.variable_info) {
-            refined[property.key] = { enabled: false };
+        if (inheritanceMode === 'NONE') return;
+
+        if (!inheritanceMode || inheritanceMode === 'KEY_MATCHING') { // default inheritance mode is KEY_MATCHING
+            const variableKey = property.key;
+            const variableProperty = variablesSchema?.properties[variableKey];
+            if (!variableKey) return;
+            if (!variableProperty?.use) return;
+            refined[optionName] = { enabled: true, variable_key: variableKey };
             return;
         }
 
-        const variableKey = getVariableKeyFromWidgetSchemaProperty(property.key);
-        if (variablesSchema && !variablesSchema?.properties[variableKey]?.use) {
-            refined[property.key] = { enabled: false };
-        }
+        // inheritanceMode === 'SELECTION_TYPE_MATCHING'
+        if (inheritOption?.enabled) {
+            const variableKey = inheritOption.variable_key;
 
-        refined[property.key] = inheritOption;
+            if (variableKey) {
+                const variableProperty = variablesSchema?.properties[variableKey];
+                if (variableProperty?.use) {
+                    // if variable is available, use it
+                    refined[optionName] = { enabled: true, variable_key: variableKey };
+                }
+            }
+
+            // find variable key from variables schema by selection type
+            const matchedPropertyTuples = Object.entries(variablesSchema?.properties ?? {})
+                .find(([, d]) => d.use && d.selection_type === property.selection_type);
+            if (matchedPropertyTuples) {
+                refined[optionName] = { enabled: true, variable_key: matchedPropertyTuples[0] };
+            }
+        }
     });
 
     return refined;
@@ -37,7 +57,7 @@ export const getInheritingProperties = (variableKey: string, inheritOptions: Inh
     const properties: string[] = [];
     Object.entries(inheritOptions).forEach(([property, inheritOption]) => {
         if (!inheritOption.enabled) return;
-        if (inheritOption.variable_info?.key === variableKey) properties.push(property);
+        if (inheritOption.variable_key === variableKey) properties.push(property);
     });
     return properties;
 };
