356c4ed452bf3abc6a8bf9ae51c8644fff2a733a
yuda <yuda@megazone.com>
2023-10-30 10:34:02 +0900

    fix: fix `getIsRaised()` in cost analysis data table
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/cost-explorer/cost-analysis/modules/CostAnalysisDataTable.vue
+++ apps/web/src/services/cost-explorer/cost-analysis/modules/CostAnalysisDataTable.vue
@@ -235,12 +235,13 @@ const getLink = (item: CostAnalyzeRawData, fieldName: string) => {
     };
 };
 const getIsRaised = (item: CostAnalyzeRawData, fieldName: string): boolean => {
-    const currDate: string = fieldName.split('.')[1]; // cost.2022-01-04 -> 2022-01-04
-    const prevDate: string = dayjs.utc(currDate).subtract(1, state.timeUnit).format(state.dateFormat);
-    const currValue: number|undefined = item.cost_sum?.find(({ date }) => date === currDate)?.value;
-    const prevValue: number|undefined = item.cost_sum?.find(({ date }) => date === prevDate)?.value;
+    const currIndex = Number(fieldName.split('.')[1]); // value_sum.0.value -> 0
+    if (currIndex === 0) return false;
 
-    if (prevValue === undefined || currValue === undefined) return false;
+    const prevIndex = currIndex - 1;
+    const currValue = item.value_sum?.[currIndex]?.value ?? 0;
+    const prevValue = item.value_sum?.[prevIndex]?.value ?? 0;
+
     if (currValue < prevValue) return false;
     if (currValue > 0) {
         if (prevValue < 0) return true;
