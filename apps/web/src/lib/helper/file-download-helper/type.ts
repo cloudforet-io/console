b3eb86302e5201e99f886c9eff8b6d62a74a91b5
sulmo <sulmo@megazone.com>
2023-11-01 21:00:59 +0900

    feat(file-download-helper): create download helper for excel download
    Signed-off-by: sulmo <sulmo@megazone.com>

---
+++ apps/web/src/lib/helper/file-download-helper/type.ts
@@ -0,0 +1,31 @@
+import type { Reference } from '@/lib/reference/type';
+
+interface Options {
+    currency?: string;
+    currencyRates?: Record<string, number>;
+}
+
+export interface ExcelDataField {
+    key: string;
+    name: string;
+    type?: 'datetime'|'enum'|'currency';
+    enum_items?: any;
+    reference?: Reference;
+    options?: Options;
+}
+
+interface HeaderMessage {
+    title: string;
+}
+
+export interface ExcelPayload {
+    url: string;
+    param: any;
+    data?: any[];
+    fields: ExcelDataField[];
+    sheet_name?: string;
+    header_message?: HeaderMessage;
+    file_name_prefix?: string;
+    version?: string;
+    timezone?: string;
+}
