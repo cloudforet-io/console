792dfee56b34d3bd5ad428c5f1c212b4b63a5484
yuda <yuda@megazone.com>
2023-11-03 17:18:52 +0900

    feat: use 'data field' rather than 'group by'
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/dashboards/widgets/_configs/view-config.ts
+++ apps/web/src/services/dashboards/widgets/_configs/view-config.ts
@@ -1,25 +1,3 @@
-import { ASSET_GROUP_BY, COST_GROUP_BY } from '@/services/dashboards/widgets/_configs/config';
-
-export const COST_GROUP_BY_ITEM_MAP = {
-    [COST_GROUP_BY.PROJECT_GROUP]: { name: COST_GROUP_BY.PROJECT_GROUP, label: 'Project Group' },
-    [COST_GROUP_BY.PROJECT]: { name: COST_GROUP_BY.PROJECT, label: 'Project' },
-    [COST_GROUP_BY.PROVIDER]: { name: COST_GROUP_BY.PROVIDER, label: 'Provider' },
-    [COST_GROUP_BY.SERVICE_ACCOUNT]: { name: COST_GROUP_BY.SERVICE_ACCOUNT, label: 'Service Account' },
-    [COST_GROUP_BY.PRODUCT]: { name: COST_GROUP_BY.PRODUCT, label: 'Product' },
-    [COST_GROUP_BY.REGION]: { name: COST_GROUP_BY.REGION, label: 'Region' },
-    [COST_GROUP_BY.USAGE_TYPE]: { name: COST_GROUP_BY.USAGE_TYPE, label: 'Type' },
-} as const;
-
-export const ASSET_GROUP_BY_ITEM_MAP = {
-    [ASSET_GROUP_BY.PROJECT]: { name: ASSET_GROUP_BY.PROJECT, label: 'Project' },
-    [ASSET_GROUP_BY.PROVIDER]: { name: ASSET_GROUP_BY.PROVIDER, label: 'Provider' },
-    [ASSET_GROUP_BY.REGION]: { name: ASSET_GROUP_BY.REGION, label: 'Region' },
-    [ASSET_GROUP_BY.COMPLIANCE_FRAMEWORK]: { name: ASSET_GROUP_BY.COMPLIANCE_FRAMEWORK, label: 'Compliance Framework' },
-    // [ASSET_GROUP_BY.REQUIREMENT_ID]: { name: ASSET_GROUP_BY.REQUIREMENT_ID, label: 'Requirement ID' },
-    [ASSET_GROUP_BY.SERVICE]: { name: ASSET_GROUP_BY.SERVICE, label: 'Service' },
-    [ASSET_GROUP_BY.ACCOUNT]: { name: ASSET_GROUP_BY.ACCOUNT, label: 'AWS Account ID' },
-};
-
 export const WIDGET_THEMES = ['violet', 'blue', 'coral', 'yellow', 'gray', 'green', 'indigo', 'peacock'] as const;
 export type WidgetTheme = typeof WIDGET_THEMES[number];
 export type WidgetColorSetType = 'basic'|'massive';
