945bae05683f2438281892cb8cfeeb57d2935f6f
NaYeong,Kim <nayeongkim@megazone.com>
2023-11-08 21:17:00 +0900

    feat: apply selected menu list at context-menu (#2178)
    * fix: bug fix due to changes in the usage of the widgetFormStore state
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>
    
    * feat: apply selected menu list at context-menu
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>
    
    * chore: add type
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/services/dashboards/shared/dashboard-widget-input-form/dashboard-widget-options-form/DashboardCostWidgetValueOptionDropdown.vue
+++ apps/web/src/services/dashboards/shared/dashboard-widget-input-form/dashboard-widget-options-form/DashboardCostWidgetValueOptionDropdown.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import {
-    reactive, watch, ref,
+    reactive, watch, ref, computed,
 } from 'vue';
 
-import { PIconButton, PSelectDropdown, PBadge } from '@spaceone/design-system';
+import {
+    PIconButton, PSelectDropdown, PBadge, PCheckbox,
+} from '@spaceone/design-system';
 import type {
     AutocompleteHandler,
     SelectDropdownMenuItem,
@@ -32,7 +34,7 @@ const emit = defineEmits<{(e: 'update:selected', selected: SelectDropdownMenuIte
 }>();
 
 const widgetFormStore = useWidgetFormStore();
-const widgetFormState = widgetFormStore.$state;
+const widgetFormState = widgetFormStore.state;
 
 const selectDropdownRef = ref<null|HTMLElement>(null);
 
@@ -41,6 +43,10 @@ const state = reactive({
     selectedKey: undefined as SelectDropdownMenuItem|undefined,
     valueTarget: props.optionKey === 'cost_tag_value' ? 'tags' : 'additional_info',
     selectedItems: [] as SelectDropdownMenuItem[],
+    selectedMenuList: computed<string[]>(() => {
+        const selectedMenuListName = state.selectedItems.map((d) => d.name);
+        return state.selectedItems.length > 0 ? selectedMenuListName : [] as string[];
+    }),
 });
 
 const variableModelKeys = {
@@ -48,6 +54,7 @@ const variableModelKeys = {
     cost_additional_info_value: 'cost_additional_info_keys',
 } as const;
 
+/* Handler */
 const menuHandler: AutocompleteHandler = async (inputText, pageStart, pageLimit = 10) => {
     // key handler
     if (!state.selectedKey) {
@@ -115,6 +122,7 @@ const menuHandler: AutocompleteHandler = async (inputText, pageStart, pageLimit 
     }
 };
 
+/* Components */
 const handleUpdateSelected = (selected: SelectDropdownMenuItem) => {
     if (!state.selectedKey) {
         state.selectedKey = selected;
@@ -133,11 +141,17 @@ const handleDeleteButton = () => {
     emit('update:selected', []);
     initMenu();
 };
+const handleChangeSelectedListItem = (list: (SelectDropdownMenuItem|string)[]) => {
+    const selectedListItemIdx = state.selectedItems.findIndex((d) => d.name === list[list.length - 1]);
+    state.selectedItems.splice(selectedListItemIdx, 1);
+    emit('update:selected', state.selectedItems);
+};
 const initMenu = () => {
     state.visibleMenu = false;
     state.selectedKey = undefined;
 };
 
+/* Watcher */
 watch(() => state.visibleMenu, (visibleMenu) => {
     if (!visibleMenu) {
         initMenu();
@@ -174,6 +188,21 @@ watch(() => props.selected, (selected) => {
                            :invalid="props.invalid"
                            @select="handleUpdateSelected"
         >
+            <template #menu-header>
+                <div v-if="state.selectedItems.length > 0"
+                     class="selected-menu-wrapper"
+                >
+                    <p-checkbox v-for="(item, idx) in state.selectedItems"
+                                :key="idx"
+                                v-model="state.selectedMenuList"
+                                class="selected-menu-list-item"
+                                :value="item.name"
+                                @change="handleChangeSelectedListItem"
+                    >
+                        {{ item.label }}
+                    </p-checkbox>
+                </div>
+            </template>
             <template #dropdown-button="item">
                 <div v-if="item.target === 'value'"
                      class="dropdown-inner"
@@ -216,6 +245,13 @@ watch(() => props.selected, (selected) => {
         .key-item-header {
             @apply text-label-sm text-gray-500 font-bold;
             margin-left: 0.5rem;
+        }
+        .selected-menu-wrapper {
+            @apply flex flex-col bg-blue-200 text-label-md;
+            .selected-menu-list-item {
+                width: 100%;
+                padding: 0.5rem 0.25rem;
+            }
         }
     }
     .selected-item-badge {
