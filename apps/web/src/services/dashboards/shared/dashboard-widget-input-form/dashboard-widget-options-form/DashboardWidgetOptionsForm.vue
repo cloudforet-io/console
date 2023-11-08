74de613df7fda8d0282c31270c40a70a70fe73dc
Dahyun Yu <yuda@megazone.com>
2023-11-08 10:15:44 +0900

    feat: add refresh event to widget form (#2169)
    * feat: add refresh event to widget form
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * feat: disable dropdown when its inherit 'KEY_MATCHING'
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/dashboards/shared/dashboard-widget-input-form/dashboard-widget-options-form/DashboardWidgetOptionsForm.vue
+++ apps/web/src/services/dashboards/shared/dashboard-widget-input-form/dashboard-widget-options-form/DashboardWidgetOptionsForm.vue
@@ -2,14 +2,17 @@
 import { computed, reactive } from 'vue';
 
 import { PDataLoader, PTextButton } from '@spaceone/design-system';
+import { v4 as uuidv4 } from 'uuid';
 
 import type { DashboardVariablesSchema } from '@/services/dashboards/config';
 import DashboardWidgetOptionDropdown
     from '@/services/dashboards/shared/dashboard-widget-input-form/dashboard-widget-options-form/DashboardWidgetOptionDropdown.vue';
 import { useWidgetFormStore } from '@/services/dashboards/shared/dashboard-widget-input-form/widget-form-store';
+import { useDashboardDetailInfoStore } from '@/services/dashboards/store/dashboard-detail-info';
 import type {
     WidgetOptionsSchema,
 } from '@/services/dashboards/widgets/_configs/widget-options-schema';
+import { mergeBaseWidgetState } from '@/services/dashboards/widgets/_hooks/use-widget/merge-base-widget-state';
 
 const props = defineProps<{
     projectId?: string;
@@ -18,15 +21,33 @@ const props = defineProps<{
 
 const widgetFormStore = useWidgetFormStore();
 const widgetFormState = widgetFormStore.$state;
-
+const dashboardDetailStore = useDashboardDetailInfoStore();
+const dashboardDetailState = dashboardDetailStore.$state;
 const state = reactive({
     properties: computed<WidgetOptionsSchema['properties']>(() => widgetFormStore.widgetConfig?.options_schema?.properties ?? {}),
     optionsValidMap: {} as Record<string, boolean>,
+    uuid: uuidv4(),
 });
 
 /* event handlers */
 const handleReturnToInitialSettings = () => {
-    // TODO: implement
+    const mergedWidgetState = mergeBaseWidgetState({
+        inheritOptions: widgetFormStore.originWidgetInfo?.inherit_options,
+        widgetOptions: widgetFormStore.originWidgetInfo?.widget_options,
+        widgetName: widgetFormStore.originWidgetInfo?.widget_name ?? '',
+        dashboardSettings: dashboardDetailState.settings,
+        dashboardVariablesSchema: dashboardDetailState.variablesSchema,
+        dashboardVariables: dashboardDetailState.variables,
+        title: widgetFormStore.originWidgetInfo?.title,
+        schemaProperties: widgetFormStore.originWidgetInfo?.schema_properties,
+    });
+    widgetFormStore.$patch((_state) => {
+        _state.widgetTitle = mergedWidgetState.title ?? '';
+        _state.schemaProperties = mergedWidgetState.schemaProperties ?? [];
+        _state.widgetOptions = mergedWidgetState.options ?? {};
+        _state.inheritOptions = mergedWidgetState.inheritOptions ?? {};
+    });
+    state.uuid = uuidv4();
 };
 const handleDeleteProperty = (propertyName: string) => {
     widgetFormStore.updateInheritOption(propertyName, false);
@@ -55,7 +76,7 @@ const handleUpdateIsValid = (propertyName: string, isValid: boolean) => {
             {{ $t('DASHBOARDS.FORM.RETURN_TO_INITIAL_SETTINGS') }}
         </p-text-button>
         <dashboard-widget-option-dropdown v-for="propertyName in widgetFormState.schemaProperties"
-                                          :key="`option-dropdown-${propertyName}`"
+                                          :key="`option-dropdown-${propertyName}-${state.uuid}`"
                                           :property-name="propertyName"
                                           :variables-schema="props.variablesSchema"
                                           :is-valid="state.optionsValidMap[propertyName]"
