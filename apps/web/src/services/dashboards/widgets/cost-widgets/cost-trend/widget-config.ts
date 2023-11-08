99adf5e8721d84fc9628f9ea7864d140d2981492
Wanjin Noh <wanjin@megazone.com>
2023-11-08 11:14:42 +0900

    fix: update cost trend widget config
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/dashboards/widgets/cost-widgets/cost-trend/widget-config.ts
+++ apps/web/src/services/dashboards/widgets/cost-widgets/cost-trend/widget-config.ts
@@ -1,5 +1,5 @@
 import type { WidgetConfig } from '@/services/dashboards/widgets/_configs/config';
-import { CHART_TYPE, COST_DATA_FIELD_MAP, GRANULARITY } from '@/services/dashboards/widgets/_configs/config';
+import { CHART_TYPE, COST_DATA_FIELD_MAP } from '@/services/dashboards/widgets/_configs/config';
 import { getWidgetOptionsSchema } from '@/services/dashboards/widgets/_configs/widget-options-schema';
 
 const costTrendWidgetConfig: Partial<WidgetConfig> = {
@@ -13,7 +13,6 @@ const costTrendWidgetConfig: Partial<WidgetConfig> = {
     },
     scopes: ['PROJECT', 'WORKSPACE'],
     options: {
-        granularity: GRANULARITY.MONTHLY,
         chart_type: CHART_TYPE.LINE,
         legend_options: {
             enabled: true,
