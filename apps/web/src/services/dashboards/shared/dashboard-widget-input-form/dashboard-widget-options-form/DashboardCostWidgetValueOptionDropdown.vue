3eecdd0b768d0a33d4b4ced3d613cfc584ad3062
NaYeong,Kim <nayeongkim@megazone.com>
2023-11-13 13:19:42 +0900

    refactor: implementing a context-menu from scratch (#2202)
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/services/dashboards/shared/dashboard-widget-input-form/dashboard-widget-options-form/DashboardCostWidgetValueOptionDropdown.vue
+++ apps/web/src/services/dashboards/shared/dashboard-widget-input-form/dashboard-widget-options-form/DashboardCostWidgetValueOptionDropdown.vue
@@ -1,21 +1,28 @@
 <script setup lang="ts">
+import { onClickOutside } from '@vueuse/core';
 import {
-    reactive, watch, ref, computed,
+    reactive, watch, ref, toRef,
 } from 'vue';
 
 import {
-    PIconButton, PSelectDropdown, PBadge, PCheckbox,
+    PIconButton, PBadge, PContextMenu, PI, useContextMenuController,
 } from '@spaceone/design-system';
 import type {
     AutocompleteHandler,
     SelectDropdownMenuItem,
 } from '@spaceone/design-system/types/inputs/dropdown/select-dropdown/type';
+import { debounce } from 'lodash';
 
 import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 
 import ErrorHandler from '@/common/composables/error/errorHandler';
 
 import { useWidgetFormStore } from '@/services/dashboards/shared/dashboard-widget-input-form/widget-form-store';
+
+const VARIABLE_MODEL_KEYS = {
+    cost_tag_value: 'cost_tag_keys',
+    cost_additional_info_value: 'cost_additional_info_keys',
+} as const;
 
 interface Props {
     optionKey?: string;
@@ -36,23 +43,17 @@ const emit = defineEmits<{(e: 'update:selected', selected: SelectDropdownMenuIte
 const widgetFormStore = useWidgetFormStore();
 const widgetFormState = widgetFormStore.state;
 
-const selectDropdownRef = ref<null|HTMLElement>(null);
+const menuRef = ref<any|null>(null);
+const targetRef = ref<HTMLElement|null>(null);
+const containerRef = ref<HTMLElement|null>(null);
 
 const state = reactive({
     visibleMenu: false,
+    searchText: '',
+    selectedItems: [] as SelectDropdownMenuItem[],
     selectedKey: undefined as SelectDropdownMenuItem|undefined,
     valueTarget: props.optionKey === 'cost_tag_value' ? 'tags' : 'additional_info',
-    selectedItems: [] as SelectDropdownMenuItem[],
-    selectedMenuList: computed<string[]>(() => {
-        const selectedMenuListName = state.selectedItems.map((d) => d.name);
-        return state.selectedItems.length > 0 ? selectedMenuListName : [] as string[];
-    }),
 });
-
-const variableModelKeys = {
-    cost_tag_value: 'cost_tag_keys',
-    cost_additional_info_value: 'cost_additional_info_keys',
-} as const;
 
 /* Handler */
 const menuHandler: AutocompleteHandler = async (inputText, pageStart, pageLimit = 6) => {
@@ -61,30 +62,38 @@ const menuHandler: AutocompleteHandler = async (inputText, pageStart, pageLimit 
         const param = {
             data_source_id: widgetFormState.widgetOptions.cost_data_source,
             query: {
-                only: [variableModelKeys[props.optionKey]],
+                only: [VARIABLE_MODEL_KEYS[props.optionKey]],
                 filter: [
                     {
-                        key: variableModelKeys[props.optionKey],
+                        key: VARIABLE_MODEL_KEYS[props.optionKey],
                         value: null,
                         operator: 'not',
-                    },
-                    {
-                        key: variableModelKeys[props.optionKey],
-                        value: inputText,
-                        operator: 'contain',
                     },
                 ],
             },
         };
-        const { results } = await SpaceConnector.clientV2.costAnalysis.dataSource.list(param);
-        const refinedResultsItems = results[0][variableModelKeys[props.optionKey]].reduce((result, d) => {
-            if (d !== '' && d !== undefined && d !== null) result.push({ label: d, name: `${state.valueTarget}.${d}` });
-            return result;
-        }, []);
-        return {
-            results: refinedResultsItems,
-        };
+        try {
+            const { results } = await SpaceConnector.clientV2.costAnalysis.dataSource.list(param);
+            const refinedMenuItems = results[0][VARIABLE_MODEL_KEYS[props.optionKey]].reduce((result, d) => {
+                if (d !== '' && d !== undefined && d !== null) result.push({ label: d, name: `${state.valueTarget}.${d}` });
+                return result;
+            }, []);
+
+            const refinedMenuItemsFilteredByInputValue = refinedMenuItems.filter((d) => (d.label as string).toLowerCase().includes(inputText.toLowerCase()));
+            const slicedResults = refinedMenuItemsFilteredByInputValue?.slice((pageStart ?? 1) - 1, pageLimit);
+
+            return {
+                results: slicedResults,
+                more: pageLimit < refinedMenuItemsFilteredByInputValue.length,
+            };
+        } catch (e) {
+            ErrorHandler.handleError(e);
+            return {
+                results: [],
+            };
+        }
     }
+
     // value handler
     const param = {
         query: {
@@ -102,6 +111,15 @@ const menuHandler: AutocompleteHandler = async (inputText, pageStart, pageLimit 
             },
         },
     };
+
+    if (inputText !== '') {
+        param.query.filter.push({
+            key: state.selectedKey?.name,
+            value: inputText,
+            operator: 'contain',
+        });
+    }
+
     try {
         const { results, total_count } = await SpaceConnector.clientV2.costAnalysis.cost.stat(param);
         const refinedResultsItems = results.reduce((result, d, index) => {
@@ -126,49 +144,84 @@ const menuHandler: AutocompleteHandler = async (inputText, pageStart, pageLimit 
 };
 
 /* Components */
-const handleUpdateSelected = (selected: SelectDropdownMenuItem) => {
-    if (!state.selectedKey) {
-        state.selectedKey = selected;
-        menuHandler('');
-        if (selectDropdownRef.value) {
-            selectDropdownRef.value.reloadMenu();
+const updateSearchText = debounce(async (searchText: string) => {
+    state.searchText = searchText;
+    await reloadMenu();
+}, 200);
+const handleClickDropdownButton = () => {
+    state.visibleMenu = !state.visibleMenu;
+};
+const handleSelectMenuItem = async (item: SelectDropdownMenuItem) => {
+    if (item?.target !== 'value') {
+        if (!state.selectedKey) {
+            state.selectedKey = item;
+            state.searchText = '';
+            await reloadMenu();
+        } else {
+            const selectedItem = { name: item.name, label: `${state.selectedKey?.label}: ${item.label}`, target: 'value' };
+            emit('update:selected', [...state.selectedItems, selectedItem]);
+            await resetMenu();
         }
     } else {
-        const selectedItem = { name: selected.name, label: `${state.selectedKey?.label}: ${selected.label}`, target: 'value' };
-        emit('update:selected', [...state.selectedItems, selectedItem]);
-        initMenu();
+        await reloadMenu();
     }
+};
+const handleClickShowMore = async () => {
+    await showMoreMenu();
 };
 const handleDeleteButton = () => {
     emit('delete');
     emit('update:selected', []);
-    initMenu();
+    resetMenu();
 };
-const handleChangeSelectedListItem = (list: (SelectDropdownMenuItem|string)[]) => {
-    const selectedListItemIdx = state.selectedItems.findIndex((d) => d.name === list[list.length - 1]);
-    state.selectedItems.splice(selectedListItemIdx, 1);
-    emit('update:selected', state.selectedItems);
-};
-const initMenu = () => {
+
+/* context menu controller */
+const {
+    contextMenuStyle,
+    loading,
+    refinedMenu,
+    initiateMenu,
+    reloadMenu,
+    showMoreMenu,
+} = useContextMenuController({
+    useFixedStyle: true,
+    targetRef,
+    contextMenuRef: menuRef,
+    visibleMenu: toRef(state, 'visibleMenu'),
+    useMenuFiltering: true,
+    useReorderBySelection: true,
+    searchText: toRef(state, 'searchText'),
+    selected: toRef(state, 'selectedItems'),
+    handler: ref(menuHandler),
+    pageSize: 6,
+});
+
+const resetMenu = () => {
     state.visibleMenu = false;
+    state.searchText = '';
     state.selectedKey = undefined;
 };
+
+onClickOutside(containerRef, resetMenu);
 
 /* Watcher */
 watch(() => state.visibleMenu, (visibleMenu) => {
     if (!visibleMenu) {
-        initMenu();
+        resetMenu();
+    } else {
+        initiateMenu();
+        reloadMenu();
     }
 });
 watch(() => props.selected, (selected) => {
     if (selected.length > 0) {
         // excluding the initial value
         state.selectedItems = selected.reduce((items, d) => {
-            if (d.target === 'value') {
+            if (d?.target === 'value') {
                 items.push({
-                    name: d.name,
-                    label: d.label,
-                    target: d.target || 'key',
+                    name: d?.name,
+                    label: d?.label,
+                    target: d?.target || 'key',
                 });
             }
             return items;
@@ -178,88 +231,139 @@ watch(() => props.selected, (selected) => {
 </script>
 
 <template>
-    <div class="select-form-wrapper">
-        <!-- TODO: Applying selection list and 'Show more' button soon -->
-        <p-select-dropdown ref="selectDropdownRef"
-                           class="select-dropdown"
-                           use-fixed-menu-style
-                           is-fixed-width
-                           multi-selectable
-                           :visible-menu.sync="state.visibleMenu"
-                           :handler="menuHandler"
-                           :selected="state.selectedItems"
-                           :invalid="props.invalid"
-                           :page-size="6"
-                           @select="handleUpdateSelected"
+    <div ref="containerRef"
+         class="select-form-wrapper"
+    >
+        <div ref="targetRef"
+             class="dropdown-button-wrapper"
         >
-            <template #menu-header>
-                <div v-if="state.selectedItems.length > 0"
-                     class="selected-menu-wrapper"
-                >
-                    <p-checkbox v-for="(item, idx) in state.selectedItems"
-                                :key="idx"
-                                v-model="state.selectedMenuList"
-                                class="selected-menu-list-item"
-                                :value="item.name"
-                                @change="handleChangeSelectedListItem"
+            <div :class="{
+                     'dropdown-button': true,
+                     invalid: props.invalid,
+                     opened: state.visibleMenu,
+                 }"
+                 @click="handleClickDropdownButton"
+            >
+                <div class="selection-display-wrapper">
+                    <div v-if="state.selectedItems.length > 0"
+                         class="dropdown-inner"
                     >
-                        {{ item.label }}
-                    </p-checkbox>
-                </div>
-            </template>
-            <template #dropdown-button="item">
-                <div v-if="item.target === 'value'"
-                     class="dropdown-inner"
-                >
-                    <span class="item-label">{{ item.label }}</span>
-                    <p-badge v-if="state.selectedItems.length > 1"
-                             class="selected-item-badge"
-                             style-type="blue200"
-                             badge-type="Subtle"
+                        <span class="item-label">{{ state.selectedItems[0].label }}</span>
+                        <p-badge v-if="state.selectedItems.length > 1"
+                                 class="selected-item-badge"
+                                 style-type="blue200"
+                                 badge-type="Subtle"
+                        >
+                            + {{ state.selectedItems.length - 1 }}
+                        </p-badge>
+                    </div>
+                    <span v-else
+                          class="placeholder"
                     >
-                        + {{ state.selectedItems.length - 1 }}
-                    </p-badge>
+                        {{ $t('COMPONENT.SELECT_DROPDOWN.SELECT') }}
+                    </span>
                 </div>
-                <span v-else
-                      class="placeholder"
-                >{{ $t('COMPONENT.SELECT_DROPDOWN.SELECT') }}</span>
-            </template>
-            <template #menu-item--format="{item}">
-                <span>{{ item.label }}</span>
-            </template>
-        </p-select-dropdown>
-        <p-icon-button class="delete-button"
-                       shape="square"
-                       style-type="negative-secondary"
-                       name="ic_delete"
-                       @click="handleDeleteButton"
+                <span class="arrow-button">
+                    <p-i :name="state.visibleMenu ? 'ic_chevron-up' : 'ic_chevron-down'"
+                         width="1.5rem"
+                         height="1.5rem"
+                         color="inherit"
+                    />
+                </span>
+            </div>
+            <p-icon-button class="delete-button"
+                           shape="square"
+                           style-type="negative-secondary"
+                           name="ic_delete"
+                           @click="handleDeleteButton"
+            />
+        </div>
+        <p-context-menu v-if="state.visibleMenu"
+                        ref="menuRef"
+                        class="dropdown-context-menu"
+                        searchable
+                        show-select-marker
+                        multi-selectable
+                        :style="contextMenuStyle"
+                        :selected="state.selectedItems"
+                        :loading="loading"
+                        :menu="refinedMenu"
+                        :search-text="state.searchText"
+                        :highlight-term="state.searchText"
+                        @keyup:esc="resetMenu"
+                        @update:search-text="updateSearchText"
+                        @click-done="resetMenu"
+                        @click-show-more="handleClickShowMore"
+                        @select="handleSelectMenuItem"
         />
     </div>
 </template>
 
 <style scoped lang="postcss">
 .select-form-wrapper {
-    @apply flex;
-    gap: 0.25rem;
-    .select-dropdown {
-        flex: 1;
-        .placeholder {
-            @apply text-label-md text-gray-600;
-        }
-        .key-item-header {
-            @apply text-label-sm text-gray-500 font-bold;
-            margin-left: 0.5rem;
-        }
-        .selected-menu-wrapper {
-            @apply flex flex-col bg-blue-200 text-label-md;
-            .selected-menu-list-item {
-                width: 100%;
-                padding: 0.5rem 0.25rem;
+    .dropdown-button-wrapper {
+        @apply flex items-center;
+        width: 100%;
+        gap: 0.25rem;
+        .dropdown-button {
+            @apply flex justify-between items-center bg-white text-label-md font-normal border rounded border-gray-300 cursor-pointer;
+            width: 100%;
+            max-width: 15.25rem;
+            min-height: 2rem;
+            gap: 0.25rem;
+            padding-left: 0.5rem;
+            .selection-display-wrapper {
+                @apply flex flex-grow flex-shrink items-center text-label-md;
+                width: calc(100% - 3rem);
+                padding-top: 0.375rem;
+                padding-bottom: 0.375rem;
+                .placeholder {
+                    @apply text-gray-600;
+                }
+                .dropdown-inner {
+                    @apply truncate flex items-center;
+                    .item-label {
+                        @apply truncate;
+                        flex: 1;
+                    }
+                    .selected-item-badge {
+                        margin-left: 0.25rem;
+                    }
+                }
+            }
+            .arrow-button {
+                @apply inline-flex items-center text-gray-600 cursor-pointer;
+                width: 1.75rem;
+            }
+            &:not(.invalid):hover {
+                @apply border-secondary;
+            }
+            &.invalid {
+                @apply border-alert;
+            }
+            &.opened {
+                @apply border-secondary;
+                .arrow-button {
+                    @apply text-secondary;
+                }
             }
         }
     }
-    .selected-item-badge {
-        margin-left: 0.25rem;
+    .dropdown-context-menu {
+        @apply fixed;
+        z-index: 1000;
+        max-height: 15rem;
+    }
+}
+
+/* custom design-system component - p-context-menu */
+:deep(.p-context-menu) {
+    .p-context-menu-item {
+        &:not(.selected) {
+            .select-marker {
+                @apply hidden;
+            }
+        }
     }
 }
 </style>
