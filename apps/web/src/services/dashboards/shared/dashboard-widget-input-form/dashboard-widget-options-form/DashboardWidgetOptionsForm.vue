00523fd7674ed997038de9056df52ea338805ae2
Wanjin Noh <wanjin@megazone.com>
2023-11-14 11:04:11 +0900

    fix: show empty case if global option info is not ready
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/dashboards/shared/dashboard-widget-input-form/dashboard-widget-options-form/DashboardWidgetOptionsForm.vue
+++ apps/web/src/services/dashboards/shared/dashboard-widget-input-form/dashboard-widget-options-form/DashboardWidgetOptionsForm.vue
@@ -12,7 +12,6 @@ import DashboardWidgetOptionDropdown
 import { useWidgetFormStore } from '@/services/dashboards/shared/dashboard-widget-input-form/widget-form-store';
 import type {
     WidgetOptionsSchema,
-    WidgetOptionsSchemaProperty,
 } from '@/services/dashboards/widgets/_configs/widget-options-schema';
 
 const props = defineProps<{
@@ -26,24 +25,9 @@ const widgetFormGetters = widgetFormStore.getters;
 
 const state = reactive({
     properties: computed<WidgetOptionsSchema['properties']>(() => widgetFormGetters.widgetConfig?.options_schema?.properties ?? {}),
-    optionsValidMap: {} as Record<string, boolean>,
-    globalOptionTuple: computed<[optionKey: string, schema: WidgetOptionsSchemaProperty] | undefined>(() => {
-        const globalOptionTuple = Object.entries(state.properties).find(([, property]) => {
-            if ((property as WidgetOptionsSchemaProperty).scope === 'GLOBAL') {
-                return true;
-            }
-            return false;
-        });
-        return globalOptionTuple;
-    }),
-    globalOptionName: computed<string | undefined>(() => {
-        if (!state.globalOptionTuple) return undefined;
-        return state.globalOptionTuple[1].name;
-    }),
     globalOptionHasValue: computed<boolean>(() => {
-        if (!state.globalOptionTuple) return true;
-        const optionKey = state.globalOptionTuple[0];
-        return !!widgetFormState.widgetOptions[optionKey];
+        if (!widgetFormGetters.globalOptionInfo) return true;
+        return !!widgetFormGetters.globalOptionInfo.value;
     }),
     uuid: uuidv4(),
 });
@@ -55,19 +39,14 @@ const handleReturnToInitialSettings = () => {
 };
 const handleDeleteProperty = (propertyName: string) => {
     widgetFormStore.updateInheritOption(propertyName, false);
-    delete state.optionsValidMap[propertyName];
-    widgetFormStore.updateOptionsValidMap({ ...state.optionsValidMap });
-};
-const handleUpdateIsValid = (propertyName: string, isValid: boolean) => {
-    state.optionsValidMap[propertyName] = isValid;
-    widgetFormStore.updateOptionsValidMap({ ...state.optionsValidMap });
+    const optionsValidMap = widgetFormState.optionsValidMap;
+    delete optionsValidMap[propertyName];
+    widgetFormStore.updateOptionsValidMap(optionsValidMap);
 };
 </script>
 
 <template>
-    <p-data-loader :loading="false"
-                   class="widget-options-form-wrapper"
-    >
+    <div class="dashboard-widget-options-form">
         <p-text-button icon-left="ic_refresh"
                        style-type="highlight"
                        class="return-to-initial-settings-button"
@@ -75,55 +54,62 @@ const handleUpdateIsValid = (propertyName: string, isValid: boolean) => {
         >
             {{ $t('DASHBOARDS.FORM.RETURN_TO_INITIAL_SETTINGS') }}
         </p-text-button>
-        <template v-if="state.globalOptionHasValue">
+        <p-data-loader class="widget-options-form-wrapper"
+                       :loading="!widgetFormGetters.isAllOptionsInitiated"
+                       :data="widgetFormGetters.globalOptionInfo ? widgetFormGetters.globalOptionInfo.ready : true"
+        >
             <dashboard-widget-option-dropdown v-for="propertyName in widgetFormState.schemaProperties"
                                               :key="`option-dropdown-${propertyName}-${state.uuid}`"
                                               :property-name="propertyName"
                                               :variables-schema="props.variablesSchema"
-                                              :global-option-key="state.globalOptionTuple?.[0]"
-                                              :is-valid="state.optionsValidMap[propertyName]"
-                                              @update:is-valid="handleUpdateIsValid(propertyName, $event)"
                                               @delete="handleDeleteProperty(propertyName)"
             />
-        </template>
-        <div v-else
-             class="no-global-option-wrapper"
-        >
-            <!-- TODO: apply translation -->
-            <p class="no-global-option-header">
-                <p-i name="ic_error-filled"
-                     height="1.25rem"
-                     width="1.25rem"
-                     :color="red[400]"
-                />
-                Unable to add the widget.
-            </p>
-            <div class="no-global-option-desc">
-                There's currently no available {{ state.globalOptionName }} in this domain. <br>
-                Please contact the system administrator.
-            </div>
-        </div>
-    </p-data-loader>
+            <template #no-data>
+                <div v-if="widgetFormGetters.globalOptionInfo && widgetFormGetters.globalOptionInfo.initiated"
+                     class="no-global-option-wrapper"
+                >
+                    <p class="no-global-option-header">
+                        <p-i name="ic_error-filled"
+                             height="1.25rem"
+                             width="1.25rem"
+                             :color="red[400]"
+                        />
+                        {{ $t('DASHBOARDS.FORM.UNABLE_TO_ADD_WIDGET') }}
+                    </p>
+                    <div class="no-global-option-desc">
+                        {{ $t('DASHBOARDS.FORM.UNABLE_TO_ADD_WIDGET_DESC', {field: widgetFormGetters.globalOptionInfo?.name}) }} <br>
+                        {{ $t('DASHBOARDS.FORM.UNABLE_TO_ADD_WIDGET_DESC2') }}
+                    </div>
+                </div>
+            </template>
+        </p-data-loader>
+    </div>
 </template>
 
 <style scoped lang="postcss">
-.widget-options-form-wrapper {
+.dashboard-widget-options-form {
     display: flex;
     flex-direction: column;
     height: 100%;
-    min-height: 15rem;
     .return-to-initial-settings-button {
         padding: 1rem 0;
         margin-left: auto;
     }
-    .no-global-option-wrapper {
-        @apply bg-red-100;
-        padding: 1rem;
-        .no-global-option-header {
-            @apply text-label-lg font-bold;
-        }
-        .no-global-option-desc {
-            @apply text-paragraph-md;
+    .widget-options-form-wrapper {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        min-height: 15rem;
+        .no-global-option-wrapper {
+            @apply bg-red-100 text-gray-900;
+            align-self: flex-start;
+            padding: 1rem;
+            .no-global-option-header {
+                @apply text-label-lg font-bold;
+            }
+            .no-global-option-desc {
+                @apply text-paragraph-md;
+            }
         }
     }
 }
