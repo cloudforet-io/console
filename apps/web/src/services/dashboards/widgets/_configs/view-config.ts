f8aeeaebef5a1459ee8123dddef6a1b1ab4ced3f
Dahyun Yu <yuda@megazone.com>
2023-11-17 19:15:17 +0900

    fix: show yAxis label in MonthlyCostWidget (#2244)
    * chore: delete gray color from chant colors
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * feat: display yAxis values compactly on the chart
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * fix: show yAxis label in MonthlyCostWidget
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/dashboards/widgets/_configs/view-config.ts
+++ apps/web/src/services/dashboards/widgets/_configs/view-config.ts
@@ -1,3 +1,3 @@
-export const WIDGET_THEMES = ['violet', 'blue', 'coral', 'yellow', 'gray', 'green', 'indigo', 'peacock'] as const;
+export const WIDGET_THEMES = ['violet', 'blue', 'coral', 'yellow', 'green', 'indigo', 'peacock'] as const;
 export type WidgetTheme = typeof WIDGET_THEMES[number];
 export type WidgetColorSetType = 'basic'|'massive';
