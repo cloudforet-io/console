1d6175e65e0150792852f99d92a80ed9dc89b44f
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2023-11-20 15:49:58 +0900

    feat: create api helper
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

---
+++ apps/web/src/lib/helper/multi-factor-authentication-helper.ts
@@ -0,0 +1,48 @@
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+
+import { store } from '@/store';
+import { i18n } from '@/translations';
+
+import { showErrorMessage, showSuccessMessage } from '@/lib/helper/notice-alert-helper';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+
+export const postEnableMfa = async (body): Promise<void|Error> => {
+    try {
+        const response = await SpaceConnector.clientV2.identity.user.enableMfa(body);
+        await store.dispatch('user/setUser', response);
+        await showSuccessMessage(i18n.t('COMMON.MFA_MODAL.SUCCESS'), '');
+    } catch (e: any) {
+        showErrorMessage(e.message, e);
+        ErrorHandler.handleError(e);
+    }
+};
+
+export const postDisableMfa = async (body): Promise<void|Error> => {
+    const {
+        user_id, domain_id, force,
+    } = body;
+    try {
+        const response = await SpaceConnector.clientV2.identity.user.disableMfa({
+            user_id,
+            domain_id,
+        });
+        await store.dispatch('user/setUser', response);
+        if (!force) {
+            await showSuccessMessage(i18n.t('COMMON.MFA_MODAL.SUCCESS'), '');
+        }
+    } catch (e: any) {
+        showErrorMessage(e.message, e);
+        ErrorHandler.handleError(e);
+    }
+};
+
+export const postValidationMfaCode = async (body): Promise<void|Error> => {
+    try {
+        const response = await SpaceConnector.clientV2.identity.user.confirmMfa(body);
+        await store.dispatch('user/setUser', response);
+    } catch (e: any) {
+        ErrorHandler.handleError(e);
+        throw e;
+    }
+};
