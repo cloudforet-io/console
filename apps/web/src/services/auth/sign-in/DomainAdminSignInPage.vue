68135ff2ceb2bde228e2ec4ead8fdb61498931c8
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2023-11-21 15:03:03 +0900

    refactor: changing the template structure
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/services/auth/sign-in/DomainAdminSignInPage.vue
+++ apps/web/src/services/auth/sign-in/DomainAdminSignInPage.vue
@@ -1,18 +1,82 @@
+<script lang="ts" setup>
+import {
+    reactive, computed,
+} from 'vue';
+import { useRouter } from 'vue-router/composables';
+
+import { SpaceRouter } from '@/router';
+import { store } from '@/store';
+
+import { isUserAccessibleToRoute } from '@/lib/access-control';
+import config from '@/lib/config';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+
+import { getDefaultRouteAfterSignIn } from '@/services/auth/lib/helper';
+import IDPWSignIn from '@/services/auth/sign-in/local/template/ID_PW.vue';
+import SignInLeftContainer from '@/services/auth/sign-in/modules/SignInLeftContainer.vue';
+import SignInRightContainer from '@/services/auth/sign-in/modules/SignInRightContainer.vue';
+
+interface Props {
+    isDomainOwner: boolean;
+    nextPath?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    isDomainOwner: false,
+    nextPath: undefined,
+});
+
+const router = useRouter();
+
+const state = reactive({
+    userType: computed(() => (props.isDomainOwner ? 'DOMAIN_OWNER' : 'USER')),
+    authType: computed(() => store.state.domain.extendedAuthType),
+    ciLogoImage: computed(() => config.get('DOMAIN_IMAGE.CI_LOGO')),
+    ciTextWithTypeImage: computed(() => config.get('DOMAIN_IMAGE.CI_TEXT_WITH_TYPE')),
+    showErrorMessage: computed(() => store.state.display.isSignInFailed),
+    userId: '' as string | undefined,
+    password: '',
+});
+
+const onSignIn = async () => {
+    try {
+        const defaultRoute = getDefaultRouteAfterSignIn(store.getters['user/isDomainOwner'], store.getters['user/hasSystemRole'], store.getters['user/hasPermission']);
+
+        if (!props.nextPath) {
+            await router.push(defaultRoute);
+            return;
+        }
+
+        const resolvedRoute = SpaceRouter.router.resolve(props.nextPath);
+        const isAccessible = isUserAccessibleToRoute(resolvedRoute.route, store.getters['user/pagePermissionList']);
+        if (isAccessible) {
+            await router.push(props.nextPath);
+        } else {
+            await router.push(defaultRoute);
+        }
+    } catch (e) {
+        ErrorHandler.handleError(e);
+        await store.dispatch('display/showSignInErrorMessage');
+    }
+};
+</script>
+
 <template>
     <div class="wrapper">
         <div class="ci-wrapper">
-            <img v-if="ciLogoImage"
+            <img v-if="state.ciLogoImage"
                  class="logo-character"
-                 :src="ciLogoImage"
+                 :src="state.ciLogoImage"
             >
             <img v-else
                  class="logo-character"
                  src="@/assets/images/brand/brand_logo.png"
             >
 
-            <img v-if="ciTextWithTypeImage"
+            <img v-if="state.ciTextWithTypeImage"
                  class="logo-text"
-                 :src="ciTextWithTypeImage"
+                 :src="state.ciTextWithTypeImage"
             >
             <img v-else
                  class="logo-text"
@@ -24,7 +88,7 @@
         />
         <sign-in-right-container
             is-domain-owner
-            :show-error-message="showErrorMessage"
+            :show-error-message="state.showErrorMessage"
         >
             <template #input>
                 <i-d-p-w-sign-in class="local-sign-in-wrapper"
@@ -35,110 +99,6 @@
         </sign-in-right-container>
     </div>
 </template>
-
-<script lang="ts">
-import {
-    toRefs, reactive, computed, getCurrentInstance,
-} from 'vue';
-import type { TranslateResult } from 'vue-i18n';
-import type { Vue } from 'vue/types/vue';
-
-import { SpaceRouter } from '@/router';
-import { store } from '@/store';
-
-import { isUserAccessibleToRoute } from '@/lib/access-control';
-import config from '@/lib/config';
-
-import ErrorHandler from '@/common/composables/error/errorHandler';
-
-import { getDefaultRouteAfterSignIn } from '@/services/auth/lib/helper';
-import { AUTH_ROUTE } from '@/services/auth/route-config';
-import IDPWSignIn from '@/services/auth/sign-in/local/template/ID_PW.vue';
-import SignInLeftContainer from '@/services/auth/sign-in/modules/SignInLeftContainer.vue';
-import SignInRightContainer from '@/services/auth/sign-in/modules/SignInRightContainer.vue';
-
-export default {
-    name: 'DomainAdminSignInPage',
-    components: {
-        SignInRightContainer,
-        SignInLeftContainer,
-        IDPWSignIn,
-    },
-    beforeRouteEnter(to, from, next) {
-        if (from.meta.isSignInPage) {
-            next((vm) => {
-                vm.$router.replace({
-                    query: { ...to.query, nextPath: from.query.nextPath },
-                }).catch(() => {});
-            });
-        } else next();
-    },
-    props: {
-        isDomainOwner: {
-            type: Boolean,
-            default: false,
-        },
-        nextPath: {
-            type: String,
-            default: undefined,
-        },
-    },
-    setup(props) {
-        const vm = getCurrentInstance()?.proxy as Vue;
-
-        const state = reactive({
-            userType: computed(() => (props.isDomainOwner ? 'DOMAIN_OWNER' : 'USER')),
-            authType: computed(() => store.state.domain.extendedAuthType),
-            ciLogoImage: computed(() => config.get('DOMAIN_IMAGE.CI_LOGO')),
-            ciTextWithTypeImage: computed(() => config.get('DOMAIN_IMAGE.CI_TEXT_WITH_TYPE')),
-            showErrorMessage: computed(() => store.state.display.isSignInFailed),
-            userId: '' as string | undefined,
-            password: '',
-        });
-
-        const validationState = reactive({
-            isIdValid: undefined as undefined | boolean,
-            idInvalidText: '' as TranslateResult | string,
-            isPasswordValid: undefined as undefined | boolean,
-            passwordInvalidText: '' as TranslateResult | string,
-            isPasswordCheckValid: undefined as undefined | boolean,
-            passwordCheckInvalidText: '' as TranslateResult | string,
-        });
-        const onSignIn = async () => {
-            try {
-                const defaultRoute = getDefaultRouteAfterSignIn(store.getters['user/isDomainOwner'], store.getters['user/hasSystemRole'], store.getters['user/hasPermission']);
-
-                if (!props.nextPath) {
-                    await vm.$router.push(defaultRoute);
-                    return;
-                }
-
-                const resolvedRoute = SpaceRouter.router.resolve(props.nextPath);
-                const isAccessible = isUserAccessibleToRoute(resolvedRoute.route, store.getters['user/pagePermissionList']);
-                if (isAccessible) {
-                    await vm.$router.push(props.nextPath);
-                } else {
-                    await vm.$router.push(defaultRoute);
-                }
-            } catch (e) {
-                ErrorHandler.handleError(e);
-                await store.dispatch('display/showSignInErrorMessage');
-            }
-        };
-
-        const goToUserSignIn = () => {
-            if (props.isDomainOwner) vm.$router.replace({ name: AUTH_ROUTE.SIGN_IN._NAME });
-        };
-        return {
-            ...toRefs(state),
-            ...toRefs(validationState),
-            onSignIn,
-            goToUserSignIn,
-        };
-    },
-};
-
-</script>
 
 <style lang="postcss" scoped>
 .wrapper {
--- apps/web/src/services/auth/sign-in/SignInPage.vue
+++ apps/web/src/services/auth/sign-in/SignInPage.vue
@@ -1,55 +1,8 @@
-<template>
-    <div class="wrapper">
-        <div class="ci-wrapper">
-            <template v-if="images">
-                <img class="logo-character"
-                     :src="images.ciLogo"
-                >
-                <img class="logo-text"
-                     :src="images.ciTextWithType"
-                >
-            </template>
-            <template v-else>
-                <img class="logo-character"
-                     src="@/assets/images/brand/brand_logo.png"
-                >
-                <img class="logo-text"
-                     src="@/assets/images/brand/spaceone-logotype-with-Service-Type.svg"
-                >
-            </template>
-        </div>
-        <sign-in-left-container
-            :is-admin="false"
-            :images="images"
-        />
-        <sign-in-right-container
-            :is-admin="false"
-            :images="images"
-            :show-error-message="showErrorMessage"
-        >
-            <template #input>
-                <i-d-p-w-sign-in class="id-pw-wrapper"
-                                 @sign-in="onSignIn"
-                />
-                <div v-if="component"
-                     class="btn-divider"
-                >
-                    <span>{{ $t('COMMON.SIGN_IN.OR') }}</span>
-                </div>
-                <component :is="component"
-                           class="sign-in-template"
-                           @sign-in="onSignIn"
-                />
-            </template>
-        </sign-in-right-container>
-    </div>
-</template>
-
-<script lang="ts">
+<script lang="ts" setup>
 import {
-    toRefs, reactive, computed, getCurrentInstance, watch,
+    reactive, computed, watch,
 } from 'vue';
-import type { Vue } from 'vue/types/vue';
+import { useRoute, useRouter } from 'vue-router/composables';
 
 import { isEmpty } from 'lodash';
 
@@ -66,105 +19,123 @@ import IDPWSignIn from '@/services/auth/sign-in/local/template/ID_PW.vue';
 import SignInLeftContainer from '@/services/auth/sign-in/modules/SignInLeftContainer.vue';
 import SignInRightContainer from '@/services/auth/sign-in/modules/SignInRightContainer.vue';
 
-export default {
-    name: 'SignInPage',
-    components: {
-        SignInRightContainer,
-        SignInLeftContainer,
-        IDPWSignIn,
-    },
-    beforeRouteEnter(to, from, next) {
-        if (from?.meta.isSignInPage) {
-            next((vm) => {
-                vm.$router.replace({
-                    query: { ...to.query, nextPath: from.query.nextPath },
-                }).catch(() => {});
-            });
-        } else next();
-    },
-    props: {
-        admin: {
-            type: Boolean,
-            default: false,
-        },
-        nextPath: {
-            type: String,
-            default: undefined,
-        },
-        error: {
-            type: String,
-            default: '',
-        },
-    },
-    setup(props) {
-        const vm = getCurrentInstance()?.proxy as Vue;
-        const state = reactive({
-            userType: computed(() => (props.admin ? 'DOMAIN_OWNER' : 'USER')),
-            authType: computed(() => store.state.domain.extendedAuthType),
-            beforeUser: store.state.user.userId,
-            component: computed(() => {
-                let component;
-                const auth = state.authType;
-                if (auth) {
-                    try {
-                        component = () => import(`./external/${auth}/template/${auth}.vue`);
-                    } catch (e) {
-                        ErrorHandler.handleError(e);
-                    }
-                }
-                return component;
-            }),
-            images: computed(() => {
-                const domainImage = config.get('DOMAIN_IMAGE');
-                if (!isEmpty(domainImage)) {
-                    return {
-                        ciLogo: config.get('DOMAIN_IMAGE.CI_LOGO'),
-                        ciTextWithType: config.get('DOMAIN_IMAGE.CI_TEXT_WITH_TYPE'),
-                        signIn: config.get('DOMAIN_IMAGE.SIGN_IN'),
-                    };
-                }
-                return undefined;
-            }),
-            showErrorMessage: vm.$route.query.error === 'error' || computed(() => store.state.display.isSignInFailed),
-        });
-        const onSignIn = async (userId:string) => {
+interface Props {
+    nextPath?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    nextPath: undefined,
+});
+
+const route = useRoute();
+const router = useRouter();
+
+const state = reactive({
+    authType: computed(() => store.state.domain.extendedAuthType),
+    beforeUser: store.state.user.userId,
+    component: computed(() => {
+        let component;
+        const auth = state.authType;
+        if (auth) {
             try {
-                const isSameUserAsPreviouslyLoggedInUser = state.beforeUser === userId;
-                const defaultRoute = getDefaultRouteAfterSignIn(store.getters['user/isDomainOwner'], store.getters['user/hasSystemRole'], store.getters['user/hasPermission']);
-
-                if (!props.nextPath || !isSameUserAsPreviouslyLoggedInUser) {
-                    await vm.$router.push(defaultRoute);
-                    return;
-                }
-
-                const resolvedRoute = SpaceRouter.router.resolve(props.nextPath);
-                const isAccessible = isUserAccessibleToRoute(resolvedRoute.route, store.getters['user/pagePermissionList']);
-                if (isAccessible) {
-                    await vm.$router.push(resolvedRoute.location);
-                } else {
-                    await vm.$router.push(defaultRoute);
-                }
+                component = () => import(`./external/${auth}/template/${auth}.vue`);
             } catch (e) {
                 ErrorHandler.handleError(e);
             }
-        };
+        }
+        return component;
+    }),
+    images: computed(() => {
+        const domainImage = config.get('DOMAIN_IMAGE');
+        if (!isEmpty(domainImage)) {
+            return {
+                ciLogo: config.get('DOMAIN_IMAGE.CI_LOGO'),
+                ciTextWithType: config.get('DOMAIN_IMAGE.CI_TEXT_WITH_TYPE'),
+                signIn: config.get('DOMAIN_IMAGE.SIGN_IN'),
+            };
+        }
+        return undefined;
+    }),
+    showErrorMessage: route.query.error === 'error' || computed(() => store.state.display.isSignInFailed),
+});
+const onSignIn = async (userId:string) => {
+    try {
+        const isSameUserAsPreviouslyLoggedInUser = state.beforeUser === userId;
+        const defaultRoute = getDefaultRouteAfterSignIn(store.getters['user/isDomainOwner'], store.getters['user/hasSystemRole'], store.getters['user/hasPermission']);
 
-        /* Watcher */
-        watch(() => vm.$route.query.error, (value) => {
-            state.showErrorMessage = !!value;
-        });
+        if (!props.nextPath || !isSameUserAsPreviouslyLoggedInUser) {
+            await router.push(defaultRoute);
+            return;
+        }
 
-        watch(() => vm.$route.name, () => {
-            store.dispatch('display/hideSignInErrorMessage');
-        }, { immediate: true });
-
-        return {
-            ...toRefs(state),
-            onSignIn,
-        };
-    },
+        const resolvedRoute = SpaceRouter.router.resolve(props.nextPath);
+        const isAccessible = isUserAccessibleToRoute(resolvedRoute.route, store.getters['user/pagePermissionList']);
+        if (isAccessible) {
+            await router.push(resolvedRoute.location);
+        } else {
+            await router.push(defaultRoute);
+        }
+    } catch (e) {
+        ErrorHandler.handleError(e);
+    }
 };
+
+/* Watcher */
+watch(() => route.query.error, (value) => {
+    state.showErrorMessage = !!value;
+});
+
+watch(() => route.name, () => {
+    store.dispatch('display/hideSignInErrorMessage');
+}, { immediate: true });
 </script>
+
+<template>
+    <div class="wrapper">
+        <div class="ci-wrapper">
+            <template v-if="state.images">
+                <img class="logo-character"
+                     :src="state.images.ciLogo"
+                >
+                <img class="logo-text"
+                     :src="state.images.ciTextWithType"
+                >
+            </template>
+            <template v-else>
+                <img class="logo-character"
+                     src="@/assets/images/brand/brand_logo.png"
+                >
+                <img class="logo-text"
+                     src="@/assets/images/brand/spaceone-logotype-with-Service-Type.svg"
+                >
+            </template>
+        </div>
+        <sign-in-left-container
+            :is-admin="false"
+            :images="state.images"
+        />
+        <sign-in-right-container
+            :is-admin="false"
+            :images="state.images"
+            :show-error-message="state.showErrorMessage"
+        >
+            <template #input>
+                <i-d-p-w-sign-in class="id-pw-wrapper"
+                                 @sign-in="onSignIn"
+                />
+                <div v-if="state.component"
+                     class="btn-divider"
+                >
+                    <span>{{ $t('COMMON.SIGN_IN.OR') }}</span>
+                </div>
+                <component :is="state.component"
+                           class="sign-in-template"
+                           @sign-in="onSignIn"
+                />
+            </template>
+        </sign-in-right-container>
+    </div>
+</template>
 
 <style lang="postcss" scoped>
 .wrapper {
