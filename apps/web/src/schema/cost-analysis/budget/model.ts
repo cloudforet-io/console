8c5b20c41c257865fd332a66e95aaa2395cd845b
Wanjin Noh <wanjin@megazone.com>
2023-11-22 17:49:02 +0900

    refactor: update schema directory
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/schema/cost-analysis/budget/model.ts
+++ apps/web/src/schema/cost-analysis/budget/model.ts
@@ -1,6 +1,7 @@
+import type { Currency } from '@/store/modules/settings/type';
+
 import type { Tags } from '@/api-schema/common/model';
 
-import type { Currency } from '@/store/modules/settings/type';
 
 
 type BudgetTimeUnit = 'MONTHLY' | 'YEARLY' | 'TOTAL';
