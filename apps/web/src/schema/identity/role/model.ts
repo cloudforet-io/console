38de8241bc37c14f332284d5b27fea33229ad590
Yongtae Park <samuel.park@megazone.com>
2023-11-24 19:51:18 +0900

    fix: replace RoleModel
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/schema/identity/role/model.ts
+++ apps/web/src/schema/identity/role/model.ts
@@ -1,25 +1,12 @@
 import type { Tags, TimeStamp } from '@/schema/_common/model';
 import type { RoleType, Policy } from '@/schema/identity/role/type';
 
-
-export interface RoleModel {
-    created_at: TimeStamp;
-    deleted_at?: TimeStamp;
-    domain_id: string;
-    name: string;
-    policies?: Policy[];
-    page_permissions: PagePermission[];
-    role_id: string;
-    role_type: RoleType;
-    tags?: Tags; // [ description: string ]
-}
-
 export interface PagePermission {
     page: string;
     permission: 'VIEW'|'MANAGE';
 }
 
-export interface RoleModel2 {
+export interface RoleModel {
     role_id: string;
     name: string;
     role_type: RoleType;
