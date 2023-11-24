7317fd2569dfa2b6bfea649fe83852356a9d5e6b
Wanjin Noh <wanjin@megazone.com>
2023-11-24 16:21:28 +0900

    fix: add invalid style for dashboard variable dropdown
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/dashboards/components/DashboardVariableDropdown.vue
+++ apps/web/src/services/dashboards/components/DashboardVariableDropdown.vue
@@ -195,7 +195,8 @@ const {
     >
         <button ref="targetRef"
                 class="dropdown-box"
-                :class="{ 'is-visible': visibleMenu, 'filled-value': state.selected.length }"
+                :class="{ 'is-visible': visibleMenu, 'filled-value': state.selected.length,
+                          invalid: dashboardDetailStore.isAllVariablesInitialized && state.variableProperty?.required && !state.selected.length }"
                 :disabled="state.variableProperty?.readonly || props.disabled"
                 @click="toggleMenu"
         >
@@ -266,6 +267,10 @@ const {
         border-radius: 0.75rem;
         height: 2rem;
         padding: 0 0.25rem 0 0.75rem;
+
+        &.invalid {
+            @apply border-alert;
+        }
 
         &[disabled=disabled] {
             @apply bg-gray-100;
