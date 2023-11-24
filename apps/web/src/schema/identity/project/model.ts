68fdc855f960cd70021d1a158784534050630a42
yuda <yuda@megazone.com>
2023-11-24 22:54:00 +0900

    feat: update project, projectGroup v2 model
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/schema/identity/project/model.ts
+++ apps/web/src/schema/identity/project/model.ts
@@ -1,13 +1,16 @@
 import type { Tags, TimeStamp } from '@/schema/_common/model';
-import type { ProjectGroupModel } from '@/schema/identity/project-group/model';
 
 
+type ProjectType = 'PRIVATE' | 'PUBLIC';
 export interface ProjectModel {
     project_id: string;
     name: string;
+    project_type: ProjectType;
     tags: Tags;
-    project_group_info: ProjectGroupModel;
+    users: string[];
+    user_groups: string[];
+    project_group_id: string;
+    workspace_id: string;
     domain_id: string;
-    created_by: string;
     created_at: TimeStamp;
 }
