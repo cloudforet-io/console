9eee218f7485ddb35a6e0742b3a035d79c700458
sulmo <sulmo@megazone.com>
2023-11-28 16:14:31 +0900

    chore: move the parameters type from type to api-verbs
    Signed-off-by: sulmo <sulmo@megazone.com>

---
+++ apps/web/src/schema/identity/service-account/api-verbs/list.ts
@@ -0,0 +1,11 @@
+export interface ServiceAccountListParameter {
+    domain_id?: string;
+    query?: any;
+    service_account_id?: string;
+    name?: string;
+    provider?: string;
+    has_secret?: boolean;
+    workspace_id?: string;
+    project_id?: string;
+    trusted_service_account_id?: string;
+}
