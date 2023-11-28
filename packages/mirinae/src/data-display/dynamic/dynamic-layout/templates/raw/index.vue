553a619ed9b03401ea66766de9cea26807dc7b61
Wanjin Noh <wanjin@megazone.com>
2023-11-28 13:49:46 +0900

    fix: change the type
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- packages/mirinae/src/data-display/dynamic/dynamic-layout/templates/raw/index.vue
+++ packages/mirinae/src/data-display/dynamic/dynamic-layout/templates/raw/index.vue
@@ -58,7 +58,7 @@ export default {
 
         const state = reactive({
             layoutName: computed(() => (props.options.translation_id ? vm.$t(props.options.translation_id) : props.name)),
-            rootData: computed<any[]>(() => {
+            rootData: computed<any>(() => {
                 if (props.options.root_path) {
                     const rootData = getValueByPath(props.data, props.options.root_path) ?? {};
                     return rootData;
