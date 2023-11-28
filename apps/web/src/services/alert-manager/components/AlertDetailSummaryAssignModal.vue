25247c18f8e3e9041726a9a8d9478381139cf08f
yuda <yuda@megazone.com>
2023-11-28 13:09:17 +0900

    refactor: use setup script
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/alert-manager/components/AlertDetailSummaryAssignModal.vue
+++ apps/web/src/services/alert-manager/components/AlertDetailSummaryAssignModal.vue
@@ -1,32 +1,6 @@
-<template>
-    <p-button-modal
-        class="alert-assign-modal"
-        :header-title="$t('MONITORING.ALERT.DETAIL.ASSIGN_MODAL_TITLE')"
-        size="md"
-        :disabled="!selectedUserID"
-        :visible.sync="proxyVisible"
-        @confirm="handleClickReassign"
-    >
-        <template #body>
-            <p-toolbox-table :excel-visible="false"
-                             selectable
-                             :multi-select="false"
-                             :fields="fields"
-                             :items="refinedItems.slice(pageStart - 1, pageStart + pageLimit - 1)"
-                             :select-index.sync="selectIndex"
-                             :loading="loading"
-                             :total-count="totalCount"
-                             @change="handleChangeTable"
-                             @refresh="handleChangeTable()"
-            />
-        </template>
-    </p-button-modal>
-</template>
-
-<script lang="ts">
-import type { SetupContext } from 'vue';
+<script lang="ts" setup>
 import {
-    computed, reactive, toRefs,
+    computed, reactive,
 } from 'vue';
 
 import { PButtonModal, PToolboxTable } from '@spaceone/design-system';
@@ -52,118 +26,128 @@ interface UserItem {
     user_id: string;
     user_name: string;
 }
-export default {
-    name: 'AlertDetailSummaryAssignModal',
-    components: {
-        PButtonModal,
-        PToolboxTable,
-    },
-    props: {
-        visible: {
-            type: Boolean,
-            default: false,
-        },
-        projectId: {
-            type: String,
-            default: undefined,
-        },
-        alertId: {
-            type: String,
-            default: undefined,
-        },
-    },
-    setup(props, { emit }: SetupContext) {
-        const alertPageStore = useAlertPageStore();
 
-        const storeState = reactive({
-            users: computed<UserReferenceMap>(() => store.getters['reference/userItems']),
-        });
-        const state = reactive({
-            proxyVisible: useProxyValue('visible', props, emit),
-            //
-            loading: true,
-            selectIndex: [] as number[],
-            selectedUserID: computed(() => state.refinedItems[state.selectIndex]?.resource_id),
-            fields: [
-                { label: 'User ID', name: 'user_id', type: 'item' },
-                { label: 'Name', name: 'user_name', type: 'item' },
-            ],
-            projectUserIdList: [] as string[],
-            refinedItems: computed<UserItem[]>(() => {
-                const users: UserItem[] = state.projectUserIdList.map((d) => ({
-                    user_id: d,
-                    user_name: storeState.users[d]?.name ?? d,
-                }));
-                return users.filter((d) => {
-                    const searchText = state.searchText.toLowerCase();
-                    return d.user_id.toLowerCase().includes(searchText)
+interface Props {
+    visible?: boolean;
+    projectId?: string;
+    alertId?: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+    visible: false,
+    projectId: '',
+    alertId: '',
+});
+const emit = defineEmits<{(e: 'update:visible', value: boolean): void;
+}>();
+
+const alertPageStore = useAlertPageStore();
+
+const storeState = reactive({
+    users: computed<UserReferenceMap>(() => store.getters['reference/userItems']),
+});
+const state = reactive({
+    proxyVisible: useProxyValue('visible', props, emit),
+    //
+    loading: true,
+    selectIndex: [] as number[],
+    selectedUserID: computed(() => state.refinedItems[state.selectIndex]?.resource_id),
+    fields: [
+        { label: 'User ID', name: 'user_id', type: 'item' },
+        { label: 'Name', name: 'user_name', type: 'item' },
+    ],
+    projectUserIdList: [] as string[],
+    refinedItems: computed<UserItem[]>(() => {
+        const users: UserItem[] = state.projectUserIdList.map((d) => ({
+            user_id: d,
+            user_name: storeState.users[d]?.name ?? d,
+        }));
+        return users.filter((d) => {
+            const searchText = state.searchText.toLowerCase();
+            return d.user_id.toLowerCase().includes(searchText)
                         || d.user_name.toLowerCase().includes(searchText);
-                });
-            }),
-            totalCount: 0,
-            searchText: '',
-            pageLimit: 15,
-            pageStart: 1,
         });
+    }),
+    totalCount: 0,
+    searchText: '',
+    pageLimit: 15,
+    pageStart: 1,
+});
 
-        const reassignMember = async () => {
-            try {
-                await alertPageStore.updateAlertData({
-                    updateParams: {
-                        assignee: state.selectedUserID,
-                    },
-                    alertId: props.alertId,
-                });
-                showSuccessMessage(i18n.t('MONITORING.ALERT.DETAIL.HEADER.ALT_S_ASSIGN_MEMBER'), '');
-            } catch (e) {
-                ErrorHandler.handleRequestError(e, i18n.t('MONITORING.ALERT.DETAIL.HEADER.ALT_E_ASSIGN_MEMBER'));
-            } finally {
-                state.proxyVisible = false;
-            }
-        };
-
-        const handleClickReassign = async () => {
-            await reassignMember();
-        };
-
-        const getProjectUserData = async () => {
-            try {
-                state.loading = true;
-                const params: ProjectGetRequestParams = {
-                    workspace_id: '', // TODO: workspace_id
-                    project_id: props.projectId,
-                };
-                const res: ProjectModel = await SpaceConnector.clientV2.identity.project.get(params);
-                state.projectUserIdList = res.users ?? [];
-                state.totalCount = res.users?.length ?? 0;
-            } catch (e) {
-                ErrorHandler.handleError(e);
-                state.projectUserIdList = [];
-            } finally {
-                state.loading = false;
-            }
-        };
-
-        const handleChangeTable = async (options: any = {}) => {
-            // TODO: check it's working
-            if (options.searchText !== undefined) state.searchText = options.searchText;
-            if (options.pageLimit !== undefined) state.pageLimit = options.pageLimit;
-            if (options.pageStart !== undefined) state.pageStart = options.pageStart;
-        };
-
-        // LOAD REFERENCE STORE
-        (async () => {
-            await Promise.allSettled([
-                getProjectUserData(),
-                store.dispatch('reference/user/load'),
-            ]);
-        })();
-
-        return {
-            ...toRefs(state),
-            handleClickReassign,
-            handleChangeTable,
-        };
-    },
+const reassignMember = async () => {
+    try {
+        await alertPageStore.updateAlertData({
+            updateParams: {
+                assignee: state.selectedUserID,
+            },
+            alertId: props.alertId,
+        });
+        showSuccessMessage(i18n.t('MONITORING.ALERT.DETAIL.HEADER.ALT_S_ASSIGN_MEMBER'), '');
+    } catch (e) {
+        ErrorHandler.handleRequestError(e, i18n.t('MONITORING.ALERT.DETAIL.HEADER.ALT_E_ASSIGN_MEMBER'));
+    } finally {
+        state.proxyVisible = false;
+    }
 };
+
+const handleClickReassign = async () => {
+    await reassignMember();
+};
+
+const getProjectUserData = async () => {
+    try {
+        state.loading = true;
+        const params: ProjectGetRequestParams = {
+            workspace_id: '', // TODO: workspace_id
+            project_id: props.projectId,
+        };
+        const res: ProjectModel = await SpaceConnector.clientV2.identity.project.get(params);
+        state.projectUserIdList = res.users ?? [];
+        state.totalCount = res.users?.length ?? 0;
+    } catch (e) {
+        ErrorHandler.handleError(e);
+        state.projectUserIdList = [];
+    } finally {
+        state.loading = false;
+    }
+};
+
+const handleChangeTable = async (options: any = {}) => {
+    // TODO: check it's working
+    if (options.searchText !== undefined) state.searchText = options.searchText;
+    if (options.pageLimit !== undefined) state.pageLimit = options.pageLimit;
+    if (options.pageStart !== undefined) state.pageStart = options.pageStart;
+};
+
+// LOAD REFERENCE STORE
+(async () => {
+    await Promise.allSettled([
+        getProjectUserData(),
+        store.dispatch('reference/user/load'),
+    ]);
+})();
 </script>
+
+<template>
+    <p-button-modal
+        class="alert-assign-modal"
+        :header-title="$t('MONITORING.ALERT.DETAIL.ASSIGN_MODAL_TITLE')"
+        size="md"
+        :disabled="!state.selectedUserID"
+        :visible.sync="state.proxyVisible"
+        @confirm="handleClickReassign"
+    >
+        <template #body>
+            <p-toolbox-table :excel-visible="false"
+                             selectable
+                             :multi-select="false"
+                             :fields="state.fields"
+                             :items="state.refinedItems.slice(state.pageStart - 1, state.pageStart + state.pageLimit - 1)"
+                             :select-index.sync="state.selectIndex"
+                             :loading="state.loading"
+                             :total-count="state.totalCount"
+                             @change="handleChangeTable"
+                             @refresh="handleChangeTable()"
+            />
+        </template>
+    </p-button-modal>
+</template>
--- apps/web/src/services/asset-inventory/components/CloudServiceAdmin.vue
+++ apps/web/src/services/asset-inventory/components/CloudServiceAdmin.vue
@@ -1,23 +1,6 @@
-<template>
-    <div>
-        <p-heading heading-type="sub"
-                   :title="$t('INVENTORY.CLOUD_SERVICE.ADMIN.MEMBER')"
-                   :total-count="totalCount"
-        />
-        <p-toolbox-table :fields="fields"
-                         :items="refinedItems.slice(pageStart - 1, pageStart + pageLimit - 1)"
-                         :loading="loading"
-                         :total-count="totalCount"
-                         @export="handleExport"
-                         @change="handleChangeTable"
-                         @refresh="handleChangeTable()"
-        />
-    </div>
-</template>
-
-<script lang="ts">
+<script lang="ts" setup>
 import {
-    computed, reactive, toRefs, watch,
+    computed, reactive, watch,
 } from 'vue';
 
 import {
@@ -38,104 +21,107 @@ import { downloadExcel } from '@/lib/helper/file-download-helper';
 import ErrorHandler from '@/common/composables/error/errorHandler';
 
 
+interface Props {
+    cloudServiceProjectId?: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+    cloudServiceProjectId: '',
+});
 interface UserItem {
     user_id: string;
     user_name: string;
 }
-export default {
-    name: 'CloudServiceAdmin',
-    components: {
-        PHeading,
-        PToolboxTable,
-    },
-    props: {
-        cloudServiceProjectId: {
-            type: String,
-            default: undefined,
-        },
-    },
-    setup(props) {
-        const storeState = reactive({
-            users: computed<UserReferenceMap>(() => store.getters['reference/userItems']),
-        });
-        const state = reactive({
-            fields: [
-                { label: 'User ID', name: 'user_id' },
-                { label: 'User Name', name: 'user_name' },
-            ],
-            projectUserIdList: [] as string[],
-            refinedItems: computed<UserItem[]>(() => {
-                const users: UserItem[] = state.projectUserIdList.map((d) => ({
-                    user_id: d,
-                    user_name: storeState.users[d]?.name ?? d,
-                }));
-                return users.filter((d) => {
-                    const searchText = state.searchText.toLowerCase();
-                    return d.user_id.toLowerCase().includes(searchText)
+
+const storeState = reactive({
+    users: computed<UserReferenceMap>(() => store.getters['reference/userItems']),
+});
+const state = reactive({
+    fields: [
+        { label: 'User ID', name: 'user_id' },
+        { label: 'User Name', name: 'user_name' },
+    ],
+    projectUserIdList: [] as string[],
+    refinedItems: computed<UserItem[]>(() => {
+        const users: UserItem[] = state.projectUserIdList.map((d) => ({
+            user_id: d,
+            user_name: storeState.users[d]?.name ?? d,
+        }));
+        return users.filter((d) => {
+            const searchText = state.searchText.toLowerCase();
+            return d.user_id.toLowerCase().includes(searchText)
                         || d.user_name.toLowerCase().includes(searchText);
-                });
-            }),
-            loading: true,
-            totalCount: 0,
-            searchText: '',
-            pageLimit: 15,
-            pageStart: 1,
-            timezone: computed(() => store.state.user.timezone ?? 'UTC'),
         });
+    }),
+    loading: true,
+    totalCount: 0,
+    searchText: '',
+    pageLimit: 15,
+    pageStart: 1,
+    timezone: computed(() => store.state.user.timezone ?? 'UTC'),
+});
 
-        const getProjectUserData = async () => {
-            state.loading = true;
-            try {
-                const params: ProjectGetRequestParams = {
-                    workspace_id: '', // TODO: workspace_id
-                    project_id: props.cloudServiceProjectId,
-                };
-                const res: ProjectModel = await SpaceConnector.clientV2.identity.project.get(params);
-                state.projectUserIdList = res.users ?? [];
-                state.totalCount = res.users?.length ?? 0;
-            } catch (e) {
-                ErrorHandler.handleError(e);
-                state.projectUserIdList = [];
-            } finally {
-                state.loading = false;
-            }
+const getProjectUserData = async () => {
+    state.loading = true;
+    try {
+        const params: ProjectGetRequestParams = {
+            workspace_id: '', // TODO: workspace_id
+            project_id: props.cloudServiceProjectId,
         };
-
-        const handleChangeTable = async (options: any = {}) => {
-            // TODO: check it's working
-            if (options.searchText !== undefined) state.searchText = options.searchText;
-            if (options.pageLimit !== undefined) state.pageLimit = options.pageLimit;
-            if (options.pageStart !== undefined) state.pageStart = options.pageStart;
-        };
-
-        const handleExport = async () => {
-            await downloadExcel({
-                data: state.refinedItems,
-                fields: state.fields,
-                file_name_prefix: FILE_NAME_PREFIX.cloudService,
-                timezone: state.timezone,
-            });
-        };
-
-        watch(() => props.cloudServiceProjectId, (after, before) => {
-            if (!after) {
-                state.projectUserIdList = [];
-            } else if (after !== before) getProjectUserData();
-        }, { immediate: true });
-
-        // LOAD REFERENCE STORE
-        (async () => {
-            await store.dispatch('reference/user/load');
-        })();
-
-        return {
-            ...toRefs(state),
-            handleChangeTable,
-            handleExport,
-        };
-    },
+        const res: ProjectModel = await SpaceConnector.clientV2.identity.project.get(params);
+        state.projectUserIdList = res.users ?? [];
+        state.totalCount = res.users?.length ?? 0;
+    } catch (e) {
+        ErrorHandler.handleError(e);
+        state.projectUserIdList = [];
+    } finally {
+        state.loading = false;
+    }
 };
+
+const handleChangeTable = async (options: any = {}) => {
+    // TODO: check it's working
+    if (options.searchText !== undefined) state.searchText = options.searchText;
+    if (options.pageLimit !== undefined) state.pageLimit = options.pageLimit;
+    if (options.pageStart !== undefined) state.pageStart = options.pageStart;
+};
+
+const handleExport = async () => {
+    await downloadExcel({
+        data: state.refinedItems,
+        fields: state.fields,
+        file_name_prefix: FILE_NAME_PREFIX.cloudService,
+        timezone: state.timezone,
+    });
+};
+
+watch(() => props.cloudServiceProjectId, (after, before) => {
+    if (!after) {
+        state.projectUserIdList = [];
+    } else if (after !== before) getProjectUserData();
+}, { immediate: true });
+
+// LOAD REFERENCE STORE
+(async () => {
+    await store.dispatch('reference/user/load');
+})();
 </script>
+
+<template>
+    <div>
+        <p-heading heading-type="sub"
+                   :title="$t('INVENTORY.CLOUD_SERVICE.ADMIN.MEMBER')"
+                   :total-count="state.totalCount"
+        />
+        <p-toolbox-table :fields="state.fields"
+                         :items="state.refinedItems.slice(state.pageStart - 1, state.pageStart + state.pageLimit - 1)"
+                         :loading="state.loading"
+                         :total-count="state.totalCount"
+                         @export="handleExport"
+                         @change="handleChangeTable"
+                         @refresh="handleChangeTable()"
+        />
+    </div>
+</template>
 
 <style lang="postcss" scoped>
 /* custom design-system component - p-search-table */
