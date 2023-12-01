0201c96f52969ecb8e1576b70ed98d7539089d50
yuda <yuda@megazone.com>
2023-12-01 19:31:31 +0900

    feat: delete KB auth
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/auth/authenticator/loader.ts
+++ apps/web/src/services/auth/authenticator/loader.ts
@@ -1,11 +1,9 @@
 import { GoogleAuth } from '@/services/auth/authenticator/external/GOOGLE_OAUTH2/authenticator/google-auth';
-import { KbAuth } from '@/services/auth/authenticator/external/KB_SSO/authenticator/kb-auth';
 import { KeycloakAuth } from '@/services/auth/authenticator/external/KEYCLOAK/authenticator/keycloak-auth';
 import { SpaceAuth } from '@/services/auth/authenticator/local/authenticator/space-auth';
 
 export const loadAuth = (authType?): any => {
     if (authType === 'GOOGLE_OAUTH2') return GoogleAuth;
     if (authType === 'KEYCLOAK') return KeycloakAuth;
-    if (authType === 'KB_SSO') return KbAuth;
     return SpaceAuth;
 };
