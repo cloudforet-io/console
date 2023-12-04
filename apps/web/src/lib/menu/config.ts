aac84ecc059198642dd151580e605eef661227c8
Yongtae Park <samuel.park@megazone.com>
2023-12-04 11:47:05 +0900

    fix(menu): refactor menu-id & apply to menu configs
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/lib/menu/config.ts
+++ apps/web/src/lib/menu/config.ts
@@ -4,49 +4,49 @@ import type { HighlightTagType } from '@/store/modules/display/type';
 export const MENU_ID = Object.freeze({
     HOME_DASHBOARD: 'home-dashboard',
     DASHBOARDS: 'dashboards',
-    DASHBOARDS_PROJECT: 'dashboards.project',
-    DASHBOARDS_WORKSPACE: 'dashboards.workspace',
+    PROJECT_DASHBOARDS: 'project_dashboards',
+    WORKSPACE_DASHBOARDS: 'workspace_dashboards',
     PROJECT: 'project',
     ASSET_INVENTORY: 'asset_inventory',
-    ASSET_INVENTORY_CLOUD_SERVICE: 'asset_inventory.cloud_service',
-    ASSET_INVENTORY_SERVER: 'asset_inventory.server',
-    ASSET_INVENTORY_COLLECTOR: 'asset_inventory.collector',
-    ASSET_INVENTORY_SERVICE_ACCOUNT: 'asset_inventory.service_account',
+    CLOUD_SERVICE: 'cloud_service',
+    COLLECTOR: 'collector',
+    SERVICE_ACCOUNT: 'service_account',
     COST_EXPLORER: 'cost_explorer',
-    COST_EXPLORER_LANDING: 'cost_explorer.landing',
-    COST_EXPLORER_COST_ANALYSIS: 'cost_explorer.cost_analysis',
-    COST_EXPLORER_BUDGET: 'cost_explorer.budget',
+    COST_ANALYSIS: 'cost_analysis',
+    BUDGET: 'budget',
     ALERT_MANAGER: 'alert_manager',
-    ALERT_MANAGER_DASHBOARD: 'alert_manager.dashboard',
-    ALERT_MANAGER_ALERT: 'alert_manager.alert',
-    ALERT_MANAGER_ESCALATION_POLICY: 'alert_manager.escalation_policy',
+    ALERT_MANAGER_DASHBOARD: 'alert_manager_dashboard',
+    ALERT: 'alert',
+    ESCALATION_POLICY: 'escalation_policy',
     ADMINISTRATION: 'administration',
-    ADMINISTRATION_IAM: 'administration.iam',
-    ADMINISTRATION_USER: 'administration.user',
-    ADMINISTRATION_ROLE: 'administration.role',
-    ADMINISTRATION_POLICY: 'administration.policy',
-    ADMINISTRATION_PREFERENCE: 'administration.preference',
-    ADMINISTRATION_DOMAIN_SETTINGS: 'administration.domain_settings',
-    ADMINISTRATION_WORKSPACES: 'administration.workspaces',
+    IAM: 'iam',
+    USER: 'user',
+    ROLE: 'role',
+    POLICY: 'policy',
+    PREFERENCE: 'preference',
+    DOMAIN_SETTINGS: 'domain_settings',
+    WORKSPACES: 'workspaces',
     MY_PAGE: 'my_page',
-    MY_PAGE_ACCOUNT: 'my_page.account',
-    MY_PAGE_ACCOUNT_PROFILE: 'my_page.account_profile',
-    MY_PAGE_API_KEY: 'my_page.api_key',
-    MY_PAGE_NOTIFICATIONS: 'my_page.notifications',
+    ACCOUNT: 'account',
+    ACCOUNT_PROFILE: 'account_profile',
+    API_KEY: 'api_key',
+    NOTIFICATIONS: 'notifications',
     INFO: 'info',
-    INFO_NOTICE: 'info.notice',
+    NOTICE: 'notice',
 } as const);
 
 export type MenuId = typeof MENU_ID[keyof typeof MENU_ID];
 
 export interface Menu {
     id: MenuId;
+    needPermissionByRole?: boolean;
     subMenuList?: Menu[];
     hideOnGNB?: boolean;
     hideOnSiteMap?: boolean;
 }
 
 export interface MenuInfo {
+    menuId: MenuId;
     translationId: string;
     icon?: string;
     highlightTag?: HighlightTagType;
--- apps/web/src/lib/menu/menu-info.ts
+++ apps/web/src/lib/menu/menu-info.ts
@@ -3,139 +3,162 @@ import { MENU_ID } from '@/lib/menu/config';
 
 export const MENU_INFO_MAP: Record<MenuId, MenuInfo> = Object.freeze({
     [MENU_ID.HOME_DASHBOARD]: {
+        menuId: MENU_ID.HOME_DASHBOARD,
         translationId: 'MENU.HOME_DASHBOARD',
         icon: 'ic_service_home',
     },
     [MENU_ID.DASHBOARDS]: {
+        menuId: MENU_ID.DASHBOARDS,
         translationId: 'MENU.DASHBOARDS',
         icon: 'ic_service_dashboard',
         needPermissionByRole: true,
         highlightTag: 'new',
     },
-    [MENU_ID.DASHBOARDS_PROJECT]: {
+    [MENU_ID.PROJECT_DASHBOARDS]: {
+        menuId: MENU_ID.PROJECT_DASHBOARDS,
         translationId: 'MENU.DASHBOARDS_PROJECT',
         icon: 'ic_service_dashboard',
         needPermissionByRole: true,
     },
-    [MENU_ID.DASHBOARDS_WORKSPACE]: {
+    [MENU_ID.WORKSPACE_DASHBOARDS]: {
+        menuId: MENU_ID.WORKSPACE_DASHBOARDS,
         translationId: 'MENU.DASHBOARDS_WORKSPACE',
         icon: 'ic_service_dashboard',
         needPermissionByRole: true,
     },
     [MENU_ID.PROJECT]: {
+        menuId: MENU_ID.PROJECT,
         translationId: 'MENU.PROJECT',
         icon: 'ic_service_project',
         needPermissionByRole: true,
     },
     [MENU_ID.ASSET_INVENTORY]: {
+        menuId: MENU_ID.ASSET_INVENTORY,
         translationId: 'MENU.ASSET_INVENTORY',
         icon: 'ic_service_asset-inventory',
         needPermissionByRole: true,
     },
-    [MENU_ID.ASSET_INVENTORY_CLOUD_SERVICE]: {
+    [MENU_ID.CLOUD_SERVICE]: {
+        menuId: MENU_ID.CLOUD_SERVICE,
         translationId: 'MENU.ASSET_INVENTORY_CLOUD_SERVICE',
         needPermissionByRole: true,
     },
-    [MENU_ID.ASSET_INVENTORY_SERVER]: {
-        translationId: 'MENU.ASSET_INVENTORY_SERVER',
-        needPermissionByRole: true,
-    },
-    [MENU_ID.ASSET_INVENTORY_COLLECTOR]: {
+    [MENU_ID.COLLECTOR]: {
+        menuId: MENU_ID.COLLECTOR,
         translationId: 'MENU.ASSET_INVENTORY_COLLECTOR',
         needPermissionByRole: true,
     },
-    [MENU_ID.ASSET_INVENTORY_SERVICE_ACCOUNT]: {
+    [MENU_ID.SERVICE_ACCOUNT]: {
+        menuId: MENU_ID.SERVICE_ACCOUNT,
         translationId: 'MENU.ASSET_INVENTORY_SERVICE_ACCOUNT',
         needPermissionByRole: true,
     },
     [MENU_ID.COST_EXPLORER]: {
+        menuId: MENU_ID.COST_EXPLORER,
         translationId: 'MENU.COST_EXPLORER',
         icon: 'ic_service_cost-explorer',
         needPermissionByRole: true,
     },
-    [MENU_ID.COST_EXPLORER_LANDING]: {
-        translationId: 'MENU.COST_EXPLORER_LANDING',
-        needPermissionByRole: true,
-    },
-    [MENU_ID.COST_EXPLORER_COST_ANALYSIS]: {
+    [MENU_ID.COST_ANALYSIS]: {
+        menuId: MENU_ID.COST_ANALYSIS,
         translationId: 'MENU.COST_EXPLORER_COST_ANALYSIS',
         needPermissionByRole: true,
     },
-    [MENU_ID.COST_EXPLORER_BUDGET]: {
+    [MENU_ID.BUDGET]: {
+        menuId: MENU_ID.BUDGET,
         translationId: 'MENU.COST_EXPLORER_BUDGET',
         needPermissionByRole: true,
     },
     [MENU_ID.ALERT_MANAGER]: {
+        menuId: MENU_ID.ALERT_MANAGER,
         translationId: 'MENU.ALERT_MANAGER',
         icon: 'ic_service_alert-manager',
         needPermissionByRole: true,
     },
     [MENU_ID.ALERT_MANAGER_DASHBOARD]: {
+        menuId: MENU_ID.ALERT_MANAGER_DASHBOARD,
         translationId: 'MENU.ALERT_MANAGER_DASHBOARD',
         needPermissionByRole: true,
     },
-    [MENU_ID.ALERT_MANAGER_ALERT]: {
+    [MENU_ID.ALERT]: {
+        menuId: MENU_ID.ALERT,
         translationId: 'MENU.ALERT_MANAGER_ALERT',
         needPermissionByRole: true,
     },
-    [MENU_ID.ALERT_MANAGER_ESCALATION_POLICY]: {
+    [MENU_ID.ESCALATION_POLICY]: {
+        menuId: MENU_ID.ESCALATION_POLICY,
         translationId: 'MENU.ALERT_MANAGER_ESCALATION_POLICY',
         needPermissionByRole: true,
     },
     [MENU_ID.ADMINISTRATION]: {
+        menuId: MENU_ID.ADMINISTRATION,
         translationId: 'MENU.ADMINISTRATION',
         icon: 'ic_service_administration',
         needPermissionByRole: true,
     },
-    [MENU_ID.ADMINISTRATION_IAM]: {
+    [MENU_ID.IAM]: {
+        menuId: MENU_ID.IAM,
         translationId: 'MENU.ADMINISTRATION_IAM',
         needPermissionByRole: true,
     },
-    [MENU_ID.ADMINISTRATION_USER]: {
+    [MENU_ID.USER]: {
+        menuId: MENU_ID.USER,
         translationId: 'MENU.ADMINISTRATION_USER',
         needPermissionByRole: true,
     },
-    [MENU_ID.ADMINISTRATION_ROLE]: {
+    [MENU_ID.ROLE]: {
+        menuId: MENU_ID.ROLE,
         translationId: 'MENU.ADMINISTRATION_ROLE',
         needPermissionByRole: true,
     },
-    [MENU_ID.ADMINISTRATION_POLICY]: {
+    [MENU_ID.POLICY]: {
+        menuId: MENU_ID.POLICY,
         translationId: 'MENU.ADMINISTRATION_POLICY',
         needPermissionByRole: true,
     },
-    [MENU_ID.ADMINISTRAION_PREFERENCE]: {
+    [MENU_ID.PREFERENCE]: {
+        menuId: MENU_ID.PREFERENCE,
         translationId: 'MENU.ADMINISTRATION_PREFRENCE',
         needPermissionByRole: true,
     },
-    [MENU_ID.ADMINISTRATION_DOMAIN_SETTINGS]: {
+    [MENU_ID.DOMAIN_SETTINGS]: {
+        menuId: MENU_ID.DOMAIN_SETTINGS,
         translationId: 'MENU.ADMINISTRATION_DOMAIN_SETTINGS',
         needPermissionByRole: true,
     },
-    [MENU_ID.ADMINISTRATION_WORKSPACES]: {
+    [MENU_ID.WORKSPACES]: {
+        menuId: MENU_ID.WORKSPACES,
         translationId: 'MENU.ADMINISTRATION_WORKSPACES',
         needPermissionByRole: true,
     },
     [MENU_ID.MY_PAGE]: {
+        menuId: MENU_ID.MY_PAGE,
         translationId: 'MENU.MY_PAGE',
         icon: 'ic_service_my-page',
     },
-    [MENU_ID.MY_PAGE_ACCOUNT]: {
+    [MENU_ID.ACCOUNT]: {
+        menuId: MENU_ID.ACCOUNT,
         translationId: 'MENU.MY_PAGE_ACCOUNT',
     },
-    [MENU_ID.MY_PAGE_ACCOUNT_PROFILE]: {
+    [MENU_ID.ACCOUNT_PROFILE]: {
+        menuId: MENU_ID.ACCOUNT_PROFILE,
         translationId: 'MENU.MY_PAGE_ACCOUNT_PROFILE',
     },
-    [MENU_ID.MY_PAGE_API_KEY]: {
+    [MENU_ID.API_KEY]: {
+        menuId: MENU_ID.API_KEY,
         translationId: 'MENU.MY_PAGE_API_KEY',
     },
-    [MENU_ID.MY_PAGE_NOTIFICATIONS]: {
+    [MENU_ID.NOTIFICATIONS]: {
+        menuId: MENU_ID.NOTIFICATIONS,
         translationId: 'MENU.MY_PAGE_NOTIFICATIONS',
     },
     [MENU_ID.INFO]: {
+        menuId: MENU_ID.INFO,
         translationId: 'MENU.INFO',
         icon: 'ic_service_info',
     },
-    [MENU_ID.INFO_NOTICE]: {
+    [MENU_ID.NOTICE]: {
+        menuId: MENU_ID.NOTICE,
         translationId: 'MENU.INFO_NOTICE',
     },
 });
