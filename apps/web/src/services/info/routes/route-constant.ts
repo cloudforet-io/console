12ff661cfeef3dfb3c87ffc7eec9710f1d337e2e
Yongtae Park <samuel.park@megazone.com>
2023-12-04 12:03:51 +0900

    refactor(info): apply new menu-id structure
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/services/info/routes/route-constant.ts
+++ apps/web/src/services/info/routes/route-constant.ts
@@ -3,9 +3,9 @@ import { MENU_ID } from '@/lib/menu/config';
 export const INFO_ROUTE = Object.freeze({
     _NAME: MENU_ID.INFO,
     NOTICE: {
-        _NAME: MENU_ID.INFO_NOTICE,
-        CREATE: { _NAME: `${MENU_ID.INFO_NOTICE}.create` },
-        UPDATE: { _NAME: `${MENU_ID.INFO_NOTICE}.update` },
-        DETAIL: { _NAME: `${MENU_ID.INFO_NOTICE}.detail` },
+        _NAME: `${MENU_ID.INFO}.${MENU_ID.NOTICE}`,
+        CREATE: { _NAME: `${MENU_ID.INFO}.${MENU_ID.NOTICE}.create` },
+        UPDATE: { _NAME: `${MENU_ID.INFO}.${MENU_ID.NOTICE}.update` },
+        DETAIL: { _NAME: `${MENU_ID.INFO}.${MENU_ID.NOTICE}.detail` },
     },
 });
