26a19c6faedf764dc331f376d13e359261658b98
yuda <yuda@megazone.com>
2023-12-04 22:07:57 +0900

    refactor: use setup script
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/project/components/ProjectAlertSettingsNotificationPolicyUpdateModal.vue
+++ apps/web/src/services/project/components/ProjectAlertSettingsNotificationPolicyUpdateModal.vue
@@ -1,36 +1,6 @@
-<template>
-    <p-button-modal
-        :header-title="$t('PROJECT.DETAIL.ALERT.SET_NOTIFICATION_POLICY_MODAL_TITLE')"
-        size="sm"
-        fade
-        :visible.sync="proxyVisible"
-        @confirm="onClickConfirm"
-    >
-        <template #body>
-            <div class="content-wrapper">
-                <p>{{ $t('PROJECT.DETAIL.ALERT.SET_NOTIFICATION_MODAL_HELP_TEXT') }}</p>
-                <div class="select-card-wrapper">
-                    <p-select-card v-for="(option, index) in selectOptions"
-                                   :key="option.name"
-                                   v-model="notificationUrgency"
-                                   :tab-index="index"
-                                   :value="option.name"
-                                   :label="option.label"
-                                   :icon="option.icon"
-                                   :icon-color="urgencyColor"
-                                   block
-                    />
-                </div>
-            </div>
-        </template>
-    </p-button-modal>
-</template>
-
-<script lang="ts">
-
-import type { SetupContext } from 'vue';
+<script lang="ts" setup>
 import {
-    reactive, toRefs, watch,
+    reactive, watch,
 } from 'vue';
 
 import {
@@ -48,65 +18,81 @@ import { useProxyValue } from '@/common/composables/proxy-state';
 
 import { red } from '@/styles/colors';
 
-export default {
-    name: 'ProjectAlertSettingsNotificationPolicyUpdateModal',
-    components: {
-        PButtonModal,
-        PSelectCard,
-    },
-    props: {
-        projectId: {
-            type: String,
-            default: undefined,
-        },
-        visible: {
-            type: Boolean,
-            required: true,
-        },
-        selectOptions: {
-            type: Array,
-            default: () => ([]),
-        },
-        selectedOption: {
-            type: String,
-            default: undefined,
-        },
-    },
-    setup(props, { emit }: SetupContext) {
-        const state = reactive({
-            proxyVisible: useProxyValue('visible', props, emit),
-            notificationUrgency: undefined,
+
+const URGENCY_COLOR = red[400];
+
+interface Props {
+    projectId?: string;
+    visible: boolean;
+    selectOptions: {
+        name: string;
+        label: string;
+        icon?: string;
+    }[];
+    selectedOption?: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+    projectId: undefined,
+    visible: false,
+    selectOptions: () => ([]),
+    selectedOption: undefined,
+});
+const emit = defineEmits<{(e: 'confirm'): void}>();
+
+const state = reactive({
+    proxyVisible: useProxyValue('visible', props, emit),
+    notificationUrgency: undefined as string|undefined,
+});
+
+const onClickConfirm = async () => {
+    try {
+        await SpaceConnector.client.monitoring.projectAlertConfig.update({
+            project_id: props.projectId,
+            options: {
+                notification_urgency: state.notificationUrgency,
+            },
         });
-
-        const onClickConfirm = async () => {
-            try {
-                await SpaceConnector.client.monitoring.projectAlertConfig.update({
-                    project_id: props.projectId,
-                    options: {
-                        notification_urgency: state.notificationUrgency,
-                    },
-                });
-                showSuccessMessage(i18n.t('PROJECT.DETAIL.ALERT.ALT_S_CHANGE_NOTIFICATION_POLICY'), '');
-                emit('confirm');
-            } catch (e) {
-                ErrorHandler.handleRequestError(e, i18n.t('PROJECT.DETAIL.ALERT.ALT_E_CHANGE_NOTIFICATION_POLICY'));
-            } finally {
-                state.proxyVisible = false;
-            }
-        };
-
-        watch([() => props.selectedOption, () => props.visible], ([selectedOption]) => {
-            state.notificationUrgency = selectedOption;
-        });
-
-        return {
-            ...toRefs(state),
-            onClickConfirm,
-            urgencyColor: red[400],
-        };
-    },
+        showSuccessMessage(i18n.t('PROJECT.DETAIL.ALERT.ALT_S_CHANGE_NOTIFICATION_POLICY'), '');
+        emit('confirm');
+    } catch (e) {
+        ErrorHandler.handleRequestError(e, i18n.t('PROJECT.DETAIL.ALERT.ALT_E_CHANGE_NOTIFICATION_POLICY'));
+    } finally {
+        state.proxyVisible = false;
+    }
 };
+
+watch([() => props.selectedOption, () => props.visible], ([selectedOption]) => {
+    state.notificationUrgency = selectedOption;
+});
 </script>
+
+<template>
+    <p-button-modal
+        :header-title="$t('PROJECT.DETAIL.ALERT.SET_NOTIFICATION_POLICY_MODAL_TITLE')"
+        size="sm"
+        fade
+        :visible.sync="state.proxyVisible"
+        @confirm="onClickConfirm"
+    >
+        <template #body>
+            <div class="content-wrapper">
+                <p>{{ $t('PROJECT.DETAIL.ALERT.SET_NOTIFICATION_MODAL_HELP_TEXT') }}</p>
+                <div class="select-card-wrapper">
+                    <p-select-card v-for="(option, index) in selectOptions"
+                                   :key="option.name"
+                                   v-model="state.notificationUrgency"
+                                   :tab-index="index"
+                                   :value="option.name"
+                                   :label="option.label"
+                                   :icon="option.icon"
+                                   :icon-color="URGENCY_COLOR"
+                                   block
+                    />
+                </div>
+            </div>
+        </template>
+    </p-button-modal>
+</template>
 
 <style lang="postcss" scoped>
 .content-wrapper {
