378b1c53c1e66a5528d726bb026fc662321bd4af
Wanjin Noh <wanjin@megazone.com>
2023-12-05 17:36:22 +0900

    refactor: add NoticeDetail component and notice-detail-store to reuse in pages
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

---
+++ apps/web/src/services/info/stores/notice-detail-store.ts
@@ -0,0 +1,58 @@
+import { reactive } from 'vue';
+
+import { defineStore } from 'pinia';
+
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+
+import type { PostDeleteParameters } from '@/schema/board/post/api-verbs/delete';
+import type { PostGetParameters } from '@/schema/board/post/api-verbs/get';
+import type { PostModel } from '@/schema/board/post/model';
+
+import { getNoticeBoardId } from '@/lib/helper/notice-helper';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+
+export const useNoticeDetailStore = defineStore('notice-detail', () => {
+    const state = reactive({
+        boardId: undefined as undefined | string,
+        post: undefined as undefined | PostModel,
+    });
+
+    const setBoardId = async (boardId?: string) => {
+        if (boardId && boardId === state.boardId) return;
+        state.boardId = await getNoticeBoardId();
+    };
+    const actions = {
+        getNoticePost: async (postId: string, boardId?: string) => {
+            try {
+                await setBoardId(boardId);
+                if (!state.boardId) throw new Error('Notice board not found');
+
+                const result = await SpaceConnector.clientV2.board.post.get<PostGetParameters, PostModel>({
+                    board_id: state.boardId,
+                    post_id: postId,
+                });
+                state.post = result;
+            } catch (e) {
+                ErrorHandler.handleError(e);
+            }
+        },
+        deleteNoticePost: async (postId: string) => {
+            if (!state.boardId) throw new Error('Notice board not found');
+            await SpaceConnector.clientV2.board.post.delete<PostDeleteParameters>({
+                post_id: postId,
+                board_id: state.boardId,
+            });
+            state.post = undefined;
+        },
+        reset: () => {
+            state.boardId = undefined;
+            state.post = undefined;
+        },
+    };
+
+    return {
+        state,
+        ...actions,
+    };
+});
