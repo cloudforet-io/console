9a5faa5ccef50cac76d4d7e6e2b5a3dd8b9d2195
Piggy Park (박용태) <samuel.park@megazone.com>
2023-12-06 11:03:41 +0900

    fix(admin-mode): create admin route mode (#2399)
    * fix(admin-mode): create admin route mode
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * fix: use appContextStore by finding router's app instead of use-store
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * fix: not difficult
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    ---------
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/router/constant.ts
+++ apps/web/src/router/constant.ts
@@ -1,6 +1,13 @@
+
 export const ERROR_ROUTE = Object.freeze({
     _NAME: 'error',
 });
 export const ROOT_ROUTE = Object.freeze({
     _NAME: 'root',
+    WORKSPACE: {
+        _NAME: 'workspace',
+    },
+    ADMIN: {
+        _NAME: 'admin',
+    },
 });
