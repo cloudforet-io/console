b4bab4a9d278f3e8dbc5f9ccee935f89dc2bd94d
yuda <yuda@megazone.com>
2023-12-07 15:37:11 +0900

    refactor: change to setup script
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/cost-explorer/components/BudgetCreateTargetSelect.vue
+++ apps/web/src/services/cost-explorer/components/BudgetCreateTargetSelect.vue
@@ -16,10 +16,8 @@
     </p-field-group>
 </template>
 
-<script lang="ts">
-
-import type { SetupContext } from 'vue';
-import { defineComponent, watch } from 'vue';
+<script lang="ts" setup>
+import { watch } from 'vue';
 
 import { PFieldGroup } from '@spaceone/design-system';
 import { debounce } from 'lodash';
@@ -29,61 +27,35 @@ import { i18n } from '@/translations';
 import { useFormValidator } from '@/common/composables/form-validator';
 import ProjectSelectDropdown from '@/common/modules/project/ProjectSelectDropdown.vue';
 
+
 interface Props {
     targets: string[];
     multiSelectable?: boolean;
     disableValidation?: boolean;
 }
-export default defineComponent<Props>({
-    name: 'BudgetCreateTargetSelect',
-    components: {
-        PFieldGroup,
-        ProjectSelectDropdown,
-    },
-    props: {
-        targets: {
-            type: Array,
-            default: () => [],
-        },
-        multiSelectable: {
-            type: Boolean,
-            default: false,
-        },
-        disableValidation: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    setup(props, { emit }: SetupContext) {
-        const {
-            forms: {
-                selectedTargets,
-            },
-            setForm,
-            invalidState,
-            invalidTexts,
-            isAllValid,
-            validate,
-        } = useFormValidator({
-            selectedTargets: [] as string[],
-        }, {
-            selectedTargets(value: string[]) { return value.length ? '' : i18n.t('BILLING.COST_MANAGEMENT.BUDGET.FORM.BASE_INFO.REQUIRED_TARGET'); },
-        });
-
-        watch([() => selectedTargets.value, () => isAllValid.value], debounce(([targets, isValid]) => {
-            const target: string[]|string = props.multiSelectable ? targets : targets[0];
-            emit('update', target, isValid);
-        }, 300) as any, { immediate: true });
-
-        return {
-            selectedTargets,
-            invalidState,
-            invalidTexts,
-            setForm,
-            validate,
-        };
-    },
+const props = withDefaults(defineProps<Props>(), {
+    targets: () => [],
 });
+const emit = defineEmits<{(e: 'update', target: string|string[], isValid: boolean): void; }>();
+const {
+    forms: {
+        selectedTargets,
+    },
+    setForm,
+    invalidState,
+    invalidTexts,
+    isAllValid,
+    validate,
+} = useFormValidator({
+    selectedTargets: [] as string[],
+}, {
+    selectedTargets(value: string[]) { return value.length ? '' : i18n.t('BILLING.COST_MANAGEMENT.BUDGET.FORM.BASE_INFO.REQUIRED_TARGET'); },
+});
+
+watch([() => selectedTargets.value, () => isAllValid.value], debounce(([targets, isValid]) => {
+    const target: string[]|string = props.multiSelectable ? targets : targets[0];
+    emit('update', target, isValid);
+}, 300) as any, { immediate: true });
 </script>
 
 <style lang="postcss" scoped>
