e6220f939eff26d5cb0d36e66cd79f7f9bfab522
NaYeong,Kim <nayeongkim@megazone.com>
2023-12-11 16:18:52 +0900

    feat: add password form at invitation modal (#2434)
    * feat: add translation for invitation
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * refactor: minor changed
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * feat: add password form at invitation modal
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

---
+++ apps/web/src/services/administration/components/UserManagementAddPassword.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import type { TranslateResult } from 'vue-i18n';
+
+import {
+    PTextInput, PFieldGroup, PToggleButton, PDivider, PFieldTitle, PButton, PCopyButton,
+} from '@spaceone/design-system';
+
+import { generatePassword } from '@/services/administration/helpers/generate-helper';
+
+const emit = defineEmits<{(e: 'change-input', formState): void}>();
+
+const state = reactive({
+    passwordStatus: false,
+    isGenerate: false,
+    copyButtonVisible: true,
+});
+const formState = reactive({
+    password: '',
+});
+const validationState = reactive({
+    isPasswordValid: undefined as undefined | boolean,
+    passwordInvalidText: '' as TranslateResult | string,
+});
+
+/* Components */
+const handleChangeToggleButton = () => {
+    state.passwordStatus = !state.passwordStatus;
+};
+const handleChangeInput = (value) => {
+    formState.password = value;
+    emit('change-input', {
+        password: formState.password,
+    });
+};
+const handleClickGenerate = () => {
+    formState.password = generatePassword();
+};
+</script>
+
+<template>
+    <div class="user-management-add-password">
+        <div class="title-wrapper">
+            <p-field-title :label="$t('IDENTITY.USER.FORM.PASSWORD_SEND_LINK')" />
+            <p-toggle-button v-model="state.passwordStatus"
+                             @change-toggle="handleChangeToggleButton"
+            />
+        </div>
+        <div v-if="state.passwordStatus"
+             class="password-form-view"
+        >
+            <p-divider />
+            <p-field-group
+                :label="$t('IDENTITY.USER.FORM.PASSWORD')"
+                :required="true"
+                :invalid="validationState.isPasswordValid"
+                :invalid-text="validationState.passwordInvalidText"
+                class="password-form-wrapper"
+            >
+                <div class="password-form">
+                    <p-button style-type="tertiary"
+                              @click="handleClickGenerate"
+                    >
+                        {{ $t('IDENTITY.USER.FORM.GENERATE') }}
+                    </p-button>
+                    <p-text-input :value="formState.password"
+                                  class="password-input"
+                                  :placeholder="$t('IDENTITY.USER.FORM.GENERATE_PLACEHOLDER')"
+                                  :invalid="validationState.isPasswordValid"
+                                  @update:value="handleChangeInput"
+                                  @focusin="() => state.copyButtonVisible = false"
+                                  @focusout="() => state.copyButtonVisible = true"
+                    >
+                        <template #right-extra>
+                            <p-copy-button v-if="state.copyButtonVisible && formState.password !== ''"
+                                           class="icon"
+                                           :value="formState.password"
+                            />
+                        </template>
+                    </p-text-input>
+                </div>
+            </p-field-group>
+        </div>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+.user-management-add-password {
+    @apply flex flex-col bg-white border border-primary-3 rounded-md;
+    padding: 0.75rem;
+    gap: 0.75rem;
+    .title-wrapper {
+        @apply flex justify-between;
+    }
+    .password-form-view {
+        @apply flex flex-col;
+        gap: 0.75rem;
+        .password-form-wrapper {
+            margin-bottom: 0;
+            .password-form {
+                @apply flex;
+                gap: 0.5rem;
+                .password-input {
+                    width: 100%;
+                }
+            }
+        }
+    }
+}
+
+</style>
