e2ce077f6fcf79a8ae12c7ff0021963e0bd94ed3
Yongtae Park <samuel.park@megazone.com>
2023-12-15 00:37:34 +0900

    fix(token-grant): create token.grant types
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/schema/identity/user/type.ts
+++ apps/web/src/schema/identity/user/type.ts
@@ -4,4 +4,3 @@ export type AuthType = 'EXTERNAL' | 'LOCAL';
 export type Action = 'UPDATE_PASSWORD';
 export type UserMfaState = 'ENABLED' | 'DISABLED';
 export type UserMfaType = 'EMAIL';
-export type GrantType = 'PASSWORD'|'CLIENT_CREDENTIALS'|'AUTHORIZATION_CODE'|'REFRESH_TOKEN';
