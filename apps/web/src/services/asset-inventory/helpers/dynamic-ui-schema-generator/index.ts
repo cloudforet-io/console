bf958932f37bae81eb5a21f874d5f0e92da3b666
sulmo <50662170+sulmoJ@users.noreply.github.com>
2023-12-19 15:22:03 +0900

    feat: apply userConfig api to customFieldModal (#2523)
    * feat: apply userConfig api to customFieldModal
    
    Signed-off-by: sulmo <sulmo@megazone.com>
    
    * fix: fix minor thing
    
    Signed-off-by: sulmo <sulmo@megazone.com>
    
    ---------
    
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/services/asset-inventory/helpers/dynamic-ui-schema-generator/index.ts
+++ apps/web/src/services/asset-inventory/helpers/dynamic-ui-schema-generator/index.ts
@@ -4,28 +4,55 @@ import type { JsonSchema } from '@spaceone/design-system/types/inputs/forms/json
 
 import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 
+import type { ListResponse } from '@/schema/_common/api-verbs/list';
+import type { CreateUserConfigParameters } from '@/schema/config/user-config/api-verbs/create';
 import type { GetUserConfigParameters } from '@/schema/config/user-config/api-verbs/get';
+import type { UpdateUserConfigParameters } from '@/schema/config/user-config/api-verbs/update';
 import type { UserConfigModel } from '@/schema/config/user-config/model';
-import type { SchemaType } from '@/schema/identity/schema/type';
+import type { SchemaListParameters } from '@/schema/identity/schema/api-verbs/list';
+import type { SchemaModel } from '@/schema/identity/schema/model';
 
 import ErrorHandler from '@/common/composables/error/errorHandler';
 
 import { getDefaultDetailSchema, getDefaultSearchSchema, getDefaultTableSchema } from '@/services/asset-inventory/helpers/dynamic-ui-schema-generator/dynamic-layout-schema-template';
-import type { GetSchemaParams } from '@/services/asset-inventory/helpers/dynamic-ui-schema-generator/type';
+import type { GetSchemaParams, ResourceType } from '@/services/asset-inventory/helpers/dynamic-ui-schema-generator/type';
 
+const getCustomTableSchemaKey = (userData:{userType:string, userId: string}, resourceType:ResourceType, provider:string) => {
+    const { userType, userId } = userData;
+    return `console:${userType}:${userId}:page-schema:${resourceType}?provider=${provider}:table`;
+};
 
-const getCustomSchema = async ({ userType, userId }, provider, schemaType?:SchemaType):Promise<DynamicLayout | undefined> => {
-    const userConfigName = `console:${userType}:${userId}:page-schema:${schemaType}?provider=${provider}:table`;
+export const getCustomTableSchema = async (userData:{userType:string, userId: string}, resourceType:ResourceType, provider:string):Promise<DynamicLayout | undefined> => {
     let userConfig:UserConfigModel<DynamicLayout>|undefined;
     try {
         userConfig = await SpaceConnector.clientV2.config.userConfig.get<GetUserConfigParameters, UserConfigModel<DynamicLayout>>({
-            name: userConfigName,
+            name: getCustomTableSchemaKey(userData, resourceType, provider),
         });
     } catch (e) {
         ErrorHandler.handleError(e);
-        userConfig = undefined;
+        return undefined;
     }
     return Object.keys(userConfig?.data ?? {}).length ? userConfig?.data : undefined;
+};
+
+export const updateCustomTableSchema = async (userData:{userType:string, userId: string}, resourceType:ResourceType, provider:string, data:any) => {
+    const client = SpaceConnector.clientV2.config;
+    const customSchemaData = await getCustomTableSchema(userData, resourceType, provider);
+    try {
+        if (customSchemaData) {
+            await client.userConfig.update<UpdateUserConfigParameters, UserConfigModel>({
+                name: getCustomTableSchemaKey(userData, resourceType, provider),
+                data,
+            });
+        } else {
+            await client.userConfig.create<CreateUserConfigParameters, UserConfigModel>({
+                name: getCustomTableSchemaKey(userData, resourceType, provider),
+                data,
+            });
+        }
+    } catch (e) {
+        ErrorHandler.handleError(e);
+    }
 };
 
 
@@ -35,15 +62,35 @@ const getAccountFields = (accountSchema) => Object.entries<JsonSchema>(accountSc
     type: 'text',
 }));
 
-export const getTableSchema = async ({ accountSchema, isTrustedAccount, userConfig }: GetSchemaParams) => {
+const getAccountSchema = async ({ options, resourceType }: Pick<GetSchemaParams, 'options'|'resourceType'>) => {
+    const isTrustedAccount = resourceType === 'identity.TrustedAccount';
+    try {
+        const res = await SpaceConnector.clientV2.identity.schema.list<SchemaListParameters, ListResponse<SchemaModel>>({
+            provider: options?.provider,
+        });
+        if (res?.results?.length) {
+            return res.results.find((schema) => schema.schema_type === (isTrustedAccount ? 'TRUSTED_ACCOUNT' : 'SERVICE_ACCOUNT'));
+        } return undefined;
+    } catch (e) {
+        ErrorHandler.handleError(e);
+        return undefined;
+    }
+};
+
+export const getServiceAccountTableSchema = async ({
+    userData, resourceType, options,
+}: GetSchemaParams) => {
+    const isTrustedAccount = resourceType === 'identity.TrustedAccount';
+    const accountSchema = await getAccountSchema({ options, resourceType });
+    if (!accountSchema) {
+        ErrorHandler.handleError(new Error('Service Account schema not found'));
+        return undefined;
+    }
     const fields:DynamicField[] = getAccountFields(accountSchema);
     let schemaData = getDefaultTableSchema(fields, isTrustedAccount);
 
-    const customSchemaData = await getCustomSchema(userConfig, accountSchema?.provider, accountSchema?.schema_type);
+    const customSchemaData = accountSchema?.provider ? await getCustomTableSchema(userData, resourceType, accountSchema?.provider) : undefined;
     if (customSchemaData) schemaData = customSchemaData;
-    else if (schemaData.options) {
-        schemaData.options.fields = (schemaData.options.fields.filter((d) => !d?.options?.is_optional)) ?? [];
-    }
 
     const searchSchemaData = getDefaultSearchSchema(fields, isTrustedAccount);
     if (schemaData.options) schemaData.options.search = searchSchemaData.search;
@@ -51,7 +98,9 @@ export const getTableSchema = async ({ accountSchema, isTrustedAccount, userConf
     return schemaData;
 };
 
-export const getDetailSchema = ({ accountSchema, isTrustedAccount }: Pick<GetSchemaParams, 'accountSchema'|'isTrustedAccount' >): { details: Partial<DynamicLayout>[] } => {
+export const getDetailSchema = async ({ resourceType, options }: Pick<GetSchemaParams, 'resourceType'|'options' >): Promise<{ details: Partial<DynamicLayout>[] }> => {
+    const isTrustedAccount = resourceType === 'identity.TrustedAccount';
+    const accountSchema = await getAccountSchema({ options, resourceType });
     const fields:DynamicField[] = getAccountFields(accountSchema);
     return getDefaultDetailSchema(fields, isTrustedAccount);
 };
