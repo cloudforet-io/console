8515f0a0cb8dd040828edb69b74bbdc869dec135
NaYeong,Kim <nayeongkim@megazone.com>
2023-12-20 10:48:06 +0900

    feat: create ui for add user form (#2531)
    * feat: add translation for add user form
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>
    
    * feat: create ui for add user form
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/services/administration/components/UserManagementAddUser.vue
+++ apps/web/src/services/administration/components/UserManagementAddUser.vue
@@ -9,11 +9,13 @@ import {
     PContextMenu, PEmpty, PFieldGroup, PIconButton, PTextInput, PSelectDropdown,
 } from '@spaceone/design-system';
 import type { MenuItem } from '@spaceone/design-system/src/inputs/context-menu/type';
-import { debounce, isEmpty } from 'lodash';
+import { debounce } from 'lodash';
 
 import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 
 import type { ListResponse } from '@/schema/_common/api-verbs/list';
+import type { UserGetParameters } from '@/schema/identity/user/api-verbs/get';
+import type { UserModel } from '@/schema/identity/user/model';
 import type { AuthType } from '@/schema/identity/user/type';
 import type { FindWorkspaceUserParameters } from '@/schema/identity/workspace-user/api-verbs/find';
 import type { WorkspaceUserGetParameters } from '@/schema/identity/workspace-user/api-verbs/get';
@@ -24,13 +26,24 @@ import { i18n } from '@/translations';
 import ErrorHandler from '@/common/composables/error/errorHandler';
 
 import type { AddModalMenuItem } from '@/services/administration/components/UserManagementAddModal.vue';
+import { USER_MODAL_TYPE } from '@/services/administration/constants/user-constant';
 import { checkEmailFormat } from '@/services/administration/helpers/user-management-form-validations';
+import { useUserPageStore } from '@/services/administration/store/user-page-store';
+
+const userPageStore = useUserPageStore();
+const userPageState = userPageStore.$state;
 
 const containerRef = ref<HTMLElement|null>(null);
 const contextMenuRef = ref<any|null>(null);
 const targetRef = ref<HTMLElement | null>(null);
 
 const emit = defineEmits<{(e: 'change-list', selectedItems: MenuItem[]): void}>();
+
+const contextMenuItems = [
+    { label: 'Google', name: 'GOOGLE', key: 'EXTERNAl' },
+    { label: 'Keycloak', name: 'KEYCLOAK', key: 'EXTERNAl' },
+    { label: 'Local', name: 'LOCAL', key: 'LOCAL' },
+];
 
 const storeState = reactive({
     userAuthType: computed(() => store.state.user.authType),
@@ -51,6 +64,9 @@ const state = reactive({
             { label: 'external', name: 'EXTERNAL' },
         ];
     }),
+});
+const dropdownState = reactive({
+    selectedMenuItem: contextMenuItems[0].name,
 });
 const formState = reactive({
     searchText: '',
@@ -75,15 +91,15 @@ const handleChangeTextInput = debounce(async (searchText: string) => {
     validationState.userIdInvalidText = '';
     await fetchListUsers();
 }, 200);
-const handleEnterTextInput = async () => {
+const handleEnterTextInput = () => {
     if (formState.searchText === '') return;
     const { isValid, invalidText } = checkEmailFormat(formState.searchText);
     if (!isValid) {
         validationState.userIdInvalid = true;
         validationState.userIdInvalidText = invalidText;
-        await hideMenu();
+        hideMenu();
     } else {
-        await fetchGetWorkspaceUsers(formState.searchText);
+        getUserList();
     }
 };
 const handleSelectMenuItem = async (menuItem: AddModalMenuItem) => {
@@ -95,6 +111,39 @@ const handleClickDeleteButton = (idx: number) => {
 };
 const handleSelectAuthTypeItem = (idx: number) => {
     state.selectedItems[idx].auth_type = state.selectedAuthType;
+};
+const handleSelectDropdownItem = (selected: string) => {
+    dropdownState.selectedMenuItem = selected;
+};
+const getUserList = async () => {
+    try {
+        if (userPageState.isAdminMode) {
+            await fetchGetUsers(formState.searchText);
+        } else {
+            await fetchGetWorkspaceUsers(formState.searchText);
+        }
+    } catch (e) {
+        state.selectedItems.push({
+            user_id: formState.searchText,
+            label: formState.searchText,
+            name: formState.searchText,
+            isNew: true,
+            auth_type: userPageState.isAdminMode ? dropdownState.selectedMenuItem : state.authTypeMenuItems[0].name as AuthType,
+        });
+        formState.searchText = '';
+    }
+};
+const checkValidation = () => {
+    if (formState.searchText === '') {
+        validationState.userIdInvalid = true;
+        validationState.userIdInvalidText = i18n.t('IAM.USER.FORM.ALT_E_INVALID_FULL_NAME');
+        return;
+    }
+    const { isValid, invalidText } = checkEmailFormat(formState.searchText);
+    if (!isValid) {
+        validationState.userIdInvalid = true;
+        validationState.userIdInvalidText = invalidText;
+    }
 };
 
 /* API */
@@ -118,22 +167,19 @@ const fetchListUsers = async () => {
     }
 };
 const fetchGetWorkspaceUsers = async (userId: string) => {
-    const response = await SpaceConnector.clientV2.identity.workspaceUser.get<WorkspaceUserGetParameters, WorkspaceUserModel>({
+    await SpaceConnector.clientV2.identity.workspaceUser.get<WorkspaceUserGetParameters, WorkspaceUserModel>({
         user_id: userId,
     });
-    if (isEmpty(response)) {
-        state.selectedItems.push({
-            user_id: userId,
-            label: userId,
-            name: userId,
-            isNew: true,
-            auth_type: state.authTypeMenuItems[0].name as AuthType,
-        });
-        formState.searchText = '';
-    } else {
-        validationState.userIdInvalid = true;
-        validationState.userIdInvalidText = i18n.t('IDENTITY.USER.FORM.USER_ID_INVALID_WORKSPACE', { userId });
-    }
+    validationState.userIdInvalid = true;
+    validationState.userIdInvalidText = i18n.t('IAM.USER.FORM.USER_ID_INVALID_WORKSPACE', { userId });
+    await hideMenu();
+};
+const fetchGetUsers = async (userId: string) => {
+    await SpaceConnector.clientV2.identity.user.get<UserGetParameters, UserModel>({
+        user_id: userId,
+    });
+    validationState.userIdInvalid = true;
+    validationState.userIdInvalidText = i18n.t('IAM.USER.FORM.USER_ID_INVALID_DOMAIN', { userId });
     await hideMenu();
 };
 
@@ -146,6 +192,7 @@ watch(() => state.menuVisible, async (menuVisible) => {
         state.selectedAuthType = state.authTypeMenuItems[0].name as AuthType;
         await fetchListUsers();
     } else {
+        await checkValidation();
         state.menuItems = [];
         emit('change-list', state.selectedItems);
     }
@@ -154,32 +201,51 @@ watch(() => state.menuVisible, async (menuVisible) => {
 
 <template>
     <div class="user-info-wrapper">
-        <p-field-group :label="$t('IAM.USER.FORM.USER_ID')"
-                       required
+        <p-field-group required
                        :invalid="validationState.userIdInvalid"
                        :invalid-text="validationState.userIdInvalidText"
+                       :class="{'is-admin-mode': userPageState.isAdminMode}"
         >
+            <template #label>
+                <strong v-if="userPageState.modal.type === USER_MODAL_TYPE.INVITE">
+                    {{ $t('IAM.USER.FORM.USER_ID') }}
+                </strong>
+                <span v-else>
+                    <span>{{ $t('IAM.USER.FORM.AUTH_TYPE') }}</span>
+                    <span class="and-mark">&</span>
+                    <span>{{ $t('IAM.USER.FORM.USER_ID') }}</span>
+                </span>
+            </template>
             <template #default="{invalid}">
                 <div ref="containerRef"
-                     class="input-form-wrapper"
+                     class="input-wrapper"
                 >
-                    <p-text-input ref="targetRef"
-                                  :value="formState.searchText"
-                                  :invalid="invalid"
-                                  class="user-id-input"
-                                  @click="handleClickTextInput"
-                                  @keyup.enter="handleEnterTextInput"
-                                  @update:value="handleChangeTextInput"
-                    />
-                    <p-context-menu v-if="state.menuVisible && state.menuItems.length > 0"
-                                    ref="contextMenuRef"
-                                    :loading="state.loading"
-                                    :menu="state.menuItems"
-                                    :selected="state.selectedItems"
-                                    multi-selectable
-                                    class="user-context-menu"
-                                    @select="handleSelectMenuItem"
-                    />
+                    <div v-if="userPageState.isAdminMode">
+                        <p-select-dropdown :menu="contextMenuItems"
+                                           :selected="dropdownState.selectedMenuItem"
+                                           class="type-dropdown"
+                                           @update:selected="handleSelectDropdownItem"
+                        />
+                    </div>
+                    <div class="input-form-wrapper">
+                        <p-text-input ref="targetRef"
+                                      :value="formState.searchText"
+                                      :invalid="invalid"
+                                      class="user-id-input"
+                                      @click="handleClickTextInput"
+                                      @keyup.enter="handleEnterTextInput"
+                                      @update:value="handleChangeTextInput"
+                        />
+                        <p-context-menu v-if="state.menuVisible && state.menuItems.length > 0"
+                                        ref="contextMenuRef"
+                                        :loading="state.loading"
+                                        :menu="state.menuItems"
+                                        :selected="state.selectedItems"
+                                        multi-selectable
+                                        class="user-context-menu"
+                                        @select="handleSelectMenuItem"
+                        />
+                    </div>
                 </div>
             </template>
         </p-field-group>
@@ -205,8 +271,8 @@ watch(() => state.menuVisible, async (menuVisible) => {
                 </div>
                 <div class="selected-toolbox">
                     <p-select-dropdown v-if="item.isNew"
-                                       :selected.sync="state.selectedAuthType"
-                                       :menu="state.authTypeMenuItems"
+                                       :selected="item.auth_type"
+                                       :menu="contextMenuItems"
                                        class="auth-type-dropdown"
                                        style-type="transparent"
                                        @select="handleSelectAuthTypeItem(idx)"
@@ -228,17 +294,28 @@ watch(() => state.menuVisible, async (menuVisible) => {
     @apply flex flex-col bg-white border border-primary-3 rounded-md;
     height: 18rem;
     padding: 0.75rem;
-    .input-form-wrapper {
-        @apply relative;
-        .user-id-input {
+    .and-mark {
+        @apply font-normal;
+        margin-right: 0.25rem;
+        margin-left: 0.25rem;
+    }
+    .input-wrapper {
+        @apply flex;
+        margin-top: 0.25rem;
+        gap: 0.25rem;
+        .input-form-wrapper {
+            @apply relative;
             width: 100%;
-        }
-        .user-context-menu {
-            @apply absolute;
-            top: 2rem;
-            left: 0;
-            max-height: 13.5rem;
-            z-index: 10;
+            .user-id-input {
+                width: 100%;
+            }
+            .user-context-menu {
+                @apply absolute;
+                top: 2rem;
+                left: 0;
+                max-height: 13.5rem;
+                z-index: 10;
+            }
         }
     }
     .empty-wrapper {
@@ -266,6 +343,21 @@ watch(() => state.menuVisible, async (menuVisible) => {
             .selected-toolbox {
                 @apply flex items-center;
             }
+        }
+    }
+}
+
+/* custom design-system component - p-button */
+:deep(.p-field-group) {
+    @apply relative;
+    .invalid-feedback {
+        @apply absolute;
+        bottom: -1.125rem;
+        left: 0;
+    }
+    &.is-admin-mode {
+        .invalid-feedback {
+            left: 6.75rem;
         }
     }
 }
