8e908ff252150a3b389e5fa849f26113134ce430
Wanjin Noh <wanjin@megazone.com>
2023-12-20 16:23:17 +0900

    feat: update some fields in alert detail, alert table
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/alert-manager/components/AlertDetailTabsDetails.vue
+++ apps/web/src/services/alert-manager/components/AlertDetailTabsDetails.vue
@@ -8,17 +8,6 @@
                             :skeleton-rows="7"
                             block
         >
-            <template #data-created_at>
-                {{ iso8601Formatter(data.created_at, timezone) }}
-            </template>
-            <template #data-acknowledged_at>
-                <span v-if="data.acknowledged_at"> {{ iso8601Formatter(data.acknowledged_at, timezone) }}</span>
-                <span v-else>--</span>
-            </template>
-            <template #data-resolved_at>
-                <span v-if="data.resolved_at"> {{ iso8601Formatter(data.resolved_at, timezone) }}</span>
-                <span v-else>--</span>
-            </template>
             <template #data-rule="{value}">
                 <span v-if="Object.keys(value).length === 0">
                     --
@@ -73,9 +62,6 @@ export default {
                 { name: 'resource.name', label: i18n.t('MONITORING.ALERT.DETAIL.DETAILS.RESOURCE_NAME') },
                 { name: 'resource.resource_id', label: i18n.t('MONITORING.ALERT.DETAIL.DETAILS.RESOURCE_ID') },
                 { name: 'resource.resource_type', label: i18n.t('MONITORING.ALERT.DETAIL.DETAILS.RESOURCE_TYPE') },
-                { name: 'created_at', label: i18n.t('MONITORING.ALERT.DETAIL.INFO.CREATED'), disableCopy: true },
-                { name: 'acknowledged_at', label: i18n.t('MONITORING.ALERT.DETAIL.INFO.ACKNOWLEDGED'), disableCopy: true },
-                { name: 'resolved_at', label: i18n.t('MONITORING.ALERT.DETAIL.INFO.RESOLVED'), disableCopy: true },
             ]),
             data: computed(() => alertPageState.alertData ?? {}),
             escalationPolicyName: '',
