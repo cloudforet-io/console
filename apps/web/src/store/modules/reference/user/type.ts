3b4b3d21a74ab2630e8440e6a489576118967759
yuda <yuda@megazone.com>
2023-12-21 23:03:13 +0900

    feat: delete vuex reference store (project, projectGroup, user)
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/store/modules/reference/user/type.ts
+++
@@ -1,9 +0,0 @@
-import type { ReferenceItem, ReferenceMap } from '@/store/modules/reference/type';
-
-export type UserReferenceItem = Required<Pick<ReferenceItem<undefined>, 'key'|'label'|'name'>>;
-
-export type UserReferenceMap = ReferenceMap<UserReferenceItem>;
-
-export interface UserReferenceState {
-    items?: UserReferenceMap;
-}
