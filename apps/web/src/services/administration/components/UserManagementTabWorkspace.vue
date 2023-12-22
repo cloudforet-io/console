187750c8920bed9c82990734d865d7d41f7c3f9c
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2023-12-23 02:38:54 +0900

    feat: apply changes and deletions to workspace role permissions
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/services/administration/components/UserManagementTabWorkspace.vue
+++ apps/web/src/services/administration/components/UserManagementTabWorkspace.vue
@@ -3,8 +3,13 @@ import { computed, reactive, watch } from 'vue';
 import { useRouter } from 'vue-router/composables';
 
 import {
-    PDataTable, PHeading, PI,
+    PButton,
+    PDataTable, PHeading, PI, PSelectDropdown,
 } from '@spaceone/design-system';
+import type {
+    AutocompleteHandler,
+    SelectDropdownMenuItem,
+} from '@spaceone/design-system/types/inputs/dropdown/select-dropdown/type';
 
 import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 import { ApiQueryHelper } from '@cloudforet/core-lib/space-connector/helper';
@@ -14,22 +19,31 @@ import type { ListResponse } from '@/schema/_common/api-verbs/list';
 import { RESOURCE_GROUP } from '@/schema/_common/constant';
 import type { TimeStamp } from '@/schema/_common/model';
 import type { RoleBindingListParameters } from '@/schema/identity/role-binding/api-verbs/list';
+import type { RoleUpdateParameters } from '@/schema/identity/role-binding/api-verbs/update';
 import type { RoleBindingModel } from '@/schema/identity/role-binding/model';
+import { ROLE_TYPE } from '@/schema/identity/role/constant';
 import type { WorkspaceListParameters } from '@/schema/identity/workspace/api-verbs/list';
 import type { WorkspaceModel } from '@/schema/identity/workspace/model';
 import { store } from '@/store';
 import { i18n } from '@/translations';
 
+import { showErrorMessage, showSuccessMessage } from '@/lib/helper/notice-alert-helper';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+
 import { useRoleFormatter } from '@/services/administration/composables/refined-table-data';
 import { useUserPageStore } from '@/services/administration/store/user-page-store';
 import { HOME_DASHBOARD_ROUTE } from '@/services/home-dashboard/routes/route-constant';
 
+interface WorkspaceItem {
+    name: string;
+    id: string;
+}
 interface TableItem {
-    project_id?: string;
-    name?: string;
-    date?: TimeStamp;
-    key?: string;
-    value?: string;
+    workspace: WorkspaceItem;
+    role_type: string;
+    created_at: TimeStamp;
+    role_binding_id: string;
 }
 interface Props {
     activeTab: string;
@@ -56,11 +70,22 @@ const state = reactive({
 });
 const tableState = reactive({
     fields: computed(() => [
-        { name: 'workspace_id', label: i18n.t('IAM.USER.MAIN.WORKSPACE') as string },
-        { name: 'role_type', label: i18n.t('IAM.USER.MAIN.ROLE_TYPE') as string },
-        { name: 'created_at', label: i18n.t('IAM.USER.MAIN.INVITED') as string },
+        { name: 'workspace', label: i18n.t('IAM.USER.MAIN.WORKSPACE') as string },
+        { name: 'role_type', label: i18n.t('IAM.USER.MAIN.ROLE_TYPE') as string, sortable: false },
+        { name: 'created_at', label: i18n.t('IAM.USER.MAIN.INVITED') as string, sortable: false },
+        { name: 'remove_button', label: ' ', sortable: false },
     ]),
 });
+const dropdownState = reactive({
+    loading: false,
+    visibleMenu: false,
+    selectedMenuIndex: undefined as undefined | number,
+    searchText: '',
+    selectedItems: [] as SelectDropdownMenuItem[],
+    menuItems: [] as SelectDropdownMenuItem[],
+});
+
+/* Component */
 const handleChangeSort = (sortBy, sortDesc) => {
     state.sortBy = sortBy;
     state.sortDesc = sortDesc;
@@ -69,8 +94,13 @@ const handleChangeSort = (sortBy, sortDesc) => {
 const handleClickLink = (id: string) => {
     router.push({ name: HOME_DASHBOARD_ROUTE._NAME, params: { workspaceId: id } });
 };
+const handleMenuVisible = (idx: number) => {
+    if (!dropdownState.visibleMenu) return;
+    dropdownState.selectedMenuIndex = idx;
+};
 
 /* API */
+const roleListApiQueryHelper = new ApiQueryHelper();
 const workspaceApiHelper = new ApiQueryHelper()
     .setPage(1, 15);
 const getWorkspaceList = async () => {
@@ -90,17 +120,76 @@ const getWorkspaceList = async () => {
             return;
         }
         state.items = (results ?? []).map((k) => ({
-            workspace_id: {
-                name: workspaceResults?.find((w) => w.workspace_id === k.workspace_id)?.name,
+            workspace: {
+                name: workspaceResults?.find((w) => w.workspace_id === k.workspace_id)?.name || '',
                 id: k.workspace_id,
             },
             role_type: k.role_type,
             created_at: k.created_at,
+            role_binding_id: k.role_binding_id,
         }));
     } catch (e) {
         state.items = [];
     } finally {
         state.loading = false;
+    }
+};
+const dropdownMenuHandler: AutocompleteHandler = async (inputText: string) => {
+    dropdownState.loading = true;
+
+    roleListApiQueryHelper.setFilters([{
+        k: 'role_type',
+        v: [ROLE_TYPE.WORKSPACE_OWNER, ROLE_TYPE.WORKSPACE_MEMBER],
+        o: '=',
+    }]);
+    if (inputText) {
+        roleListApiQueryHelper.addFilter({
+            k: 'name',
+            v: inputText,
+            o: '',
+        });
+    }
+    try {
+        const results = await userPageStore.listRoles({
+            query: roleListApiQueryHelper.data,
+        });
+        dropdownState.menuItems = (results ?? []).map((role) => ({
+            label: role.name,
+            name: role.role_id,
+            role_type: role.role_type,
+        }));
+    } catch (e) {
+        ErrorHandler.handleError(e);
+    } finally {
+        dropdownState.loading = false;
+    }
+
+    return {
+        results: dropdownState.menuItems,
+    };
+};
+const handleSelectDropdownItem = async (value, rowIndex) => {
+    try {
+        await SpaceConnector.clientV2.identity.roleBinding.updateRole<RoleUpdateParameters, RoleBindingModel>({
+            role_binding_id: state.items[rowIndex].role_binding_id,
+            role_id: value.name || '',
+        });
+        showSuccessMessage(i18n.t('IAM.USER.MAIN.ALT_S_CHANGE_ROLE'), '');
+        await getWorkspaceList();
+    } catch (e: any) {
+        ErrorHandler.handleRequestError(e, e.message);
+    }
+};
+const handleClickButton = async (value: RoleBindingModel) => {
+    try {
+        await SpaceConnector.clientV2.identity.roleBinding.delete({
+            role_binding_id: value.role_binding_id,
+        });
+        showSuccessMessage(i18n.t('IDENTITY.USER.MAIN.ALT_S_REMOVE_USER'), '');
+        await getWorkspaceList();
+    } catch (e) {
+        showErrorMessage(i18n.t('IDENTITY.USER.MAIN.ALT_E_REMOVE_USER'), '');
+        ErrorHandler.handleError(e);
     }
 };
 
@@ -125,7 +214,7 @@ watch([() => props.activeTab, () => state.selectedUser.user_id], async () => {
                       sortable
                       @changeSort="handleChangeSort"
         >
-            <template #col-workspace_id-format="{value}">
+            <template #col-workspace-format="{value}">
                 <span class="workspace-id-wrapper">
                     <span>{{ value.name }}</span>
                     <router-link :to="{ name: HOME_DASHBOARD_ROUTE._NAME, params: { workspaceId: value.id } }"
@@ -140,17 +229,52 @@ watch([() => props.activeTab, () => state.selectedUser.user_id], async () => {
                     </router-link>
                 </span>
             </template>
-            <template #col-role_type-format="{value}">
+            <template #col-role_type-format="{value, rowIndex}">
                 <span class="role-type">
                     <img :src="useRoleFormatter(value).image"
                          alt="role-type-icon"
                          class="role-type-icon"
                     >
                     <span>{{ useRoleFormatter(value).name }}</span>
+                    <p-select-dropdown is-filterable
+                                       use-fixed-menu-style
+                                       menu-position="right"
+                                       style-type="icon-button"
+                                       :visible-menu="dropdownState.visibleMenu"
+                                       :loading="dropdownState.loading"
+                                       :search-text.sync="dropdownState.searchText"
+                                       :selected="dropdownState.selectedItems"
+                                       :handler="dropdownMenuHandler"
+                                       class="role-select-dropdown"
+                                       @update:visible-menu="handleMenuVisible(rowIndex)"
+                                       @select="handleSelectDropdownItem($event, rowIndex)"
+                    >
+                        <template #menu-item--format="{item}">
+                            <div class="role-menu-item">
+                                <img :src="useRoleFormatter(item.role_type).image"
+                                     alt="role-type-icon"
+                                     class="role-type-icon"
+                                >
+                                <div class="role-info-wrapper">
+                                    <span>{{ item.label }}</span>
+                                    <span class="role-type">({{ item.role_type }})</span>
+                                </div>
+                            </div>
+                        </template>
+                    </p-select-dropdown>
                 </span>
             </template>
             <template #col-created_at-format="{value}">
                 {{ iso8601Formatter(value, storeState.timezone) }}
+            </template>
+            <template #col-remove_button-format="value">
+                <p-button style-type="tertiary"
+                          size="sm"
+                          class="remove-button"
+                          @click="handleClickButton(value.role_binding_info.role_binding_id)"
+                >
+                    {{ $t('IAM.USER.REMOVE') }}
+                </p-button>
             </template>
         </p-data-table>
     </div>
@@ -166,6 +290,24 @@ watch([() => props.activeTab, () => state.selectedUser.user_id], async () => {
             @apply rounded-full;
             width: 1.5rem;
             height: 1.5rem;
+        }
+        .role-select-dropdown {
+            width: auto;
+            .role-menu-item {
+                @apply flex items-center;
+                gap: 0.25rem;
+                .role-type-icon {
+                    width: 1rem;
+                    height: 1rem;
+                }
+                .role-info-wrapper {
+                    @apply flex flex-col;
+                    gap: 0.25rem;
+                    .role-type {
+                        @apply text-gray-500;
+                    }
+                }
+            }
         }
     }
     .workspace-id-wrapper {
