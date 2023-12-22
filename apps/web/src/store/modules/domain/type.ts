abe478bd785f9a4014ff350edf82f6e961280ed1
Dahyun Yu <yuda@megazone.com>
2023-12-22 17:49:47 +0900

    fix: fix domain config name for domain settings (#2581)
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/store/modules/domain/type.ts
+++ apps/web/src/store/modules/domain/type.ts
@@ -25,5 +25,5 @@ export interface ExtraSubMenu {
 
 export const DOMAIN_CONFIG_TYPE = {
     EXTRA_MENU: 'console:ext-menu',
-    SETTINGS: 'console:domain-settings',
+    SETTINGS: 'settings',
 } as const;
