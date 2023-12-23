44bdfaf8dfcdc6125ed2f9500740f9bb1a9a1838
yuda <yuda@megazone.com>
2023-12-23 09:57:10 +0900

    refactor: change to script setup
    Signed-off-by: yuda <yuda@megazone.com>

--- packages/mirinae/src/feedbacks/modals/advanced/double-check-modal/PDoubleCheckModal.vue
+++ packages/mirinae/src/feedbacks/modals/advanced/double-check-modal/PDoubleCheckModal.vue
@@ -1,10 +1,56 @@
+<script lang="ts" setup>
+import { computed, reactive } from 'vue';
+
+import PButtonModal from '@/feedbacks/modals/button-modal/PButtonModal.vue';
+import type { ModalSizeType } from '@/feedbacks/modals/type';
+import { useProxyValue } from '@/hooks';
+import PFieldGroup from '@/inputs/forms/field-group/PFieldGroup.vue';
+import PTextInput from '@/inputs/input/text-input/PTextInput.vue';
+
+
+interface Props {
+    modalSize?: ModalSizeType;
+    visible?: boolean;
+    headerTitle?: string;
+    verificationText?: string;
+    loading?: boolean;
+}
+const props = withDefaults(defineProps<Props>(), {
+    modalSize: 'md',
+    visible: false,
+    headerTitle: undefined,
+    verificationText: '',
+    loading: false,
+});
+const emit = defineEmits<{(e: 'update:visible', value: boolean): void;
+    (e: 'confirm'): void;
+    (e: 'cancel'): void;
+}>();
+
+const state = reactive({
+    proxyVisible: useProxyValue('visible', props, emit),
+    inputText: undefined,
+    invalid: computed(() => props.verificationText !== state.inputText),
+});
+
+const handleClose = () => {
+    state.inputText = undefined;
+    emit('cancel');
+};
+const handleConfirm = () => {
+    if (state.invalid) return;
+    state.inputText = undefined;
+    emit('confirm');
+};
+</script>
+
 <template>
-    <p-button-modal :header-title="headerTitle"
-                    :size="modalSize"
-                    :visible.sync="proxyVisible"
+    <p-button-modal :header-title="props.headerTitle"
+                    :size="props.modalSize"
+                    :visible.sync="state.proxyVisible"
                     theme-color="alert"
-                    :loading="loading"
-                    :disabled="invalid || inputText.length === 0"
+                    :loading="props.loading"
+                    :disabled="state.invalid || state.inputText.length === 0"
                     @cancel="handleClose"
                     @close="handleClose"
                     @confirm="handleConfirm"
@@ -13,20 +59,20 @@
             <div>
                 <p-field-group
                     :required="true"
-                    :invalid-text="$t('COMPONENT.DOUBLE_CHECK_MODAL.INVALID_TEXT', {text: verificationText})"
-                    :invalid="inputText !== undefined && invalid"
+                    :invalid-text="$t('COMPONENT.DOUBLE_CHECK_MODAL.INVALID_TEXT', { text: props.verificationText })"
+                    :invalid="state.inputText !== undefined && state.invalid"
                 >
                     <template #label>
                         <i18n path="COMPONENT.DOUBLE_CHECK_MODAL.INPUT_DESC">
                             <template #text>
-                                <strong>[{{ verificationText }}]</strong>
+                                <strong>[{{ props.verificationText }}]</strong>
                             </template>
                         </i18n>
                     </template>
                     <template #default="{invalid}">
-                        <p-text-input v-model="inputText"
+                        <p-text-input v-model="state.inputText"
                                       :invalid="invalid"
-                                      :disabled="loading"
+                                      :disabled="props.loading"
                                       block
                                       @keyup.enter="handleConfirm()"
                         />
@@ -38,74 +84,3 @@
         </template>
     </p-button-modal>
 </template>
-<script lang="ts">
-import { computed, reactive, toRefs } from 'vue';
-
-import PButtonModal from '@/feedbacks/modals/button-modal/PButtonModal.vue';
-import { SizeMapping } from '@/feedbacks/modals/type';
-import { useProxyValue } from '@/hooks';
-import PFieldGroup from '@/inputs/forms/field-group/PFieldGroup.vue';
-import PTextInput from '@/inputs/input/text-input/PTextInput.vue';
-
-
-export default {
-    name: 'PDoubleCheckModal',
-    components: { PButtonModal, PTextInput, PFieldGroup },
-    props: {
-        modalSize: {
-            type: String,
-            default: 'md',
-            validator: (value) => Object.keys(SizeMapping).includes(value),
-        },
-        visible: { // sync
-            type: Boolean,
-            default: false,
-        },
-        headerTitle: {
-            type: String,
-            default: undefined,
-        },
-        verificationText: {
-            type: String,
-            required: true,
-        },
-        loading: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    setup(props, { emit }) {
-        const state = reactive({
-            proxyVisible: useProxyValue('visible', props, emit),
-            inputText: undefined,
-            invalid: computed(() => props.verificationText !== state.inputText),
-        });
-
-        const handleClose = () => {
-            state.inputText = undefined;
-            emit('cancel');
-        };
-        const handleConfirm = () => {
-            if (state.invalid) return;
-            state.inputText = undefined;
-            emit('confirm');
-        };
-
-        return {
-            ...toRefs(state),
-            handleClose,
-            handleConfirm,
-        };
-    },
-};
-</script>
-
-<style lang="postcss" scoped>
-.p-double-check-modal-sub-title {
-    font-style: normal;
-    font-weight: normal;
-    font-size: 1.5rem;
-    line-height: 1.9rem;
-    margin-bottom: 1.875rem;
-}
-</style>
