68ec3ff8d61ea6e69dd412538fe029126052d2d8
yuda <yuda@megazone.com>
2023-12-24 17:50:18 +0900

    fix: init `lastLoadedTime` when flush reference stores
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/store/reference/workspace-reference-store.ts
+++ apps/web/src/store/reference/workspace-reference-store.ts
@@ -99,10 +99,15 @@ export const useWorkspaceReferenceStore = defineStore('workspace-reference', () 
         };
     };
 
+    const flush = () => {
+        state.items = null;
+        lastLoadedTime = 0;
+    };
+
     const actions = {
         load,
         sync,
-        flush: () => { state.items = null; },
+        flush,
     };
 
     return {
