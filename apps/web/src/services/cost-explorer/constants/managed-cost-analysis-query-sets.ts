122afb32ebcdad0e476c039c7e24a9e41e6062cb
yuda <yuda@megazone.com>
2023-12-24 15:41:39 +0900

    chore: hide 'Monthly cost by workspace' from admin mode
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/cost-explorer/constants/managed-cost-analysis-query-sets.ts
+++ apps/web/src/services/cost-explorer/constants/managed-cost-analysis-query-sets.ts
@@ -4,14 +4,14 @@ import type { CostQuerySetModel } from '@/services/cost-explorer/types/cost-expl
 export const DYNAMIC_COST_QUERY_SET_PARAMS = 'dynamic';
 
 export const MANAGED_COST_QUERY_SET_IDS = {
-    MONTHLY_WORKSPACE: 'Monthly cost by workspace',
+    // MONTHLY_WORKSPACE: 'Monthly cost by workspace',
     MONTHLY_PROJECT: 'Monthly cost by project',
     MONTHLY_PRODUCT: 'Monthly cost by product',
     DAILY_PRODUCT: 'Daily cost by product',
 } as const;
 
 export const MANAGED_COST_QUERY_SET_ID_LIST: string[] = [
-    MANAGED_COST_QUERY_SET_IDS.MONTHLY_WORKSPACE,
+    // MANAGED_COST_QUERY_SET_IDS.MONTHLY_WORKSPACE,
     MANAGED_COST_QUERY_SET_IDS.MONTHLY_PROJECT,
     MANAGED_COST_QUERY_SET_IDS.MONTHLY_PRODUCT,
     MANAGED_COST_QUERY_SET_IDS.DAILY_PRODUCT,
@@ -78,23 +78,23 @@ export const MANAGED_COST_QUERY_SET_LIST: Omit<CostQuerySetModel, 'data_source_i
     ...DEFAULT_MANAGED_COST_QUERY_SET_LIST,
 ];
 export const ADMIN_MANAGED_COST_QUERY_SET_LIST: Omit<CostQuerySetModel, 'data_source_id'>[] = [
-    {
-        cost_query_set_id: MANAGED_COST_QUERY_SET_IDS.MONTHLY_WORKSPACE,
-        name: MANAGED_COST_QUERY_SET_IDS.MONTHLY_WORKSPACE,
-        options: {
-            group_by: [GROUP_BY.WORKSPACE],
-            granularity: GRANULARITY.MONTHLY,
-            relative_period: {
-                unit: 'month',
-                value: 5,
-                include_today: true,
-            },
-            metadata: {
-                filters_schema: {
-                    enabled_properties: Object.values(GROUP_BY),
-                },
-            },
-        },
-    },
+    // {
+    //     cost_query_set_id: MANAGED_COST_QUERY_SET_IDS.MONTHLY_WORKSPACE,
+    //     name: MANAGED_COST_QUERY_SET_IDS.MONTHLY_WORKSPACE,
+    //     options: {
+    //         group_by: [GROUP_BY.WORKSPACE],
+    //         granularity: GRANULARITY.MONTHLY,
+    //         relative_period: {
+    //             unit: 'month',
+    //             value: 5,
+    //             include_today: true,
+    //         },
+    //         metadata: {
+    //             filters_schema: {
+    //                 enabled_properties: Object.values(GROUP_BY),
+    //             },
+    //         },
+    //     },
+    // },
     ...DEFAULT_MANAGED_COST_QUERY_SET_LIST,
 ];
