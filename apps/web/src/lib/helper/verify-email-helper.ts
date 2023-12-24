e035ba72b63f684f0112547f3f449c407837f05a
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2023-12-25 02:20:37 +0900

    refactor: notification email api user permission-based differentiation
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/lib/helper/verify-email-helper.ts
+++ apps/web/src/lib/helper/verify-email-helper.ts
@@ -2,6 +2,7 @@ import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 
 import type { UserProfileConfirmEmailParameters } from '@/schema/identity/user-profile/api-verbs/confirm-email';
 import type { UserProfileVerifyEmailParameters } from '@/schema/identity/user-profile/api-verbs/verify-email';
+import type { UserVerifyEmailParameters } from '@/schema/identity/user/api-verbs/verify-email';
 import { store } from '@/store';
 import { i18n } from '@/translations';
 
@@ -9,7 +10,7 @@ import { showSuccessMessage } from '@/lib/helper/notice-alert-helper';
 
 import ErrorHandler from '@/common/composables/error/errorHandler';
 
-export const postValidationEmail = async (params: UserProfileVerifyEmailParameters): Promise<void|Error> => {
+export const postUserProfileValidationEmail = async (params: UserProfileVerifyEmailParameters): Promise<void|Error> => {
     try {
         await SpaceConnector.clientV2.identity.userProfile.verifyEmail<UserProfileVerifyEmailParameters>(params);
         await showSuccessMessage(i18n.t('COMMON.NOTIFICATION_MODAL.SUCCESS'), '');
@@ -25,6 +26,17 @@ export const postValidationCode = async (params: UserProfileConfirmEmailParamete
         const response = await SpaceConnector.clientV2.identity.userProfile.confirmEmail<UserProfileConfirmEmailParameters>(params);
         await store.dispatch('user/setUser', { email: response.email, email_verified: response.email_verified });
         showSuccessMessage(i18n.t('IDENTITY.USER.NOTIFICATION_EMAIL.SUCCESS'), '');
+    } catch (e: any) {
+        ErrorHandler.handleRequestError(e, e.message);
+        throw e;
+    }
+};
+
+export const postUserValidationEmail = async (params: UserVerifyEmailParameters): Promise<void|Error> => {
+    try {
+        await SpaceConnector.clientV2.identity.user.verifyEmail<UserVerifyEmailParameters>(params);
+        await showSuccessMessage(i18n.t('COMMON.NOTIFICATION_MODAL.SUCCESS'), '');
+        return undefined;
     } catch (e: any) {
         ErrorHandler.handleRequestError(e, e.message);
         throw e;
--- apps/web/src/schema/identity/user-profile/api-verbs/verify-email.ts
+++ apps/web/src/schema/identity/user-profile/api-verbs/verify-email.ts
@@ -1,3 +1,3 @@
 export interface UserProfileVerifyEmailParameters {
-    email: string;
+    email?: string;
 }
