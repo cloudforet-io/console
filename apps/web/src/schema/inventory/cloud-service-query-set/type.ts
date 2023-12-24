ef5bb745a8b6e776fe9489ace2acc517fd0f8013
sulmo <sulmo@megazone.com>
2023-12-24 14:50:03 +0900

    feat: add api schema (cloudSerivceStats, cloudServiceQuerySet)
    Signed-off-by: sulmo <sulmo@megazone.com>

---
+++ apps/web/src/schema/inventory/cloud-service-query-set/type.ts
@@ -0,0 +1,1 @@
+export type CloudServiceQueryType = 'MANAGED' | 'CUSTOM';
---
+++ apps/web/src/schema/inventory/cloud-service-stats/model.ts
@@ -0,0 +1,15 @@
+export interface CloudServiceStatModel {
+    query_set_id: string;
+    values: object;
+    unit: object;
+    provider: string;
+    cloud_service_group: string;
+    cloud_service_type: string;
+    region_code: string;
+    account: string;
+    additional_info: object;
+    project_id: string;
+    workspace_id: string;
+    domain_id: string;
+    created_date: string;
+}
