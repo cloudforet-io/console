c469110190b44ca9dbcf523c9ec29e6bf4bc6430
sulmo <sulmo@megazone.com>
2023-12-29 15:47:49 +0900

    feat: update domain state to new auth data
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/store/modules/domain/actions.ts
+++ apps/web/src/store/modules/domain/actions.ts
@@ -13,27 +13,16 @@ const EXTENDED_AUTH_TYPE_MAP = {
 
 const getExtendedAuthType = (authType): ExtendedAuthType | undefined => EXTENDED_AUTH_TYPE_MAP[authType];
 
-const getAuthOptions = (pluginInfo): any => {
-    const pluginOptions = pluginInfo?.options || {};
-    const pluginMetadata = pluginInfo?.metadata || {};
-    return {
-        ...pluginOptions,
-        ...pluginMetadata,
-    };
-};
-
 export const load = async ({ commit }, name: string): Promise<void|Error> => {
     const response = await SpaceConnector.clientV2.identity.domain.getAuthInfo<DomainGetAuthInfoParams, DomainGetAuthInfoResponse>({ name });
 
-    // TODO: refactor below logic with new response
     if (response.domain_id) {
         const authMetadata = response.metadata;
         commit('setDomain', {
             domainId: response.domain_id,
             name: response.name,
             extendedAuthType: getExtendedAuthType(authMetadata.auth_type),
-            authOptions: getAuthOptions(authMetadata.plugin_info),
-            config: response.config,
+            authOptions: authMetadata,
         });
     } else {
         throw new Error(`Can not find '${name}' domain.`);
