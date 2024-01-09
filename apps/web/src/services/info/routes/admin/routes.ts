be52de91f9fdbe35d5551607d673fa937575a56c
Wanjin Noh <wanjin@megazone.com>
2024-01-09 10:15:20 +0900

    fix: show breadcrumbs for notice pages
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/info/routes/admin/routes.ts
+++ apps/web/src/services/info/routes/admin/routes.ts
@@ -4,6 +4,7 @@ import { makeAdminRouteName } from '@/router/helpers/route-helper';
 
 import { ACCESS_LEVEL } from '@/lib/access-control/config';
 import { MENU_ID } from '@/lib/menu/config';
+import { MENU_INFO_MAP } from '@/lib/menu/menu-info';
 
 import { INFO_ROUTE } from '@/services/info/routes/route-constant';
 
@@ -17,13 +18,21 @@ const AdminNoticeUpdatePage = () => import('@/services/info/pages/admin/AdminNot
 const infoRoute: RouteConfig = {
     path: 'info',
     name: makeAdminRouteName(INFO_ROUTE._NAME),
-    meta: { menuId: MENU_ID.INFO, accessLevel: ACCESS_LEVEL.ADMIN_PERMISSION },
+    meta: {
+        menuId: MENU_ID.INFO,
+        accessLevel: ACCESS_LEVEL.ADMIN_PERMISSION,
+        translationId: MENU_INFO_MAP[MENU_ID.INFO].translationId,
+    },
     redirect: () => ({ name: makeAdminRouteName(INFO_ROUTE.NOTICE._NAME) }),
     component: InfoContainer,
     children: [
         {
             path: 'notice',
-            meta: { lnbVisible: true, menuId: MENU_ID.NOTICE },
+            meta: {
+                lnbVisible: true,
+                menuId: MENU_ID.NOTICE,
+                translationId: MENU_INFO_MAP[MENU_ID.NOTICE].translationId,
+            },
             redirect: () => ({ name: makeAdminRouteName(INFO_ROUTE.NOTICE._NAME) }),
             component: { template: '<router-view />' },
             children: [
