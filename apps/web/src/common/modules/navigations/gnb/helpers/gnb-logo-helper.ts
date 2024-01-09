2ab60c0a18b2eab002d25f90faf3ccba98163a2d
Yongtae Park <samuel.park@megazone.com>
2024-01-09 19:13:58 +0900

    fix: remove unnecessary helper
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/common/modules/navigations/gnb/helpers/gnb-logo-helper.ts
+++
@@ -1,7 +0,0 @@
-import { WORKSPACE_LOGO_ICON_THEMES } from '@/common/modules/navigations/gnb/constants/constant';
-import type { GNBLogoIconTheme } from '@/common/modules/navigations/gnb/types/type';
-
-export const getRandomWorkspaceIconTheme = (): GNBLogoIconTheme => {
-    const randomIndex = Math.floor(Math.random() * WORKSPACE_LOGO_ICON_THEMES.length);
-    return WORKSPACE_LOGO_ICON_THEMES[randomIndex];
-};
