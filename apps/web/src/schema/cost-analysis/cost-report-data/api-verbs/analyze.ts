6b40490281be08dc329bbe7c9818d049dd978859
yuda <yuda@megazone.com>
2024-01-26 11:28:54 +0900

    feat: add analyze api to costTrend card

--- apps/web/src/schema/cost-analysis/cost-report-data/api-verbs/analyze.ts
+++ apps/web/src/schema/cost-analysis/cost-report-data/api-verbs/analyze.ts
@@ -3,11 +3,11 @@ import type { Query } from '@cloudforet/core-lib/space-connector/type';
 
 export interface CostReportDataAnalyzeParameters {
     query?: Query;
-    cost_report_data_id: string;
     report_year?: string;
     report_month?: string;
     product?: string;
     provider?: string;
     data_source_id?: string;
+    cost_report_data_id?: string;
     workspace_id?: string;
 }
