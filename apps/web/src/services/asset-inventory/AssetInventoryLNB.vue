7f6a4367ec8f2e73ad6847217c9e3e8eba7cd6b3
nayeongkim <nayeongkim@megazone.com>
2024-01-30 11:07:59 +0900

    revert: revert asset inventory admin mode
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/services/asset-inventory/AssetInventoryLNB.vue
+++ apps/web/src/services/asset-inventory/AssetInventoryLNB.vue
@@ -8,7 +8,11 @@
 
 <script lang="ts">
 import {
-    computed, defineComponent, getCurrentInstance, reactive, toRefs, watch,
+    computed,
+    defineComponent,
+    getCurrentInstance,
+    reactive, toRefs,
+    watch,
 } from 'vue';
 import type { Vue } from 'vue/types/vue';
 
@@ -19,7 +23,9 @@ import { i18n } from '@/translations';
 
 import { FAVORITE_TYPE } from '@/store/modules/favorite/type';
 
-import { filterLNBMenuByAccessPermission } from '@/lib/access-control/page-access-helper';
+import {
+    filterLNBMenuByAccessPermission,
+} from '@/lib/access-control/page-access-helper';
 import { MENU_ID } from '@/lib/menu/config';
 import { MENU_INFO_MAP } from '@/lib/menu/menu-info';
 
@@ -31,7 +37,9 @@ import type {
 
 import { ASSET_INVENTORY_ROUTE } from '@/services/asset-inventory/routes/route-constant';
 import { useCloudServiceDetailPageStore } from '@/services/asset-inventory/stores/cloud-service-detail-page-store';
-import type { CloudServiceDetailPageParams } from '@/services/asset-inventory/types/cloud-service-detail-page-type';
+import type {
+    CloudServiceDetailPageParams,
+} from '@/services/asset-inventory/types/cloud-service-detail-page-type';
 
 
 export default defineComponent({
@@ -49,7 +57,7 @@ export default defineComponent({
                 if (state.isCloudServiceDetailPage) return vm.$route.params as unknown as CloudServiceDetailPageParams;
                 return undefined;
             }),
-            header: computed(() => i18n.t(MENU_INFO_MAP[MENU_ID.ASSET_INVENTORY].translationId) as string),
+            header: computed(() => i18n.t(MENU_INFO_MAP[MENU_ID.ASSET_INVENTORY].translationId)),
             backLink: computed<BackLink|undefined>(() => {
                 if (!state.isCloudServiceDetailPage) return undefined;
                 return { label: i18n.t(MENU_INFO_MAP[MENU_ID.CLOUD_SERVICE].translationId), to: { name: ASSET_INVENTORY_ROUTE.CLOUD_SERVICE._NAME } };
@@ -104,14 +112,6 @@ export default defineComponent({
             adminModeMenuSet: computed<LNBMenu[]>(() => [
                 {
                     type: 'item',
-                    id: MENU_ID.SERVER,
-                    label: i18n.t(MENU_INFO_MAP[MENU_ID.SERVER].translationId),
-                    to: getProperRouteLocation({
-                        name: ASSET_INVENTORY_ROUTE.SERVER._NAME,
-                    }),
-                },
-                {
-                    type: 'item',
                     id: MENU_ID.COLLECTOR,
                     label: i18n.t(MENU_INFO_MAP[MENU_ID.COLLECTOR].translationId),
                     to: getProperRouteLocation({
@@ -121,6 +121,7 @@ export default defineComponent({
 
             ]),
             menuSet: computed<LNBMenu[]>(() => {
+                if (isAdminMode.value) return state.adminModeMenuSet;
                 const menu: LNBMenu[] = (state.isCloudServiceDetailPage ? [] : [{
                     type: 'item',
                     id: MENU_ID.CLOUD_SERVICE,
@@ -129,11 +130,11 @@ export default defineComponent({
                         name: ASSET_INVENTORY_ROUTE.CLOUD_SERVICE._NAME,
                     }),
                 }]);
-                if (isAdminMode.value) return menu.concat(state.adminModeMenuSet);
-                return [
+                const result = [
                     (state.isCloudServiceDetailPage ? state.cloudServiceDetailMenuSet : []),
                     ...filterLNBMenuByAccessPermission(menu.concat(state.userModeMenuSet), store.getters['user/pageAccessPermissionList']),
                 ];
+                return result;
             }),
         });
 
