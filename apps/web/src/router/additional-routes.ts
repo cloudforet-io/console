438dc09c097bc0310d0dce4346c609eafaa3412a
Yongtae Park <samuel.park@megazone.com>
2024-01-30 16:05:06 +0900

    fix(integral-routes): separate cost-report route
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/router/additional-routes.ts
@@ -0,0 +1,18 @@
+import type { RouteConfig } from 'vue-router';
+
+import { ROOT_ROUTE } from '@/router/constant';
+
+import CostReportDetailPage from '@/common/pages/CostReportDetailPage.vue';
+
+export const additionalRoutes: RouteConfig[] = [
+    {
+        path: 'cost-report',
+        name: ROOT_ROUTE.COST_REPORT._NAME,
+        props: (route) => ({
+            accessToken: route.query.sso_access_token,
+            costReportId: route.query.cost_report_id,
+            language: route.query.language,
+        }),
+        component: CostReportDetailPage,
+    },
+];
