50896bdcb182838e97d04fdb575b879c1269271b
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2024-02-01 11:56:02 +0900

    feat: apply scopedSlots at PDynamicLayout type components
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- packages/mirinae/src/data-display/dynamic/dynamic-layout/templates/list/index.vue
+++ packages/mirinae/src/data-display/dynamic/dynamic-layout/templates/list/index.vue
@@ -18,6 +18,14 @@
                       v-bind="scope"
                 />
             </template>
+
+            <template v-for="(_, slot) of $scopedSlots"
+                      #[slot]="scope"
+            >
+                <slot :name="slot"
+                      v-bind="scope"
+                />
+            </template>
         </p-dynamic-layout>
     </div>
 </template>
