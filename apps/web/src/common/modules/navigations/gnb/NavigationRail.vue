001e2cef0a8bfc0525a058bee4449144dd4cdc2a
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2024-02-16 14:45:07 +0900

    feat: apply menu
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/common/modules/navigations/gnb/NavigationRail.vue
+++ apps/web/src/common/modules/navigations/gnb/NavigationRail.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { computed, reactive } from 'vue';
+import { useRoute } from 'vue-router/composables';
 
 import { PI } from '@spaceone/design-system';
+import type { ContextMenuType } from '@spaceone/design-system/src/inputs/context-menu/type';
 
+import { store } from '@/store';
+
+import type { DisplayMenu } from '@/store/modules/display/type';
+import { FAVORITE_TYPE } from '@/store/modules/favorite/type';
+
+import FavoriteButton from '@/common/modules/favorites/favorite-button/FavoriteButton.vue';
+
+interface GNBMenuType extends DisplayMenu {
+    type: string;
+}
 interface Props {
     isMinimizeGnb?: boolean;
 }
@@ -11,13 +23,42 @@ const props = withDefaults(defineProps<Props>(), {
     isMinimizeGnb: false,
 });
 
+const route = useRoute();
+
 const state = reactive({
     isHovered: false,
+    gnbMenuList: computed<GNBMenuType[]>(() => [...store.getters['display/GNBMenuList']]),
+    visibleGnbMenuList: computed<GNBMenuType[]>(() => {
+        let result = [] as GNBMenuType[];
+        state.gnbMenuList.forEach((menu) => {
+            result = [
+                ...result,
+                {
+                    ...menu,
+                    name: menu.id,
+                    type: 'header',
+                },
+            ];
+            if (menu.subMenuList) {
+                result = [...result, ...convertGNBMenuToMenuItem(menu.subMenuList)];
+            }
+        });
+        return result;
+    }),
+    selectedMenuId: computed(() => {
+        const selectedMenu = state.visibleGnbMenuList.find((menu) => route.matched[route.matched.length - 1].meta.menuId === menu.id);
+        return selectedMenu.id;
+    }),
 });
 
 const handleMouseEvent = (value: boolean) => {
     state.isHovered = value;
 };
+const convertGNBMenuToMenuItem = (menuList: GNBMenuType[], menuType: ContextMenuType = 'item'): GNBMenuType[] => menuList.map((menu) => ({
+    ...menu,
+    name: menu.id,
+    type: menuType,
+}));
 </script>
 
 <template>
@@ -26,40 +67,80 @@ const handleMouseEvent = (value: boolean) => {
          @mouseover="handleMouseEvent(true)"
          @mouseleave="handleMouseEvent(false)"
     >
-        <!--  TODO: will be updated -->
-        <div class="icon-wrapper">
-            <p-i name="ic_service_home"
-                 class="menu-button"
-                 height="1.25rem"
-                 width="1.25rem"
-                 color="inherit"
+        <router-link v-for="(item, idx) in state.visibleGnbMenuList"
+                     :key="`navigation-rail-item-${idx}`"
+                     :to="item.to"
+                     class="service-menu"
+                     :class="{
+                         'is-selected': state.selectedMenuId === item.id,
+                         'is-only-label': item.type === 'header' && item.subMenuList?.length > 0
+                     }"
+        >
+            <div class="menu-wrapper">
+                <p-i v-if="item.subMenuList?.length === 0"
+                     :name="item.icon"
+                     class="menu-button"
+                     height="1.25rem"
+                     width="1.25rem"
+                     color="inherit"
+                />
+                <span v-if="!props.isMinimizeGnb || state.isHovered"
+                      class="menu-title"
+                >
+                    {{ item.label }}
+                </span>
+            </div>
+            <favorite-button v-if="item.subMenuList?.length === 0"
+                             class="favorite-button"
+                             :item-id="item.id"
+                             :favorite-type="FAVORITE_TYPE.MENU"
+                             scale="0.65"
             />
-            <span v-if="!props.isMinimizeGnb || state.isHovered"
-                  class="menu-title"
-            >
-                menu title
-            </span>
-        </div>
+        </router-link>
     </div>
 </template>
 
 <style scoped lang="postcss">
 .navigation-rail {
-    @apply flex-col border-r;
+    @apply flex-col items-start border-r;
     top: $gnb-toolbox-height;
     width: $gnb-navigation-rail-max-width;
     height: 100%;
-    padding-top: 1rem;
-    padding-bottom: 1rem;
+    padding: 1rem 0.75rem;
     transition: width 0.3s ease;
-    .icon-wrapper {
-        @apply flex items-center;
+    .service-menu {
+        @apply flex items-center justify-between text-label-md;
+        width: 100%;
+        height: 2.125rem;
+        padding-right: 0.5rem;
+        padding-left: 0.5rem;
         gap: 0.75rem;
-        .menu-title {
-            @apply text-label-md;
+        .favorite-button {
+            @apply hidden;
         }
-        &:not(.is-minimize) {
-            @apply cursor-pointer;
+        &:hover:not(.is-only-label) {
+            @apply bg-violet-100 cursor-pointer;
+            .favorite-button {
+                @apply block;
+            }
+        }
+        &.is-only-label {
+            @apply items-end text-gray-500;
+            height: 2.875rem;
+            padding-bottom: 0.5rem;
+        }
+        &.is-selected {
+            @apply relative bg-violet-100 text-violet-600;
+            &::before {
+                @apply absolute bg-violet;
+                content: '';
+                top: 0;
+                left: -0.75rem;
+                width: 0.25rem;
+                height: 100%;
+                border-top-right-radius: 0.125rem;
+                border-bottom-right-radius: 0.125rem;
+            }
         }
     }
     &.is-minimize {
