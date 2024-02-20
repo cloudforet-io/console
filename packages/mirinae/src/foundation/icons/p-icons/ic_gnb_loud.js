d0278fd1a8e3695c6c78ce62284dcf57cbe7696a
sulmo <sulmo@megazone.com>
2024-02-20 22:18:26 +0900

    feat: update icons

---
+++ packages/mirinae/src/foundation/icons/p-icons/ic_gnb_loud.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_gnb_loud': {
+    width: 32,
+    height: 32,
+    viewBox: '0 0 32 32',
+    data: '<path pid="0" d="M7 17a8 8 0 018 8M7 11a14 14 0 0114 14M7 5a20 20 0 0120 20M9 25a2 2 0 11-4 0 2 2 0 014 0z" _stroke="#898995" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>'
+  }
+})
