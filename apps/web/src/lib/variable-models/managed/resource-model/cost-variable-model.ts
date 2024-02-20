ea98abcd93b9fa0b234fe0fd17685f1c8ea0a367
yuda <yuda@megazone.com>
2024-02-20 11:31:21 +0900

    chore: delete `labelsSchema` from cost model

--- apps/web/src/lib/variable-models/managed/resource-model/cost-variable-model.ts
+++ apps/web/src/lib/variable-models/managed/resource-model/cost-variable-model.ts
@@ -8,17 +8,4 @@ export default class CostVariableModel extends ResourceVariableModel {
     resourceType = 'cost_analysis.Cost';
 
     idKey = 'cost_id';
-
-    labelsSchema = {
-        product: {
-            key: 'product',
-            name: 'Product',
-            type: 'REFERENCE_KEY',
-        },
-        usage_type: {
-            key: 'usage_type',
-            name: 'Usage Type',
-            type: 'REFERENCE_KEY',
-        },
-    };
 }
