dc409b40049c18c46fe408463ad3ac449fbdecfe
Yongtae Park <samuel.park@megazone.com>
2024-02-21 18:07:45 +0900

    fix: apply data key at middle handler
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/lib/variable-models/variable-model-menu-handler.ts
+++ apps/web/src/lib/variable-models/variable-model-menu-handler.ts
@@ -1,56 +1,124 @@
 import type { AutocompleteHandler } from '@spaceone/design-system/types/inputs/dropdown/select-dropdown/type';
 
-import type { VariableModel } from '@/lib/variable-models/index';
+import type { IBaseVariableModel, IEnumVariableModel, IResourceVariableModel } from '@/lib/variable-models/_base/types';
 
 import ErrorHandler from '@/common/composables/error/errorHandler';
 
-export const getVariableModelMenuHandler = (variableModel: VariableModel|VariableModel[], options?: Record<string, any>): AutocompleteHandler => {
-    const variableModels = Array.isArray(variableModel) ? variableModel : [variableModel];
+
+const _getTitle = (modelInfo: VariableModelInfo) => {
+    const _dataKey = modelInfo.dataKey;
+    return _dataKey ? modelInfo.variableModel[_dataKey].name : modelInfo.variableModel.meta?.name;
+};
+// dataKey
+interface VariableModelInfo {
+    variableModel: IBaseVariableModel|IResourceVariableModel|IEnumVariableModel;
+    dataKey: 'product';
+}
+
+export const getVariableModelMenuHandler = (variableModelInfoList: VariableModelInfo[], options?: Record<string, any>): AutocompleteHandler => {
+    const _variableModelInfoList = variableModelInfoList;
     return async (inputText: string, pageStart, pageLimit, filters, resultIndex) => {
-    // if resultIndex is empty, it means that the handler is called for the first time. so, we need to call all variableModels' list().
+        const _query = {
+            start: pageStart,
+            limit: pageLimit ?? 10,
+            search: inputText,
+            filters: filters?.length ? filters.map((f) => f.name as string) : undefined,
+            options,
+        };
+
+        // if resultIndex is empty, it means that the handler is called for the first time. so, we need to call all variableModels' list().
         if (resultIndex === undefined) {
-            const responses = await Promise.all(variableModels.map((varModel) => varModel.list({
-                start: pageStart,
-                limit: pageLimit ?? 10,
-                search: inputText,
-                filters: filters?.length ? filters.map((f) => f.name as string) : undefined,
-                options,
-            })));
+            const responses = await Promise.all(_variableModelInfoList.map(({ variableModel, dataKey }) => {
+                if (dataKey) {
+                    return variableModel[dataKey].values(_query);
+                }
+                return variableModel.list(_query);
+            }));
             const handlerResults = responses.map((result, resIndex) => ({
                 results: result.results.map((d) => ({
-                    name: d.key, label: d.name,
+                    name: d.key,
+                    label: d.name,
                 })),
                 more: result.more,
-                title: variableModels[resIndex].name,
+                title: _getTitle(_variableModelInfoList[resIndex]),
             }));
 
             return handlerResults;
         }
 
         // if resultIndex is given, just call the specific variableModel's list().
-        const varModel = variableModels[resultIndex];
+        const variableModelInfo = _variableModelInfoList[resultIndex];
 
-        if (!varModel) {
+        if (!variableModelInfo) {
             ErrorHandler.handleError(new Error(`No variable model found for index ${resultIndex}`));
             return [];
         }
+        let response;
 
-        const response = await varModel.list({
-            start: pageStart,
-            limit: pageLimit ?? 10,
-            search: inputText,
-            filters: (Array.isArray(filters) && filters.length > 0) ? filters.map((f) => f.name as string) : undefined,
-            options,
-        });
-        return variableModels.map((model, i) => {
-            if (i !== resultIndex) return { results: [], title: model.name };
+        if (variableModelInfo.dataKey) {
+            response = await variableModelInfo.variableModel[variableModelInfo.dataKey].values(_query);
+        } else {
+            response = await variableModelInfo.variableModel.list(_query);
+        }
+        return _variableModelInfoList.map((modelInfo, i) => {
+            if (i !== resultIndex) return { results: [], title: _getTitle(modelInfo) };
             return {
                 results: response.results.map((d) => ({
                     name: d.key, label: d.name,
                 })),
                 more: response.more,
-                title: model.name,
+                title: _getTitle(modelInfo),
             };
         });
     };
 };
+// export const getVariableModelMenuHandler = (variableModel: VariableModel|VariableModel[], options?: Record<string, any>): AutocompleteHandler => {
+//     const variableModels = Array.isArray(variableModel) ? variableModel : [variableModel];
+//     return async (inputText: string, pageStart, pageLimit, filters, resultIndex) => {
+//     // if resultIndex is empty, it means that the handler is called for the first time. so, we need to call all variableModels' list().
+//         if (resultIndex === undefined) {
+//             const responses = await Promise.all(variableModels.map((varModel) => varModel.list({
+//                 start: pageStart,
+//                 limit: pageLimit ?? 10,
+//                 search: inputText,
+//                 filters: filters?.length ? filters.map((f) => f.name as string) : undefined,
+//                 options,
+//             })));
+//             const handlerResults = responses.map((result, resIndex) => ({
+//                 results: result.results.map((d) => ({
+//                     name: d.key, label: d.name,
+//                 })),
+//                 more: result.more,
+//                 title: variableModels[resIndex].name,
+//             }));
+//
+//             return handlerResults;
+//         }
+//
+//         // if resultIndex is given, just call the specific variableModel's list().
+//         const varModel = variableModels[resultIndex];
+//
+//         if (!varModel) {
+//             ErrorHandler.handleError(new Error(`No variable model found for index ${resultIndex}`));
+//             return [];
+//         }
+//
+//         const response = await varModel.list({
+//             start: pageStart,
+//             limit: pageLimit ?? 10,
+//             search: inputText,
+//             filters: (Array.isArray(filters) && filters.length > 0) ? filters.map((f) => f.name as string) : undefined,
+//             options,
+//         });
+//         return variableModels.map((model, i) => {
+//             if (i !== resultIndex) return { results: [], title: model.name };
+//             return {
+//                 results: response.results.map((d) => ({
+//                     name: d.key, label: d.name,
+//                 })),
+//                 more: response.more,
+//                 title: model.name,
+//             };
+//         });
+//     };
+// };
