485a067fd07d20c705ce17204426604d404e9c35
Yongtae Park <samuel.park@megazone.com>
2024-02-21 18:05:56 +0900

    fix: temporarily remove prefetch

--- apps/web/src/lib/site-initializer/resource-prefetch.ts
+++ apps/web/src/lib/site-initializer/resource-prefetch.ts
@@ -1,11 +1,11 @@
 import { VariableModel } from '@/lib/variable-models';
 import { MANAGED_VARIABLE_MODEL_CONFIGS } from '@/lib/variable-models/managed-model-config/base-managed-model-config';
 
-export const prefetchResources = () => {
-    Object.values(MANAGED_VARIABLE_MODEL_CONFIGS).forEach((config) => {
-        if (config.prefetch) {
-            const model = new VariableModel({ type: 'MANAGED', key: config.key });
-            model.list();
-        }
-    });
-};
+// export const prefetchResources = () => {
+//     Object.values(MANAGED_VARIABLE_MODEL_CONFIGS).forEach((config) => {
+//         if (config.prefetch) {
+//             const model = new VariableModel({ type: 'MANAGED', key: config.key });
+//             model.list();
+//         }
+//     });
+// };
