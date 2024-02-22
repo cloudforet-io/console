59f4cb9c3efff2024be375662a82e794577b6242
NaYeong,Kim <nayeongkim@megazone.com>
2024-02-22 17:56:47 +0900

    chore: run icons (#3147)
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

---
+++ packages/mirinae/src/foundation/icons/p-icons/ic_gnb_loud-unread.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_gnb_loud-unread': {
+    width: 32,
+    height: 32,
+    viewBox: '0 0 32 32',
+    data: '<path pid="0" fill-rule="evenodd" clip-rule="evenodd" d="M6.1 5a.9.9 0 01.9-.9A20.9 20.9 0 0127.9 25a.9.9 0 01-1.8 0A19.1 19.1 0 007 5.9a.9.9 0 01-.9-.9zm0 6a.9.9 0 01.9-.9A14.9 14.9 0 0121.9 25a.9.9 0 01-1.8 0A13.1 13.1 0 007 11.9a.9.9 0 01-.9-.9zm0 6a.9.9 0 01.9-.9 8.9 8.9 0 018.9 8.9.9.9 0 01-1.8 0A7.1 7.1 0 007 17.9a.9.9 0 01-.9-.9zm.9 6.9a1.1 1.1 0 100 2.2 1.1 1.1 0 000-2.2zM4.1 25a2.9 2.9 0 115.8 0 2.9 2.9 0 01-5.8 0z" _fill="#898995"/><path pid="1" d="M29 8a5 5 0 11-10 0 5 5 0 0110 0z" _fill="#49A7F7"/><path pid="2" fill-rule="evenodd" clip-rule="evenodd" d="M32 8a8 8 0 11-16 0 8 8 0 0116 0zm-8 5a5 5 0 100-10 5 5 0 000 10z" _fill="#fff"/>'
+  }
+})
