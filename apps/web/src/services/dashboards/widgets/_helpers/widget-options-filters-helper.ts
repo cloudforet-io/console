67d5cacd1e6d33ce98553e7c5999fe982d6b2290
yuda <yuda@megazone.com>
2024-02-26 17:49:45 +0900

    fix: fix filters helper of widget

--- apps/web/src/services/dashboards/widgets/_helpers/widget-options-filters-helper.ts
+++ apps/web/src/services/dashboards/widgets/_helpers/widget-options-filters-helper.ts
@@ -3,6 +3,7 @@ import { cloneDeep, union } from 'lodash';
 import type { WidgetFiltersMap, WidgetFilterKey } from '@/schema/dashboard/_types/widget-type';
 
 import { MANAGED_WIDGET_FILTERS_SCHEMA_PROPERTIES } from '@/services/dashboards/widgets/_constants/managed-widget-options-schema';
+import { MANAGED_VARIABLE_MODELS } from '@/lib/variable-models/managed-model-config/base-managed-model-config';
 
 
 /**
@@ -20,8 +21,8 @@ export const setFilterAndGetWidgetFiltersMap = (filtersMap: WidgetFiltersMap = {
     const _filtersMap = cloneDeep(filtersMap);
 
     targetProperty.item_options?.forEach((itemOption) => {
-        const idKey = itemOption.key;
         const dataKey = itemOption.dataKey;
+        const idKey = MANAGED_VARIABLE_MODELS[itemOption.key]._meta.idKey;
         if (!idKey && !dataKey) {
             console.error(new Error(`Invalid referencing idKey|dataKey of variable model by options filter key: ${filterKey}`));
         } else {
@@ -29,9 +30,9 @@ export const setFilterAndGetWidgetFiltersMap = (filtersMap: WidgetFiltersMap = {
             const filters = _filtersMap[filterKey];
             if (filters?.length) {
                 const _originValue = Array.isArray(filters[0].v) ? filters[0].v : [filters[0].v];
-                _filtersMap[filterKey] = [{ k: idKey ?? dataKey, v: union(_originValue, _value), o: '=' }];
+                _filtersMap[filterKey] = [{ k: dataKey ?? idKey, v: union(_originValue, _value), o: '=' }];
             } else {
-                _filtersMap[filterKey] = [{ k: idKey ?? dataKey, v: _value, o: '=' }];
+                _filtersMap[filterKey] = [{ k: dataKey ?? idKey, v: _value, o: '=' }];
             }
         }
     });
