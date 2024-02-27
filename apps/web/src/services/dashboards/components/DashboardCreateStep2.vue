393359623e274014591a82aea071f26e8c735478
Yongtae Park <samuel.park@megazone.com>
2024-02-27 20:46:08 +0900

    chore: apply review & small fix
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/services/dashboards/components/DashboardCreateStep2.vue
+++ apps/web/src/services/dashboards/components/DashboardCreateStep2.vue
@@ -8,11 +8,6 @@ import DashboardCreateScopeForm from '@/services/dashboards/components/Dashboard
 import type { DashboardModel } from '@/services/dashboards/types/dashboard-api-schema-type';
 import type { ProjectTreeNodeData } from '@/services/project/types/project-tree-type';
 
-
-// interface ProviderBoardSet {
-//     value: string;
-//     title: string;
-// }
 interface Props {
     selectedTemplate: DashboardModel;
 }
@@ -22,29 +17,6 @@ const props = withDefaults(defineProps<Props>(), {
 });
 
 const emit = defineEmits<{(e: 'select-project', value: ProjectTreeNodeData): void}>();
-//
-// const storeState = reactive({
-//     isWorkspaceOwner: computed(() => store.getters['user/getCurrentRoleInfo']?.roleType === ROLE_TYPE.WORKSPACE_OWNER),
-//     providers: computed<ProviderReferenceMap>(() => store.getters['reference/providerItems']),
-// });
-// const state = reactive({
-//     // TODO: temp data
-//     providerBoardSets: computed<ProviderBoardSet[]>(() => ([
-//         {
-//             value: 'aws',
-//             title: storeState.providers.aws?.label ?? 'AWS',
-//         },
-//         {
-//             value: 'google_cloud',
-//             title: storeState.providers.google_cloud?.label ?? 'Google Cloud',
-//         },
-//         {
-//             value: 'azure',
-//             title: storeState.providers.azure?.label ?? 'Azure',
-//         },
-//     ])),
-//     selectedProviderId: undefined as undefined|string,
-// });
 
 const handleSelectProject = (project: ProjectTreeNodeData) => {
     emit('select-project', project);
@@ -70,44 +42,6 @@ const handleSelectProject = (project: ProjectTreeNodeData) => {
                 </div>
             </div>
         </div>
-        <!--        <div class="selected-ootb-wrapper">-->
-        <!--            <p-i name="ic_dashboard-template_blank"-->
-        <!--                 width="3.5rem"-->
-        <!--                 height="3.5rem"-->
-        <!--            />-->
-        <!--            <div class="description-wrapper">-->
-        <!--                <p class="description-title">-->
-        <!--                    {{ $t('DASHBOARDS.CREATE.BLANK') }}-->
-        <!--                </p>-->
-        <!--                <p class="description-text">-->
-        <!--                    {{ $t('DASHBOARDS.CREATE.BLANK_DESC') }}-->
-        <!--                </p>-->
-        <!--            </div>-->
-        <!--        </div>-->
-        <!--        <div class="provider-board-wrapper">-->
-        <!--            <p-field-title class="field-title">-->
-        <!--                &lt;!&ndash;TODO: set language after spec is ready&ndash;&gt;-->
-        <!--                Provider-->
-        <!--            </p-field-title>-->
-        <!--            <p-board style-type="cards"-->
-        <!--                     :style-options="{ column: 2 }"-->
-        <!--                     :board-sets="state.providerBoardSets"-->
-        <!--                     selectable-->
-        <!--                     :selected-item="state.selectedProviderId"-->
-        <!--                     @item-click="handleClickProviderItem"-->
-        <!--            >-->
-        <!--                <template #item-left-content="{board}">-->
-        <!--                    <p-lazy-img width="2.5rem"-->
-        <!--                                height="2.5rem"-->
-        <!--                                :src="storeState.providers[board.value]?.icon"-->
-        <!--                                class="mr-3"-->
-        <!--                    />-->
-        <!--                </template>-->
-        <!--                <template #item-content="{board}">-->
-        <!--                    {{ board.title }}-->
-        <!--                </template>-->
-        <!--            </p-board>-->
-        <!--        </div>-->
         <dashboard-create-scope-form @set-project="handleSelectProject" />
     </div>
 </template>
