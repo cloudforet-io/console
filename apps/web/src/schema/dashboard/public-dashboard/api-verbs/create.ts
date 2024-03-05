14be3e8262ee892ba23cc4233b0f7b9763017abd
Yongtae Park <samuel.park@megazone.com>
2024-03-05 19:29:11 +0900

    fix: apply template info to dashboard create
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/schema/dashboard/public-dashboard/api-verbs/create.ts
+++ apps/web/src/schema/dashboard/public-dashboard/api-verbs/create.ts
@@ -2,11 +2,14 @@ import type { Tags } from '@/schema/_common/model';
 import type { ResourceGroupType } from '@/schema/_common/type';
 import type {
     DashboardLayoutWidgetInfo, DashboardSettings, DashboardVariables, DashboardVariablesSchema,
+    TemplateType,
 } from '@/schema/dashboard/_types/dashboard-type';
 
 
 export interface PublicDashboardCreateParameters {
     name: string;
+    template_id: string;
+    template_type: TemplateType;
     layouts?: DashboardLayoutWidgetInfo[][];
     variables?: DashboardVariables;
     settings?: DashboardSettings;
