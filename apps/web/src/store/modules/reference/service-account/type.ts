888bbec917aa850f7f19ec16aa712f9abf937495
sulmo <sulmo@megazone.com>
2024-03-11 10:42:20 +0900

    feat: apply accountId to recent labels

--- apps/web/src/store/modules/reference/service-account/type.ts
+++ apps/web/src/store/modules/reference/service-account/type.ts
@@ -1,6 +1,10 @@
 import type { ReferenceItem, ReferenceMap } from '@/store/modules/reference/type';
 
-export type ServiceAccountReferenceItem = Required<Pick<ReferenceItem<undefined>, 'key'|'label'|'name'>>;
+export type ServiceAccountReferenceItem = Required<Pick<ReferenceItem<{
+    account_id?: string;
+    subscription_id?: string;
+    project_id?: string;
+}>, 'key'|'label'|'name'|'provider'|'data'>>;
 
 export type ServiceAccountReferenceMap = ReferenceMap<ServiceAccountReferenceItem>;
 
