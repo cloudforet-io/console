16544412256908ece5ea9a0b596b603a709c008f
Dahyun Yu <yuda@megazone.com>
2024-03-13 13:37:06 +0900

    feat: use `trusted_account` pinia store instead of vuex store (#3267)
    * feat: delete trusted_account vuex store
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * feat: use trusted_account pinia store
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/store/reference/all-reference-store.ts
+++ apps/web/src/store/reference/all-reference-store.ts
@@ -17,14 +17,24 @@ import { useProjectGroupReferenceStore } from '@/store/reference/project-group-r
 import type { ProjectReferenceMap } from '@/store/reference/project-reference-store';
 import { useProjectReferenceStore } from '@/store/reference/project-reference-store';
 import { usePublicDashboardReferenceStore } from '@/store/reference/public-dashboard-reference-store';
+import { useTrustedAccountReferenceStore } from '@/store/reference/trusted-account-reference-store';
 import type { UserReferenceMap } from '@/store/reference/user-reference-store';
 import { useUserReferenceStore } from '@/store/reference/user-reference-store';
 import { useWebhookReferenceStore } from '@/store/reference/webhook-reference-store';
 import type { WorkspaceReferenceMap } from '@/store/reference/workspace-reference-store';
 import { useWorkspaceReferenceStore } from '@/store/reference/workspace-reference-store';
 
-import type { ManagedVariableModelKey } from '@/lib/variable-models/managed';
 
+type PiniaStoreReferenceType =
+    'cost_data_source'
+    |'cloud_service_query_set'
+    |'project'
+    |'project_group'
+    |'workspace'
+    |'user'
+    |'public_dashboard'
+    |'webhook'
+    |'trusted_account';
 
 export const useAllReferenceStore = defineStore('all-reference-store', () => {
     const costDataSourceReferenceStore = useCostDataSourceReferenceStore();
@@ -35,6 +45,7 @@ export const useAllReferenceStore = defineStore('all-reference-store', () => {
     const userReferenceStore = useUserReferenceStore();
     const publicDashboardReferenceStore = usePublicDashboardReferenceStore();
     const webhookReferenceStore = useWebhookReferenceStore();
+    const trustedAccountReferenceStore = useTrustedAccountReferenceStore();
 
     const getters = reactive({
         projectGroup: computed<ProjectGroupReferenceMap>(() => projectGroupReferenceStore.getters.projectGroupItems),
@@ -45,10 +56,11 @@ export const useAllReferenceStore = defineStore('all-reference-store', () => {
         workspace: computed<WorkspaceReferenceMap>(() => workspaceReferenceStore.getters.workspaceItems),
         publicDashboard: computed(() => publicDashboardReferenceStore.getters.publicDashboardItems),
         webhook: computed(() => webhookReferenceStore.getters.webhookItems),
+        trustedAccount: computed(() => trustedAccountReferenceStore.getters.trustedAccountItems),
     });
 
     const actions = {
-        async sync(type: ManagedVariableModelKey|'public_dashboard', data?: any) {
+        async sync(type: PiniaStoreReferenceType, data?: any) {
             switch (type) {
             case 'project':
                 await projectReferenceStore.sync(data); break;
@@ -62,10 +74,12 @@ export const useAllReferenceStore = defineStore('all-reference-store', () => {
                 await publicDashboardReferenceStore.sync(data); break;
             case 'webhook':
                 await webhookReferenceStore.sync(data); break;
+            case 'trusted_account':
+                await trustedAccountReferenceStore.sync(data); break;
             default: break;
             }
         },
-        async load(type: ManagedVariableModelKey|'public_dashboard', options?: ReferenceLoadOptions) {
+        async load(type: PiniaStoreReferenceType, options?: ReferenceLoadOptions) {
             switch (type) {
             case 'cost_data_source':
                 await costDataSourceReferenceStore.load(options); break;
@@ -83,6 +97,8 @@ export const useAllReferenceStore = defineStore('all-reference-store', () => {
                 await publicDashboardReferenceStore.load(options); break;
             case 'webhook':
                 await webhookReferenceStore.load(options); break;
+            case 'trusted_account':
+                await trustedAccountReferenceStore.load(options); break;
             default: break;
             }
         },
@@ -95,6 +111,7 @@ export const useAllReferenceStore = defineStore('all-reference-store', () => {
             userReferenceStore.flush();
             publicDashboardReferenceStore.flush();
             webhookReferenceStore.flush();
+            trustedAccountReferenceStore.flush();
         },
     };
 
