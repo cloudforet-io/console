b4a4d96d0fe5ae94fddb356afb0cd518a11b69cc
sulmo <sulmo@megazone.com>
2024-03-14 11:55:42 +0900

    fix: remove vuex provider store
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/store/modules/reference/provider/type.ts
+++
@@ -1,9 +0,0 @@
-import type { ReferenceItem, ReferenceMap } from '@/store/modules/reference/type';
-
-export type ProviderReferenceItem = Required<Pick<ReferenceItem<undefined>, 'key'|'label'|'name'|'icon'|'color'|'linkTemplate'>>;
-
-export type ProviderReferenceMap = ReferenceMap<ProviderReferenceItem>;
-
-export interface ProviderReferenceState {
-    items?: ProviderReferenceMap;
-}
