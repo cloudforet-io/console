7b7f60b863e54d0dab7f98d094d53d079e82e6a0
nayeongkim <nayeongkim@megazone.com>
2024-03-15 14:39:23 +0900

    refactor: changing the template structure
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/services/asset-inventory/AssetInventoryContainer.vue
+++ apps/web/src/services/asset-inventory/AssetInventoryContainer.vue
@@ -1,29 +1,8 @@
-<template>
-    <fragment>
-        <vertical-page-layout v-if="$route.meta.lsbVisible">
-            <template #sidebar>
-                <asset-inventory-l-s-b />
-            </template>
-            <template #default>
-                <router-view />
-            </template>
-        </vertical-page-layout>
-        <centered-page-layout v-if="$route.meta.centeredLayout"
-                              has-nav-bar
-        >
-            <router-view />
-        </centered-page-layout>
-        <general-page-layout v-else>
-            <router-view />
-        </general-page-layout>
-    </fragment>
-</template>
-
-<script lang="ts">
+<script lang="ts" setup>
 import {
-    computed,
-    defineComponent, onUnmounted,
+    computed, onUnmounted,
 } from 'vue';
+import { useRoute } from 'vue-router/composables';
 
 import { LocalStorageAccessor } from '@cloudforet/core-lib/local-storage-accessor';
 
@@ -38,39 +17,49 @@ import { useAssetInventorySettingsStore } from '@/services/asset-inventory/store
 import { useCloudServiceDetailPageStore } from '@/services/asset-inventory/stores/cloud-service-detail-page-store';
 import { useCloudServicePageStore } from '@/services/asset-inventory/stores/cloud-service-page-store';
 
+const cloudServicePageStore = useCloudServicePageStore();
+const cloudServiceDetailPageStore = useCloudServiceDetailPageStore();
 
-export default defineComponent({
-    name: 'AssetInventoryContainer',
-    components: {
-        AssetInventoryLSB,
-        VerticalPageLayout,
-        GeneralPageLayout,
-        CenteredPageLayout,
-    },
-    setup() {
-        const cloudServicePageStore = useCloudServicePageStore();
-        const cloudServiceDetailPageStore = useCloudServiceDetailPageStore();
+const route = useRoute();
 
-        const userId = computed(() => store.state.user.userId);
-        const assetInventorySettings = useAssetInventorySettingsStore();
-        assetInventorySettings.initState();
-        assetInventorySettings.$onAction((action) => {
-            action.after(() => {
-                if (window) {
-                    const settings = LocalStorageAccessor.getItem(userId.value) ?? {};
-                    settings.assetInventory = action.store.$state;
-                    LocalStorageAccessor.setItem(userId.value, settings);
-                }
-            });
-        });
-
-        onUnmounted(() => {
-            cloudServicePageStore.$dispose();
-            cloudServicePageStore.$reset();
-            cloudServiceDetailPageStore.$dispose();
-            cloudServiceDetailPageStore.$reset();
-        });
-    },
+const userId = computed(() => store.state.user.userId);
+const assetInventorySettings = useAssetInventorySettingsStore();
+assetInventorySettings.initState();
+assetInventorySettings.$onAction((action) => {
+    action.after(() => {
+        if (window) {
+            const settings = LocalStorageAccessor.getItem(userId.value) ?? {};
+            settings.assetInventory = action.store.$state;
+            LocalStorageAccessor.setItem(userId.value, settings);
+        }
+    });
 });
 
+onUnmounted(() => {
+    cloudServicePageStore.$dispose();
+    cloudServicePageStore.$reset();
+    cloudServiceDetailPageStore.$dispose();
+    cloudServiceDetailPageStore.$reset();
+});
 </script>
+
+<template>
+    <fragment>
+        <vertical-page-layout v-if="route.meta.lsbVisible">
+            <template #sidebar>
+                <asset-inventory-l-s-b />
+            </template>
+            <template #default>
+                <router-view />
+            </template>
+        </vertical-page-layout>
+        <centered-page-layout v-if="route.meta.centeredLayout"
+                              has-nav-bar
+        >
+            <router-view />
+        </centered-page-layout>
+        <general-page-layout v-else>
+            <router-view />
+        </general-page-layout>
+    </fragment>
+</template>
