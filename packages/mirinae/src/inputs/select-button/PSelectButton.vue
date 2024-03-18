6d90f5686b776d4b92b4207e97f050056cb6680b
sulmo <sulmo@megazone.com>
2024-03-18 18:35:03 +0900

    feat: add icon layout spec to PSelectButton
    Signed-off-by: sulmo <sulmo@megazone.com>

--- packages/mirinae/src/inputs/select-button/PSelectButton.vue
+++ packages/mirinae/src/inputs/select-button/PSelectButton.vue
@@ -1,26 +1,51 @@
 <template>
-    <button class="p-select-button" :class="{selected: isSelected, [styleType]: true, [size]: true}"
+    <button class="p-select-button"
+            :class="{selected: isSelected, [styleType]: disabled ? false : true, [state.size]: true, disabled}"
             @click="onClick"
             v-on="$listeners"
     >
-        <slot />
+        <p-i v-if="layout === SELECT_BUTTON_LAYOUT_TYPE.ICON_ONLY"
+             :color="state.color"
+             :width="state.iconSize"
+             :height="state.iconSize"
+             :name="iconName"
+        />
+        <slot v-else />
     </button>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
+import { computed, defineComponent, reactive } from 'vue';
 
+
+
+import PI from '@/foundation/icons/PI.vue';
 import type { SelectProps } from '@/hooks/select';
 import { useSelect } from '@/hooks/select';
-import { SELECT_BUTTON_SIZE, SELECT_BUTTON_STYLE_TYPE } from '@/inputs/select-button/config';
+import type {
+    SelectButtonLayoutType,
+    SelectButtonSize,
+    SelectButtonStyleType,
+} from '@/inputs/select-button/config';
+import {
+    SELECT_BUTTON_LAYOUT_TYPE,
+    SELECT_BUTTON_SIZE,
+    SELECT_BUTTON_STYLE_TYPE,
+} from '@/inputs/select-button/config';
+
+import { gray, white } from '@/styles/colors.cjs';
+
 
 interface Props extends SelectProps {
-    styleType?: SELECT_BUTTON_STYLE_TYPE;
-    size?: SELECT_BUTTON_SIZE;
+    layout?: SelectButtonLayoutType;
+    styleType?: SelectButtonStyleType;
+    size?: SelectButtonSize;
+    iconName?: string;
 }
 
 export default defineComponent<Props>({
     name: 'PSelectButton',
+    components: { PI },
     model: {
         prop: 'selected',
         event: 'change',
@@ -43,7 +68,18 @@ export default defineComponent<Props>({
             type: Boolean,
             default: false,
         },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
         /* select button props */
+        layout: {
+            type: String,
+            default: SELECT_BUTTON_LAYOUT_TYPE.TEXT_ONLY,
+            validator(layout: any) {
+                return Object.values(SELECT_BUTTON_LAYOUT_TYPE).includes(layout);
+            },
+        },
         styleType: {
             type: String,
             default: SELECT_BUTTON_STYLE_TYPE.secondary,
@@ -58,6 +94,10 @@ export default defineComponent<Props>({
                 return Object.values(SELECT_BUTTON_SIZE).includes(size);
             },
         },
+        iconName: {
+            type: String,
+            default: '',
+        },
     },
     setup(props, { emit }) {
         const {
@@ -68,10 +108,43 @@ export default defineComponent<Props>({
             selected: computed(() => props.selected),
             predicate: computed(() => props.predicate),
             multiSelectable: computed(() => props.multiSelectable),
+            disabled: computed(() => props.disabled),
+        });
+
+        const state = reactive({
+            size: computed(() => {
+                if (props.layout === SELECT_BUTTON_LAYOUT_TYPE.ICON_ONLY) {
+                    return props.size === SELECT_BUTTON_SIZE.md ? 'icon-md' : 'icon-sm';
+                }
+                return props.size;
+            }),
+            color: computed(() => {
+                if (props.styleType === SELECT_BUTTON_STYLE_TYPE.secondary) {
+                    if (isSelected.value) {
+                        return white;
+                    }
+                    return gray[900];
+                } if (props.styleType === SELECT_BUTTON_STYLE_TYPE.gray) {
+                    if (isSelected.value) {
+                        return white;
+                    }
+                    return gray[500];
+                }
+                return '';
+            }),
+            iconSize: computed(() => {
+                if (props.size === SELECT_BUTTON_SIZE.md) {
+                    return '1.5rem';
+                }
+                return '1rem';
+            }),
         });
 
         /* event */
         const onClick = () => {
+            if (props.disabled) {
+                return;
+            }
             const newSelected = getSelected();
             if (props.multiSelectable) {
                 emit('change', newSelected, !isSelected.value);
@@ -81,8 +154,10 @@ export default defineComponent<Props>({
         };
 
         return {
+            state,
             isSelected,
             onClick,
+            SELECT_BUTTON_LAYOUT_TYPE,
         };
     },
 });
@@ -93,6 +168,16 @@ export default defineComponent<Props>({
     @apply border rounded;
     border-width: 1px;
     border-style: solid;
+    padding: 0.25rem;
+
+    &.disabled {
+        @apply border border-solid border-gray-300;
+        cursor: not-allowed;
+        opacity: 0.5;
+        &:hover {
+            @apply bg-white;
+        }
+    }
 
     @define-mixin style-type $bg-color, $border-color, $text-color, $selected-bg-color, $selected-text-color, $hover-bg-color {
         color: $text-color;
@@ -101,6 +186,7 @@ export default defineComponent<Props>({
 
         &.selected {
             background-color: $selected-bg-color;
+            border-color: $selected-bg-color;
             color: $selected-text-color;
         }
 
@@ -111,7 +197,7 @@ export default defineComponent<Props>({
         }
     }
 
-    &.secondary { @mixin style-type transparent, theme('colors.gray.300'), theme('colors.gray.900'), theme('colors.blue.600'), theme('colors.white'), theme('colors.blue.200'); }
+    &.secondary { @mixin style-type transparent, theme('colors.gray.300'), theme('colors.gray.900'), theme('colors.blue.600'), theme('colors.white'), theme('colors.blue.200') ; }
     &.gray { @mixin style-type transparent, theme('colors.gray.500'), theme('colors.gray.500'), theme('colors.gray.500'), theme('colors.white'), theme('colors.gray.100'); }
 
     @define-mixin size $font-size, $padding-x, $padding-y, $line-height {
