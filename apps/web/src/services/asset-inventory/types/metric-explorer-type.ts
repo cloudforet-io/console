d4fc10f27fd13b0957dd500a683d13b295209bf1
Piggy Park (박용태) <samuel.park@megazone.com>
2024-03-19 10:34:31 +0900

    fix(metric-lsb): edit lsb component & update metric page store for metric lsb  (#3305)
    * fix: edit usage of lsb-collapsible-slot
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * fix: add store action for request of namespaces
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * fix: add lsbVisible to metric-explorer route
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    ---------
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/services/asset-inventory/types/metric-explorer-type.ts
+++ apps/web/src/services/asset-inventory/types/metric-explorer-type.ts
@@ -17,3 +17,8 @@ export type RelativePeriod = {
 export type Granularity = typeof GRANULARITY[keyof typeof GRANULARITY];
 
 export type Operator = typeof OPERATOR[keyof typeof OPERATOR];
+
+export interface MetricNamespace {
+    provider: string;
+    cloud_service_group: string;
+}
