bcb1d6e2117073949f80b2735d612dea987dab1b
yuda <yuda@megazone.com>
2024-03-25 18:58:34 +0900

    feat: add schema for namespace, metric, metric-data
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ apps/web/src/schema/inventory/namespace/model.ts
@@ -0,0 +1,13 @@
+import type { Tags } from '@/schema/_common/model';
+import type { NamespaceCategory } from '@/schema/inventory/namespace/type';
+
+
+export interface NamespaceModel {
+    namespace_id: string;
+    name: string;
+    category: NamespaceCategory;
+    provider: string;
+    tags: Tags;
+    domain_id: string;
+    created_at: string;
+}
