eb775f9530c9ec2e9fcded1a643440ddb7cbb5a6
Dahyun Yu <yuda@megazone.com>
2024-03-26 15:22:50 +0900

    feat: add filters popper to metric page (#3350)
    * feat: create `loadMetrics` action
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * feat: add filters popper to metric page
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ apps/web/src/services/asset-inventory/components/MetricExplorerFiltersPopper.vue
@@ -0,0 +1,180 @@
+<script setup lang="ts">
+import {
+    computed, reactive, watch,
+} from 'vue';
+
+import {
+    PSelectDropdown, PTextButton,
+} from '@spaceone/design-system';
+import type { SelectDropdownMenuItem, AutocompleteHandler } from '@spaceone/design-system/types/inputs/dropdown/select-dropdown/type';
+import { cloneDeep } from 'lodash';
+
+import { VariableModel } from '@/lib/variable-models';
+import { getVariableModelMenuHandler } from '@/lib/variable-models/variable-model-menu-handler';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+
+import { useMetricExplorerPageStore } from '@/services/asset-inventory/stores/metric-explorer-page-store';
+
+
+const props = defineProps<{
+    visible: boolean;
+}>();
+
+const GROUP_BY_TO_VAR_MODELS: Record<string, VariableModel[]> = {
+};
+const metricExplorerPageStore = useMetricExplorerPageStore();
+const metricExplorerPageState = metricExplorerPageStore.state;
+const state = reactive({
+    loading: true,
+    enabledFilters: computed<SelectDropdownMenuItem[]>(() => {
+        if (!metricExplorerPageState.enabledFiltersProperties) return [];
+        return metricExplorerPageState.enabledFiltersProperties.map((d) => {
+            if (metricExplorerPageState.groupByList.includes(d)) {
+                return { name: d, label: d };
+            }
+            return { name: d, label: d };
+        });
+    }),
+    groupByItems: computed<SelectDropdownMenuItem[]>(() => []),
+    selectedItemsMap: {} as Record<string, SelectDropdownMenuItem[]>,
+    handlerMap: computed(() => {
+        const handlerMaps = {};
+        state.groupByItems.forEach(({ name }) => {
+            handlerMaps[name] = getMenuHandler(name);
+        });
+        return handlerMaps;
+    }),
+});
+
+/* Util */
+const getMenuHandler = (groupBy: string): AutocompleteHandler => {
+    try {
+        let variableModels: VariableModel|VariableModel[] = GROUP_BY_TO_VAR_MODELS[groupBy];
+        if (!variableModels) {
+            variableModels = new VariableModel(({
+                type: 'RESOURCE_VALUE',
+                resource_type: 'inventory.CloudService',
+                reference_key: groupBy,
+                name: groupBy,
+            }));
+        }
+        const handler = getVariableModelMenuHandler(variableModels);
+
+        return async (...args) => {
+            if (!groupBy) return { results: [] };
+            try {
+                state.loading = true;
+                const results = await handler(...args);
+                return results;
+            } catch (e) {
+                ErrorHandler.handleError(e);
+                return { results: [] };
+            } finally {
+                state.loading = false;
+            }
+        };
+    } catch (e) {
+        ErrorHandler.handleError(e);
+        return async () => ({ results: [] });
+    }
+};
+const initSelectedFilters = () => {
+    const _filters = metricExplorerPageState.filters;
+    const _selectedItemsMap = {};
+    Object.keys(_filters ?? {}).forEach((groupBy) => {
+        _selectedItemsMap[groupBy] = _filters?.[groupBy].map((d) => ({ name: d })) ?? [];
+        if (metricExplorerPageState.enabledFiltersProperties?.indexOf(groupBy) === -1) {
+            metricExplorerPageStore.setEnabledFiltersProperties([
+                ...(metricExplorerPageState.enabledFiltersProperties ?? []),
+                groupBy,
+            ]);
+        }
+    });
+    state.selectedItemsMap = _selectedItemsMap;
+};
+
+/* Event */
+const handleUpdateFiltersDropdown = (groupBy: string, selectedItems: SelectDropdownMenuItem[]) => {
+    const selectedItemsMap = cloneDeep(state.selectedItemsMap);
+    selectedItemsMap[groupBy] = selectedItems;
+    state.selectedItemsMap = selectedItemsMap;
+
+    metricExplorerPageStore.setFilters({
+        ...metricExplorerPageState.filters,
+        [groupBy]: selectedItems.map((d) => d.name as string),
+    });
+};
+const handleClickResetFilters = () => {
+    state.selectedItemsMap = {};
+    metricExplorerPageStore.setFilters({});
+    // TODO: get saved filters if exists
+};
+
+watch(() => props.visible, (visible) => {
+    if (!visible) return;
+    initSelectedFilters();
+}, { immediate: true });
+
+</script>
+
+<template>
+    <div class="metric-explorer-filters-popper">
+        <p-select-dropdown
+            v-for="groupBy in state.enabledFilters"
+            :key="`filters-dropdown-${groupBy.name}`"
+            class="filters-popper-dropdown"
+            is-filterable
+            :handler="state.handlerMap[groupBy.name]"
+            :selected="state.selectedItemsMap[groupBy.name] ?? []"
+            :loading="state.loading"
+            multi-selectable
+            style-type="rounded"
+            appearance-type="badge"
+            show-select-marker
+            use-fixed-menu-style
+            selection-highlight
+            :init-selected-with-handler="props.visible && !!GROUP_BY_TO_VAR_MODELS[groupBy.name]"
+            :selection-label="groupBy.label"
+            :show-delete-all-button="false"
+            :page-size="10"
+            @update:selected="handleUpdateFiltersDropdown(groupBy.name, $event)"
+        />
+        <!--add more button-->
+        <p-text-button icon-left="ic_refresh"
+                       style-type="highlight"
+                       class="reset-button"
+                       @click="handleClickResetFilters"
+        >
+            {{ $t('INVENTORY.METRIC_EXPLORER.RESET') }}
+        </p-text-button>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+.metric-explorer-filters-popper {
+    @apply flex items-center flex-wrap;
+    flex: 1;
+    gap: 0.5rem;
+    .filters-popper-dropdown {
+        width: initial;
+    }
+
+    .reset-button {
+        display: inline-block;
+        vertical-align: middle;
+        padding: 0.5rem 0;
+    }
+}
+
+/* custom design-system component - p-context-menu */
+:deep(.p-context-menu) {
+    /*
+        CAUTION:
+        When the parent has a specific style attribute called 'transform,' 'fixed' behaves like 'absolute,' causing the context-menu's top position not to work correctly,
+        so it is manually forced to be specified.
+    */
+    top: initial !important;
+    left: initial !important;
+}
+</style>
