da6a39db7883dbbe1281fec03ac4be0059173dbc
Yongtae Park <samuel.park@megazone.com>
2024-03-26 14:53:31 +0900

    feat: create add-cluster step 2
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/services/asset-inventory/components/ServiceAccountAddClusterModal.vue
+++ apps/web/src/services/asset-inventory/components/ServiceAccountAddClusterModal.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { computed, reactive, watch } from 'vue';
 
 import {
-    PIconModal, PButton, PProgressBar, PFieldGroup, PTextInput, PI,
+    PIconModal, PButton, PProgressBar, PFieldGroup, PTextInput, PI, PTextButton,
 } from '@spaceone/design-system';
 
 import { useProxyValue } from '@/common/composables/proxy-state';
 
 import { violet } from '@/styles/colors';
+
+import ServiceAccountAddClusterScriptField from '@/services/asset-inventory/components/ServiceAccountAddClusterScriptField.vue';
 
 interface Props {
     visible: boolean;
@@ -23,13 +25,67 @@ const emit = defineEmits<{(e: 'update:visible'): void;
 
 const state = reactive({
     proxyVisible: useProxyValue('visible', props, emit),
-    clusterName: '',
+    step: 1,
 });
 
-// const handleClickButton = () => {
-//     state.proxyVisible = false;
-//     emit('close');
-// };
+const formState = reactive({
+    clusterName: '',
+    firstStepValid: computed(() => formState.clusterName.length > 0),
+    commonValidForDelay: false,
+    isValid: computed(() => {
+        if (state.step === 1) return formState.firstStepValid;
+        // TODO: apply loading state
+        return true;
+    }),
+});
+
+const scriptState = reactive({
+    firstScript: computed(() => 'helm version\n'),
+    secondScript: computed(() => 'helm repo add sp1-agent https://github.com.~\nhelm repo update sp1-agent\n'),
+});
+
+
+/* Event */
+const handleClickContinueButton = () => {
+    if (!formState.commonValidForDelay) return;
+    formState.commonValidForDelay = false;
+
+    if (state.step === 1) {
+        goStep();
+    } else if (state.step === 2) {
+        // TODO: Generate API key
+        // TODO: Generate Script
+        // TODO: Generate App Name
+        goStep();
+    } else if (state.step === 3) {
+        // TODO: Do something
+        closeModal();
+        state.step = 1;
+    }
+};
+const handleClickNotNowButton = () => {
+    closeModal();
+};
+const handleClickPreviousButton = () => {
+    goStep(-1);
+};
+
+/* Method */
+const closeModal = () => {
+    state.proxyVisible = false;
+    emit('close');
+};
+const goStep = (n?: number) => {
+    if (n === -1) state.step -= 1;
+    else if (n) state.step = n;
+    else state.step += 1;
+};
+
+watch(() => state.step, () => {
+    setTimeout(() => {
+        formState.commonValidForDelay = true;
+    }, 1500);
+}, { immediate: true });
 
 </script>
 
@@ -47,16 +103,17 @@ const state = reactive({
         <template #body>
             <div class="cluster-content-wrapper">
                 <div class="add-cluster-form">
-                    <p-progress-bar class="progress-bar"
-                                    :percentage="100/3"
+                    <p-progress-bar :percentage="(state.step/3)*100"
                                     :color="violet[400]"
                     />
-                    <div class="first-section">
+                    <div v-if="state.step === 1"
+                         class="first-section"
+                    >
                         <p-field-group :label="$t('Kubernetes Cluster Name')"
                                        required
                         >
                             <p-text-input class="cluster-name-input"
-                                          :value.sync="state.clusterName"
+                                          :value.sync="formState.clusterName"
                                           :placeholder="$t('Cluster Name')"
                             />
                         </p-field-group>
@@ -72,15 +129,41 @@ const state = reactive({
                             </span>
                         </div>
                     </div>
+                    <div v-else-if="state.step === 2"
+                         class="second-section"
+                    >
+                        <div class="script-wrapper">
+                            <service-account-add-cluster-script-field :script="scriptState.firstScript"
+                                                                      :description="$t('INVENTORY.SERVICE_ACCOUNT.CLUSTER_MODAL.SCRIPT_GUIDE_FIRST')"
+                                                                      script-height="3.5rem"
+                            />
+                            <service-account-add-cluster-script-field :script="scriptState.secondScript"
+                                                                      :description="$t('INVENTORY.SERVICE_ACCOUNT.CLUSTER_MODAL.SCRIPT_GUIDE_SECOND')"
+                                                                      script-height="5rem"
+                            />
+                        </div>
+                        <p-text-button class="previous-button"
+                                       style-type="highlight"
+                                       icon-left="ic_arrow-left"
+                                       @click="handleClickPreviousButton"
+                        >
+                            {{ $t('INVENTORY.SERVICE_ACCOUNT.CLUSTER_MODAL.PREVIOUS_BUTTON') }}
+                        </p-text-button>
+                    </div>
                 </div>
                 <div class="button-wrapper">
-                    <p-button class="continue-button"
+                    <p-button style-type="substitutive"
                               icon-right="ic_arrow-right"
+                              :disabled="!formState.isValid || !formState.commonValidForDelay"
+                              @click="handleClickContinueButton"
                     >
-                        Continue
+                        {{ $t('INVENTORY.SERVICE_ACCOUNT.CLUSTER_MODAL.CONTINUE_BUTTON') }}
                     </p-button>
-                    <p-button style-type="transparent">
-                        Not now
+                    <p-button v-if="state.step === 1"
+                              style-type="transparent"
+                              @click="handleClickNotNowButton"
+                    >
+                        {{ $t('INVENTORY.SERVICE_ACCOUNT.CLUSTER_MODAL.NOT_NOW_BUTTON') }}
                     </p-button>
                 </div>
             </div>
@@ -89,7 +172,6 @@ const state = reactive({
 </template>
 
 <style lang="postcss" scoped>
-
 .service-account-add-cluster-modal {
     .title {
         @apply text-violet-500 text-display-md font-bold;
@@ -103,16 +185,14 @@ const state = reactive({
         text-align: left;
 
         .add-cluster-form {
-            @apply relative bg-violet-100 rounded;
-            height: 20.875rem;
+            @apply bg-violet-100;
+            min-height: 20.875rem;
             margin-bottom: 1.5rem;
-
-            .progress-bar {
-                @apply absolute top-0;
-            }
+            overflow: hidden;
+            border-radius: 0.375rem;
 
             .first-section {
-                padding: 2.5rem 1rem;
+                padding: 2.125rem 1rem 2.5rem;
 
                 .cluster-name-input {
                     @apply w-full;
@@ -129,13 +209,16 @@ const state = reactive({
                     }
                 }
             }
+            .second-section {
+                padding: 2.125rem 1rem 1.5rem;
+                .script-wrapper {
+                    padding-bottom: 1rem;
+                }
+            }
         }
 
         .button-wrapper {
             @apply flex flex-col gap-2;
-            .continue-button {
-                @apply bg-violet-400;
-            }
         }
     }
 }
