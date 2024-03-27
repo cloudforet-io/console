04688d74e7b94e79850cc22076a1b6bfcb66fc04
sulmo <sulmo@megazone.com>
2024-03-27 15:58:32 +0900

    feat: update scope badge style
    Signed-off-by: sulmo <sulmo@megazone.com>
    
    fix: same
    
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/services/asset-inventory/helpers/dynamic-ui-schema-generator/dynamic-layout-schema-template.ts
+++ apps/web/src/services/asset-inventory/helpers/dynamic-ui-schema-generator/dynamic-layout-schema-template.ts
@@ -24,8 +24,8 @@ export const getDefaultDetailSchema = (fields: DynamicField[], isTrustedAccount:
                             name: 'Workspace',
                             type: 'badge',
                             options: {
-                                text_color: 'violet.600',
-                                background_color: 'violet.200',
+                                text_color: 'green.700',
+                                background_color: 'green.200',
                             },
                         },
                         DOMAIN: {
@@ -99,8 +99,8 @@ export const getDefaultTableSchema = (dynamicFields: DynamicField[], options: {
                                 name: 'Workspace',
                                 type: 'badge',
                                 options: {
-                                    text_color: 'violet.600',
-                                    background_color: 'violet.200',
+                                    text_color: 'green.700',
+                                    background_color: 'green.200',
                                 },
                             },
                             DOMAIN: {
