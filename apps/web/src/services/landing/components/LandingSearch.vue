c2777fc08a29c0b04e9dd3a056f3137a16b9839a
nayeongkim <nayeongkim@megazone.com>
2024-04-03 11:12:19 +0900

    refactor: separation of 'search area' and modification of banner layout
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

---
+++ apps/web/src/services/landing/components/LandingSearch.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { computed, reactive } from 'vue';
+
+import { PSearch } from '@spaceone/design-system';
+
+import type { WorkspaceModel } from '@/schema/identity/workspace/model';
+
+interface Props {
+    workspaceList?: WorkspaceModel[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    workspaceList: () => ([]),
+});
+
+const state = reactive({
+    searchText: '',
+    menuList: computed(() => props.workspaceList),
+});
+</script>
+
+<template>
+    <div class="landing-search">
+        <p-search v-model="state.searchText"
+                  :placeholder="$t('LADING.SEARCH_WORKSPACE')"
+        />
+    </div>
+</template>
+
+<style scoped lang="postcss">
+.landing-search {
+    @apply flex flex-col;
+}
+</style>
