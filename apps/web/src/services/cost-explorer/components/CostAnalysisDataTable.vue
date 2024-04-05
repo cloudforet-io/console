c24c0310a2197e95a3adc12d3645ad5bbef9e22f
Dahyun Yu <yuda@megazone.com>
2024-04-05 16:52:58 +0900

    fix: show document title as 'SpaceONE' when there's no domain name (#3449)
    * fix: show document title as 'SpaceONE' when there's no domain name
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * fix: show refined workspace name in cost analysis excel
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/cost-explorer/components/CostAnalysisDataTable.vue
+++ apps/web/src/services/cost-explorer/components/CostAnalysisDataTable.vue
@@ -137,6 +137,7 @@ const tableState = reactive({
         fields.push(...tableState.costFields);
         return fields.map((d) => {
             const field: ExcelDataField = { key: d.name, name: (d.label) ?? '' };
+            if (d.name === GROUP_BY.WORKSPACE) field.reference = { reference_key: 'workspace_id', resource_type: 'identity.Workspace' };
             if (d.name === GROUP_BY.PROJECT) field.reference = { reference_key: 'project_id', resource_type: 'identity.Project' };
             if (d.name === GROUP_BY.SERVICE_ACCOUNT) field.reference = { reference_key: 'service_account_id', resource_type: 'identity.ServiceAccount' };
             if (d.name === GROUP_BY.REGION) field.reference = { reference_key: 'region_code', resource_type: 'inventory.Region' };
