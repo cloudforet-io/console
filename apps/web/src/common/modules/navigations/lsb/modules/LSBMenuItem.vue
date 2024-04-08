dbb964f8d3d03c0af19b1742cbb6768d863d63e5
NaYeong,Kim <nayeongkim@megazone.com>
2024-04-08 14:26:50 +0900

    refactor: componentization of menu items and addition of button-title type (#3453)
    * chore: change starred icon css style
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * refactor: refactor dashboard LSB
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * refactor: componentization of menu items and addition of button-title type
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/common/modules/navigations/lsb/modules/LSBMenuItem.vue
+++ apps/web/src/common/modules/navigations/lsb/modules/LSBMenuItem.vue
@@ -3,16 +3,15 @@ import {
     computed, reactive,
 } from 'vue';
 
-import { PI, PSelectDropdown, PLazyImg } from '@spaceone/design-system';
-
 import { useAppContextStore } from '@/store/app-context/app-context-store';
 
-import { assetUrlConverter } from '@/lib/helper/asset-helper';
-
+import LSBButtonTitleMenuItem from '@/common/modules/navigations/lsb/modules/LSBButtonTitleMenuItem.vue';
 import LSBCollapsibleMenuItem from '@/common/modules/navigations/lsb/modules/LSBCollapsibleMenuItem.vue';
 import LSBDividerMenuItem from '@/common/modules/navigations/lsb/modules/LSBDividerMenuItem.vue';
+import LSBDropdownMenuItem from '@/common/modules/navigations/lsb/modules/LSBDropdownMenuItem.vue';
 import LSBRouterMenuItem from '@/common/modules/navigations/lsb/modules/LSBRouterMenuItem.vue';
 import LSBStarredMenuItem from '@/common/modules/navigations/lsb/modules/LSBStarredMenuItem.vue';
+import LSBTopTitleMenuItem from '@/common/modules/navigations/lsb/modules/LSBTopTitleMenuItem.vue';
 import type { LSBItem, LSBMenu } from '@/common/modules/navigations/lsb/type';
 import { MENU_ITEM_TYPE } from '@/common/modules/navigations/lsb/type';
 
@@ -32,15 +31,8 @@ const appContextStore = useAppContextStore();
 const state = reactive({
     isAdminMode: computed(() => appContextStore.getters.isAdminMode),
     processedMenuData: computed<LSBMenu>(() => (Array.isArray(props.menuData) ? props.menuData : [props.menuData])),
-    isFolded: false,
-    isFoldableMenu: computed(() => state.processedMenuData?.some((item) => item.foldable)),
-    showMenu: computed(() => (state.isFoldableMenu && !state.isFolded) || !state.isFoldableMenu), // toggle menu
     hoveredItem: '',
 });
-
-const handleFoldableToggle = () => {
-    state.isFolded = !state.isFolded;
-};
 
 const handleSelect = (id: string, selected: string) => {
     emit('select', id, selected);
@@ -54,55 +46,26 @@ const handleSelect = (id: string, selected: string) => {
              :key="item.id"
              class="l-s-b-menu-item"
         >
-            <p v-if="item.type === MENU_ITEM_TYPE.TITLE"
-               class="title-wrapper"
+            <l-s-b-top-title-menu-item v-if="item.type === MENU_ITEM_TYPE.TOP_TITLE"
+                                       :item="item"
+            />
+            <l-s-b-button-title-menu-item v-if="item.type === MENU_ITEM_TYPE.BUTTON_TITLE"
+                                          :item="item"
             >
-                <span v-if="item.foldable"
-                      class="toggle-button"
-                      @click="handleFoldableToggle"
-                >
-                    <p-i width="1rem"
-                         height="1rem"
-                         name="ic_chevron-down"
-                         color="inherit transparent"
-                         class="arrow-button"
-                         :class="{ 'is-Folded': state.isFolded }"
-                    />
-                    <span class="title">
-                        {{ item.label }}
-                    </span>
-                </span>
-            </p>
-            <p v-else-if="item.type === MENU_ITEM_TYPE.TOP_TITLE"
-               class="top-title-wrapper"
-            >
-                <p-lazy-img
-                    v-if="item.titleIcon"
-                    :src="assetUrlConverter(item.titleIcon)"
-                    width="1.5rem"
-                    height="1.5rem"
-                    class="top-title-image"
-                />
-                <span class="top-title">
-                    {{ item.label }}
-                </span>
-                <p-i v-if="item.icon"
-                     :name="item.icon.name"
-                     class="top-title-icon"
-                     width="0.75rem"
-                     height="0.75rem"
-                     :color="item.icon.color"
-                />
-            </p>
-            <div v-else-if="item.type === MENU_ITEM_TYPE.DROPDOWN"
-                 class="select-options-wrapper"
-            >
-                <p-select-dropdown class="select-options-dropdown"
-                                   :menu="item.selectOptions.items"
-                                   :selected="item.selectOptions.defaultSelected"
-                                   @update:selected="handleSelect(item.id, $event)"
-                />
-            </div>
+                <template #extra>
+                    <slot name="button-title-extra" />
+                </template>
+            </l-s-b-button-title-menu-item>
+
+            <l-s-b-divider-menu-item v-else-if="item.type === MENU_ITEM_TYPE.DIVIDER" />
+
+            <l-s-b-dropdown-menu-item v-else-if="item.type === MENU_ITEM_TYPE.DROPDOWN"
+                                      :item="item"
+                                      @select="handleSelect"
+            />
+            <l-s-b-starred-menu-item v-else-if="item.type === MENU_ITEM_TYPE.STARRED"
+                                     :item="item"
+            />
             <l-s-b-collapsible-menu-item v-else-if="item.type === MENU_ITEM_TYPE.COLLAPSIBLE"
                                          :item="item"
             >
@@ -114,11 +77,7 @@ const handleSelect = (id: string, selected: string) => {
                     />
                 </template>
             </l-s-b-collapsible-menu-item>
-            <l-s-b-starred-menu-item v-else-if="item.type === MENU_ITEM_TYPE.STARRED"
-                                     :item="item"
-            />
-            <l-s-b-divider-menu-item v-else-if="item.type === MENU_ITEM_TYPE.DIVIDER && state.showMenu" />
-            <l-s-b-router-menu-item v-else-if="item.type === MENU_ITEM_TYPE.ITEM && state.showMenu"
+            <l-s-b-router-menu-item v-else-if="item.type === MENU_ITEM_TYPE.ITEM"
                                     :item="item"
                                     :depth="depth"
                                     :is-admin-mode="state.isAdminMode"
@@ -136,48 +95,3 @@ const handleSelect = (id: string, selected: string) => {
         </div>
     </div>
 </template>
-
-<style lang="postcss" scoped>
-.l-s-b-menu-list {
-    .title-wrapper {
-        @apply text-gray-400 font-bold inline-flex items-center;
-        font-size: 0.75rem;
-        line-height: 125%;
-        padding-top: 0.25rem;
-        padding-right: 0.25rem;
-        padding-left: 0.25rem;
-        .toggle-button {
-            @apply text-gray-600 cursor-pointer;
-            &:hover {
-                @apply text-gray-600 underline;
-            }
-            .arrow-button {
-                transition: transform 0.3s ease-in-out;
-                &.is-Folded {
-                    transform: rotate(-90deg);
-                }
-            }
-        }
-    }
-    .top-title-wrapper {
-        @apply text-label-md font-bold inline-flex items-center;
-        height: 2rem;
-        margin-top: 0.25rem;
-        padding-right: 0.5rem;
-        padding-bottom: 0.5rem;
-        padding-left: 0.5rem;
-        .top-title-icon {
-            margin-left: 0.25rem;
-        }
-        .top-title-image {
-            margin-right: 0.25rem;
-        }
-    }
-    .select-options-wrapper {
-        padding: 0 0.5rem;
-        .select-options-dropdown {
-            @apply w-full;
-        }
-    }
-}
-</style>
