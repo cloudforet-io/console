dffbbd52aa35effecfb93f545690047b8990dac0
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2024-04-09 10:33:28 +0900

    feat: create workspace home base
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

---
+++ apps/web/src/services/workspace-home/components/UserConfigs.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="user-configs">
+        user-configs
+    </div>
+</template>
---
+++ apps/web/src/services/workspace-home/components/WorkspaceInfo.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="workspace-info">
+        workspace-info
+    </div>
+</template>
