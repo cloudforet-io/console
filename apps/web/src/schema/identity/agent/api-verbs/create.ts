04ceadba7367be70023f74c33cee1b419ae0dbbf
Yongtae Park <samuel.park@megazone.com>
2024-04-09 16:37:10 +0900

    feat(schema): create agent resource schema
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/schema/identity/agent/api-verbs/create.ts
@@ -0,0 +1,4 @@
+export interface AgentCreateParameters {
+    service_account_id: string;
+    options: Record<string, any>;
+}
---
+++ apps/web/src/schema/identity/agent/model.ts
@@ -0,0 +1,25 @@
+import type { Tags } from '@/schema/_common/model';
+import type { AppStatusType } from '@/schema/identity/app/type';
+
+export interface AgentModel {
+    agent_id: string;
+    options: Record<string, any>;
+    client_secret?: string;
+    name: string;
+    state: AppStatusType;
+    is_managed: boolean;
+    tags: Tags;
+    client_id: string;
+    role_id: string;
+    app_id: string;
+    service_account_id: string;
+    workspace_id: string;
+    domain_id: string;
+    created_at: string;
+    expired_at: string;
+    last_accessed_at: string;
+}
+
+/* options
+*
+* */
