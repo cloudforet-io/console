6f785ea04af12a3a45c5c2b2dfc5392331bad9ea
sulmo <sulmo@megazone.com>
2024-04-11 16:27:37 +0900

    fix: fix multiple click bug
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/services/asset-inventory/components/ServiceAccountAttachedGeneralAccounts.vue
+++ apps/web/src/services/asset-inventory/components/ServiceAccountAttachedGeneralAccounts.vue
@@ -53,6 +53,7 @@ const emit = defineEmits<{(e: 'update:attached-general-accounts', attachedGenera
 const state = reactive({
     project: computed(() => allReferenceStore.getters.project),
     loading: true,
+    syncReqLoading: false,
     items: [] as any,
     sortBy: 'name',
     sortDesc: true,
@@ -136,6 +137,7 @@ const handleSort = async (sortBy, sortDesc) => {
 
 const handleSync = async () => {
     try {
+        state.syncReqLoading = true;
         await SpaceConnector.clientV2.identity.trustedAccount.sync({
             trusted_account_id: serviceAccountPageStore.state.originServiceAccountItem.trusted_account_id,
         });
@@ -146,6 +148,7 @@ const handleSync = async () => {
         });
     } finally {
         if (state.trustedAccountId) await serviceAccountPageStore.fetchSyncJobList(state.trustedAccountId);
+        state.syncReqLoading = false;
     }
 };
 
@@ -196,7 +199,7 @@ watch(() => state.trustedAccountId, async (ta) => {
             <template #extra>
                 <p-button v-if="state.isSyncEnabled"
                           style-type="secondary"
-                          :loading="state.isSyncing || state.loading"
+                          :loading="state.isSyncing || state.syncReqLoading"
                           @click="handleSync"
                 >
                     {{ $t('INVENTORY.SERVICE_ACCOUNT.DETAIL.SYNC_NOW') }}
