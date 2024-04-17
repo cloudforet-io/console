637c9211549ed75629337300f3a10f5d16a02c15
Piggy Park (박용태) <samuel.park@megazone.com>
2024-04-17 15:06:12 +0900

    feat(custom-metric): apply create api & add unit field (#3530)
    * feat(custom-metric): apply create api & add unit field
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * chore: add translation
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * fix: apply review
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * fix: apply review
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    ---------
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/services/asset-inventory/components/AddCustomMetricModal.vue
+++ apps/web/src/services/asset-inventory/components/AddCustomMetricModal.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 
-import { reactive } from 'vue';
+import { computed, reactive } from 'vue';
 
 import {
     PButtonModal, PFieldGroup, PTextInput, PTextEditor,
 } from '@spaceone/design-system';
 
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+
+import type { MetricCreateParameters } from '@/schema/inventory/metric/api-verbs/create';
+import { METRIC_TYPE } from '@/schema/inventory/metric/constant';
+import type { MetricModel } from '@/schema/inventory/metric/model';
+import { i18n } from '@/translations';
+
+import { showSuccessMessage } from '@/lib/helper/notice-alert-helper';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
 import { useFormValidator } from '@/common/composables/form-validator';
 import { useProxyValue } from '@/common/composables/proxy-state';
+
+import { useMetricExplorerPageStore } from '@/services/asset-inventory/stores/metric-explorer-page-store';
 
 interface Props {
     visible?: boolean;
@@ -18,24 +30,37 @@ const props = withDefaults(defineProps<Props>(), {
 });
 
 const emit = defineEmits<{(e: 'update:visible', value: boolean): void}>();
+const metricExplorerPageStore = useMetricExplorerPageStore();
+
+const storeState = reactive({
+    metricNameList: computed(() => metricExplorerPageStore.state.metricList.map((metric) => metric.name)),
+});
 
 const state = reactive({
     proxyVisible: useProxyValue('visible', props, emit),
-    title: 'Add Custom Metric with Query',
 });
 
 const {
     forms: {
         name,
+        unit,
         code,
     },
-    // invalidState,
-    // invalidTexts,
-    // setForm, isAllValid,
+    invalidState,
+    invalidTexts,
+    isAllValid,
+    setForm,
+    initForm,
 } = useFormValidator({
     name: '',
-    code: '{}',
+    unit: '',
+    code: '',
 }, {
+    name: (value: string) => {
+        if (!value.length) return i18n.t('INVENTORY.METRIC_EXPLORER.CUSTOM_METRIC.REQUIRED_FIELD');
+        if (storeState.metricNameList.includes(value)) return i18n.t('INVENTORY.METRIC_EXPLORER.CUSTOM_METRIC.DUPLICATED');
+        return true;
+    },
     code: (value: string) => {
         try {
             JSON.parse(value);
@@ -48,29 +73,65 @@ const {
 
 
 /* Event */
-const handleClose = () => { state.proxyVisible = false; };
+const handleClose = () => {
+    initForm();
+    state.proxyVisible = false;
+};
+
+const handleConfirm = async () => {
+    try {
+        const jsonParsedQuery = JSON.parse(code.value.trim());
+        const createParameters: MetricCreateParameters = {
+            name: name.value,
+            unit: unit.value,
+            metric_type: METRIC_TYPE.GAUGE,
+            resource_type: 'inventory.CloudService',
+            query_options: jsonParsedQuery,
+            namespace_id: metricExplorerPageStore.state.selectedNamespace?.namespace_id || '',
+        };
+        await SpaceConnector.clientV2.inventory.metric.create<MetricCreateParameters, MetricModel>(createParameters);
+        initForm();
+        state.proxyVisible = false;
+        showSuccessMessage(i18n.t('INVENTORY.METRIC_EXPLORER.CUSTOM_METRIC.SUCCESSFULLY_ADDED'), '');
+    } catch (e) {
+        ErrorHandler.handleError(e);
+    }
+};
 
 </script>
 
 <template>
     <p-button-modal class="add-custom-metric-modal"
                     :visible.sync="state.proxyVisible"
-                    :header-title="state.title"
+                    :header-title="$t('INVENTORY.METRIC_EXPLORER.CUSTOM_METRIC.ADD_TITLE')"
                     size="lg"
-                    @confirm="handleClose"
+                    :disabled="!isAllValid"
+                    @confirm="handleConfirm"
                     @cancel="handleClose"
                     @close="handleClose"
     >
         <template #body>
-            <p-field-group label="name"
+            <p-field-group :label="$t('INVENTORY.METRIC_EXPLORER.CUSTOM_METRIC.NAME')"
                            required
+                           :invalid="invalidState.name"
+                           :invalid-text="invalidTexts.name"
             >
-                <p-text-input v-model="name"
-                              class="custom-metric-name-input"
+                <p-text-input :value="name"
+                              :invalid="invalidState.name"
+                              @update:value="setForm('name', $event)"
                 />
             </p-field-group>
-            <p-field-group required>
-                <p-text-editor :code.sync="code" />
+            <p-field-group :label="$t('INVENTORY.METRIC_EXPLORER.CUSTOM_METRIC.UNIT')">
+                <p-text-input :value="unit"
+                              @update:value="setForm('unit', $event)"
+                />
+            </p-field-group>
+            <p-field-group class="query-field"
+                           required
+            >
+                <p-text-editor :code="code"
+                               @update:code="setForm('code', $event)"
+                />
             </p-field-group>
         </template>
     </p-button-modal>
@@ -78,8 +139,22 @@ const handleClose = () => { state.proxyVisible = false; };
 
 <style scoped lang="postcss">
 .add-custom-metric-modal {
-    .custom-metric-name-input {
-        @apply w-full;
+    :deep(.p-text-input) {
+        @apply w-5/12;
+        .input-container {
+            @apply w-full;
+        }
+    }
+
+    .query-field {
+        @apply border rounded-lg;
+        overflow: hidden;
+
+        :deep(.p-text-editor) {
+            .CodeMirror {
+                height: 31rem;
+            }
+        }
     }
 }
 </style>
