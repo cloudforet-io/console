a51b4cb7285a32ac3c0b8f460ea9456d3dc386aa
Yongtae Park <samuel.park@megazone.com>
2024-04-18 16:36:24 +0900

    feat(project-card): create project group card
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/services/project/components/ProjectMainProjectGroupCard.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+
+import { useRouter } from 'vue-router/composables';
+
+import { PI } from '@spaceone/design-system';
+
+import { useProperRouteLocation } from '@/common/composables/proper-route-location';
+import FavoriteButton from '@/common/modules/favorites/favorite-button/FavoriteButton.vue';
+import { FAVORITE_TYPE } from '@/common/modules/favorites/favorite-button/type';
+
+import { indigo } from '@/styles/colors';
+
+import { PROJECT_ROUTE } from '@/services/project/routes/route-constant';
+import type { ProjectCardItemType } from '@/services/project/types/project-type';
+
+interface Props {
+    item: ProjectCardItemType;
+}
+
+const props = defineProps<Props>();
+const router = useRouter();
+const { getProperRouteLocation } = useProperRouteLocation();
+const handleSelectProjectGroup = () => {
+    router.push(getProperRouteLocation({
+        name: PROJECT_ROUTE._NAME,
+        params: {
+            projectGroupId: props.item.id as string,
+        },
+    }));
+};
+</script>
+
+<template>
+    <div class="project-main-project-group-card"
+         @click="handleSelectProjectGroup"
+    >
+        <div class="project-group-item">
+            <p-i name="ic_folder-filled"
+                 :color="indigo[500]"
+                 width="1rem"
+                 height="1rem"
+            />
+            <span class="project-group-name">{{ props.item.name }}</span>
+        </div>
+        <favorite-button :item-id="props.item.id"
+                         :favorite-type="FAVORITE_TYPE.PROJECT_GROUP"
+                         scale="0.8"
+                         class="favorite-button"
+        />
+    </div>
+</template>
+
+<style scoped lang="postcss">
+.project-main-project-group-card {
+    @apply flex items-center justify-between bg-white border border-gray-200 rounded-lg cursor-pointer;
+    height: 2.625rem;
+    padding: 0.75rem;
+
+    .project-group-item {
+        @apply flex gap-1;
+
+        .project-group-name {
+            @apply text-label-md text-gray-900;
+        }
+    }
+
+    .favorite-button {
+        display: none;
+
+        &.starred {
+            display: block;
+        }
+    }
+
+    &:hover {
+        @apply bg-blue-100;
+
+        .favorite-button {
+            display: block;
+        }
+    }
+}
+</style>
