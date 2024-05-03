27e0b40ff7903f8d2b9c783deb725845664c30a8
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2024-05-03 11:35:43 +0900

    feat: apply heading
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/services/cost-explorer/pages/admin/AdminDataSourcePage.vue
+++ apps/web/src/services/cost-explorer/pages/admin/AdminDataSourcePage.vue
@@ -1,5 +1,41 @@
 <script setup lang="ts">
-import { PHeading } from '@spaceone/design-system';
+import { computed, onMounted, reactive } from 'vue';
+
+import { PHeading, PLink } from '@spaceone/design-system';
+
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+
+import type { ListResponse } from '@/schema/_common/api-verbs/list';
+import type { CostDataSourceListParameters } from '@/schema/cost-analysis/data-source/api-verbs/list';
+import type { DataSourceModel } from '@/schema/monitoring/data-source/model';
+import { store } from '@/store';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+
+
+const storeState = reactive({
+    language: computed(() => store.state.user.language),
+});
+const state = reactive({
+    dataSourceList: [] as DataSourceModel[],
+    totalCount: 0,
+});
+
+const fetchDataSourceList = async () => {
+    try {
+        const { results, total_count } = await SpaceConnector.clientV2.costAnalysis.dataSource.list<CostDataSourceListParameters, ListResponse<DataSourceModel>>();
+        state.dataSourceList = results || [];
+        state.totalCount = total_count || 0;
+    } catch (e) {
+        ErrorHandler.handleError(e);
+        state.dataSourceList = [];
+        state.totalCount = 0;
+    }
+};
+
+onMounted(() => {
+    fetchDataSourceList();
+});
 </script>
 
 <template>
@@ -7,9 +43,29 @@ import { PHeading } from '@spaceone/design-system';
         <p-heading :title="$t('MENU.COST_EXPLORER_DATA_SOURCES')"
                    use-total-count
                    use-selected-count
-                   :total-count="2"
+                   :total-count="state.totalCount"
                    :selected-count="1"
-                   @click-back-button="$router.go(-1)"
-        />
+        >
+            <template #extra>
+                <p-link :text="$t('BILLING.COST_MANAGEMENT.DATA_SOURCES.GUIDE')"
+                        :href="`https://cloudforet.io/${storeState.language}/docs/guides/admin-mode/data-sources`"
+                        size="sm"
+                        icon-left="ic_info-circle"
+                        action-icon="external-link"
+                        new-tab
+                        class="guide-link"
+                />
+            </template>
+        </p-heading>
     </div>
 </template>
+
+<style lang="postcss" scoped>
+.admin-data-source-page {
+    /* custom design-system component - p-link */
+    :deep(.p-link) {
+        @apply flex items-center;
+        gap: 0.125rem;
+    }
+}
+</style>
