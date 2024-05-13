ba72a58f3e141b4685b813e542436e739c36596d
NaYeong,Kim <nayeongkim@megazone.com>
2024-05-13 17:29:36 +0900

    feat: apply modal (#3752)
    * chore: update translations
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * feat: apply modal
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

---
+++ apps/web/src/services/cost-explorer/components/DataSourceManagementModal.vue
@@ -0,0 +1,271 @@
+<script setup lang="ts">
+import { computed, reactive } from 'vue';
+import type { TranslateResult } from 'vue-i18n';
+
+import { PButtonModal, PSelectDropdown, PFieldTitle } from '@spaceone/design-system';
+import type { AutocompleteHandler } from '@spaceone/design-system/types/inputs/dropdown/select-dropdown/type';
+
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+import { ApiQueryHelper } from '@cloudforet/core-lib/space-connector/helper';
+
+import type { ListResponse } from '@/schema/_common/api-verbs/list';
+import type { CostDataSourceAccountResetParameters } from '@/schema/cost-analysis/data-source-account/api-verbs/reset';
+import type { CostDataSourceAccountUpdateParameters } from '@/schema/cost-analysis/data-source-account/api-verbs/update';
+import type { CostDataSourceAccountModel } from '@/schema/cost-analysis/data-source-account/model';
+import type { WorkspaceListParameters } from '@/schema/identity/workspace/api-verbs/list';
+import type { WorkspaceModel } from '@/schema/identity/workspace/model';
+import { i18n } from '@/translations';
+
+import { useUserWorkspaceStore } from '@/store/app-context/workspace/user-workspace-store';
+
+import { hideLoadingMessage, showLoadingMessage, showSuccessMessage } from '@/lib/helper/notice-alert-helper';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+import WorkspaceLogoIcon from '@/common/modules/navigations/top-bar/modules/top-bar-header/WorkspaceLogoIcon.vue';
+
+import { useDataSourcesPageStore } from '@/services/cost-explorer/stores/data-sources-page-store';
+import type { CostLinkedAccountModalType } from '@/services/cost-explorer/types/data-sources-type';
+
+const userWorkspaceStore = useUserWorkspaceStore();
+const userWorkspaceGetters = userWorkspaceStore.getters;
+const dataSourcesPageStore = useDataSourcesPageStore();
+const dataSourcesPageState = dataSourcesPageStore.state;
+const dataSourcesPageGetters = dataSourcesPageStore.getters;
+
+const workspaceListApiQueryHelper = new ApiQueryHelper();
+
+const emit = defineEmits<{(e: 'confirm'): void; }>();
+
+const storeState = reactive({
+    workspaceList: computed<WorkspaceModel[]>(() => userWorkspaceGetters.workspaceList),
+    visible: computed<boolean>(() => dataSourcesPageState.modal.visible),
+    type: computed<CostLinkedAccountModalType|undefined>(() => dataSourcesPageState.modal.type),
+    linkedAccounts: computed<CostDataSourceAccountModel[]>(() => dataSourcesPageGetters.linkedAccounts),
+    selectedLinkedAccountsIndices: computed<number[]>(() => dataSourcesPageState.selectedLinkedAccountsIndices),
+});
+const state = reactive({
+    loading: false,
+    activeItemCount: 0,
+    headerTitle: computed<TranslateResult>(() => {
+        if (storeState.type === 'RESET') {
+            return i18n.t('BILLING.COST_MANAGEMENT.DATA_SOURCES.RESET_MODAL_TITLE', { count: storeState.selectedLinkedAccountsIndices.length });
+        }
+        if (storeState.type === 'UPDATE') {
+            return i18n.t('BILLING.COST_MANAGEMENT.DATA_SOURCES.UPDATE_MODAL_TITLE', { count: storeState.selectedLinkedAccountsIndices.length });
+        }
+        return '';
+    }),
+});
+const dropdownState = reactive({
+    visible: false,
+    loading: false,
+    searchText: '',
+    selectedMenuId: '',
+    menu: computed(() => storeState.workspaceList.map((i) => ({ label: i.name, name: i.workspace_id }))),
+});
+
+const handleConfirm = async () => {
+    state.loading = true;
+
+    const promises = storeState.selectedLinkedAccountsIndices.map((idx) => {
+        state.activeItemCount += 1;
+        const item = storeState.linkedAccounts[idx];
+        const defaultParams = {
+            data_source_id: item.data_source_id,
+            account_id: item.account_id,
+        };
+        if (storeState.type === 'RESET') {
+            return resetLinkedAccount(defaultParams);
+        }
+        return updateLinkedAccount({
+            ...defaultParams,
+            workspace_id: dropdownState.selectedMenuId,
+        }, idx);
+    });
+
+    try {
+        await Promise.allSettled(promises);
+
+        emit('confirm');
+        if (storeState.type === 'RESET') {
+            showSuccessMessage(i18n.t('BILLING.COST_MANAGEMENT.DATA_SOURCES.ALT_S_RESET'), '');
+        } else {
+            showSuccessMessage(i18n.t('BILLING.COST_MANAGEMENT.DATA_SOURCES.ALT_S_UPDATE'), '');
+        }
+
+        handleClose();
+    } finally {
+        state.loading = false;
+    }
+};
+const handleClose = () => {
+    dataSourcesPageStore.setModal(false, undefined);
+    state.activeItemCount = 0;
+};
+const getWorkspaceInfo = (id: string): WorkspaceModel|undefined => {
+    if (!id) return undefined;
+    return storeState.workspaceList.find((i) => i.workspace_id === id);
+};
+const handleSelectDropdownItem = async (menuItem: string) => {
+    dropdownState.selectedMenuId = menuItem;
+};
+
+const resetLinkedAccount = (params: CostDataSourceAccountResetParameters) => {
+    const loadingMessageId = showLoadingMessage(i18n.t(
+        'BILLING.COST_MANAGEMENT.DATA_SOURCES.RESETTING',
+        {
+            count: state.activeItemCount,
+            totalCount: storeState.selectedLinkedAccountsIndices.length,
+        },
+    ), '');
+    dataSourcesPageStore.resetLinkedAccount(params)
+        .then(() => hideLoadingMessage(loadingMessageId));
+};
+const updateLinkedAccount = (params: CostDataSourceAccountUpdateParameters, idx: number) => {
+    const loadingMessageId = showLoadingMessage(i18n.t(
+        'BILLING.COST_MANAGEMENT.DATA_SOURCES.UPDATING',
+        {
+            count: state.activeItemCount,
+            totalCount: storeState.selectedLinkedAccountsIndices.length,
+        },
+    ), '');
+    dataSourcesPageStore.updateLinkedAccount(params, idx)
+        .then(() => hideLoadingMessage(loadingMessageId));
+};
+const workspaceMenuHandler: AutocompleteHandler = async (inputText: string, pageStart, pageLimit = 10) => {
+    dropdownState.loading = true;
+
+    workspaceListApiQueryHelper.setFilters([
+        { k: 'name', v: inputText, o: '' },
+    ]);
+    try {
+        const { results } = await SpaceConnector.clientV2.identity.workspace.list<WorkspaceListParameters, ListResponse<WorkspaceModel>>({
+            query: workspaceListApiQueryHelper.data,
+        });
+        const refinedMenuItems = (results ?? []).map((i) => ({
+            label: i.name,
+            name: i.workspace_id,
+        }));
+        const slicedResults = refinedMenuItems?.slice((pageStart ?? 1) - 1, pageLimit);
+
+        return {
+            results: slicedResults,
+            more: pageLimit < refinedMenuItems.length,
+        };
+    } catch (e) {
+        ErrorHandler.handleError(e);
+        return {
+            results: [],
+        };
+    } finally {
+        dropdownState.loading = false;
+    }
+};
+</script>
+
+<template>
+    <p-button-modal class="data-source-management-modal"
+                    :header-title="state.headerTitle"
+                    size="sm"
+                    :fade="true"
+                    :backdrop="true"
+                    :loading="state.loading"
+                    :theme-color="storeState.type === 'RESET' ? 'alert' : 'primary'"
+                    :visible="storeState.visible"
+                    @confirm="handleConfirm"
+                    @cancel="handleClose"
+                    @close="handleClose"
+    >
+        <template #body>
+            <div v-if="storeState.type === 'UPDATE'"
+                 class="modal-content"
+            >
+                <p-field-title class="title"
+                               :label="$t('BILLING.COST_MANAGEMENT.DATA_SOURCES.COL_WORKSPACE')"
+                />
+                <p-select-dropdown use-fixed-menu-style
+                                   :visible-menu="dropdownState.visible"
+                                   :loading="dropdownState.loading"
+                                   :search-text.sync="dropdownState.searchText"
+                                   show-select-marker
+                                   is-filterable
+                                   :handler="workspaceMenuHandler"
+                                   class="workspace-select-dropdown"
+                                   @select="handleSelectDropdownItem"
+                >
+                    <template #dropdown-button>
+                        <div v-if="dropdownState.selectedMenuId"
+                             class="workspace-wrapper"
+                        >
+                            <workspace-logo-icon :text="getWorkspaceInfo(dropdownState.selectedMenuId)?.name || ''"
+                                                 :theme="getWorkspaceInfo(dropdownState.selectedMenuId)?.tags?.theme"
+                                                 size="xs"
+                            />
+                            <span>{{ getWorkspaceInfo(dropdownState.selectedMenuId)?.name || '' }}</span>
+                        </div>
+                        <span v-else
+                              class="select"
+                        >{{ $t('BILLING.COST_MANAGEMENT.DATA_SOURCES.SELECT_WORKSPACE') }}</span>
+                    </template>
+                    <template #menu-item--format="{item}">
+                        <div class="workspace-menu-item">
+                            <workspace-logo-icon :text="getWorkspaceInfo(item.name)?.name || ''"
+                                                 :theme="getWorkspaceInfo(item.name)?.tags?.theme"
+                                                 size="xs"
+                            />
+                            <span>{{ item.label }}</span>
+                            <span class="state"
+                                  :class="[getWorkspaceInfo(item.name)?.state.toLowerCase()]"
+                            />
+                            <span class="description">{{ getWorkspaceInfo(item.name)?.tags?.description }}</span>
+                        </div>
+                    </template>
+                </p-select-dropdown>
+            </div>
+        </template>
+        <template #confirm-button>
+            <span>
+                {{ storeState.type === 'RESET' ? $t('BILLING.COST_MANAGEMENT.DATA_SOURCES.RESET') : $t('BILLING.COST_MANAGEMENT.DATA_SOURCES.UPDATE') }}
+                <span> {{ storeState.selectedLinkedAccountsIndices.length }}</span>
+            </span>
+        </template>
+    </p-button-modal>
+</template>
+
+<style scoped lang="postcss">
+.data-source-management-modal {
+    .modal-content {
+        @apply flex flex-col;
+        padding: 1.125rem 0 3.625rem;
+        border: none;
+        gap: 0.25rem;
+        .workspace-select-dropdown {
+            .workspace-wrapper {
+                @apply flex items-center;
+                gap: 0.25rem;
+            }
+            .select {
+                @apply text-gray-600;
+            }
+            .workspace-menu-item {
+                @apply flex items-center;
+                gap: 0.375rem;
+                .state {
+                    @apply rounded-full;
+                    width: 0.5rem;
+                    height: 0.5rem;
+                    &.enabled {
+                        @apply bg-green-600;
+                    }
+                    &.disabled {
+                        @apply bg-gray-300;
+                    }
+                }
+                .description {
+                    @apply text-label-md text-gray-500;
+                }
+            }
+        }
+    }
+}
+</style>
--- apps/web/src/services/cost-explorer/components/DataSourceManagementTabLinkedAccount.vue
+++ apps/web/src/services/cost-explorer/components/DataSourceManagementTabLinkedAccount.vue
@@ -1,24 +1,56 @@
 <script setup lang="ts">
 import {
-    computed, reactive,
+    computed, reactive, watch,
 } from 'vue';
 
 import {
     PButton, PHeading,
 } from '@spaceone/design-system';
 
+import { ApiQueryHelper } from '@cloudforet/core-lib/space-connector/helper';
+
+import DataSourceManagementModal from '@/services/cost-explorer/components/DataSourceManagementModal.vue';
 import DataSourceManagementTabLinkedAccountTable
     from '@/services/cost-explorer/components/DataSourceManagementTabLinkedAccountTable.vue';
 import { useDataSourcesPageStore } from '@/services/cost-explorer/stores/data-sources-page-store';
+import type { CostLinkedAccountModalType, DataSourceItem } from '@/services/cost-explorer/types/data-sources-type';
 
 const dataSourcesPageStore = useDataSourcesPageStore();
 const dataSourcesPageState = dataSourcesPageStore.state;
+const dataSourcesPageGetters = dataSourcesPageStore.getters;
+
+const listApiQueryHelper = new ApiQueryHelper();
 
 const storeState = reactive({
+    activeTab: computed<string>(() => dataSourcesPageState.activeTab),
+    selectedDataSourceItem: computed<DataSourceItem>(() => dataSourcesPageGetters.selectedDataSourceItem),
     totalCount: computed<number>(() => dataSourcesPageState.linkedAccountsTotalCount),
     selectedLinkedAccountsIndices: computed<number[]>(() => dataSourcesPageState.selectedLinkedAccountsIndices),
+    modalVisible: computed<boolean>(() => dataSourcesPageState.modal.visible),
+    linkedAccountsPageStart: computed<number>(() => dataSourcesPageState.linkedAccountsPageStart),
+    linkedAccountsPageLimit: computed<number>(() => dataSourcesPageState.linkedAccountsPageLimit),
 });
 
+const handleClickAction = (action: CostLinkedAccountModalType) => {
+    dataSourcesPageStore.setModal(true, action);
+};
+
+const fetchLinkedAccountList = async () => {
+    dataSourcesPageStore.setLinkedAccountsLoading(true);
+    try {
+        listApiQueryHelper.setPage(storeState.linkedAccountsPageStart, storeState.linkedAccountsPageLimit);
+        await dataSourcesPageStore.fetchLinkedAccount({
+            data_source_id: storeState.selectedDataSourceItem?.data_source_id || '',
+            query: listApiQueryHelper.data,
+        });
+    } finally {
+        dataSourcesPageStore.setLinkedAccountsLoading(false);
+    }
+};
+
+watch([() => storeState.activeTab, () => storeState.selectedDataSourceItem], async () => {
+    await fetchLinkedAccountList();
+}, { immediate: true });
 </script>
 
 <template>
@@ -35,18 +67,23 @@ const storeState = reactive({
                 <div class="extra-wrapper">
                     <p-button style-type="tertiary"
                               :disabled="storeState.selectedLinkedAccountsIndices.length === 0"
+                              @click="handleClickAction('RESET')"
                     >
                         {{ $t('BILLING.COST_MANAGEMENT.DATA_SOURCES.RESET') }}
                     </p-button>
                     <p-button style-type="tertiary"
                               :disabled="storeState.selectedLinkedAccountsIndices.length === 0"
+                              @click="handleClickAction('UPDATE')"
                     >
                         {{ $t('BILLING.COST_MANAGEMENT.DATA_SOURCES.UPDATE') }}
                     </p-button>
                 </div>
             </template>
         </p-heading>
-        <data-source-management-tab-linked-account-table />
+        <data-source-management-tab-linked-account-table @confirm="fetchLinkedAccountList" />
+        <data-source-management-modal v-if="storeState.modalVisible"
+                                      @confirm="fetchLinkedAccountList"
+        />
     </div>
 </template>
 
