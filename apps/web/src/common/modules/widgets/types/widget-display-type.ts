c96c5f6e96f7f5d811d63370156ba712da1f8b52
yuda <yuda@megazone.com>
2024-05-16 14:01:31 +0900

    feat: update widget data type
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/common/modules/widgets/types/widget-display-type.ts
+++ apps/web/src/common/modules/widgets/types/widget-display-type.ts
@@ -57,17 +57,6 @@ export interface Legend {
     disabled?: boolean; // this is used only in widget data table
 }
 
-
-export interface CostAnalyzeResponse<Result> {
-    more?: boolean;
-    results: Result[];
-}
-
-export interface BudgetUsageAnalyzeResponse<Result> {
-    more?: boolean;
-    results: Result[];
-}
-
 export const WIDGET_THEMES = ['violet', 'blue', 'coral', 'yellow', 'green', 'indigo', 'peacock'] as const;
 export type WidgetTheme = typeof WIDGET_THEMES[number];
 export type WidgetColorSetType = 'basic'|'massive';
