234a25d6fcca63982c689f0e2ecc838dc167ceab
NaYeong,Kim <nayeongkim@megazone.com>
2024-05-20 19:07:28 +0900

    chore: apply qa issue
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/services/workspace-home/components/BookmarkManagedBadge.vue
+++ apps/web/src/services/workspace-home/components/BookmarkManagedBadge.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
 import { PI } from '@spaceone/design-system';
 
-import { white } from '@/styles/colors';
+import { violet } from '@/styles/colors';
 </script>
 
 <template>
     <span class="bookmark-managed-badge">
-        <p-i name="ic_sparkle-filled"
+        <p-i name="ic_badge-with-ribbon"
              width="0.625rem"
              height="0.625rem"
-             :color="white"
+             :color="violet[700]"
         />
     </span>
 </template>
 
 <style scoped lang="postcss">
 .bookmark-managed-badge {
-    @apply block flex items-center justify-center bg-violet-500 rounded-full;
+    @apply block flex items-center justify-center bg-violet-200 rounded-full;
     width: 0.875rem;
     height: 0.875rem;
 }
