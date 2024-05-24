c2d7e75a27c6274b7579dc3054f5041fdb822729
yuda <yuda@megazone.com>
2024-05-24 20:23:07 +0900

    chore: change file name
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/common/modules/widgets/types/widget-config-type.ts
+++ apps/web/src/common/modules/widgets/types/widget-config-type.ts
@@ -1,6 +1,6 @@
 import type { Granularity } from '@/schema/dashboard/_types/widget-type';
 
-import type { WIDGET_SIZE } from '@/common/modules/widgets/_constants/widget-constant';
+import type { WIDGET_SIZE } from '@/common/modules/widgets/_constants/widget-display-constant';
 
 
 export type WidgetSize = typeof WIDGET_SIZE[keyof typeof WIDGET_SIZE];
