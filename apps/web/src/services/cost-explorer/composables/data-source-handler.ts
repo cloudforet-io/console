8715ed6ea144d2e2e79752ff801c1679c7930f2c
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2024-05-29 02:54:49 +0900

    feat: apply table filter(search, pagination)
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/services/cost-explorer/composables/data-source-handler.ts
+++ apps/web/src/services/cost-explorer/composables/data-source-handler.ts
@@ -1,8 +1,9 @@
 import type { KeyItem } from '@spaceone/design-system/src/inputs/search/query-search/type';
-import type { ValueHandler } from '@spaceone/design-system/types/inputs/search/query-search/type';
+import type { ValueHandler, KeyDataType } from '@spaceone/design-system/types/inputs/search/query-search/type';
 import type { ToolboxOptions } from '@spaceone/design-system/types/navigation/toolbox/type';
-import { map } from 'lodash';
+import { cloneDeep, map } from 'lodash';
 
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 import type { ApiFilter } from '@cloudforet/core-lib/space-connector/type';
 
 import type { WorkspaceModel } from '@/schema/identity/workspace/model';
@@ -38,14 +39,59 @@ export const makeDataSourceDistinctValueHandler = (
     };
 };
 
+export const makeDataSourceSyncValueHandler = (
+    filters?: ApiFilter[],
+): ValueHandler|undefined => {
+    const staticParam: any = {
+        resource_type: 'cost_analysis.DataSourceAccount',
+        options: { limit: 10 },
+        distinct_key: 'is_sync',
+    };
+
+    return async (inputText: string|number, keyItem: KeyItem, currentDataType?: KeyDataType, subPath?: string) => {
+        const param = cloneDeep(staticParam);
+        param.search = inputText;
+        if (subPath) {
+            param.distinct_key = `'is_sync'.${subPath}`;
+        }
+        if (filters) {
+            param.options.filter = filters;
+        }
+
+        try {
+            const res = await SpaceConnector.client.addOns.autocomplete.distinct(param);
+
+            return {
+                results: res.results.reduce((results, d) => {
+                    if (d.name !== '' && d.name !== undefined && d.name !== null) results.push({ label: d.name.toString(), name: d.key.toString() });
+                    return results;
+                }, []),
+                totalCount: res.total_count,
+            };
+        } catch (e) {
+            return {
+                results: [],
+                totalCount: 0,
+            };
+        }
+    };
+};
+
 export const convertWorkspaceSearchValue = (options, workspaceList?: WorkspaceModel[]): ToolboxOptions => {
     const { queryTags } = options;
     if (!queryTags || queryTags.length === 0) return options;
-    const workspaceOptionIndex = queryTags.findIndex((tag) => tag.key.name === 'workspace_id');
-    const workspaceOption = queryTags[workspaceOptionIndex];
-    const searchedWorkspace = workspaceList?.filter((workspace) => workspace.name.includes(workspaceOption.value.name))[0];
-    if (searchedWorkspace) {
-        queryTags[workspaceOptionIndex].value.name = searchedWorkspace.workspace_id;
+    const workspaceOptionIndex = queryTags.findIndex((tag) => tag.key?.name === 'workspace_id');
+    const syncOptionIndex = queryTags.findIndex((tag) => tag.key?.name === 'is_sync');
+    if (workspaceOptionIndex !== -1) {
+        const workspaceOption = queryTags[workspaceOptionIndex];
+        const searchedWorkspace = workspaceList?.filter((workspace) => workspace.name.includes(workspaceOption.value.name))[0];
+        if (searchedWorkspace) {
+            queryTags[workspaceOptionIndex].value.name = searchedWorkspace.workspace_id;
+        }
+    } else if (syncOptionIndex !== -1) {
+        const name = queryTags[syncOptionIndex]?.value.name;
+        queryTags[syncOptionIndex].value.name = name === 'true';
     }
+
     return options;
 };
