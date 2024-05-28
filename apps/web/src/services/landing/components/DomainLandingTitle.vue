146713cde502dfcecf1d93c07c1f8e6c13ff5a77
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2024-05-28 15:37:55 +0900

    feat: apply title section at domain landing
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

---
+++ apps/web/src/services/landing/components/DomainLandingTitle.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { computed, reactive } from 'vue';
+
+import { PI } from '@spaceone/design-system';
+
+import { store } from '@/store';
+
+const storeState = reactive({
+    domainName: computed<string>(() => store.state.domain.name),
+});
+</script>
+
+<template>
+    <div class="domain-landing-title">
+        <h1>{{ $t('LADING.DOMAIN.WELCOME_TITLE', {'domain_name': storeState.domainName}) }}</h1>
+        <i18n class="title"
+              path="LADING.DOMAIN.TITLE"
+        >
+            <template #domain_icon>
+                <p-i name="ic_three-boxes-with-sparkles"
+                     width="2rem"
+                     height="2rem"
+                     color="inherit"
+                />
+            </template>
+            <template #domain_name>
+                <span>{{ storeState.domainName }}</span>
+            </template>
+            <template #boost_icon>
+                <p-i name="ic_arrow-right-up-with-sparkles"
+                     width="2rem"
+                     height="2rem"
+                     color="inherit"
+                />
+            </template>
+        </i18n>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+.domain-landing-title {
+    @apply flex flex-col;
+    gap: 1rem;
+    .title {
+        @apply text-display-lg font-normal;
+        max-width: 55rem;
+    }
+}
+</style>
--- apps/web/src/services/landing/pages/DomainLandingPage.vue
+++ apps/web/src/services/landing/pages/DomainLandingPage.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import DomainLandingTitle from '@/services/landing/components/DomainLandingTitle.vue';
 
 </script>
 
 <template>
-    <div>
-        domain landing page
+    <div class="domain-landing-page">
+        <domain-landing-title />
     </div>
 </template>
+
+<style lang="postcss" scoped>
+.domain-landing-page {
+    @apply flex flex-col;
+    width: 100%;
+    padding-top: 7rem;
+    gap: 4rem;
+}
+</style>
