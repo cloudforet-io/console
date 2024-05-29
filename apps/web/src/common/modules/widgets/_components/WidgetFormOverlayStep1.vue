fa078c6f98257fc18e39a4597ae72e15da01094b
Yongtae Park <samuel.park@megazone.com>
2024-05-29 23:57:18 +0900

    feat(widget-add-form): create resizer in step1 table area
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/common/modules/widgets/_components/WidgetFormOverlayStep1.vue
+++ apps/web/src/common/modules/widgets/_components/WidgetFormOverlayStep1.vue
@@ -1,15 +1,17 @@
 <script lang="ts" setup>
-import { computed, reactive } from 'vue';
+import {
+    computed, onMounted, onUnmounted, reactive, ref,
+} from 'vue';
 
 import {
-    PButton, PToolboxTable, PFieldTitle, PEmpty, PI,
+    PToolboxTable, PI, PTooltip,
 } from '@spaceone/design-system';
 
 import { i18n } from '@/translations';
 
-
 import WidgetFormDataSourcePopover from '@/common/modules/widgets/_components/WidgetFormDataSourcePopover.vue';
 
+const dataTableContentsRef = ref<HTMLElement|null>(null);
 
 const state = reactive({
     // data table
@@ -18,100 +20,131 @@ const state = reactive({
         { name: 'value', label: 'table', type: 'item' },
     ]),
     selectedData: undefined as string|undefined,
-    chartTypes: computed(() => [
-        { name: 'table', label: 'Table', icon: '' },
-        { name: 'area', label: 'Area', icon: '' },
-        { name: 'verticalBar', label: 'Vertical Bar', icon: '' },
-        { name: 'line', label: 'Line', icon: '' },
-        { name: 'donut', label: 'Donut', icon: '' },
-        { name: 'pie', label: 'Pie', icon: '' },
-        { name: 'treemap', label: 'Treemap', icon: '' },
-        { name: 'heatmap', label: 'Heatmap', icon: '' },
-        { name: 'number', label: 'Number', icon: '' },
-        { name: 'regionMap', label: 'Region Map', icon: '' },
-    ]),
-    selectedChartType: undefined as string|undefined,
 });
 
+const displayState = reactive({
+    dataTableAreaOpen: true,
+    tableAreaHeight: (dataTableContentsRef.value?.clientHeight || 1000) / 5,
+    minHeight: 32,
+    resizerStyle: computed(() => ({
+        bottom: `${displayState.tableAreaHeight}px`,
+        cursor: displayState.dataTableAreaOpen ? 'ns-resize' : 'auto',
+    })),
+    tableContainerStyle: computed(() => ({
+        height: `${displayState.tableAreaHeight}px`,
+        minHeight: `${displayState.minHeight}px`,
+        width: '100%',
+    })),
+    tableStyle: computed(() => ({
+        height: `${displayState.tableAreaHeight}px`,
+        minHeight: `${(dataTableContentsRef.value?.clientHeight || 1000) / 5}px`,
+    })),
+    resizing: false,
+    clientY: null,
+    transition: false,
+});
+
+
 /* Event */
-const handleClickGenerate = () => {
-    // TODO: add event
+
+/* Hide Toggle */
+const offTransition = () => { displayState.transition = false; };
+const handleToggleDataTableArea = () => {
+    if (displayState.dataTableAreaOpen) {
+        displayState.dataTableAreaOpen = false;
+        displayState.transition = true;
+        displayState.minHeight = 0;
+        displayState.tableAreaHeight = 0;
+        setTimeout(offTransition, 500);
+    } else {
+        displayState.dataTableAreaOpen = true;
+        displayState.transition = true;
+        displayState.tableAreaHeight = (dataTableContentsRef.value?.clientHeight || 1000) / 5;
+        setTimeout(offTransition, 500);
+    }
 };
 
-const handleClickChartType = (chartType: string) => {
-    state.selectedChartType = chartType;
+/* Resizing */
+const startResizing = () => {
+    if (!displayState.dataTableAreaOpen) return;
+    displayState.resizing = true;
+};
+const isResizing = (event) => {
+    if (displayState.resizing) {
+        if (displayState.clientY === null) {
+            displayState.clientY = event.clientY;
+            return;
+        }
+        const delta = -event.movementY;
+        const height = displayState.tableAreaHeight + delta;
+        if (!(height <= displayState.minHeight || height > (dataTableContentsRef.value?.clientHeight || 1000) - 32)) {
+            displayState.tableAreaHeight = height;
+        }
+        displayState.clientY = event.clientY;
+    }
+};
+const endResizing = () => {
+    displayState.resizing = false;
+    displayState.clientY = null;
+};
+const documentEventMount = (eventName: string, func: any) => {
+    onMounted(() => document.addEventListener(eventName, func));
+    onUnmounted(() => document.removeEventListener(eventName, func));
 };
 
+documentEventMount('mousemove', isResizing);
+documentEventMount('mouseup', endResizing);
 </script>
 
 <template>
     <div class="sidebar-contents">
-        <div class="left-part">
+        <div ref="dataTableContentsRef"
+             class="data-table-contents"
+        >
             <div class="data-source-wrapper">
                 <widget-form-data-source-popover />
                 <!--                <widget-form-data-source-card />-->
             </div>
-            <p-toolbox-table :fields="state.fields"
-                             :items="[]"
-                             :searchable="false"
-                             :page-size-changeable="false"
-                             :refreshable="false"
-                             class="view-table-wrapper"
+            <div class="resizer-area"
+                 :style="displayState.resizerStyle"
+                 @mousedown="startResizing"
             >
-                <template #toolbox-left>
-                    <div class="toolbox-left-wrapper">
-                        <span class="view-table-title">
-                            {{ i18n.t('DASHBOARDS.WIDGET.OVERLAY.STEP_1.DATA_TABLE') }}
-                        </span>
-                        <p-button style-type="secondary"
-                                  icon-left="ic_refresh"
-                                  @click="handleClickGenerate"
-                        >
-                            {{ $t('DASHBOARDS.WIDGET.OVERLAY.STEP_1.GENERATE') }}
-                        </p-button>
-                    </div>
-                </template>
-            </p-toolbox-table>
-        </div>
-        <div class="right-part">
-            <div class="chart-type-wrapper">
-                <p-field-title :label="$t('DASHBOARDS.WIDGET.OVERLAY.STEP_1.CHART_TYPE')" />
-                <div class="chart-type-select-wrapper">
-                    <div v-if="!state.selectedData"
-                         class="button-wrapper"
+                <hr v-if="displayState.dataTableAreaOpen"
+                    class="resizer-bar"
+                >
+                <p-tooltip :contents="displayState.dataTableAreaOpen ? $t('COMPONENT.VERTICAL_LAYOUT.COLLAPSE') : $t('COMPONENT.VERTICAL_LAYOUT.EXPAND')"
+                           position="top"
+                           :class="{ 'data-table-hide-button': true, 'hide': !displayState.dataTableAreaOpen }"
+                           @click="handleToggleDataTableArea"
+                >
+                    <p-i class="hide-icon"
+                         :name="displayState.dataTableAreaOpen ? 'ic_chevron-down': 'ic_chevron-up'"
+                         size="sm"
+                         color="inherit"
+                    />
+                </p-tooltip>
+            </div>
+            <div class="data-table-area"
+                 :class="{ 'transition': displayState.transition }"
+                 :style="displayState.tableContainerStyle"
+            >
+                <div class="data-table-wrapper">
+                    <p-toolbox-table :fields="state.fields"
+                                     :style="displayState.tableStyle"
+                                     :items="[]"
+                                     :searchable="false"
+                                     :page-size-changeable="false"
+                                     :refreshable="false"
+                                     class="view-table-wrapper"
                     >
-                        <button v-for="chartType in state.chartTypes"
-                                :key="chartType.name"
-                                class="chart-type-button"
-                                :class="{ 'disabled': false, 'selected': state.selectedChartType === chartType.name }"
-                                @click="handleClickChartType(chartType.name)"
-                        >
-                            <p-i :name="chartType.icon" />
-                            {{ chartType.label }}
-                            <p-i v-if="state.selectedChartType === chartType.name"
-                                 class="selected-icon"
-                                 name="ic_checkbox-circle-selected"
-                                 width="1.25rem"
-                                 height="1.25rem"
-                            />
-                        </button>
-                    </div>
-                    <p-empty v-else
-                             show-image
-                             image-size="sm"
-                             class="empty-box"
-                             :title="$t('DASHBOARDS.WIDGET.OVERLAY.STEP_1.SELECT_A_DATA_SOURCE')"
-                    >
-                        {{ $t('DASHBOARDS.WIDGET.OVERLAY.STEP_1.EMPTY_DESC') }}
-                    </p-empty>
-
-                    <div v-if="state.selectedChartType"
-                         class="selected-data-info-pannel"
-                    >
-                        <p class="info-text">
-                            Create a <strong>{{ state.selectedChartType }}</strong> widget with <strong>Data Source 01</strong>
-                        </p>
-                    </div>
+                        <template #toolbox-left>
+                            <div class="toolbox-left-wrapper">
+                                <span class="view-table-title">
+                                    {{ i18n.t('DASHBOARDS.WIDGET.OVERLAY.STEP_1.DATA_TABLE') }}
+                                </span>
+                            </div>
+                        </template>
+                    </p-toolbox-table>
                 </div>
             </div>
         </div>
@@ -124,8 +157,8 @@ const handleClickChartType = (chartType: string) => {
     display: flex;
     gap: 1rem;
     padding: 0 1.5rem 1rem 1.5rem;
-    .left-part {
-        @apply bg-gray-150 rounded-md;
+    .data-table-contents {
+        @apply bg-gray-150 rounded-md relative;
         flex-grow: 1;
         display: flex;
         flex-direction: column;
@@ -135,63 +168,77 @@ const handleClickChartType = (chartType: string) => {
             overflow: auto;
             padding: 1rem;
         }
-        .view-table-wrapper {
-            @apply rounded-md;
-            max-height: 18.75rem;
-            .toolbox-left-wrapper {
-                display: flex;
-                align-items: center;
-                gap: 0.5rem;
-                .view-table-title {
-                    @apply text-label-lg font-bold;
+        .resizer-area {
+            @apply absolute w-full flex items-center justify-center;
+            padding: 0.5rem 1rem;
+            transition: top 0.2s;
+            z-index: 1;
+
+            .resizer-bar {
+                @apply w-full bg-blue-600;
+                height: 0.0625rem;
+                opacity: 0;
+            }
+
+            &:hover {
+                cursor: ns-resize;
+                .resizer-bar {
+                    opacity: 1;
                 }
             }
-        }
-    }
-    .right-part {
-        display: flex;
-        flex-direction: column;
-        width: 25%;
-        min-width: 2rem;
-        .chart-type-wrapper {
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            .chart-type-select-wrapper {
-                @apply flex flex-col justify-between h-full;
-                .button-wrapper {
-                    @apply grid gap-3;
-                    grid-template-columns: repeat(3, 1fr);
-                    padding-top: 1rem;
 
-                    .chart-type-button {
-                        @apply relative border border-gray-200 rounded-lg flex flex-col items-center justify-center text-label-md cursor-pointer;
-                        height: 6.25rem;
-
-                        &.disabled {
-                            cursor: not-allowed;
-                            opacity: 0.3;
-                        }
-
-                        .selected-icon {
-                            @apply absolute;
-                            left: 0.5rem;
-                            top: 0.5rem;
-                        }
-                        &.selected {
-                            @apply border-blue-600 text-blue-600;
+            .data-table-hide-button {
+                @apply absolute flex items-center justify-center bg-white border border-gray-300 rounded-full text-gray-600 cursor-pointer;
+                width: 1.5rem;
+                height: 1.5rem;
+                left: 0.75rem;
+                top: 0.375rem;
+                z-index: 2;
+                &.hide {
+                    @apply bg-white justify-end;
+                    top: -0.125rem;
+                    height: 1.5rem;
+                    margin-top: -0.375rem;
+                    border-bottom-left-radius: 50%;
+                    border-bottom-right-radius: 50%;
+                    border-bottom: 0;
+                    &:hover {
+                        @apply text-secondary;
+                        top: -1.125rem;
+                        height: 2.5rem;
+                        .hide-icon {
+                            margin-top: -1rem;
                         }
                     }
                 }
-                .empty-box {
-                    height: 100%;
+                &:hover {
+                    @apply bg-blue-200 cursor-pointer;
                 }
-                .selected-data-info-pannel {
-                    @apply border border-indigo-200 bg-indigo-100 rounded-lg w-full;
-                    height: 2.625rem;
-                    padding: 0.75rem 1rem;
-                    .info-text {
-                        @apply text-label-md text-indigo-600;
+            }
+        }
+        .data-table-area {
+            @apply relative;
+            height: 20%;
+            max-height: calc(100% - 2rem);
+
+            &.transition {
+                transition: height 0.2s;
+            }
+
+            .data-table-wrapper {
+                height: 100%;
+                overflow-y: scroll;
+
+                .view-table-wrapper {
+                    @apply rounded-md;
+
+                    .toolbox-left-wrapper {
+                        display: flex;
+                        align-items: center;
+                        gap: 0.5rem;
+                        .view-table-title {
+                            @apply text-label-lg font-bold;
+                        }
                     }
                 }
             }
