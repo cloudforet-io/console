3d38e819a86c8fb29cf3cc5fa6f48d66e62e90db
Dahyun Yu <yuda@megazone.com>
2024-05-30 12:56:30 +0900

    refactor: change widget names and file name (#3859)
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ apps/web/src/common/modules/widgets/_base-widgets/gauge/widget-config.ts
@@ -0,0 +1,17 @@
+import type { NewWidgetConfig } from '@/common/modules/widgets/types/widget-config-type';
+
+
+const gauge: NewWidgetConfig = {
+    widget_name: 'gauge',
+    meta: {
+        title: 'Gauge',
+        sizes: ['full'],
+    },
+    data_mapping_schema: {
+    },
+    chart_options_schema: {
+    },
+};
+
+
+export default gauge;
---
+++ apps/web/src/common/modules/widgets/_base-widgets/geo-map/widget-config.ts
@@ -0,0 +1,17 @@
+import type { NewWidgetConfig } from '@/common/modules/widgets/types/widget-config-type';
+
+
+const geoMap: NewWidgetConfig = {
+    widget_name: 'geoMap',
+    meta: {
+        title: 'Geo Map',
+        sizes: ['full'],
+    },
+    data_mapping_schema: {
+    },
+    chart_options_schema: {
+    },
+};
+
+
+export default geoMap;
