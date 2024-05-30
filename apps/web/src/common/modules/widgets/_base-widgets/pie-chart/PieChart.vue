ac507308968cbccd378ddcdffc7216ae5dc7e74b
sulmo <sulmo@megazone.com>
2024-05-30 14:53:59 +0900

    feat: add pieChart as Echart
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/common/modules/widgets/_base-widgets/pie-chart/PieChart.vue
+++ apps/web/src/common/modules/widgets/_base-widgets/pie-chart/PieChart.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import {
-    computed,
-    reactive, ref, watch,
+    computed, onMounted,
+    reactive, ref,
 } from 'vue';
 
-import type { PieChart, IPieSeriesSettings } from '@amcharts/amcharts5/percent';
 import { PDataLoader } from '@spaceone/design-system';
+import type { EChartsOption } from 'echarts';
+import { init } from 'echarts';
+import { LineChart } from 'echarts/charts';
+import {
+    DatasetComponent, GridComponent, LegendComponent, TooltipComponent,
+} from 'echarts/components';
+import { use } from 'echarts/core';
+import { CanvasRenderer } from 'echarts/renderers';
 
-import { useAmcharts5 } from '@/common/composables/amcharts5';
 import WidgetFrame from '@/common/modules/widgets/_components/WidgetFrame.vue';
 import { useWidgetFrame } from '@/common/modules/widgets/_composables/use-widget/use-widget-frame';
 import type {
@@ -18,128 +24,107 @@ const props = defineProps<NewWidgetProps>();
 const emit = defineEmits<WidgetEmit>();
 
 const chartContext = ref<HTMLElement|null>(null);
-const chartHelper = useAmcharts5(chartContext);
+
+use([
+    CanvasRenderer,
+    GridComponent,
+    DatasetComponent,
+    LineChart,
+    TooltipComponent,
+    LegendComponent,
+]);
 
 
 const state = reactive({
     loading: true,
     data: null as any | null,
-    chart: null as null | PieChart,
+    chart: null as null,
     chartData: computed(() => {
         if (!state.data?.results) return [];
         return state.data?.results;
     }),
-    // legends: computed<Legend[]>(() => {
-    //     const data = state.data?.results ?? [];
-    //     const legends: Legend[] = getXYChartLegends(data, widgetState.parsedDataField, props.allReferenceTypeInfo);
-    //     return legends;
-    // }),
+    chartOptions: computed<EChartsOption>(() => ({
+        tooltip: {
+            trigger: 'item',
+            position: 'inside',
+        },
+        grid: {
+            containLabel: true,
+        },
+        legend: {
+            orient: 'vertical',
+            type: 'scroll',
+            left: '240px',
+            top: '0%',
+            icon: 'circle',
+            textStyle: {
+                overflow: 'truncate',
+                width: 150,
+            },
+            formatter: (name: string) => {
+                const series = state.chart.getOption().series[0];
+                const value = series.data.filter((row) => row.name === name)[0].value;
+                return `${(name.length > 10 ? `${name.slice(0, 10)}...` : name)}     ${value}`;
+            },
+        },
+        series: [
+            {
+                name: 'Access From',
+                type: 'pie',
+                radius: ['30%', '70%'],
+                center: ['120px', '50%'],
+                data: state.chartData,
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)',
+                    },
+                },
+                avoidLabelOverlap: false,
+                label: {
+                    show: false,
+                },
+            },
+        ],
+    })),
 });
 // const { widgetState } = useWidget(props, emit);
 const { widgetFrameProps, widgetFrameEventHandlers } = useWidgetFrame(props, emit);
 /* Util */
-const drawChart = (chartData: any[]) => {
-    const seriesSettings: IPieSeriesSettings = {
-        categoryField: 'category', // here
-        valueField: 'value', // here
-    };
 
-    if (!chartHelper.root.value) return;
-
-    const chart = chartHelper.createDonutChart({
-        innerRadius: 45,
-        x: chartHelper.percent(-25),
-        layout: undefined,
-    });
-    const series = chartHelper.createPieSeries(seriesSettings);
-    chart.series.push(series);
-    series.data.setAll(chartData);
-
-    const chartLegend = chartHelper.createLegend({
-        x: chartHelper.percent(75),
-        y: chartHelper.percent(50),
-        centerY: chartHelper.percent(50),
-        layout: chartHelper.root.value.verticalLayout,
-        height: chartHelper.percent(100),
-        verticalScrollbar: chartHelper.verticalScrollbarSetting({
-            orientation: 'vertical',
-        }),
-    });
-    const legend = chart.children.push(chartLegend);
-    legend.labels.template.setAll({
-        width: 148,
-        maxWidth: 128,
-        oversizedBehavior: 'truncate',
-        ellipsis: '...',
-    });
-    legend.valueLabels.template.setAll({
-        fontSize: 12,
-    });
-    legend.data.setAll(series.dataItems);
-
-    state.chart = chart;
-};
 
 const initWidget = async () => {
     state.loading = true;
     // TODO: data source를 돌며 fetchData
     state.data = {
         results: [
-            { category: 'google cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloud', value: 5000 },
-            { category: 'aws', value: 1000 },
-            { category: 'azure', value: 1000 },
-            { category: 'alibaba', value: 1000 },
-            { category: 'google cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloud', value: 5000 },
-            { category: 'aws', value: 1000 },
-            { category: 'azure', value: 1000 },
-            { category: 'alibaba', value: 1000 },
-            { category: 'google cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloudgoogle cloud', value: 5000 },
-            { category: 'aws', value: 1000 },
-            { category: 'azure', value: 1000 },
-            { category: 'alibaba', value: 1000 },
+            { name: 'google cloudgoogle cloudgoogle cloudgoogle ', value: 5000 },
+            { name: 'aws', value: 1000 },
+            { name: 'azure', value: 1000 },
+            { name: 'alibaba', value: 1000 },
+            { name: '22google cloudgoogle cloudgoogle cloudgoogle ', value: 5000 },
+            { name: '2aws', value: 1000 },
+            { name: '2azure', value: 1000 },
+            { name: '2alibaba', value: 1000 },
+            { name: '3google cloudgoogle cloudgoogle cloudgoogle ', value: 5000 },
+            { name: '3aws', value: 1000 },
+            { name: '3azure', value: 1000 },
+            { name: '3alibaba', value: 1000 },
+            { name: '4google cloudgoogle cloudgoogle cloudgoogle ', value: 5000 },
+            { name: '4aws', value: 1000 },
+            { name: '4azure', value: 1000 },
+            { name: '4alibaba', value: 1000 },
         ],
     };
     // await nextTick();
-    if (chartHelper.root.value) drawChart(state.chartData);
+    state.chart = init(chartContext.value);
+    state.chart.setOption(state.chartOptions);
     state.loading = false;
 };
-//
-// const refreshWidget = async (_thisPage = 1): Promise<Response> => {
-//     await nextTick();
-//     state.loading = true;
-//     thisPage.value = _thisPage;
-//     state.data = await fetchData();
-//     chartHelper.refreshRoot();
-//     await nextTick();
-//     if (chartHelper.root.value) drawChart(state.chartData);
-//     state.loading = false;
-//     return state.data;
-// };
 
-// useWidgetLifecycle({
-//     disposeWidget: chartHelper.disposeRoot,
-//     initWidget,
-//     refreshWidget,
-//     props,
-//     emit,
-//     widgetState,
-//     onCurrencyUpdate: async () => {
-//         if (!state.data) return;
-//         chartHelper.refreshRoot();
-//         await nextTick();
-//         if (chartHelper.root.value) drawChart(state.chartData);
-//     },
-// });
-
-// defineExpose<WidgetExpose<Response>>({
-//     initWidget,
-//     refreshWidget,
-// });
-
-watch(() => chartHelper.root.value, () => {
+onMounted(() => {
     initWidget();
-}, {
-    immediate: true,
 });
 </script>
 
@@ -147,40 +132,25 @@ watch(() => chartHelper.root.value, () => {
     <widget-frame v-bind="widgetFrameProps"
                   v-on="widgetFrameEventHandlers"
     >
-        <div class="data-container">
-            <div v-if="state.data?.results?.length"
-                 class="chart-wrapper"
-            >
-                <p-data-loader class="chart-loader"
-                               :data="state.chartData"
-                               loader-type="skeleton"
-                               disable-empty-case
-                               :loading="state.loading"
-                               :loader-backdrop-opacity="1"
-                               show-data-from-scratch
-                >
-                    <div ref="chartContext"
-                         class="chart"
-                    />
-                </p-data-loader>
-            </div>
-        </div>
+        <p-data-loader class="chart-loader"
+                       loader-type="skeleton"
+                       disable-empty-case
+                       :loading="state.loading"
+                       :loader-backdrop-opacity="1"
+                       show-data-from-scratch
+        >
+            <div ref="chartContext"
+                 class="chart"
+            />
+        </p-data-loader>
     </widget-frame>
 </template>
 
 <style lang="postcss" scoped>
-.data-container {
-    display: flex;
-    flex-direction: column;
+.chart-loader {
     height: 100%;
-    .chart-wrapper {
-        height: 15.875rem;
-        .chart-loader {
-            height: 100%;
-            .chart {
-                height: 100%;
-            }
-        }
+    .chart {
+        height: 100%;
     }
 }
 </style>
