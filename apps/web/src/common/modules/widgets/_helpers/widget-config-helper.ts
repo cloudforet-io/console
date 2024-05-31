230675acd367ed428eacbeaa36bce616a06d665f
yuda <yuda@megazone.com>
2024-05-31 11:22:46 +0900

    refactor: change variable name
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/common/modules/widgets/_helpers/widget-config-helper.ts
+++ apps/web/src/common/modules/widgets/_helpers/widget-config-helper.ts
@@ -1,4 +1,4 @@
-import { NEW_CONSOLE_WIDGET_CONFIG } from '@/common/modules/widgets/_constants/widget-config-list-constant';
+import { CONSOLE_WIDGET_CONFIG } from '@/common/modules/widgets/_constants/widget-config-list-constant';
 import type { NewWidgetConfig } from '@/common/modules/widgets/types/widget-config-type';
 
 
@@ -7,7 +7,7 @@ const consoleWidgetConfigCacheMap = new Map<string, NewWidgetConfig>();
 export const getWidgetConfig = (widgetName: string): NewWidgetConfig => {
     if (consoleWidgetConfigCacheMap.has(widgetName)) return consoleWidgetConfigCacheMap.get(widgetName) as NewWidgetConfig;
 
-    const config = NEW_CONSOLE_WIDGET_CONFIG[widgetName] as NewWidgetConfig;
+    const config = CONSOLE_WIDGET_CONFIG[widgetName] as NewWidgetConfig;
     consoleWidgetConfigCacheMap.set(widgetName, config);
     return config;
 };
