4d4af7d9262fa130a5a16a653e4ebfb818073368
Yongtae Park <samuel.park@megazone.com>
2024-05-31 12:41:15 +0900

    fix: edit namespace list parameters & edit group type
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/schema/inventory/namespace/type.ts
+++ apps/web/src/schema/inventory/namespace/type.ts
@@ -2,4 +2,4 @@ import type { NAMESPACE_CATEGORY } from '@/schema/inventory/namespace/constant';
 
 export type NamespaceCategory = typeof NAMESPACE_CATEGORY[keyof typeof NAMESPACE_CATEGORY];
 
-export type NamespaceGroup = 'Common' | 'AWS' | 'Azure' | 'Google Cloud' | 'Kubernetes';
+export type NamespaceGroup = 'common' | 'aws' | 'azure' | 'google_cloud' | 'kubernetes' | 'etc' | string;
