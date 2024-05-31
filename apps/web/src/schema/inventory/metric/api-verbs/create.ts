d680a54643eb4182180abfc97e7e54087e4436e6
Yongtae Park <samuel.park@megazone.com>
2024-05-31 12:10:48 +0900

    fix: edit namespace model
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/schema/inventory/metric/api-verbs/create.ts
+++ apps/web/src/schema/inventory/metric/api-verbs/create.ts
@@ -1,11 +1,13 @@
 import type { Tags } from '@/schema/_common/model';
+import type { ResourceGroupType } from '@/schema/_common/type';
 import type { MetricType } from '@/schema/inventory/metric/type';
 
 
 export interface MetricCreateParameters {
     name: string;
     metric_type: MetricType;
-    resource_type: string;
+    resource_type?: string;
+    resource_group: Extract<ResourceGroupType, 'DOMAIN'|'WORKSPACE'>;
     query_options: object;
     unit?: string;
     tags?: Tags;
--- apps/web/src/schema/inventory/namespace/type.ts
+++ apps/web/src/schema/inventory/namespace/type.ts
@@ -1,3 +1,5 @@
 import type { NAMESPACE_CATEGORY } from '@/schema/inventory/namespace/constant';
 
 export type NamespaceCategory = typeof NAMESPACE_CATEGORY[keyof typeof NAMESPACE_CATEGORY];
+
+export type NamespaceGroup = 'Common' | 'AWS' | 'Azure' | 'Google Cloud' | 'Kubernetes';
