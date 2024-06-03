2071921eb1fb0e3eecc31625f79c46b9ab80dff4
yuda <yuda@megazone.com>
2024-06-03 18:16:20 +0900

    chore: add widget components
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ apps/web/src/common/modules/widgets/_widgets/number-card/NumberCard.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import {
+    reactive,
+} from 'vue';
+
+import {
+    PDataLoader,
+} from '@spaceone/design-system';
+
+import WidgetFrame from '@/common/modules/widgets/_components/WidgetFrame.vue';
+import { useWidget } from '@/common/modules/widgets/_composables/use-widget/use-widget';
+import { useWidgetFrame } from '@/common/modules/widgets/_composables/use-widget/use-widget-frame';
+import type {
+    NewWidgetProps, WidgetEmit,
+} from '@/common/modules/widgets/types/widget-display-type';
+
+
+const props = defineProps<NewWidgetProps>();
+const emit = defineEmits<WidgetEmit>();
+
+const { widgetState } = useWidget(props, emit);
+const { widgetFrameProps, widgetFrameEventHandlers } = useWidgetFrame(props, emit, widgetState);
+
+const state = reactive({
+    loading: false,
+    data: null as Response | null,
+});
+</script>
+
+<template>
+    <widget-frame v-bind="widgetFrameProps"
+                  v-on="widgetFrameEventHandlers"
+    >
+        <p-data-loader class="chart-loader"
+                       :loading="state.loading"
+                       loader-type="skeleton"
+                       disable-empty-case
+                       :loader-backdrop-opacity="1"
+                       show-data-from-scratch
+        >
+            <div class="chart">
+                <span>icon</span>
+                <span>text</span>
+            </div>
+        </p-data-loader>
+    </widget-frame>
+</template>
+
+<style lang="postcss" scoped>
+.chart-loader {
+    height: 100%;
+    .chart {
+        height: 100%;
+    }
+}
+</style>
