bbde069a186cef20c0b9b5d6a23d2433a524a8b2
Yongtae Park <samuel.park@megazone.com>
2024-06-03 16:21:06 +0900

    fix(widget-generate-store): update widget store with real api
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/common/modules/widgets/_store/widget-generate-store.ts
+++ apps/web/src/common/modules/widgets/_store/widget-generate-store.ts
@@ -1,15 +1,32 @@
-import { reactive } from 'vue';
+import { computed, reactive } from 'vue';
 
 import { defineStore } from 'pinia';
+
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+
+import type { ListResponse } from '@/schema/_common/api-verbs/list';
+import type { DataTableAddParameters } from '@/schema/dashboard/public-data-table/api-verbs/add';
+import type { DataTableUpdateParameters } from '@/schema/dashboard/public-data-table/api-verbs/delete';
+import type { DataTableTransformParameters } from '@/schema/dashboard/public-data-table/api-verbs/transform';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+import type { DataTableModel } from '@/common/modules/widgets/types/widget-model';
 
 export const useWidgetGenerateStore = defineStore('widget-generate', () => {
     const state = reactive({
         // display
         showOverlay: false,
         overlayStep: 1,
+        // Widget
+        widget_id: '',
+        // Data Table
+        dataTables: [] as DataTableModel[],
+        selectedDataTable: undefined as DataTableModel|undefined,
     });
 
-    const getters = reactive({});
+    const getters = reactive({
+        selectedDataTableId: computed(() => state.selectedDataTable?.data_table_id),
+    });
 
     /* Mutations */
     const setShowOverlay = (showOverlay: boolean) => {
@@ -22,8 +39,55 @@ export const useWidgetGenerateStore = defineStore('widget-generate', () => {
     const mutations = {
         setShowOverlay,
         setOverlayStep,
+        setSelectedDataTable: (dataTableId: string) => {
+            state.selectedDataTable = state.dataTables.find((dataTable) => dataTable.data_table_id === dataTableId);
+        },
     };
-    const actions = {};
+    const actions = {
+        listDataTable: async () => {
+            try {
+                const { results } = await SpaceConnector.clientV2.dashboard.publicDataTable.list<DataTableModel, ListResponse<DataTableModel>>();
+                state.dataTables = results ?? [];
+            } catch (e) {
+                ErrorHandler.handleError(e);
+            }
+        },
+        /* Step - 1 */
+        createAddDataTable: async (addParams: Partial<DataTableAddParameters>) => {
+            const parameters = {
+                widiget_id: state.widget_id,
+                ...addParams,
+            } as DataTableAddParameters;
+            try {
+                const result = await SpaceConnector.clientV2.dashboard.publicDataTable.add<DataTableAddParameters, DataTableModel>(parameters);
+                state.dataTables.push(result);
+            } catch (e) {
+                ErrorHandler.handleError(e);
+            }
+        },
+        createTransformDataTable: async (transformParams: Partial<DataTableTransformParameters>) => {
+            const parameters = {
+                widiget_id: state.widget_id,
+                ...transformParams,
+            } as DataTableTransformParameters;
+            try {
+                const result = await SpaceConnector.clientV2.dashboard.publicDataTable.transform<DataTableTransformParameters, DataTableModel>(parameters);
+                state.dataTables.push(result);
+            } catch (e) {
+                ErrorHandler.handleError(e);
+            }
+        },
+        updateDataTable: async (updateParams: DataTableUpdateParameters) => {
+            try {
+                await SpaceConnector.clientV2.dashboard.publicDataTable.update<DataTableUpdateParameters, DataTableModel>(updateParams);
+            } catch (e) {
+                ErrorHandler.handleError(e);
+            }
+        },
+        loadDataTable: async (dataTableId: string) => {
+            console.debug('loadDataTable', dataTableId);
+        },
+    };
 
     return {
         state,
