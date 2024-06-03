a47fddb585cdef1074807f1c1c841f057d728b12
yuda <yuda@megazone.com>
2024-06-03 15:43:01 +0900

    feat: add widget field components
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ apps/web/src/common/modules/widgets/_widget-fields/WidgetFieldDataField.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { computed, reactive } from 'vue';
+
+import { PSelectDropdown, PFieldGroup } from '@spaceone/design-system';
+import type { MenuItem } from '@spaceone/design-system/types/inputs/context-menu/type';
+
+import type { WidgetFieldSchema, DataFieldOptions } from '@/common/modules/widgets/types/widget-config-type';
+
+
+interface Props {
+    widgetFieldSchema: WidgetFieldSchema;
+    required: boolean;
+}
+const props = withDefaults(defineProps<Props>(), {
+    widgetFieldSchema: () => ({
+        label: '',
+    }),
+});
+
+const state = reactive({
+    menuItems: computed<MenuItem[]>(() => []), // TODO: generate menu items with options.dataTarget
+    fieldOptions: computed<DataFieldOptions>(() => props.widgetFieldSchema?.options || {}),
+});
+</script>
+
+<template>
+    <div>
+        <p-field-group :label="props.widgetFieldSchema.label"
+                       :required="props.required"
+        >
+            <p-select-dropdown :menu="state.menuItems"
+                               :multi-selectable="state.fieldOptions?.multiSelectable"
+            />
+        </p-field-group>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+.p-select-dropdown {
+    width: 100%;
+}
+</style>
---
+++ apps/web/src/common/modules/widgets/_widget-fields/WidgetFieldXAxis.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+import { computed, reactive } from 'vue';
+
+import {
+    PSelectDropdown, PFieldGroup, PTextInput,
+} from '@spaceone/design-system';
+import type { MenuItem } from '@spaceone/design-system/types/inputs/context-menu/type';
+
+import type {
+    WidgetFieldSchema,
+} from '@/common/modules/widgets/types/widget-config-type';
+
+
+interface Props {
+    widgetFieldSchema: WidgetFieldSchema;
+    required: boolean;
+}
+const props = withDefaults(defineProps<Props>(), {
+    widgetFieldSchema: () => ({
+        label: '',
+    }),
+});
+
+const state = reactive({
+    menuItems: computed<MenuItem[]>(() => []), // TODO: generate menu items with options.dataTarget
+});
+</script>
+
+<template>
+    <div>
+        <p-field-group :label="props.widgetFieldSchema.label"
+                       :required="props.required"
+        >
+            <div class="field-form-wrapper">
+                <p-select-dropdown :menu="state.menuItems" />
+                <p-text-input type="number"
+                              :min="0"
+                />
+            </div>
+        </p-field-group>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+.field-form-wrapper {
+    display: flex;
+    gap: 0.5rem;
+    .p-select-dropdown {
+        width: 100%;
+    }
+
+    /* custom design-system component - p-text-input */
+    :deep(.p-text-input) {
+        width: 6.5rem;
+        .input-container {
+            padding-right: 1.5rem;
+        }
+    }
+}
+</style>
