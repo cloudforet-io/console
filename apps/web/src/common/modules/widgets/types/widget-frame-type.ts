b982092deb5de42190e92ee43e64354f47c4b1da
yuda <yuda@megazone.com>
2024-06-04 18:58:52 +0900

    feat: add `mode` props to WidgetFrame and Widget
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/common/modules/widgets/types/widget-frame-type.ts
+++ apps/web/src/common/modules/widgets/types/widget-frame-type.ts
@@ -1,26 +1,24 @@
 import type { TranslateResult } from 'vue-i18n';
-import type { Location } from 'vue-router/types/router';
 
 import type { WidgetSize } from '@/schema/dashboard/_types/widget-type';
-
-import type { Currency } from '@/store/modules/settings/type';
 
 
 export interface WidgetFrameProps {
     widgetId: string;
     widgetSizes: WidgetSize[];
-
+    //
     title?: TranslateResult;
     description?: string;
     size?: WidgetSize;
     width?: number;
-    widgetLocation?: Location;
+    // widgetLocation?: Location;
+    //
+    mode: 'overlay'|'customize'|'view';
+    // dateText?: string;
+    // currency?: Currency;
 
-    dateText?: string;
-    currency?: Currency;
-
-    loading?: boolean;
-    editMode?: boolean;
-    errorMode?: boolean;
+    // loading?: boolean;
+    // editMode?: boolean;
+    // errorMode?: boolean;
     // theme?: WidgetTheme;
 }
