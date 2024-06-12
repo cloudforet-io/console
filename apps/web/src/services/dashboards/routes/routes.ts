a68fa466925e2fbe05f3a21201afadf72616720f
yuda <yuda@megazone.com>
2024-06-12 21:19:04 +0900

    feat: delete unused files
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/dashboards/routes/routes.ts
+++ apps/web/src/services/dashboards/routes/routes.ts
@@ -1,18 +1,13 @@
 import type { RouteConfig } from 'vue-router';
 
-import { i18n } from '@/translations';
-
 import { MENU_ID } from '@/lib/menu/config';
 import { MENU_INFO_MAP } from '@/lib/menu/menu-info';
-
-import type { Breadcrumb } from '@/common/modules/page-layouts/type';
 
 import { DASHBOARDS_ROUTE } from '@/services/dashboards/routes/route-constant';
 
 const DashboardsContainer = () => import('@/services/dashboards/DashboardsContainer.vue');
 const DashboardsMainPage = () => import('@/services/dashboards/pages/DashboardsMainPage.vue');
 const DashboardCreatePage = () => import('@/services/dashboards/pages/DashboardCreatePage.vue');
-const DashboardCustomizePage = () => import('@/services/dashboards/pages/DashboardCustomizePage.vue');
 const DashboardDetailPage = () => import('@/services/dashboards/pages/DashboardDetailPage.vue');
 //
 const DashboardPlaygroundPage = () => import('@/services/dashboards/pages/DashboardPlaygroundPage.vue');
@@ -46,37 +41,6 @@ const dashboardsRoute: RouteConfig = {
             meta: { lsbVisible: true, label: ({ params }) => params.dashboardId, copiable: true },
             props: true,
             component: DashboardDetailPage,
-        },
-        {
-            path: 'customize/:dashboardId?',
-            name: DASHBOARDS_ROUTE.CUSTOMIZE._NAME,
-            meta: {
-                breadcrumbs: ({ params }) => {
-                    const breadcrumbs: Breadcrumb[] = [
-                        {
-                            name: i18n.t('DASHBOARDS.DETAIL.CUSTOMIZE'),
-                            to: {
-                                name: DASHBOARDS_ROUTE.CUSTOMIZE._NAME,
-                            },
-                        },
-                    ];
-                    if (params.dashboardId) {
-                        breadcrumbs.push({
-                            name: params.dashboardId,
-                            to: {
-                                name: DASHBOARDS_ROUTE.CUSTOMIZE._NAME,
-                                params: {
-                                    dashboardId: params.dashboardId,
-                                },
-                            },
-                            copiable: true,
-                        });
-                    }
-                    return breadcrumbs;
-                },
-            },
-            props: true,
-            component: DashboardCustomizePage,
         },
         {
             path: 'playground',
