93273c4162a41cb3d2542111dcd68865265cc654
yuda <yuda@megazone.com>
2024-06-14 13:59:12 +0900

    chore: add icons
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ packages/mirinae/src/foundation/icons/p-icons/ic_db-where.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_db-where': {
+    width: 32,
+    height: 32,
+    viewBox: '0 0 32 32',
+    data: '<path pid="0" fill-rule="evenodd" clip-rule="evenodd" d="M27 3H5a2.003 2.003 0 00-2 2v22a2.003 2.003 0 002 2h9.5v-2H5V10h22v4h2V5a2.003 2.003 0 00-2-2zm0 2v3H5V5h22z" _fill="#232533"/><path pid="1" d="M11 17H8v2h3v-2zM11 13H8v2h3v-2zM11 21H8v2h3v-2z" _fill="#232533"/><path pid="2" fill-rule="evenodd" clip-rule="evenodd" d="M25.606 24.192a7 7 0 10-1.414 1.414l3.1 3.101a1 1 0 001.415-1.414l-3.1-3.1zM15 20a5 5 0 1110 0 5 5 0 01-10 0z" _fill="#232533"/>'
+  }
+})
