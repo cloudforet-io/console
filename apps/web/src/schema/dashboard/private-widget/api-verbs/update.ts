f5386ac16a235fd978c3437a4e99a4be949e33c7
yuda <yuda@megazone.com>
2024-06-14 12:52:35 +0900

    chore: update parameter type of private widget
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/schema/dashboard/private-widget/api-verbs/update.ts
+++ apps/web/src/schema/dashboard/private-widget/api-verbs/update.ts
@@ -1,12 +1,15 @@
 import type { Tags } from '@/schema/_common/model';
 
+import type { WidgetSize } from '@/common/modules/widgets/types/widget-display-type';
 import type { WidgetType } from '@/common/modules/widgets/types/widget-model';
 
 export interface PrivateWidgetUpdateParameters {
     widget_id: string;
     name?: string;
     description?: string;
+    size?: WidgetSize;
     widget_type: WidgetType;
     options?: Record<string, any>;
+    data_table_id?: string;
     tags?: Tags;
 }
