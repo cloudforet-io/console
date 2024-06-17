e4bbd6df138ef909f018189852d2355e5e8e312b
yuda <yuda@megazone.com>
2024-06-17 15:13:57 +0900

    chore: update types
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/common/modules/widgets/types/widget-data-type.ts
+++ apps/web/src/common/modules/widgets/types/widget-data-type.ts
@@ -1,7 +1,4 @@
-
-interface SubData { date: string; value: number }
-export interface AnalyzeResult {
-    value_sum: SubData[];
-    _total_value_sum: number;
-    [dataField: string]: any; // provider: aws or provider: azure
+export interface DateRange {
+    start?: string;
+    end: string;
 }
