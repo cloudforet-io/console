5547a0cd2ef5a041450c6d029f7efb8eb27c3342
sulmo <50662170+sulmoJ@users.noreply.github.com>
2024-06-17 16:53:33 +0900

    fix(widget-form): fix validation bug (#4015)
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/common/modules/widgets/_components/WidgetFieldDropdownAndMax.vue
+++ apps/web/src/common/modules/widgets/_components/WidgetFieldDropdownAndMax.vue
@@ -32,7 +32,7 @@ const emit = defineEmits<{(e: 'update:is-valid', isValid:boolean): void;
 }>();
 const state = reactive({
     proxyValue: useProxyValue('value', props, emit),
-    isMaxValid: computed<boolean>(() => (props.max ? ((state.proxyValue?.count <= props.max) && !!state.proxyValue.count) : true)),
+    isMaxValid: computed<boolean>(() => (props.max ? ((state.proxyValue?.count <= props.max) && !!state.proxyValue?.count) : true)),
     tooltipDesc: computed(() => i18n.t('COMMON.WIDGETS.MAX_ITEMS_DESC', {
         fieldName: props.fieldName,
         max: props.max,
@@ -53,7 +53,7 @@ const handleUpdateCount = (val: number) => {
 /* Watcher */
 watch(() => state.isAllValid, (isValid) => {
     emit('update:is-valid', isValid);
-});
+}, { immediate: true });
 
 /* Init */
 onMounted(() => {
