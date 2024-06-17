040d5a0803342ae186ee3f7d03e1cc2b5c64e094
yuda <yuda@megazone.com>
2024-06-17 15:34:37 +0900

    feat: implement table loadWidget
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/common/modules/widgets/_widgets/table/Table.vue
+++ apps/web/src/common/modules/widgets/_widgets/table/Table.vue
@@ -1,20 +1,29 @@
 <script setup lang="ts">
 import {
-    defineExpose, reactive,
+    defineExpose, reactive, computed,
 } from 'vue';
 
 import {
     PDataLoader,
 } from '@spaceone/design-system';
 
-import type { ListResponse } from '@/schema/_common/api-verbs/list';
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 
+import type { ListResponse } from '@/schema/_common/api-verbs/list';
+import type { PrivateWidgetLoadParameters } from '@/schema/dashboard/private-widget/api-verbs/load';
+import type { PublicWidgetLoadParameters } from '@/schema/dashboard/public-widget/api-verbs/load';
+
+import type { APIErrorToast } from '@/common/composables/error/errorHandler';
+import ErrorHandler from '@/common/composables/error/errorHandler';
 import WidgetFrame from '@/common/modules/widgets/_components/WidgetFrame.vue';
 import { useWidgetInitAndRefresh } from '@/common/modules/widgets/_composables/use-widget-init-and-refresh';
 import { useWidgetFrame } from '@/common/modules/widgets/_composables/use-widget/use-widget-frame';
+import { getWidgetBasedOnDate } from '@/common/modules/widgets/_helpers/widget-date-helper';
+import type { DateRange } from '@/common/modules/widgets/types/widget-data-type';
 import type {
     WidgetProps, WidgetEmit, WidgetExpose,
 } from '@/common/modules/widgets/types/widget-display-type';
+import type { GroupByValue, TableDataFieldValue } from '@/common/modules/widgets/types/widget-field-value-type';
 
 
 type Data = ListResponse<{
@@ -29,9 +38,60 @@ const { widgetFrameProps, widgetFrameEventHandlers } = useWidgetFrame(props, emi
 const state = reactive({
     loading: false,
     data: null as Data | null,
+    //
+    granularity: computed<string>(() => props.widgetOptions?.granularity as string),
+    basedOnDate: computed(() => getWidgetBasedOnDate(state.granularity, props.dashboardOptions?.date_range?.end)),
+    tableDataField: computed<string|string[]|undefined>(() => (props.widgetOptions?.tableDataField as TableDataFieldValue)?.value),
+    groupByField: computed<string[]|undefined>(() => (props.widgetOptions?.groupBy as GroupByValue)?.value as string[]|undefined),
+    dateRange: computed<DateRange>(() => {
+        const _start = state.basedOnDate;
+        const _end = state.basedOnDate;
+        // if (state.xAxisField === DATE_FIELD) {
+        //     [_start, _end] = getWidgetDateRange(state.granularity, state.basedOnDate, state.xAxisCount);
+        // }
+        return { start: _start, end: _end };
+    }),
 });
 
-const loadWidget = async (data?: Data): Promise<Data> => data;
+const fetchWidget = async (): Promise<Data|APIErrorToast> => {
+    try {
+        state.loading = true;
+        const _isPrivate = props.widgetId.startsWith('private');
+        const _fetcher = _isPrivate
+            ? SpaceConnector.clientV2.dashboard.privateWidget.load<PrivateWidgetLoadParameters, Data>
+            : SpaceConnector.clientV2.dashboard.publicWidget.load<PublicWidgetLoadParameters, Data>;
+        const _fields = {};
+        if (Array.isArray(state.tableDataField)) {
+            state.tableDataField?.forEach((field) => {
+                _fields[field] = { key: field, operator: 'sum' };
+            });
+        } else {
+            _fields[state.tableDataField] = { key: state.tableDataField, operator: 'sum' };
+        }
+        return await _fetcher({
+            widget_id: props.widgetId,
+            query: {
+                granularity: state.granularity,
+                start: state.dateRange.start,
+                end: state.dateRange.end,
+                group_by: state.groupByField,
+                fields: _fields,
+            },
+            vars: props.dashboardVariables,
+        });
+    } catch (e) {
+        ErrorHandler.handleError(e);
+        return ErrorHandler.makeAPIErrorToast(e);
+    } finally {
+        state.loading = false;
+    }
+};
+const loadWidget = async (data?: Data): Promise<Data|APIErrorToast> => {
+    const res = data ?? await fetchWidget();
+    if (typeof res === 'function') return res;
+    state.data = res;
+    return state.data;
+};
 
 useWidgetInitAndRefresh({ props, emit, loadWidget });
 defineExpose<WidgetExpose<Data>>({
@@ -51,7 +111,7 @@ defineExpose<WidgetExpose<Data>>({
                        show-data-from-scratch
         >
             <div class="chart">
-                Table
+                {{ state.data }}
             </div>
         </p-data-loader>
     </widget-frame>
