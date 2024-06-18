bcff676d141f354f4d0d89d1af5df38f9092195d
nayeongkim <nayeongkim@megazone.com>
2024-06-18 15:14:24 +0900

    feat: add recipients form
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

---
+++ apps/web/src/services/cost-explorer/components/AnomalyDetectionConfigurationRecipientsForm.vue
@@ -0,0 +1,167 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+import {
+    PHeading, PFieldGroup, PSelectDropdown, PTextHighlighting, PBadge,
+} from '@spaceone/design-system';
+import type { SelectDropdownMenuItem, AutocompleteHandler } from '@spaceone/design-system/types/inputs/dropdown/select-dropdown/type';
+
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+import { ApiQueryHelper } from '@cloudforet/core-lib/space-connector/helper';
+
+import type { ListResponse } from '@/schema/_common/api-verbs/list';
+import type { RoleListParameters } from '@/schema/identity/role/api-verbs/list';
+import { ROLE_STATE, ROLE_TYPE } from '@/schema/identity/role/constant';
+import type { RoleModel } from '@/schema/identity/role/model';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+
+import { useRoleFormatter } from '@/services/iam/composables/refined-table-data';
+
+interface DropdownMenuItem extends SelectDropdownMenuItem {
+    role_type?: string;
+}
+
+const roleListApiQueryHelper = new ApiQueryHelper()
+    .setPageStart(1).setPageLimit(15)
+    .setSort('name', true);
+
+const dropdownState = reactive({
+    loading: false,
+    menuItems: [] as DropdownMenuItem[],
+    selectedMenuItems: [] as DropdownMenuItem[],
+});
+
+const menuHandler: AutocompleteHandler = async (inputText: string) => {
+    await fetchListRoles(inputText);
+    return {
+        results: dropdownState.menuItems,
+    };
+};
+const fetchListRoles = async (inputText?: string) => {
+    dropdownState.loading = true;
+
+    roleListApiQueryHelper.setFilters([
+        { k: 'role_type', v: [ROLE_TYPE.WORKSPACE_OWNER], o: '=' },
+        { k: 'state', v: ROLE_STATE.ENABLED, o: '=' },
+    ]);
+    if (inputText) {
+        roleListApiQueryHelper.addFilter({
+            k: 'name',
+            v: inputText,
+            o: '',
+        });
+    }
+
+    try {
+        const { results } = await SpaceConnector.clientV2.identity.role.list<RoleListParameters, ListResponse<RoleModel>>({
+            query: roleListApiQueryHelper.data,
+        });
+        dropdownState.menuItems = (results ?? []).map((role) => ({
+            label: role.name,
+            name: role.role_id,
+            role_type: role.role_type,
+        }));
+    } catch (e) {
+        ErrorHandler.handleError(e);
+    } finally {
+        dropdownState.loading = false;
+    }
+};
+</script>
+
+<template>
+    <div class="anomaly-detection-configuration-recipients-form">
+        <p-heading :title="$t('BILLING.COST_MANAGEMENT.ANOMALY_DETECTION.CONFIG.RECIPIENTS')"
+                   heading-type="sub"
+                   class="heading"
+        />
+        <p-field-group :label="$t('BILLING.COST_MANAGEMENT.ANOMALY_DETECTION.CONFIG.COL_SEND_TO')"
+                       required
+                       class="field"
+        >
+            <p-select-dropdown class="select-options-dropdown"
+                               multi-selectable
+                               use-fixed-menu-style
+                               show-select-marker
+                               appearance-type="badge"
+                               is-filterable
+                               :loading="dropdownState.loading"
+                               :search-text.sync="dropdownState.searchText"
+                               :selected.sync="dropdownState.selectedMenuItems"
+                               :handler="menuHandler"
+            >
+                <template #dropdown-button>
+                    <div v-if="dropdownState.selectedMenuItems.length > 0"
+                         class="selected-workspace-wrapper"
+                    >
+                        <img :src="useRoleFormatter(dropdownState.selectedMenuItems[0].role_type).image"
+                             alt="role-type-icon"
+                             class="role-type-icon"
+                        >
+                        <span>{{ dropdownState.selectedMenuItems[0].label }}</span>
+                        <p-badge v-if="dropdownState.selectedMenuItems.length > 1"
+                                 style-type="blue200"
+                                 badge-type="subtle"
+                        >
+                            + {{ dropdownState.selectedMenuItems.length - 1 }}
+                        </p-badge>
+                    </div>
+                    <span v-else
+                          class="placeholder"
+                    >
+                        {{ $t('BILLING.COST_MANAGEMENT.ANOMALY_DETECTION.CONFIG.COL_SEND_TO_PLACEHOLDER') }}
+                    </span>
+                </template>
+                <template #menu-item--format="{item}">
+                    <div class="role-menu-item">
+                        <img :src="useRoleFormatter(item.role_type).image"
+                             alt="role-type-icon"
+                             class="role-type-icon"
+                        >
+                        <p-text-highlighting class="title"
+                                             :term="dropdownState.searchText"
+                                             :text="item.label"
+                        />
+                    </div>
+                </template>
+            </p-select-dropdown>
+        </p-field-group>
+    </div>
+</template>
+
+<style scoped lang="postcss">
+.anomaly-detection-configuration-recipients-form {
+    @apply flex flex-col bg-white border border-gray-200;
+    padding: 2rem 1rem 1.5rem;
+    border-radius: 0.375rem;
+    .heading {
+        margin-top: 0;
+        margin-right: 0;
+        margin-left: 0;
+    }
+    .field {
+        max-width: 30rem;
+        margin-top: 0.5rem;
+        .select-options-dropdown {
+            @apply block w-full;
+            .role-menu-item {
+                @apply flex items-center;
+                gap: 0.25rem;
+            }
+            .role-type-icon {
+                @apply rounded-full;
+                width: 1rem;
+                height: 1rem;
+            }
+            .selected-workspace-wrapper {
+                @apply flex items-center;
+                gap: 0.25rem;
+            }
+            .placeholder {
+                @apply text-gray-600;
+            }
+        }
+    }
+}
+</style>
--- apps/web/src/services/cost-explorer/pages/AnomalyDetectionConfigurationCreatePage.vue
+++ apps/web/src/services/cost-explorer/pages/AnomalyDetectionConfigurationCreatePage.vue
@@ -3,14 +3,25 @@ import { PHeading } from '@spaceone/design-system';
 
 import AnomalyDetectionConfigurationInformationForm
     from '@/services/cost-explorer/components/AnomalyDetectionConfigurationInformationForm.vue';
+import AnomalyDetectionConfigurationRecipientsForm
+    from '@/services/cost-explorer/components/AnomalyDetectionConfigurationRecipientsForm.vue';
 </script>
 
 <template>
-    <div>
+    <div class="anomaly-detection-configuration-create-page">
         <p-heading :title="$t('BILLING.COST_MANAGEMENT.ANOMALY_DETECTION.CONFIG.CREATE_TITLE')"
                    show-back-button
                    @click-back-button="$router.go(-1)"
         />
-        <anomaly-detection-configuration-information-form />
+        <anomaly-detection-configuration-information-form class="form" />
+        <anomaly-detection-configuration-recipients-form class="form" />
     </div>
 </template>
+
+<style scoped lang="postcss">
+.anomaly-detection-configuration-create-page {
+    .form + .form {
+        margin-top: 1rem;
+    }
+}
+</style>
