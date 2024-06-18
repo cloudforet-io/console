498043758cfe279f60cc9f5c4a25e524c2874f4b
nayeongkim <nayeongkim@megazone.com>
2024-06-18 13:49:18 +0900

    feat: apply configuration table
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/services/cost-explorer/pages/AnomalyDetectionConfigurationPage.vue
+++ apps/web/src/services/cost-explorer/pages/AnomalyDetectionConfigurationPage.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { PHeading, PButton } from '@spaceone/design-system';
+
+import AnomalyDetectionConfigurationTable
+    from '@/services/cost-explorer/components/AnomalyDetectionConfigurationTable.vue';
 </script>
 
 <template>
-    <div>
+    <div class="anomaly-detection-configuration-page">
         <p-heading :title="$t('BILLING.COST_MANAGEMENT.ANOMALY_DETECTION.CONFIG.TITLE')">
             <template #extra>
                 <p-button icon-left="ic_plus">
@@ -11,5 +14,12 @@ import { PHeading, PButton } from '@spaceone/design-system';
                 </p-button>
             </template>
         </p-heading>
+        <anomaly-detection-configuration-table />
     </div>
 </template>
+
+<style scoped lang="postcss">
+.anomaly-detection-configuration-page {
+    @apply flex flex-col;
+}
+</style>
