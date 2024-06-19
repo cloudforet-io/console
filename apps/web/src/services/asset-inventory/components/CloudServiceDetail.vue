1ba09fe14c3c72be16c57f3b217c5d9de923e4a8
Yongtae Park <samuel.park@megazone.com>
2024-06-19 09:28:13 +0900

    fix: edit only rule in more type field
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/services/asset-inventory/components/CloudServiceDetail.vue
+++ apps/web/src/services/asset-inventory/components/CloudServiceDetail.vue
@@ -194,7 +194,11 @@ const setOnlyQuery = (query:ApiQueryHelper, type?: DynamicLayoutType) => {
     if (type === 'list') return;
     const fields: DynamicField[] = state.currentLayout.options?.fields ?? [];
     const only:string[] = [];
-    fields.forEach((d) => { if (d) only.push(d.key); });
+    fields.forEach((d) => {
+        if (d.type === 'more' && d.options?.sub_key) {
+            only.push(d.options.sub_key);
+        } else if (d) only.push(d.key);
+    });
     query.setOnly(...only);
 };
 const setListQuery = (options, type?:DynamicLayoutType): any => {
