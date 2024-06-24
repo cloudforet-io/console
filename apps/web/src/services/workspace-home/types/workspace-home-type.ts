0bc2a2e7fd2a36014a733d99a0ef6ada9199ce0f
NaYeong,Kim <nayeongkim@megazone.com>
2024-06-24 22:47:44 +0900

    feat: apply qa (#4100)
    * feat: apply button loading
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>
    
    * chore: component separation
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>
    
    * feat: apply loading
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>
    
    * fix: fixed calc bug
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/services/workspace-home/types/workspace-home-type.ts
+++ apps/web/src/services/workspace-home/types/workspace-home-type.ts
@@ -22,6 +22,10 @@ export interface CloudServiceData {
     delete_warning?: boolean;
     display_name?: string;
 }
+export type DailyUpdatesListItem = {
+    created: CloudServiceData[],
+    deleted: CloudServiceData[],
+};
 export type ProviderResourceDataItem = ProviderItem & {
     server?: number;
     database?: number;
