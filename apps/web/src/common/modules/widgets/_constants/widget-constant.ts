7b340d917626d3a77984bc76232ce3f2916827f7
Yongtae Park <samuel.park@megazone.com>
2024-06-24 15:30:05 +0900

    chore: add HACK annotation
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/common/modules/widgets/_constants/widget-constant.ts
+++ apps/web/src/common/modules/widgets/_constants/widget-constant.ts
@@ -1,5 +1,6 @@
 export const DATE_FIELD = 'Date';
 
+// HACK: need to be integrated with "DATE_FIELD" (above), after updating other widgets.s
 export const _DATE_FIELD = {
     DATE: 'Date',
     YEAR: 'Year',
