6b5c9e79e76f8e3f8e133d559a3416d7f2f670d0
Dahyun Yu <yuda@megazone.com>
2024-06-25 11:18:09 +0900

    fix: set multiple full data link, units in metadata popover (#4104)
    * chore: change from 'ETC' to 'etc'
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * fix: set multiple full data link, units in metadata popover
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/common/modules/widgets/_widgets/line-chart/LineChart.vue
+++ apps/web/src/common/modules/widgets/_widgets/line-chart/LineChart.vue
@@ -163,7 +163,7 @@ const drawChart = (rawData: Data|null) => {
     rawData.results?.forEach((d) => {
         const _slicedData = orderBy(d[state.dataField], 'value', 'desc').slice(0, state.lineByCount);
         const _etcData = d[state.dataField].slice(state.lineByCount).reduce((acc, v) => {
-            acc[state.lineByField] = 'ETC';
+            acc[state.lineByField] = 'etc';
             acc.value += v.value;
             return acc;
         }, { value: 0 });
