c82b2bb258ae7e340e95f2dea11b4f6c03736854
nayeongkim <nayeongkim@megazone.com>
2024-06-26 13:07:17 +0900

    feat: create domain settings LSB
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

---
+++ apps/web/src/services/preference/components/DomainSettingsLSB.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { computed, reactive } from 'vue';
+
+import { i18n } from '@/translations';
+
+import { makeAdminRouteName } from '@/router/helpers/route-helper';
+
+import LSB from '@/common/modules/navigations/lsb/LSB.vue';
+import { MENU_ITEM_TYPE } from '@/common/modules/navigations/lsb/type';
+import type { LSBItem } from '@/common/modules/navigations/lsb/type';
+
+import { PREFERENCE_ROUTE } from '@/services/preference/routes/route-constant';
+
+const state = reactive({
+    menuSet: computed<LSBItem[]>(() => [
+        {
+            type: MENU_ITEM_TYPE.ITEM,
+            label: i18n.t('IAM.DOMAIN_SETTINGS.BASE_INFORMATION'),
+            to: { name: makeAdminRouteName(PREFERENCE_ROUTE.DOMAIN_SETTINGS.BASE_INFORMATION._NAME) },
+            hideFavorite: true,
+        },
+        {
+            type: MENU_ITEM_TYPE.ITEM,
+            label: i18n.t('IAM.DOMAIN_SETTINGS.BRAND_ASSETS'),
+            to: { name: makeAdminRouteName(PREFERENCE_ROUTE.DOMAIN_SETTINGS.BRAND_ASSETS._NAME) },
+            hideFavorite: true,
+        },
+        {
+            type: MENU_ITEM_TYPE.ITEM,
+            label: i18n.t('IAM.DOMAIN_SETTINGS.TIMEZONE_AND_LANGUAGE'),
+            to: { name: makeAdminRouteName(PREFERENCE_ROUTE.DOMAIN_SETTINGS.TIMEZONE_AND_LANGUAGE._NAME) },
+            hideFavorite: true,
+        },
+        {
+            type: MENU_ITEM_TYPE.ITEM,
+            label: i18n.t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION'),
+            to: { name: makeAdminRouteName(PREFERENCE_ROUTE.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION._NAME) },
+            hideFavorite: true,
+        },
+    ]),
+});
+</script>
+
+<template>
+    <l-s-b class="domain-settings-l-s-b"
+           :menu-set="state.menuSet"
+    />
+</template>
