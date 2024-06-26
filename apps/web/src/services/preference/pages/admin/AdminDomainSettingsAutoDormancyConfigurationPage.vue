5ffae81d60a02512a3ded26aedc520bc5f7655d1
NaYeong,Kim <nayeongkim@megazone.com>
2024-06-26 15:50:10 +0900

    feat: create auto-dormancy-configuration-page (design-only) (#4130)
    * feat: create auto-dormancy-configuration-page
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * chore: update translations
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/services/preference/pages/admin/AdminDomainSettingsAutoDormancyConfigurationPage.vue
+++ apps/web/src/services/preference/pages/admin/AdminDomainSettingsAutoDormancyConfigurationPage.vue
@@ -1,127 +1,188 @@
 <script lang="ts" setup>
-import { computed, reactive, watch } from 'vue';
+import { computed, reactive } from 'vue';
+import type { TranslateResult } from 'vue-i18n';
 
 import {
-    PPaneLayout, PFieldTitle, PButton, PSelectDropdown,
+    PPaneLayout, PDivider, PTextButton, PCheckbox, PToggleButton, PFieldGroup, PTextInput, PButton, PTooltip, PI,
 } from '@spaceone/design-system';
-import type { SelectDropdownMenuItem } from '@spaceone/design-system/types/inputs/dropdown/select-dropdown/type';
-import { map } from 'lodash';
 
-import { store } from '@/store';
 import { i18n } from '@/translations';
 
-import { useDomainSettingsStore } from '@/store/domain-settings/domain-settings-store';
-import { languages, timezoneList } from '@/store/modules/user/config';
-import type { LanguageCode } from '@/store/modules/user/type';
+import { useFormValidator } from '@/common/composables/form-validator';
 
-import { showSuccessMessage } from '@/lib/helper/notice-alert-helper';
-
-import ErrorHandler from '@/common/composables/error/errorHandler';
-
-
-const domainConfigStore = useDomainSettingsStore();
-const domainConfigGetters = domainConfigStore.getters;
-
-const storeState = reactive({
-    domainConfig: computed(() => store.state.domain.config),
-});
 const state = reactive({
-    isChanged: computed(() => {
-        if ([state.selectedTimezone, state.selectedLanguage,
-            domainConfigGetters.timezone, domainConfigGetters.language]
-            .every((d) => !d)) return false;
-        return (state.selectedTimezone !== domainConfigGetters.timezone)
-            || (state.selectedLanguage !== domainConfigGetters.language);
-    }),
-    selectedTimezone: undefined as string | undefined,
-    selectedLanguage: undefined as LanguageCode | undefined,
-    languageMenuList: map(languages, (d, k) => ({
-        type: 'item', label: k === 'en' ? `${d} (default)` : d, name: k,
-    })) as SelectDropdownMenuItem[],
-    timezoneMenuList: map(timezoneList, (d) => ({
-        type: 'item', label: d === 'UTC' ? `${d} (default)` : d, name: d,
-    })) as SelectDropdownMenuItem[],
-    config: computed(() => ({
-        ...storeState.domainConfig,
-        settings: {
-            timezone: state.selectedTimezone,
-            language: state.selectedLanguage,
-        },
-    })),
+    statusToggle: false,
+    toggleText: computed<TranslateResult>(() => (state.statusToggle
+        ? i18n.t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_TOGGLE _ENABLED')
+        : i18n.t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_TOGGLE _DISABLED')
+    )),
+    checkbox: false,
+    attributes: { type: 'number' },
 });
 
-/* Event */
-const handleSaveChanges = async () => {
-    try {
-        await domainConfigStore.updateDomainSettings({
-            timezone: state.selectedTimezone,
-            language: state.selectedLanguage,
-        });
-        await store.commit('domain/setDomainConfigs', {
-            config: state.config,
-        });
-        showSuccessMessage(i18n.t('IAM.DOMAIN_SETTINGS.ALT_S_UPDATE_TIMEZONE_AND_LANGUAGE'), '');
-    } catch (e) {
-        ErrorHandler.handleRequestError(e, i18n.t('IAM.DOMAIN_SETTINGS.ALT_E_UPDATE_TIMEZONE_AND_LANGUAGE'));
-    }
-};
+const {
+    forms: { cost },
+    invalidState,
+    invalidTexts,
+    setForm,
+    initForm,
+    isAllValid,
+} = useFormValidator({
+    cost: '',
+}, {
+    cost: (val: string) => {
+        if (!val.trim()) return i18n.t('IAM.DOMAIN_SETTINGS.ALT_E_EMPTY_COST');
+        return true;
+    },
+});
 
-/* Watcher */
-watch(() => domainConfigGetters.timezone, (val) => {
-    if (val) state.selectedTimezone = val;
-}, { immediate: true });
-watch(() => domainConfigGetters.language, (val) => {
-    state.selectedLanguage = val;
-}, { immediate: true });
+(() => {
+    initForm();
+})();
 </script>
 
 <template>
     <p-pane-layout class="admin-domain-settings-auto-dormancy-configuration-page">
         <div class="content-wrapper">
-            <div class="field-wrapper">
-                <p-field-title :label="$t('IAM.DOMAIN_SETTINGS.TIMEZONE')" />
-                <p-select-dropdown :menu="state.timezoneMenuList"
-                                   :selected.sync="state.selectedTimezone"
-                                   :page-size="10"
-                                   show-delete-all-button
-                                   is-filterable
+            <div class="toggle-wrapper">
+                <p-toggle-button :value.sync="state.statusToggle"
+                                 class="toggle-button"
                 />
+                <p class="toggle-text">
+                    <strong class="title">{{ $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_TOGGLE') }}</strong>
+                    <span>{{ state.toggleText }}</span>
+                </p>
             </div>
-            <div class="field-wrapper">
-                <p-field-title :label="$t('IAM.DOMAIN_SETTINGS.LANGUAGE')" />
-                <p-select-dropdown :menu="state.languageMenuList"
-                                   :selected.sync="state.selectedLanguage"
-                                   :page-size="10"
-                                   show-delete-all-button
-                                   is-filterable
-                />
+            <div class="cost-wrapper"
+                 :class="{ disabled: !state.statusToggle }"
+            >
+                <p-field-group :label="$t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_COST_THRESHOLD')"
+                               :invalid-text="invalidTexts.cost"
+                               :invalid="invalidState.cost"
+                               required
+                >
+                    <template #label-extra>
+                        <p-tooltip :contents="!state.statusToggle ? '' : $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_COST_THRESHOLD_DESC')"
+                                   position="top-start"
+                                   class="tooltip"
+                        >
+                            <p-i width="1rem"
+                                 height="1rem"
+                                 name="ic_info-circle"
+                                 class="icon"
+                            />
+                        </p-tooltip>
+                    </template>
+                    <template #default="{invalid}">
+                        <p-text-input :value="cost"
+                                      :invalid="invalid"
+                                      :disabled="!state.statusToggle"
+                                      type="number"
+                                      masking-mode
+                                      placeholder="$ USD"
+                                      class="cost-input"
+                                      @update:value="setForm('cost', $event)"
+                        />
+                    </template>
+                </p-field-group>
+                <p-divider />
+                <p-checkbox :value.sync="state.checkbox"
+                            :disabled="!state.statusToggle"
+                >
+                    {{ $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_CHECKBOX') }}
+                </p-checkbox>
+                <p-text-button icon-left="ic_service_cost-report"
+                               style-type="highlight"
+                               class="cost-report-button"
+                >
+                    {{ $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_COST_REPORT') }}
+                </p-text-button>
             </div>
+            <p-button :disabled="!isAllValid"
+                      class="save-button"
+            >
+                {{ $t('IAM.DOMAIN_SETTINGS.SAVE_CHANGE') }}
+            </p-button>
         </div>
-        <p-button :disabled="!state.isChanged"
-                  class="save-button"
-                  @click="handleSaveChanges"
-        >
-            {{ $t('IAM.DOMAIN_SETTINGS.SAVE_CHANGES') }}
-        </p-button>
     </p-pane-layout>
 </template>
 
 <style lang="postcss" scoped>
 .admin-domain-settings-auto-dormancy-configuration-page {
     .content-wrapper {
-        display: flex;
-        flex-direction: column;
+        @apply flex flex-col;
         gap: 1.5rem;
-        max-width: 28.75rem;
-        padding: 1rem;
-        .field-wrapper {
-            .p-field-title {
-                padding-bottom: 0.25rem;
+        padding: 1.5rem 1rem;
+        .toggle-wrapper {
+            @apply flex items-start;
+            gap: 1rem;
+            .toggle-button {
+                padding-top: 0.25rem;
+            }
+            .toggle-text {
+                @apply flex flex-col text-paragraph-md;
+                .title {
+                    @apply text-label-lg;
+                }
+            }
+        }
+        .cost-wrapper {
+            @apply relative flex flex-col text-label-md border border-gray-200;
+            padding: 1.5rem;
+            gap: 1.5rem;
+            border-radius: 0.375rem;
+            .cost-report-button {
+                @apply absolute;
+                top: 1.5rem;
+                right: 1.5rem;
+            }
+            .icon {
+                margin-top: -0.25rem;
+            }
+
+            &.disabled {
+                @apply bg-gray-100;
+
+                /* custom design-system component - p-field-group */
+                :deep(.p-field-group) {
+                    .title-wrapper {
+                        .title {
+                            @apply text-gray-600;
+                        }
+                    }
+                }
+                .tooltip, .cost-input {
+                    @apply cursor-not-allowed;
+                }
+            }
+
+            /* custom design-system component - p-text-input */
+            :deep(.p-text-input) {
+                .delete-all-icon {
+                    @apply hidden;
+                }
+                input[type="number"] {
+                    -moz-appearance: textfield;
+                    appearance: textfield;
+                    width: 100%;
+                }
+
+                input[type="number"]::-webkit-outer-spin-button,
+                input[type="number"]::-webkit-inner-spin-button {
+                    -webkit-appearance: none;
+                    margin: 0;
+                }
+
+                input::placeholder {
+                    @apply absolute text-right;
+                    right: 0.125rem;
+                }
             }
         }
     }
     .save-button {
-        margin: 1.25rem 1rem;
+        width: 7.5rem;
+        margin-bottom: 1rem;
     }
 }
 </style>
