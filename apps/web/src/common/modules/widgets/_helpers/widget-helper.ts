d80e2bbdb1bab254c05528c85dff616d2518b078
Yongtae Park <samuel.park@megazone.com>
2024-06-27 17:17:22 +0900

    feat: create widget data table helper
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/common/modules/widgets/_helpers/widget-helper.ts
@@ -0,0 +1,35 @@
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+
+import type { PrivateDataTableModel } from '@/schema/dashboard/private-data-table/model';
+import type { DataTableGetParameters } from '@/schema/dashboard/public-data-table/api-verbs/get';
+import type { PublicDataTableModel } from '@/schema/dashboard/public-data-table/model';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+import { DATE_FIELD } from '@/common/modules/widgets/_constants/widget-constant';
+import type { WidgetDataTableModel } from '@/common/modules/widgets/types/widget-data-type';
+
+export const getWidgetDataTable = async (widgetId: string): Promise<WidgetDataTableModel|undefined> => {
+    const isPrivate = widgetId.startsWith('private-');
+    const fetcher = isPrivate
+        ? SpaceConnector.clientV2.dashboard.privateDataTable.get<DataTableGetParameters, PrivateDataTableModel>
+        : SpaceConnector.clientV2.dashboard.publicDataTable.get<DataTableGetParameters, PublicDataTableModel>;
+    try {
+        const result = await fetcher({ data_table_id: widgetId });
+        return result;
+    } catch (e) {
+        ErrorHandler.handleError(e);
+        return undefined;
+    }
+};
+
+export const sortWidgetTableFields = (fields: string[]) => {
+    const priorityFields = Object.values(DATE_FIELD) as string[];
+    const prioritySet = new Set(priorityFields);
+
+    const priority = fields.filter((field) => prioritySet.has(field));
+    const others = fields.filter((field) => !prioritySet.has(field)).sort();
+
+    const sortedPriority = priorityFields.filter((field) => priority.includes(field));
+
+    return [...sortedPriority, ...others];
+};
