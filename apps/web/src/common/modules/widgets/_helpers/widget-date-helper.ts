3cd0d9a4cb598a1a470a637979d2f183e9063ac0
Piggy Park (박용태) <samuel.park@megazone.com>
2024-06-30 20:44:03 +0900

    fix: edit `getWidgetBasedOnDate` helper (#4180)
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/common/modules/widgets/_helpers/widget-date-helper.ts
+++ apps/web/src/common/modules/widgets/_helpers/widget-date-helper.ts
@@ -26,7 +26,11 @@ export const getDateLabelFormat = (granularity: string): string => {
  */
 export const getWidgetBasedOnDate = (granularity: string, end?: string): string => {
     const _dateFormat = getDateFormat(granularity);
-    if (end) return dayjs.utc(end).format(_dateFormat);
+    if (end) {
+        if (granularity === 'DAILY') return dayjs.utc(end).endOf('month').format(_dateFormat);
+        if (granularity === 'MONTHLY') return dayjs.utc(end).endOf('year').format(_dateFormat);
+        return dayjs.utc(end).format(_dateFormat);
+    }
     return dayjs.utc().format(_dateFormat);
 };
 
