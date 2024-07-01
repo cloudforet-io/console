f6b1ebee2a9d17c77b11d568bc94c922cf87184a
yuda <yuda@megazone.com>
2024-07-01 11:51:22 +0900

    fix: fix reorder sidebar bug
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/common/modules/widgets/_helpers/widget-config-helper.ts
+++ apps/web/src/common/modules/widgets/_helpers/widget-config-helper.ts
@@ -4,7 +4,8 @@ import type { WidgetConfig } from '@/common/modules/widgets/types/widget-config-
 
 const consoleWidgetConfigCacheMap = new Map<string, WidgetConfig>();
 
-export const getWidgetConfig = (widgetName: string): WidgetConfig => {
+export const getWidgetConfig = (widgetName?: string): WidgetConfig|undefined => {
+    if (!widgetName) return undefined;
     if (consoleWidgetConfigCacheMap.has(widgetName)) return consoleWidgetConfigCacheMap.get(widgetName) as WidgetConfig;
 
     const config = CONSOLE_WIDGET_CONFIG[widgetName] as WidgetConfig;
