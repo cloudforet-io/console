93c5b5f0641b75f417b8d5e674ee63d72aeda567
Dahyun Yu <yuda@megazone.com>
2024-07-02 17:32:02 +0900

    feat: do not load unless widget has all required fields (#4220)
    * feat: do not load unless widget has all required fields
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * chore: fix minor
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * refactor: check `allRequiredFieldsFilled` in each widget
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/common/modules/widgets/_widgets/stacked-area-chart/StackedAreaChart.vue
+++ apps/web/src/common/modules/widgets/_widgets/stacked-area-chart/StackedAreaChart.vue
@@ -28,6 +28,7 @@ import { useWidgetFrame } from '@/common/modules/widgets/_composables/use-widget
 import { useWidgetInitAndRefresh } from '@/common/modules/widgets/_composables/use-widget-init-and-refresh';
 import { DATE_FIELD } from '@/common/modules/widgets/_constants/widget-constant';
 import {
+    getAllRequiredFieldsFilled,
     getDateLabelFormat, getReferenceLabel,
     getWidgetBasedOnDate,
     getWidgetDateFields,
@@ -50,6 +51,7 @@ const chartContext = ref<HTMLElement|null>(null);
 const state = reactive({
     loading: false,
     errorMessage: undefined as string|undefined,
+    allRequiredFieldsFilled: computed(() => getAllRequiredFieldsFilled(props.widgetName, props.widgetOptions)),
     data: null as Data | null,
     chart: null as EChartsType | null,
     xAxisData: [],
@@ -128,7 +130,8 @@ const { widgetFrameProps, widgetFrameEventHandlers } = useWidgetFrame(props, emi
 });
 
 /* Util */
-const fetchWidget = async (): Promise<Data|APIErrorToast> => {
+const fetchWidget = async (): Promise<Data|APIErrorToast|undefined> => {
+    if (!state.allRequiredFieldsFilled) return undefined;
     try {
         const _isPrivate = props.widgetId.startsWith('private');
         const _fetcher = _isPrivate
