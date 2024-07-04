ce8d965cea18a91f6d5cae2e77de8f4f993a2ee2
yuda <yuda@megazone.com>
2024-07-04 23:12:20 +0900

    feat: add field menu items helper
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/common/modules/widgets/_helpers/widget-field-helper.ts
+++ apps/web/src/common/modules/widgets/_helpers/widget-field-helper.ts
@@ -8,3 +8,14 @@ export const getDefaultMenuItemIndex = (menuItems: MenuItem[], idx = 0, excludeD
     const _dateFieldCount = menuItems.filter((item) => Object.values(DATE_FIELD).includes(item.name as string)).length;
     return idx + _dateFieldCount;
 };
+
+export const getInitialSelectedMenuItem = (menuItems: MenuItem[], data: string[]|string, idx = 0): string|Array<string|undefined>|undefined => {
+    if (Array.isArray(data)) {
+        const isAllDataIncluded = data.every((d) => menuItems.some((m) => m.name === d));
+        if (isAllDataIncluded) return data;
+        return [menuItems[idx]?.name];
+    }
+    const isAllDataIncluded = menuItems.some((m) => m.name === data);
+    if (isAllDataIncluded) return data;
+    return menuItems[idx]?.name;
+};
