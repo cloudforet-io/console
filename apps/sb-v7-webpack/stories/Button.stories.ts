7311f4255011e9d0c59f0f7ec3e89f6bfe67c4c8
yubeom <kimub1204@mz.co.kr>
2024-07-08 18:14:30 +0900

    feat: webpack5 storybook 추가

---
+++ apps/sb-v7-webpack/stories/Button.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/vue';
+
+import Button  from './Button.vue';
+
+const meta : Meta<typeof Button> = {
+    component: Button,
+}
+
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+ 
+export const Primary: Story = {
+};
