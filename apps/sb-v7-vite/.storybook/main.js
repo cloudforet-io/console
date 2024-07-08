ad7bcf443a7a76926b9b080564928d4dcf1823e0
ubeom kim <kimub1204@gmail.com>
2024-07-09 00:30:27 +0900

    feat: vite storybook 추가

--- apps/sb-v7-vite/.storybook/main.js
+++ apps/sb-v7-vite/.storybook/main.js
@@ -15,13 +15,16 @@ const config = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
   ],
   framework: {
-    name: "@storybook/vue-vite",
+    name: getAbsolutePath("@storybook/vue-vite"),
     options: {},
-  }
+  },
+  // docs: {
+  //   autodocs: "tag",
+  // },
 };
 export default config;
