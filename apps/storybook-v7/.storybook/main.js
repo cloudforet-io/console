17d89b0af4f493195673c9517f1e234db84664b6
yubeom <kimub1204@mz.co.kr>
2024-07-08 17:51:36 +0900

    feat: Button 컴포넌트, 스토리, mdx 추가

---
+++ apps/storybook-v7/.storybook/main.js
@@ -0,0 +1,30 @@
+import { join, dirname } from "path";
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+
+/** @type { import('@storybook/vue-vite').StorybookConfig } */
+const config = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/vue-vite"),
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
---
+++ apps/storybook-v7/stories/Button.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <button class="primary">버튼</button>
+</template>
+
+<style lang="postcss">
+.primary {
+    padding: 8px;
+}
+</style>
+
