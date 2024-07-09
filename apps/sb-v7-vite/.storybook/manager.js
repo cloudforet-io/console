00f9f12c7ed059e5ce82de61d13be2a74416fb64
yubeom <kimub1204@mz.co.kr>
2024-07-09 18:00:38 +0900

    feat: logo 이미지 추가

---
+++ apps/sb-v7-vite/.storybook/manager.js
@@ -0,0 +1,10 @@
+import { addons } from '@storybook/manager-api';
+
+ 
+import SpaceOneTheme from './CloudforetTheme';
+
+window.STORYBOOK_GA_ID = 'UA-159327743-4';
+
+addons.setConfig({
+  theme: SpaceOneTheme
+});
