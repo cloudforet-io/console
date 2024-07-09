499ac6a8c32fc9d7dbe503c02a30436b205aa757
yubeom <kimub1204@mz.co.kr>
2024-07-09 14:01:52 +0900

    feat: story v7, mirinae 컴포넌트 호출

--- apps/sb-v7-vite/.storybook/main.js
+++ apps/sb-v7-vite/.storybook/main.js
@@ -11,8 +11,8 @@ function getAbsolutePath(value) {
 /** @type { import('@storybook/vue-vite').StorybookConfig } */
 const config = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    '../../../packages/mirinae/src/yubeom/**/*.mdx',
+    '../../../packages/mirinae/src/yubeom/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
@@ -22,7 +22,7 @@ const config = {
   framework: {
     name: getAbsolutePath("@storybook/vue-vite"),
     options: {},
-  },
+  }
   // docs: {
   //   autodocs: "tag",
   // },
--- apps/sb-v7-vite/vite.config.js
+++ apps/sb-v7-vite/vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue2';
+import commonjs from '@rollup/plugin-commonjs';
+import path from 'path';
 
 export default defineConfig({
     plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '../../packages/mirinae/src'),
+            "@storybook/blocks": path.resolve(__dirname, "./node_modules/@storybook/blocks"),
+        },
+    },
+    optimizeDeps: {
+        include: ['@/styles/colors.cjs'],
+    },
+    rollupInputOptions: {
+        plugins: [commonjs()]
+    }
 });
