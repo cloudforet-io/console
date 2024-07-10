a3ffc37e90cf256784cb59cfeecc436955b3f099
yubeom <kimub1204@mz.co.kr>
2024-07-10 16:56:24 +0900

    remove: preview-head, preview-body 파일 제거

--- apps/sb-v7-vite/.storybook/main.js
+++ apps/sb-v7-vite/.storybook/main.js
@@ -17,7 +17,33 @@ const config = {
     getAbsolutePath("@storybook/addon-designs"),
     getAbsolutePath("@storybook/addon-storysource")
   ],
-  framework: getAbsolutePath("@storybook/vue-vite")
+  framework: getAbsolutePath("@storybook/vue-vite"),
+  previewHead: (head) => {
+    return `
+      ${head}
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <link rel='shortcut icon' type='image/x-icon' href="<%= BASE_URL %>favicon.ico">
+      <link rel='manifest' type='image/x-icon' href="<%= BASE_URL %>manifest.json">
+      <link rel="apple-touch-icon" href="<%= BASE_URL %>images/icons/icon-192x192.png">
+      `
+  },
+  previewBody: (body) => {
+    return `
+      ${body}
+      <style>
+        .sb-show-main.sb-main-centered #storybook-root {
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-wrap: wrap;
+        }
+      </style>
+    `
+  }
 };
 
 export default config;
