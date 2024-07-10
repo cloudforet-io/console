cb9335d1bf7c2a70764dbadda43b1edca5f3ec27
yubeom <kimub1204@mz.co.kr>
2024-07-10 16:39:06 +0900

    feat: Controls 기능 추가

--- apps/sb-v7-vite/.storybook/main.js
+++ apps/sb-v7-vite/.storybook/main.js
@@ -1,9 +1,5 @@
 import { join, dirname } from "path";
 
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
 function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, "package.json")));
 }
@@ -21,9 +17,7 @@ const config = {
     getAbsolutePath("@storybook/addon-designs"),
     getAbsolutePath("@storybook/addon-storysource")
   ],
-  framework: {
-    name: getAbsolutePath("@storybook/vue-vite"),
-    options: {},
-  }
+  framework: getAbsolutePath("@storybook/vue-vite")
 };
+
 export default config;
