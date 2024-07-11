1598429853bfb7bc12206ff58845dd9fc4860a21
NaYeong,Kim <nayeongkim@megazone.com>
2024-07-11 14:43:13 +0900

    feat: changed design (#4308)
    * feat: changed design
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * chore: changed LSB menu name
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * chore: changed domain name, timezone position
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

---
+++ apps/web/src/services/preference/pages/admin/AdminDomainSettingsDomainInformationPage.vue
@@ -0,0 +1,146 @@
+<script lang="ts" setup>
+import { computed, reactive, watch } from 'vue';
+
+import {
+    PPaneLayout, PFieldTitle, PCopyButton, PButton, PSelectDropdown,
+} from '@spaceone/design-system';
+import type { SelectDropdownMenuItem } from '@spaceone/design-system/types/inputs/dropdown/select-dropdown/type';
+import { map } from 'lodash';
+
+import { store } from '@/store';
+import { i18n } from '@/translations';
+
+import { useDomainSettingsStore } from '@/store/domain-settings/domain-settings-store';
+import { languages, timezoneList } from '@/store/modules/user/config';
+import type { LanguageCode } from '@/store/modules/user/type';
+
+import { showSuccessMessage } from '@/lib/helper/notice-alert-helper';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+
+const domainConfigStore = useDomainSettingsStore();
+const domainConfigGetters = domainConfigStore.getters;
+const storeState = reactive({
+    domainId: computed<string>(() => store.state.domain.domainId),
+    domainName: computed<string>(() => store.state.domain.name),
+    domainConfig: computed(() => store.state.domain.config),
+});
+const state = reactive({
+
+    isChanged: computed(() => {
+        if ([state.selectedTimezone, state.selectedLanguage,
+            domainConfigGetters.timezone, domainConfigGetters.language]
+            .every((d) => !d)) return false;
+        return (state.selectedTimezone !== domainConfigGetters.timezone)
+            || (state.selectedLanguage !== domainConfigGetters.language);
+    }),
+    selectedTimezone: undefined as string | undefined,
+    selectedLanguage: undefined as LanguageCode | undefined,
+    languageMenuList: computed<SelectDropdownMenuItem[]>(() => map(languages, (d, k) => ({
+        type: 'item', label: k === 'en' ? `${d} (default)` : d, name: k,
+    }))),
+    timezoneMenuList: computed<SelectDropdownMenuItem[]>(() => map(timezoneList, (d) => ({
+        type: 'item', label: d === 'UTC' ? `${d} (default)` : d, name: d,
+    }))),
+    config: computed(() => ({
+        ...storeState.domainConfig,
+        settings: {
+            timezone: state.selectedTimezone,
+            language: state.selectedLanguage,
+        },
+    })),
+});
+
+/* Event */
+const handleSaveChanges = async () => {
+    try {
+        await domainConfigStore.updateDomainSettings({
+            timezone: state.selectedTimezone,
+            language: state.selectedLanguage,
+        });
+        await store.commit('domain/setDomainConfigs', {
+            config: state.config,
+        });
+        showSuccessMessage(i18n.t('IAM.DOMAIN_SETTINGS.ALT_S_UPDATE_TIMEZONE_AND_LANGUAGE'), '');
+    } catch (e) {
+        ErrorHandler.handleRequestError(e, i18n.t('IAM.DOMAIN_SETTINGS.ALT_E_UPDATE_TIMEZONE_AND_LANGUAGE'));
+    }
+};
+
+/* Watcher */
+watch(() => domainConfigGetters.timezone, (val) => {
+    if (val) state.selectedTimezone = val;
+}, { immediate: true });
+watch(() => domainConfigGetters.language, (val) => {
+    state.selectedLanguage = val;
+}, { immediate: true });
+</script>
+
+<template>
+    <p-pane-layout class="admin-domain-settings-base-information-page">
+        <div class="content-wrapper">
+            <div class="field-wrapper">
+                <p-field-title :label="$t('IAM.DOMAIN_SETTINGS.DOMAIN_ID')" />
+                <p class="input-wrapper">
+                    {{ storeState.domainId }}
+                    <p-copy-button :value="storeState.domainId" />
+                </p>
+            </div>
+            <div class="field-wrapper">
+                <p-field-title :label="$t('IAM.DOMAIN_SETTINGS.DOMAIN_NAME')" />
+                <p class="input-wrapper">
+                    {{ storeState.domainName }}
+                    <p-copy-button :value="storeState.domainName" />
+                </p>
+            </div>
+            <div class="field-wrapper">
+                <p-field-title :label="$t('IAM.DOMAIN_SETTINGS.TIMEZONE')" />
+                <p-select-dropdown :menu="state.timezoneMenuList"
+                                   :selected.sync="state.selectedTimezone"
+                                   :page-size="10"
+                                   show-delete-all-button
+                                   is-filterable
+                />
+            </div>
+            <div class="field-wrapper">
+                <p-field-title :label="$t('IAM.DOMAIN_SETTINGS.LANGUAGE')" />
+                <p-select-dropdown :menu="state.languageMenuList"
+                                   :selected.sync="state.selectedLanguage"
+                                   :page-size="10"
+                                   show-delete-all-button
+                                   is-filterable
+                />
+            </div>
+        </div>
+        <p-button :disabled="!state.isChanged"
+                  class="save-button"
+                  @click="handleSaveChanges"
+        >
+            {{ $t('IAM.DOMAIN_SETTINGS.SAVE_CHANGES') }}
+        </p-button>
+    </p-pane-layout>
+</template>
+
+<style lang="postcss" scoped>
+.admin-domain-settings-base-information-page {
+    .content-wrapper {
+        @apply flex flex-col;
+        width: 15rem;
+        gap: 1.5rem;
+        padding: 1rem;
+        .field-wrapper {
+            .input-wrapper {
+                @apply flex items-center;
+                gap: 0.25rem;
+                padding-top: 0.25rem;
+            }
+            .p-field-title {
+                padding-bottom: 0.25rem;
+            }
+        }
+    }
+    .save-button {
+        margin: 0.5rem 1rem 3rem;
+    }
+}
+</style>
--- apps/web/src/services/preference/routes/admin/routes.ts
+++ apps/web/src/services/preference/routes/admin/routes.ts
@@ -9,9 +9,8 @@ import { PREFERENCE_ROUTE } from '@/services/preference/routes/route-constant';
 
 const PreferenceContainer = () => import('@/services/preference/PreferenceContainer.vue');
 const DomainSettingsContainer = () => import('@/services/preference/components/DomainSettingsContainer.vue');
-const AdminDomainSettingsBaseInformationPage = () => import('@/services/preference/pages/admin/AdminDomainSettingsBaseInformationPage.vue');
-const AdminDomainSettingsBrandAssetsPage = () => import('@/services/preference/pages/admin/AdminDomainSettingsBrandAssetsPage.vue');
-const AdminDomainSettingsTimezoneAndLanguagePage = () => import('@/services/preference/pages/admin/AdminDomainSettingsTimezoneAndLanguagePage.vue');
+const AdminDomainSettingsBaseInformationPage = () => import('@/services/preference/pages/admin/AdminDomainSettingsDomainInformationPage.vue');
+const AdminDomainSettingsBrandAssetsPage = () => import('@/services/preference/pages/admin/AdminDomainSettingsAppearancePage.vue');
 const AdminDomainSettingsAutoDormancyConfigurationPage = () => import('@/services/preference/pages/admin/AdminDomainSettingsAutoDormancyConfigurationPage.vue');
 const WorkspacesPage = () => import('@/services/preference/pages/admin/AdminWorkspacesPage.vue');
 // const BookmarkPage = () => import('@/services/preference/pages/admin/AdminBookmarkPage.vue');
@@ -79,27 +78,21 @@ const adminPreferenceRoutes: RouteConfig = {
                 translationId: MENU_INFO_MAP[MENU_ID.DOMAIN_SETTINGS].translationId,
             },
             redirect: () => ({
-                name: makeAdminRouteName(PREFERENCE_ROUTE.DOMAIN_SETTINGS.BASE_INFORMATION._NAME),
+                name: makeAdminRouteName(PREFERENCE_ROUTE.DOMAIN_SETTINGS.DOMAIN_INFORMATION._NAME),
             }),
             component: DomainSettingsContainer,
             children: [
                 {
-                    path: '/base-information',
-                    name: makeAdminRouteName(PREFERENCE_ROUTE.DOMAIN_SETTINGS.BASE_INFORMATION._NAME),
-                    meta: { lsbVisible: true, translationId: MENU_INFO_MAP[MENU_ID.BASE_INFORMATION].translationId },
+                    path: '/domain-information',
+                    name: makeAdminRouteName(PREFERENCE_ROUTE.DOMAIN_SETTINGS.DOMAIN_INFORMATION._NAME),
+                    meta: { lsbVisible: true, translationId: MENU_INFO_MAP[MENU_ID.DOMAIN_INFORMATION].translationId },
                     component: AdminDomainSettingsBaseInformationPage,
                 },
                 {
-                    path: '/brand-assets',
-                    name: makeAdminRouteName(PREFERENCE_ROUTE.DOMAIN_SETTINGS.BRAND_ASSETS._NAME),
-                    meta: { lsbVisible: true, translationId: MENU_INFO_MAP[MENU_ID.BRAND_ASSETS].translationId },
+                    path: '/appearance',
+                    name: makeAdminRouteName(PREFERENCE_ROUTE.DOMAIN_SETTINGS.APPEARANCE._NAME),
+                    meta: { lsbVisible: true, translationId: MENU_INFO_MAP[MENU_ID.APPEARANCE].translationId },
                     component: AdminDomainSettingsBrandAssetsPage,
-                },
-                {
-                    path: '/timezone-and-language',
-                    name: makeAdminRouteName(PREFERENCE_ROUTE.DOMAIN_SETTINGS.TIMEZONE_AND_LANGUAGE._NAME),
-                    meta: { lsbVisible: true, translationId: MENU_INFO_MAP[MENU_ID.TIMEZONE_AND_LANGUAGE].translationId },
-                    component: AdminDomainSettingsTimezoneAndLanguagePage,
                 },
                 {
                     path: '/auto-dormancy-configuration',
