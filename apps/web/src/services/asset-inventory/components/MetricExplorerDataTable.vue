19192d103e5a00b69cf9ed886c54b157b6bf80f7
yuda <yuda@megazone.com>
2024-07-11 15:57:17 +0900

    chore: get reference label to metric data table
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/asset-inventory/components/MetricExplorerDataTable.vue
+++ apps/web/src/services/asset-inventory/components/MetricExplorerDataTable.vue
@@ -26,6 +26,7 @@ import type { ExcelDataField } from '@/lib/helper/file-download-helper/type';
 
 import ErrorHandler from '@/common/composables/error/errorHandler';
 import { useProperRouteLocation } from '@/common/composables/proper-route-location';
+import { getReferenceLabel } from '@/common/modules/widgets/_helpers/widget-date-helper';
 
 import { GRANULARITY, SIZE_UNITS } from '@/services/asset-inventory/constants/asset-analysis-constant';
 import {
@@ -35,6 +36,10 @@ import {
 import { ASSET_INVENTORY_ROUTE } from '@/services/asset-inventory/routes/route-constant';
 import { useMetricExplorerPageStore } from '@/services/asset-inventory/stores/metric-explorer-page-store';
 import type { MetricDataAnalyzeResult } from '@/services/asset-inventory/types/asset-analysis-type';
+import type { AllReferenceTypeInfo } from '@/services/dashboards/stores/all-reference-type-info-store';
+import {
+    useAllReferenceTypeInfoStore,
+} from '@/services/dashboards/stores/all-reference-type-info-store';
 
 
 const DATE_FORMAT_MAP = {
@@ -48,6 +53,10 @@ const { getProperRouteLocation } = useProperRouteLocation();
 const metricExplorerPageStore = useMetricExplorerPageStore();
 const metricExplorerPageState = metricExplorerPageStore.state;
 const metricExplorerPageGetters = metricExplorerPageStore.getters;
+const allReferenceTypeInfoStore = useAllReferenceTypeInfoStore();
+const storeState = reactive({
+    allReferenceTypeInfo: computed<AllReferenceTypeInfo>(() => allReferenceTypeInfoStore.getters.allReferenceTypeInfo),
+});
 const state = reactive({
     loading: false,
     currentMetricId: computed<string>(() => route.params.metricId),
@@ -101,7 +110,8 @@ const getRefinedColumnValue = (field, value) => {
         }
         return numberFormatter(value, { notation: 'compact' }) || 0;
     }
-    return metricExplorerPageGetters.labelKeysReferenceMap?.[field.name]?.[value]?.label || value;
+    const _label = metricExplorerPageGetters.labelKeysReferenceMap?.[field.name]?.[value]?.label || value;
+    return getReferenceLabel(storeState.allReferenceTypeInfo, field.name, _label);
 };
 
 /* Api */
