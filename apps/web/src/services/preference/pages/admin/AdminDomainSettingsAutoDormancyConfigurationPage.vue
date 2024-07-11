b9f8a0ea8f1c757a868cba9dcf524cc7eace62d2
nayeongkim <nayeongkim@megazone.com>
2024-07-11 13:43:51 +0900

    feat: changed design
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/services/preference/pages/admin/AdminDomainSettingsAutoDormancyConfigurationPage.vue
+++ apps/web/src/services/preference/pages/admin/AdminDomainSettingsAutoDormancyConfigurationPage.vue
@@ -1,20 +1,19 @@
 <script lang="ts" setup>
 import { computed, reactive } from 'vue';
-import type { TranslateResult } from 'vue-i18n';
 
 import {
     PPaneLayout, PDivider, PTextButton, PCheckbox, PToggleButton, PFieldGroup, PTextInput, PButton, PTooltip, PI,
 } from '@spaceone/design-system';
 
-import { i18n } from '@/translations';
+import { i18n as _i18n } from '@/translations';
 
 import { useFormValidator } from '@/common/composables/form-validator';
 
 const state = reactive({
     statusToggle: false,
-    toggleText: computed<TranslateResult>(() => (state.statusToggle
-        ? i18n.t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_TOGGLE _ENABLED')
-        : i18n.t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_TOGGLE _DISABLED')
+    toggleText: computed<string>(() => (state.statusToggle
+        ? 'IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_TOGGLE_ENABLED'
+        : 'IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_TOGGLE_DISABLED'
     )),
     checkbox: false,
     attributes: { type: 'number' },
@@ -31,7 +30,7 @@ const {
     cost: '',
 }, {
     cost: (val: string) => {
-        if (!val.trim()) return i18n.t('IAM.DOMAIN_SETTINGS.ALT_E_EMPTY_COST');
+        if (state.statusToggle && !val.trim()) return _i18n.t('IAM.DOMAIN_SETTINGS.ALT_E_EMPTY_COST');
         return true;
     },
 });
@@ -50,55 +49,79 @@ const {
                 />
                 <p class="toggle-text">
                     <strong class="title">{{ $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_TOGGLE') }}</strong>
-                    <span>{{ state.toggleText }}</span>
+                    <i18n class="toggle-text-desc"
+                          :path="state.toggleText"
+                    >
+                        <template #emphasis>
+                            <span v-if="state.statusToggle"
+                                  class="emphasis"
+                            >
+                                {{ $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_TOGGLE_ENABLED_EMPHASIS') }}
+                            </span>
+                            <span v-else
+                                  class="emphasis"
+                            >
+                                {{ $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_TOGGLE_DISABLED_EMPHASIS') }}
+                            </span>
+                        </template>
+                    </i18n>
                 </p>
             </div>
             <div class="cost-wrapper"
                  :class="{ disabled: !state.statusToggle }"
             >
-                <p-field-group :label="$t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_COST_THRESHOLD')"
-                               :invalid-text="invalidTexts.cost"
-                               :invalid="invalidState.cost"
-                               required
+                <img alt="cost-threshold-chart"
+                     src="/images/domain-settings/img_auto-dormancy-configuration_cost-threshold-chart.png"
+                     srcset="/images/domain-settings/img_auto-dormancy-configuration_cost-threshold-chart@2x.png 2x,
+                        /images/domain-settings/img_auto-dormancy-configuration_cost-threshold-chart@3x.png 3x"
+                     class="cost-threshold-chart"
                 >
-                    <template #label-extra>
-                        <p-tooltip :contents="!state.statusToggle ? '' : $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_COST_THRESHOLD_DESC')"
-                                   position="top-start"
-                                   class="tooltip"
-                        >
-                            <p-i width="1rem"
-                                 height="1rem"
-                                 name="ic_info-circle"
-                                 class="icon"
+                <div class="cost-content-wrapper">
+                    <p-field-group :label="$t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_COST_THRESHOLD')"
+                                   :invalid-text="invalidTexts.cost"
+                                   :invalid="invalidState.cost"
+                                   required
+                    >
+                        <template #label-extra>
+                            <p-tooltip :contents="!state.statusToggle ? '' : $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_COST_THRESHOLD_DESC')"
+                                       position="top-start"
+                                       class="tooltip"
+                            >
+                                <p-i width="1rem"
+                                     height="1rem"
+                                     name="ic_info-circle"
+                                     class="icon"
+                                />
+                            </p-tooltip>
+                        </template>
+                        <template #default="{invalid}">
+                            <p-text-input :value="cost"
+                                          :invalid="invalid"
+                                          :disabled="!state.statusToggle"
+                                          type="number"
+                                          masking-mode
+                                          placeholder="$ USD"
+                                          class="cost-input"
+                                          @update:value="setForm('cost', $event)"
                             />
-                        </p-tooltip>
-                    </template>
-                    <template #default="{invalid}">
-                        <p-text-input :value="cost"
-                                      :invalid="invalid"
-                                      :disabled="!state.statusToggle"
-                                      type="number"
-                                      masking-mode
-                                      placeholder="$ USD"
-                                      class="cost-input"
-                                      @update:value="setForm('cost', $event)"
-                        />
-                    </template>
-                </p-field-group>
-                <p-divider />
-                <p-checkbox :value.sync="state.checkbox"
-                            :disabled="!state.statusToggle"
-                >
-                    {{ $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_CHECKBOX') }}
-                </p-checkbox>
-                <p-text-button icon-left="ic_service_cost-report"
-                               style-type="highlight"
-                               class="cost-report-button"
-                >
-                    {{ $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_COST_REPORT') }}
-                </p-text-button>
+                        </template>
+                    </p-field-group>
+                    <p-divider />
+                    <p-checkbox :value.sync="state.checkbox"
+                                :disabled="!state.statusToggle"
+                    >
+                        {{ $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_CHECKBOX') }}
+                    </p-checkbox>
+                    <p-text-button icon-left="ic_service_cost-report"
+                                   style-type="highlight"
+                                   class="cost-report-button"
+                    >
+                        {{ $t('IAM.DOMAIN_SETTINGS.AUTO_DORMANCY_CONFIGURATION_COST_REPORT') }}
+                    </p-text-button>
+                </div>
             </div>
-            <p-button :disabled="!isAllValid"
+            <p-button v-if="state.statusToggle"
+                      :disabled="!isAllValid"
                       class="save-button"
             >
                 {{ $t('IAM.DOMAIN_SETTINGS.SAVE_CHANGE') }}
@@ -124,58 +147,74 @@ const {
                 .title {
                     @apply text-label-lg;
                 }
+                .toggle-text-desc {
+                    @apply text-gray-600;
+                    .emphasis {
+                        @apply text-gray-900 font-medium;
+                    }
+                }
             }
         }
         .cost-wrapper {
-            @apply relative flex flex-col text-label-md border border-gray-200;
+            @apply relative flex text-label-md bg-gray-100 border border-gray-200;
+            border-radius: 0.375rem;
             padding: 1.5rem;
             gap: 1.5rem;
-            border-radius: 0.375rem;
-            .cost-report-button {
-                @apply absolute;
-                top: 1.5rem;
-                right: 1.5rem;
+            .cost-threshold-chart {
+                width: 13rem;
+                height: 7.75rem;
+                margin-top: auto;
+                margin-bottom: auto;
             }
-            .icon {
-                margin-top: -0.25rem;
-            }
+            .cost-content-wrapper {
+                @apply flex flex-col;
+                gap: 1.25rem;
+                .cost-report-button {
+                    @apply absolute;
+                    top: 1.5rem;
+                    right: 1.5rem;
+                }
+                .icon {
+                    margin-top: -0.25rem;
+                }
 
-            &.disabled {
-                @apply bg-gray-100;
+                &.disabled {
+                    @apply bg-gray-100;
 
-                /* custom design-system component - p-field-group */
-                :deep(.p-field-group) {
-                    .title-wrapper {
-                        .title {
-                            @apply text-gray-600;
+                    /* custom design-system component - p-field-group */
+                    :deep(.p-field-group) {
+                        .title-wrapper {
+                            .title {
+                                @apply text-gray-600;
+                            }
                         }
                     }
-                }
-                .tooltip, .cost-input {
-                    @apply cursor-not-allowed;
-                }
-            }
-
-            /* custom design-system component - p-text-input */
-            :deep(.p-text-input) {
-                .delete-all-icon {
-                    @apply hidden;
-                }
-                input[type="number"] {
-                    -moz-appearance: textfield;
-                    appearance: textfield;
-                    width: 100%;
+                    .tooltip, .cost-input {
+                        @apply cursor-not-allowed;
+                    }
                 }
 
-                input[type="number"]::-webkit-outer-spin-button,
-                input[type="number"]::-webkit-inner-spin-button {
-                    -webkit-appearance: none;
-                    margin: 0;
-                }
+                /* custom design-system component - p-text-input */
+                :deep(.p-text-input) {
+                    .delete-all-icon {
+                        @apply hidden;
+                    }
+                    input[type="number"] {
+                        -moz-appearance: textfield;
+                        appearance: textfield;
+                        width: 100%;
+                    }
 
-                input::placeholder {
-                    @apply absolute text-right;
-                    right: 0.125rem;
+                    input[type="number"]::-webkit-outer-spin-button,
+                    input[type="number"]::-webkit-inner-spin-button {
+                        -webkit-appearance: none;
+                        margin: 0;
+                    }
+
+                    input::placeholder {
+                        @apply absolute text-right;
+                        right: 0.125rem;
+                    }
                 }
             }
         }
