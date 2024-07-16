868c0f4f1fb45cee39e58a9d02a24b53413e2106
yubeom <kimub1204@mz.co.kr>
2024-07-16 17:37:13 +0900

    feat: migrate mirinae/icon-modal/PIconModal v6 -> v7

---
+++ packages/mirinae/src/feedbacks/modals/icon-modal/PIconModal.stories.ts
@@ -0,0 +1,87 @@
+import {
+    computed, reactive, toRefs, ref,
+} from 'vue';
+
+import { faker } from '@faker-js/faker';
+import type { Meta, StoryObj } from '@storybook/vue';
+import type { ComponentProps } from 'vue-component-type-helpers';
+
+import { getIconModalDefaultArgs, getIconModalArgTypes } from '@/feedbacks/modals/icon-modal/story-helper';
+import PButton from '@/inputs/buttons/button/PButton.vue';
+
+
+
+
+import PIconModal from './PIconModal.vue';
+
+type PIconModalPropsAndCustomArgs = ComponentProps<typeof PIconModal>;
+
+const meta : Meta<PIconModalPropsAndCustomArgs> = {
+    title: 'Feedbacks/Modals/Icon Modal',
+    component: PIconModal,
+    argTypes: {
+        ...getIconModalArgTypes(),
+    },
+    parameters: {
+        centered: { disable: true },
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/file/IS6P8y1Wn2nfBC4jGlSiya/Components?node-id=10%3A138158&t=kwTRXVZQtJLDw0Ei-4',
+        },
+    },
+    args: {
+        ...getIconModalDefaultArgs(),
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof PIconModal>;
+
+
+const Template: Story = {
+    render: (args, { argTypes }) => ({
+        props: Object.keys(argTypes),
+        components: { PIconModal, PButton },
+        template: `
+            <div>
+                <p-button @click="launchModal">Launch a modal</p-button>
+                <p-icon-modal :visible.sync="modalVisible"
+                                :size="size"
+                                :icon-name="iconName"
+                                :icon-color="iconColor"
+                                :emoji="emoji"
+                                :header-title="headerTitle"
+                                :header-desc="headerDesc"
+                                :button-text="buttonText"
+                                :button-style-type="buttonStyleType"
+                                :backdrop="backdrop"
+                                :hide-button="hideButton"
+                                @close="closeModal"
+                />
+            </div>
+        `,
+        setup(props) {
+            const selectItem = ref('init');
+            const state = reactive({
+                modalVisible: props.visible,
+                contents: computed(() => faker.lorem.lines()),
+            });
+            const launchModal = () => {
+                state.modalVisible = true;
+            };
+            const closeModal = () => {
+                state.modalVisible = false;
+            };
+            return {
+                selectItem,
+                ...toRefs(state),
+                launchModal,
+                closeModal,
+            };
+        },
+    }),
+};
+
+export const Playground: Story = {
+    ...Template,
+};
