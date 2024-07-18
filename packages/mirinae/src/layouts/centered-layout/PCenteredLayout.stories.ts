675e95449660c2f2874031d7119a425df77049bb
yubeom <kimub1204@mz.co.kr>
2024-07-18 15:52:15 +0900

    feat: migrate mirinae/centered-layout/PCenteredLayout v6 -> v7

---
+++ packages/mirinae/src/layouts/centered-layout/PCenteredLayout.stories.ts
@@ -0,0 +1,94 @@
+import { faker } from '@faker-js/faker';
+import type { Meta, StoryObj } from '@storybook/vue';
+import type { ComponentProps } from 'vue-component-type-helpers';
+
+import { getCenteredLayoutArgs, getCenteredLayoutParameters, getCenteredLayoutArgTypes } from '@/layouts/centered-layout/story-helper';
+
+import PCenteredLayout from './PCenteredLayout.vue';
+
+type PCenteredLayoutPropsAndCustomArgs = ComponentProps<typeof PCenteredLayout>;
+
+const meta : Meta<PCenteredLayoutPropsAndCustomArgs> = {
+    title: 'Layouts/Centered Layout',
+    component: PCenteredLayout,
+    argTypes: {
+        ...getCenteredLayoutArgTypes(),
+    },
+    parameters: {
+        ...getCenteredLayoutParameters(),
+    },
+    args: {
+        ...getCenteredLayoutArgs(),
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof PCenteredLayout>;
+
+const Template: Story = {
+    render: (args, { argTypes }) => ({
+        props: Object.keys(argTypes),
+        components: {
+            PCenteredLayout,
+        },
+        template: `
+            <div class="w-full h-full fixed">
+                <p-centered-layout>
+                    <template>
+                        <div class="flex justify-center items-center font-bold text-lg">
+                            <div v-html="defaultSlot"></div>
+                        </div>
+                    </template>
+                </p-centered-layout>
+            </div>
+        `,
+    }),
+};
+
+export const Basic: Story = {
+    render: () => ({
+        components: {
+            PCenteredLayout,
+        },
+        template: `
+            <div class="w-full h-full fixed">
+                <p-centered-layout>
+                    <template>
+                        <div class="font-bold text-lg" style="width: 300px;">
+                            {{ faker.lorem.paragraph(100) }}
+                        </div>
+                    </template>
+                </p-centered-layout>
+            </div>
+        `,
+        setup() {
+            return {
+                faker,
+            };
+        },
+    }),
+};
+
+export const WithTopContentsSlot: Story = {
+    render: () => ({
+        components: { PCenteredLayout },
+        template: `
+            <div class="w-full h-full fixed">
+                <p-centered-layout>
+                    <template #top-contents>
+                        <img src="images/SpaceONE_logoTypeA.svg">
+                    </template>
+                    <template>
+                        <div class="flex justify-center items-center font-bold text-lg">
+                            <span>This is contents</span>
+                        </div>
+                    </template>
+                </p-centered-layout>
+            </div>
+        `,
+    }),
+};
+
+export const Playground: Story = {
+    ...Template,
+};
