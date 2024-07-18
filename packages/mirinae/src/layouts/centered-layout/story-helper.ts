e308ffad2830e399a724dc3f05dc29203f923e76
yubeom <kimub1204@mz.co.kr>
2024-07-18 15:52:15 +0900

    feat: migrate mirinae/centered-layout/PCenteredLayout v6 -> v7

--- packages/mirinae/src/layouts/centered-layout/story-helper.ts
+++ packages/mirinae/src/layouts/centered-layout/story-helper.ts
@@ -1,11 +1,22 @@
-import type { ArgTypes } from '@storybook/addons';
+import type { ArgTypes, Args, Parameters } from '@storybook/vue';
+
+export const getCenteredLayoutArgs = (): Args => ({
+    defaultSlot: 'This is contents',
+    topContentsSlot: null,
+});
+
+export const getCenteredLayoutParameters = (): Parameters => ({
+    design: {
+        type: 'figma',
+        url: 'https://www.figma.com/file/IS6P8y1Wn2nfBC4jGlSiya/Components?node-id=10%3A228226&t=GVsFwo5txLv6VtoA-4',
+    },
+});
 
 export const getCenteredLayoutArgTypes = (): ArgTypes => ({
     // slots
     defaultSlot: {
         name: 'default',
         description: 'Slot for layout contents.',
-        defaultValue: 'This is contents',
         table: {
             type: {
                 summary: null,
@@ -15,14 +26,11 @@ export const getCenteredLayoutArgTypes = (): ArgTypes => ({
             },
             category: 'slots',
         },
-        control: {
-            type: 'text',
-        },
+        control: 'text',
     },
     topContentsSlot: {
         name: 'top-contents',
         description: 'Slot for top contents.',
-        defaultValue: null,
         table: {
             type: {
                 summary: null,
@@ -34,4 +42,7 @@ export const getCenteredLayoutArgTypes = (): ArgTypes => ({
         },
         control: null,
     },
+    // default
+    'top-contents': { table: { disable: true } },
+    default: { table: { disable: true } },
 });
