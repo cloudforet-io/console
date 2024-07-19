2cb5b57b3084de65b3081dfb22d94e173447a6f2
yuda <yuda@megazone.com>
2024-07-19 09:06:12 +0900

    refactor(budget): change from am5 to echarts
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/cost-explorer/components/BudgetDetailSummaryChart.vue
+++ apps/web/src/services/cost-explorer/components/BudgetDetailSummaryChart.vue
@@ -1,138 +1,125 @@
 <script setup lang="ts">
+import { useResizeObserver } from '@vueuse/core/index';
 import {
     computed, reactive, ref, watch,
 } from 'vue';
 
-import type { XYChart } from '@amcharts/amcharts5/xy';
 import dayjs from 'dayjs';
+import type { BarSeriesOption } from 'echarts/charts';
+import type { EChartsType } from 'echarts/core';
+import { init } from 'echarts/core';
+import { throttle } from 'lodash';
 
 import { PDataLoader, PSkeleton } from '@cloudforet/mirinae';
 import { numberFormatter } from '@cloudforet/utils';
 
-import { useAmcharts5 } from '@/common/composables/amcharts5';
+import {
+    getDateLabelFormat, getWidgetDateFields,
+} from '@/common/modules/widgets/_helpers/widget-date-helper';
 
 import {
     gray, indigo, red,
 } from '@/styles/colors';
 
 import { GRANULARITY } from '@/services/cost-explorer/constants/cost-explorer-constant';
-import { getStackedChartData } from '@/services/cost-explorer/helpers/cost-explorer-chart-data-helper';
 import { useBudgetDetailPageStore } from '@/services/cost-explorer/stores/budget-detail-page-store';
-import type { XYChartData } from '@/services/cost-explorer/types/cost-explorer-chart-type';
 
 
 interface Props {
     loading?: boolean;
 }
-
 const props = withDefaults(defineProps<Props>(), {
     loading: true,
 });
 
 const budgetPageStore = useBudgetDetailPageStore();
 const budgetPageState = budgetPageStore.$state;
-
 const chartContext = ref<HTMLElement | null>(null);
 const state = reactive({
-    chart: null as null | XYChart,
     limitProperty: computed(() => ((budgetPageState.budgetData?.time_unit === 'TOTAL') ? 'total_limit' : 'limit')),
-    chartData: computed<XYChartData[]>(() => {
-        if (props.loading || !budgetPageState.budgetUsageData?.length) return [];
-        const accumulatedData = getStackedChartData(budgetPageState.budgetUsageData, GRANULARITY.MONTHLY, {
-            start: budgetPageState.budgetData?.start,
-            end: budgetPageState.budgetData?.end,
-        });
-        if (budgetPageState.budgetData?.time_unit === 'TOTAL') {
-            accumulatedData.forEach((data) => {
-                data.total_limit = budgetPageState.budgetData?.limit;
-            });
-        }
-        return accumulatedData;
-    }),
+    xAxisData: computed(() => getWidgetDateFields(GRANULARITY.MONTHLY, budgetPageState.budgetData?.start, budgetPageState.budgetData?.end)),
+    chartData: [],
+    chart: null as EChartsType | null,
+    chartOptions: computed<BarSeriesOption>(() => ({
+        grid: {
+            left: '3%',
+            right: 10,
+            top: 10,
+            bottom: 30,
+        },
+        legend: {
+            show: false,
+        },
+        tooltip: {
+        },
+        xAxis: {
+            type: 'category',
+            data: state.xAxisData,
+            axisLabel: {
+                formatter: (val) => dayjs.utc(val).format(getDateLabelFormat(GRANULARITY.MONTHLY)),
+            },
+        },
+        yAxis: {
+            type: 'value',
+            axisLabel: {
+                formatter: (val) => numberFormatter(val, { notation: 'compact' }),
+            },
+        },
+        series: state.chartData,
+    })),
 });
 
-const makeMonthlyPlanningLineSeries = (chart: XYChart) => {
-    const lineSeries = chartHelper.createXYLineSeries(chart, {
-        name: 'date',
-        valueYField: state.limitProperty,
-        stroke: chartHelper.color(gray[600]),
-        fill: chartHelper.color(gray[600]),
-    });
-    lineSeries.strokes.template.setAll({
-        strokeWidth: 1.5,
-        strokeDasharray: [4, 2],
-    });
-    if (state.limitProperty === 'total_limit') {
-        lineSeries.bullets.clear();
-    }
-    return lineSeries;
-};
-
-const chartHelper = useAmcharts5(chartContext);
 const drawChart = () => {
-    const { chart, xAxis, yAxis } = chartHelper.createXYDateChart();
-
-    // set base interval of xAxis
-    xAxis.get('baseInterval').timeUnit = 'month';
-    // set label adapter of yAxis
-    yAxis.get('renderer').labels.template.adapters.add('text', (text) => {
-        if (text) {
-            const convertedText = text.replace(/,/g, '');
-            const num = Number(convertedText);
-            if (Number.isNaN(num)) return text;
-            return numberFormatter(num);
-        }
-        return text;
+    const _totalLimit = state.limitProperty === 'total_limit' ? budgetPageState.budgetData?.limit : undefined;
+    let _accumulatedValue = 0;
+    const _accumulatedData: any[] = [];
+    state.xAxisData.forEach((d) => {
+        const _data = budgetPageState.budgetUsageData?.find((v) => v.date === d);
+        const _limit = _totalLimit || _data?.limit || 0;
+        const _cost = _data?.cost || 0;
+        _accumulatedValue += _cost;
+        _accumulatedData.push({
+            value: _accumulatedValue,
+            itemStyle: {
+                color: _limit < _accumulatedValue ? red[400] : indigo[600],
+            },
+        });
     });
+    state.chartData = [
+        {
+            name: '',
+            type: 'bar',
+            barMaxWidth: 25,
+            data: _accumulatedData,
+        },
+        {
+            name: '',
+            type: 'line',
+            symbol: 'none',
+            lineStyle: {
+                type: 'dashed',
+                width: 1,
+                color: gray[600],
+            },
+            data: state.xAxisData.map((d) => {
+                const _data = budgetPageState.budgetUsageData?.find((v) => v.date === d);
+                return _totalLimit || _data?.limit || 0;
+            }),
+        },
+    ];
 
-    // create column series
-    const columnSeries = chartHelper.createXYColumnSeries(chart, {
-        name: 'date',
-        valueYField: 'cost',
-        stacked: true,
-        stroke: undefined,
-    });
-    columnSeries.columns.template.set('width', chartHelper.percent(25));
-    columnSeries.columns.template.adapters.add('fill', (fill, target) => {
-        const MonthToDate = dayjs.utc().startOf('month').format('YYYY-MM-DD');
-        const data = target.dataItem?.dataContext;
-        if (!data) return chartHelper.color(indigo[600]);
-        if (data?.date === MonthToDate) {
-            return chartHelper.color(indigo[300]);
-        }
-        if (data[state.limitProperty] < data?.cost) {
-            return chartHelper.color(red[400]);
-        }
-        return chartHelper.color(indigo[600]);
-    });
-
-    // set data processor
-    columnSeries.data.processor = chartHelper.createDataProcessor({
-        dateFormat: 'YYYY-MM',
-        dateFields: ['date'],
-    });
-
-    // set series to chart and set data
-    columnSeries.data.setAll(state.chartData);
-    chart.series.push(columnSeries);
-
-    // set tooltip
-    const tooltip = chartHelper.createTooltip();
-    chartHelper.setXYSingleTooltipText(chart, tooltip, budgetPageState.budgetData?.currency);
-    columnSeries.set('tooltip', tooltip);
-
-    // set budget line series
-    const lineSeries = makeMonthlyPlanningLineSeries(chart);
-    chart.series.push(lineSeries);
-    lineSeries.data.setAll(state.chartData);
-    return chart;
+    state.chart = init(chartContext.value);
+    state.chart.setOption(state.chartOptions, true);
 };
 
 watch([() => chartContext.value, () => props.loading], async ([_chartContext, loading]) => {
     if (_chartContext && !loading) {
-        state.chart = drawChart();
+        drawChart();
     }
-}, { immediate: false });
+}, { immediate: true });
+useResizeObserver(chartContext, throttle(() => {
+    state.chart?.resize();
+}, 500));
 </script>
 
 <template>
