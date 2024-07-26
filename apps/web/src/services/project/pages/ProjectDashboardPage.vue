b7265536875f2bdc871d902ec7c75ebf619a827a
yuda <yuda@megazone.com>
2024-07-26 16:07:25 +0900

    feat: get legacy project dashboard
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/project/pages/ProjectDashboardPage.vue
+++ apps/web/src/services/project/pages/ProjectDashboardPage.vue
@@ -1,192 +1,95 @@
 <script lang="ts" setup>
 import {
-    computed, onUnmounted,
-    reactive, watch,
+    reactive,
 } from 'vue';
-import { useRouter } from 'vue-router/composables';
 
-import { PSelectDropdown, PEmpty, PButton } from '@cloudforet/mirinae';
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 
-import { i18n } from '@/translations';
-
-import { useDashboardStore } from '@/store/dashboard/dashboard-store';
+import type { ListResponse } from '@/schema/_common/api-verbs/list';
+import type { ProjectAlertConfigListParameters } from '@/schema/monitoring/project-alert-config/api-verbs/list';
+import type { ProjectAlertConfigModel } from '@/schema/monitoring/project-alert-config/model';
 
 import ErrorHandler from '@/common/composables/error/errorHandler';
-import { useProperRouteLocation } from '@/common/composables/proper-route-location';
+import DailyUpdates from '@/common/modules/widgets/DailyUpdates.vue';
 
-import DashboardLabels from '@/services/dashboards/components/DashboardLabels.vue';
-import DashboardRefreshDropdown from '@/services/dashboards/components/DashboardRefreshDropdown.vue';
-import DashboardToolsetDateDropdown from '@/services/dashboards/components/DashboardToolsetDateDropdown.vue';
-import DashboardVariablesV2 from '@/services/dashboards/components/DashboardVariablesV2.vue';
-import DashboardWidgetContainerV2 from '@/services/dashboards/components/DashboardWidgetContainerV2.vue';
-import { DASHBOARDS_ROUTE } from '@/services/dashboards/routes/route-constant';
-import { useDashboardDetailInfoStore } from '@/services/dashboards/stores/dashboard-detail-info-store';
+import CloudServices from '@/services/asset-inventory/components/CloudServices.vue';
+import ProjectSummaryAlertWidget from '@/services/project/components/ProjectSummaryAlertWidget.vue';
+import ProjectSummaryAllSummaryWidget from '@/services/project/components/ProjectSummaryAllSummaryWidget.vue';
+// import ProjectSummaryBillingWidget from '@/services/project/components/ProjectSummaryBillingWidget.vue';
+import ProjectSummaryBillingWidget from '@/services/project/components/ProjectSummaryBillingWidget.vue';
+import ProjectSummaryPersonalHealthDashboardWidget from '@/services/project/components/ProjectSummaryPersonalHealthDashboardWidget.vue';
+import ProjectSummaryServiceAccountsWidget from '@/services/project/components/ProjectSummaryServiceAccountsWidget.vue';
+import ProjectSummaryTrustedAdvisorWidget from '@/services/project/components/ProjectSummaryTrustedAdvisorWidget.vue';
+
 
 interface Props {
     id: string;
 }
 const props = defineProps<Props>();
-const router = useRouter();
-const { getProperRouteLocation } = useProperRouteLocation();
-
-const dashboardStore = useDashboardStore();
-const dashboardDetailStore = useDashboardDetailInfoStore();
-const dashboardDetailGetters = dashboardDetailStore.getters;
-const dashboardDetailState = dashboardDetailStore.state;
-
-const storeState = reactive({
-    dashboardList: computed(() => dashboardStore.state.publicDashboardItems),
-});
-
 const state = reactive({
-    dashboardMenu: computed(() => [
-        {
-            name: 'Dashboards',
-            label: i18n.t('PROJECT.DETAIL.DASHBOARD.DASHBOARD_LIST'),
-            type: 'header',
-        },
-        ...storeState.dashboardList,
-    ]),
     hasAlertConfig: false,
-    dashboardVariablesLoading: false,
-    currentDashboardId: undefined as string|undefined,
 });
-
-
-const handleUpdateDashboardVariables = async (params) => {
-    if (!state.currentDashboardId) return;
-    state.dashboardVariablesLoading = true;
-    try {
-        const updatedDashboard = await dashboardStore.updateDashboard(state.currentDashboardId, params);
-        dashboardDetailStore.setDashboardInfo(updatedDashboard);
-    } catch (e) {
-        ErrorHandler.handleError(e);
-    } finally {
-        state.dashboardVariablesLoading = false;
-    }
-};
-const handleRefresh = async () => {
-    await dashboardDetailStore.listDashboardWidgets();
-};
-const handleSelectDashboard = (dashboardName: string) => {
-    const selectedDashbaord = storeState.dashboardList.find((dashboard) => dashboard.name === dashboardName);
-    if (selectedDashbaord) {
-        state.currentDashboardId = selectedDashbaord.dashboard_id;
-    }
-};
-const handleClickGoToDashboard = () => {
-    router.push(getProperRouteLocation({
-        name: DASHBOARDS_ROUTE._NAME,
-    }));
-};
 
 /* api */
-
-const listProjectDashboard = async () => {
-    await dashboardStore.load(true);
-};
-const setDefaultDashboard = () => {
-    if (storeState.dashboardList.length === 0) return;
-    const defaultDashboard = storeState.dashboardList[0];
-    if (defaultDashboard) {
-        state.currentDashboardId = defaultDashboard.dashboard_id;
-    }
-};
-const getDashboardData = async (dashboardId: string) => {
+const getProjectAlertConfig = async () => {
     try {
-        await dashboardDetailStore.getDashboardInfo(dashboardId);
+        const { results } = await SpaceConnector.clientV2.monitoring.projectAlertConfig.list<ProjectAlertConfigListParameters, ListResponse<ProjectAlertConfigModel>>({
+            project_id: props.id,
+        });
+        state.hasAlertConfig = !!results?.length;
     } catch (e) {
         ErrorHandler.handleError(e);
     }
 };
 
 (async () => {
-    await listProjectDashboard();
-    setDefaultDashboard();
+    await Promise.allSettled([
+        getProjectAlertConfig(),
+    ]);
 })();
-
-watch(() => state.currentDashboardId, async (dashboardId, prevDashboardId) => {
-    if (!dashboardId || !props.id) return;
-    /* NOTE: The dashboard data is reset in first entering case */
-    if (dashboardId && !prevDashboardId) { // this includes all three cases
-        dashboardDetailStore.reset();
-    }
-    dashboardDetailStore.setProjectId(props.id);
-    await getDashboardData(dashboardId);
-}, { immediate: true });
-
-onUnmounted(() => {
-    dashboardDetailStore.reset();
-});
-
 </script>
 
 <template>
-    <div class="project-dashboard-page">
-        <div v-if="storeState.dashboardList.length && state.currentDashboardId"
-             class="dashboard-wrapper"
-        >
-            <div class="title-wrapper">
-                <div class="title">
-                    <p-select-dropdown style-type="tertiary-icon-button"
-                                       button-icon="ic_dots-4-square"
-                                       :menu="state.dashboardMenu"
-                                       :selected="dashboardDetailState.name"
-                                       @select="handleSelectDashboard"
-                    >
-                        <template #menu-item--format="{ item }">
-                            <span class="dashboard-menu-item">{{ item.name }}</span>
-                        </template>
-                    </p-select-dropdown>
-                    <span>{{ dashboardDetailState.name }}</span>
-                </div>
-                <dashboard-toolset-date-dropdown :date-range="dashboardDetailState.options.date_range" />
-            </div>
-            <dashboard-labels v-if="!!dashboardDetailState.labels.length"
-                              class="labels"
+    <div class="grid grid-cols-12 project-dashboard-page">
+        <project-summary-all-summary-widget
+            class="col-span-12"
+            :project-id="props.id"
+        />
+        <div class="col-span-12 lg:col-span-9 grid grid-cols-12 left-part">
+            <project-summary-alert-widget
+                v-if="state.hasAlertConfig"
+                class="col-span-12"
+                :project-id="props.id"
             />
-            <div class="contents-wrapper">
-                <div class="dashboard-selectors">
-                    <dashboard-variables-v2 class="variable-selector-wrapper"
-                                            is-project-dashboard
-                                            :disable-save-button="dashboardDetailGetters.disableManageButtons"
-                                            :loading="state.dashboardVariablesLoading"
-                                            @update="handleUpdateDashboardVariables"
-                    />
-                    <dashboard-refresh-dropdown :dashboard-id="state.currentDashboardId"
-                                                :loading="dashboardDetailState.loadingWidgets"
-                                                @refresh="handleRefresh"
-                    />
-                </div>
-                <dashboard-widget-container-v2 ref="widgetContainerRef" />
-            </div>
+            <!--            <project-summary-billing-widget-->
+            <!--                class="col-span-12"-->
+            <!--                :project-id="props.id"-->
+            <!--            />-->
+            <project-summary-billing-widget
+                class="col-span-12"
+                :project-id="props.id"
+            />
+            <project-summary-personal-health-dashboard-widget
+                class="col-span-12"
+                :project-id="props.id"
+            />
+            <project-summary-service-accounts-widget
+                class="col-span-12 service-accounts-table"
+                :project-id="props.id"
+            />
         </div>
-        <div v-else-if="!dashboardStore.state.loading"
-             class="empty-contents"
-        >
-            <p-empty image-size="sm"
-                     show-image
-                     show-button
-                     :title="$t('PROJECT.DETAIL.DASHBOARD.NO_DASHBOARD')"
-            >
-                <template #image>
-                    <img src="@/assets/images/img_ghost.png"
-                         alt="empty-options"
-                         class="empty-options-image"
-                    >
-                </template>
-                <template #default>
-                    {{ $t('PROJECT.DETAIL.DASHBOARD.NO_DASHBOARD_DESC') }}
-                </template>
-                <template #button>
-                    <p-button icon-left="ic_service_dashboard"
-                              style-type="substitutive"
-                              @click="handleClickGoToDashboard"
-                    >
-                        {{ $t('PROJECT.DETAIL.GO_TO_DASHBOARD') }}
-                    </p-button>
-                </template>
-            </p-empty>
+        <div class="col-span-12 lg:col-span-3 grid grid-cols-12 right-part">
+            <daily-updates class="col-span-12 daily-updates"
+                           :project-id="props.id"
+            />
+            <cloud-services class="col-span-12 cloud-services"
+                            :more-info="true"
+                            :project-id="props.id"
+            />
+            <project-summary-trusted-advisor-widget
+                class="col-span-12 trusted-advisor"
+                :project-id="props.id"
+            />
         </div>
     </div>
 </template>
@@ -202,38 +105,37 @@ onUnmounted(() => {
 }
 
 .project-dashboard-page {
+    grid-gap: 1rem;
     padding: 2rem 1rem 0;
 
-    .dashboard-wrapper {
-        .title-wrapper {
-            @apply flex justify-between items-center;
-            padding-bottom: 1rem;
-            .title {
-                @apply inline-flex items-center text-display-md font-medium;
-                gap: 0.5rem;
-            }
-        }
-        .labels {
-            padding-bottom: 1rem;
-        }
+    .left-part, .right-part {
+        display: grid;
+        grid-auto-rows: max-content;
+        row-gap: 1rem;
+    }
 
-        .contents-wrapper {
-            @apply bg-gray-100;
-            padding: 0 0.5rem;
-            .dashboard-selectors {
-                @apply relative flex justify-between items-start z-10;
-                padding: 1rem 0;
+    .cloud-services {
+        @apply border border-gray-200 rounded-md;
+        min-height: 25rem;
+        max-height: 35rem;
 
-                .variable-selector-wrapper {
-                    @apply relative flex items-center flex-wrap;
-                    gap: 0.5rem;
-                    padding-right: 1rem;
-                }
-            }
+        @screen tablet {
+            height: 26rem;
         }
     }
-    .empty-contents {
-        padding-top: 1rem;
+
+    .trusted-advisor {
+        @apply border border-gray-200 rounded-md;
+    }
+
+    .service-accounts-table {
+        @apply border border-gray-200 rounded-md;
+    }
+
+    .daily-updates {
+        @apply border border-gray-200 rounded-md;
+        max-height: 35rem;
     }
 }
 </style>
+
