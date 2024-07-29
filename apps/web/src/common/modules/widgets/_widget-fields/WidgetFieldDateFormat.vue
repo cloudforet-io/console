a4fa42b4b095f01ca791fc56e6fd1e541f60ee4c
Yongtae Park <samuel.park@megazone.com>
2024-07-29 15:25:37 +0900

    feat: create date format field component
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/common/modules/widgets/_widget-fields/WidgetFieldDateFormat.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import {
+    computed, onMounted, reactive,
+} from 'vue';
+
+import { PFieldGroup, PSelectDropdown } from '@cloudforet/mirinae';
+import type { MenuItem } from '@cloudforet/mirinae/types/inputs/context-menu/type';
+
+import { useProxyValue } from '@/common/composables/proxy-state';
+import { DATE_FORMAT } from '@/common/modules/widgets/_constants/widget-field-constant';
+import type {
+    WidgetFieldComponentEmit,
+    WidgetFieldComponentProps,
+    DateFormatOptions,
+} from '@/common/modules/widgets/types/widget-field-type';
+import type { DateFormatValue } from '@/common/modules/widgets/types/widget-field-value-type';
+
+
+const props = defineProps<WidgetFieldComponentProps<DateFormatOptions, DateFormatValue>>();
+const emit = defineEmits<WidgetFieldComponentEmit<DateFormatValue>>();
+const state = reactive({
+    menuItems: computed<MenuItem[]>(() => Object.values(DATE_FORMAT).map((d) => ({
+        name: d,
+        label: d,
+    }))),
+    proxyValue: useProxyValue('value', props, emit),
+    selectedMenuItem: props.value ?? props.widgetConfig?.optionalFieldsSchema.dateFormat?.options?.default ?? Object.values(DATE_FORMAT)[0] as undefined | MenuItem[] | string,
+});
+
+/* Event */
+const handleUpdateSelect = (val: string|MenuItem[]) => {
+    state.selectedMenuItem = val;
+    state.proxyValue = val;
+};
+
+onMounted(() => {
+    emit('update:is-valid', true);
+    state.proxyValue = props.value ?? props.widgetConfig?.optionalFieldsSchema.dateFormat?.options?.default ?? Object.values(DATE_FORMAT)[0];
+});
+
+
+</script>
+
+<template>
+    <div class="widget-date-format">
+        <p-field-group :label="$t('Date Format')"
+                       required
+        >
+            <p-select-dropdown :menu="state.menuItems"
+                               :selected="state.selectedMenuItem"
+                               use-fixed-menu-style
+                               @update:selected="handleUpdateSelect"
+            />
+        </p-field-group>
+    </div>
+</template>
