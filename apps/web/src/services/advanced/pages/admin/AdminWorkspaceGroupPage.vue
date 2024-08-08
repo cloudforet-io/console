f709525f10769385741e1e3ddb52aaca625c6a25
yubeom <163384398+yubeom@users.noreply.github.com>
2024-08-08 15:19:51 +0900

    feat: add workspace group page route (#4500)
    Signed-off-by: yubeom <kimub1204@mz.co.kr>

---
+++ apps/web/src/services/advanced/pages/admin/AdminWorkspaceGroupPage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import WorkspaceGroupHeader from '@/services/advanced/components/WorkspaceGroupHeader.vue';
+
+</script>
+
+<template>
+    <section class="workspace-group-page">
+        <workspace-group-header />
+    </section>
+</template>
