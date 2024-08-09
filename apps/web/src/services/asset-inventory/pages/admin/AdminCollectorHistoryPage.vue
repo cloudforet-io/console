8f7a87e3933f034b4c8873966ab254d3481823cb
sulmo <sulmo@megazone.com>
2024-08-09 11:34:57 +0900

    fix: add apiQueryHelper logic
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/services/asset-inventory/pages/admin/AdminCollectorHistoryPage.vue
+++ apps/web/src/services/asset-inventory/pages/admin/AdminCollectorHistoryPage.vue
@@ -165,6 +165,7 @@ const handleChange = async (options: ToolboxOptions = {}) => {
 };
 const handleChangePagination = () => {
     state.pageStart = getPageStart(state.thisPage, state.pageSize);
+    apiQueryHelper.setPage(state.pageStart, state.pageSize);
     getJobs();
 };
 
