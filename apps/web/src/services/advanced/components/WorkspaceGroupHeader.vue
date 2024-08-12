f786e877287b956b100b4d9fef0e9f018ce8dc19
yubeom <163384398+yubeom@users.noreply.github.com>
2024-08-12 10:32:46 +0900

    feat: markup workspace group page header, table  (#4509)
    * feat: markup WorkspaceGroupHeader
    
    Signed-off-by: yubeom <kimub1204@mz.co.kr>
    
    * feat: markup WorkspaceGroupTable
    
    Signed-off-by: yubeom <kimub1204@mz.co.kr>
    
    * feat: markup WorkspaceGroupTab(Group User)
    
    Signed-off-by: yubeom <kimub1204@mz.co.kr>
    
    * feat: apply code review
    
    Signed-off-by: yubeom <kimub1204@mz.co.kr>
    
    ---------
    
    Signed-off-by: yubeom <kimub1204@mz.co.kr>

---
+++ apps/web/src/services/advanced/components/WorkspaceGroupHeader.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { PHeading, PButton } from '@cloudforet/mirinae';
+</script>
+
+<template>
+    <section class="workspace-group-header">
+        <p-heading class="workspace-group-header"
+                   :title="$t('IAM.WORKSPACE_GROUP.TITLE')"
+                   use-total-count
+                   :total-count="12"
+        >
+            <template #extra>
+                <p-button style-type="primary"
+                          icon-left="ic_plus_bold"
+                >
+                    {{ $t('IAM.WORKSPACE_GROUP.CREATE') }}
+                </p-button>
+            </template>
+        </p-heading>
+    </section>
+</template>
