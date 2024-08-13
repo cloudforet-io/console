e0e2830699cec579b31a2e6601f326c0adca2973
yubeom <kimub1204@mz.co.kr>
2024-08-13 13:22:25 +0900

    fix: fix store.$dispose() error
    Signed-off-by: yubeom <kimub1204@mz.co.kr>

--- apps/web/src/services/advanced/store/workspace-group-page-store.ts
+++ apps/web/src/services/advanced/store/workspace-group-page-store.ts
@@ -2,14 +2,20 @@ import { reactive, computed } from 'vue';
 
 import { defineStore } from 'pinia';
 
+import type { ConsoleFilter } from '@cloudforet/core-lib/query/type';
+
 import type { WorkspaceGroupModel } from '@/schema/identity/workspace-group/model';
 
 export const useWorkspaceGroupPageStore = defineStore('page-workspace-group', () => {
     const state = reactive({
+        loading: false,
         groups: [] as WorkspaceGroupModel[],
         groupUsers: [],
         workspaces: [],
         selectedIndices: [] as number[],
+        pageStart: 1,
+        pageLimit: 15,
+        searchFilters: [] as ConsoleFilter[],
         modal: {
             type: '',
             title: '',
@@ -18,12 +24,15 @@ export const useWorkspaceGroupPageStore = defineStore('page-workspace-group', ()
         },
     });
 
-    const getters = reactive({
+    // The getters method using reactive will not work when using the store.$dispose method with the error
+    //  "Write operation failed: computed value is readonly" error message when using the store.$dispose method,
+    // so we change to a method that doesn't use the reactive API.
+    const getters = {
         selectedGroup: computed(() => {
             const [index] = state.selectedIndices;
             return state.groups[index];
         }),
-    });
+    };
 
     const actions = {
         updateModalSettings: ({
