040fb1242497dfde5306daaae797d75ff1d50bcf
yuda <yuda@megazone.com>
2024-08-20 10:41:04 +0900

    chore: update icons
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ packages/mirinae/src/foundation/icons/p-icons/ic_arrows-expand-height.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_arrows-expand-height': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: '<path pid="0" d="M12 22.296l4.243-4.243-1.06-1.06-2.433 2.432V13.5h-1.5v5.925l-2.432-2.432-1.06 1.06L12 22.296zM12.75 10.5h-1.5V4.575L8.818 7.007l-1.06-1.06L12 1.704l4.243 4.243-1.061 1.06-2.432-2.432V10.5z" _fill="#232533"/>'
+  }
+})
---
+++ packages/mirinae/src/foundation/icons/p-icons/ic_arrows-expand-width.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_arrows-expand-width': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: '<path pid="0" d="M22.296 12l-4.243-4.243-1.06 1.06 2.432 2.433H13.5v1.5h5.925l-2.432 2.432 1.06 1.06L22.296 12zM10.5 11.25v1.5H4.575l2.432 2.432-1.06 1.06L1.704 12l4.243-4.243 1.06 1.061-2.432 2.432H10.5z" _fill="#232533"/>'
+  }
+})
