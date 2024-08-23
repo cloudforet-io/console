bd4eed30dc51ca18dc2927585ddb91431417b941
yubeom <kimub1204@mz.co.kr>
2024-08-23 19:12:26 +0900

    fix: fix Selected workspace variables not reset
    Signed-off-by: yubeom <kimub1204@mz.co.kr>

--- apps/web/src/services/advanced/components/WorkspaceGroupDeleteStatusModal.vue
+++ apps/web/src/services/advanced/components/WorkspaceGroupDeleteStatusModal.vue
@@ -133,6 +133,7 @@ const handleConfirm = async () => {
 const handleCloseModal = () => {
     workspaceGroupPageStore.closeModal();
     workspaceGroupPageStore.resetSelectedGroupUser();
+    workspaceGroupPageStore.resetSelectedWorkspace();
 };
 </script>
 
