bb3d78b58d892ed5e807f97ad7bec307d231d8bd
yuda <yuda@megazone.com>
2024-08-23 11:54:51 +0900

    feat: add `showPositionField` to legend field option and change value type
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/common/modules/widgets/_widget-fields/WidgetFieldLegend.vue
+++ apps/web/src/common/modules/widgets/_widget-fields/WidgetFieldLegend.vue
@@ -1,15 +1,20 @@
 <script lang="ts" setup>
-import { onMounted, reactive } from 'vue';
+import { computed, onMounted, reactive } from 'vue';
 
-import { PFieldTitle, PToggleButton } from '@cloudforet/mirinae';
+import {
+    PFieldGroup, PFieldTitle, PToggleButton, PSelectDropdown,
+} from '@cloudforet/mirinae';
+
+import { i18n } from '@/translations';
 
 import { useProxyValue } from '@/common/composables/proxy-state';
 import type { LegendOptions, WidgetFieldComponentProps, WidgetFieldComponentEmit } from '@/common/modules/widgets/types/widget-field-type';
+import type { LegendValue } from '@/common/modules/widgets/types/widget-field-value-type';
 
 
 const emit = defineEmits<WidgetFieldComponentEmit<boolean|undefined>>();
 
-const props = withDefaults(defineProps<WidgetFieldComponentProps<LegendOptions, boolean>>(), {
+const props = withDefaults(defineProps<WidgetFieldComponentProps<LegendOptions, LegendValue>>(), {
     widgetFieldSchema: () => ({
         options: {
             default: false,
@@ -18,31 +23,80 @@ const props = withDefaults(defineProps<WidgetFieldComponentProps<LegendOptions, 
 });
 
 const state = reactive({
-    proxyValue: useProxyValue<boolean>('value', props, emit),
+    proxyValue: useProxyValue<LegendValue>('value', props, emit),
+    showPositionField: computed<boolean>(() => !!props.widgetFieldSchema.options?.showPositionField),
+    positionMenuItems: computed(() => ([
+        { name: 'right', label: i18n.t('COMMON.WIDGETS.LEGEND.RIGHT') },
+        { name: 'bottom', label: i18n.t('COMMON.WIDGETS.LEGEND.BOTTOM') },
+    ])),
 });
 
-const handleUpdateValue = (value: boolean) => {
-    state.proxyValue = value;
-    emit('update:value', value);
+const handleUpdateToggleValue = (val: boolean) => {
+    if (val) {
+        state.proxyValue = {
+            toggleValue: val,
+            position: state.showPositionField ? 'right' : undefined,
+        };
+    } else {
+        state.proxyValue = {
+            toggleValue: val,
+            position: undefined,
+        };
+    }
+};
+const handleSelectPosition = (val: 'right'|'bottom') => {
+    state.proxyValue = {
+        ...state.proxyValue,
+        position: val,
+    };
 };
 
 onMounted(() => {
     emit('update:is-valid', true);
-    state.proxyValue = props.value ?? props.widgetFieldSchema.options?.default ?? false;
+    const _toggleValue = props.value?.toggleValue ?? props.widgetFieldSchema.options?.default ?? false;
+    let _position = props.value?.position;
+    if (_toggleValue && state.showPositionField && !_position) {
+        _position = 'right';
+    }
+    state.proxyValue = {
+        toggleValue: _toggleValue,
+        position: _position,
+    };
 });
 </script>
 
 <template>
     <div class="widget-field-legend">
-        <p-field-title>{{ $t('DASHBOARDS.WIDGET.OVERLAY.STEP_2.LEGEND') }}</p-field-title>
-        <p-toggle-button :value="state.proxyValue"
-                         @update:value="handleUpdateValue"
-        />
+        <div class="top-part">
+            <p-field-title>{{ $t('COMMON.WIDGETS.LEGEND.LEGEND') }}</p-field-title>
+            <p-toggle-button :value="state.proxyValue.toggleValue"
+                             @update:value="handleUpdateToggleValue"
+            />
+        </div>
+        <p-field-group v-if="props.widgetFieldSchema.options?.showPositionField"
+                       :label="$t('COMMON.WIDGETS.LEGEND.POSITION')"
+                       style-type="secondary"
+                       class="position-wrapper"
+        >
+            <p-select-dropdown class="w-full"
+                               use-fixed-menu-style
+                               reset-selection-on-menu-close
+                               :menu="state.positionMenuItems"
+                               :selected="state.proxyValue?.position"
+                               @select="handleSelectPosition"
+            />
+        </p-field-group>
     </div>
 </template>
 
 <style lang="postcss" scoped>
 .widget-field-legend {
-    @apply flex items-center gap-1 justify-between;
+    .top-part {
+        @apply flex items-center gap-1 justify-between;
+    }
+    .position-wrapper {
+        padding-top: 0.5rem;
+        margin-bottom: 0;
+    }
 }
 </style>
