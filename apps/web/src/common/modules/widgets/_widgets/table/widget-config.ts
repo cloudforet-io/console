0a060f9337621449823c039c93ffc8a68358fdc4
Yongtae Park <samuel.park@megazone.com>
2024-08-27 16:38:13 +0900

    fix: rollback table missing value
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/common/modules/widgets/_widgets/table/widget-config.ts
+++ apps/web/src/common/modules/widgets/_widgets/table/widget-config.ts
@@ -57,7 +57,7 @@ const table: WidgetConfig = {
         tableColumnWidth: {},
         customTableColumnWidth: {},
         displayAnnotation: {},
-        missingValue: {},
+        // missingValue: {},
     },
     dependencies: {
         groupBy: ['comparison', 'customTableColumnWidth'],
