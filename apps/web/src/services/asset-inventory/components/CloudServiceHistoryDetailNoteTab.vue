83f067af4673e56f22f408a3dcb3e298d4d5696f
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2024-08-29 00:23:22 +0900

    refactor: changing the template structure
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/services/asset-inventory/components/CloudServiceHistoryDetailNoteTab.vue
+++ apps/web/src/services/asset-inventory/components/CloudServiceHistoryDetailNoteTab.vue
@@ -1,63 +1,7 @@
-<template>
-    <p-pane-layout class="cloud-service-history-detail-note">
-        <p-heading heading-type="sub"
-                   use-total-count
-                   :total-count="totalCount"
-                   :title="$t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.NOTE')"
-        />
-        <article class="note-wrapper">
-            <p-collapsible-list :items="noteList"
-                                toggle-position="contents"
-                                :line-clamp="2"
-            >
-                <template #title="{data, title, index}">
-                    <div class="title-wrapper">
-                        <p>
-                            <span class="author">{{ title }}</span>
-                            <span class="date">{{ iso8601Formatter(noteList[index].created_at, timezone) }}</span>
-                        </p>
-                        <p-select-dropdown style-type="icon-button"
-                                           button-icon="ic_ellipsis-horizontal"
-                                           :menu="menuItems"
-                                           menu-position="right"
-                                           :disabled="manageDisabled"
-                                           @select="handleSelect(data.note_id)"
-                        />
-                    </div>
-                </template>
-                <template #default="{data}">
-                    <p-text-beautifier class="note-content"
-                                       :value="data.note"
-                    />
-                </template>
-            </p-collapsible-list>
-        </article>
-        <article class="add-note-wrapper">
-            <p-textarea :value="noteInput"
-                        @input="handleChangeNoteInput"
-            />
-            <p-button style-type="tertiary"
-                      class="add-btn"
-                      :disabled="(noteInput.trim()).length === 0 || manageDisabled"
-                      @click="handleCreateNote"
-            >
-                {{ $t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.NOTE_TAB.ADD_NOTE') }}
-            </p-button>
-        </article>
-        <delete-modal :header-title="checkDeleteState.headerTitle"
-                      :visible.sync="checkDeleteState.visible"
-                      :disabled="checkDeleteState.loading"
-                      @confirm="handleDeleteNote"
-        />
-    </p-pane-layout>
-</template>
-
-<script lang="ts">
-
+<script lang="ts" setup>
 import {
-    computed, reactive, toRefs, watch,
+    computed, reactive, watch,
 } from 'vue';
-
 
 import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 import { ApiQueryHelper } from '@cloudforet/core-lib/space-connector/helper';
@@ -77,147 +21,174 @@ import { i18n } from '@/translations';
 import DeleteModal from '@/common/components/modals/DeleteModal.vue';
 import ErrorHandler from '@/common/composables/error/errorHandler';
 
+interface Props {
+    recordId: string;
+    manageDisabled: boolean;
+}
 
+const props = withDefaults(defineProps<Props>(), {
+    recordId: undefined,
+    manageDisabled: false,
+});
 
-export default {
-    name: 'CloudServiceHistoryDetailNote',
-    components: {
-        PPaneLayout,
-        PHeading,
-        PTextarea,
-        PButton,
-        PCollapsibleList,
-        PSelectDropdown,
-        PTextBeautifier,
-        DeleteModal,
-    },
-    props: {
-        recordId: {
-            type: String,
-            default: undefined,
+const emit = defineEmits<{(e: 'refresh-note-count'): void}>();
+
+const state = reactive({
+    id: '',
+    noteInput: '',
+    noteList: [] as NoteModel[],
+    loading: true,
+    timezone: computed(() => store.state.user.timezone),
+    userId: computed(() => store.state.user.userId),
+    menuItems: computed(() => [
+        {
+            label: i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.NOTE_TAB.DELETE'), name: 'delete',
         },
-        manageDisabled: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    setup(props, { emit }) {
-        const state = reactive({
-            id: '',
-            noteInput: '',
-            noteList: [] as NoteModel[],
-            loading: true,
-            timezone: computed(() => store.state.user.timezone),
-            userId: computed(() => store.state.user.userId),
-            menuItems: computed(() => [
-                {
-                    label: i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.NOTE_TAB.DELETE'), name: 'delete',
-                },
-            ]),
-            selectedNoteIdForDelete: '',
-            totalCount: 0,
+    ]),
+    selectedNoteIdForDelete: '',
+    totalCount: 0,
+});
+
+const handleChangeNoteInput = (e) => {
+    state.noteInput = e.target?.value;
+};
+const apiQuery = new ApiQueryHelper();
+const listNote = async () => {
+    try {
+        state.loading = true;
+        apiQuery.setFilters([{ k: 'record_id', v: props.recordId, o: '=' }]).setSort('created_at');
+        const res = await SpaceConnector.clientV2.inventory.note.list<NoteListParameters, ListResponse<NoteModel>>({
+            query: apiQuery.data,
         });
-
-        const handleChangeNoteInput = (e) => {
-            state.noteInput = e.target?.value;
-        };
-        const apiQuery = new ApiQueryHelper();
-        const listNote = async () => {
-            try {
-                state.loading = true;
-                apiQuery.setFilters([{ k: 'record_id', v: props.recordId, o: '=' }]).setSort('created_at');
-                const res = await SpaceConnector.clientV2.inventory.note.list<NoteListParameters, ListResponse<NoteModel>>({
-                    query: apiQuery.data,
-                });
-                state.noteList = (res.results ?? []).map((d) => ({
-                    title: d.created_by,
-                    data: {
-                        note: d.note,
-                        note_id: d.note_id,
-                    },
-                    ...d,
-                }));
-                state.totalCount = res.total_count ?? 0;
-            } catch (e) {
-                ErrorHandler.handleError(e);
-                state.noteList = [];
-            } finally {
-                state.loading = false;
-            }
-        };
-
-        const handleCreateNote = async () => {
-            try {
-                await SpaceConnector.clientV2.inventory.note.create<NoteCreateParameters, NoteModel>({
-                    record_id: props.recordId,
-                    note: state.noteInput,
-                });
-            } catch (e) {
-                ErrorHandler.handleRequestError(e, 'Failed to Create Note');
-            } finally {
-                state.noteInput = '';
-                await listNote();
-                emit('refresh-note-count');
-            }
-        };
-
-        const checkDeleteState = reactive({
-            headerTitle: computed(() => i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.NOTE_TAB.DELETE_HELP_TEXT')),
-            visible: false,
-            loading: false,
-        });
-
-        const openDeleteModal = () => {
-            checkDeleteState.visible = true;
-        };
-
-        const handleSelect = (noteId) => {
-            state.selectedNoteIdForDelete = noteId;
-            openDeleteModal();
-        };
-
-        const handleDeleteNote = async () => {
-            checkDeleteState.loading = true;
-            try {
-                await SpaceConnector.clientV2.inventory.note.delete<NoteDeleteParameters>({
-                    note_id: state.selectedNoteIdForDelete,
-                });
-            } catch (e) {
-                ErrorHandler.handleRequestError(e, i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.NOTE_TAB.ALT_E_DELETE_NOTE'));
-            } finally {
-                checkDeleteState.loading = false;
-                checkDeleteState.visible = false;
-                await listNote();
-                emit('refresh-note-count');
-            }
-        };
-
-        watch(() => props.recordId, (recordId) => {
-            state.id = recordId;
-        });
-
-        watch(() => state.id, () => {
-            listNote();
-        });
-
-        (async () => {
-            await listNote();
-        })();
-
-        return {
-            ...toRefs(state),
-            checkDeleteState,
-            iso8601Formatter,
-            handleChangeNoteInput,
-            handleCreateNote,
-            handleSelect,
-            handleDeleteNote,
-            openDeleteModal,
-        };
-    },
+        state.noteList = (res.results ?? []).map((d) => ({
+            title: d.created_by,
+            data: {
+                note: d.note,
+                note_id: d.note_id,
+            },
+            ...d,
+        }));
+        state.totalCount = res.total_count ?? 0;
+    } catch (e) {
+        ErrorHandler.handleError(e);
+        state.noteList = [];
+    } finally {
+        state.loading = false;
+    }
 };
 
+const handleCreateNote = async () => {
+    try {
+        await SpaceConnector.clientV2.inventory.note.create<NoteCreateParameters, NoteModel>({
+            record_id: props.recordId,
+            note: state.noteInput,
+        });
+    } catch (e) {
+        ErrorHandler.handleRequestError(e, 'Failed to Create Note');
+    } finally {
+        state.noteInput = '';
+        await listNote();
+        emit('refresh-note-count');
+    }
+};
+
+const checkDeleteState = reactive({
+    headerTitle: computed(() => i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.NOTE_TAB.DELETE_HELP_TEXT')),
+    visible: false,
+    loading: false,
+});
+
+const openDeleteModal = () => {
+    checkDeleteState.visible = true;
+};
+
+const handleSelect = (noteId) => {
+    state.selectedNoteIdForDelete = noteId;
+    openDeleteModal();
+};
+
+const handleDeleteNote = async () => {
+    checkDeleteState.loading = true;
+    try {
+        await SpaceConnector.clientV2.inventory.note.delete<NoteDeleteParameters>({
+            note_id: state.selectedNoteIdForDelete,
+        });
+    } catch (e) {
+        ErrorHandler.handleRequestError(e, i18n.t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.NOTE_TAB.ALT_E_DELETE_NOTE'));
+    } finally {
+        checkDeleteState.loading = false;
+        checkDeleteState.visible = false;
+        await listNote();
+        emit('refresh-note-count');
+    }
+};
+
+watch(() => props.recordId, (recordId) => {
+    state.id = recordId;
+});
+
+watch(() => state.id, () => {
+    listNote();
+});
+
+(async () => {
+    await listNote();
+})();
 </script>
+
+<template>
+    <p-pane-layout class="cloud-service-history-detail-note">
+        <p-heading heading-type="sub"
+                   use-total-count
+                   :total-count="state.totalCount"
+                   :title="$t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.NOTE')"
+        />
+        <article class="note-wrapper">
+            <p-collapsible-list :items="state.noteList"
+                                toggle-position="contents"
+                                :line-clamp="2"
+            >
+                <template #title="{data, title, index}">
+                    <div class="title-wrapper">
+                        <p>
+                            <span class="author">{{ title }}</span>
+                            <span class="date">{{ iso8601Formatter(state.noteList[index].created_at, state.timezone) }}</span>
+                        </p>
+                        <p-select-dropdown style-type="icon-button"
+                                           button-icon="ic_ellipsis-horizontal"
+                                           :menu="state.menuItems"
+                                           menu-position="right"
+                                           :disabled="props.manageDisabled"
+                                           @select="handleSelect(data.note_id)"
+                        />
+                    </div>
+                </template>
+                <template #default="{data}">
+                    <p-text-beautifier class="note-content"
+                                       :value="data.note"
+                    />
+                </template>
+            </p-collapsible-list>
+        </article>
+        <article class="add-note-wrapper">
+            <p-textarea :value="state.noteInput"
+                        @input="handleChangeNoteInput"
+            />
+            <p-button style-type="tertiary"
+                      class="add-btn"
+                      :disabled="(state.noteInput.trim()).length === 0 || props.manageDisabled"
+                      @click="handleCreateNote"
+            >
+                {{ $t('INVENTORY.CLOUD_SERVICE.HISTORY.DETAIL.NOTE_TAB.ADD_NOTE') }}
+            </p-button>
+        </article>
+        <delete-modal :header-title="checkDeleteState.headerTitle"
+                      :visible.sync="checkDeleteState.visible"
+                      :disabled="checkDeleteState.loading"
+                      @confirm="handleDeleteNote"
+        />
+    </p-pane-layout>
+</template>
 
 <style lang="postcss" scoped>
 .cloud-service-history-detail-note {
