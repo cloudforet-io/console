b7d1777cf8605a80d0d4927db4f13a97c0cc7c02
sulmo <50662170+sulmoJ@users.noreply.github.com>
2024-09-12 20:01:01 -0700

    feat: create removeGroupUser modal (#4701)
    Signed-off-by: sulmo <sulmo@megazone.com>

---
+++ apps/web/src/services/landing/components/workspace-landing/landing-group-workspaces/LandingWorkspaceGroupRemoveUserModal.vue
@@ -0,0 +1,146 @@
+<script setup lang="ts">
+import { reactive, computed } from 'vue';
+
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+import { PTableCheckModal, PLink, PStatus } from '@cloudforet/mirinae';
+
+import { ROLE_TYPE } from '@/schema/identity/role/constant';
+import type { MyWorkspaceGroupModel } from '@/schema/identity/user-profile/model';
+import type { WorkspaceGroupUserRemoveParameters } from '@/schema/identity/workspace-group-user/api-verbs/remove';
+import type { WorkspaceUser, WorkspaceGroupModel } from '@/schema/identity/workspace-group/model';
+
+import { useUserWorkspaceGroupStore } from '@/store/app-context/workspace/user-workspace-group-store';
+import { useAllReferenceStore } from '@/store/reference/all-reference-store';
+import type { WorkspaceReferenceMap } from '@/store/reference/workspace-reference-store';
+
+import ErrorHandler from '@/common/composables/error/errorHandler';
+import { useProxyValue } from '@/common/composables/proxy-state';
+
+import { workspaceStateFormatter, groupUserStateFormatter } from '@/services/advanced/composables/refined-table-data';
+import { ASSET_INVENTORY_ROUTE } from '@/services/asset-inventory/routes/route-constant';
+import { IAM_ROUTE } from '@/services/iam/routes/route-constant';
+import { useLandingPageStore } from '@/services/landing/store/landing-page-store';
+
+const emit = defineEmits<{(e: 'confirm'): void;
+    (e: 'update:visible'): void;
+}>();
+
+interface Props {
+    workspaceGroup?: WorkspaceGroupModel | MyWorkspaceGroupModel
+    removeUserList?: WorkspaceUser[]
+    visible: boolean;
+}
+
+const props = defineProps<Props>();
+
+const userWorkspaceGroupStore = useUserWorkspaceGroupStore();
+const landingPageStore = useLandingPageStore();
+const landingPageStoreState = landingPageStore.state;
+const landingPageStoreGroupUserState = landingPageStore.groupUserTableState;
+
+const allReferenceStore = useAllReferenceStore();
+
+const state = reactive({
+    loading: false,
+    workspaces: computed<WorkspaceReferenceMap>(() => allReferenceStore.getters.workspace),
+    proxyVisible: useProxyValue('visible', props, emit),
+});
+const userTableFields = [{ name: 'user_id', label: 'User ID' },
+    { name: 'name', label: 'Name' },
+    { name: 'state', label: 'State' },
+    { name: 'role_type', label: 'Group Role Type' }];
+
+const getUserRouteLocationByWorkspaceId = (item) => ({
+    name: IAM_ROUTE.USER._NAME,
+    params: {
+        workspaceId: item?.workspace_id,
+    },
+});
+
+const getServiceAccountRouteLocationByWorkspaceId = (item) => ({
+    name: ASSET_INVENTORY_ROUTE.SERVICE_ACCOUNT._NAME,
+    params: {
+        workspaceId: item?.workspace_id,
+    },
+});
+
+
+const deleteGroupUsers = async () => {
+    state.loading = true;
+    try {
+        await SpaceConnector.clientV2.identity.workspaceGroupUser.remove<WorkspaceGroupUserRemoveParameters>({
+            workspace_group_id: landingPageStoreState.selectedProjectGroup,
+            users: (props.removeUserList ?? []).map((item) => ({ user_id: item.user_id })),
+        });
+        await userWorkspaceGroupStore.load();
+        landingPageStoreGroupUserState.selectedIndices = [];
+    } catch (e) {
+        ErrorHandler.handleError(e);
+    } finally {
+        state.loading = false;
+    }
+};
+
+const handleConfirm = async () => {
+    await deleteGroupUsers();
+    state.proxyVisible = false;
+    emit('confirm');
+};
+
+const handleCloseModal = () => {
+    state.proxyVisible = false;
+};
+</script>
+
+<template>
+    <p-table-check-modal class="workspace-group-delete-status-modal"
+                         :header-title="$t('IAM.WORKSPACE_GROUP.MODAL.DELETE_GROUP_USER_TITLE')"
+                         theme-color="alert"
+                         :visible="state.proxyVisible"
+                         :fields="userTableFields"
+                         :items="props.removeUserList || []"
+                         size="sm"
+                         :loading="state.loading"
+                         @confirm="handleConfirm"
+                         @cancel="handleCloseModal"
+                         @close="handleCloseModal"
+    >
+        <template #col-state-format="{ value}">
+            <p-status v-bind=" groupUserStateFormatter(value)"
+                      class="capitalize"
+            />
+        </template>
+        <template #col-role_type-format="{ value }">
+            {{ value === ROLE_TYPE.WORKSPACE_OWNER ? 'Owner' : 'Member' }}
+        </template>
+        <template #col-data.state-format="{ value }">
+            <p-status v-bind="workspaceStateFormatter(value)"
+                      class="capitalize"
+            />
+        </template>
+        <template #col-data.user-format="{ value, item }">
+            <p-link :text="value"
+                    action-icon="internal-link"
+                    new-tab
+                    :to="getUserRouteLocationByWorkspaceId(item)"
+            />
+        </template>
+        <template #col-data.service_account_count-format="{ value, item }">
+            <p-link :text="value || 0"
+                    action-icon="internal-link"
+                    new-tab
+                    :to="getServiceAccountRouteLocationByWorkspaceId(item)"
+            />
+        </template>
+    </p-table-check-modal>
+</template>
+
+<style lang="postcss" scoped>
+.workspace-group-delete-status-modal {
+    .name-wrapper {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+    }
+}
+</style>
