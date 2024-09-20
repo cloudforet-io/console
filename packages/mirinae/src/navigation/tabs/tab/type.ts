135a87384c5262b14df817ecd7556f9b89fa0aab
Piggy Park (박용태) <samuel.park@megazone.com>
2024-09-20 10:18:06 +0900

    feat(p-tab): renewal `PTab` component (#4719)
    * feat(p-tab): renewal `PTab` component
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * chore: update component structure (scrip setup)
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * chore: edit props type
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * feat(storybook): update tab renewal spec
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * chore: small fix
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * chore: edit tab menu `type` property to `tabType`
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * chore: small fix
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * chore: solve handler issue
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    ---------
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- packages/mirinae/src/navigation/tabs/tab/type.ts
+++ packages/mirinae/src/navigation/tabs/tab/type.ts
@@ -1,13 +1,20 @@
 import type { TranslateResult } from 'vue-i18n';
 
+
+export const TAB_MENU_TYPE = {
+    tab: 'tab',
+    divider: 'divider',
+    folder: 'folder',
+} as const;
+
+export type TabMenuType = typeof TAB_MENU_TYPE[keyof typeof TAB_MENU_TYPE];
+
+
 export interface TabItem {
     name: string;
+    tabType?: TabMenuType;
     label?: string | TranslateResult;
     keepAlive?: boolean;
-}
-
-export interface TabProps {
-    activeTab: string;
-    tabs: Array<string|TabItem>;
-    stretch?: boolean;
+    subItems?: TabItem[];
+    icon?: string;
 }
