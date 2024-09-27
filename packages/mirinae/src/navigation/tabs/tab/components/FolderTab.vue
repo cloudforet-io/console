690430da58b8dc9a9b39a3f7e142911254fd8f50
Yongtae Park <samuel.park@megazone.com>
2024-09-27 18:10:04 +0900

    fix: edit PTab component design & separate folder tab compnent
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ packages/mirinae/src/navigation/tabs/tab/components/FolderTab.vue
@@ -0,0 +1,117 @@
+<script setup lang="ts">
+
+
+import { reactive, ref, toRef } from 'vue';
+
+import { onClickOutside } from '@vueuse/core';
+
+import PI from '@/foundation/icons/PI.vue';
+import { useContextMenuFixedStyle } from '@/hooks';
+import PContextMenu from '@/inputs/context-menu/PContextMenu.vue';
+import type { MenuItem } from '@/inputs/context-menu/type';
+import type { TabItem } from '@/navigation/tabs/tab/type';
+
+interface Props {
+    tab: TabItem;
+    activeTab: string;
+    selectedFolderTab?: string;
+    selectedContextMenuItem?: MenuItem;
+    visibleSubMenu?: boolean;
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits<{(e: 'select-tab'): void;
+    (e: 'select-tab-menu', value: MenuItem, idx?: number): void;
+}>();
+
+const folderTabRef = ref<HTMLElement | null>(null);
+const contextMenuRef = ref<HTMLElement | null>(null);
+
+const state = reactive({
+    visible: props.visibleSubMenu,
+});
+
+const handleSelectTab = () => {
+    if (state.visible) {
+        state.visible = false;
+    } else {
+        state.visible = true;
+    }
+    emit('select-tab');
+};
+
+const handleSelectTabMenu = (menu: MenuItem) => {
+    state.visible = false;
+    emit('select-tab-menu', menu);
+};
+
+const {
+    contextMenuStyle,
+} = useContextMenuFixedStyle({
+    useFixedMenuStyle: true,
+    visibleMenu: toRef(state, 'visible'),
+    targetRef: folderTabRef,
+    position: 'left',
+    menuRef: contextMenuRef,
+    multiSelectable: false,
+});
+
+onClickOutside(folderTabRef, () => {
+    state.visible = false;
+});
+
+</script>
+
+<template>
+    <li ref="folderTabRef"
+        :class="{'folder-tab': true, active: props.tab.subItems?.some((subItem) => activeTab === subItem.name)}"
+        role="tab"
+        @keydown.enter="handleSelectTab"
+        @click="handleSelectTab"
+    >
+        <div class="content-wrapper">
+            <span class="label">
+                {{ tab.label }}
+            </span>
+            <p-i class="sub-item-dropdown-icon"
+                 :name="state.visible ? 'ic_chevron-up' : 'ic_chevron-down'"
+                 width="1.25rem"
+                 height="1.25rem"
+                 color="inherit"
+            />
+        </div>
+        <p-context-menu v-if="state.visible"
+                        ref="contextMenuRef"
+                        class="sub-item-menu"
+                        :style="contextMenuStyle"
+                        :menu="props.tab?.subItems || []"
+                        :selected="props.selectedContextMenuItem ? [props.selectedContextMenuItem]: undefined"
+                        @select="handleSelectTabMenu"
+        />
+    </li>
+</template>
+
+<style lang="postcss">
+.folder-tab {
+    &.active {
+        @apply text-primary border-primary;
+    }
+    .content-wrapper {
+        @apply flex items-center justify-center w-full h-full;
+        .label {
+            @apply w-full text-label-md block truncate;
+        }
+        .sub-item-dropdown-icon {
+            min-width: 1.25rem;
+            margin-left: 0.25rem;
+        }
+        .sub-item-menu {
+            position: absolute;
+            top: 100%;
+            left: 0;
+            z-index: 100;
+            width: max-content;
+        }
+    }
+}
+</style>
