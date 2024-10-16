dd24ac8d478fb2e9eafa2cc9e88a92f02efb2358
Piggy Park (박용태) <samuel.park@megazone.com>
2024-10-16 17:47:07 +0900

    feat: create global-variable model  (#4853)
    * feat: create global-variable model
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * fix: edit ReferenceVariable type
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * fix: edit reference type model
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    ---------
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/services/dashboards/types/global-variable-type.ts
@@ -0,0 +1,70 @@
+// Base Model
+interface DashboardGlobalVariableBase {
+    management: 'custom'|'managed';
+    key: string; // new_variable
+    name: string; // New Variable
+    type: 'text'|'number'|'reference';
+}
+
+interface ReferenceVariable extends DashboardGlobalVariableBase {
+    type: 'reference';
+    reference: {
+        resourceType: string; // identity.User, inventory.Collector, etc..
+        dataSourceId?: string;
+        dataKey?: string;
+    }
+    options: {
+        selectionType: 'multi'|'single';
+    }
+}
+
+// 'text' type / 'any' valueType
+interface TextAnyVariable extends DashboardGlobalVariableBase {
+    type: 'text';
+    valueType: 'any';
+    options: {
+        defaultValue?: string;
+    }
+}
+
+// 'number' type / 'any' valueType
+interface NumberAnyVariable extends DashboardGlobalVariableBase {
+    type: 'number';
+    valueType: 'any';
+    options: {
+        min: number;
+        max: number;
+        step?: number;
+        inputType: 'input'|'slider';
+    }
+}
+
+// 'text' type / 'enum' valueType
+interface TextEnumVariable extends DashboardGlobalVariableBase {
+    type: 'text';
+    valueType: 'enum';
+    values: Array<{label: string; key: string;}>;
+    options: {
+        selectionType: 'multi'|'single';
+    }
+}
+
+// 'number' type / 'enum' valueType
+interface NumberEnumVariable extends DashboardGlobalVariableBase {
+    type: 'number';
+    valueType: 'enum';
+    values: Array<{label: string; key: number;}>;
+    options: {
+        selectionType: 'multi'|'single';
+    }
+}
+
+// Model
+export type DashboardGlobalVariableModel =
+    | ReferenceVariable
+    | TextAnyVariable
+    | NumberAnyVariable
+    | TextEnumVariable
+    | NumberEnumVariable;
+
+
