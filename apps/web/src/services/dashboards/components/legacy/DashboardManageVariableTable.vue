4a47f39c8773be2c8e9c12bf76771692c6c0e3b7
yuda <yuda@megazone.com>
2024-10-21 18:29:58 +0900

    feat: set managed GV to GV overlay table
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/dashboards/components/legacy/DashboardManageVariableTable.vue
+++ apps/web/src/services/dashboards/components/legacy/DashboardManageVariableTable.vue
@@ -1,45 +1,58 @@
 <script setup lang="ts">
 import {
-    computed, reactive, watch,
+    computed, reactive,
 } from 'vue';
 import type { TranslateResult } from 'vue-i18n';
-
-import { isEqual } from 'lodash';
 
 import {
     PBadge, PDataTable, PToggleButton, PIconButton,
 } from '@cloudforet/mirinae';
+import type { DataTableField } from '@cloudforet/mirinae/src/data-display/tables/data-table/type';
 
-import type {
-    DashboardVariableSchemaProperty,
-} from '@/schema/dashboard/_types/dashboard-type';
 import { i18n } from '@/translations';
 
-import { useDashboardDetailInfoStore } from '@/services/dashboards/stores/dashboard-detail-info-store';
+import {
+    MANAGED_DASHBOARD_GLOBAL_VARIABLES_SCHEMA,
+} from '@/services/dashboards/constants/managed-dashboard-global-variables';
 
 
 
-interface VariablesPropertiesForManage extends DashboardVariableSchemaProperty {
-    propertyName: string;
-    manageable?: boolean;
+interface GlobalVariableTableItem {
+    key: string;
+    name: string;
+    type: string;
+    management: VariableType;
+    use: boolean;
+    created_by: string;
+    reference: string|TranslateResult;
 }
 interface EmitFn {
     (e: 'delete', value: string): void;
     (e: 'edit', name: string): void;
     (e: 'clone', name: string): void;
 }
-
-type VariableType = 'ALL'|'MANAGED'|'CUSTOM';
-
+type VariableType = 'managed'|'custom';
 const emit = defineEmits<EmitFn>();
-
-const dashboardDetailStore = useDashboardDetailInfoStore();
-const dashboardDetailState = dashboardDetailStore.state;
-const dashboardDetailGetters = dashboardDetailStore.getters;
-
 const state = reactive({
-    orderedVariables: [] as VariablesPropertiesForManage[],
-    selectedVariableType: 'ALL' as VariableType,
+    globalVariablesTableItems: computed<GlobalVariableTableItem[]>(() => {
+        const _managedItems: GlobalVariableTableItem[] = [];
+        Object.values(MANAGED_DASHBOARD_GLOBAL_VARIABLES_SCHEMA).forEach((d) => {
+            _managedItems.push({
+                key: d.key,
+                name: d.name,
+                type: '-',
+                management: 'managed',
+                use: true, // TODO: get use
+                created_by: 'System',
+                reference: i18n.t('DASHBOARDS.DETAIL.VARIABLES.COMMON'),
+            });
+        });
+        const _customItems = [];
+        return [
+            ..._managedItems,
+            ..._customItems,
+        ];
+    }),
     variableFields: [
         { name: 'name', label: 'Name' },
         { name: 'type', label: 'Type' },
@@ -48,10 +61,10 @@ const state = reactive({
         { name: 'use', label: 'Use', width: '90px' },
         { name: 'created_by', label: 'Created By' },
         { name: 'buttons', label: ' ', width: '144px' },
-    ],
+    ] as DataTableField[],
     variableType: computed<Partial<Record<VariableType, TranslateResult>>>(() => ({
-        MANAGED: i18n.t('DASHBOARDS.CUSTOMIZE.VARIABLES.FILTER_MANAGED'),
-        CUSTOM: i18n.t('DASHBOARDS.CUSTOMIZE.VARIABLES.FILTER_CUSTOM'),
+        managed: i18n.t('DASHBOARDS.CUSTOMIZE.VARIABLES.FILTER_MANAGED'),
+        custom: i18n.t('DASHBOARDS.CUSTOMIZE.VARIABLES.FILTER_CUSTOM'),
     })),
 });
 
@@ -71,35 +84,22 @@ const handleToggleUse = () => {
 
 /* Helper */
 const variableTypeBadgeStyleFormatter = (type: VariableType) => {
-    if (type === 'MANAGED') return 'gray500';
+    if (type === 'managed') return 'gray500';
     return 'primary';
 };
-const convertAndUpdateVariablesForTable = (order: string[]) => {
-    const properties = dashboardDetailGetters.refinedVariablesSchema.properties;
-    state.orderedVariables = order.map((d) => ({
-        ...properties[d],
-        propertyName: d,
-        manageable: properties[d].variable_type !== 'MANAGED' ? true : undefined,
-    }));
-};
-
-watch(() => dashboardDetailState.variablesSchema.order, (after, before) => {
-    if (isEqual(after, before)) return;
-    convertAndUpdateVariablesForTable(after);
-}, { immediate: true });
 </script>
 
 <template>
     <div class="list-wrapper">
         <p-data-table class="variable-table"
-                      :items="state.orderedVariables"
+                      :items="state.globalVariablesTableItems"
                       :fields="state.variableFields"
         >
             <template #col-management-format="{ value }">
                 <p-badge :style-type="variableTypeBadgeStyleFormatter(value)"
                          badge-type="solid-outline"
                 >
-                    {{ state.variableType[value] ? state.variableType[value] : state.variableType.CUSTOM }}
+                    {{ state.variableType[value] }}
                 </p-badge>
             </template>
             <template #col-use-format="{ value, item }">
@@ -108,17 +108,20 @@ watch(() => dashboardDetailState.variablesSchema.order, (after, before) => {
                                  @change-toggle="handleToggleUse"
                 />
             </template>
-            <template #col-buttons-format="{ item, value }">
-                <div v-if="value"
-                     class="button-wrapper"
-                >
+            <template #col-buttons-format="{ item }">
+                <div class="button-wrapper">
                     <p-icon-button name="ic_clone"
+                                   size="sm"
                                    @click="handleCloneVariable(item.key)"
                     />
-                    <p-icon-button name="ic_edit"
+                    <p-icon-button v-if="item.management === 'custom'"
+                                   name="ic_edit"
+                                   size="sm"
                                    @click="handleEditVariable(item.key)"
                     />
-                    <p-icon-button name="ic_delete"
+                    <p-icon-button v-if="item.management === 'custom'"
+                                   name="ic_delete"
+                                   size="sm"
                                    style-type="negative-transparent"
                                    @click="handleDeleteVariable(item.key)"
                     />
@@ -131,11 +134,8 @@ watch(() => dashboardDetailState.variablesSchema.order, (after, before) => {
 <style lang="postcss" scoped>
 .list-wrapper {
     .variable-table {
-        .options-area {
-            padding: 0;
-        }
         .button-wrapper {
-            @apply w-full flex items-center justify-between;
+            @apply w-full flex items-center;
         }
     }
 }
