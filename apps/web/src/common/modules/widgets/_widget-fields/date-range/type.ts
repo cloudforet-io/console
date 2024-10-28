461b75aeec043284b8fe5daad22350c8dcb3529f
Piggy Park (박용태) <samuel.park@megazone.com>
2024-10-28 10:17:17 +0900

    feat(widget-date-range): create date-range field (#4909)
    * feat(widget-date-range): create date-range field
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * feat(date-range): create date-range field config and apply to step2
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * chore: translation
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * chore: apply review
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    ---------
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/common/modules/widgets/_widget-fields/date-range/type.ts
@@ -0,0 +1,47 @@
+import type {
+    DATE_RANGE_MONTHLY_VALUE_MAP, DATE_RANGE_YEARLY_VALUE_MAP, DATE_RANGE_DAILY_VALUE_MAP,
+} from '@/common/modules/widgets/_widget-fields/date-range/constant';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface DateRangeOptions {
+    //
+}
+
+export type DateRangeValue = InheritDateRangeValue | DisinheritDateRangeValue;
+
+interface BaseDateRangeValue {
+    inherit: boolean;
+    options: {
+        value: DateRangeValueType;
+        start?: string|number; // value "custom" only
+        end?: string|number; // value "custom" only
+    }
+}
+
+interface InheritDateRangeValue extends BaseDateRangeValue {
+    inherit: true;
+    options: {
+        value: DateRangeValueType;
+        start?: number; // granularity MONTHLY 1 ~ 12
+        end?: number; // granularity MONTHLY 1 ~ 12
+    }
+}
+
+interface DisinheritDateRangeValue extends BaseDateRangeValue {
+    inherit: false;
+    options: {
+        value: DateRangeValueType;
+        start?: string; // "2024-01-01"
+        end?: string; // "2024-12-31"
+    }
+}
+
+export type DateRangeValueType = 'auto'
+    | DateRangeMonthlyValueType
+    | DateRangeDailyValueType
+    | DateRangeYearlyValueType
+    | 'custom';
+
+type DateRangeMonthlyValueType = keyof typeof DATE_RANGE_MONTHLY_VALUE_MAP;
+type DateRangeDailyValueType = keyof typeof DATE_RANGE_DAILY_VALUE_MAP;
+type DateRangeYearlyValueType = keyof typeof DATE_RANGE_YEARLY_VALUE_MAP;
