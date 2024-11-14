8d5c9a07fb92262fdc0ac2a0ec928bc149dcea9c
nayeongkim <nayeongkim@megazone.com>
2024-11-14 19:51:27 +0900

    fix: user permission application
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/services/iam/pages/UserMainPage.vue
+++ apps/web/src/services/iam/pages/UserMainPage.vue
@@ -110,11 +110,17 @@ onUnmounted(() => {
             </template>
         </p-horizontal-layout>
         <user-management-tab :has-read-write-access="state.hasReadWriteAccess" />
-        <user-management-add-modal @confirm="refreshUserList" />
-        <user-management-only-remove-workspace-group-type-modal />
-        <user-management-remove-mixed-type-modal />
-        <user-management-status-modal @confirm="refreshUserList" />
-        <user-management-form-modal @confirm="refreshUserList" />
+        <user-management-add-modal v-if="state.hasReadWriteAccess"
+                                   @confirm="refreshUserList"
+        />
+        <user-management-only-remove-workspace-group-type-modal v-if="state.hasReadWriteAccess" />
+        <user-management-remove-mixed-type-modal v-if="state.hasReadWriteAccess" />
+        <user-management-status-modal v-if="state.hasReadWriteAccess"
+                                      @confirm="refreshUserList"
+        />
+        <user-management-form-modal v-if="state.hasReadWriteAccess"
+                                    @confirm="refreshUserList"
+        />
     </section>
 </template>
 
