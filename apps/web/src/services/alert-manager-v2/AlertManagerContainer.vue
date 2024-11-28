1102190b133e94a48df5a8001b26628c4997be2d
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2024-11-28 23:05:07 +0900

    feat: create alert manager container
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

---
+++ apps/web/src/services/alert-manager-v2/AlertManagerContainer.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { useRoute } from 'vue-router/composables';
+
+import CenteredPageLayout from '@/common/modules/page-layouts/CenteredPageLayout.vue';
+import GeneralPageLayout from '@/common/modules/page-layouts/GeneralPageLayout.vue';
+
+const route = useRoute();
+
+</script>
+
+<template>
+    <fragment>
+        <centered-page-layout v-if="route.meta.centeredLayout"
+                              has-nav-bar
+        >
+            <router-view />
+        </centered-page-layout>
+        <general-page-layout v-else>
+            <router-view />
+        </general-page-layout>
+    </fragment>
+</template>
