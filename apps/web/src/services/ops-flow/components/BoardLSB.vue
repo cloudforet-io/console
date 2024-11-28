9d08206cb4546bb1471fbc4d29dd00cbde3108a8
Wanjin Noh <wanjin@megazone.com>
2024-11-28 10:01:56 +0900

    feat: update lsb component structure with new navigation elements
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/ops-flow/components/BoardLSB.vue
+++ apps/web/src/services/ops-flow/components/BoardLSB.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-// import { useRoute, useRouter } from 'vue-router/composables';
-
-import LSB from '@/common/modules/navigations/lsb/LSB.vue';
-import type { LSBMenu } from '@/common/modules/navigations/lsb/type';
-
-//
-// const router = useRouter();
-// const route = useRoute();
-
-const menuSet = ref<LSBMenu[]>([]);
-
+import LSBContainer from '@/common/modules/navigations/new-lsb/LSBContainer.vue';
+import LSBDivider from '@/common/modules/navigations/new-lsb/LSBDivider.vue';
+import LSBRouterButton from '@/common/modules/navigations/new-lsb/LSBRouterButton.vue';
+import LSBRouterItem from '@/common/modules/navigations/new-lsb/LSBRouterItem.vue';
+import LSBTopTitle from '@/common/modules/navigations/new-lsb/LSBTopTitle.vue';
 </script>
 
 <template>
-    <l-s-b :menu-set="menuSet" />
+    <l-s-b-container>
+        <l-s-b-router-button icon="ic_dots-4-square">
+            All Categories
+        </l-s-b-router-button>
+        <l-s-b-divider />
+        <l-s-b-top-title>Category</l-s-b-top-title>
+        <l-s-b-router-item id="test"
+                           to="/test"
+        >
+            Test
+        </l-s-b-router-item>
+    </l-s-b-container>
 </template>
