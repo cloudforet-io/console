91206e11f060f80fdd040301957bbd50a7b7c6e6
Wanjin Noh <wanjin@megazone.com>
2024-11-29 17:05:32 +0900

    feat: add LabelsInput component for managing label inputs in forms
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/ops-flow/task-fields-form/field-templates/LabelsTaskField.vue
+++ apps/web/src/services/ops-flow/task-fields-form/field-templates/LabelsTaskField.vue
@@ -1,7 +1,40 @@
 <script setup lang="ts">
+import {
+    PFieldGroup,
+} from '@cloudforet/mirinae';
 
+import type { TextTaskField } from '@/schema/opsflow/_types/task-field-type';
+
+import LabelsInput from '@/common/components/inputs/LabelsInput.vue';
+
+import { useTaskFieldValidation } from '@/services/ops-flow/task-fields-form/composables/use-task-field-validation';
+import type { TaskFieldFormProps } from '@/services/ops-flow/task-fields-form/types/task-field-form-type';
+
+const props = defineProps<TaskFieldFormProps<TextTaskField, string[]>>();
+
+const emit = defineEmits<{(event: 'update:value', value: string[]): void;
+}>();
+
+const {
+    value, setValue,
+    isInvalid, invalidText,
+} = useTaskFieldValidation(props);
+
+const handleUpdate = (val: string[]) => {
+    setValue(val);
+    emit('update:value', val);
+};
 </script>
 
 <template>
-    <div>LabelsTaskFieldForm</div>
+    <p-field-group :label="field.name"
+                   :required="field.is_required"
+                   :invalid="isInvalid"
+                   :invalid-text="invalidText"
+    >
+        <labels-input :labels="value"
+                      editable
+                      @update:value="handleUpdate"
+        />
+    </p-field-group>
 </template>
