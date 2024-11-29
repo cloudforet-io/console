80b649a416fe1ece6c61a6c137ea3db75f14fe04
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2024-11-29 12:57:41 +0900

    feat: create header at service detail page
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/services/alert-manager-v2/pages/ServiceDetailPage.vue
+++ apps/web/src/services/alert-manager-v2/pages/ServiceDetailPage.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
 
+import ServiceDetailHeader from '@/services/alert-manager-v2/components/ServiceDetailHeader.vue';
+
 const props = defineProps<{
     serviceId: string;
 }>();
 </script>
 
 <template>
-    <div>
-        {{ props.serviceId }}
+    <div class="service-detail-page">
+        <service-detail-header :service-id="props.serviceId" />
     </div>
 </template>
