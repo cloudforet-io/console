6719cff775f57bb0ce8aee1e9b1a24b6424dcbda
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2024-12-03 01:00:04 +0900

    feat: markup for step 3 of the service creation page
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/services/alert-manager-v2/pages/ServiceCreatePage.vue
+++ apps/web/src/services/alert-manager-v2/pages/ServiceCreatePage.vue
@@ -9,6 +9,7 @@ import { i18n } from '@/translations';
 
 import ServiceCreateStep1 from '@/services/alert-manager-v2/components/ServiceCreateStep1.vue';
 import ServiceCreateStep2 from '@/services/alert-manager-v2/components/ServiceCreateStep2.vue';
+import ServiceCreateStep3 from '@/services/alert-manager-v2/components/ServiceCreateStep3.vue';
 import { ALERT_MANAGER_V2_ROUTE } from '@/services/alert-manager-v2/routes/route-constant';
 import { useServiceFormStore } from '@/services/alert-manager-v2/store/service-form-store';
 
@@ -79,6 +80,7 @@ onUnmounted(() => {
         />
         <service-create-step1 v-if="storeState.step === 1" />
         <service-create-step2 v-if="storeState.step === 2" />
+        <service-create-step3 v-if="storeState.step === 3" />
     </div>
 </template>
 
