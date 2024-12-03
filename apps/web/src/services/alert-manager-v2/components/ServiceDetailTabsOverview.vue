f5893cd497c10a49bb9a9ccba28d228aa1e6763d
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2024-12-03 16:00:29 +0900

    feat: markup for the extra section in the overview tab of the service detail page
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/services/alert-manager-v2/components/ServiceDetailTabsOverview.vue
+++ apps/web/src/services/alert-manager-v2/components/ServiceDetailTabsOverview.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { PDivider } from '@cloudforet/mirinae';
 
+import ServiceDetailTabsOverviewEscalationPolicy
+    from '@/services/alert-manager-v2/components/ServiceDetailTabsOverviewEscalationPolicy.vue';
+import ServiceDetailTabsOverviewNotification
+    from '@/services/alert-manager-v2/components/ServiceDetailTabsOverviewNotification.vue';
 import ServiceDetailTabsOverviewStatusTable
     from '@/services/alert-manager-v2/components/ServiceDetailTabsOverviewStatusTable.vue';
 import ServiceDetailTabsOverviewWebhook
@@ -14,6 +18,14 @@ import ServiceDetailTabsOverviewWebhook
         <p-divider class="divider" />
         <service-detail-tabs-overview-webhook />
         <p-divider class="divider" />
+        <div class="section-wrapper">
+            <service-detail-tabs-overview-notification class="section" />
+            <p-divider class="divider"
+                       vertical
+            />
+            <service-detail-tabs-overview-escalation-policy class="section" />
+        </div>
+        <p-divider class="divider" />
     </div>
 </template>
 
@@ -22,6 +34,17 @@ import ServiceDetailTabsOverviewWebhook
     .divider {
         @apply bg-gray-100;
         height: 0.25rem;
+    }
+    .section-wrapper {
+        @apply flex;
+        .divider {
+            width: 0.25rem;
+            height: unset;
+        }
+        .section {
+            width: calc(50% - 0.125rem);
+            height: 17.25rem;
+        }
     }
 }
 </style>
--- apps/web/src/services/alert-manager-v2/components/ServiceDetailTabsOverviewNotification.vue
+++ apps/web/src/services/alert-manager-v2/components/ServiceDetailTabsOverviewNotification.vue
@@ -8,7 +8,7 @@ import {
 
 import { assetUrlConverter } from '@/lib/helper/asset-helper';
 
-const ITEM_DEFAULT_WIDTH = 184 + 8;
+const ITEM_DEFAULT_WIDTH = 120 + 8;
 const DEFAULT_LEFT_PADDING = 16;
 
 const rowItemsWrapperRef = ref<null | HTMLElement>(null);
@@ -16,40 +16,33 @@ const itemEl = ref<null | HTMLElement>(null);
 
 const { width: rowItemsWrapperWidth } = useElementSize(rowItemsWrapperRef);
 
-
 const state = reactive({
     pageStart: 0,
     // TODO: temp data
     items: [
         {
             title: 'title',
-            requests: 12,
-            icon: 'https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/aws-cloudtrail.svg',
+            icon: 'ic_notification-protocol_envelope',
         },
         {
             title: 'title1',
-            requests: 12,
-            icon: 'https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/aws-cloudtrail.svg',
+            icon: 'ic_notification-protocol_envelope',
         },
         {
             title: 'title2',
-            requests: 12,
-            icon: 'https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/aws-cloudtrail.svg',
+            icon: 'ic_notification-protocol_envelope',
         },
         {
             title: 'title3',
-            requests: 12,
-            icon: 'https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/aws-cloudtrail.svg',
+            icon: 'ic_notification-protocol_envelope',
         },
         {
             title: 'title4',
-            requests: 12,
-            icon: 'https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/aws-cloudtrail.svg',
+            icon: 'ic_notification-protocol_envelope',
         },
         {
             title: 'title1',
-            requests: 12,
-            icon: 'https://spaceone-custom-assets.s3.ap-northeast-2.amazonaws.com/console-assets/icons/aws-cloudtrail.svg',
+            icon: 'ic_notification-protocol_envelope',
         },
     ],
     visibleCount: computed<number>(() => Math.floor((rowItemsWrapperWidth.value - DEFAULT_LEFT_PADDING) / ITEM_DEFAULT_WIDTH)),
@@ -75,78 +68,76 @@ const handleRouteDetail = () => {
 </script>
 
 <template>
-    <div class="service-detail-tabs-overview-webhook">
+    <div class="service-detail-tabs-overview-notification">
         <p-field-title class="field-title"
-                       :label="$t('ALERT_MANAGER.WEBHOOK.TITLE')"
+                       :label="$t('ALERT_MANAGER.NOTIFICATIONS.NOTIFICATIONS_CHANNEL')"
                        size="lg"
                        font-weight="regular"
         />
-        <div ref="rowItemsWrapperRef"
-             class="row-items-wrapper"
-        >
-            <div ref="itemEl"
-                 class="row-items-container"
+        <div class="flex-1 pt-2">
+            <div ref="rowItemsWrapperRef"
+                 class="row-items-wrapper"
             >
-                <div v-for="(item, idx) in state.items"
-                     :key="`service-detail-webhook-item-${idx}`"
-                     class="item"
+                <div ref="itemEl"
+                     class="row-items-container"
                 >
-                    <div class="image-wrapper">
-                        <p-lazy-img :src="assetUrlConverter(item.icon)"
-                                    width="1.25rem"
-                                    height="1.25rem"
-                                    error-icon="ic_webhook"
-                                    class="image"
-                        />
-                    </div>
-                    <div>
-                        <p class="text-label-md leading-8">
+                    <div v-for="(item, idx) in state.items"
+                         :key="`service-detail-notification-item-${idx}`"
+                         class="item"
+                    >
+                        <div class="image-wrapper">
+                            <p-lazy-img :src="assetUrlConverter(item.icon)"
+                                        width="1.25rem"
+                                        height="1.25rem"
+                                        error-icon="ic_notification-protocol_envelope"
+                                        class="image"
+                            />
+                        </div>
+                        <p class="text-label-md leading-8 flex-1">
                             {{ item.title }}
-                        </p>
-                        <p class="text-label-sm text-gray-700">
-                            {{ $t('ALERT_MANAGER.WEBHOOK.REQUEST', { cnt: item.requests}) }}
                         </p>
                     </div>
                 </div>
+                <p-icon-button v-if="state.pageStart !== 0"
+                               class="arrow-button left"
+                               name="ic_chevron-left"
+                               color="inherit transparent"
+                               width="1.5rem"
+                               height="1.5rem"
+                               @click="handleClickArrowButton(-1)"
+                />
+                <p-icon-button v-if="state.pageStart !== Number(state.pageMax)"
+                               class="arrow-button right"
+                               name="ic_chevron-right"
+                               color="inherit transparent"
+                               width="1.5rem"
+                               height="1.5rem"
+                               @click="handleClickArrowButton(1)"
+                />
             </div>
-            <p-icon-button v-if="state.pageStart !== 0"
-                           class="arrow-button left"
-                           name="ic_chevron-left"
-                           color="inherit transparent"
-                           width="1.5rem"
-                           height="1.5rem"
-                           @click="handleClickArrowButton(-1)"
-            />
-            <p-icon-button v-if="state.pageStart !== Number(state.pageMax)"
-                           class="arrow-button right"
-                           name="ic_chevron-right"
-                           color="inherit transparent"
-                           width="1.5rem"
-                           height="1.5rem"
-                           @click="handleClickArrowButton(1)"
-            />
         </div>
-        <p-divider class="bg-gray-150" />
-        <div class="link-wrapper">
-            <p-link :text="$t('ALERT_MANAGER.SERVICE.DETAILS')"
-                    action-icon="internal-link"
-                    new-tab
-                    highlight
-                    :to="handleRouteDetail"
-            />
+        <div>
+            <p-divider class="bg-gray-150" />
+            <div class="link-wrapper">
+                <p-link :text="$t('ALERT_MANAGER.SERVICE.DETAILS')"
+                        action-icon="internal-link"
+                        new-tab
+                        highlight
+                        :to="handleRouteDetail"
+                />
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="postcss">
-.service-detail-tabs-overview-webhook {
+.service-detail-tabs-overview-notification {
+    @apply flex flex-col;
     .field-title {
         padding: 1.375rem 1rem;
     }
     .row-items-wrapper {
         @apply relative overflow-hidden;
-        padding-top: 0.75rem;
-        padding-bottom: 1.75rem;
         .row-items-container {
             @apply flex overflow-hidden;
             gap: 0.5rem;
@@ -159,9 +150,9 @@ const handleRouteDetail = () => {
                 padding: 0.375rem;
             }
             .item {
-                @apply flex items-start border border-gray-150;
-                width: 11.5rem;
-                min-width: 11.5rem;
+                @apply flex items-start bg-gray-100;
+                width: 7.5rem;
+                min-width: 7.5rem;
                 padding: 0.375rem 0.5rem;
                 border-radius: 0.75rem;
             }
@@ -177,7 +168,7 @@ const handleRouteDetail = () => {
         }
         .arrow-button {
             @apply absolute bg-white border border-gray-300 rounded-full;
-            top: calc(50% - 1.5rem);
+            top: calc(50% - 1rem);
             width: 2rem;
             height: 2rem;
             box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
@@ -197,6 +188,7 @@ const handleRouteDetail = () => {
     }
     .link-wrapper {
         @apply flex items-center justify-center;
+        margin-top: auto;
         padding-top: 0.625rem;
         padding-bottom: 0.75rem;
     }
