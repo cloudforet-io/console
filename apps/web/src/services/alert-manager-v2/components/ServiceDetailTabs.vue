30f93ffe3f49e0f29bc82ad1f9f6e226d1ed0333
NaYeong,Kim(김나영) <nayeongkim@megazone.com>
2024-12-03 12:17:33 +0900

    feat: create tabs at service detail page
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

---
+++ apps/web/src/services/alert-manager-v2/components/ServiceDetailTabs.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { computed, reactive } from 'vue';
+
+import { PTab } from '@cloudforet/mirinae';
+import type { TabItem } from '@cloudforet/mirinae/types/navigation/tabs/tab/type';
+
+import { i18n } from '@/translations';
+
+import { SERVICE_DETAIL_TABS } from '@/services/alert-manager-v2/constants/alert-manager-constant';
+
+const tabState = reactive({
+    tabs: computed<TabItem[]>(() => ([
+        { label: i18n.t('ALERT_MANAGER.SERVICE.OVERVIEW'), name: SERVICE_DETAIL_TABS.OVERVIEW },
+        { label: i18n.t('ALERT_MANAGER.WEBHOOK.TITLE'), name: SERVICE_DETAIL_TABS.WEBHOOK },
+        { label: i18n.t('ALERT_MANAGER.NOTIFICATIONS.TITLE'), name: SERVICE_DETAIL_TABS.NOTIFICATIONS },
+        { label: i18n.t('ALERT_MANAGER.EVENT_RULE.TITLE'), name: SERVICE_DETAIL_TABS.EVENT_RULE },
+        { label: i18n.t('ALERT_MANAGER.ESCALATION_POLICY.TITLE'), name: SERVICE_DETAIL_TABS.ESCALATION_POLICY },
+    ])),
+    activeTab: SERVICE_DETAIL_TABS.OVERVIEW,
+});
+</script>
+
+<template>
+    <p-tab :tabs="tabState.tabs"
+           :active-tab.sync="tabState.activeTab"
+    >
+        <template #overview>
+            overview
+        </template>
+        <template #webhook>
+            webhook
+        </template>
+        <template #notifications>
+            notifications
+        </template>
+        <template #event_rule>
+            event_rule
+        </template>
+        <template #escalation_policy>
+            escalation_policy
+        </template>
+    </p-tab>
+</template>
