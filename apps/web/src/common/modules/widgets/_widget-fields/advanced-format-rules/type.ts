901418f0c92f40afb2b6357dacc49dae0125bc2c
Yongtae Park <samuel.park@megazone.com>
2024-12-04 15:47:57 +0900

    feat: create temporary formatRules type and constant
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/common/modules/widgets/_widget-fields/advanced-format-rules/type.ts
+++ apps/web/src/common/modules/widgets/_widget-fields/advanced-format-rules/type.ts
@@ -1,4 +1,4 @@
-import type { ADVANCED_FORMAT_RULE_TYPE } from '@/common/modules/widgets/_constants/widget-field-constant';
+import type { _FORMAT_RULE_TYPE, ADVANCED_FORMAT_RULE_TYPE } from '@/common/modules/widgets/_constants/widget-field-constant';
 import type { FormatRulesValue } from '@/common/modules/widgets/_widget-fields/format-rules/type';
 
 export interface AdvancedFormatRulesValue {
@@ -10,7 +10,31 @@ export interface AdvancedFormatRulesValue {
 export type AdvancedFormatRulesType = typeof ADVANCED_FORMAT_RULE_TYPE[keyof typeof ADVANCED_FORMAT_RULE_TYPE];
 
 export interface AdvancedFormatRulesOptions {
+    useField?: boolean;
     formatRulesType: AdvancedFormatRulesType;
     description?: string;
     baseColor?: string;
+    default?: FormatRulesValue[];
 }
+
+export interface ThresholdValue {
+    text?: string;
+    number?: number;
+    color: string;
+}
+
+export interface _FormatRulesValue {
+    field?: string;
+    rules: ThresholdValue[];
+    baseColor?: string;
+}
+
+export interface _FormatRulesOptions {
+    useField?: boolean;
+    formatRulesType: _FormatRulesType;
+    description?: string;
+    baseColor?: string;
+    default?: ThresholdValue[];
+}
+
+export type _FormatRulesType = typeof _FORMAT_RULE_TYPE[keyof typeof _FORMAT_RULE_TYPE];
