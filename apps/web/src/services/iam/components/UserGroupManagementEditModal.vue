8cba3abd48999960af7b010dd8f140eef9fde307
이승연 <sylee1274@mz.co.kr>
2024-12-06 18:09:08 +0900

    feat: update edit user group modal details
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>

--- apps/web/src/services/iam/components/UserGroupManagementEditModal.vue
+++ apps/web/src/services/iam/components/UserGroupManagementEditModal.vue
@@ -1,11 +1,10 @@
 <script lang="ts" setup>
 import { reactive } from 'vue';
 
-import { PButtonModal } from '@cloudforet/mirinae';
+import { PButtonModal, PButton } from '@cloudforet/mirinae';
 
 import UserGroupManagementAddGroupInfo from '@/services/iam/components/UserGroupManagementAddGroupInfo.vue';
-import UserGroupManagementAddNotificationChannelInfo
-    from '@/services/iam/components/UserGroupManagementAddNotificationChannelInfo.vue';
+import UserGroupManagementScheduleSetting from '@/services/iam/components/UserGroupManagementScheduleSetting.vue';
 import { USER_GROUP_MODAL_TYPE } from '@/services/iam/constants/user-group-constant';
 import { useUserGroupPageStore } from '@/services/iam/store/user-group-page-store';
 
@@ -33,6 +32,16 @@ const handleConfirm = () => {
 
 const handleClose = () => {
     console.log('TODO: handleClose');
+    userGroupPageState.modal.type = '';
+};
+
+/* API */
+const handleCreate = () => {
+    console.log('TODO: Create API');
+};
+
+const handleUpdate = () => {
+    console.log('TODO: Update API');
 };
 </script>
 
@@ -42,13 +51,28 @@ const handleClose = () => {
                     :visible="userGroupPageState.modal.type === USER_GROUP_MODAL_TYPE.CREATE || userGroupPageState.modal.type === USER_GROUP_MODAL_TYPE.UPDATE"
                     size="md"
                     @confirm="handleConfirm"
+                    @cancel="handleClose"
                     @close="handleClose"
     >
         <template #body>
             <div class="modal-contents">
                 <user-group-management-add-group-info />
-                <user-group-management-add-notification-channel-info />
+                <user-group-management-schedule-setting />
             </div>
+        </template>
+        <template #confirm-button>
+            <p-button v-if="userGroupPageState.modal.type === USER_GROUP_MODAL_TYPE.CREATE"
+                      theme="primary"
+                      @click="handleCreate"
+            >
+                {{ $t('IAM.USER_GROUP.MODAL.CREATE_USER_GROUP.CONFIRM') }}
+            </p-button>
+            <p-button v-else-if="userGroupPageState.modal.type === USER_GROUP_MODAL_TYPE.UPDATE"
+                      theme="primary"
+                      @click="handleUpdate"
+            >
+                {{ $t('IAM.USER_GROUP.MODAL.CREATE_USER_GROUP.UPDATE') }}
+            </p-button>
         </template>
     </p-button-modal>
 </template>
---
+++ apps/web/src/services/iam/components/UserGroupManagementScheduleSetting.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { PFieldGroup, PRadioGroup, PRadio } from '@cloudforet/mirinae';
+
+import { i18n } from '@/translations';
+
+const SCHEDULE_SETTING_LIST = [
+    i18n.t('IAM.USER_GROUP.MODAL.CREATE_USER_GROUP.WEEKDAYS'),
+    i18n.t('IAM.USER_GROUP.MODAL.CREATE_USER_GROUP.EVERYDAY'),
+    i18n.t('IAM.USER_GROUP.MODAL.CREATE_USER_GROUP.CUSTOM'),
+];
+</script>
+
+<template>
+    <div class="user-group-schedule-setting-wrapper">
+        <p-field-group :label="$t('IAM.USER_GROUP.MODAL.CREATE_USER_GROUP.SCHEDULE_SETTING')"
+                       required
+        >
+            <p-radio-group direction="horizontal">
+                <p-radio v-for="(schedule_setting, i) in SCHEDULE_SETTING_LIST"
+                         :key="i"
+                >
+                    {{ schedule_setting }}
+                </p-radio>
+            </p-radio-group>
+        </p-field-group>
+    </div>
+</template>
+
+<style scoped lang="postcss">
+.user-group-schedule-setting-wrapper {
+    @apply flex flex-col bg-white border border-primary-3 rounded-md;
+    padding: 0.75rem;
+}
+</style>
