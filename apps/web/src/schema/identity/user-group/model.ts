4fa385e6008384f82f09b4375329f5dc3aabd6be
이승연 <sylee1274@mz.co.kr>
2024-12-06 14:49:37 +0900

    fix: fix user group model as api type
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>

--- apps/web/src/schema/identity/user-group/model.ts
+++ apps/web/src/schema/identity/user-group/model.ts
@@ -1,18 +1,15 @@
-import type { AuthType } from '@/schema/identity/user/type';
+import type { Tags } from '@/schema/_common/model';
+import type { UserModel } from '@/schema/identity/user/model';
 
 export interface UserGroupModel {
     user_group_id: string;
-    description: string;
-    notification: number;
-    users: number;
-    created: string;
-}
-
-export interface UserPerUserGroupModel {
-    user_id: string;
     name: string;
-    auth_type: AuthType;
-    last_activity: string;
+    description?: string;
+    users: UserModel[];
+    tags: Tags;
+    workspace_id: string;
+    domain_id: string;
+    created_at: string;
 }
 
 export interface NotificationChannelPerUserGroupModel {
