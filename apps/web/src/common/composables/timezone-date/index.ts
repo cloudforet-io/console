b4b771f84a1f5e34664f02f93f5dcf09d782202b
Wanjin Noh <wanjin@megazone.com>
2024-12-09 21:17:19 +0900

    test: add unit tests for useTimezoneDate composable functionality
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/common/composables/timezone-date/index.ts
+++ apps/web/src/common/composables/timezone-date/index.ts
@@ -8,17 +8,14 @@ import { useUserStore } from '@/store/user/user-store';
 interface UseTimezoneDateOptions {
     date?: Ref<string>;
 }
-export const useTimezoneDate = <T extends UseTimezoneDateOptions = UseTimezoneDateOptions>({
+export const useTimezoneDate = ({
     date,
-}: UseTimezoneDateOptions = {}): {
-    getTimezoneDate: (date: string) => string;
-    timezoneDate: T['date'] extends Ref<string> ? Ref<string> : undefined;
-} => {
+}: UseTimezoneDateOptions = {}) => {
     const userStore = useUserStore();
     const getTimezoneDate = (_date: string): string => dayjs.utc(_date).tz(userStore.state.timezone ?? 'UTC').format('YYYY/MM/DD HH:mm:ss');
     const timezoneDate = date ? computed(() => getTimezoneDate(date.value)) : undefined;
     return {
         getTimezoneDate,
-        timezoneDate: timezoneDate as T['date'] extends Ref<string> ? Ref<string> : undefined,
+        timezoneDate,
     };
 };
