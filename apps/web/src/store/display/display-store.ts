185accd03bdd2a60d4ac666cb3d39ca47e0e1336
yuda <yuda@megazone.com>
2024-12-11 08:19:25 +0900

    chore: delete console.debug in display-store
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/store/display/display-store.ts
+++ apps/web/src/store/display/display-store.ts
@@ -217,11 +217,9 @@ export const useDisplayStore = defineStore('display-store', () => {
     let notificationListApiToken: CancelTokenSource | undefined;
     const checkNotification = async (): Promise<void> => {
         if (notificationListApiToken) {
-            console.debug('[CHECK NOTI]', ' pending...');
             return;
         }
         try {
-            console.debug('[CHECK NOTI]', ' start');
             notificationListApiToken = axios.CancelToken.source();
 
             const currentTime = dayjs.tz(dayjs.utc(), userStore.state.timezone);
@@ -232,7 +230,6 @@ export const useDisplayStore = defineStore('display-store', () => {
                 currentTime,
                 lastNotificationReadTime,
             );
-            console.debug('[NOTI QUERY.FILTER]', param.query.filter);
             const { total_count } = await SpaceConnector.clientV2.notification.notification.list<NotificationListParameters, ListResponse<NotificationModel>>(param, {
                 cancelToken: notificationListApiToken.token,
             });
@@ -246,20 +243,17 @@ export const useDisplayStore = defineStore('display-store', () => {
             }
         } finally {
             notificationListApiToken = undefined;
-            console.debug('[CHECK NOTI]', ' finished');
         }
     };
 
     let checkNotificationInterval: undefined|ReturnType<typeof setTimeout>;
     const stopCheckNotification = (): void => {
         if (notificationListApiToken) {
-            console.debug('[NOTI API]', 'canceled');
             notificationListApiToken.cancel();
             notificationListApiToken = undefined;
         }
 
         if (checkNotificationInterval) {
-            console.debug('[NOTI INTERVAL]', 'stopped');
             clearInterval(checkNotificationInterval);
             checkNotificationInterval = undefined;
         }
@@ -267,15 +261,12 @@ export const useDisplayStore = defineStore('display-store', () => {
 
     const startCheckNotification = (): void => {
         if (notificationListApiToken) {
-            console.debug('[NOTI API]', 'previous canceled');
             notificationListApiToken.cancel();
             notificationListApiToken = undefined;
         }
         if (checkNotificationInterval) {
-            console.debug('[NOTI INTERVAL]', 'previous stopped');
             clearInterval(checkNotificationInterval);
         } else {
-            console.debug('[NOTI INTERVAL]', 'start');
             checkNotification();
         }
 
