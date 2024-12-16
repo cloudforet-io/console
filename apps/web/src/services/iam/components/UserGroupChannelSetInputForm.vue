e2d3690342b31df0d02f6fa60d9463810b29a388
이승연 <sylee1274@mz.co.kr>
2024-12-16 14:13:40 +0900

    feat: change to `ChannelUserSelect` Component
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>

--- apps/web/src/services/iam/components/UserGroupChannelSetInputForm.vue
+++ apps/web/src/services/iam/components/UserGroupChannelSetInputForm.vue
@@ -1,103 +1,20 @@
 <script lang="ts" setup>
 import {
-    computed, reactive, watch,
-} from 'vue';
-import type { TranslateResult } from 'vue-i18n';
-
-import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
-import {
-    PFieldGroup, PTextInput, PRadioGroup, PRadio, PSelectDropdown,
+    PFieldGroup, PTextInput,
 } from '@cloudforet/mirinae';
 import type { MenuItem } from '@cloudforet/mirinae/types/inputs/context-menu/type';
 
-import type { ListResponse } from '@/schema/_common/api-verbs/list';
-import type { WorkspaceUserListParameters } from '@/schema/identity/workspace-user/api-verbs/list';
-import type { WorkspaceUserModel } from '@/schema/identity/workspace-user/model';
-import { i18n } from '@/translations';
+import ChannelUserSelect from '@/common/components/channel-user-select/ChannelUserSelect.vue';
 
-import ErrorHandler from '@/common/composables/error/errorHandler';
-
-import { useUserGroupPageStore } from '@/services/iam/store/user-group-page-store';
-
-const userGroupPageStore = useUserGroupPageStore();
-const userGroupPageState = userGroupPageStore.state;
-
-interface DropdownState {
-  visible: boolean;
-  loading: boolean;
-  selectedAction: MenuItem[];
-  menuItems: MenuItem[]
+interface UserModeInfo {
+  userMode: MenuItem;
+  users: MenuItem[]
 }
 
-const state = reactive({
-    userMode: computed<MenuItem[]>(() => [{
-        label: i18n.t('IAM.USER_GROUP.MODAL.CREATE_CHANNEL.DESC.USER_MODE.ALL_MEMBERS'),
-        name: 'allMembers',
-    }, {
-        label: i18n.t('IAM.USER_GROUP.MODAL.CREATE_CHANNEL.DESC.USER_MODE.USER_GROUP'),
-        name: 'selectedUserGroup',
-    }, {
-        label: i18n.t('IAM.USER_GROUP.MODAL.CREATE_CHANNEL.DESC.USER_MODE.SPECIFIC_USER'),
-        name: 'specificUser',
-    }]),
-    selectedUserModeIdx: 0,
-});
+const emit = defineEmits<{(e: 'update-user', value:UserModeInfo): void}>();
 
-
-const dropdownState = reactive<DropdownState>({
-    visible: false,
-    loading: false,
-    selectedAction: [],
-    menuItems: [],
-});
-
-/* Watcher */
-watch(() => state.selectedUserModeIdx, async (selectedIdx) => {
-    switch (state.userMode[selectedIdx].name) {
-    case 'allMembers':
-        dropdownState.menuItems = [];
-        break;
-    case 'selectedUserGroup':
-        dropdownState.menuItems = userGroupPageState.userGroups.map((userGroup) => ({
-            label: userGroup.name,
-            name: userGroup.user_group_id,
-        }));
-        break;
-    case 'specificUser':
-        dropdownState.menuItems = (await fetchUserList({})).map((user) => ({
-            label: user.user_id,
-            name: user.user_id,
-        }));
-        break;
-    default:
-        break;
-    }
-});
-
-watch(() => state.selectedUserModeIdx, (nv_userMode, ov_userMode) => {
-    if (nv_userMode !== ov_userMode) {
-        dropdownState.selectedAction = [];
-    }
-});
-
-/* Component */
-const handleChange = (idx: number) => {
-    state.selectedUserModeIdx = idx;
-};
-
-const handleSelectDropdown = (value: { label: string | TranslateResult; name: string; }) => {
-    dropdownState.selectedAction.push(value);
-};
-
-/* API */
-const fetchUserList = async (params: WorkspaceUserListParameters) => {
-    try {
-        const response = await SpaceConnector.clientV2.identity.workspaceUser.list<WorkspaceUserListParameters, ListResponse<WorkspaceUserModel>>(params);
-        return response.results ?? [];
-    } catch (e) {
-        ErrorHandler.handleError(e, true);
-        return [];
-    }
+const handleUpdateUser = (value: UserModeInfo) => {
+    emit('update-user', value);
 };
 </script>
 
@@ -123,29 +40,6 @@ const fetchUserList = async (params: WorkspaceUserListParameters) => {
                           block
             />
         </p-field-group>
-        <p-field-group :label="$t('IAM.USER_GROUP.MODAL.CREATE_CHANNEL.DESC.USER_MODE.TITLE')"
-                       required
-        >
-            <p-radio-group>
-                <p-radio v-for="(mode, idx) in state.userMode"
-                         :key="idx"
-                         v-model="state.selectedUserModeIdx"
-                         :value="idx"
-                         @change="handleChange"
-                >
-                    {{ mode.label }}
-                </p-radio>
-            </p-radio-group>
-            <p-select-dropdown class="pt-3"
-                               :menu.sync="dropdownState.menuItems"
-                               placeholder="Select"
-                               multi-selectable
-                               appearance-type="stack"
-                               selection-highlight
-                               is-fixed-width
-                               :selected="dropdownState.selectedAction"
-                               @select="handleSelectDropdown"
-            />
-        </p-field-group>
+        <channel-user-select @update-user="handleUpdateUser" />
     </div>
 </template>
