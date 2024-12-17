e4977378ba33ece5a940d0ca4b80c7eacffcd0f6
nayeongkim <nayeongkim@megazone.com>
2024-12-17 19:23:49 +0900

    feat: apply service filter at alert list
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/services/alert-manager-v2/components/AlertsManagementTable.vue
+++ apps/web/src/services/alert-manager-v2/components/AlertsManagementTable.vue
@@ -1,21 +1,27 @@
 <script setup lang="ts">
-import { computed, reactive } from 'vue';
+import { computed, onMounted, reactive } from 'vue';
 
 import {
     makeDistinctValueHandler,
     makeEnumValueHandler,
 } from '@cloudforet/core-lib/component-util/query-search';
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 import {
-    PToolboxTable, PButton, PLink, PBadge, PI,
+    PToolboxTable, PSelectDropdown, PLink, PBadge, PI,
 } from '@cloudforet/mirinae';
 import type { DataTableFieldType } from '@cloudforet/mirinae/src/data-display/tables/data-table/type';
 import { ACTION_ICON } from '@cloudforet/mirinae/src/navigation/link/type';
+import type { SelectDropdownMenuItem } from '@cloudforet/mirinae/types/controls/dropdown/select-dropdown/type';
 import type { KeyItemSet, ValueHandlerMap } from '@cloudforet/mirinae/types/controls/search/query-search/type';
 
+import type { ListResponse } from '@/schema/_common/api-verbs/list';
 import { ALERT_URGENCY } from '@/schema/alert-manager/alert/constants';
+import type { ServiceListParameters } from '@/schema/alert-manager/service/api-verbs/list';
+import type { ServiceModel } from '@/schema/alert-manager/service/model';
 
 import { referenceRouter } from '@/lib/reference/referenceRouter';
 
+import ErrorHandler from '@/common/composables/error/errorHandler';
 import { useProperRouteLocation } from '@/common/composables/proper-route-location';
 
 import { red } from '@/styles/colors';
@@ -24,6 +30,7 @@ import { useAlertStateI18n } from '@/services/alert-manager-v2/composables/alert
 import { useAlertUrgencyI18n } from '@/services/alert-manager-v2/composables/alert-urgency-i18n';
 import { alertStateBadgeStyleTypeFormatter } from '@/services/alert-manager-v2/helpers/alert-badge-helper';
 import { ALERT_MANAGER_ROUTE_V2 } from '@/services/alert-manager-v2/routes/route-constant';
+
 
 const { getProperRouteLocation } = useProperRouteLocation();
 
@@ -76,13 +83,16 @@ const state = reactive({
         resolved_by: '나다',
         acknowledged: '나다',
     }],
+    serviceDropdownList: [] as SelectDropdownMenuItem[],
+    selectedServiceId: '',
 
     alertStateLabels: useAlertStateI18n(),
     urgencyLabels: useAlertUrgencyI18n(),
 });
 
-const handleClickCreateButton = () => {
-    console.log('TODO: handleClickCreateButton');
+const handleSelectServiceDropdownItem = (id: string) => {
+    state.selectedServiceId = id;
+    fetchServiceList();
 };
 const handleChange = () => {
     console.log('TODO: handleChange');
@@ -96,36 +106,60 @@ const fetchAlertsList = () => {
 const handleExportToExcel = () => {
     console.log('TODO: handleExportToExcel');
 };
+
+const fetchServiceList = async () => {
+    try {
+        let params: ServiceListParameters = {};
+        if (state.selectedServiceId) {
+            params = {
+                service_id: state.selectedServiceId,
+            };
+        }
+        const { results } = await SpaceConnector.clientV2.alertManager.service.list<ServiceListParameters, ListResponse<ServiceModel>>(params);
+        state.serviceDropdownList = (results || []).map((i) => ({
+            name: i.service_id,
+            label: i.name,
+        }));
+    } catch (e) {
+        ErrorHandler.handleError(e, true);
+        state.serviceDropdownList = [];
+    }
+};
+
+onMounted(() => {
+    fetchServiceList();
+});
 </script>
 
 <template>
     <div class="alert-data-table">
-        <p-toolbox-table
-            searchable
-            selectable
-            sortable
-            exportable
-            search-type="query"
-            sort-by="created_at"
-            :sort-desc="true"
-            :loading="state.loading"
-            :fields="tableState.fields"
-            :items="state.items"
-            :key-item-sets="tableState.keyItemSets"
-            :value-handler-map="tableState.valueHandlerMap"
-            settings-visible
-            @change="handleChange"
-            @click-settings="handleClickSettings"
-            @refresh="fetchAlertsList"
-            @export="handleExportToExcel"
+        <p-toolbox-table searchable
+                         selectable
+                         sortable
+                         exportable
+                         search-type="query"
+                         sort-by="created_at"
+                         :sort-desc="true"
+                         :loading="state.loading"
+                         :fields="tableState.fields"
+                         :items="state.items"
+                         :key-item-sets="tableState.keyItemSets"
+                         :value-handler-map="tableState.valueHandlerMap"
+                         settings-visible
+                         @change="handleChange"
+                         @click-settings="handleClickSettings"
+                         @refresh="fetchAlertsList"
+                         @export="handleExportToExcel"
         >
-            <template #toolbox-left>
-                <p-button style-type="primary"
-                          icon-left="ic_plus_bold"
-                          @click="handleClickCreateButton"
-                >
-                    {{ $t('MONITORING.ALERT.ALERT_LIST.CREATE') }}
-                </p-button>
+            <template #toolbox-top>
+                <p-select-dropdown :menu="state.serviceDropdownList"
+                                   :selection-label="$t('ALERT_MANAGER.ALERTS.SERVICE')"
+                                   style-type="rounded"
+                                   use-fixed-menu-style
+                                   :selected="state.selectedServiceId"
+                                   class="service-dropdown pt-6 pl-4"
+                                   @select="handleSelectServiceDropdownItem"
+                />
             </template>
             <template #col-title-format="{ value, item }">
                 <template v-if="value">
@@ -178,3 +212,12 @@ const handleExportToExcel = () => {
         </p-toolbox-table>
     </div>
 </template>
+
+<style scoped lang="postcss">
+.alert-data-table {
+    .service-dropdown {
+        margin-bottom: -0.5rem;
+        width: max-content;
+    }
+}
+</style>
