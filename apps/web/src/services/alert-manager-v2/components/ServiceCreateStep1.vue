e25fbb1fab0df5296e84115071b9ac90fc116afc
NaYeong,Kim <nayeongkim@megazone.com>
2024-12-18 21:16:53 +0900

    feat: apply user select dropdown at service create step1 (#5253)
    * feat: apply user select dropdown at service create step1
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * refactor: redefine container button function
    
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>
    
    * chore: update translation
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    Signed-off-by: NaYeong,Kim <skdud4659@gmail.com>

--- apps/web/src/services/alert-manager-v2/components/ServiceCreateStep1.vue
+++ apps/web/src/services/alert-manager-v2/components/ServiceCreateStep1.vue
@@ -5,22 +5,21 @@ import {
 
 import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 import {
-    PFieldGroup, PTextInput, PTooltip, PI, PSelectDropdown, PAvatar,
+    PFieldGroup, PTextInput, PTooltip, PI,
 } from '@cloudforet/mirinae';
-import type { SelectDropdownMenuItem } from '@cloudforet/mirinae/types/controls/dropdown/select-dropdown/type';
 
 import type { ListResponse } from '@/schema/_common/api-verbs/list';
 import type { ServiceCreateParameters } from '@/schema/alert-manager/service/api-verbs/create';
 import type { ServiceListParameters } from '@/schema/alert-manager/service/api-verbs/list';
 import type { ServiceModel } from '@/schema/alert-manager/service/model';
-import type { WorkspaceUserListParameters } from '@/schema/identity/workspace-user/api-verbs/list';
-import type { WorkspaceUserModel } from '@/schema/identity/workspace-user/model';
 import { i18n } from '@/translations';
+
+import { showSuccessMessage } from '@/lib/helper/notice-alert-helper';
 
 import ErrorHandler from '@/common/composables/error/errorHandler';
 import { useFormValidator } from '@/common/composables/form-validator';
-
-import { indigo } from '@/styles/colors';
+import type { SelectedUserDropdownIdsType } from '@/common/modules/user/typte';
+import UserSelectDropdown from '@/common/modules/user/UserSelectDropdown.vue';
 
 import ServiceCreateStepContainer from '@/services/alert-manager-v2/components/ServiceCreateStepContainer.vue';
 import { useServiceCreateFormStore } from '@/services/alert-manager-v2/stores/service-create-form-store';
@@ -28,29 +27,13 @@ import { useServiceCreateFormStore } from '@/services/alert-manager-v2/stores/se
 const serviceFormStore = useServiceCreateFormStore();
 
 const dropdownState = reactive({
-    loading: false,
-    workspaceUsersData: [] as WorkspaceUserModel[],
-    // TODO: Add user group model type
-    userGroupData: [] as any[],
-    userDropdownData: computed<SelectDropdownMenuItem[]>(() => [
-        { type: 'header', label: i18n.t('ALERT_MANAGER.SERVICE.USER') },
-        ...dropdownState.workspaceUsersData.map((i) => ({
-            label: i.user_id,
-            name: i.user_id,
-        })),
-        { type: 'header', label: i18n.t('ALERT_MANAGER.SERVICE.USER_GROUP') },
-        ...dropdownState.userGroupData.map((i) => ({
-            label: i.name,
-            name: i.user_group_id,
-        })),
-    ]),
-    selectedMemberItems: [] as SelectDropdownMenuItem[],
+    selectedMemberItems: [] as SelectedUserDropdownIdsType[],
 });
 const state = reactive({
     isFocusedKey: false,
     isAllFormValid: computed<boolean>(() => (isAllValid && (name && name.value !== '') && (key && key.value !== ''))),
-    selectedWorkspaceMemberList: computed<string[]>(() => dropdownState.selectedMemberItems.filter((i) => !checkUserGroup(i.name)).map((i) => i.name)),
-    selectedUserGroupList: computed<string[]>(() => dropdownState.selectedMemberItems.filter((i) => checkUserGroup(i.name)).map((i) => i.name)),
+    selectedWorkspaceMemberList: computed<string[]>(() => dropdownState.selectedMemberItems.filter((i) => i.type === 'USER').map((i) => i.value)),
+    selectedUserGroupList: computed<string[]>(() => dropdownState.selectedMemberItems.filter((i) => i.type === 'USER_GROUP').map((i) => i.value)),
     serviceList: [] as ServiceModel[],
 });
 
@@ -93,43 +76,11 @@ const convertToSnakeCase = (str) => {
         .filter((word) => word.trim() !== '')
         .join('_');
 };
-const getUserName = (id: string): string => dropdownState.workspaceUsersData.find((i) => i.user_id === id)?.name;
-const checkUserGroup = (id: string): boolean => dropdownState.userGroupData.some((i) => i.user_group_id === id);
 
 const handleChangeInput = (label: 'name'|'key'|'description', value?: string) => {
     setForm(label, value);
 };
-const getUserDropdownData = async () => {
-    dropdownState.loading = true;
-    try {
-        await fetchWorkspaceUsersList();
-        await fetchUserGroupList();
-    } catch (e) {
-        ErrorHandler.handleError(e);
-    } finally {
-        dropdownState.loading = false;
-    }
-};
 
-const fetchWorkspaceUsersList = async () => {
-    try {
-        const { results } = await SpaceConnector.clientV2.identity.workspaceUser.list<WorkspaceUserListParameters, ListResponse<WorkspaceUserModel>>();
-        dropdownState.workspaceUsersData = results || [];
-    } catch (e) {
-        ErrorHandler.handleError(e);
-        dropdownState.workspaceUsersData = [];
-    }
-};
-const fetchUserGroupList = async () => {
-    try {
-        // TODO: Add user group params type
-        const { results } = await SpaceConnector.clientV2.identity.userGroup.list();
-        dropdownState.userGroupData = results || [];
-    } catch (e) {
-        ErrorHandler.handleError(e);
-        dropdownState.userGroupData = [];
-    }
-};
 const fetchServiceList = async () => {
     try {
         const { results } = await SpaceConnector.clientV2.alertManager.service.list<ServiceListParameters, ListResponse<ServiceModel>>();
@@ -150,6 +101,7 @@ const handleCreateService = async () => {
             },
             description: description.value,
         });
+        showSuccessMessage(i18n.t('ALERT_MANAGER.SERVICE.ALT_S_CREATE_SERVICE'), '');
         serviceFormStore.setCreatedServiceId(createdServiceInfo.service_id);
         serviceFormStore.setCurrentStep(2);
     } catch (e) {
@@ -164,7 +116,6 @@ watch(() => state.isFocusedKey, (isFocusedKey) => {
 });
 
 onMounted(async () => {
-    await getUserDropdownData();
     await fetchServiceList();
 });
 </script>
@@ -213,33 +164,11 @@ onMounted(async () => {
                 </template>
             </p-field-group>
             <p-field-group :label="$t('ALERT_MANAGER.SERVICE.MEMBER')">
-                <p-select-dropdown :loading="dropdownState.loading"
-                                   :menu="dropdownState.userDropdownData"
-                                   :selected.sync="dropdownState.selectedMemberItems"
-                                   multi-selectable
-                                   appearance-type="stack"
-                                   use-fixed-menu-style
-                                   is-filterable
-                                   show-delete-all-button
-                                   show-select-marker
-                >
-                    <template #menu-item--format="{ item }">
-                        <div class="member-menu-item">
-                            <p-avatar v-if="checkUserGroup(item.name)"
-                                      class="menu-icon"
-                                      icon="ic_member"
-                                      :color="indigo[300]"
-                                      size="xs"
-                            />
-                            <p-avatar v-else
-                                      class="menu-icon"
-                                      size="xs"
-                            />
-                            <span>{{ item.label }}</span>
-                            <span v-if="getUserName(item.name)">({{ getUserName(item.name) }})</span>
-                        </div>
-                    </template>
-                </p-select-dropdown>
+                <user-select-dropdown selection-type="multiple"
+                                      appearance-type="stack"
+                                      use-fixed-menu-style
+                                      :selected-ids.sync="dropdownState.selectedMemberItems"
+                />
             </p-field-group>
             <p-field-group :label="$t('ALERT_MANAGER.DESCRIPTION')">
                 <p-text-input :value="description"
@@ -250,12 +179,3 @@ onMounted(async () => {
         </div>
     </service-create-step-container>
 </template>
-
-<style scoped lang="postcss">
-.service-create-step1 {
-    .member-menu-item {
-        @apply flex items-center;
-        gap: 0.25rem;
-    }
-}
-</style>
