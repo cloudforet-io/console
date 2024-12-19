04573103a0a5d8f3611e1b29395aae3c5cc801c6
Piggy Park (박용태) <samuel.park@megazone.com>
2024-12-19 23:11:10 +0900

    fix(widget-field): add allSelected options spec to dataField (#5258)
    * chore(widget-field): add allSelected options spec to dataField
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * chore: add annotation
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    ---------
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/common/modules/widgets/_widget-fields/data-field/type.ts
+++ apps/web/src/common/modules/widgets/_widget-fields/data-field/type.ts
@@ -1,6 +1,7 @@
 
 export interface DataFieldOptions {
     multiSelectable?: boolean;
+    allSelected?: boolean; // if true, all options are selected (use with multiSelectable(true))
 }
 
 export interface DataFieldValue {
