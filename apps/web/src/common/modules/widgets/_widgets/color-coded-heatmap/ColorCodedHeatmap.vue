4f75c6696eda26a1dbe403f29780bfd2c162c493
Yongtae Park <samuel.park@megazone.com>
2024-12-22 20:59:47 +0900

    fix(color-coded-heatmap): refactor color-coded-heatmap widget & apply vue query
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/common/modules/widgets/_widgets/color-coded-heatmap/ColorCodedHeatmap.vue
+++ apps/web/src/common/modules/widgets/_widgets/color-coded-heatmap/ColorCodedHeatmap.vue
@@ -1,34 +1,39 @@
 <script setup lang="ts">
 import {
-    computed, defineExpose, reactive, watch,
+    computed, defineExpose, onMounted, reactive, watch,
 } from 'vue';
 
 
+import { useQuery } from '@tanstack/vue-query';
 import { orderBy } from 'lodash';
 
 import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
-import { getCancellableFetcher } from '@cloudforet/core-lib/space-connector/cancellable-fetcher';
 import {
     PEmpty,
 } from '@cloudforet/mirinae';
 import { numberFormatter } from '@cloudforet/utils';
 
 import type { ListResponse } from '@/schema/_common/api-verbs/list';
+import type { PrivateDataTableModel } from '@/schema/dashboard/private-data-table/model';
 import type { PrivateWidgetLoadParameters } from '@/schema/dashboard/private-widget/api-verbs/load';
+import type { PublicDataTableModel } from '@/schema/dashboard/public-data-table/model';
 import type { PublicWidgetLoadParameters } from '@/schema/dashboard/public-widget/api-verbs/load';
 
-import type { APIErrorToast } from '@/common/composables/error/errorHandler';
-import ErrorHandler from '@/common/composables/error/errorHandler';
 import WidgetCustomLegend from '@/common/modules/widgets/_components/WidgetCustomLegend.vue';
 import WidgetFrame from '@/common/modules/widgets/_components/WidgetFrame.vue';
 import { useWidgetDateRange } from '@/common/modules/widgets/_composables/use-widget-date-range';
 import { useWidgetFrame } from '@/common/modules/widgets/_composables/use-widget-frame';
 import { useWidgetInitAndRefresh } from '@/common/modules/widgets/_composables/use-widget-init-and-refresh';
+import { sortObjectByKeys } from '@/common/modules/widgets/_helpers/widget-data-table-helper';
 import {
     getReferenceLabel,
 } from '@/common/modules/widgets/_helpers/widget-date-helper';
-import type { AdvancedFormatRulesValue } from '@/common/modules/widgets/_widget-fields/advanced-format-rules/type';
+import { getWidgetDataTable } from '@/common/modules/widgets/_helpers/widget-helper';
+import type { DataFieldValue } from '@/common/modules/widgets/_widget-fields/data-field/type';
 import type { DateRangeValue } from '@/common/modules/widgets/_widget-fields/date-range/type';
+import type {
+    FormatRulesValue,
+} from '@/common/modules/widgets/_widget-fields/format-rules/type';
 import type { GranularityValue } from '@/common/modules/widgets/_widget-fields/granularity/type';
 import type { GroupByValue } from '@/common/modules/widgets/_widget-fields/group-by/type';
 import type { WidgetEmit, WidgetExpose, WidgetProps } from '@/common/modules/widgets/types/widget-display-type';
@@ -50,109 +55,120 @@ const { dateRange } = useWidgetDateRange({
     granularity: computed<GranularityValue>(() => props.widgetOptions?.granularity?.value as GranularityValue),
 });
 const state = reactive({
-    loading: false,
-    errorMessage: undefined as string|undefined,
-    data: null as Data | null,
+    runQueries: false,
+    isPrivateWidget: computed<boolean>(() => props.widgetId.startsWith('private')),
+    dataTable: undefined as PublicDataTableModel|PrivateDataTableModel|undefined,
     boxWidth: computed<number>(() => {
         if (!props.width) return BOX_MIN_WIDTH;
         const widgetContentWidth = props.width;
         if (props.width >= 990) return widgetContentWidth / 10;
         return widgetContentWidth / 8 < BOX_MIN_WIDTH ? BOX_MIN_WIDTH : widgetContentWidth / 8;
     }),
-    refinedData: computed(() => {
-        if (!state.data) return [];
-        const _filteredData = state.data.results?.filter((d) => {
-            const _label = getReferenceLabel(props.allReferenceTypeInfo, state.formatRulesField, d[state.formatRulesField]);
-            return state.legendList.find((l) => l.name === _label)?.disabled !== true;
-        }) || [];
-        const _orderedData = orderBy(_filteredData, [state.dataField], ['desc']);
-        return _orderedData.map((d) => ({
-            name: d[state.groupByField],
-            label: getReferenceLabel(props.allReferenceTypeInfo, state.groupByField, d[state.groupByField]),
-            value: numberFormatter(d[state.dataField], { minimumFractionDigits: 2 }),
-            color: getColor(d[state.formatRulesField], state.formatRulesField),
-            legendValue: getReferenceLabel(props.allReferenceTypeInfo, state.formatRulesField, d[state.formatRulesField]),
-        }));
-    }),
     legendList: [] as WidgetLegend[],
-    // required fields
-    granularity: computed<string>(() => props.widgetOptions?.granularity as string),
-    dataField: computed<string|undefined>(() => props.widgetOptions?.dataField as string),
-    groupByField: computed<string|undefined>(() => (props.widgetOptions?.groupBy as GroupByValue)?.value as string),
-    formatRulesValue: computed<AdvancedFormatRulesValue>(() => props.widgetOptions?.advancedFormatRules as AdvancedFormatRulesValue),
-    formatRulesField: computed<string|undefined>(() => state.formatRulesValue?.field),
 });
-const { widgetFrameProps, widgetFrameEventHandlers } = useWidgetFrame(props, emit, {
-    dateRange,
-    errorMessage: computed(() => state.errorMessage),
-    widgetLoading: computed(() => state.loading),
-    noData: computed(() => (state.data ? !state.data.results?.length : false)),
+
+const widgetOptionsState = reactive({
+    granularityInfo: computed<GranularityValue>(() => props.widgetOptions?.granularity?.value as GranularityValue),
+    dataFieldInfo: computed<DataFieldValue>(() => props.widgetOptions?.dataField?.value as DataFieldValue),
+    groupByInfo: computed<GroupByValue>(() => props.widgetOptions?.groupBy?.value as GroupByValue),
+    formatRulesInfo: computed<FormatRulesValue>(() => props.widgetOptions?.formatRules?.value as FormatRulesValue),
 });
 
 /* Api */
-const privateWidgetFetcher = getCancellableFetcher<PrivateWidgetLoadParameters, Data>(SpaceConnector.clientV2.dashboard.privateWidget.load);
-const publicWidgetFetcher = getCancellableFetcher<PublicWidgetLoadParameters, Data>(SpaceConnector.clientV2.dashboard.publicWidget.load);
-const fetchWidget = async (): Promise<Data|APIErrorToast|undefined> => {
-    if (props.widgetState === 'INACTIVE') return undefined;
-    try {
-        state.loading = true;
-        const _isPrivate = props.widgetId.startsWith('private');
-        const _fetcher = _isPrivate ? privateWidgetFetcher : publicWidgetFetcher;
-        const { status, response } = await _fetcher({
-            widget_id: props.widgetId,
-            query: {
-                granularity: state.granularity,
-                start: dateRange.value.start,
-                end: dateRange.value.end,
-                group_by: [state.groupByField, state.formatRulesField],
-                fields: {
-                    [state.dataField]: {
-                        key: state.dataField,
-                        operator: 'sum',
-                    },
-                },
-                page: { start: 1, limit: MAX_COUNT },
-            },
-            vars: props.dashboardVars,
-        });
-        if (status === 'succeed') {
-            state.errorMessage = undefined;
-            state.loading = false;
-            return response;
-        }
-        return undefined;
-    } catch (e: any) {
-        state.loading = false;
-        state.errorMessage = e.message;
-        ErrorHandler.handleError(e);
-        return ErrorHandler.makeAPIErrorToast(e);
-    }
+const fetchWidgetData = async (params: PrivateWidgetLoadParameters|PublicWidgetLoadParameters): Promise<Data> => {
+    const defaultFetcher = state.isPrivateWidget
+        ? SpaceConnector.clientV2.dashboard.privateWidget.load<PrivateWidgetLoadParameters, Data>
+        : SpaceConnector.clientV2.dashboard.publicWidget.load<PublicWidgetLoadParameters, Data>;
+    const res = await defaultFetcher(params);
+    return res;
 };
 
+const queryKey = computed(() => [
+    'widget-load-color-coded-heatmap',
+    props.widgetId,
+    {
+        start: dateRange.value.start,
+        end: dateRange.value.end,
+        granularity: widgetOptionsState.granularityInfo?.granularity,
+        dataTableId: state.dataTable?.data_table_id,
+        dataTableOptions: JSON.stringify(sortObjectByKeys(state.dataTable?.options) ?? {}),
+        groupBy: JSON.stringify([widgetOptionsState.groupByInfo.data as string, widgetOptionsState.formatRulesInfo.field as string]),
+    },
+]);
+
+const queryResult = useQuery({
+    queryKey,
+    queryFn: () => fetchWidgetData({
+        widget_id: props.widgetId,
+        granularity: widgetOptionsState.granularityInfo?.granularity,
+        group_by: [widgetOptionsState.groupByInfo.data as string, widgetOptionsState.formatRulesInfo.field as string],
+        start: dateRange.value.start,
+        end: dateRange.value.end,
+        vars: props.dashboardVars,
+        page: { start: 1, limit: MAX_COUNT },
+    }),
+});
+
+const loading = computed(() => queryResult.isLoading);
+const errorMessage = computed(() => queryResult.error?.value?.message);
+
+const refinedData = computed(() => {
+    const data = queryResult.data?.value;
+    if (!data?.results || !widgetOptionsState.groupByInfo?.data || !widgetOptionsState.formatRulesInfo?.field) return [];
+    const groupByField = widgetOptionsState.groupByInfo.data as string;
+    const formatRulesField = widgetOptionsState.formatRulesInfo.field as string;
+    const dataField = widgetOptionsState.dataFieldInfo.data as string;
+
+    const _filteredData = data.results?.filter((d) => {
+        const _label = getReferenceLabel(props.allReferenceTypeInfo, formatRulesField, d[formatRulesField as string] as string);
+        return state.legendList.find((l) => l.name === _label)?.disabled !== true;
+    }) || [];
+    const _orderedData = orderBy(_filteredData, [dataField], ['desc']);
+    return _orderedData.map((d) => {
+        const groupByValue = d[groupByField] as string;
+        const dataFieldValue = d[dataField] as number;
+        const formatRulesValue = d[formatRulesField] as string;
+        return {
+            name: d[groupByField],
+            label: getReferenceLabel(props.allReferenceTypeInfo, groupByField, groupByValue),
+            value: numberFormatter(dataFieldValue, { minimumFractionDigits: 2 }),
+            color: getColor(formatRulesValue, formatRulesField),
+            legendValue: getReferenceLabel(props.allReferenceTypeInfo, formatRulesField, formatRulesValue),
+        };
+    });
+});
+const { widgetFrameProps, widgetFrameEventHandlers } = useWidgetFrame(props, emit, {
+    dateRange,
+    errorMessage: errorMessage.value,
+    widgetLoading: loading.value,
+    noData: computed(() => (refinedData.value ? !(refinedData.value?.length) : false)),
+});
+
 /* Util */
-const loadWidget = async (): Promise<Data|APIErrorToast> => {
-    const res = await fetchWidget();
-    if (!res) return state.data;
-    if (typeof res === 'function') return res;
-    state.data = res;
-    return state.data;
+const loadWidget = () => {
+    state.runQueries = true;
 };
 const getColor = (val: string, field: string): string => {
     const _label = getReferenceLabel(props.allReferenceTypeInfo, field, val);
-    return state.formatRulesValue.value.find((d) => d.text === _label)?.color ?? state.formatRulesValue.baseColor;
+    return (widgetOptionsState.formatRulesInfo?.rules ?? []).find((d) => d.text === _label || _label.includes(d.text) || val.includes(d.text))?.color
+        ?? widgetOptionsState.formatRulesInfo.baseColor as string;
 };
 
 /* Watcher */
-watch(() => state.formatRulesValue, async () => {
-    state.legendList = state.formatRulesValue.value.map((d) => ({
-        name: d.text,
+watch(() => widgetOptionsState.formatRulesInfo, async () => {
+    state.legendList = (widgetOptionsState.formatRulesInfo?.rules ?? []).map((d) => ({
+        name: d.text as string,
         color: d.color,
         disabled: false,
     }));
 }, { immediate: true });
 
 useWidgetInitAndRefresh({ props, emit, loadWidget });
-defineExpose<WidgetExpose<Data>>({
+onMounted(async () => {
+    if (!props.dataTableId) return;
+    state.dataTable = await getWidgetDataTable(props.dataTableId);
+});
+defineExpose<WidgetExpose>({
     loadWidget,
 });
 </script>
@@ -164,11 +180,11 @@ defineExpose<WidgetExpose<Data>>({
     >
         <!--Do not delete div element below. It's defense code for redraw-->
         <div class="content-wrapper">
-            <div v-if="state.refinedData?.length"
+            <div v-if="refinedData?.length"
                  class="box-wrapper"
                  :style="{'grid-template-columns': `repeat(auto-fill, ${state.boxWidth-4}px)`}"
             >
-                <div v-for="(data, idx) in state.refinedData"
+                <div v-for="(data, idx) in (refinedData ?? [])"
                      :key="`box-${idx}`"
                      v-tooltip.bottom="`${data.label}: ${data.value} (${ data.legendValue })`"
                      class="value-box"
