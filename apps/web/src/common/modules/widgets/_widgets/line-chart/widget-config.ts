845f17b568138b0d6d2109d0d3bed444053b0f4e
yuda <yuda@megazone.com>
2024-12-22 16:44:14 +0900

    refactor(lineChart): use new widget/load api
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/common/modules/widgets/_widgets/line-chart/widget-config.ts
+++ apps/web/src/common/modules/widgets/_widgets/line-chart/widget-config.ts
@@ -17,7 +17,11 @@ const lineChart: WidgetConfig = {
                 max: 31,
             },
         },
-        dataField: {},
+        dataField: {
+            options: {
+                multiSelectable: true,
+            },
+        },
     },
     optionalFieldsSchema: {
         legend: {
