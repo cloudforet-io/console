1703c9c73a74d7c4c0f7449099333063f88d79da
Wanjin Noh <wanjin@megazone.com>
2024-12-26 09:18:46 +0900

    test: remove console log from tokenApi initialization in tests
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- packages/core-lib/__tests__/space-connector/__tests__/index.test.ts
+++ packages/core-lib/__tests__/space-connector/__tests__/index.test.ts
@@ -3,7 +3,7 @@ import TokenAPI from '@/space-connector/token-api';
 
 describe('SpaceConnector', () => {
     const endpoints = ['https://localhost:3000', 'https://localhost:3001'];
-    const tokenApi = new TokenAPI(endpoints[0], () => { console.log('session expired!'); });
+    const tokenApi = new TokenAPI(endpoints[0], () => {});
     test('init SpaceConnector', () => {
         SpaceConnector.init(endpoints, tokenApi);
         expect(typeof SpaceConnector.client).toBe('object');
