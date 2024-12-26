d36dd44476f02b97c4e8ed56c75905914803901c
Wanjin Noh <wanjin@megazone.com>
2024-12-26 12:19:12 +0900

    feat: add public config store using Pinia for state management
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

---
+++ apps/web/src/store/config/public-config-store.ts
@@ -0,0 +1,62 @@
+import type { Ref } from 'vue';
+import { reactive, computed } from 'vue';
+
+import { defineStore } from 'pinia';
+
+import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
+
+
+import type { PublicConfigGetParameters } from '@/schema/config/public-config/api-verbs/get';
+import type { PublicConfigSetParameters } from '@/schema/config/public-config/api-verbs/set';
+import { PUBLIC_CONFIG_NAMES } from '@/schema/config/public-config/constant';
+import type { PublicConfigModel } from '@/schema/config/public-config/model';
+import type { PublicConfigKey } from '@/schema/config/public-config/type';
+
+
+
+interface UsePublicConfigStoreState {
+    publicConfigMap: Partial<Record<PublicConfigKey, PublicConfigModel>>;
+}
+type UsePublicConfigStoreGetters = Record<PublicConfigKey, Ref<PublicConfigModel|undefined>>;
+
+export const usePublicConfigStore = defineStore('public-config', () => {
+    const state = reactive<UsePublicConfigStoreState>({
+        publicConfigMap: {},
+    });
+    const _getterObj = {} as UsePublicConfigStoreGetters;
+    Object.keys(PUBLIC_CONFIG_NAMES).forEach((key) => {
+        _getterObj[key] = computed<PublicConfigModel|undefined>(() => state.publicConfigMap[key]);
+    });
+    const getters = reactive<UsePublicConfigStoreGetters>(_getterObj);
+
+    const actions = {
+        async get<T extends Record<string, any> = Record<string, any>>(key: PublicConfigKey): Promise<PublicConfigModel<T>> {
+            const name = PUBLIC_CONFIG_NAMES[key];
+            if (state.publicConfigMap[key]) return state.publicConfigMap[key] as PublicConfigModel<T>;
+            const publicConfig = await SpaceConnector.client.config.publicConfig.get<PublicConfigGetParameters, PublicConfigModel<T>>({
+                name,
+            });
+            state.publicConfigMap = { ...state.publicConfigMap, [key]: publicConfig };
+            return publicConfig;
+        },
+        async set<T extends Record<string, any> = Record<string, any>>(key: PublicConfigKey, data: T) {
+            const name = PUBLIC_CONFIG_NAMES[key];
+            const publicConfig = await SpaceConnector.client.config.publicConfig.set<PublicConfigSetParameters, PublicConfigModel<T>>({
+                name,
+                data,
+            });
+            state.publicConfigMap = { ...state.publicConfigMap, [key]: publicConfig };
+            return publicConfig;
+        },
+
+        reset() {
+            state.publicConfigMap = {};
+        },
+    };
+
+    return {
+        state,
+        getters,
+        ...actions,
+    };
+});
