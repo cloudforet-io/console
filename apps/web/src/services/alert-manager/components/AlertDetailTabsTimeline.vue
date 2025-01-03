a7c20f665a9af4ac8514d8e6a4bf2f999d1dab31
NaYeong,Kim <nayeongkim@megazone.com>
2025-01-03 11:36:01 +0900

    feat: apply history (#5388)
    * feat: apply history data at timeline
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * feat: apply quick filter at timeline
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * feat: apply alert manager webhook at reference store
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * chore: update translations
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    * feat: apply code modal and search
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>
    
    ---------
    
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/services/alert-manager/components/AlertDetailTabsTimeline.vue
+++ apps/web/src/services/alert-manager/components/AlertDetailTabsTimeline.vue
@@ -6,7 +6,7 @@ import {
 
 import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 import {
-    PButton, PButtonModal, PCodeEditor, PToolbox, PHeading, PHeadingLayout, PDataLoader,
+    PButton, PButtonModal, PCodeEditor, PToolbox, PHeading, PHeadingLayout, PDataLoader, PDivider, PSelectStatus,
 } from '@cloudforet/mirinae';
 
 import type { ListResponse } from '@/schema/_common/api-verbs/list';
@@ -14,6 +14,10 @@ import type { AlertHistoryParameters } from '@/schema/alert-manager/alert/api-ve
 import { ALERT_EVENT_ACTION } from '@/schema/alert-manager/alert/constants';
 import type { AlertModel, AlertEventModel } from '@/schema/alert-manager/alert/model';
 import type { AlertEventActionType } from '@/schema/alert-manager/alert/type';
+import { i18n } from '@/translations';
+
+import { useAllReferenceStore } from '@/store/reference/all-reference-store';
+import type { WebhookReferenceMap } from '@/store/reference/webhook-reference-store';
 
 import { copyAnyData } from '@/lib/helper/copy-helper';
 
@@ -21,48 +25,95 @@ import VerticalTimelineItem from '@/common/components/vertical-timeline/Vertical
 import ErrorHandler from '@/common/composables/error/errorHandler';
 
 import { useAlertDetailPageStore } from '@/services/alert-manager/stores/alert-detail-page-store';
+import type { AlertFilterType } from '@/services/alert-manager/types/alert-manager-type';
+
+type HistoryItemInfo = {
+    title: string;
+    styleType?: string;
+};
 
 const alertDetailPageStore = useAlertDetailPageStore();
 const alertDetailPageState = alertDetailPageStore.state;
 const alertDetailPageGetters = alertDetailPageStore.getters;
+const allReferenceStore = useAllReferenceStore();
+const allReferenceGetters = allReferenceStore.getters;
 
 const storeState = reactive({
     alertInfo: computed<AlertModel>(() => alertDetailPageState.alertInfo),
     timezone: computed<string>(() => alertDetailPageGetters.timezone),
+    webhook: computed<WebhookReferenceMap>(() => allReferenceGetters.webhook),
 });
 const state = reactive({
     loading: true,
     historyList: [] as AlertEventModel[],
-    slicedHistoryList: computed<AlertEventModel[]>(() => state.historyList.filter((i) => i.alert_id === state.searchText)
-        .slice(0, state.pageStart * state.pageLimit)),
-    totalCount: computed<string>(() => state.historyList.length),
+    slicedHistoryList: computed<AlertEventModel[]>(() => {
+        let _list = state.historyList;
+        if (filterState.searchText) {
+            _list = state.historyList.filter((item) => item.description.toLowerCase().includes(filterState.searchText.toLowerCase()));
+        } else {
+            _list = state.historyList;
+        }
+        if (filterState.selectedAction !== 'ALL') {
+            _list = state.historyList.filter((item) => item.action === filterState.selectedAction);
+        }
+        return _list.slice(0, state.pageStart * state.pageLimit);
+    }),
     pageStart: 1,
     pageLimit: 10,
-    searchText: '',
     selectedItem: {} as any,
     modalVisible: false,
     isAlertVisible: false,
 });
+const filterState = reactive({
+    statusFields: computed<AlertFilterType[]>(() => ([
+        { label: i18n.t('ALERT_MANAGER.ALERTS.ALL'), name: 'ALL' },
+        { label: i18n.t('ALERT_MANAGER.ALERTS.TRIGGERED'), name: ALERT_EVENT_ACTION.TRIGGERED },
+        { label: i18n.t('ALERT_MANAGER.ALERTS.ACKNOWLEDGED'), name: ALERT_EVENT_ACTION.ACKNOWLEDGED },
+        { label: i18n.t('ALERT_MANAGER.ALERTS.RESOLVED'), name: ALERT_EVENT_ACTION.RESOLVED },
+        { label: i18n.t('ALERT_MANAGER.ALERTS.NOTIFIED'), name: ALERT_EVENT_ACTION.NOTIFIED },
+        { label: i18n.t('ALERT_MANAGER.ALERTS.EVENT_PUSHED'), name: ALERT_EVENT_ACTION.EVENT_PUSHED },
+    ])),
+    selectedAction: 'ALL',
+    searchText: '',
+});
 
-const getStyleInfo = (item: AlertEventActionType): string => {
-    if (item === ALERT_EVENT_ACTION.TRIGGERED) return 'red';
-    if (item === ALERT_EVENT_ACTION.ACKNOWLEDGED) return 'violet';
-    if (item === ALERT_EVENT_ACTION.RESOLVED) return 'green';
-    if (item === ALERT_EVENT_ACTION.NOTIFIED) return 'yellow';
-    return 'gray';
-};
-const handleChangeToolbox = async (options: any = {}) => {
-    if (options.searchText) {
-        state.searchText = options.searchText;
-        state.pageStart = 1;
+const getCreatedByNames = (createdBy: string): string => {
+    if (createdBy.includes('webhook')) {
+        return storeState.webhook[createdBy].label || createdBy;
     }
+    return createdBy;
+};
+const getItemInfo = (item: AlertEventActionType): HistoryItemInfo => {
+    let styleType: string|undefined;
+    if (item === ALERT_EVENT_ACTION.TRIGGERED) styleType = 'red';
+    if (item === ALERT_EVENT_ACTION.ACKNOWLEDGED) styleType = 'violet';
+    if (item === ALERT_EVENT_ACTION.RESOLVED) styleType = 'green';
+    if (item === ALERT_EVENT_ACTION.NOTIFIED) styleType = 'yellow';
+    if (item === ALERT_EVENT_ACTION.EVENT_PUSHED) styleType = 'gray';
+    return {
+        title: item.toLowerCase().replace(/_/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase()),
+        styleType,
+    };
+};
+
+const handleClickHistoryItem = (item: AlertEventModel) => {
+    state.modalVisible = true;
+    state.selectedItem = item;
+};
+const handleSelectAction = (value: string) => {
+    filterState.selectedAction = value;
+    state.pageStart = 1;
+};
+const handleChangeToolbox = async (value: string) => {
+    filterState.searchText = value;
+    state.pageStart = 1;
 };
 const handleClickShowMore = async () => {
     state.pageStart += 1;
 };
 
 const handleClickCopy = () => {
-    copyAnyData(state.selectedItem.raw_data);
+    copyAnyData(state.selectedItem);
 };
 
 const fetchHistoryList = async () => {
@@ -95,36 +146,61 @@ watch(() => storeState.alertInfo, async (alertInfo) => {
                 />
             </template>
         </p-heading-layout>
+        <p-toolbox search-type="plain"
+                   filters-visible
+                   :total-count="state.historyList.length"
+                   :page-size-changeable="false"
+                   :pagination-visible="false"
+                   :search-text="filterState.searchText"
+                   @update:search-text="handleChangeToolbox"
+                   @refresh="fetchHistoryList()"
+        />
+        <div class="action-filter-wrapper">
+            <span class="font-bold">{{ $t('ALERT_MANAGER.ALERTS.TYPE') }}</span>
+            <p-divider class="divider"
+                       vertical
+            />
+            <p-select-status v-for="(item, idx) in filterState.statusFields"
+                             :key="idx"
+                             :selected="filterState.selectedAction"
+                             :value="item.name"
+                             class="text-gray-600"
+                             @change="handleSelectAction"
+            >
+                {{ item.label }}
+            </p-select-status>
+        </div>
         <p-data-loader :loading="state.loading"
                        :data="state.slicedHistoryList"
         >
-            <p-toolbox search-type="plain"
-                       filters-visible
-                       :total-count="state.totalCount"
-                       :page-size-changeable="false"
-                       :pagination-visible="false"
-                       class="mb-3"
-                       @change="handleChangeToolbox"
-                       @refresh="handleChangeToolbox"
-            />
             <template v-if="state.slicedHistoryList.length > 0">
-                <vertical-timeline-item v-for="(item, idx) in state.slicedHistoryList"
-                                        :key="`history-item-${idx}`"
-                                        :title="item.alert_id"
-                                        :description="item.description"
-                                        :datetime="item.created_at"
-                                        :timezone="storeState.timezone"
-                                        :style-type="getStyleInfo(item.action)"
-                                        :is-last-item="idx === state.slicedHistoryList?.length - 1"
-                                        class="timeline"
-                />
+                <p-divider />
+                <div class="mt-4">
+                    <vertical-timeline-item v-for="(item, idx) in state.slicedHistoryList"
+                                            :key="`history-item-${idx}`"
+                                            :title="getItemInfo(item.action).title"
+                                            :datetime="item.created_at"
+                                            :timezone="storeState.timezone"
+                                            :style-type="getItemInfo(item.action)?.styleType"
+                                            :is-last-item="idx === state.slicedHistoryList?.length - 1"
+                                            @click="handleClickHistoryItem(item)"
+                    >
+                        <template #top-right>
+                            <span class="ml-auto text-label-sm text-gray-600">
+                                {{ $t('ALERT_MANAGER.ALERTS.CREATED_BY', { user: getCreatedByNames(item.created_by) }) }}
+                            </span>
+                        </template>
+                        <span class="description text-label-sm text-gray-600">{{ item.description }}</span>
+                    </vertical-timeline-item>
+                </div>
             </template>
             <template #no-data>
                 <span class="pt-12">{{ $t('ALERT_MANAGER.ALERTS.NO_EVENT') }}</span>
             </template>
         </p-data-loader>
         <div class="flex justify-center mt-6">
-            <p-button v-if="state.historyList.length !== 0
+            <p-button v-if="state.slicedHistoryList.length > 0
+                          && state.historyList.length > 10
                           && state.historyList.length > state.slicedHistoryList.length"
                       icon-right="ic_chevron-down"
                       size="sm"
@@ -170,6 +246,21 @@ watch(() => storeState.alertInfo, async (alertInfo) => {
     }
     .code-block {
         min-height: 100%;
+    }
+    .action-filter-wrapper {
+        @apply flex items-center flex-wrap text-label-sm;
+        gap: 0.75rem;
+        margin-bottom: 1rem;
+        .divider {
+            height: 1rem;
+            padding-top: 0.25rem;
+            padding-bottom: 0.25rem;
+        }
+    }
+    .description {
+        &:hover {
+            @apply text-secondary cursor-pointer;
+        }
     }
 }
 </style>
