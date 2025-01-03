177d076610d8a20e93763d8b0e7733b417d6d43f
nayeongkim <nayeongkim@megazone.com>
2025-01-03 22:17:55 +0900

    feat: add icon
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

---
+++ packages/mirinae/src/foundation/icons/p-icons/ic_check_healthy.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'ic_check_healthy': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: '<path pid="0" d="M12 0a12 12 0 1012 12A12.013 12.013 0 0012 0zm0 22.286A10.286 10.286 0 1122.286 12 10.297 10.297 0 0112 22.286zm5.749-14.32a.857.857 0 010 1.211l-6.857 6.858a.857.857 0 01-1.212 0L6.25 12.606a.857.857 0 011.212-1.212l2.823 2.823 6.25-6.252a.857.857 0 011.213 0z" _fill="#60B731"/>'
+  }
+})
