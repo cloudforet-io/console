7f83cd0d83dd9f413a7a76e79d069aaaa658bdec
sulmo <sulmo@megazone.com>
2025-01-06 14:23:18 +0900

    refactor(storybook): update config exports and tailwind setup
    - Change export syntax in ESLint configuration
    - Adjust Tailwind configuration to include content paths and extend settings
    
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/storybook/tailwind.config.js
+++ apps/storybook/tailwind.config.js
@@ -1,4 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const tailwindConfig = require('../../packages/mirinae/tailwind.config.cjs');
 
-module.exports = tailwindConfig;
+module.exports = {
+    content: [
+        '../../packages/mirinae/src/**/*.{js,ts,jsx,tsx,vue}',
+    ],
+    theme: tailwindConfig.theme,
+    variants: tailwindConfig.variants,
+    plugins: tailwindConfig.plugins,
+};
