a42ffcafdaeb479c548c08beb1b061fe88d815a7
nayeongkim <nayeongkim@megazone.com>
2025-01-10 17:15:32 +0900

    feat: apply notification icon at escalation policy channel dropdown
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/services/alert-manager/components/ServiceDetailTabsSettingsEscalationPolicyFormChannelDropdown.vue
+++ apps/web/src/services/alert-manager/components/ServiceDetailTabsSettingsEscalationPolicyFormChannelDropdown.vue
@@ -7,7 +7,7 @@ import { isEqual } from 'lodash';
 
 import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 import {
-    getTextHighlightRegex, PAvatar, PSelectDropdown, PTag,
+    getTextHighlightRegex, PSelectDropdown, PTag, PI, PLazyImg,
 } from '@cloudforet/mirinae';
 import type {
     AutocompleteHandler,
@@ -16,22 +16,21 @@ import type {
 
 import type { ListResponse } from '@/schema/_common/api-verbs/list';
 import type { ServiceChannelListParameters } from '@/schema/alert-manager/service-channel/api-verbs/list';
+import { SERVICE_CHANNEL_TYPE } from '@/schema/alert-manager/service-channel/constants';
 import type { ServiceChannelModel } from '@/schema/alert-manager/service-channel/model';
-import { i18n } from '@/translations';
+
+import { assetUrlConverter } from '@/lib/helper/asset-helper';
 
 import ErrorHandler from '@/common/composables/error/errorHandler';
 
 import { useServiceDetailPageStore } from '@/services/alert-manager/stores/service-detail-page-store';
+import type { ProtocolCardItemType, ProtocolInfo } from '@/services/alert-manager/types/alert-manager-type';
 
 interface DropdownItem extends SelectDropdownMenuItem {
     name: string;
     label: string;
     members?: number;
 }
-type DropdownCategoriesType = {
-    key: string;
-    title: string;
-};
 
 const props = withDefaults(defineProps<{
      selectedIds?: string[];
@@ -47,54 +46,36 @@ const serviceDetailPageGetters = serviceDetailPageStore.getters;
 
 const storeState = reactive({
     serviceId: computed<string>(() => serviceDetailPageGetters.serviceInfo.service_id),
+    notificationProtocolList: computed<ProtocolCardItemType[]>(() => serviceDetailPageGetters.notificationProtocolList),
 });
 const state = reactive({
     loading: false,
-    serviceChannelList: [] as DropdownItem[],
-    userGroupChannelList: [] as DropdownItem[],
-    dropdownCategories: computed<DropdownCategoriesType[]>(() => {
-        const result: DropdownCategoriesType[] = [];
-        if (state.serviceChannelList.length > 0) {
-            result.push({
-                key: 'service_channel',
-                title: i18n.t('ALERT_MANAGER.ESCALATION_POLICY.CHANNEL') as string,
-            });
-        }
-        return result;
-    }),
+    serviceChannelList: [] as ServiceChannelModel[],
+    serviceChannelDropdownList: computed<SelectDropdownMenuItem[]>(() => state.serviceChannelList.map((item) => ({
+        name: item.channel_id,
+        label: item.name,
+    })).sort((a, b) => a.label.localeCompare(b.label))),
     selectedItems: [] as SelectDropdownMenuItem[],
 });
 
-const menuItemsHandler = (): AutocompleteHandler => async (keyword: string, pageStart = 1, pageLimit = 10, filters, resultIndex) => {
+const getProtocolInfo = (id: string): ProtocolInfo => {
+    const channel = state.serviceChannelList.find((item) => item.channel_id === id);
+    const protocol = storeState.notificationProtocolList.find((item) => item.protocol_id === channel.protocol_id);
+    return {
+        name: channel?.name || '',
+        icon: channel.channel_type === SERVICE_CHANNEL_TYPE.FORWARD ? '' : protocol?.icon || '',
+    };
+};
+
+const menuItemsHandler = (): AutocompleteHandler => async (keyword: string, pageStart = 1, pageLimit = 10) => {
     const _totalCount = pageStart - 1 + pageLimit;
     const filterItems = (items: DropdownItem[]): DropdownItem[] => items.filter((item) => getTextHighlightRegex(keyword).test(item.name)).slice(pageStart - 1, _totalCount);
 
-    if (resultIndex === undefined) {
-        return state.dropdownCategories.map((c) => {
-            const items: DropdownItem[] = [
-                { type: 'header', label: c.title, name: 'header' },
-            ];
-            if (c.key === 'service_channel') items.push(...state.serviceChannelList);
-
-            const _slicedItems = filterItems(items);
-            _slicedItems.unshift();
-            return {
-                results: _slicedItems,
-                more: _totalCount < items.length,
-            };
-        });
-    }
-
-    return state.dropdownCategories.map((c, i) => {
-        const items: DropdownItem[] = [];
-        if (c.key === 'service_channel') items.push(...state.serviceChannelList);
-        const _slicedItems = filterItems(items);
-        if (i !== resultIndex) return { results: [], title: c.title };
-        return {
-            results: _slicedItems,
-            more: _totalCount < items.length,
-        };
-    });
+    const _slicedItems = filterItems(state.serviceChannelDropdownList);
+    return {
+        results: _slicedItems,
+        more: _totalCount < state.serviceChannelDropdownList.length,
+    };
 };
 
 const currentChannelIds = computed<string[]>(() => state.selectedItems.map((item) => item.name));
@@ -125,10 +106,7 @@ const fetchServiceChannelList = async () => {
         const { results } = await SpaceConnector.clientV2.alertManager.serviceChannel.list<ServiceChannelListParameters, ListResponse<ServiceChannelModel>>({
             service_id: storeState.serviceId,
         });
-        state.serviceChannelList = (results || []).map((item) => ({
-            name: item.channel_id,
-            label: item.name,
-        })).sort((a, b) => a.label.localeCompare(b.label));
+        state.serviceChannelList = results || [];
     } catch (e) {
         ErrorHandler.handleError(e);
         state.serviceChannelList = [];
@@ -172,10 +150,17 @@ watch(() => storeState.serviceId, async (serviceId) => {
                            @delete="handleTagDelete(idx)"
                     >
                         <div class="member-menu-item h-4">
-                            <p-avatar icon="ic_my-page_notifications-channel"
-                                      size="xs"
+                            <p-i v-if="!getProtocolInfo(item.name).icon"
+                                 name="ic_notification-protocol_users"
+                                 width="1rem"
+                                 height="1rem"
                             />
-                            <span class="leading-4">{{ item.label }}</span>
+                            <p-lazy-img v-else
+                                        :src="assetUrlConverter(getProtocolInfo(item.name).icon)"
+                                        width="1rem"
+                                        height="1rem"
+                            />
+                            <span class="leading-4">{{ getProtocolInfo(item.name).name }}</span>
                         </div>
                     </p-tag>
                 </div>
@@ -189,12 +174,18 @@ watch(() => storeState.serviceId, async (serviceId) => {
         <template #menu-item--format="{ item }">
             <div class="member-menu-item">
                 <div>
-                    <p-avatar class="menu-icon"
-                              icon="ic_my-page_notifications-channel"
-                              size="xs"
+                    <p-i v-if="!getProtocolInfo(item.name).icon"
+                         name="ic_notification-protocol_users"
+                         width="1rem"
+                         height="1rem"
+                    />
+                    <p-lazy-img v-else
+                                :src="assetUrlConverter(getProtocolInfo(item.name).icon)"
+                                width="1rem"
+                                height="1rem"
                     />
                 </div>
-                <span>{{ item.label }}</span>
+                <span>{{ getProtocolInfo(item.name).name }}</span>
             </div>
         </template>
     </p-select-dropdown>
