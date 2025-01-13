858aa0cc11288a2b29715fbb6c43605812175085
Yongtae Park <samuel.park@megazone.com>
2025-01-14 08:55:53 +0900

    chore: solve biased random number issue
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/services/iam/helpers/generate-helper.ts
+++ apps/web/src/services/iam/helpers/generate-helper.ts
@@ -7,10 +7,21 @@ export const generatePassword = () => {
 
     do {
         randomPassword = Array.from({ length: 12 }, () => {
-            const randomIndex = window.crypto.getRandomValues(new Uint32Array(1))[0] % allCharacters.length;
+            const randomIndex = getSecureRandomIndex(allCharacters.length);
             return allCharacters[randomIndex];
         }).join('');
     } while (!regex.test(randomPassword));
 
     return randomPassword;
+};
+
+const getSecureRandomIndex = (max: number) => {
+    const maxAllowedValue = Math.floor(0xffffffff / max) * max;
+    let randomValue;
+
+    do {
+        randomValue = window.crypto.getRandomValues(new Uint32Array(1))[0];
+    } while (randomValue >= maxAllowedValue);
+
+    return randomValue % max;
 };
