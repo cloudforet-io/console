e1422ce9f0d330d828f9cb06300e9a7708be3f95
Yongtae Park <samuel.park@megazone.com>
2025-01-22 14:33:26 +0900

    chore(dashboard-variable): add project group variable
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/services/dashboards/helpers/dashboard-global-variables-helper.ts
+++ apps/web/src/services/dashboards/helpers/dashboard-global-variables-helper.ts
@@ -6,11 +6,10 @@ import { DASHBOARD_VARS_SCHEMA_PRESET } from '@/services/dashboards/constants/da
 
 export const getOrderedGlobalVariables = (variables: DashboardGlobalVariable[]): DashboardGlobalVariable[] => {
     const _presetKeys: string[] = Object.keys(DASHBOARD_VARS_SCHEMA_PRESET.properties);
-    console.debug('preset keys', _presetKeys, variables);
     const _presetItems = variables.filter((d) => _presetKeys.includes(d.key));
     const _customItems = variables.filter((d) => !_presetKeys.includes(d.key));
     return [
-        ..._presetItems,
+        ...orderBy(_presetItems, 'name', 'asc'),
         ...orderBy(_customItems, 'name', 'asc'),
     ];
 };
