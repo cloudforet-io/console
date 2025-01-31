8506f945153b2e2105221882486e95905c778e4a
sulmo <sulmo@megazone.com>
2025-01-31 14:53:43 +0900

    feat: create unified-storybook app
    feat(unified-storybook): add initial setup for unified storybook
    
    - Added ESLint configuration and ignore files
    - Configured Storybook with custom themes and settings
    - Introduced PostCSS and Tailwind CSS configurations
    - Included icons, manifest, and other public assets
    - Set up TypeScript configuration for the project
    feat(unified-storybook): add initial setup for unified storybook
    
    - Added ESLint configuration and ignore files
    - Configured Storybook with custom themes and settings
    - Introduced PostCSS and Tailwind CSS configurations
    - Included icons, manifest, and other public assets
    
    Signed-off-by: sulmo <sulmo@megazone.com>

---
+++ apps/unified-storybook/.storybook/CloudforetTheme.js
@@ -0,0 +1,30 @@
+export default {
+    base: 'light',
+    colorPrimary: '#6638B6',
+    colorSecondary: 'rgba(102, 56, 182, 0.9)',
+
+    // UI
+    appBg: 'white',
+    appContentBg: 'white',
+    appBorderColor: 'silver',
+    appBorderRadius: 4,
+
+    // Text colors
+    textColor: 'black',
+    textInverseColor: 'rgba(255,255,255,0.9)',
+
+    // Toolbar default and active colors
+    barTextColor: 'silver',
+    barSelectedColor: '#6638B6',
+    barBg: 'white',
+
+    // Form colors
+    inputBg: 'white',
+    inputBorder: 'silver',
+    inputTextColor: 'black',
+    inputBorderRadius: 4,
+
+    brandTitle: 'Cloudforet Design System',
+    brandUrl: 'https://github.com/cloudforet-io/console',
+    brandImage: './images/SpaceONE_logoTypeA.svg',
+};
