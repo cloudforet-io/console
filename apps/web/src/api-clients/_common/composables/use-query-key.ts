650e5de854757f6e155c9965793362543890748d
Yongtae Park <samuel.park@megazone.com>
2025-02-05 01:58:24 +0900

    fix(use-query-key): apply global options
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/api-clients/_common/composables/use-query-key.ts
+++ apps/web/src/api-clients/_common/composables/use-query-key.ts
@@ -1,20 +1,31 @@
 import type { ComputedRef } from 'vue';
 import { computed, reactive } from 'vue';
 
+import { useAppContextStore } from '@/store/app-context/app-context-store';
+import { useUserWorkspaceStore } from '@/store/app-context/workspace/user-workspace-store';
+
 type QueryKey = Array<unknown>;
 
 interface GlobalQueryParams {
-    workspaceId: string;
-    mode: string;
+    workspaceId?: string;
+    isAdminMode?: boolean;
 }
 
 export const useQueryKey = (
     resourceKey: string,
     additionalGlobalParams?: Partial<GlobalQueryParams>,
 ): ComputedRef<QueryKey> => {
+    const appContextStore = useAppContextStore();
+    const userWorkspaceStore = useUserWorkspaceStore();
+
+    const _state = reactive({
+        currentWorkdpaceId: computed(() => userWorkspaceStore.getters.currentWorkspaceId),
+        isAdminMode: computed(() => appContextStore.getters.isAdminMode),
+    });
+
     const globalQueryParams = reactive<GlobalQueryParams>({
-        workspaceId: 'defaultWorkspace', // TODO: develop
-        mode: 'defaultMode', // TODO: develop
+        workspaceId: _state.currentWorkdpaceId,
+        isAdminMode: _state.isAdminMode,
         ...additionalGlobalParams,
     });
 
