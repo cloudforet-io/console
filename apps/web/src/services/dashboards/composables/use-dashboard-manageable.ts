b8a21ab96cfff0e837681f50c47f53e9256c566b
Yongtae Park <samuel.park@megazone.com>
2025-02-05 02:00:03 +0900

    feat(dashboard): create button manageable composable and widget helper
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/services/dashboards/composables/use-dashboard-manageable.ts
@@ -0,0 +1,43 @@
+import type { ComputedRef } from 'vue';
+import { computed, reactive } from 'vue';
+
+import { ROLE_TYPE } from '@/schema/identity/role/constant';
+
+import { useAppContextStore } from '@/store/app-context/app-context-store';
+import { useUserStore } from '@/store/user/user-store';
+
+import { useDashboardDetailQuery } from '@/services/dashboards/composables/use-dashboard-detail-query';
+
+interface UseDashboardManageableOptions {
+    dashboardId: ComputedRef<string|undefined>;
+}
+
+interface UseDashboardManageableReturn {
+    isManageable: ComputedRef<boolean>;
+}
+
+export const useDashboardManageable = ({ dashboardId }: UseDashboardManageableOptions): UseDashboardManageableReturn => {
+    const appContextStore = useAppContextStore();
+    const userStore = useUserStore();
+    const storeState = reactive({
+        isAdminMode: computed(() => appContextStore.getters.isAdminMode),
+        isWorkspaceOwner: computed(() => userStore.state.currentRoleInfo?.roleType === ROLE_TYPE.WORKSPACE_OWNER),
+    });
+
+    const { dashboard } = useDashboardDetailQuery({ dashboardId });
+
+    const isManageable = computed(() => {
+        if (storeState.isAdminMode) return true;
+        if (dashboard.value?.dashboard_id.startsWith('private')) return true;
+        if (storeState.isWorkspaceOwner) {
+            if (dashboard.value?.project_id) return false;
+            return true;
+        }
+        if (dashboard.value?.project_id) return false;
+        return false;
+    });
+
+    return {
+        isManageable,
+    };
+};
