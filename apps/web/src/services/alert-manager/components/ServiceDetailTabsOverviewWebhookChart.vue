5a900a38d3309ecdb765bb5b28aa249d9d2bbb11
이승연 <165753731+seungyeoneeee@users.noreply.github.com>
2025-02-11 16:52:33 +0900

    Feat: mark-up and api connect daily chart of wehbook (#5632)
    * feat: update languages
    
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>
    
    * feat: mark-up and api connect daily chart of wehbook
    
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>
    
    * feat: update minor change of code and file name
    
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>
    
    ---------
    
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>

---
+++ apps/web/src/services/alert-manager/components/ServiceDetailTabsOverviewWebhookChart.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import ServiceDetailTabsOverviewWebhookBarChartDaily
+    from '@/services/alert-manager/components/ServiceDetailTabsOverviewWebhookBarChartDaily.vue';
+import ServiceDetailTabsOverviewWebhookChartMonthly
+    from '@/services/alert-manager/components/ServiceDetailTabsOverviewWebhookChartMonthly.vue';
+
+interface Props {
+  dateOption: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    dateOption: 'monthly',
+});
+</script>
+
+<template>
+    <div>
+        <service-detail-tabs-overview-webhook-chart-monthly v-if="props.dateOption === 'monthly'" />
+        <service-detail-tabs-overview-webhook-bar-chart-daily v-else />
+    </div>
+</template>
