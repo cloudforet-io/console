b5e823352a720c23e4994d2cad767677b826b3af
Piggy Park (박용태) <samuel.park@megazone.com>
2025-02-22 16:49:56 +0900

    fix(dashboard): solve dashboard list bug & refactor dataTable updating ux (#5656)
    * fix(dashboard): solve dashboard list bug
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * fix(widget-clone): solve widget clone bug
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * fix(data-table): refactor updating dataTable function
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * fix(data-table): refactor dataTable updating (apply all)
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * chore: small fix
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * fix(data-table): improve dataTable creating ux
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    ---------
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/common/modules/widgets/_composables/use-data-table-cascade-update.ts
+++ apps/web/src/common/modules/widgets/_composables/use-data-table-cascade-update.ts
@@ -4,9 +4,8 @@ import { computed, reactive } from 'vue';
 import type { ListResponse } from '@/api-clients/_common/schema/api-verbs/list';
 
 import { useWidgetFormQuery } from '@/common/modules/widgets/_composables/use-widget-form-query';
-import { DATA_TABLE_OPERATOR, DATA_TABLE_TYPE } from '@/common/modules/widgets/_constants/data-table-constant';
+import { createDataTableReferenceMap } from '@/common/modules/widgets/_helpers/widget-data-table-helper';
 import type { DataTableModel, DataTableReference } from '@/common/modules/widgets/types/widget-data-table-type';
-import type { ConcatOptions, DataTableOperator, JoinOptions } from '@/common/modules/widgets/types/widget-model';
 
 
 interface UseDataTableCascadeUpdateOptions {
@@ -30,73 +29,9 @@ export const useDataTableCascadeUpdate = ({ widgetId }: UseDataTableCascadeUpdat
 
     const _state = reactive({
         isPrivate: computed(() => !!widgetId.value?.startsWith('private')),
-        dataTableReferenceMap: computed<Record<string, DataTableReference>>(() => {
-            const referenceMap = {} as Record<string, DataTableReference>;
-            const savedDataTables = dataTableList.value.filter((dataTable) => dataTable.data_table_id && !dataTable.data_table_id.startsWith('UNSAVED-')) as DataTableModel[];
-            const MULTIPE_DATA_TABLE_OPERATORS = [DATA_TABLE_OPERATOR.CONCAT, DATA_TABLE_OPERATOR.JOIN] as DataTableOperator[];
-            savedDataTables.forEach((dataTable) => {
-                if (!referenceMap[dataTable.data_table_id]) {
-                    referenceMap[dataTable.data_table_id] = _setIniitialDataTableReferenceProperty(dataTable.data_table_id);
-                }
-
-                if (dataTable.data_type === DATA_TABLE_TYPE.TRANSFORMED) {
-                    if (dataTable?.operator && MULTIPE_DATA_TABLE_OPERATORS.includes(dataTable?.operator)) {
-                        const [firstReferenceDataTableId, secondReferenceDataTableId] = (dataTable.options[dataTable?.operator] as ConcatOptions|JoinOptions)?.data_tables ?? [];
-                        if (!referenceMap[firstReferenceDataTableId]) {
-                            referenceMap[firstReferenceDataTableId] = _setIniitialDataTableReferenceProperty(firstReferenceDataTableId);
-                        }
-                        if (!referenceMap[secondReferenceDataTableId]) {
-                            referenceMap[secondReferenceDataTableId] = _setIniitialDataTableReferenceProperty(secondReferenceDataTableId);
-                        }
-                        referenceMap[firstReferenceDataTableId] = {
-                            ...referenceMap[firstReferenceDataTableId],
-                            children: [
-                                ...referenceMap[firstReferenceDataTableId].children,
-                                dataTable.data_table_id,
-                            ],
-                        };
-                        referenceMap[secondReferenceDataTableId] = {
-                            ...referenceMap[secondReferenceDataTableId],
-                            children: [
-                                ...referenceMap[secondReferenceDataTableId].children,
-                                dataTable.data_table_id,
-                            ],
-                        };
-                        referenceMap[dataTable.data_table_id] = {
-                            ...referenceMap[dataTable.data_table_id],
-                            parents: [
-                                firstReferenceDataTableId,
-                                secondReferenceDataTableId,
-                            ],
-                        };
-                    } else {
-                        const referenceDataTableId = dataTable.options[dataTable?.operator]?.data_table_id;
-                        if (!referenceMap[referenceDataTableId]) {
-                            referenceMap[referenceDataTableId] = _setIniitialDataTableReferenceProperty(referenceDataTableId);
-                        }
-                        referenceMap[referenceDataTableId] = {
-                            ...referenceMap[referenceDataTableId],
-                            children: [
-                                ...referenceMap[referenceDataTableId].children,
-                                dataTable.data_table_id,
-                            ],
-                        };
-                        referenceMap[dataTable.data_table_id] = {
-                            ...referenceMap[dataTable.data_table_id],
-                            parents: [referenceDataTableId],
-                        };
-                    }
-                }
-            });
-            return referenceMap;
-        }),
+        dataTableReferenceMap: computed<Record<string, DataTableReference>>(() => createDataTableReferenceMap(dataTableList.value)),
     });
 
-    const _setIniitialDataTableReferenceProperty = (dataTableId: string): DataTableReference => ({
-        data_table_id: dataTableId,
-        parents: [],
-        children: [],
-    });
     const _invalidateLoadQueries = async (data: DataTableModel) => {
         await queryClient.invalidateQueries({
             queryKey: [
@@ -104,24 +39,6 @@ export const useDataTableCascadeUpdate = ({ widgetId }: UseDataTableCascadeUpdat
                 data.data_table_id,
             ],
         });
-        /*
-        * Reference cascade updating do not affect the widget load query
-        * Because DataTable is Change, not Widget's selected DataTable
-        *  */
-        // await queryClient.invalidateQueries({
-        //     queryKey: [
-        //         ...(_state.isPrivate ? keys.privateWidgetLoadQueryKey.value : keys.publicWidgetLoadQueryKey.value),
-        //         dashboardId.value,
-        //         widgetId.value,
-        //     ],
-        // });
-        // await queryClient.invalidateQueries({
-        //     queryKey: [
-        //         ...(_state.isPrivate ? keys.privateWidgetLoadSumQueryKey.value : keys.publicWidgetLoadSumQueryKey.value),
-        //         dashboardId.value,
-        //         widgetId.value,
-        //     ],
-        // });
     };
 
     const cascadeUpdateDataTable = async (dataTableId: string) => {
