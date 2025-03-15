cc4fde0cf1ea67b6bbdc15c8e450ff76ae5e3f08
Wanjin Noh <wanjin@megazone.com>
2025-03-15 20:15:14 +0900

    chore: replace tsc with npm run ts-check in lint-staged configuration for TypeScript and Vue files
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/.lintstagedrc.cjs
+++ apps/web/.lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
     "**/!(*spec).{ts,tsx,js,jsx,vue}": ["eslint --fix --max-warnings=0"],
-    "**/!(*spec).{ts,tsx,vue}": ["tsc --noEmit"],
+    "**/!(*spec).{ts,tsx,vue}": ["npm run ts-check"],
     "src/**/*.{css,vue,pcss,scss}": ["stylelint --fix"]
 }
