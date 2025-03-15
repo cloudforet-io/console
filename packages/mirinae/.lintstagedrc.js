541850f3cc7bf162c280f3525c13d77ee377a6f8
Wanjin Noh <wanjin@megazone.com>
2025-03-15 20:28:14 +0900

    chore: remove function-based TypeScript type checking from lint-staged configuration
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- packages/mirinae/.lintstagedrc.js
+++ packages/mirinae/.lintstagedrc.js
@@ -1,5 +1,4 @@
 module.exports = {
     '**/*.{ts,tsx,js,jsx,vue}': ['eslint --fix --max-warnings=0'],
-    '**/*.{ts,tsx,vue}': ['vue-tsc -p tsconfig.json --noEmit'],
     'src/**/*.{css,vue,pcss,scss}': ['stylelint --fix'],
 };
