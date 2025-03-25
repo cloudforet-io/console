bcef26e22ae8cf2622f19a892451a6aa8de882db
Yongtae Park <samuel.park@megazone.com>
2025-03-25 16:38:15 +0900

    feat(query-key): create app context query key composable
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/query/composables/use-app-context-query-key.ts
@@ -0,0 +1,44 @@
+import { computed, reactive } from 'vue';
+
+import { useAppContextStore } from '@/store/app-context/app-context-store';
+import { useUserWorkspaceStore } from '@/store/app-context/workspace/user-workspace-store';
+
+
+
+
+interface AdminModeState {
+    isAdminMode: true;
+    workspaceId?: undefined;
+}
+
+interface WorkspaceModeState {
+    isAdminMode: false;
+    workspaceId: string;
+}
+
+type QueryKeyState = AdminModeState | WorkspaceModeState;
+
+
+export const useQueryKeyAppContext = () => {
+    const appContextStore = useAppContextStore();
+    const userWorkspaceStore = useUserWorkspaceStore();
+
+    const _state = reactive({
+        isAdminMode: computed<boolean>(() => appContextStore.getters.isAdminMode),
+        workspaceId: computed<string|undefined>(() => userWorkspaceStore.getters.currentWorkspaceId),
+    });
+
+    return computed(() => {
+        const state: QueryKeyState = _state.isAdminMode
+            ? { isAdminMode: true }
+            : {
+                isAdminMode: false,
+                // workspaceId: _state.workspaceId!
+                workspaceId: _state.workspaceId ?? '',
+            };
+
+        return state.isAdminMode
+            ? ['admin']
+            : ['workspace', state.workspaceId];
+    });
+};
