1bc22ce4c051b2d763c0bbd012f5574caa90133f
박용태 <samuel.park@mz.co.kr>
2025-04-06 18:56:35 +0900

    fix(dashboard): apply new query key composable
    Signed-off-by: piggggggggy <samuel.park@mz.co.kr>

--- apps/web/src/services/dashboards/composables/use-dashboard-detail-query.ts
+++ apps/web/src/services/dashboards/composables/use-dashboard-detail-query.ts
@@ -8,17 +8,22 @@ import { useScopedQuery } from '@/api-clients/_common/composables/use-scoped-que
 import type { DashboardModel, DashboardUpdateParams } from '@/api-clients/dashboard/_types/dashboard-type';
 import type { WidgetModel, WidgetUpdateParams } from '@/api-clients/dashboard/_types/widget-type';
 import { usePrivateDashboardApi } from '@/api-clients/dashboard/private-dashboard/composables/use-private-dashboard-api';
+import type { PrivateDashboardGetParameters } from '@/api-clients/dashboard/private-dashboard/schema/api-verbs/get';
 import type { PrivateDashboardUpdateParameters } from '@/api-clients/dashboard/private-dashboard/schema/api-verbs/update';
 import type { PrivateDashboardModel } from '@/api-clients/dashboard/private-dashboard/schema/model';
 import { usePrivateWidgetApi } from '@/api-clients/dashboard/private-widget/composables/use-private-widget-api';
+import type { PrivateWidgetListParameters } from '@/api-clients/dashboard/private-widget/schema/api-verbs/list';
 import type { PrivateWidgetUpdateParameters } from '@/api-clients/dashboard/private-widget/schema/api-verbs/update';
 import type { PrivateWidgetModel } from '@/api-clients/dashboard/private-widget/schema/model';
 import { usePublicDashboardApi } from '@/api-clients/dashboard/public-dashboard/composables/use-public-dashboard-api';
+import type { PublicDashboardGetParameters } from '@/api-clients/dashboard/public-dashboard/schema/api-verbs/get';
 import type { PublicDashboardUpdateParameters } from '@/api-clients/dashboard/public-dashboard/schema/api-verbs/update';
 import type { PublicDashboardModel } from '@/api-clients/dashboard/public-dashboard/schema/model';
 import { usePublicWidgetApi } from '@/api-clients/dashboard/public-widget/composables/use-public-widget-api';
+import type { PublicWidgetListParameters } from '@/api-clients/dashboard/public-widget/schema/api-verbs/list';
 import type { PublicWidgetUpdateParameters } from '@/api-clients/dashboard/public-widget/schema/api-verbs/update';
 import type { PublicWidgetModel } from '@/api-clients/dashboard/public-widget/schema/model';
+import { useServiceQueryKey } from '@/query/query-key/use-service-query-key';
 
 const DEFAULT_LIST_DATA = { results: [] };
 const STALE_TIME = 1000 * 60 * 5;
@@ -54,54 +59,55 @@ interface UseDashboardDetailQueryReturn {
 export const useDashboardDetailQuery = ({
     dashboardId,
 }: UseDashboardDetailQueryOptions): UseDashboardDetailQueryReturn => {
-    const { publicDashboardAPI, publicDashboardGetQueryKey } = usePublicDashboardApi();
-    const { privateDashboardAPI, privateDashboardGetQueryKey } = usePrivateDashboardApi();
-    const { publicWidgetAPI, publicWidgetListQueryKey } = usePublicWidgetApi();
-    const { privateWidgetAPI, privateWidgetListQueryKey } = usePrivateWidgetApi();
+    const { publicDashboardAPI } = usePublicDashboardApi();
+    const { privateDashboardAPI } = usePrivateDashboardApi();
+    const { publicWidgetAPI } = usePublicWidgetApi();
+    const { privateWidgetAPI } = usePrivateWidgetApi();
     const queryClient = useQueryClient();
 
     const isPrivate = computed(() => !!dashboardId.value?.startsWith('private'));
 
+
     /* Query Keys */
-    const _publicDashboardGetQueryKey = computed(() => [
-        ...publicDashboardGetQueryKey.value,
-        dashboardId.value,
-    ]);
-    const _privateDashboardGetQueryKey = computed(() => [
-        ...privateDashboardGetQueryKey.value,
-        dashboardId.value,
-    ]);
-    const _publicWidgetListQueryKey = computed(() => [
-        ...publicWidgetListQueryKey.value,
-        dashboardId.value,
-    ]);
-    const _privateWidgetListQueryKey = computed(() => [
-        ...privateWidgetListQueryKey.value,
-        dashboardId.value,
-    ]);
+    const { key: publicDashboardGetQueryKey, params: publicDashboardGetParams } = useServiceQueryKey('dashboard', 'public-dashboard', 'get', {
+        contextKey: computed(() => dashboardId.value),
+        params: computed<PublicDashboardGetParameters>(() => ({
+            dashboard_id: dashboardId.value as string,
+        })),
+    });
+    const { key: privateDashboardGetQueryKey, params: privateDashboardGetParams } = useServiceQueryKey('dashboard', 'private-dashboard', 'get', {
+        contextKey: computed(() => dashboardId.value),
+        params: computed<PrivateDashboardGetParameters>(() => ({
+            dashboard_id: dashboardId.value as string,
+        })),
+    });
+    const { key: publicWidgetListQueryKey, params: publicWidgetListParams } = useServiceQueryKey('dashboard', 'public-widget', 'list', {
+        params: computed<PublicWidgetListParameters>(() => ({
+            dashboard_id: dashboardId.value as string,
+        })),
+    });
+    const { key: privateWidgetListQueryKey, params: privateWidgetListParams } = useServiceQueryKey('dashboard', 'private-widget', 'list', {
+        params: computed<PrivateWidgetListParameters>(() => ({
+            dashboard_id: dashboardId.value as string,
+        })),
+    });
 
     /* Querys */
     const publicDashboardQuery = useScopedQuery({
-        queryKey: _publicDashboardGetQueryKey,
-        queryFn: () => publicDashboardAPI.get({
-            dashboard_id: dashboardId.value as string,
-        }),
+        queryKey: publicDashboardGetQueryKey,
+        queryFn: () => publicDashboardAPI.get(publicDashboardGetParams.value),
         enabled: computed(() => !!dashboardId.value && !isPrivate.value),
         staleTime: STALE_TIME,
     }, ['DOMAIN', 'WORKSPACE']);
     const privateDashboardQuery = useScopedQuery({
-        queryKey: _privateDashboardGetQueryKey,
-        queryFn: () => privateDashboardAPI.get({
-            dashboard_id: dashboardId.value as string,
-        }),
+        queryKey: privateDashboardGetQueryKey,
+        queryFn: () => privateDashboardAPI.get(privateDashboardGetParams.value),
         enabled: computed(() => !!dashboardId.value && isPrivate.value),
         staleTime: STALE_TIME,
     }, ['WORKSPACE']);
     const publicWidgetListQuery = useScopedQuery({
-        queryKey: _publicWidgetListQueryKey,
-        queryFn: () => publicWidgetAPI.list({
-            dashboard_id: dashboardId.value as string,
-        }),
+        queryKey: publicWidgetListQueryKey,
+        queryFn: () => publicWidgetAPI.list(publicWidgetListParams.value),
         select: (data) => data?.results || [],
         enabled: computed(() => !!dashboardId.value && publicDashboardQuery.isSuccess && !isPrivate.value),
         initialData: DEFAULT_LIST_DATA,
@@ -109,10 +115,8 @@ export const useDashboardDetailQuery = ({
         staleTime: STALE_TIME,
     }, ['DOMAIN', 'WORKSPACE']);
     const privateWidgetListQuery = useScopedQuery({
-        queryKey: _privateWidgetListQueryKey,
-        queryFn: () => privateWidgetAPI.list({
-            dashboard_id: dashboardId.value as string,
-        }),
+        queryKey: privateWidgetListQueryKey,
+        queryFn: () => privateWidgetAPI.list(privateWidgetListParams.value),
         select: (data) => data?.results || [],
         enabled: computed(() => !!dashboardId.value && privateDashboardQuery.isSuccess && isPrivate.value),
         initialData: DEFAULT_LIST_DATA,
@@ -156,10 +160,10 @@ export const useDashboardDetailQuery = ({
             privateWidgetAPI,
         },
         keys: {
-            publicDashboardGetQueryKey: _publicDashboardGetQueryKey,
-            privateDashboardGetQueryKey: _privateDashboardGetQueryKey,
-            publicWidgetListQueryKey: _publicWidgetListQueryKey,
-            privateWidgetListQueryKey: _privateWidgetListQueryKey,
+            publicDashboardGetQueryKey,
+            privateDashboardGetQueryKey,
+            publicWidgetListQueryKey,
+            privateWidgetListQueryKey,
         },
         fetcher: {
             updateDashboardFn,
