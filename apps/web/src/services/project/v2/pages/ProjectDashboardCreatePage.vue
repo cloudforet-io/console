af41a4a532ea6989762ee59fe544f98c76ff157c
Yongtae Park <samuel.park@megazone.com>
2025-04-09 09:44:05 +0900

    feat(project-dashboard): create project dashboard route & project create page
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/services/project/v2/pages/ProjectDashboardCreatePage.vue
@@ -0,0 +1,122 @@
+<script lang="ts">
+// eslint-disable-next-line import/order,import/no-duplicates
+import { defineComponent, type ComponentPublicInstance } from 'vue';
+
+interface IInstance extends ComponentPublicInstance {
+    setPathFrom(from: any): void
+}
+
+export default defineComponent({
+    beforeRouteEnter(to, from, next) {
+        next((vm) => {
+            const instance = vm as unknown as IInstance;
+            instance.setPathFrom(from);
+        });
+    },
+});
+</script>
+
+<script setup lang="ts">
+/* eslint-disable import/first */
+import {
+    computed, defineExpose, onUnmounted, reactive,
+// eslint-disable-next-line import/no-duplicates
+} from 'vue';
+import type { TranslateResult } from 'vue-i18n';
+import { useRoute } from 'vue-router/composables';
+
+import {
+    PCenteredLayoutHeader,
+} from '@cloudforet/mirinae';
+
+import { i18n } from '@/translations';
+
+import ConfirmBackModal from '@/common/components/modals/ConfirmBackModal.vue';
+import { useGoBack } from '@/common/composables/go-back';
+
+import DashboardCreateStep1 from '@/services/dashboards/components/dashboard-create/DashboardCreateStep1.vue';
+import DashboardCreateStep2 from '@/services/dashboards/components/dashboard-create/DashboardCreateStep2.vue';
+import { useDashboardCreatePageStore } from '@/services/dashboards/stores/dashboard-create-page-store';
+
+import { PROJECT_ROUTE_V2 } from '../routes/route-constant';
+
+
+interface Step {
+    step: number;
+    description: TranslateResult;
+}
+const dashboardCreatePageStore = useDashboardCreatePageStore();
+const dashboardCreatePageState = dashboardCreatePageStore.state;
+const route = useRoute();
+const state = reactive({
+    loading: false,
+    steps: computed<Step[]>(() => [
+        {
+            step: 1,
+            description: i18n.t('PROJECT.DASHBOARDS.CREATE.STEP1_DESC'),
+        },
+        {
+            step: 2,
+            description: i18n.t('PROJECT.DASHBOARDS.CREATE.STEP2_DESC'),
+        },
+    ]),
+    closeConfirmModalVisible: false,
+});
+
+/* Event */
+const handleClickClose = () => {
+    state.closeConfirmModalVisible = true;
+};
+const { setPathFrom, handleClickBackButton } = useGoBack({
+    name: PROJECT_ROUTE_V2._NAME,
+    params: {
+        projectGroupOrProjectId: route.params.projectGroupOrProjectId,
+    },
+});
+const handleClickNext = () => {
+    dashboardCreatePageStore.setCreateType('BUNDLE');
+    dashboardCreatePageStore.setCurrentStep(2);
+};
+
+defineExpose({ setPathFrom });
+
+onUnmounted(() => {
+    dashboardCreatePageStore.reset();
+});
+</script>
+
+<template>
+    <div class="project-dashboard-create-page"
+         :class="[`step-${dashboardCreatePageState.currentStep}`]"
+    >
+        <p-centered-layout-header :title="$t('PROJECT.DASHBOARDS.CREATE.TITLE')"
+                                  :description="state.steps[dashboardCreatePageState.currentStep - 1]?.description"
+                                  :total-steps="state.steps.length"
+                                  :current-step="dashboardCreatePageState.currentStep"
+                                  show-step
+                                  show-close-button
+                                  @close="handleClickClose"
+        />
+        <dashboard-create-step1 v-if="dashboardCreatePageState.currentStep === 1"
+                                @click-next="handleClickNext"
+        />
+        <template v-else-if="dashboardCreatePageState.currentStep === 2">
+            <dashboard-create-step2 />
+        </template>
+        <confirm-back-modal :visible.sync="state.closeConfirmModalVisible"
+                            @confirm="handleClickBackButton"
+        />
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+.project-dashboard-create-page {
+    margin: 0 auto;
+    &.step-1 {
+        width: 60rem;
+    }
+    &.step-2 {
+        width: 45rem;
+    }
+}
+</style>
