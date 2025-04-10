d0277ab5bb09d72f39ffd301c3f6cae1888cb318
Wanjin Noh <wanjin@megazone.com>
2025-04-10 21:03:58 +0900

    refactor(workspace-home): remove daily updates handling and related types
    Signed-off-by: Wanjin Noh <wanjin@megazone.com>

--- apps/web/src/services/workspace-home/types/workspace-home-type.ts
+++ apps/web/src/services/workspace-home/types/workspace-home-type.ts
@@ -4,25 +4,6 @@ import type { MenuItem } from '@cloudforet/mirinae/types/controls/context-menu/t
 
 import type { BOOKMARK_TYPE } from '@/services/workspace-home/constants/workspace-home-constant';
 
-// summary
-
-export const DEFAULT_PADDING = 24;
-export interface CloudServiceData {
-    cloud_service_group: string;
-    cloud_service_type: string;
-    total_count: number;
-    icon?: string;
-    provider: string;
-    created_count: number;
-    deleted_count: number;
-    create_warning?: boolean;
-    delete_warning?: boolean;
-    display_name?: string;
-}
-export type DailyUpdatesListItem = {
-    created: CloudServiceData[],
-    deleted: CloudServiceData[],
-};
 
 // bookmark
 export interface MoreMenuItem extends MenuItem {
