be7c104171fefce5411e0c09f9081299bcee14ca
Yongtae Park <samuel.park@megazone.com>
2025-04-11 17:35:22 +0900

    chore: fix wrong route name
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/services/project/v2/routes/route-constant.ts
+++ apps/web/src/services/project/v2/routes/route-constant.ts
@@ -2,6 +2,6 @@ import { MENU_ID } from '@/lib/menu/config';
 
 export const PROJECT_ROUTE_V2 = Object.freeze({
     _NAME: MENU_ID.PROJECT,
-    DETAIL: { _NAME: `${MENU_ID.PROJECT}-detail` },
-    DASHBOARD_CREATE: { _NAME: `${MENU_ID.PROJECT}-dashboard-create` },
+    DETAIL: { _NAME: `${MENU_ID.PROJECT}.detail` },
+    DASHBOARD_CREATE: { _NAME: `${MENU_ID.PROJECT}.dashboard-create` },
 });
