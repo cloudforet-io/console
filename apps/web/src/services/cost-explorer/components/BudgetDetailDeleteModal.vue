a01bd10e951fe60b0515000d8babcd52d78ffadf
이승연 <sylee1274@mz.co.kr>
2025-04-14 17:35:36 +0900

    feat: update reactivity after CRUD at main page
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>

--- apps/web/src/services/cost-explorer/components/BudgetDetailDeleteModal.vue
+++ apps/web/src/services/cost-explorer/components/BudgetDetailDeleteModal.vue
@@ -1,17 +1,5 @@
-<template>
-    <p-double-check-modal :visible="proxyVisible"
-                          :header-title="$t('BILLING.COST_MANAGEMENT.BUDGET.FORM.DELETE_BUDGET')"
-                          :verification-text="verificationText"
-                          modal-size="sm"
-                          @confirm="handleConfirm"
-                          @update:visible="handleUpdate"
-    />
-</template>
-
-<script lang="ts">
-import {
-    computed, reactive, toRefs, watch,
-} from 'vue';
+<script lang="ts" setup>
+import { computed } from 'vue';
 
 import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 import { PDoubleCheckModal } from '@cloudforet/mirinae';
@@ -23,55 +11,48 @@ import ErrorHandler from '@/common/composables/error/errorHandler';
 
 import { useBudgetDetailPageStore } from '@/services/cost-explorer/stores/budget-detail-page-store';
 
+interface Props {
+    visible: boolean;
+}
+const props = withDefaults(defineProps<Props>(), {
+    visible: false,
+});
 
-export default {
-    name: 'BudgetDetailDeleteModal',
-    components: {
-        PDoubleCheckModal,
-    },
-    props: {
-        visible: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    setup(props, { emit }) {
-        const budgetPageStore = useBudgetDetailPageStore();
-        const budgetPageState = budgetPageStore.$state;
+const emit = defineEmits<{(e: 'update:visible', visible: boolean): void;
+    (e: 'confirm'): void;
+}>();
 
-        const state = reactive({
-            proxyVisible: false,
-            verificationText: computed(() => budgetPageState.budgetData?.name || ''),
+const verificationText = computed(() => budgetPageState.budgetData?.name || '');
+
+const budgetPageStore = useBudgetDetailPageStore();
+const budgetPageState = budgetPageStore.$state;
+
+
+const handleConfirm = async () => {
+    try {
+        await SpaceConnector.clientV2.costAnalysis.budget.delete<BudgetDeleteParameters>({
+            budget_id: budgetPageState.budgetData?.budget_id ?? '',
         });
+        emit('update:visible', false);
+        emit('confirm');
+    } catch (e) {
+        ErrorHandler.handleError(e);
+    }
+};
 
-        const handleConfirm = async () => {
-            try {
-                await SpaceConnector.client.costAnalysis.budget.delete<BudgetDeleteParameters>({
-                    budget_id: budgetPageState.budgetData?.budget_id ?? '',
-                });
-                state.proxyVisible = false;
-                emit('confirm');
-            } catch (e) {
-                ErrorHandler.handleError(e);
-            }
-        };
-
-        const handleUpdate = (visible) => {
-            state.proxyVisible = visible;
-            emit('update', state.proxyVisible);
-        };
-
-        watch(() => props.visible, (visible) => {
-            if (visible !== state.proxyVisible) {
-                state.proxyVisible = visible;
-            }
-        });
-
-        return {
-            ...toRefs(state),
-            handleConfirm,
-            handleUpdate,
-        };
-    },
+const handleUpdate = () => {
+    emit('update:visible', false);
 };
 </script>
+
+<template>
+    <p-double-check-modal :visible="props.visible"
+                          :header-title="$t('BILLING.COST_MANAGEMENT.BUDGET.FORM.DELETE_BUDGET')"
+                          :verification-text="verificationText"
+                          modal-size="sm"
+                          @confirm="handleConfirm"
+                          @cancel="handleUpdate"
+                          @close="handleUpdate"
+    />
+</template>
+
