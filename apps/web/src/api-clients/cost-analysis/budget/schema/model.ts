d7a7a0bf430abe6839baeba2765ee41950c55f29
이승연 <sylee1274@mz.co.kr>
2025-04-14 17:33:02 +0900

    feat: update budget schema
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>

--- apps/web/src/api-clients/cost-analysis/budget/schema/model.ts
+++ apps/web/src/api-clients/cost-analysis/budget/schema/model.ts
@@ -1,30 +1,35 @@
+
 import type { Tags } from '@/api-clients/_common/schema/model';
 import type { ResourceGroupType } from '@/api-clients/_common/schema/type';
 import type {
-    BudgetNotification, BudgetPlannedLimit, BudgetTimeUnit,
+    BudgetNotification,
+    BudgetPlannedLimit,
+    BudgetTimeUnit,
 } from '@/api-clients/cost-analysis/budget/schema/type';
 
 import type { Currency } from '@/store/display/type';
 
-
 export interface BudgetModel {
-    budget_id: string;
-    name: string;
-    limit: number;
-    planned_limits: BudgetPlannedLimit[];
-    currency: Currency;
-    time_unit: BudgetTimeUnit;
-    start: string;
-    end: string;
-    notification: BudgetNotification;
-    tags: Tags;
-    data_source_id: string;
-    resource_group: Extract<ResourceGroupType, 'WORKSPACE' | 'PROJECT'>
-    project_id: string;
-    workspace_id: string;
-    service_account_id: string;
-    budget_manager_id: string;
-    domain_id: string;
-    created_at: string;
-    updated_at: string;
+  budget_id: string;
+  name: string;
+  limit: number;
+  planned_limits: BudgetPlannedLimit[];
+  currency: Currency;
+  time_unit: BudgetTimeUnit;
+  start: string;
+  end: string;
+  notification: BudgetNotification;
+  tags: Tags;
+  data_source_id: string;
+  resource_group: Extract<ResourceGroupType, 'WORKSPACE' | 'PROJECT'>;
+  project_id: string;
+  workspace_id: string;
+  service_account_id: string;
+  budget_manager_id: string;
+  domain_id: string;
+  created_at: string;
+  updated_at: string;
+  budget_year: string;
+  notified_month: string;
+  utilization_rate: number;
 }
