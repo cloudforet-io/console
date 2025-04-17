fefb66a273c7a3bcaadc58a051a88eac57b35e47
Yongtae Park <samuel.park@megazone.com>
2025-04-17 15:21:01 +0900

    feat(project-dashboard): create project dashboard edit overlay & separate dashboard tree
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/services/_shared/dashboard/core/composables/use-dashboard-control-menu-helper.ts
+++ apps/web/src/services/_shared/dashboard/core/composables/use-dashboard-control-menu-helper.ts
@@ -8,25 +8,46 @@ import type { PublicDashboardModel } from '@/api-clients/dashboard/public-dashbo
 import type { PublicFolderModel } from '@/api-clients/dashboard/public-folder/schema/model';
 import { i18n } from '@/translations';
 
+import { DASHBOARD_CONTROL_MENU_ACTION_TYPES } from '@/services/_shared/dashboard/core/constants/dashboard-control-menu-constant';
+
+
 export const useDashboardControlMenuHelper = () => {
     const CLONE_MENU_ITEM = computed<MenuItem>(() => ({
         type: 'item',
-        name: 'clone',
+        name: DASHBOARD_CONTROL_MENU_ACTION_TYPES.CLONE,
         label: i18n.t('DASHBOARDS.DETAIL.CLONE'),
         icon: 'ic_clone',
     }));
 
     const DELETE_MENU_ITEM = computed<MenuItem>(() => ({
         type: 'item',
-        name: 'delete',
+        name: DASHBOARD_CONTROL_MENU_ACTION_TYPES.DELETE,
         label: i18n.t('DASHBOARDS.ALL_DASHBOARDS.DELETE'),
         icon: 'ic_delete',
+    }));
+    const EDIT_MENU_ITEM = computed<MenuItem>(() => ({
+        type: 'item',
+        name: DASHBOARD_CONTROL_MENU_ACTION_TYPES.EDIT,
+        label: i18n.t('DASHBOARDS.ALL_DASHBOARDS.EDIT_NAME'),
+        icon: 'ic_edit-text',
+    }));
+    const MOVE_MENU_ITEM = computed<MenuItem>(() => ({
+        type: 'item',
+        name: DASHBOARD_CONTROL_MENU_ACTION_TYPES.MOVE,
+        label: i18n.t('DASHBOARDS.ALL_DASHBOARDS.MOVE'),
+        icon: 'ic_move',
+    }));
+    const SHARE_WITH_CODE_MENU_ITEM = computed<MenuItem>(() => ({
+        type: 'item',
+        name: DASHBOARD_CONTROL_MENU_ACTION_TYPES.SHARE_WITH_CODE,
+        label: i18n.t('DASHBOARDS.ALL_DASHBOARDS.SHARE_WITH_CODE'),
+        icon: 'ic_share-code',
     }));
 
     const getShareMenuItems = (isShared: boolean): MenuItem[] => [
         {
             type: 'item',
-            name: 'share',
+            name: DASHBOARD_CONTROL_MENU_ACTION_TYPES.SHARE,
             label: isShared ? i18n.t('DASHBOARDS.ALL_DASHBOARDS.UNSHARE') : i18n.t('DASHBOARDS.ALL_DASHBOARDS.SHARE'),
             icon: 'ic_share',
         },
@@ -46,53 +67,42 @@ export const useDashboardControlMenuHelper = () => {
         if (isDeprecated) return [DELETE_MENU_ITEM.value];
 
         const shareContextMenuItems: MenuItem[] = [
-            {
-                type: 'item',
-                name: 'shareWithCode',
-                label: i18n.t('DASHBOARDS.DETAIL.SHARE_WITH_CODE'),
-                icon: 'ic_share-code',
-            },
+            SHARE_WITH_CODE_MENU_ITEM.value,
             ...(!isPrivate ? getShareMenuItems(isShared) : []),
             { type: 'divider', name: 'divider' },
         ];
 
         return [
-            {
-                type: 'item',
-                name: 'edit',
-                label: i18n.t('DASHBOARDS.ALL_DASHBOARDS.EDIT_NAME'),
-                icon: 'ic_edit-text',
-            },
+            EDIT_MENU_ITEM.value,
             CLONE_MENU_ITEM.value,
-            {
-                type: 'item',
-                name: 'move',
-                label: i18n.t('DASHBOARDS.DETAIL.MOVE'),
-                icon: 'ic_move',
-            },
+            MOVE_MENU_ITEM.value,
             { type: 'divider', name: 'divider' },
             ...(!isProject ? shareContextMenuItems : []),
             DELETE_MENU_ITEM.value,
         ];
     };
 
-    const getControlFolderMenuItems = (folderId: string, manageable: boolean, folder: FolderModel): MenuItem[] => {
+    const getControlFolderMenuItems = (
+        folderId: string,
+        manageable: boolean,
+        folder: FolderModel,
+        isProject = false,
+    ): MenuItem[] => {
         const isPrivate = folderId.startsWith('private');
         const isShared = !!(folder as PublicFolderModel)?.shared;
 
         if (!manageable) return [CLONE_MENU_ITEM.value];
 
-        return [
-            {
-                type: 'item',
-                name: 'edit',
-                label: i18n.t('DASHBOARDS.ALL_DASHBOARDS.EDIT_NAME'),
-                icon: 'ic_edit-text',
-            },
-            CLONE_MENU_ITEM.value,
-            { type: 'divider', name: 'divider' },
+        const shareContextMenuItems: MenuItem[] = [
             ...(!isPrivate ? getShareMenuItems(isShared) : []),
             { type: 'divider', name: 'divider' },
+        ];
+
+        return [
+            EDIT_MENU_ITEM.value,
+            CLONE_MENU_ITEM.value,
+            { type: 'divider', name: 'divider' },
+            ...(!isProject ? shareContextMenuItems : []),
             DELETE_MENU_ITEM.value,
         ];
     };
