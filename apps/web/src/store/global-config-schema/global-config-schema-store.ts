b450113edccf2aa7936efe66fc10c2b580180b7e
Yongtae Park <samuel.park@megazone.com>
2025-04-23 16:32:45 +0900

    refactor(global-config-schema-store): remove menu concern from gcs-store
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/store/global-config-schema/global-config-schema-store.ts
+++ apps/web/src/store/global-config-schema/global-config-schema-store.ts
@@ -1,22 +1,12 @@
-import { computed, reactive } from 'vue';
+import { reactive } from 'vue';
 
-import { orderBy } from 'lodash';
 import { defineStore } from 'pinia';
 
-import { useAppContextStore } from '@/store/app-context/app-context-store';
-import type { DisplayMenu } from '@/store/display/type';
 
 import type {
     GeneratedRouteSchema,
     GeneratedMenuSchema, GeneratedRouteMetadataSchema, GeneratedUiAffectSchema,
 } from '@/lib/config/global-config/types/type';
-import type { Menu, MenuId } from '@/lib/menu/config';
-import { MENU_ID } from '@/lib/menu/config';
-import { DEFAULT_MENU_LIST, DEFAULT_ADMIN_MENU_LIST } from '@/lib/menu/menu-architecture';
-
-import { useTaskManagementTemplateStore } from '@/services/ops-flow/task-management-templates/stores/use-task-management-template-store';
-
-export type FlattenedMenuMap = Partial<Record<MenuId, MenuId[]>>;
 
 interface GlobalConfigSchemaStoreState {
     uiAffectsSchema: GeneratedUiAffectSchema;
@@ -25,121 +15,12 @@ interface GlobalConfigSchemaStoreState {
     routeSchema: GeneratedRouteSchema;
 }
 
-interface MenuTransformer {
-    menuId: MenuId;
-    transform: (menu: DisplayMenu, getters: any) => Partial<DisplayMenu>;
-    shouldInclude?: (getters: any) => boolean;
-}
-
-const MENU_TRANSFORMERS_MAP = new Map<MenuId, MenuTransformer>([
-    [MENU_ID.OPS_FLOW_LANDING, {
-        menuId: MENU_ID.OPS_FLOW_LANDING,
-        transform: (menu: DisplayMenu, getters: any) => ({
-            label: getters.templateName,
-        }),
-        shouldInclude: (getters: any) => getters.templateId !== 'default' && getters.enableLanding,
-    }],
-    [MENU_ID.TASK_BOARD, {
-        menuId: MENU_ID.TASK_BOARD,
-        transform: (menu: DisplayMenu, getters: any) => ({
-            label: getters.taskBoardName,
-        }),
-    }],
-]);
-
 export const useGlobalConfigSchemaStore = defineStore('global-config-schema-store', () => {
-    const appContextStore = useAppContextStore();
-    const taskManagementTemplateStore = useTaskManagementTemplateStore();
-
     const state = reactive<GlobalConfigSchemaStoreState>({
         uiAffectsSchema: {} as GeneratedUiAffectSchema,
         menuSchema: {} as GeneratedMenuSchema,
         routeMetadataSchema: {} as GeneratedRouteMetadataSchema,
         routeSchema: {} as GeneratedRouteSchema,
-    });
-
-    const _getters = reactive({
-        isAdminMode: computed<boolean>(() => appContextStore.getters.isAdminMode),
-        templateName: computed<string>(() => taskManagementTemplateStore.templates.TemplateName),
-        taskBoardName: computed<string>(() => taskManagementTemplateStore.templates.TaskBoard),
-        templateId: computed<string>(() => taskManagementTemplateStore.state.templateId),
-        enableLanding: computed<boolean>(() => taskManagementTemplateStore.state.enableLanding),
-    });
-
-    const baseMenuList = computed<Menu[]>(() => {
-        const menuList: Menu[] = _getters.isAdminMode ? [] : [...DEFAULT_MENU_LIST];
-
-        Object.values(state.menuSchema).forEach((featureSetting) => {
-            if (featureSetting) {
-                const menu = _getters.isAdminMode ? featureSetting.adminMenu : featureSetting.menu;
-                if (menu && !menuList.some((existingMenu) => existingMenu.id === menu.id)) {
-                    menuList.push(menu);
-                }
-            }
-        });
-
-        if (_getters.isAdminMode) {
-            menuList.push(...DEFAULT_ADMIN_MENU_LIST);
-        }
-
-        return menuList;
-    });
-
-    const transformSubMenu = (subMenu: DisplayMenu): DisplayMenu | null => {
-        const transformer = MENU_TRANSFORMERS_MAP.get(subMenu.id);
-        if (!transformer) return subMenu;
-        if (transformer.shouldInclude && !transformer.shouldInclude(_getters)) return null;
-
-        return {
-            ...subMenu,
-            ...transformer.transform(subMenu, _getters),
-        };
-    };
-
-    const transformMenu = (menu: DisplayMenu): DisplayMenu => {
-        if (!menu.subMenuList) return menu;
-
-        const transformedSubMenus = menu.subMenuList
-            .map(transformSubMenu)
-            .filter((subMenu): subMenu is DisplayMenu => subMenu !== null);
-
-        return {
-            ...menu,
-            subMenuList: transformedSubMenus,
-        };
-    };
-
-    const getters = reactive({
-        menuList: computed<Menu[]>(() => {
-            const refinedMenuList = baseMenuList.value
-                .map((menu) => transformMenu(menu as DisplayMenu));
-
-            const orderedMenus = refinedMenuList.filter((menu) => menu.order !== undefined);
-            const unorderedMenus = refinedMenuList.filter((menu) => menu.order === undefined);
-
-            return [...orderBy(orderedMenus, ['order'], ['asc']), ...unorderedMenus];
-        }),
-        generateFlattenedMenuMap: computed<FlattenedMenuMap>(() => {
-            const map: FlattenedMenuMap = {};
-
-            const getSubMenuIdsToMap = (menu: Menu, flattenedMenuMap: FlattenedMenuMap) => {
-                let results: MenuId[] = [];
-                const subMenuList = menu.subMenuList;
-                if (subMenuList) {
-                    results = subMenuList.map((d) => d.id);
-                    subMenuList.forEach((subMenu) => {
-                        getSubMenuIdsToMap(subMenu, flattenedMenuMap);
-                    });
-                }
-                flattenedMenuMap[menu.id] = results;
-            };
-
-            getters.menuList.forEach((menu) => {
-                getSubMenuIdsToMap(menu, map);
-            });
-
-            return map;
-        }),
     });
 
     const actions = {
@@ -159,7 +40,6 @@ export const useGlobalConfigSchemaStore = defineStore('global-config-schema-stor
 
     return {
         state,
-        getters,
         ...actions,
     };
 });
