3464766ff976980363c0a68094a9a240c085f704
Dahyun Yu <yuda@megazone.com>
2025-05-09 13:53:02 +0900

    chore: markup "cost report configuration" section (#5838)
    * chore: markup "cost report configuration" section
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * feat: update costReportConfig model
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * feat: add cost report config form handling and submission logic
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * chore: update language
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * chore: edit `CostReportConfigUpdateParameters`
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * fix: add `DOMAIN` scope to `costReportConfigApiHelper`
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * chore: delete console.log()
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/api-clients/cost-analysis/cost-report-config/schema/api-verbs/list.ts
+++ apps/web/src/api-clients/cost-analysis/cost-report-config/schema/api-verbs/list.ts
@@ -1,7 +1,10 @@
 import type { Query } from '@cloudforet/core-lib/space-connector/type';
 
+import type { CostReportConfigStatus } from '@/api-clients/cost-analysis/cost-report-config/schema/type';
 
 export interface CostReportConfigListParameters {
     query?: Query;
     cost_report_config_id?: string;
+    state?: CostReportConfigStatus;
+    scope?: 'WORKSPACE'|'PROJECT';
 }
