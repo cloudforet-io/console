8040ff026a2bd797c2ab3b178aa01ec262da5cb3
Dahyun Yu <yuda@megazone.com>
2025-05-09 10:51:20 +0900

    refactor: use VueQuery rather than SpaceConnector (#5835)
    * feat: add vue query and mutation of `CostReport`
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * refactor: use VueQuery rather than SpaceConnector
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * fix: use `select` in `useCostReportConfigQuery`
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ apps/web/src/services/cost-explorer/composables/queries/use-cost-report-config-query.ts
@@ -0,0 +1,26 @@
+import { ApiQueryHelper } from '@cloudforet/core-lib/space-connector/helper';
+
+import { useCostReportConfigApi } from '@/api-clients/cost-analysis/cost-report-config/composables/use-cost-report-config-api';
+import { useScopedQuery } from '@/query/composables/use-scoped-query';
+import { useServiceQueryKey } from '@/query/query-key/use-service-query-key';
+
+const costReportConfigApiHelper = new ApiQueryHelper().setSort('created_at', true);
+export const useCostReportConfigQuery = () => {
+    const { costReportConfigAPI } = useCostReportConfigApi();
+    const { key, params } = useServiceQueryKey('cost-analysis', 'cost-report-config', 'list', {
+        params: {
+            query: costReportConfigApiHelper.data,
+        },
+    });
+    const { data, isLoading, error } = useScopedQuery({
+        queryKey: key,
+        queryFn: () => costReportConfigAPI.list(params.value),
+        select: (d) => d.results?.[0] ?? null,
+        staleTime: 1000 * 60 * 5, // 5 minutes
+        gcTime: 1000 * 60 * 1, // 1 minutes
+    }, ['WORKSPACE']);
+
+    return {
+        costReportConfig: data, isLoading, error,
+    };
+};
