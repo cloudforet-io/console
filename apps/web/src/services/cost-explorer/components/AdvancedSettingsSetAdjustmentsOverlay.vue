955af236866e6f297467a79870968cb4ba8628b6
Dahyun Yu <yuda@megazone.com>
2025-05-15 15:49:32 +0900

    feat: init adjustment data on overlay open &  add advanced-settings-page-store (#5850)
    * feat: add advanced-settings-page-store
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * refactor: separate component
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * feat: init adjustment data on overlay open
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/cost-explorer/components/AdvancedSettingsSetAdjustmentsOverlay.vue
+++ apps/web/src/services/cost-explorer/components/AdvancedSettingsSetAdjustmentsOverlay.vue
@@ -1,40 +1,112 @@
 <script setup lang="ts">
-import { computed, reactive } from 'vue';
+import { computed, reactive, watch } from 'vue';
 
 import {
     PButton, POverlayLayout,
 } from '@cloudforet/mirinae';
 
-import AdvancedSettingsAdjustmentForm from '@/services/cost-explorer/components/AdvancedSettingsAdjustmentForm.vue';
-import { useCostExplorerSettingsStore } from '@/services/cost-explorer/stores/cost-explorer-settings-store';
+import { useReportAdjustmentPolicyApi } from '@/api-clients/cost-analysis/report-adjustment-policy/composables/use-report-adjustment-policy-api';
+import type { ReportAdjustmentPolicyModel } from '@/api-clients/cost-analysis/report-adjustment-policy/schema/model';
+import { useReportAdjustmentApi } from '@/api-clients/cost-analysis/report-adjustment/composables/use-report-adjustment-api';
+import type { ReportAdjustmentModel } from '@/api-clients/cost-analysis/report-adjustment/schema/model';
 
-const costExplorerSettingsStore = useCostExplorerSettingsStore();
-const costExplorerSettingsState = costExplorerSettingsStore.$state;
+import AdvancedSettingsAdjustmentGroupForm from '@/services/cost-explorer/components/AdvancedSettingsAdjustmentGroupForm.vue';
+import { useAdvancedSettingsPageStore } from '@/services/cost-explorer/stores/advanced-settings-page-store';
+import type { AdjustmentType, AdjustmentData, AdjustmentPolicyData } from '@/services/cost-explorer/types/report-adjustment-type';
+
+const advancedSettingsPageStore = useAdvancedSettingsPageStore();
+const advancedSettingsPageState = advancedSettingsPageStore.$state;
+
+const { reportAdjustmentPolicyAPI } = useReportAdjustmentPolicyApi();
+const { reportAdjustmentAPI } = useReportAdjustmentApi();
 
 const state = reactive({
-    loading: false,
-    disableConfirmButton: computed<boolean>(() => {
-        if (state.loading) return true;
-        return false;
-    }),
+    isAllValid: computed<boolean>(() => advancedSettingsPageStore.isAdjustmentPolicyValid && advancedSettingsPageStore.isAdjustmentValid),
 });
+
+/* Util */
+const convertRawPolicy = (rawPolicy: ReportAdjustmentPolicyModel): AdjustmentPolicyData => ({
+    id: rawPolicy.report_adjustment_policy_id,
+    workspaceIdList: rawPolicy.policy_filter?.workspace_ids || [],
+});
+const convertRawAdjustment = (rawAdjustment: ReportAdjustmentModel): AdjustmentData => {
+    let adjustment: AdjustmentType;
+    if (rawAdjustment.method === 'FIXED') {
+        adjustment = rawAdjustment.value > 0 ? 'FIXED_DEDUCTION' : 'FIXED_ADDITION';
+    } else {
+        adjustment = rawAdjustment.value > 0 ? 'PERCENTAGE_DEDUCTION' : 'PERCENTAGE_ADDITION';
+    }
+    return {
+        id: rawAdjustment.report_adjustment_id,
+        policyId: rawAdjustment.report_adjustment_policy_id,
+        name: rawAdjustment.name,
+        provider: rawAdjustment.provider,
+        adjustment,
+        amount: rawAdjustment.value,
+        description: rawAdjustment.description,
+    };
+};
+
+/* Init */
+const initForm = async () => {
+    try {
+        // 1. List Adjustment Policy and convert
+        const policyListRes = await reportAdjustmentPolicyAPI.list({});
+        const policyList: AdjustmentPolicyData[] = policyListRes.results?.map(convertRawPolicy) || [];
+
+        // 2. List Adjustment and convert
+        const adjustmentListRes = await reportAdjustmentAPI.list({});
+        const adjustmentList: AdjustmentData[] = adjustmentListRes.results?.map(convertRawAdjustment) || [];
+
+        // 3. Group Adjustment by Policy
+        const adjustmentsByPolicy: Record<string, AdjustmentData[]> = {};
+        policyList.forEach((policy) => {
+            adjustmentsByPolicy[policy.id] = adjustmentList.filter(
+                (adj) => adj.policyId === policy.id,
+            );
+        });
+
+        // 4. Store them
+        advancedSettingsPageStore.setAdjustmentPolicyList(policyList);
+        advancedSettingsPageStore.setAdjustmentListMap(adjustmentsByPolicy);
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 /* Event */
 const handleClose = () => {
-    costExplorerSettingsStore.setShowAdjustmentsOverlay(false);
+    advancedSettingsPageStore.setShowAdjustmentsOverlay(false);
 };
+const handleSave = async () => {
+    advancedSettingsPageStore.setLoading(true);
+    try {
+        // const allData = advancedSettingsPageStore.getAdjustmentGroups();
+        // await saveAdjustmentGroups(allData);
+        advancedSettingsPageStore.setShowAdjustmentsOverlay(false);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        advancedSettingsPageStore.setLoading(false);
+    }
+};
+
+/* Watcher */
+watch(() => advancedSettingsPageState.showAdjustmentsOverlay, (visible) => {
+    if (visible) initForm();
+});
 </script>
 
 <template>
     <div class="advanced-settings-set-adjustments-overlay">
-        <p-overlay-layout :visible="costExplorerSettingsState.showAdjustmentsOverlay"
+        <p-overlay-layout :visible="advancedSettingsPageState.showAdjustmentsOverlay"
                           style-type="primary"
                           size="lg"
                           :title="$t('COST_EXPLORER.ADVANCED_SETTINGS.COST_REPORT_ADJUSTMENTS')"
                           @close="handleClose"
         >
             <div class="sidebar-contents">
-                <advanced-settings-adjustment-form />
+                <advanced-settings-adjustment-group-form />
             </div>
             <template #footer>
                 <div class="footer-wrapper">
@@ -44,7 +116,8 @@ const handleClose = () => {
                         {{ $t('COST_EXPLORER.ADVANCED_SETTINGS.CANCEL') }}
                     </p-button>
                     <p-button style-type="primary"
-                              :disabled="state.disableConfirmButton"
+                              :disabled="!state.isAllValid"
+                              @click="handleSave"
                     >
                         {{ $t('COST_EXPLORER.ADVANCED_SETTINGS.SAVE_CHANGES') }}
                     </p-button>
