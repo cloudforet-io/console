38cff7ee357336213affbcde1345eb5e1ff0d826
Dahyun Yu <yuda@megazone.com>
2025-05-15 15:15:01 +0900

    feat: update adjustment model (#5849)
    * feat: update adjustment model
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * feat: update adjustment model
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/api-clients/cost-analysis/report-adjustment-policy/schema/api-verbs/update.ts
+++ apps/web/src/api-clients/cost-analysis/report-adjustment-policy/schema/api-verbs/update.ts
@@ -2,6 +2,5 @@ import type { Tags } from '@/api-clients/_common/schema/model';
 
 export interface ReportAdjustmentPolicyUpdateParameters {
     report_adjustment_policy_id: string;
-    name?: string;
     tags?: Tags;
 }
