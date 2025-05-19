c59f21117ee899952ace8ad8708a65625bf57b6f
yuda <yuda@megazone.com>
2025-05-19 17:07:22 +0900

    fix: set proper return value
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/api-clients/cost-analysis/cost-report-config/composables/use-cost-report-config-api.ts
+++ apps/web/src/api-clients/cost-analysis/cost-report-config/composables/use-cost-report-config-api.ts
@@ -10,7 +10,7 @@ interface UseCostReportConfigApiReturn {
     costReportConfigAPI: {
         update: (params: CostReportConfigUpdateParameters) => Promise<CostReportConfigModel>;
         list: (params: CostReportConfigListParameters) => Promise<ListResponse<CostReportConfigModel>>;
-        run: (params: CostReportConfigRunParameters) => Promise<CostReportConfigModel>;
+        run: (params: CostReportConfigRunParameters) => Promise<void>;
     }
 }
 
@@ -18,7 +18,7 @@ export const useCostReportConfigApi = (): UseCostReportConfigApiReturn => {
     const actions = {
         update: SpaceConnector.clientV2.costAnalysis.costReportConfig.update<CostReportConfigUpdateParameters, CostReportConfigModel>,
         list: SpaceConnector.clientV2.costAnalysis.costReportConfig.list<CostReportConfigListParameters, ListResponse<CostReportConfigModel>>,
-        run: SpaceConnector.clientV2.costAnalysis.costReportConfig.run<CostReportConfigRunParameters, CostReportConfigModel>,
+        run: SpaceConnector.clientV2.costAnalysis.costReportConfig.run<CostReportConfigRunParameters, void>,
     };
 
     return {
