5dde19c9b21b23585d179d6fed3be8b2e848caaf
yuda <yuda@megazone.com>
2025-05-20 11:49:35 +0900

    feat: set provider, adjustment select dropdown
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/cost-explorer/components/AdvancedSettingsAdjustmentsForm.vue
+++ apps/web/src/services/cost-explorer/components/AdvancedSettingsAdjustmentsForm.vue
@@ -1,18 +1,74 @@
 <script setup lang="ts">
+import { computed, reactive } from 'vue';
 import draggable from 'vuedraggable';
 
 import {
-    PButton, PIconButton, PSelectDropdown, PFieldTitle, PTextInput,
+    PButton, PIconButton, PSelectDropdown, PFieldTitle, PTextInput, PLazyImg, PI,
 } from '@cloudforet/mirinae';
+import type { SelectDropdownMenuItem } from '@cloudforet/mirinae/types/controls/dropdown/select-dropdown/type';
 
+import { i18n } from '@/translations';
+
+import { useAllReferenceStore } from '@/store/reference/all-reference-store';
+import type { ProviderReferenceMap } from '@/store/reference/provider-reference-store';
+
+import { useReportAdjustmentQuery } from '@/services/cost-explorer/composables/queries/use-report-adjustment-query';
 import { useAdvancedSettingsPageStore } from '@/services/cost-explorer/stores/advanced-settings-page-store';
+import type { AdjustmentData, AdjustmentType } from '@/services/cost-explorer/types/report-adjustment-type';
 
 const props = defineProps<{
     policyId: string;
 }>();
-
+const allReferenceStore = useAllReferenceStore();
 const advancedSettingsPageStore = useAdvancedSettingsPageStore();
 const advancedSettingsPageState = advancedSettingsPageStore.$state;
+const storeState = reactive({
+    providers: computed<ProviderReferenceMap>(() => allReferenceStore.getters.provider),
+});
+
+const {
+    reportAdjustmentList,
+} = useReportAdjustmentQuery();
+
+const providerMenuItems = computed<SelectDropdownMenuItem[]>(() => Object.values(storeState.providers).map((item) => ({
+    name: item.key,
+    label: item.name,
+    imageUrl: item.icon,
+})));
+const adjustmentMenuItems = computed<SelectDropdownMenuItem[]>(() => ([
+    {
+        name: 'FIXED_ADDITION',
+        label: i18n.t('COST_EXPLORER.ADVANCED_SETTINGS.FIXED_ADDITION'),
+    },
+    {
+        name: 'FIXED_DEDUCTION',
+        label: i18n.t('COST_EXPLORER.ADVANCED_SETTINGS.FIXED_DEDUCTION'),
+    },
+    {
+        name: 'PERCENTAGE_ADDITION',
+        label: i18n.t('COST_EXPLORER.ADVANCED_SETTINGS.PERCENTAGE_ADDITION'),
+    },
+    {
+        name: 'PERCENTAGE_DEDUCTION',
+        label: i18n.t('COST_EXPLORER.ADVANCED_SETTINGS.PERCENTAGE_DEDUCTION'),
+    },
+]));
+const adjustmentList = computed<AdjustmentData[]>(() => advancedSettingsPageState.adjustmentListMap[props.policyId] || []);
+
+/* Util */
+const getAmountRightExtra = (adjustment: AdjustmentType): string => {
+    if (adjustment === 'PERCENTAGE_DEDUCTION' || adjustment === 'PERCENTAGE_ADDITION') {
+        return '%';
+    }
+    const currency = reportAdjustmentList.value?.find((item) => item.report_adjustment_id === props.policyId)?.currency || 'USD';
+    return currency;
+};
+const getAmountSymbol = (adjustment: AdjustmentType): string => {
+    if (adjustment === 'FIXED_DEDUCTION' || adjustment === 'PERCENTAGE_DEDUCTION') {
+        return 'ic_minus';
+    }
+    return 'ic_plus';
+};
 
 /* Event */
 const handleAddAdjustmentRow = () => {
@@ -21,6 +77,22 @@ const handleAddAdjustmentRow = () => {
 const handleDeleteAdjustmentRow = (adjustmentId: string) => {
     if (!adjustmentId) return;
     advancedSettingsPageStore.deleteAdjustment(props.policyId, adjustmentId);
+};
+const handleUpdateProvider = (adjustmentId: string, selected: string) => {
+    const targetAdjustment = advancedSettingsPageState.adjustmentListMap[props.policyId].find((item) => item.id === adjustmentId);
+    if (!targetAdjustment) return;
+    advancedSettingsPageStore.updateAdjustment(props.policyId, adjustmentId, {
+        ...targetAdjustment,
+        provider: selected,
+    });
+};
+const handleUpdateAdjustment = (adjustmentId: string, selected: AdjustmentType) => {
+    const targetAdjustment = advancedSettingsPageState.adjustmentListMap[props.policyId].find((item) => item.id === adjustmentId);
+    if (!targetAdjustment) return;
+    advancedSettingsPageStore.updateAdjustment(props.policyId, adjustmentId, {
+        ...targetAdjustment,
+        adjustment: selected,
+    });
 };
 </script>
 
@@ -54,11 +126,11 @@ const handleDeleteAdjustmentRow = (adjustmentId: string) => {
                 <span class="text-gray-400 text-xs font-normal"> (optional)</span>
             </p-field-title>
         </div>
-        <draggable :list="advancedSettingsPageState.adjustmentListMap[props.policyId]"
+        <draggable :list="adjustmentList"
                    draggable=".draggable-item"
                    ghost-class="ghost"
         >
-            <div v-for="(item, idx) in advancedSettingsPageState.adjustmentListMap[props.policyId]"
+            <div v-for="(item, idx) in adjustmentList"
                  :key="`adjustment-row-${idx}`"
                  class="adjustment-row draggable-item"
             >
@@ -69,22 +141,54 @@ const handleDeleteAdjustmentRow = (adjustmentId: string) => {
                 <p-text-input v-model="item.name"
                               block
                 />
-                <p-select-dropdown v-model="item.provider"
-                                   :menu="[]"
-                                   block
-                />
-                <p-select-dropdown v-model="item.adjustment"
-                                   :menu="[]"
-                                   block
+                <p-select-dropdown :menu="providerMenuItems"
+                                   :selected="item.provider"
+                                   use-fixed-menu-style
+                                   @update:selected="handleUpdateProvider(item.id, $event)"
+                >
+                    <template #dropdown-button="dropdownItem">
+                        <div class="flex items-center gap-2">
+                            <p-lazy-img v-if="dropdownItem && dropdownItem.imageUrl"
+                                        class="selected-icon"
+                                        :src="dropdownItem.imageUrl"
+                                        width="1rem"
+                                        height="1rem"
+                            />
+                            <p-i v-if="dropdownItem && dropdownItem.icon"
+                                 width="1rem"
+                                 height="1rem"
+                                 class="selected-icon"
+                                 :name="dropdownItem.icon"
+                            />
+                            <span class="selected-text">
+                                {{ dropdownItem?.label }}
+                            </span>
+                        </div>
+                    </template>
+                </p-select-dropdown>
+                <p-select-dropdown :menu="adjustmentMenuItems"
+                                   :selected="item.adjustment"
+                                   use-fixed-menu-style
+                                   @update:selected="handleUpdateAdjustment(item.id, $event)"
                 />
                 <p-text-input v-model="item.amount"
-                              block
                               type="number"
-                />
-                <p-text-input v-model="item.description"
-                              block
-                />
-                <p-icon-button name="ic_close"
+                >
+                    <template #input-left>
+                        <div :class="['symbol-icon', getAmountSymbol(item.adjustment) === 'ic_minus' ? 'text-red-500' : 'text-green-500']">
+                            <p-i :name="getAmountSymbol(item.adjustment)"
+                                 width="1rem"
+                                 height="1rem"
+                                 color="inherit"
+                            />
+                        </div>
+                    </template>
+                    <template #input-right>
+                        {{ getAmountRightExtra(item.adjustment) }}
+                    </template>
+                </p-text-input>
+                <p-text-input v-model="item.description" />
+                <p-icon-button name="ic_delete"
                                size="sm"
                                class="adjustment-row-delete-button"
                                @click="handleDeleteAdjustmentRow(item.id)"
@@ -124,6 +228,9 @@ const handleDeleteAdjustmentRow = (adjustmentId: string) => {
             right: 0;
             top: 0.5rem;
             z-index: 1;
+        }
+        .symbol-icon {
+            padding-right: 0.25rem;
         }
     }
     .adjustment-footer {
