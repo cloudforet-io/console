ab5bcb787527934161b023bb39512828983e00f2
Dahyun Yu <yuda@megazone.com>
2025-05-22 10:42:10 +0900

    feat: apply CRUD for Adjustment Policy  (#5872)
    * chore: edit model schema
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * feat: apply CRUD for Adjustment Policy
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * fix: fix query key
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    * chore: add translate
    
    Signed-off-by: yuda <yuda@megazone.com>
    
    ---------
    
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/api-clients/cost-analysis/report-adjustment/schema/api-verbs/create.ts
+++ apps/web/src/api-clients/cost-analysis/report-adjustment/schema/api-verbs/create.ts
@@ -1,9 +1,9 @@
-import type { ReportAdjustmentMethod } from '@/api-clients/cost-analysis/report-adjustment/schema/type';
+import type { ReportAdjustmentUnit } from '@/api-clients/cost-analysis/report-adjustment/schema/type';
 
 
 export interface ReportAdjustmentCreateParameters {
     name: string;
-    method: ReportAdjustmentMethod;
+    unit: ReportAdjustmentUnit;
     value: number;
     report_adjustment_policy_id: string;
     provider: string;
