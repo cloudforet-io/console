dfd1169949d9cbd01011679924edf30439e71587
yuda <yuda@megazone.com>
2025-06-25 16:35:48 +0900

    refactor: update cost analyze vue query
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/cost-explorer/composables/queries/use-cost-analyze-query.ts
+++ apps/web/src/services/cost-explorer/composables/queries/use-cost-analyze-query.ts
@@ -1,30 +1,38 @@
+import type { ComputedRef } from 'vue';
 import { computed } from 'vue';
 
-import type { ApiQueryHelper } from '@cloudforet/core-lib/space-connector/helper';
+import { isEmpty } from 'lodash';
+
+import type { AnalyzeQuery } from '@cloudforet/core-lib/space-connector/type';
 
 import { useCostApi } from '@/api-clients/cost-analysis/cost/composables/use-cost-api';
 import { useServiceQueryKey } from '@/query/core/query-key/use-service-query-key';
 import { useScopedQuery } from '@/query/service-query/use-scoped-query';
 
-
-export const useCostAnalysisQuery = (data_source_id: string, query: ApiQueryHelper) => {
+export const useCostAnalyzeQuery = ({
+    data_source_id,
+    query,
+}: {
+    data_source_id: ComputedRef<string>;
+    query: ComputedRef<AnalyzeQuery>;
+}) => {
     const { costAPI } = useCostApi();
     const { key, params } = useServiceQueryKey('cost-analysis', 'cost', 'analyze', {
         params: computed(() => ({
-            data_source_id,
-            query: query.data,
+            data_source_id: data_source_id.value,
+            query: query.value,
         })),
     });
 
     const { data, isLoading, error } = useScopedQuery({
         queryKey: key,
         queryFn: () => costAPI.analyze(params.value),
-        select: (d) => d.results ?? [],
         staleTime: 1000 * 60 * 5, // 5 minutes
         gcTime: 1000 * 60 * 1, // 1 minutes
+        enabled: computed(() => !!data_source_id.value && !isEmpty(query.value)), // 조건부 활성화
     }, ['DOMAIN', 'WORKSPACE']);
 
     return {
-        costAnalysisData: data, isLoading, error,
+        costAnalyzeData: data, isLoading, error,
     };
 };
