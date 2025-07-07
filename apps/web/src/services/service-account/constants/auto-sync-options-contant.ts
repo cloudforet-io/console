f793cf9fe5542795645f7eec1ef643f5e00ca825
Piggy Park (박용태) <samuel.park@megazone.com>
2025-07-07 14:38:04 +0900

    feat(trusted-account-auto-sync): apply new auto-sync mapping method for azure account (azure management group)
    feat(trusted-account-auto-sync): apply new auto-sync mapping method for azure account (azure management group)

---
+++ apps/web/src/services/service-account/constants/auto-sync-options-contant.ts
@@ -0,0 +1,77 @@
+
+export const WORKSPACE_MAPPING_OPTIONS_MAP = {
+    MULTI: 'multi',
+    SINGLE: 'single',
+    MULTI_MANAGEMENT_GROUP_FOR_AZURE: 'multi_management_group_for_azure', // only for Azure
+} as const;
+
+export const CSP_AUTO_SYNC_OPTIONS_MAP = {
+    aws: {
+        name: 'AWS Organization',
+        workspaceMappingOptions: [
+            {
+                label: 'Top-level Organization Units',
+                value: WORKSPACE_MAPPING_OPTIONS_MAP.MULTI,
+            },
+            {
+                label: 'AWS Organization',
+                value: WORKSPACE_MAPPING_OPTIONS_MAP.SINGLE,
+            },
+        ],
+        projectGroupMappingOptions: [
+            {
+                label: 'Nested Organization Units',
+                value: true,
+            },
+        ],
+    },
+    azure: {
+        name: 'Azure Tenant',
+        workspaceMappingOptions: [
+            {
+                label: 'Multitenant Organization',
+                value: WORKSPACE_MAPPING_OPTIONS_MAP.MULTI,
+            },
+            {
+                label: 'Azure Management Group',
+                value: WORKSPACE_MAPPING_OPTIONS_MAP.MULTI_MANAGEMENT_GROUP_FOR_AZURE,
+                info: 'This option is only available for Azure Management Group.',
+            },
+            {
+                label: 'Azure Tenant',
+                value: WORKSPACE_MAPPING_OPTIONS_MAP.SINGLE,
+            },
+        ],
+        projectGroupMappingOptions: [
+            {
+                label: 'Nested Management Groups',
+                value: true,
+            },
+        ],
+    },
+    google_cloud: {
+        name: 'Google Cloud Organization',
+        workspaceMappingOptions: [
+            {
+                label: 'Top-level Folders in Google Cloud Organization',
+                value: WORKSPACE_MAPPING_OPTIONS_MAP.MULTI,
+            },
+            {
+                label: 'Google Cloud Organization',
+                value: WORKSPACE_MAPPING_OPTIONS_MAP.SINGLE,
+            },
+        ],
+        projectGroupMappingOptions: [
+            {
+                label: 'Folders in Google Cloud Organization',
+                value: true,
+            },
+        ],
+    },
+} as const;
+
+
+export const PROJECT_GROUP_MAPPING_OPTIONS_MAP = {
+    NESTED: true,
+    SKIP: false,
+};
