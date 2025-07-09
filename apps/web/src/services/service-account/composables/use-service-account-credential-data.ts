a0d9e93791f931a1de6e9aeb220fcf1d85aa9f0b
Yongtae Park <samuel.park@megazone.com>
2025-07-09 12:55:08 +0900

    feat(service-account): create contextual derived query composables
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/services/service-account/composables/use-service-account-credential-data.ts
@@ -0,0 +1,34 @@
+import type { ComputedRef } from 'vue';
+import { computed } from 'vue';
+
+import { useServiceAccountSecretQuery } from './queries/use-service-account-secret-query';
+import { useServiceAccountTrustedSecretQuery } from './queries/use-service-account-trusted-secret-query';
+
+interface UseServiceAccountCredentialDataOptions {
+    secretId: ComputedRef<string|undefined>;
+    isTrustedAccount: ComputedRef<boolean>;
+}
+
+export const useServiceAccountCredentialData = ({ secretId, isTrustedAccount }: UseServiceAccountCredentialDataOptions) => {
+    const _trustedSecretId = computed(() => (isTrustedAccount.value ? secretId.value : undefined));
+    const _secretId = computed(() => (isTrustedAccount.value ? undefined : secretId.value));
+
+    const { data: secretData, isFetching: isSecretFetching } = useServiceAccountSecretQuery({
+        params: computed(() => ({
+            secret_id: _secretId.value ?? '',
+        })),
+        enabled: computed(() => !isTrustedAccount.value && !!_secretId.value),
+    });
+
+    const { data: trustedSecretData, isFetching: isTrustedSecretFetching } = useServiceAccountTrustedSecretQuery({
+        params: computed(() => ({
+            trusted_secret_id: _trustedSecretId.value ?? '',
+        })),
+        enabled: computed(() => isTrustedAccount.value && !!_trustedSecretId.value),
+    });
+
+    return {
+        credentialData: computed(() => (isTrustedAccount.value ? trustedSecretData.value : secretData.value)),
+        isLoading: computed(() => (isTrustedAccount.value ? isTrustedSecretFetching.value : isSecretFetching.value)),
+    };
+};
