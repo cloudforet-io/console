da3e997ece35e31ad19b26cf97f5e7cf2568d3ff
Piggy Park (박용태) <samuel.park@megazone.com>
2025-07-09 23:50:54 +0900

    feat(reference-data-model): refactor single pipeline for reference repository (#5995)
    * feat(reference-data-model): refactor single pipeline for reference repository
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    * chore: fix wrong import
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>
    
    ---------
    
    Signed-off-by: samuel.park <samuel.park@megazone.com>

---
+++ apps/web/src/query/resource-query/reference-model/core/repository-registry.ts
@@ -0,0 +1,35 @@
+
+import type { QueryClient } from '@tanstack/query-core';
+
+import type { Query } from '@cloudforet/core-lib/space-connector/type';
+
+import type { ListResponse } from '@/api-clients/_common/schema/api-verbs/list';
+import type { QueryKeyArray } from '@/query/core/query-key/types/query-key-type';
+import ReferenceRepository from '@/query/resource-query/reference-model/core/reference-repository';
+import type { ResourceKeyType } from '@/query/resource-query/shared/types/resource-type';
+
+const repositoryRegistry = new Map<ResourceKeyType, ReferenceRepository<any>>();
+
+
+export const getRepository = <T extends Record<string, any>>(
+    resourceKey: ResourceKeyType,
+    queryClient: QueryClient,
+    queryKey: QueryKeyArray,
+    listFetcher: (params: any) => Promise<ListResponse<T>>,
+    listQuery?: Query,
+): ReferenceRepository<T> => {
+    if (!repositoryRegistry.has(resourceKey)) {
+        if (import.meta.env.DEV) {
+            console.log(`Creating new Reference Repository for: [${resourceKey}] Resource`);
+        }
+        const repository = new ReferenceRepository<T>(
+            resourceKey,
+            queryClient,
+            queryKey,
+            listFetcher,
+            listQuery,
+        );
+        repositoryRegistry.set(resourceKey, repository);
+    }
+    return repositoryRegistry.get(resourceKey) as ReferenceRepository<T>;
+};
