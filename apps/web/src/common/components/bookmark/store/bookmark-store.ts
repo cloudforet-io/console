99f4f8c6bbea291aad2f1c8476465d06b0c84631
nayeongkim <nayeongkim@megazone.com>
2025-07-11 17:45:55 +0900

    chore: store code cleanup
    Signed-off-by: NaYeong,Kim <nayeongkim@megazone.com>

--- apps/web/src/common/components/bookmark/store/bookmark-store.ts
+++ apps/web/src/common/components/bookmark/store/bookmark-store.ts
@@ -1,15 +1,9 @@
-import { computed, reactive } from 'vue';
+import { reactive } from 'vue';
 import type { TranslateResult } from 'vue-i18n';
 
 import { defineStore } from 'pinia';
 
-import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
-
-import { useAppContextStore } from '@/store/app-context/app-context-store';
-import { useUserWorkspaceStore } from '@/store/app-context/workspace/user-workspace-store';
-
 import type { BookmarkItem, BookmarkModalStateType, BookmarkModalType } from '@/common/components/bookmark/type/type';
-import ErrorHandler from '@/common/composables/error/errorHandler';
 
 import { BOOKMARK_TYPE } from '@/services/workspace-home/constants/workspace-home-constant';
 import type { BookmarkType } from '@/services/workspace-home/types/workspace-home-type';
@@ -23,16 +17,6 @@ interface BookmarkState {
 }
 
 export const useBookmarkStore = defineStore('bookmark', () => {
-    const userWorkspaceStore = useUserWorkspaceStore();
-    const userWorkspaceStoreGetters = userWorkspaceStore.getters;
-    const appContextStore = useAppContextStore();
-    const appContextGetters = appContextStore.getters;
-
-    const _getters = reactive({
-        isAdminMode: computed(() => appContextGetters.isAdminMode),
-        currentWorkspaceId: computed<string|undefined>(() => userWorkspaceStoreGetters.currentWorkspaceId),
-    });
-
     const state = reactive<BookmarkState>({
         modal: {
             isNew: undefined,
@@ -81,31 +65,6 @@ export const useBookmarkStore = defineStore('bookmark', () => {
             state.bookmarkType = BOOKMARK_TYPE.WORKSPACE;
             state.selectedBookmark = undefined;
             state.selectedBookmarks = [];
-        },
-        updateBookmarkLink: async ({
-            id, name, link, folder,
-        }: { id: string, name?: string|TranslateResult, link?: string, folder?: string}) => {
-            try {
-                let fetcher;
-                if (state.bookmarkType === BOOKMARK_TYPE.USER) {
-                    fetcher = SpaceConnector.clientV2.config.userConfig.update;
-                } else if (_getters.isAdminMode || state.bookmarkType === BOOKMARK_TYPE.WORKSPACE) {
-                    fetcher = SpaceConnector.clientV2.config.sharedConfig.update;
-                }
-                await fetcher({
-                    name: id,
-                    data: {
-                        workspaceId: _getters.currentWorkspaceId,
-                        name,
-                        folder,
-                        link,
-                        isGlobal: _getters.isAdminMode,
-                    },
-                });
-            } catch (e) {
-                ErrorHandler.handleError(e);
-                throw e;
-            }
         },
     };
 
