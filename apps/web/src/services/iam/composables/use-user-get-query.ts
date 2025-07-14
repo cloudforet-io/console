e3cafbcf2caf9eee91d4c391589d0e33b3542eb7
이승연 <sylee1274@mz.co.kr>
2025-07-14 18:08:21 +0900

    refactor: improve reactivity  of showing tab detail(s) info of user
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>

---
+++ apps/web/src/services/iam/composables/use-user-get-query.ts
@@ -0,0 +1,50 @@
+import type { Ref } from 'vue';
+import { computed } from 'vue';
+
+import { useUserApi } from '@/api-clients/identity/user/composables/use-user-api';
+import { useWorkspaceUserApi } from '@/api-clients/identity/workspace-user/composables/use-workspace-user-api';
+import { useServiceQueryKey } from '@/query/core/query-key/use-service-query-key';
+import { useScopedQuery } from '@/query/service-query/use-scoped-query';
+
+import { useUserPageStore } from '@/services/iam/store/user-page-store';
+
+export const useUserGetQuery = (userId: Ref<string>) => {
+    const { userAPI } = useUserApi();
+    const { workspaceUserAPI } = useWorkspaceUserApi();
+
+    const userPageStore = useUserPageStore();
+    const isAdminMode = computed<boolean>(() => userPageStore.state.isAdminMode);
+
+    const { key: userQueryKey, params: userQueryParams } = useServiceQueryKey('identity', 'user', 'get', {
+        params: computed(() => ({
+            user_id: userId.value,
+        })),
+    });
+
+    const { key: workspaceUserQueryKey, params: workspaceUserQueryParams } = useServiceQueryKey('identity', 'workspace-user', 'get', {
+        params: computed(() => ({
+            user_id: userId.value,
+        })),
+    });
+
+    const { data: userData } = useScopedQuery({
+        queryKey: userQueryKey,
+        queryFn: () => userAPI.get(userQueryParams.value),
+        gcTime: 1000 * 60 * 2,
+        staleTime: 1000 * 30,
+        enabled: computed(() => isAdminMode.value),
+    }, ['DOMAIN']);
+
+    const { data: workspaceUserData } = useScopedQuery({
+        queryKey: workspaceUserQueryKey,
+        queryFn: () => workspaceUserAPI.get(workspaceUserQueryParams.value),
+        gcTime: 1000 * 60 * 2,
+        staleTime: 1000 * 30,
+        enabled: computed(() => !isAdminMode.value),
+    }, ['WORKSPACE']);
+
+    return {
+        userData,
+        workspaceUserData,
+    };
+};
