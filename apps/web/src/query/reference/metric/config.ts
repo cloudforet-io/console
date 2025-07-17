17dfff551fe259d3a4ceea43bebd7180ee548e7c
Yongtae Park <samuel.park@megazone.com>
2025-07-17 15:59:12 +0900

    chore: remove deprecated query
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/query/reference/metric/config.ts
+++
@@ -1,8 +0,0 @@
-import type { ReferenceConfig } from '@/query/reference/types/reference-type';
-
-export const metricConfig: ReferenceConfig = {
-    name: 'Metric',
-    resourceKey: 'metric',
-    idKey: 'metric_id',
-    nameKey: 'name',
-};
