62b017c58f7b5636e12436732fb5111ca5a92ae3
yuda <yuda@megazone.com>
2025-07-17 13:10:28 +0900

    chore: add `totalCount` to `useUserProfileGetWorkspacesQuery`
    Signed-off-by: yuda <yuda@megazone.com>

--- apps/web/src/services/landing/composables/use-user-profile-get-workspaces-query.ts
+++ apps/web/src/services/landing/composables/use-user-profile-get-workspaces-query.ts
@@ -1,16 +1,21 @@
+import { computed, type ComputedRef } from 'vue';
+
 import { useUserProfileApi } from '@/api-clients/identity/user-profile/composables/use-user-profile-api';
+import type { UserProfileGetWorkspacesParameters } from '@/api-clients/identity/user-profile/schema/api-verbs/get-workspaces';
 import { useServiceQueryKey } from '@/query/core/query-key/use-service-query-key';
 import { useScopedQuery } from '@/query/service-query/use-scoped-query';
 
 
-export const useUserProfileGetWorkspacesQuery = () => {
+export const useUserProfileGetWorkspacesQuery = (params?: ComputedRef<UserProfileGetWorkspacesParameters|undefined>) => {
     const { userProfileAPI } = useUserProfileApi();
 
-    const { key: userProfileGetWorkspacesQueryKey } = useServiceQueryKey('identity', 'user-profile', 'get-workspaces');
+    const { key: userProfileGetWorkspacesQueryKey, params: queryParams } = useServiceQueryKey('identity', 'user-profile', 'get-workspaces', {
+        params,
+    });
 
     const { data, isLoading } = useScopedQuery({
         queryKey: userProfileGetWorkspacesQueryKey,
-        queryFn: () => userProfileAPI.getWorkspaces(),
+        queryFn: () => userProfileAPI.getWorkspaces(queryParams.value),
         select: (d) => d.results?.filter((workspace) => workspace.state === 'ENABLED') || [],
         staleTime: 1000 * 60 * 5, // 5 minutes
         gcTime: 1000 * 60 * 1, // 1 minute
@@ -20,6 +25,7 @@ export const useUserProfileGetWorkspacesQuery = () => {
     return {
         data,
         isLoading,
+        totalCount: computed(() => data.value?.length ?? 0),
         key: userProfileGetWorkspacesQueryKey,
     };
 };
