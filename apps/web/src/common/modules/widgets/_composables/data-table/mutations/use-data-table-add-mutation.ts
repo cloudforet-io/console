6c43a25eb262860718f02fd0614dbbcdf988964e
Yongtae Park <samuel.park@megazone.com>
2025-07-18 23:17:44 +0900

    feat(data-table): add data-table/widget mutations and invalidation composable
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/common/modules/widgets/_composables/data-table/mutations/use-data-table-add-mutation.ts
+++ apps/web/src/common/modules/widgets/_composables/data-table/mutations/use-data-table-add-mutation.ts
@@ -1,9 +1,6 @@
-import type { ComputedRef } from 'vue';
-import { computed } from 'vue';
-
 import { useMutation, useQueryClient } from '@tanstack/vue-query';
 
-// import type { ListResponse } from '@/api-clients/_common/schema/api-verbs/list';
+import type { ListResponse } from '@/api-clients/_common/schema/api-verbs/list';
 import { usePrivateDataTableApi } from '@/api-clients/dashboard/private-data-table/composables/use-private-data-table-api';
 import { usePublicDataTableApi } from '@/api-clients/dashboard/public-data-table/composables/use-public-data-table-api';
 import type { DataTableAddParameters } from '@/api-clients/dashboard/public-data-table/schema/api-verbs/add';
@@ -12,7 +9,6 @@ import { useServiceQueryKey } from '@/query/core/query-key/use-service-query-key
 import type { DataTableModel } from '@/common/modules/widgets/types/widget-data-table-type';
 
 interface UseDataTableAddMutationOptions {
-    widgetId: ComputedRef<string|undefined>;
     onSuccess?: (data: DataTableModel, variables: DataTableAddParameters) => Promise<void> | void;
     onError?: (error: Error, variables: DataTableAddParameters) => Promise<void> | void;
     onSettled?: (data: DataTableModel|undefined, error: Error | null, variables: DataTableAddParameters) => Promise<void> | void;
@@ -20,7 +16,6 @@ interface UseDataTableAddMutationOptions {
 
 
 export const useDataTableAddMutation = ({
-    widgetId,
     onSuccess,
     onError,
     onSettled,
@@ -30,12 +25,8 @@ export const useDataTableAddMutation = ({
     const { privateDataTableAPI } = usePrivateDataTableApi();
 
 
-    const { key: publicDataTableListQueryKey } = useServiceQueryKey('dashboard', 'public-data-table', 'list', {
-        contextKey: computed(() => widgetId.value),
-    });
-    const { key: privateDataTableListQueryKey } = useServiceQueryKey('dashboard', 'private-data-table', 'list', {
-        contextKey: computed(() => widgetId.value),
-    });
+    const { withSuffix: publicDataTableListQueryKey } = useServiceQueryKey('dashboard', 'public-data-table', 'list');
+    const { withSuffix: privateDataTableListQueryKey } = useServiceQueryKey('dashboard', 'private-data-table', 'list');
 
     const dataTableAddFn = (params: DataTableAddParameters): Promise<DataTableModel> => {
         if (params.widget_id.startsWith('private')) {
@@ -46,22 +37,33 @@ export const useDataTableAddMutation = ({
 
     return useMutation({
         mutationFn: dataTableAddFn,
-        onSuccess: (data, variables) => {
-            const _isPrivate = widgetId.value?.startsWith('private');
-            const dataTableListQueryKey = _isPrivate ? privateDataTableListQueryKey : publicDataTableListQueryKey;
-            queryClient.invalidateQueries({ queryKey: dataTableListQueryKey });
-            // queryClient.setQueryData(dataTableListQueryKey.value, (oldData: ListResponse<DataTableModel>) => (oldData?.results?.length ? {
-            //     ...oldData, results: [...oldData.results, data],
-            // } : {
-            //     ...(oldData ?? {}), results: [data],
-            // }));
-            if (onSuccess) onSuccess(data, variables);
+        onSuccess: async (data, variables) => {
+            const _isPrivate = variables.widget_id.startsWith('private');
+            const dataTableListQueryKey = _isPrivate ? privateDataTableListQueryKey(variables.widget_id) : publicDataTableListQueryKey(variables.widget_id);
+            const currentData = queryClient.getQueryData<ListResponse<DataTableModel>>(dataTableListQueryKey);
+            if (!currentData) {
+                await queryClient.invalidateQueries({ queryKey: dataTableListQueryKey });
+            } else {
+                await queryClient.setQueryData(dataTableListQueryKey, (oldData: ListResponse<DataTableModel>) => {
+                    if (oldData?.results) {
+                        return {
+                            ...oldData,
+                            results: [...oldData.results, data],
+                        };
+                    }
+                    return {
+                        ...(oldData ?? {}),
+                        results: [data],
+                    };
+                });
+            }
+            if (onSuccess) await onSuccess(data, variables);
         },
-        onError: (error, variables) => {
-            if (onError) onError(error, variables);
+        onError: async (error, variables) => {
+            if (onError) await onError(error, variables);
         },
-        onSettled: (data, error, variables) => {
-            if (onSettled) onSettled(data, error, variables);
+        onSettled: async (data, error, variables) => {
+            if (onSettled) await onSettled(data, error, variables);
         },
     });
 };
