2c9f2cbbf9b6c8dfd57261f204b22289e9985c81
sulmo <sulmo@megazone.com>
2025-07-22 18:39:24 +0900

    refactor(workspace-tab): apply vueQuery for workspace list pagination and update state management
    Signed-off-by: sulmo <sulmo@megazone.com>

--- apps/web/src/services/advanced/composables/querys/use-workspace-group-workspace-list-pagination-query.ts
+++ apps/web/src/services/advanced/composables/querys/use-workspace-group-workspace-list-pagination-query.ts
@@ -1,20 +1,35 @@
 import type { ComputedRef } from 'vue';
 import { computed } from 'vue';
 
-
 import { useWorkspaceApi } from '@/api-clients/identity/workspace/composables/use-workspace-api';
 import type { WorkspaceListParameters } from '@/api-clients/identity/workspace/schema/api-verbs/list';
 import type { WorkspaceModel } from '@/api-clients/identity/workspace/schema/model';
 import { useServiceQueryKey } from '@/query/core/query-key/use-service-query-key';
 import { useScopedPaginationQuery } from '@/query/service-query/pagination/use-scoped-pagination-query';
 
-interface UseWorkspaceListPaginationQueryOptions {
+import ErrorHandler from '@/common/composables/error/errorHandler';
+
+interface UseWorkspaceGroupWorkspaceListPaginationQueryOptions {
     params: ComputedRef<WorkspaceListParameters>;
     thisPage: ComputedRef<number>;
     pageSize: ComputedRef<number>;
+    enabled?: ComputedRef<boolean>;
 }
 
-export const useWorkspaceListPaginationQuery = ({ params, thisPage, pageSize }: UseWorkspaceListPaginationQueryOptions) => {
+export const useWorkspaceGroupWorkspaceListPaginationQuery = ({
+    params, thisPage, pageSize, enabled,
+}: UseWorkspaceGroupWorkspaceListPaginationQueryOptions) => {
+    // NOTE: This query is not available if workspace_group_id is not set
+    if (!params.value.query?.filter?.find((filter) => filter.k === 'workspace_group_id')) {
+        ErrorHandler.handleError(new Error('workspace_group_id is required'));
+        return {
+            data: computed<WorkspaceModel[]>(() => []),
+            totalCount: computed(() => 0),
+            isLoading: computed(() => false),
+            refresh: async () => {},
+        };
+    }
+
     const { workspaceAPI } = useWorkspaceApi();
 
 
@@ -31,7 +46,10 @@ export const useWorkspaceListPaginationQuery = ({ params, thisPage, pageSize }: 
         params: workspaceListPaginationQueryParams,
         gcTime: 1000 * 60 * 2,
         staleTime: 1000 * 30,
-        enabled: true,
+        enabled: computed(() => {
+            if (enabled === undefined) return true;
+            return enabled.value;
+        }),
     }, {
         thisPage,
         pageSize,
