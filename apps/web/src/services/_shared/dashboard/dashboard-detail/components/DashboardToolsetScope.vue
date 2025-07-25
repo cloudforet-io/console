a702f3a749c29000e7051593d302a9ed0db24f7c
Yongtae Park <samuel.park@megazone.com>
2025-07-25 16:10:31 +0900

    feat(dashboard-toolset): apply vue query
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/services/_shared/dashboard/dashboard-detail/components/DashboardToolsetScope.vue
+++ apps/web/src/services/_shared/dashboard/dashboard-detail/components/DashboardToolsetScope.vue
@@ -1,78 +1,52 @@
 <script lang="ts" setup="">
 import {
-    computed, reactive, watch, onUnmounted,
+    reactive, watch, onUnmounted,
 } from 'vue';
 
 import { isEqual } from 'lodash';
 
-import { SpaceConnector } from '@cloudforet/core-lib/space-connector';
 import {
     PSelectDropdown, PCheckbox, PButtonModal, PStatus, PScopedNotification,
 } from '@cloudforet/mirinae';
-import type { SelectDropdownMenuItem, AutocompleteHandler } from '@cloudforet/mirinae/types/controls/dropdown/select-dropdown/type';
+import type { SelectDropdownMenuItem } from '@cloudforet/mirinae/types/controls/dropdown/select-dropdown/type';
 
-import type { ListResponse } from '@/api-clients/_common/schema/api-verbs/list';
-import type { WorkspaceListParameters } from '@/api-clients/identity/workspace/schema/api-verbs/list';
 import type { WorkspaceModel } from '@/api-clients/identity/workspace/schema/model';
+import { useResourceMenuHandlerMap } from '@/query/resource-query/resource-menu-handler';
 
-import { useAllReferenceStore } from '@/store/reference/all-reference-store';
-import type { WorkspaceReferenceMap } from '@/store/reference/workspace-reference-store';
-
-import ErrorHandler from '@/common/composables/error/errorHandler';
 import WorkspaceLogoIcon from '@/common/modules/navigations/top-bar/modules/top-bar-header/WorkspaceLogoIcon.vue';
 
+import { useAdminDashboardInitialWorkspaceFilter } from '@/services/_shared/dashboard/dashboard-detail/composables/use-admin-dashboard-initial-workspace-filter';
 import { useDashboardDetailInfoStore } from '@/services/_shared/dashboard/dashboard-detail/stores/dashboard-detail-info-store';
 import { workspaceStateFormatter } from '@/services/advanced/composables/refined-table-data';
 import { WORKSPACE_STATE } from '@/services/advanced/constants/workspace-constant';
 
 
-const allReferenceStore = useAllReferenceStore();
+
 const dashboardDetailStore = useDashboardDetailInfoStore();
 const dashboardDetailState = dashboardDetailStore.state;
-const storeState = reactive({
-    workspaces: computed<WorkspaceReferenceMap>(() => allReferenceStore.getters.workspace),
+
+const resourceMenuHandlerMap = useResourceMenuHandlerMap();
+const workspaceHandler = resourceMenuHandlerMap.workspace({
+    fixedFilters: {
+        is_dormant: false,
+    },
 });
+
 const state = reactive({
     selectedWorkspaceItems: [] as SelectDropdownMenuItem[],
     performanceDelayModalVisible: false as boolean,
 });
 
-const workspaceHandler: AutocompleteHandler = async (keyword: string, pageStart = 1, pageLimit = 10) => {
-    try {
-        const { results } = await SpaceConnector.clientV2.identity.workspace.list<WorkspaceListParameters, ListResponse<WorkspaceModel>>({
-            query: {
-                keyword,
-                filter: [
-                    { k: 'is_dormant', v: false, o: 'eq' },
-                ],
-            },
-        });
-        const refinedMenuItems = (results ?? []).map((d) => ({
-            name: d.workspace_id,
-            label: d.name,
-            is_dormant: d.is_dormant,
-            tags: d.tags,
-        }));
-        const totalCount = pageStart - 1 + Number(pageLimit);
-        const slicedResults = refinedMenuItems?.slice(pageStart - 1, totalCount);
-        return {
-            results: slicedResults,
-            more: totalCount < refinedMenuItems.length,
-        };
-    } catch (e) {
-        ErrorHandler.handleError(e);
-        return {
-            results: [],
-            more: false,
-        };
-    }
-};
-const initSelectedWorkspace = () => {
-    const _defaultWorkspace = Object.values(storeState.workspaces)[0];
-    dashboardDetailStore.setSelectedWorkspaceId(_defaultWorkspace?.key);
+/* API */
+const { data: initialWorkspace } = useAdminDashboardInitialWorkspaceFilter();
+
+const initSelectedWorkspace = (_initialWorkspace?: WorkspaceModel) => {
+    const workspace = _initialWorkspace || initialWorkspace.value;
+    if (!workspace) return;
+    dashboardDetailStore.setSelectedWorkspaceId(workspace.workspace_id);
     state.selectedWorkspaceItems = [{
-        name: _defaultWorkspace?.key,
-        label: _defaultWorkspace?.label,
+        name: workspace.workspace_id,
+        label: workspace.name,
     }];
 };
 
@@ -95,9 +69,10 @@ const handleConfirmPerformanceDelay = () => {
     dashboardDetailStore.setSelectedWorkspaceId('all');
 };
 
+
 /* Watcher */
-watch(() => storeState.workspaces, (workspaces) => {
-    if (workspaces) initSelectedWorkspace();
+watch(initialWorkspace, (workspace) => {
+    if (workspace) initSelectedWorkspace(workspace);
 }, { immediate: true });
 
 onUnmounted(() => {
@@ -115,10 +90,10 @@ onUnmounted(() => {
             <p-select-dropdown size="sm"
                                is-filterable
                                :handler="workspaceHandler"
-                               :selected="state.selectedWorkspaceItems"
                                menu-position="left"
                                show-select-marker
                                use-fixed-menu-style
+                               init-selected-with-handler
                                :disabled="dashboardDetailState.selectedWorkspaceId === 'all'"
                                :page-size="10"
                                @update:selected="handleSelectWorkspace"
@@ -127,11 +102,11 @@ onUnmounted(() => {
                     <div class="menu-item-wrapper">
                         <div class="label">
                             <workspace-logo-icon :text="item?.label || ''"
-                                                 :theme="item?.tags?.theme"
+                                                 :theme="item?.data?.tags?.theme"
                                                  size="xs"
                             />
                             <span class="label-text">{{ item.label }}</span>
-                            <p-status v-if="item?.is_dormant"
+                            <p-status v-if="item?.data?.is_dormant"
                                       v-bind="workspaceStateFormatter(WORKSPACE_STATE.DORMANT)"
                                       class="capitalize state"
                             />
