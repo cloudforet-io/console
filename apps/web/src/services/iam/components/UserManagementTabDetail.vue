b94e7f6aa1c581b9c832b60f616d6faf2d9ff6dc
Yongtae Park <samuel.park@megazone.com>
2025-07-28 15:18:59 +0900

    chore: add missing scenario
    Signed-off-by: samuel.park <samuel.park@megazone.com>

--- apps/web/src/services/iam/components/UserManagementTabDetail.vue
+++ apps/web/src/services/iam/components/UserManagementTabDetail.vue
@@ -130,12 +130,18 @@ const handleClickButton = (type: string) => {
         themeColor: 'alert',
         modalVisibleType: 'status',
     }); break;
-    case USER_MODAL_TYPE.UPDATE: userPageStore.updateModalSettings({
-        type,
-        title: i18n.t('IAM.USER.MAIN.MODAL.UPDATE_TITLE'),
-        themeColor: 'primary',
-        modalVisibleType: 'form',
-    }); break;
+    case USER_MODAL_TYPE.UPDATE:
+        userPageStore.updateModalSettings({
+            type,
+            title: i18n.t('IAM.USER.MAIN.MODAL.UPDATE_TITLE'),
+            themeColor: 'primary',
+            modalVisibleType: 'form',
+        });
+        if (userPageGetters.selectedUsers.length) { // NOTE: temporarily setting before vue query is ready
+            const selectedUser = userPageGetters.selectedUsers[0];
+            userPageStore.setSelectedUserForForm(selectedUser);
+        }
+        break;
     default: break;
     }
 };
