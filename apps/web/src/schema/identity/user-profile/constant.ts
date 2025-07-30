128036ca964f29944b0d7dc42abb2bda5eaeb9e1
yuda <yuda@megazone.com>
2025-07-30 10:31:15 +0900

    feat: mfa constants, types, etc
    Signed-off-by: yuda <yuda@megazone.com>

---
+++ apps/web/src/schema/identity/user-profile/constant.ts
@@ -0,0 +1,9 @@
+export const MULTI_FACTOR_AUTH_TYPE = {
+    OTP: 'OTP',
+    EMAIL: 'EMAIL',
+} as const;
+
+export const MFA_STATE = {
+    ENABLED: 'ENABLED',
+    DISABLED: 'DISABLED',
+} as const;
