674d38c83d7145e85bf4c2cf2979baaf32a998fc
이승연 <165753731+seungyeoneeee@users.noreply.github.com>
2025-08-01 15:33:19 +0900

    fix: apply select card not displaying marker as disabled status (#6079)
    * fix: apply select card not displaying marker as disabled status
    
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>
    
    * chore: fix wrong type of language
    
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>
    
    * fix: use optional chaining for MFA type (OTP/Email) handling
    
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>
    
    * chore: add updated disabled status at storybook
    
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>
    
    * chore: update languages
    
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>
    
    ---------
    
    Signed-off-by: 이승연 <sylee1274@mz.co.kr>

--- apps/web/src/services/auth/authenticator/local/template/ID_PW.vue
+++ apps/web/src/services/auth/authenticator/local/template/ID_PW.vue
@@ -103,26 +103,15 @@ const signIn = async () => {
         if (e.message.includes('MFA')) {
             const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
             const mfaTypeRegex = /mfa_type\s*=\s*(\w+)/;
-            if (e.message.includes('EMAIL')) {
-                await router.push({
-                    name: AUTH_ROUTE.SIGN_IN.MULTI_FACTOR_AUTH._NAME,
-                    params: {
-                        password: credentials.password,
-                        mfaEmail: e.message.match(emailRegex)[0],
-                        mfaType: e.message.match(mfaTypeRegex)[1],
-                        userId: state.userId?.trim() as string,
-                    },
-                });
-            } else {
-                await router.push({
-                    name: AUTH_ROUTE.SIGN_IN.MULTI_FACTOR_AUTH._NAME,
-                    params: {
-                        password: credentials.password,
-                        mfaType: e.message.match(mfaTypeRegex)[1],
-                        userId: state.userId?.trim() as string,
-                    },
-                });
-            }
+            await router.push({
+                name: AUTH_ROUTE.SIGN_IN.MULTI_FACTOR_AUTH._NAME,
+                params: {
+                    password: credentials.password,
+                    mfaEmail: e.message.match(emailRegex)?.[0],
+                    mfaType: e.message.match(mfaTypeRegex)?.[1],
+                    userId: state.userId?.trim() as string,
+                },
+            });
         } else {
             ErrorHandler.handleError(e);
             await store.dispatch('display/showSignInErrorMessage');
